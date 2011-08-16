3709f3d15d01e7effe6eb50ef82ec3c39a7fbc0c
Milen Georgiev <milen@ep-bags.com>
2011-08-16 13:00:43 +0300

    Начало на модула fin - финансови документи и операции

---
+++ dma/DaAccRegIntf.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Интерфейс за пера - дълготрайни активи
+ *
+ * @category   bgERP 2.0
+ * @package    dma
+ * @title:     Дълготрайни активи
+ * @author     Milen Georgiev <milen@download.bg>
+ * @copyright  2006-2011 Experta Ltd.
+ * @license    GPL 2
+ * @since      v 0.1
+ */
+class dma_DaAccRegIntf extends acc_RegisterIntf
+{    
+}
