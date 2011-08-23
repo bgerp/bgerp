9e8f0e416f16ce43598daa3ed6f13d1e51ffadd5
Yusein Yuseinov <yyuseinov@gmail.com>
2011-08-23 17:04:12 +0300

    Добавена е възможност за прекъсване на fromVerbal и toVerval.

--- core/Type.class.php
+++ core/Type.class.php
@@ -31,7 +31,7 @@ class core_Type extends core_BaseClass
      * Премахваме HTML елементите при визуализацията на всички типове,
      * които не пре-дефинират тази функция
      */
-    function toVerbal($value)
+    function toVerbal_($value)
     {
         if ($value === NULL)
         return NULL;
@@ -74,7 +74,7 @@ class core_Type extends core_BaseClass
      * Този метод трябва да конвертира от вербално към вътрешно
      * представяне дадената стойност
      */
-    function fromVerbal($verbalValue)
+    function fromVerbal_($verbalValue)
     {
         return $verbalValue;
     }
