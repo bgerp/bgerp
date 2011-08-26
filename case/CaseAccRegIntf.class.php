e943c7802f85a17fe88d47182f9f41e58cf7d01a
Tsvetan Michailov <tsvetan_michailov@yahoo.com>
2011-08-26 16:08:28 +0300

    'case' - добавен интерфейс към номенклатурите

---
+++ case/CaseAccRegIntf.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Интерфейс за пера - каси
+ *
+ * @category   bgERP 2.0
+ * @package    dma
+ * @title:     Каси
+ * @author     Milen Georgiev <milen@download.bg>
+ * @copyright  2006-2011 Experta Ltd.
+ * @license    GPL 2
+ * @since      v 0.1
+ */
+class case_CaseAccRegIntf extends acc_RegisterIntf
+{    
+
+}
