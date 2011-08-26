b892c0af24e932a06a62175ed81e970306527e40
Tsvetan Michailov <tsvetan_michailov@yahoo.com>
2011-08-26 16:04:11 +0300

    'bank/Currencies' - валутите са копирани от 'common' и им е добавен интерфейс за работа с номенклатурите

--- bank/CurrenciesAccRegIntf.class.php
+++ bank/CurrenciesAccRegIntf.class.php
@@ -4,13 +4,14 @@
  * Интерфейс за пера - валути
  *
  * @category   bgERP 2.0
- * @package    cat
+ * @package    dma
  * @title:     Валути
  * @author     Milen Georgiev <milen@download.bg>
  * @copyright  2006-2011 Experta Ltd.
  * @license    GPL 2
  * @since      v 0.1
  */
-class fin_CurrencyAccRegIntf extends acc_RegisterIntf
+class bank_CurrenciesAccRegIntf extends acc_RegisterIntf
 {    
+
 }
