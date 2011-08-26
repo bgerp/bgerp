bed0f800ae60113299648c6fd6aff63a23ceac14
Tsvetan Michailov <tsvetan_michailov@yahoo.com>
2011-08-26 16:09:43 +0300

    'bank'

--- bank/Wrapper.class.php
+++ bank/Wrapper.class.php
@@ -12,11 +12,22 @@ class bank_Wrapper extends core_Plugin
     {
         $tabs = cls::get('core_Tabs', array('htmlClass' => 'bank') );
         
-        $tabs->TAB('bank_BankAccounts',     'Банкови сметки');
-        $tabs->TAB('bank_BankAccountTypes', 'Типове банкови сметки');
-        $tabs->TAB('bank_BankOwnAccounts',  'Банкови сметки на фирмата');
-        $tabs->TAB('bank_BankDocuments',    'Банкови документи');
-       
+        // проверка за избрана с-ка
+        $selectedAccountId = Mode::get('selectedAccountId');
+        
+        if ($selectedAccountId) {
+	        $tabs->TAB('bank_BankAccounts',     'Банкови сметки');
+	        $tabs->TAB('bank_BankAccountTypes', 'Типове банкови сметки');
+	        $tabs->TAB('bank_BankOwnAccounts',  'Банкови сметки на фирмата');
+	        $tabs->TAB('bank_BankDocuments',    'Банкови документи');        	
+        } else {
+            $tabs->TAB('bank_BankOwnAccounts',  'Банкови сметки на фирмата');
+        }
+
+        $tabs->TAB('bank_Currencies',     'Валути');
+        $tabs->TAB('bank_CurrencyGroups', 'Групи валути');
+        $tabs->TAB('bank_CurrencyRates',  'Валутни курсове');
+        
         $tpl = $tabs->renderHtml($tpl, $invoker->tabName ? $invoker->tabName : $invoker->className);
         
         $tpl->append(tr($invoker->title) . " » ", 'PAGE_TITLE');
