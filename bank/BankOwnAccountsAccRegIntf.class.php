0cadc7c53ca6be1137261bd37055b6541dc49cda
Tsvetan Michailov <tsvetan_michailov@yahoo.com>
2011-08-26 16:09:15 +0300

    'bank/BankOwnAccounts' - добавен интерфейс към номенклатурите

---
+++ bank/BankOwnAccountsAccRegIntf.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Интерфейс за пера - Банкови сметки на фирмата
+ *
+ * @category   bgERP 2.0
+ * @package    dma
+ * @title:     Банкови сметки на фирмата
+ * @author     Milen Georgiev <milen@download.bg>
+ * @copyright  2006-2011 Experta Ltd.
+ * @license    GPL 2
+ * @since      v 0.1
+ */
+class bank_BankOwnAccountsAccRegIntf extends acc_RegisterIntf
+{    
+
+}
