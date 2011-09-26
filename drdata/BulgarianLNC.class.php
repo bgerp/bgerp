5ac0be1c71b483f9fa04b6afc17a20286903e8a8
Yusein Yuseinov <yyuseinov@gmail.com>
2011-09-26 11:55:21 +0300

    Добавен е клас за проверка на ЛНЧ номер

---
+++ drdata/BulgarianLNC.class.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Клас 'drdata_BulgarianLNC' - Проверка за валиден личен номер на чужденец
+ *
+ * @category   Experta Framework
+ * @package    drdata
+ * @author     Yusein Yuseinov
+ * @copyright  2006-2011 Experta OOD
+ * @license    GPL 2
+ * @since      v 0.1
+ */
+class drdata_BulgarianLNC
+{
+	var $weights = array(21,19,17,13,11,9,7,3,1,0);
+	
+	/**
+	 * Проверява за валиден номер на чужденец 
+	 */
+	function isLnc($value)
+	{
+		if (!isset($value)) {
+			
+			return FALSE;
+		}
+		
+		if (!preg_match('/^[0-9]{10}$/', $value)) {
+            
+			return FALSE;
+        }
+		
+		$valArr = str_split($value);
+		$sum = 0;
+		foreach ($this->weights as $key => $weight) {
+			$sum += $valArr[$key] * $weight;
+		}
+		
+		$rest = $sum % 10;
+		
+		if ($rest == $valArr[9]) {
+			
+			return TRUE;
+		}
+		
+		return "<br />Не е валидно ЛНЧ.";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
