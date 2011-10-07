1f477038e9da55634c3dd98cba0c4bf17d6f2b75
Yusein Yuseinov <yyuseinov@gmail.com>
2011-10-07 17:58:48 +0300

    Модул RIP. За създаване на клишета.

---
+++ rip/Setup.class.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ *  Задания за клишета - инсталиране / деинсталиране
+ *
+ * @category   BGERP
+ * @package    rip
+ * @author     Yusein Yuseinov <yyuseinov@gmail.com>
+ * @copyright  2006-2011 Experta OOD
+ * @license    GPL 2
+ */
+class rip_Setup
+{
+    /**
+     *  @todo Чака за документация...
+     */
+    var $version = '0.1';
+    
+    
+    /**
+     *  @todo Чака за документация...
+     */
+    var $startCtr = 'rip_Directory';
+    
+    
+    /**
+     *  @todo Чака за документация...
+     */
+    var $startAct = 'default';
+    
+  
+   /**
+     * Описание на модула
+     */
+    var $info = "Задания за клишета";
+
+
+    /**
+     *  Инсталиране на пакета
+     */
+    function install()
+    {
+        $managers = array(
+        	'rip_Directory',
+        	'rip_Files',
+        );
+        
+        // Роля за power-user на този модул
+        $role = 'rip';
+        $html = core_Roles::addRole($role) ? "<li style='color:green'>Добавена е роля <b>$role</b></li>" : '';
+        
+        $instances = array();
+        
+        foreach ($managers as $manager) {
+            $instances[$manager] = &cls::get($manager);
+            $html .= $instances[$manager]->setupMVC();
+        }
+
+        $Menu = cls::get('bgerp_Menu');
+        
+        $html .= $Menu->addItem(2, 'Задания', 'Клишета', 'rip_Directory', 'default', "{$role}, admin");
+        
+        $Bucket = cls::get('fileman_Buckets');
+        $html .= $Bucket->createBucket('Rip', 'Файлове за клишета', NULL, '104857600', 'every_one', 'every_one');
+        
+        return $html;
+    }
+    
+    
+    /**
+     *  Де-инсталиране на пакета
+     */
+    function deinstall()
+    {
+        return "";
+    }
+}
