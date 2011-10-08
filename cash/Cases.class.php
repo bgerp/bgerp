cf4c4c457e9d30a5751ac8e376151a73f9c82943
Milen Georgiev <milen@ep-bags.com>
2011-10-08 11:25:53 +0300

    Поправено https://github.com/bgerp/bgerp/issues/78 . Вече визуализира името на касата.

--- cash/Cases.class.php
+++ cash/Cases.class.php
@@ -19,7 +19,7 @@ class cash_Cases extends core_Manager {
     /**
      *  @todo Чака за документация...
      */
-    var $listFields = 'id, tools=Пулт, title, location, cashier';
+    var $listFields = 'id, tools=Пулт, name, location, cashier';
         
     
     /**
@@ -39,7 +39,7 @@ class cash_Cases extends core_Manager {
      */
     function description()
     {
-        $this->FLD('title',    'varchar(255)', 'caption=Наименование');
+        $this->FLD('name',     'varchar(255)', 'caption=Наименование,oldFiled=Title');
         $this->FLD('location', 'key(mvc=common_Locations, select=title)', 'caption=Локация');
         $this->FLD('cashier',  'user(roles=cash|admin)', 'caption=Касиер');
     }
