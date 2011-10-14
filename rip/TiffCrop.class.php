2e6ee4eaa74291315b4b0fd135e8a721988b51b9
Yusein Yuseinov <yyuseinov@gmail.com>
2011-10-14 13:32:04 +0300

    rip: Начало на работа с интерфейси.

---
+++ rip/TiffCrop.class.php
@@ -0,0 +1,30 @@
+<?php
+
+
+/**
+ * Кропване на tiff файлове
+ */
+class rip_TiffCrop
+{
+	
+	
+	var $interfaces = 'rip_FileProcessingIntf'; 
+	
+	
+	/**
+	 * 
+	 */
+	function processFile($fileId)
+	{
+		//$filePath = $this->getPath()
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
