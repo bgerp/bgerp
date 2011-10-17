b7cb1f82eed5c0393a85d5edaffafaa1bd4c416e
Milen Georgiev <milen@ep-bags.com>
2011-10-17 11:39:02 +0300

    Модул Sales

---
+++ sales/Setup.class.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ *  Покупки - инсталиране / деинсталиране
+ *
+ * @category   BGERP
+ * @package    sales
+ * @author     Милен Георгиев
+ * @copyright  2006-2011 Experta OOD
+ * @license    GPL 2
+ */
+class sales_Setup
+{
+    /**
+     *  @todo Чака за документация...
+     */
+    var $version = '0.1';
+    
+    
+    /**
+     *  @todo Чака за документация...
+     */
+    var $startCtr = 'sales_Deals';
+    
+    
+    /**
+     *  @todo Чака за документация...
+     */
+    var $startAct = 'default';
+    
+
+    /**
+     * Описание на модула
+     */
+    var $info = "Продажби на продукти и стоки";
+
+    
+    /**
+     *  Инсталиране на пакета
+     */
+    function install()
+    {
+        $managers = array(
+        	'sales_Deals',
+        );
+        
+        // Роля за power-user на този модул
+        $role = 'sales';
+        $html = core_Roles::addRole($role) ? "<li style='color:green'>Добавена е роля <b>$role</b></li>" : '';
+        
+        $instances = array();
+        
+        foreach ($managers as $manager) {
+            $instances[$manager] = &cls::get($manager);
+            $html .= $instances[$manager]->setupMVC();
+        }
+
+        $Menu = cls::get('bgerp_Menu');
+        
+        $html .= $Menu->addItem(1, 'Продажби', 'Сделки', 'sales_Deals', 'default', "{$role}, admin");
+        
+        return $html;
+    }
+    
+    
+    /**
+     *  Де-инсталиране на пакета
+     */
+    function deinstall()
+    {
+        // Изтриване на пакета от менюто
+        $res .= bgerp_Menu::remove($this);
+
+        return $res;
+    }
+}
