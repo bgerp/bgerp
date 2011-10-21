0c347f97fd3d93852663b5e9470e6ea138140225
Yusein Yuseinov <yyuseinov@gmail.com>
2011-10-21 17:52:30 +0300

    Пакет за обработка на имейли от пощенската кутия. Не е готов.

---
+++ email/Accounts.class.php
@@ -0,0 +1,88 @@
+<?php 
+
+/**
+ * 
+ * Акаунти за използване на емейли
+ *
+ */
+class email_Accounts extends core_Manager
+{
+	
+
+    /**
+     *  Заглавие на таблицата
+     */
+    var $title = "Акаунти за емейлите";
+    
+    
+    /**
+     * Права
+     */
+    var $canRead = 'admin, email';
+    
+    
+    /**
+     *  
+     */
+    var $canEdit = 'admin, email';
+    
+    
+    /**
+     *  
+     */
+    var $canAdd = 'admin, email';
+    
+    
+    /**
+     *  
+     */
+    var $canView = 'admin, rip';
+    
+    
+    /**
+     *  
+     */
+    var $canList = 'admin, email';
+    
+    /**
+     *  
+     */
+    var $canDelete = 'admin, email';
+    
+	
+	/**
+	 * 
+	 */
+	var $canRip = 'admin, email';
+	
+    
+    /**
+     * 
+     */
+	var $loadList = 'plg_State, email_Wrapper, plg_RowTools';
+    //var $loadList = 'plg_RowTools, plg_Created, plg_Selected, rip_Wrapper, plg_State';
+    
+    
+	/**
+	 * Описание на модела
+	 */
+	function description()
+	{
+		$this->FLD("eMail", "varchar", "caption=Е-мейл");
+		$this->FLD("server", "varchar", 'caption=Сървър');
+		$this->FLD('user', 'varchar', 'caption=Потребителско име');
+		$this->FLD('password', 'varchar', 'caption=Парола');
+		$this->FLD('state', 'enum(active=Активен, stopped=Спрян)', 'caption=Статус');
+		$this->FLD('period', 'int', 'caption=Период');
+		
+		$this->FLD('port', 'int', 'caption=Порт');
+		$this->FLD('subHost', 'varchar', 'caption=Хост');
+		$this->FLD('ssl', 'varchar', 'caption=Сертификат');
+		
+		
+		$this->setDbUnique('eMail');
+		
+	}
+}
+
+?>
---
+++ email/Wrapper.class.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Емейли - опаковка
+ *
+ * @category   BGERP
+ * @package    rip
+ * @author     Yusein Yuseinov <yyuseinov@gmail.com>
+ * @copyright  2006-2011 Experta OOD
+ * @license    GPL 2
+ */
+class email_Wrapper extends core_Plugin
+{
+    /**
+     *  Извиква се след рендирането на 'опаковката' на мениджъра
+     */
+    function on_AfterRenderWrapping($invoker, &$tpl)
+    {
+        $tabs = cls::get('core_Tabs');
+        
+        $tabs->TAB('email_Accounts', 'Акаунти');
+        
+        $tabs->TAB('email_Messages', 'Съобщения');
+        
+        $tpl = $tabs->renderHtml($tpl, empty($invoker->currentTab)?$invoker->className:$invoker->currentTab);
+        
+        $tpl->append(tr($invoker->title) . " » ", 'PAGE_TITLE');
+
+        $invoker->menuPage = 'Емейли';
+    }
+}
