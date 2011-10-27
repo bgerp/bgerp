d3057e702f8093e5b00dd94627612d6b8ecc70c9
arsov <Arsov@EP-BAGS.com>
2011-10-27 16:45:01 +0300

    Update bank/OwnAccounts.class.php

--- bank/OwnAccounts.class.php
+++ bank/OwnAccounts.class.php
@@ -151,7 +151,7 @@ class bank_OwnAccounts extends core_Manager {
         if ($rec = $self->fetch($objectId)) {
             $result = (object)array(
                 'num' => $rec->id,
-                'title' => bank_Accounts::getTitleById($rec->bankAccountId),
+                'title' => bank_Accounts::fetchField($rec->bankAccountId, 'title'),
                 'features' => 'foobar' // @todo!
             );
         }
