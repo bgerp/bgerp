a174118204bf253d253f97f380dafabcb31268d5
Yusein Yuseinov <yyuseinov@gmail.com>
2011-10-28 11:57:54 +0300

    еmail - Промени и допълнения

--- email/Imap.class.php
+++ email/Imap.class.php
@@ -1,10 +1,5 @@
 <?php 
 
-/**
- * Директорията, където ще се съхраняват временните файлове
- */
-defIfNot('IMAP_TEMP_PATH', EF_TEMP_PATH . "/imap/");
-
 
 /**
  * 
@@ -38,10 +33,12 @@ class email_Imap
 			$subHost = '/' . $subHost;
 		}
 		
-		$imap = imap_open("{"."{$host}:{$port}{$subHost}{$ssl}"."}{$folder}", $user, $pass);
+		@$imap = imap_open("{"."{$host}:{$port}{$subHost}{$ssl}"."}{$folder}", $user, $pass);
 
 		if ( $imap === false ) {
-	       exit ("Can't connect: " . imap_last_error() ."\n");
+			email_Accounts::log("Не може да се установи връзка с пощенската кутия на: \"{$user}\". Грешка: " . imap_last_error());
+	       
+	       	return FALSE;
 		}
 		
     	return $imap;
@@ -58,6 +55,7 @@ class email_Imap
 	function statistics($connection)        
 	{ 
 	    $check = imap_mailboxmsginfo($connection); 
+	    
 	    return (array)$check; 
 	} 
 	
@@ -105,6 +103,22 @@ class email_Imap
 	
 	
 	/**
+	 * Връща бодито на избраното съобщение
+	 * 
+	 * @param resource $connection - Връзката към пощенската кутия
+	 * @param number   $messageId  - Номера на съобщението, което да се покаже
+	 * 
+	 * @return string
+	 */
+	function body($connection,$messageId) 
+	{ 
+	    $body = imap_body($connection,$messageId);
+		
+	    return $body; 
+	} 
+	
+	
+	/**
 	 * Подготвя посоченото съобщение за изтриване
 	 * 
 	 * @param resource $connection - Връзката към пощенската кутия
@@ -117,133 +131,6 @@ class email_Imap
 		$delete = imap_delete($connection,$messageId);
 		
 	    return $delete; 
-	} 
-	
-	
-	/**
-	 * Преобразува подадения хедър в масив, за по лесен достъп
-	 * 
-	 * @param string $headers - Хедъра
-	 * 
-	 * @return array
-	 */
-	function mailParseHeaders($headers) 
-	{ 
-	    $headers=preg_replace('/\r\n\s+/m', '',$headers); 
-	    preg_match_all('/([^: ]+): (.+?(?:\r\n\s(?:.+?))*)?\r\n/m', $headers, $matches); 
-	    
-	    foreach ($matches[1] as $key =>$value) {
-	    	$result[$value]=$matches[2][$key]; 
-	    }
-	    
-	    return $result; 
-	} 
-	
-	
-	/**
-	 * Връща цялата информация за посочения мейл
-	 * 
-	 * @param resource $connection   - Връзката към пощенската кутия
-	 * @param number   $messageId    - Номера на съобщението, което да се покаже
-	 * @param boolean  $parseHeaders - Оказва дали да се обработи хедъра
-	 * 
-	 * @return array
-	 */
-	function mailMimeToArray($connection, $messageId, $parseHeaders=FALSE) 
-	{ 
-	    $mail = imap_fetchstructure($connection,$messageId);
-	    $mail = $this->mailGetParts($connection,$messageId,$mail,0); 
-	    
-	    if ($parseHeaders) {
-	    	$mail[0]["parsed"] = $this->mailParseHeaders($mail[0]["data"]); 
-	    }
-	    
-	    return $mail; 
-	} 
-	
-	
-	/**
-	 * Връща цялата информация за посочения мейл
-	 * 
-	 * @param resource $connection - Връзката към пощенската кутия
-	 * @param number   $messageId  - Номера на съобщението, което да се покаже
-	 * @param object   $part       - Оказва дали има прикрепени файлове. Получава се от imap_fetchstructure
-	 * @param number   $prefix     - Префикс
-	 * 
-	 * @return array
-	 */
-	function mailGetParts($connection, $messageId, $part, $prefix=0) 
-	{    
-	    $attachments=array(); 
-	    $attachments[$prefix] = $this->mailDecodePart($connection,$messageId,$part,$prefix); 
-	    if (isset($part->parts)) // multipart 
-	    { 
-	        if ($prefix == 0) {
-	        	$prefix = '';
-	        } else {
-	        	$prefix = $prefix.'.';
-	        }
-	        
-	        foreach ($part->parts as $number=>$subpart) {
-	        	$attachments=array_merge($attachments, $this->mailGetParts($connection,$messageId,$subpart,$prefix.($number+1))); 
-	        }
-	            
-	    } 
-	    
-	    return $attachments; 
-	} 
-	
-	
-	/**
-	 * Декодира мейла
-	 * 
-	 * @param resource $connection - Връзката към пощенската кутия
-	 * @param number   $messageId  - Номера на съобщението, което да се покаже
-	 * @param object   $part       - Оказва дали има прикрепени файлове. Получава се от imap_fetchstructure
-	 * @param number   $prefix     - Префикс
-	 * 
-	 * @return array
-	 */
-	function mailDecodePart($connection, $messageId, $part, $prefix=0) 
-	{ 
-	    $attachment = array(); 
-	
-	    if($part->ifdparameters) {
-	        foreach($part->dparameters as $object) { 
-	            $attachment[strtolower($object->attribute)]=$object->value; 
-	            if(strtolower($object->attribute) == 'filename') {
-	                $attachment['isAttachment'] = true; 
-	                $attachment['filename'] = $object->value; 
-	            } 
-	        } 
-	    } 
-	
-	    if($part->ifparameters) { 
-	        foreach($part->parameters as $object) {
-	            $attachment[strtolower($object->attribute)]=$object->value; 
-	            if(strtolower($object->attribute) == 'name') { 
-	                $attachment['isAttachment'] = true; 
-	                $attachment['name'] = $object->value; 
-	            } 
-	        } 
-	    } 
-	
-	    $attachment['data'] = imap_fetchbody($connection, $messageId, $prefix);
-	    
-	    if($part->encoding == 3) { // 3 = BASE64 
-	        //$attachment['data'] = base64_decode($attachment['data']); 
-	        $attachment['data'] = imap_base64($attachment['data']); 
-	    } 
-	    elseif($part->encoding == 4) { // 4 = QUOTED-PRINTABLE 
-	        //$attachment['data'] = quoted_printable_decode($attachment['data']); 
-	        $attachment['data'] = imap_qprint($attachment['data']); 
-	    } 
-	    
-	    if ($attachment['isAttachment']) {	    	
-	    	$attachment['fileHnd'] = $this->insertFilesToFileman($attachment);	    	
-	    }
-	    
-	    return $attachment; 
 	} 
 	
 	
@@ -277,181 +164,6 @@ class email_Imap
 		
 	    return $close; 
 	}
-	
-	
-	/**
-	 * Вкарва прикрепените файлове във Fileman
-	 * 
-	 * @param array $attachment - Масив, който съдържа необходимите данни за създаването на файла
-	 * 
-	 * @return string - FileHandler' а на файла
-	 */
-	function insertFilesToFileman($attachment)
-	{
-		//Проверяваме за името на файла
-		if (isset($attachment['filename'])) {
-			$fileName = $attachment['filename'];
-		} else {
-			if (isset($attachment['filename'])) {
-				$fileName = $attachment['name'];
-			} else {
-				$fileName = str::getUniqId();
-			}
-		}
-		
-		$filePath = IMAP_TEMP_PATH . $fileName;
-		
-		//Проверяваме дали съществува файл със същото име
-		if (is_file($filePath)) {
-			$filePath = $this->getUniqName($filePath);
-		}
-		
-		//Записваме новия файла
-		$fp = fopen($filePath, w);
-		fputs($fp,$attachment['data']);
-		fclose($fp);
-		
-		//Вкарваме файла във Fileman
-		$Fileman = cls::get('fileman_Files');
-		$fh = $Fileman->addNewFile($filePath, 'Email');
-		
-		//Изтриваме фременния файл
-		@unlink($filePath);
-		
-		return $fh;
-	}
-	
-	
-	/**
-	 * Проверява и генерира уникално име на файла
-	 * 
-	 * @param string $filePath - Пътя до файла
-	 * 
-	 * @return Новия път до файла
-	 */
-	function getUniqName($filePath)
-	{
-		$pathParts = pathinfo($filePath);
-        $baseName = $pathParts['basename'];
-        $directory = $pathParts['dirname'];
-        
-		$fn = $baseName;
-        
-        if (($dotPos = mb_strrpos($baseName, '.')) !== FALSE ) {
-	    	$firstName = mb_substr($baseName, 0, $dotPos);
-	        $ext = mb_substr($baseName, $dotPos);
-	    } else {
-            $firstName = $baseName;
-            $ext = '';
-        }
-        
-        $i = 0;
-        $files = scandir($directory);
-	    
-        // Циклим докато генерирме име, което не се среща до сега
-        while (in_array($fn, $files)) {
-            $fn = $firstName . '_' . (++$i) . $ext;
-        }
-        
-        return $directory . '/' . $fn;	
-        
-	}
-	
-	
-	/**
-	 * Взема записите от пощенската кутия и ги вкарва в модела
-	 */
-	function getMailInfo()
-	{
-		
-		$accaunt = email_Accounts::fetch("#id=1");
-		//TODO while fetch all
-		
-		$host = $accaunt->server;
-		$port = $accaunt->port;
-		//$user = $accaunt->eMail;
-		$user = 'testbgerp@gmail.com';
-		$pass = $accaunt->password;
-		$subHost = $accaunt->subHost;
-		$ssl = $accaunt->ssl;
-		$mailId = $accaunt->id;
-		
-		$imap = $this->login($host, $port, $user, $pass, $subHost,$folder="INBOX", $ssl);
-		
-		$statistics = $this->statistics($imap);
-		
-		$numMsg = $statistics['Nmsgs'];
-		$i = 1; //$messageId - Номера на съобщението
-		while ($i <= $numMsg) {
-			$rec = new stdClass();
-			
-			$lists = $this->lists($imap, $i);
-    		
-			$header = $this->header($imap, $i);
-			
-			$mailMimeToArray = $this->mailMimeToArray($imap, $i);
-			
-			unset($mailMimeToArray[0]);
-			
-			$textKey = '1';
-			$htmlKey = '2';
-			
-			if (isset($mailMimeToArray['1.1'])) {
-				$textKey = '1.1';
-				$htmlKey = '1.2';
-				unset($mailMimeToArray[1]);
-			}
-			
-			$rec->textPart = $mailMimeToArray[$textKey]['data'];
-			$rec->htmlPart = $mailMimeToArray[$htmlKey]['data'];
-			
-			if (!(strlen($rec->textPart))) {
-				$html2Text = cls::get('html2text_Html2Text');
-				$txt = $html2Text->convert2text($rec->htmlPart);
-				$rec->textPart = $txt;
-			}
-			
-			unset($mailMimeToArray[$textKey]);
-			unset($mailMimeToArray[$htmlKey]);
-			
-			if (count($mailMimeToArray)) {
-				foreach ($mailMimeToArray as $key => $value) {
-					
-					//TODO Вмъкването на id' тата на файловете в модела (в keylist поле)
-					
-					//$rec->files = $value['fileHnd'];
-				}
-			}
-			
-			
-			//bp($header, $mailMimeToArray);
-						
-			$rec->emailId = $mailId;
-			$rec->messageId = $lists[$i]['message_id'];
-			$rec->from = $lists[$i]['from'];
-			//$rec->fromName = $lists[$i]['from'];
-			$rec->to = $lists[$i]['to'];
-			//$rec->to = $lists[$i]['to'];
-			//$rec->date = $lists[$i]['date'];
-			$rec->headers = $header;
-			
-			
-			
-			
-			//$rec->subject = $lists[$i]['subject'];
-			
-			email_Messages::save($rec);
-			
-			
-			
-    		$i++;
-		}
-		bp($rec);
-	}
-	
-	
-	
-	
 	
 	
 	
--- email/Messages.class.php
+++ email/Messages.class.php
@@ -1,5 +1,18 @@
 <?php 
 
+
+/**
+ * Директорията, където ще се съхраняват временните файлове
+ */
+defIfNot('IMAP_TEMP_PATH', EF_TEMP_PATH . "/imap/");
+
+
+/**
+ * Директорията, където ще се съхраняват eml файловете
+ */
+defIfNot('IMAP_EML_PATH', EF_TEMP_PATH . "/imapeml/");
+
+
 /**
  * 
  * Емейли
@@ -59,7 +72,7 @@ class email_Messages extends core_Manager
     /**
      * 
      */
-	var $loadList = 'email_Wrapper';
+	var $loadList = 'email_Wrapper, plg_Created';
     //var $loadList = 'plg_RowTools, plg_Created, plg_Selected, rip_Wrapper, plg_State';
     
     
@@ -68,7 +81,7 @@ class email_Messages extends core_Manager
 	 */
 	function description()
 	{
-		$this->FLD('emailId', 'key(mvc=email_Accounts,select=eMail)', 'caption=Е-мейл');
+		$this->FLD('accId', 'key(mvc=email_Accounts,select=eMail)', 'caption=Е-мейл');
 		$this->FLD("messageId", "varchar", "caption=Съобщение ID");
 		$this->FLD("subject", "varchar", "caption=Тема");
 		$this->FLD("from", "varchar", 'caption=От');
@@ -80,21 +93,207 @@ class email_Messages extends core_Manager
 		$this->FLD("htmlPart", "blob(70000)", 'caption=HTML част');
 		$this->FLD("spam", "int", 'caption=Спам');
 		$this->FLD("lg", "varchar", 'caption=Език');
+		$this->FLD('hash', 'varchar(32)', 'caption=Keш');
+		$this->FLD('country', 'key(mvc=drdata_countries,select=commonName)', 'caption=Държава');
+		$this->FLD('fromIp', 'ip', 'caption=IP');
 		
 		$this->FLD('files', 'keylist(mvc=fileman_Files,select=name,maxColumns=1)', 'caption=Файлове');
+		//$this->FLD('date', 'datetime', 'Caption=Дата');
 		
 		
-		//$this->setDbUnique('eMail');
+		$this->setDbUnique('hash');
 		
 	}
 	
+		
+	/**
+	 * Взема записите от пощенската кутия и ги вкарва в модела
+	 *
+	 * @param number $oneMailId - Потребителя, за когото ще се проверяват записите.
+	 * Ако е празен, тогава ще се проверяват за всички.
+	 * 
+	 * @return boolean TRUE
+	 */
+	function getMailInfo($oneMailId=FALSE)
+	{
+		$query = email_Accounts::getQuery();
+		if (!$oneMailId) {
+			
+			//$query->where("#period = ''");
+						
+		} else {
+			$oneMailId = intval($oneMailId);
+			$query->where("#id = '$oneMailId'");
+		}
+		
+		$imapCls = cls::get('email_Imap');
+		$imapParse = cls::get('email_Parser');
+		
+		while ($accaunt = $query->fetch()) {
+			$host = $accaunt->server;
+			$port = $accaunt->port;
+			$user = $accaunt->eMail;
+			//$user = $accaunt->user;
+			$pass = $accaunt->password;
+			$subHost = $accaunt->subHost;
+			$ssl = $accaunt->ssl;
+			$accId = $accaunt->id;
+			
+			$imap = $imapCls->login($host, $port, $user, $pass, $subHost, $folder="INBOX", $ssl);
+			
+			if (!$imap) {
+				continue;
+			}
+			
+			$statistics = $imapCls->statistics($imap);
+			
+			$numMsg = $statistics['Nmsgs'];
+			$i = 1; //messageId - Номера на съобщението
+			while ($i <= $numMsg) {
+				$rec = new stdClass();
+				
+				//$lists = $imapCls->lists($imap, $i);
+	    		
+				$header = $imapCls->header($imap, $i);
+				
+				$body = $imapCls->body($imap, $i);
+				
+				$mailMimeToArray = $imapParse->mailMimeToArray($imap, $i);
+				
+				unset($mailMimeToArray[0]);
+				
+				$textKey = '1';
+				$htmlKey = '2';
+				if (isset($mailMimeToArray['1.1'])) {
+					$textKey = '1.1';
+					$htmlKey = '1.2';
+					unset($mailMimeToArray[1]);
+				}
+				$text = $mailMimeToArray[$textKey]['data']; 
+				$html = $mailMimeToArray[$htmlKey]['data'];
+				$textCharset = $mailMimeToArray[$textKey]['charset'];
+				$htmlCharset = $mailMimeToArray[$htmlKey]['charset'];
+				unset($mailMimeToArray[$textKey]);
+				unset($mailMimeToArray[$htmlKey]);
+				
+				$imapParse->setHeaders($header);
+				
+				$imapParse->setText($text);
+				$imapParse->setTextCharset($textCharset);
+				
+				$imapParse->setHtml($html);
+				$imapParse->setHtmlCharset($htmlCharset);
+				
+				$rec->textPart = $imapParse->getText();
+				$rec->htmlPart = $imapParse->getHtml();	
+				$rec->subject = $imapParse->getHeader('subject');
+				$rec->messageId = $imapParse->getHeader('message-id');
+				$rec->accId = $accId;
+				$rec->headers = $header;
+				$rec->hash = md5($header);
+				$rec->fromIp = $imapParse->getSenderIp();
+				
+				$mailFrom = $imapParse->getFrom();		
+				$rec->from = $mailFrom['mail'];
+				$rec->fromName = $mailFrom['name'];
+				
+				$mailTo = $imapParse->getTo();
+				$rec->to = $mailTo['mail'];
+				$rec->toName = $mailTo['name'];
+				
+				//TODO getCodeFromTld, getCodeFromIp - calcCountry
+				//$rec->from = $imapParse->getHeader('from');
+				//$rec->fromName = $this->getEmailName($rec->from);
+								
+				//$rec->to = $mailTo;
+				//$rec->toName = $this->getEmailName($rec->to);
+								
+				if (count($mailMimeToArray)) {
+					$Fileman = cls::get('Fileman_files');
+					foreach ($mailMimeToArray as $key => $value) {
+						$fh = $value['fileHnd'];
+						$id = $Fileman->fetchByFh($fh); 
+						$fhId[$id->id] = $fh;
+					}
+					
+					$rec->files = type_Keylist::fromArray($fhId);
+				}
+				bp($rec);
+				email_Messages::save($rec, NULL, 'IGNORE');
+				
+				$eml = $header . "\n\n" . $body;
+				$emlPath = IMAP_EML_PATH . $rec->hash . '.eml';
+				$emlPath = $imapParse->getUniqName($emlPath);
+				
+				//Записваме новия файла
+				$fp = fopen($emlPath, w);
+				fputs($fp, $eml);
+				fclose($fp);
+				
+				//TODO Да се премахне коментара
+				//$imapCls->delete($imap, $i);
+				//bp($rec);
+	    		$i++;
+	    		
+			}
+			//TODO Да се премахне коментара
+			//$imapCls->expunge($imap);
+		
+			$imapCls->close($imap);
+		}
+		
+		return TRUE;
+	}
+	
+	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	/**
-     * Изпълнява се след създаването на таблицата
+     * Да сваля имейлите
+     */
+    function cron_DownloadEmails()
+    {		
+		$mailInfo = $this->getMailInfo();
+		
+		return 'Свалянето приключи.';
+    }
+    
+	
+	/**
+     * Изпълнява се след създаването на модела
      */
 	function on_AfterSetupMVC($mvc, $res)
     {
-    	cls::get('email_Imap');
+    	$res .= "<p><i>Нагласяне на Cron</i></p>";
+        
+        $rec->systemId = 'DownloadEmails';
+        $rec->description = 'Сваля и-мейлите в модела';
+        $rec->controller = $this->className;
+        $rec->action = 'DownloadEmails';
+        $rec->period = 1;
+        $rec->offset = 0;
+        $rec->delay = 0;
+     // $rec->timeLimit = 200;
+        
+        $Cron = cls::get('core_Cron');
+        
+        if ($Cron->addOnce($rec)) {
+            $res .= "<li><font color='green'>Задаване на крон да сваля имейлите в модела.</font></li>";
+        } else {
+            $res .= "<li>Отпреди Cron е бил нагласен да сваля имейлите.</li>";
+        }
+    	
         if(!is_dir(IMAP_TEMP_PATH)) {
             if( !mkdir(IMAP_TEMP_PATH, 0777, TRUE) ) {
                 $res .= '<li><font color=red>' . tr('Не може да се създаде директорията') . ' "' . IMAP_TEMP_PATH . '</font>';
@@ -105,8 +304,19 @@ class email_Messages extends core_Manager
         	$res .= '<li>' . tr('Директорията съществува: ') . ' <font color=black>"' . IMAP_TEMP_PATH . '"</font>';
         }
         
+    	if(!is_dir(IMAP_EML_PATH)) {
+            if( !mkdir(IMAP_EML_PATH, 0777, TRUE) ) {
+                $res .= '<li><font color=red>' . tr('Не може да се създаде директорията') . ' "' . IMAP_EML_PATH . '</font>';
+            } else {
+                $res .= '<li>' . tr('Създадена е директорията') . ' <font color=green>"' . IMAP_EML_PATH . '"</font>';
+            }
+        } else {
+        	$res .= '<li>' . tr('Директорията съществува: ') . ' <font color=black>"' . IMAP_EML_PATH . '"</font>';
+        }
+        
         return $res;
     }
+
 }
 
 ?>
