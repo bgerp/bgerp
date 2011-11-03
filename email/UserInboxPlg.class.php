06a9c940b9ce755665fcd78f15ba5fdc6223c286
Yusein Yuseinov <yyuseinov@gmail.com>
2011-11-03 13:18:28 +0200

    email - Плъгин, който се прикрепя към core_Users и създава корица при създаване на акаунт.

---
+++ email/UserInboxPlg.class.php
@@ -0,0 +1,32 @@
+<?php
+
+
+/**
+ * Клас 'email_UserInboxPlg' - Създава пощенска кутия след създаване на нов потребител
+ *
+ * @category   Experta Framework
+ * @package    email
+ * @author     Yusein Yuseinov
+ * @copyright  2006-2011 Experta OOD
+ * @license    GPL 2
+ * @since      v 0.1
+ */
+class email_UserInboxPlg extends core_Plugin 
+{
+	
+	
+	/**
+     *  Извиква се след вкарване на запис в таблицата на модела users
+     */
+    function on_BeforeSave($mvc, &$id, $rec)
+    {
+    	$eRec = new stdClass();
+    	$eRec->inCharge = $rec->id;
+    	$eRec->access = "private";
+    	$eRec->name = $rec->nick;
+    	$eRec->id = $rec->id;
+    	
+    	email_Inboxes::forceCoverAndFolder($eRec);
+    }
+}
+	
