c97daeb88cbc31f3c701b8ca0d14a92ea68f7ecf
Milen Georgiev <milen@ep-bags.com>
2011-11-09 12:17:16 +0200

    Нов тип за избор на група

---
+++ type/Group.class.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Ключ към запис от модел, по който се групира друг модел 
+ * 
+ * Понякога деден модел съдържа keylist поле към друг модел (за групиране)
+ * Този тип позволява да се избере ключ от  модела за групирането
+ * Например: ключ към групите от визитника 
+ *
+ * @category   Experta Framework
+ * @package    type
+ * @author     Milen Georgiev
+ * @copyright  2006-2011 Experta OOD
+ * @license    GPL 2
+  */
+class type_Group extends type_Key
+{
+	/**
+     *  Инициализиране на обекта
+     */
+    function init($params)
+    {
+        parent::init($params);
+
+        expect($base = $this->params['base']); // Базов модел
+        expect($keylist = $this->params['keylist']); // Името на keylist полето
+        
+        $baseMvc = cls::get($base);
+        
+        expect($mvc = $baseMvc->fields[$keylist]->type->params['mvc'], $baseMvc->fields[$keylist]);
+
+        $this->params['mvc'] = $mvc;        
+    }
+    
+
+    /**
+     * Подготвя опциите според зададените параметри.
+     * Ако е посочен суфикс, извеждате се само интерфейсите
+     * чието име завършва на този суфикс
+     */
+    private function prepareOptions()
+    {
+    	if (isset($this->options)) {
+    		return;
+    	}
+    	
+        expect($base = $this->params['base']); // Базов модел
+        expect($keylist = $this->params['keylist']); // Името на keylist полето
+        
+        $mvc = cls::get($this->params['mvc']);
+
+        $baseMvc = cls::get($base);
+        $baseQuery = $baseMvc->getQuery();
+        $baseQuery->show($keylist);
+
+        while($baseRec = $baseQuery->fetch()) {
+            $arr = type_Keylist::toArray($baseRec->{$keylist}); 
+            foreach($arr as $id => $dummy) {
+                $groups[$id]++;
+            }
+        }
+
+        foreach($groups as $id => $cnt) {
+            $this->options[$id] = $mvc->getTitleById($id) . " ({$cnt})";
+        }
+     }
+    
+
+    /**
+     * Рендира HTML инпут поле
+     */
+    function renderInput_($name, $value="", $attr = array())
+    {
+		$this->prepareOptions();
+    	
+		return parent::renderInput_($name, $value, $attr);
+    }
+    
+
+    /**
+     * Конвертира стойността от вербална към (int) - ключ към core_Interfaces
+     */
+	function fromVerbal_($value)
+	{
+		$this->prepareOptions();
+
+		return parent::fromVerbal_($value);
+	}
+    
+}
