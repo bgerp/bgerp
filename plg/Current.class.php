a3940340fa11282fc512e9fa6233b59e8d478078
arsov <Arsov@EP-BAGS.com>
2011-12-06 15:55:55 +0200

    Update plg/Current.class.php

--- plg/Current.class.php
+++ plg/Current.class.php
@@ -23,7 +23,7 @@ class plg_Current extends core_Plugin
     {
         if(!$res) {
             $res = Mode::get('currentPlg_' . $mvc->className)->{$part};
-            if(!$res && ($mvc->className != Request::get('Ctr'))) {
+            if((!$res) && ($mvc->className != Request::get('Ctr'))) {
             	redirect(array($mvc), FALSE, "Моля, изберете текущ {$mvc->singleTitle}");
             }
         }
