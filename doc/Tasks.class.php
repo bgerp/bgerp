c1f1c9072049305faf37d2e7e85fdf9f004f9212
Tsvetan Michailov <tsvetan_michailov@yahoo.com>
2011-12-21 17:43:48 +0200

    doc Tasks

---
+++ doc/Tasks.class.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Клас 'doc_Tasks' - Документ - задача
+ */
+class doc_Tasks extends core_Manager
+{   
+    var $loadList = 'plg_Created, plg_RowTools, doc_Wrapper, plg_State';
+
+    var $title    = "Документ - задача";
+
+    var $listFields = 'title, details, tools=Пулт';
+    
+    var $rowToolsField = 'tools';
+    
+     
+    function description()
+    {
+    	$string = new type_Varchar();
+    	$string->load('jqdatepick_Plugin');
+        $string->suggestions = arr::make(tr("Днес,
+                                             Утре, 
+                                             Началото на следващата седмица,
+                                             Началото на следващия месец,
+                                             Началото на следващата година"), TRUE);
+    	
+    	$this->FLD('title',        'varchar(64)', 'caption=Заглавие,mandatory');
+    	$this->FLD('details',      'varchar',     'caption=Описание,mandatory');
+    	$this->FLD('timeStart',    $string,       'caption=Време->Старт,mandatory');
+    	$this->FLD('timeDuration', 'varchar(64)', 'caption=Време->Продължителност');
+    	$this->FLD('timeEnd',      $string,       'caption=Време->Край');
+    	$this->FLD('repeat',       'enum(none=няма,
+    	                                 everyDay=всеки ден,
+    	                                 everyTwoDays=на всеки 2 дена,
+    	                                 everyThreeDays=на всеки 3 дена,
+    	                                 everyWeek=всяка седмица,
+    	                                 everyMonthy=всеки месец,
+    	                                 everyThreeMonths=на всеки 3 месеца,
+    	                                 everySixMonths=на всяко полугодие,
+    	                                 everyYear=всяка година)', 'caption=Повторение,mandatory');
+        $this->FLD('notification', 'enum(NULL=няма,
+                                         0=на момента,
+                                         -5=5 мин. предварително,
+                                         -10=10 мин. предварително,
+                                         -30=30 мин. предварително,
+                                         -60=1 час предварително,
+                                         -120=2 час предварително,
+                                         -480=8 часа предварително,
+                                         -1440=1 ден предварително,
+                                         -2880=2 дни предварително,
+                                         -4320=3 дни предварително,
+                                         -10080=7 дни предварително)', 'caption=Известяване,mandatory');    	
+    }
+
+}
