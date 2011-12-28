0e9676721eda7eb11546db05475e885710c48a06
arsov <Arsov@EP-BAGS.com>
2011-12-28 19:19:30 +0200

    Замяна на "Теми" с "Нишки"

--- doc/Folders.class.php
+++ doc/Folders.class.php
@@ -17,7 +17,7 @@ class doc_Folders extends core_Master
 
     var $title    = "Папки с нишки от документи";
 
-    var $listFields = 'id,title,type=Тип,inCharge=Отговорник,threads=Теми,last=Последно';
+    var $listFields = 'id,title,type=Тип,inCharge=Отговорник,threads=Нишки,last=Последно';
 
     var $canRead   = 'user';
     var $canWrite  = 'user';
