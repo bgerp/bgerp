5635fe6de94f4b019849cb41b46f88e635dc8fb1
Yusein Yuseinov <yyuseinov@gmail.com>
2012-03-09 18:17:23 +0200

    Клас за генериране на QR кодове.

---
+++ bgerp/Qr.class.php
@@ -0,0 +1,42 @@
+<?php
+
+
+/**
+ * Клас 'bgerp_Qr' - Създаване нa QR изображения
+ *
+ *
+ * @category  bgerp
+ * @package   bgerp
+ * @author    Yusein Yuseinov <yyuseinov@gmail.com>
+ * @copyright 2006 - 2012 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class bgerp_Qr extends core_Manager
+{
+    
+    
+    /**
+     * 
+     */
+    function act_C()
+    {
+        $cid = Request::get('cid');
+        $mid = Request::get('mid');
+        
+        barcode_Qr::generate($cid, $mid);
+    }
+    
+    
+    /**
+     * 
+     */
+    static function createQrLink($cid, $mid)
+    {
+        
+        $link = toUrl(array('D', 'S', 'cid' => $cid, 'mid' => $mid), 'absolute');
+                
+        return $link;
+    }
+    
+}
