9d8f7d5bba97aeba71269468bd43658551e0f6cd
Dimitar Minekov <mitko@extrapack.com>
2012-03-21 10:07:22 +0200

    bugfix: SQL-injection

--- prosms/Dlr.class.php
+++ prosms/Dlr.class.php
@@ -34,7 +34,7 @@ class prosms_Dlr extends core_Manager
 		$status = request::get('status', 'varchar');
 		$code = request::get('code', 'varchar');
 
-		expect(sms_Sender::fetch("#uid = '" . substr($uid, -3) . "'"), "Невалидна заявка.");
+		expect(sms_Sender::fetch(array("#uid = '[#1#]'", substr($uid, -3))), "Невалидна заявка.");
 		
 		if ((int)$code !== 0) {
 			$status = 'receiveError';
