fc6f8b4c7b534b9b1d00a8de463d43cbd937a2e2
Milen Georgiev <milen@ep-bags.com>
2012-03-25 16:41:10 +0300

    Поправки

--- recently/Values.class.php
+++ recently/Values.class.php
@@ -45,8 +45,8 @@ class recently_Values extends core_Manager
      */
     function description()
     {
-        $this->FLD('name', 'varchar', 'caption=Име');
-        $this->FLD('value', 'varchar(255)', 'caption=Стойност');
+        $this->FLD('name', 'varchar(64)', 'caption=Име');
+        $this->FLD('value', 'varchar(128)', 'caption=Стойност');
         
         $this->setDbUnique('name,value,createdBy');
     }
@@ -99,7 +99,7 @@ class recently_Values extends core_Manager
     function add($name, $value)
     {
         $cu = core_Users::getCurrent();
-
+        $value = str::toFixedKey($value, 64);
         $rec = $this->fetch(array(
                 "#name = '[#1#]' AND #value = '[#2#]' AND #createdBy = {$cu}",
                 $name,
