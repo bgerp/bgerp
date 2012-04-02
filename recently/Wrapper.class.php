d4ae77258261effc5ee09210e552475383e45c3a
Milen Georgiev <milen@ep-bags.com>
2012-04-02 11:28:42 +0300

    Малки поправки

--- recently/Wrapper.class.php
+++ recently/Wrapper.class.php
@@ -31,6 +31,6 @@ class recently_Wrapper extends core_Plugin
         
         $tpl = $tabs->renderHtml($tpl, $invoker->className);
         
-        $tpl->append(tr($invoker->title) . " » ", 'PAGE_TITLE');
+        $tpl->append(tr($invoker->title) . " « ", 'PAGE_TITLE');
     }
 }
