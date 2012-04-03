3ed73eced08c27ca5dbff07a5319dc925ff80e40
Dimitar Minekov <mitko@extrapack.com>
2012-04-03 15:50:52 +0300

    Добавен параметър Codec в драйвера на ALC камерите

--- cams/driver/vlcschedule.sh
+++ cams/driver/vlcschedule.sh
@@ -3,8 +3,10 @@ set -e
 URL="$1"
 SAVEFILE="$2"
 DURATION="$3"
+CODEC="$4"
+# mp4v, h264
 DISPLAY= cvlc "$URL" \ # --run-time $DURATION \
-	--sout="#transcode{vcodec=mp4v,vb=800,acodec=mpga,ab=128,fps=4,deinterlace}:std{access=file,mux=mp4,dst='$SAVEFILE'}" \
+	--sout="#transcode{vcodec=$CODEC,vb=800,acodec=mpga,ab=128,fps=4,deinterlace}:std{access=file,mux=mp4,dst='$SAVEFILE'}" \
 	< /dev/null > /dev/null 2>&1 &
 pid=$!
 sleep $DURATION
