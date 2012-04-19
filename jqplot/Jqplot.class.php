1e06dd60e9b05e7aac8f1888b667521701c5b51d
Stefan Stefanov <stefan.bg@gmail.com>
2012-04-19 19:32:06 +0300

    Работа по пакета jqplot

--- jqplot/Jqplot.class.php
+++ jqplot/Jqplot.class.php
@@ -1,21 +1,29 @@
 <?php
 /**
- * 
+ *
  * Адаптер за пакета jqplot - http://www.jqplot.com/
- * 
+ *
  * @author Stefan Stefanov <stefan.bg@gmail.com>
  *
  */
 class jqplot_Jqplot
 {
     static protected $jqplotBasePath = 'jqplot/jquery.jqplot.1.0.0b2_r1012';
-    
-    
+
+    /**
+     * Текущо натрупаните jqplot опции
+     *
+     * @see
+     * @var array
+     */
+    static protected $options = array();
+
+
     /**
      * Инициализиране на пакета jqplot
-     * 
+     *
      * Зарежда необходимите ресурси - скриптове, CSS, jqplot-плъгини
-     * 
+     *
      * @param core_ET $tpl
      * @param array $plugins масив с имена на jqplot-плъгини
      */
@@ -23,7 +31,7 @@ class jqplot_Jqplot
     {
         // Зареждане на jquery
         jquery_Jquery::enable($tpl);
-        
+
         // Зареждане на базовите jqplot файлове
         $tpl->appendOnce("\n"
             . "<!--[if lt IE 9]>"
@@ -31,19 +39,19 @@ class jqplot_Jqplot
             . "<![endif]-->", 'HEAD');
         $tpl->push(static::resource('jquery.jqplot.min.js'), 'JS');
         $tpl->push(static::resource('jquery.jqplot.min.css'), 'CSS');
-        
+
         // Зареждане на jqplot-плъгини
         $plugins = arr::make($plugins);
-        
+
         foreach ($plugins as $plugin) {
             $tpl->push(static::resource("plugins/jqplot.{$plugin}.min.js"), 'JS');
         }
     }
-    
-    
+
+
     /**
-     * 
-     * 
+     *
+     *
      * @param array $series данните за визуализиране
      * @param array $options
      * @return core_ET
@@ -51,26 +59,25 @@ class jqplot_Jqplot
     static function chart($series, $options = NULL)
     {
         $chartAttr = array();
+
         core_Html::setUniqId($chartAttr);
-        
+
         $chart = core_Html::createElement('div', $chartAttr, '', TRUE);
-        
-        if (!isset($options)) {
-            $options = new stdClass();
-        }
-        
+
         $series  = json_encode($series);
-        $options = json_encode($options);
-        
-        jquery_Jquery::run($chart, "$.jqplot('{$chartAttr['id']}', {$series}, {$options})");
-        
+        $options = json_encode((object)$options);
+
+        $options = preg_replace('/"@(.*?)@"/', '$1', $options);
+
+        jquery_Jquery::run($chart, ";$.jqplot('{$chartAttr['id']}', {$series}, {$options});\n");
+
         return $chart;
     }
-    
-    
+
+
     /**
      * Помощен метод при зареждане на ресурсите на jqplot
-     *  
+     *
      * @param string $rPath @see sbf()
      * @param string $qt @see sbf()
      * @param boolean $absolute @see sbf()
@@ -79,6 +86,5 @@ class jqplot_Jqplot
     protected static function resource($rPath, $qt = '"', $absolute = FALSE)
     {
         return static::$jqplotBasePath . "/{$rPath}";
-        return sbf(static::$jqplotBasePath . "/{$rPath}", $qt, $absolute);
     }
 }
--- jqplot/Plugin.class.php
+++ jqplot/Plugin.class.php
@@ -1,20 +1,202 @@
 <?php
 /**
- * Плъгин за рендиране на графики, използващ jqplot - http://www.jqplot.com/ 
- * 
+ * Плъгин за рендиране на графики, използващ jqplot - http://www.jqplot.com/
+ *
  * @author Stefan Stefanov <stefan.bg@gmail.com>
  *
  */
 class jqplot_Plugin extends core_Plugin
 {
+
+    static $defaultJqplotOptions = array(
+        'seriesDefaults' => array(
+            'pointLabels' => array(
+                'show' => true,
+                'escapeHTML' => false,
+            )
+        ),
+        'axes' => array(
+            'xaxis' => array(
+                'renderer' => '@$.jqplot.CategoryAxisRenderer@',
+            )
+        )
+    );
+
+    static $defaultJqplotPlugins = array(
+        'categoryAxisRenderer',
+        'pointLabels',
+    );
+
+
+    /**
+     * Манипулации със заглавието
+     *
+     * @param core_Mvc $mvc
+     * @param stdClass $data
+     */
+    static function on_AfterRenderListTitle($mvc, &$title, $data)
+    {
+
+        $chartConfigs = $mvc::getChartConfig();
+
+        if (count($chartConfigs)) {
+            $title = new ET('[#1#]', $title);
+
+            $title->append('<div style="margin-top:5px;margin-bottom:5px;font-size:0.80em;font-family:arial;" id="chartMenu">', 'ListSummary');
+
+            $reqestedChartName = $mvc::getRequestedChartName();
+
+            if ($reqestedChartName) {
+                $title->append(ht::createLink(tr('Tаблица'), $mvc::getChartUrl(NULL)) , 'ListSummary');
+            } else {
+                $title->append(tr('Tаблица') , 'ListSummary');
+            }
+
+            foreach ($chartConfigs as $chartName=>$chartConfig) {
+                $title->append("&nbsp;|&nbsp;", 'ListSummary');
+
+                $caption = isset($chartConfig['menu']) ? $chartConfig['menu'] : $chartName;
+
+                $url = getCurrentUrl();
+
+                if($reqestedChartName != $chartName) {
+                    $title->append(ht::createLink(tr($caption), $mvc::getChartUrl($chartName)) , 'ListSummary');
+                } else {
+                    $title->append(tr($caption), 'ListSummary');
+                }
+            }
+
+            $title->append('</div>', 'ListSummary');
+        }
+    }
+
+
     /**
      * @param core_Mvc $mvc
      * @param core_ET $tpl
      */
-    static function on_AfterRenderListTable($mvc, $tpl)
+    static function on_AfterRenderListTable($mvc, $tpl, $data)
     {
-        jqplot_Jqplot::setup($tpl, array('cursor','dragable'));
-        
-        $tpl->append(jqplot_Jqplot::chart(array(1, 2, 3, 4)));
+        if (!$chartName = $mvc::getRequestedChartName()) {
+            // Не е указана графика
+            return;
+        }
+
+        if (!$chartConfig = $mvc::getChartConfig($chartName)) {
+            // Няма конфигурация - няма да показваме графика
+            return;
+        }
+
+        $tpl = new core_ET();
+
+        $jqplotPlugins     = static::$defaultJqplotPlugins;
+        $jqplotOptionsBase = static::$defaultJqplotOptions;
+
+        if (isset($chartConfig['type']) && $chartConfig['type'] == 'bars') {
+            $jqplotOptionsBase['seriesDefaults']['renderer'] = '@$.jqplot.BarRenderer@';
+            $jqplotPlugins[] = 'barRenderer';
+        }
+
+        $groups = $data->recs;
+
+
+        if (!empty($chartConfig['diff'])) {
+            // Ще правим толкова графики, колкото различни стойности на полето
+            // $chartConfig['diff'] има
+            expect($mvc->getField($chartConfig['diff']));
+
+            $groups = arr::group($groups, $chartConfig['diff']);
+        } else {
+            // Ще правим една графика
+            $groups = array($groups);
+        }
+
+        if (!empty($chartConfig['series'])) {
+            expect($mvc->getField($chartConfig['series']));
+
+            foreach($groups as $i => $recs) {
+                $groups[$i] = arr::group($recs, $chartConfig['series']);
+            }
+        } else {
+            $groups = array($groups);
+        }
+
+        foreach ($groups as $recSeries) {
+
+            $jqplotOptions = $jqplotOptionsBase;
+
+            $ticks  = array();
+            $series = array();
+            $labels = array();
+            $title  = NULL;
+
+            foreach ($recSeries as $serTitle => $recs) {
+                foreach ($recs as $i=>$rec) {
+                    $row = $data->rows[$i];
+
+                    if (!isset($title)) {
+                        if (isset($chartConfig['diff'])) {
+                            $options['axes']['yaxis']['label'] = $row->{$chartConfig['diff']};
+                            $title = $row->{$chartConfig['diff']};
+                        }
+
+                        if (isset($chartConfig['titleTpl'])) {
+                            $title = new core_ET($chartConfig['titleTpl']);
+                            $title->placeObject($row);
+                            $title = (string)$title;
+                        }
+                    }
+
+                    $ticks[$row->{$chartConfig['ax']}] = $row->{$chartConfig['ax']};
+                    $series[$serTitle][] = floatval($rec->{$chartConfig['ay']});
+
+                    if (isset($chartConfig['labelTpl'])) {
+                        $label = new core_ET($chartConfig['labelTpl']);
+                        $label->placeObject($row);
+                        $label = (string)$label;
+                    } else {
+                        $label = $row->{$chartConfig['ay']};
+                    }
+
+                    $jqplotOptions['series'][count($series)-1]['pointLabels']['labels'][] = $label;
+                }
+            }
+
+            $series = array_values($series);
+            $ticks  = array_values($ticks);
+
+            $jqplotOptions['axes']['xaxis']['ticks'] = $ticks;
+            $jqplotOptions['title'] = $title;
+
+            $tpl->append(jqplot_Jqplot::chart($series, $jqplotOptions));
+            $tpl->append('<hr/>');
+        }
+
+        jqplot_Jqplot::setup($tpl, $jqplotPlugins);
+    }
+
+    static function on_AfterGetChartConfig($mvc, &$res, $name = NULL)
+    {
+        $res = array();
+
+        if (isset($mvc::$charts)) {
+            $res = $mvc::$charts;
+
+            if (isset($name)) {
+                $res = isset($mvc::$charts[$name]) ? $mvc::$charts[$name] : array();
+            }
+        }
+    }
+
+    static function on_AfterGetRequestedChartName($mvc, &$res)
+    {
+        $res = core_Request::get('chart');
+    }
+
+
+    static function on_AfterGetChartUrl($mvc, &$url, $chartName)
+    {
+        $url = getCurrentUrl();
+        $url['chart'] = $chartName;
     }
 }
