78b334a8c44b761d355cc15c028ce7c3a592001b
Stefan Stefanov <stefan.bg@gmail.com>
2012-04-25 15:54:27 +0300

    Добавен статичен метод drdata_PhoneType::toArray()

--- drdata/PhoneType.class.php
+++ drdata/PhoneType.class.php
@@ -15,67 +15,82 @@
  * @todo:     Да се документира този клас
  */
 class drdata_PhoneType extends type_Varchar {
-    
-    
+
+
     /**
      * Оправя телефонните номера
      */
     function toVerbal_($telNumber)
     {
         if(!$telNumber) return NULL;
-        
-        $Phones = cls::get('drdata_Phones');
-        
-        setIfNot($code, $this->params['code'], '359');
-        
-        $parsedTel = $Phones->parseTel($telNumber, $code);
-        
+
+        $parsedTel = static::toArray($telNumber, $this->params);
+
         $telNumber = parent::toVerbal_($telNumber);
-        
+
         if ($parsedTel == FALSE) {
             return "<font color='red'>{$telNumber}</font>";
         } else {
             $res = new ET();
             $value = '';
-            
+
             foreach($parsedTel as $t) {
-                
+
                 $res->append($add);
-                
+
                 $value = '';
-                
+
                 if($t->countryCode) {
                     $value .= '' . $t->countryCode;
                 }
-                
+
                 if($t->areaCode) {
                     $value .= '' . $t->areaCode;
                 }
-                
+
                 if($t->number) {
                     $value .= '' . $t->number;
                 }
-                
+
                 $attr = array();
-                
+
                 if(($t->country != 'Unknown') && ($t->area != 'Unknown') && $t->area && $t->country) {
                     $attr['title'] = "{$t->country}, {$t->area}";
                 } elseif(($t->country != 'Unknown') && $t->country) {
                     $attr['title'] = "{$t->country}";
                 }
-                
+
                 $title = str_replace(' ', '&nbsp;', $t->original);
-                
+
                 $res->append(ht::createLink($title, "tel:+" . $value, NULL, $attr));
-                
+
                 if($t->internal) {
                     $res->append(tr('вътр.') . $t->internal) ;
                 }
-                
+
                 $add = ", ";
             }
         }
-        
+
         return $res;
+    }
+
+
+    /**
+     * Конвертира списък от телефонни номера до масив
+     *
+     * @param string $str
+     * @param array $params
+     * @return array резултата е същия като на @see drdata_Phones::parseTel()
+     */
+    public static function toArray($str, $params = array())
+    {
+        $Phones = cls::get('drdata_Phones');
+
+        setIfNot($code, $params['code'], '359');
+
+        $result = $Phones->parseTel($str, $code);
+
+        return $result;
     }
 }
