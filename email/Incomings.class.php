fdba24793b887e3099e6f5c648c252ee6cd9e102
Yusein Yuseinov <yyuseinov@gmail.com>
2012-04-27 11:07:46 +0300

    Добавен е бутон за създаване на факсове

--- email/Incomings.class.php
+++ email/Incomings.class.php
@@ -117,7 +117,7 @@ class email_Incomings extends core_Master
      * Плъгини за зареждане
      */
     var $loadList = 'email_Wrapper, doc_DocumentPlg, plg_RowTools, 
-         plg_Printing, email_plg_Document, doc_EmailCreatePlg, plg_Sorting';
+         plg_Printing, email_plg_Document, doc_EmailCreatePlg, doc_FaxCreatePlg, plg_Sorting';
     
     
     /**
