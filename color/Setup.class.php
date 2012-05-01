6ed269bd5d19a50a8a84eead6091ea98cf42002a
Milen Georgiev <milen@ep-bags.com>
2012-05-01 23:40:00 +0300

    Setup на пакета color

---
+++ color/Setup.class.php
@@ -0,0 +1,73 @@
+<?php
+
+
+
+/**
+ * Клас 'color_Setup' - Избор на цвят от палитра
+ *
+ *
+ * @category  vendors
+ * @package   color
+ * @author    Milen Georgiev <milen@download.bg>
+ * @copyright 2006 - 2012 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class color_Setup extends core_Manager {
+    
+    
+    /**
+     * Версия на пакета
+     */
+    var $version = '0.1';
+    
+    
+    /**
+     * Мениджър - входна точка в пакета
+     */
+    var $startCtr = '';
+    
+    
+    /**
+     * Екшън - входна точка в пакета
+     */
+    var $startAct = '';
+    
+    
+    /**
+     * Описание на модула
+     */
+    var $info = "Избор на цят от палитра";
+    
+    
+    /**
+     * Инсталиране на пакета
+     */
+    function install()
+    {
+        // Зареждаме мениджъра на плъгините
+        $Plugins = cls::get('core_Plugins');
+        
+        // Инсталираме клавиатурата към password полета
+        $Plugins->installPlugin('Избор на цвят', 'jqcolorpicker_Plugin', 'color_Type', 'private');
+        $html .= "<li>Закачане към полетата за RGB цвят - color_Type (Активно)";
+        
+        return $html;
+    }
+    
+    
+    /**
+     * Де-инсталиране на пакета
+     */
+    function deinstall()
+    {
+        // Зареждаме мениджъра на плъгините
+        $Plugins = cls::get('core_Plugins');
+        
+        // Премахваме от type_Date полета
+        $Plugins->deinstallPlugin('jqcolorpicker_Plugin');
+        $html .= "<li>Премахнати са всички инсталации на 'jqcolorpicker_Plugin'";
+        
+        return $html;
+    }
+}
