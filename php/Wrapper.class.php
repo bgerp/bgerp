9dc971d2c6ce92cef86680c7af2cf18958903443
GPetrova <gab4eto@gmail.com>
2012-05-04 10:33:57 +0300

    Разделяне на кода на отделни класове и създаване на меню към пакета
    Change-Id: Icef5231ba9b4ac4b8915b0d5d552e26302c3d075

---
+++ php/Wrapper.class.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+/**
+ * Клас 'acc_Wrapper'
+ *
+ * Поддържа системното меню и табове-те на пакета 'Acc'
+ *
+ *
+ * @category  bgerp
+ * @package   acc
+ * @author    Milen Georgiev <milen@download.bg>
+ * @copyright 2006 - 2012 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ * @link
+ */
+class php_Wrapper extends bgerp_ProtoWrapper
+{
+    
+    var $tabMenu = 'Форматер = php_Formater::Process, 
+                    Константи = php_Const, Интерфейси = php_Interfaces,  
+                    Тест система = php_Test';
+
+}
