6bc2185bbf9a88a9a901f1320517d81cfc27243a
Dimitar Minekov <mitko@extrapack.com>
2012-05-11 17:00:24 +0300

    Правописна грешка

--- modbus/Tester.class.php
+++ modbus/Tester.class.php
@@ -41,7 +41,7 @@ class modbus_Tester extends core_Manager {
         $this->FLD('type', 'enum(words,float,double)', 'caption=Тип');
         $this->FLD('mode', 'enum(normal,debug,simulation)', 'caption=Режим');
         $this->FLD('data', 'text', 'caption=Данни');
-        $this->FLD('note', 'varchar(256)', 'caption=Режим');
+        $this->FLD('note', 'varchar(256)', 'caption=Забележка');
         
     }
     
