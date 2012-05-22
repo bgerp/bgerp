f8fda9962ae1bf13c812baa2ae4c1d293f93c64c
Yusein Yuseinov <yyuseinov@gmail.com>
2012-05-22 16:58:03 +0300

    cat_products_Params - Връщане на предишната версия.

--- cat/products/Params.class.php
+++ cat/products/Params.class.php
@@ -140,7 +140,7 @@ class cat_products_Params extends core_Detail
         }
         
         if (!$form->title) {
-            $form->title = "|*" . $productRec->name;
+            $form->title = "|*" . cat_Products::getVerbal($productRec, 'name');
         }
         
         $query = static::getQuery();
