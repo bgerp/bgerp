72567e7043bf0045d606e76f8eb35cb2e536802c
Stefan Stefanov <stefan.bg@gmail.com>
2012-06-04 17:07:58 +0300

    Добавен метод doc_Container::getDocumentByHandle()

--- doc/Containers.class.php
+++ doc/Containers.class.php
@@ -349,7 +349,7 @@ class doc_Containers extends core_Manager
      *
      * @param mixed int key(mvc=doc_Containers) или обект с docId и docClass
      * @param string $intf
-     * @return object
+     * @return core_ObjectReference
      */
     static function getDocument($id, $intf = NULL)
     {
@@ -370,6 +370,18 @@ class doc_Containers extends core_Manager
         expect($rec, $id);
         
         return new core_ObjectReference($rec->docClass, $rec->docId, $intf);
+    }
+    
+    
+    static function getDocumentByHandle($handle, $intf = NULL)
+    {
+        $rec = doc_RichTextPlg::parseDocHandle($handle);
+        
+        if (!$rec) {
+            return FALSE;
+        }
+        
+        return static::getDocument((object)$rec, $intf);
     }
     
     
