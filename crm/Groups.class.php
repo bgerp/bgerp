661336ce5d72938481930c2832540628de5dca0a
Milen Georgiev <milen@ep-bags.com>
2012-07-02 22:50:41 +0300

    debug

--- crm/Groups.class.php
+++ crm/Groups.class.php
@@ -110,7 +110,7 @@ class crm_Groups extends core_Manager
     static function on_AfterSetupMvc($mvc, &$res)
     {
         // BEGIN В случай, че няма данни в таблицата, зареждаме от масив.
-        if (!$mvc->fetch('1=1')) {
+        if (!$mvc->fetch('1=1')) { bp($mvc->fetch('1=1'));
             // BEGIN масив с данни за инициализация
             $data = array(
                 array(
@@ -148,7 +148,7 @@ class crm_Groups extends core_Manager
                 $rec->PersonsCnt = 0;
                 
                 $mvc->save($rec, NULL, 'ignore');
-                
+                 
                 $nAffected++;
             }
             
