e74e93fc7b2ac184ac6aa0ca7ad28dd10924e005
Yusein Yuseinov <yyuseinov@gmail.com>
2012-07-11 10:51:55 +0300

    zoho, gdocs, pixlr вече са отделни пакети

---
+++ gdocs/Setup.class.php
@@ -0,0 +1,72 @@
+<?php
+
+
+/**
+ * Инсталатор на плъгин за добавяне на бутона за преглед на документи в gdocs.com
+ * Разширения: doc,docx,xls,xlsx,ppt,pptx,pdf,pages,ai,tiff,dxf,svg,eps,ps,ttf,xps,zip,rar
+ *
+ * @category  vendors
+ * @package   gdocs
+ * @author    Yusein Yuseinov <yyuseinov@gmail.com>
+ * @copyright 2006 - 2012 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class gdocs_Setup extends core_Manager {
+    
+    
+    /**
+     * Версия на пакета
+     */
+    var $version = '0.1';
+    
+    
+    /**
+     * Мениджър - входна точка в пакета
+     */
+    var $startCtr = '';
+    
+    
+    /**
+     * Екшън - входна точка в пакета
+     */
+    var $startAct = '';
+    
+    
+    /**
+     * Описание на модула
+     */
+    var $info = "Преглед на документи с docs.google.com";
+    
+    
+    /**
+     * Инсталиране на пакета
+     */
+    function install()
+    {
+        // Зареждаме мениджъра на плъгините
+        $Plugins = cls::get('core_Plugins');
+        
+        // Инсталираме
+        $Plugins->forcePlugin('Преглед на документи с gdocs', 'gdocs_Plugin', 'fileman_Files', 'private');
+        $html .= "<li>Закачане на gdocs_Plugin към fileman_Files (Активно)";
+        
+        return $html;
+    }
+    
+    
+    /**
+     * Де-инсталиране на пакета
+     */
+    function deinstall()
+    {
+        // Зареждаме мениджъра на плъгините
+        $Plugins = cls::get('core_Plugins');
+        
+        // Премахваме от type_Keylist полета
+        $Plugins->deinstallPlugin('gdocs_Plugin');
+        $html .= "<li>Премахнати са всички инсталации на 'gdocs_Plugin'";
+        
+        return $html;
+    }
+}
