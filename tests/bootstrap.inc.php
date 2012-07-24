ea1d97623ff29b08a28999bb2a889094ceacd688
Stefan Stefanov <stefan.bg@gmail.com>
2012-07-24 17:18:00 +0300

    Тестване - възможност тестова DB конфигурация.

--- tests/bootstrap.inc.php
+++ tests/bootstrap.inc.php
@@ -5,13 +5,9 @@
  */
 require 'config/config.inc.php' ;
 
-if (!defined('EF_DONT_AUTORUN')) {
-    define('EF_DONT_AUTORUN', TRUE);
-}
-
-if (!defined('EF_APP_NAME')) {
-    define('EF_APP_NAME', basename(dirname(dirname(__FILE__))));
-}
+define('EF_DONT_AUTORUN', TRUE);
+define('EF_CONF_PATH', __DIR__ .'/config');
+define('EF_APP_NAME', basename(dirname(dirname(__FILE__))));
 
 if (!defined('EF_WEB_INDEX')) {
     die('Не е зададена входната точка на приложението' . PHP_EOL);
