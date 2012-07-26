1c08bb957f4158d2ed9c0105542562178d1d1858
Stefan Stefanov <stefan.bg@gmail.com>
2012-07-26 20:13:23 +0300

    Допълнени тестовете за oembed пакета - вграждане на снимки.

--- tests/oembed/PluginTest.php
+++ tests/oembed/PluginTest.php
@@ -7,6 +7,12 @@ class oembed_PluginTest extends framework_TestCase
         $result = oembed_Plugin::getEmbedHtml('http://www.youtube.com/watch?v=NWYOlMWmJbE&feature=fvst');
         $this->assertTrue(strpos($result, '<iframe') !== FALSE, (string)$result);
     }
+    
+    function testEmbedFlickr()
+    {
+        $result = oembed_Plugin::getEmbedHtml('http://www.flickr.com/photos/conorkeller/7643906142/');
+        $this->assertTrue(strpos($result, '<img') !== FALSE, (string)$result);
+    }
 
     function testGetEmbedHtmlNonEmbedable()
     {
