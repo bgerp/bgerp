226b394fb563f95703e252ee77de9f2b030a6e61
Stefan Stefanov <stefan.bg@gmail.com>
2012-07-26 16:50:58 +0300

    Тестов фреймуърк: метод за "приблизително" сравнение на масиви

--- tests/framework/TestCase.class.php
+++ tests/framework/TestCase.class.php
@@ -9,4 +9,18 @@ class framework_TestCase extends PHPUnit_Framework_TestCase
 		$Packs = cls::get('core_Packs');
 		$Packs->checkSetup();
 	}
+	
+	protected function assertArrayMatch($expected, $actual)
+	{
+	    $expected = (array)$expected;
+	    $actual   = (array)$actual;
+	    
+	    foreach ($actual as $key => $val) {
+	        if (!array_key_exists($key, $expected)) {
+	            unset($actual[$key]);
+	        }
+	    }
+	    
+	    return $this->assertEquals($expected, $actual);
+	}
 }
