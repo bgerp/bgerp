1c5c9a85ab910a109969949bfa2f4d2e5c89dec1
Stefan Stefanov <stefan.bg@gmail.com>
2012-07-31 19:05:09 +0300

    Нови тестове за пакета oembed.

--- tests/oembed/PluginTest.php
+++ tests/oembed/PluginTest.php
@@ -2,6 +2,14 @@
 
 class oembed_PluginTest extends framework_TestCase
 {
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        oembed_Cache::delete('1=1');
+    }
+    
+    
     function testGetEmbedHtml()
     {
         $result = oembed_Plugin::getEmbedHtml('http://www.youtube.com/watch?v=NWYOlMWmJbE&feature=fvst');
@@ -10,8 +18,23 @@ class oembed_PluginTest extends framework_TestCase
     
     function testEmbedFlickr()
     {
-        $result = oembed_Plugin::getEmbedHtml('http://www.flickr.com/photos/conorkeller/7643906142/');
+        $flickrUrl = 'http://www.flickr.com/photos/conorkeller/7643906142/';
+        
+        $result = oembed_Plugin::getEmbedHtml($flickrUrl);
         $this->assertTrue(strpos($result, '<img') !== FALSE, (string)$result);
+        
+        // Тестваме дали резултата е кеширан правилно
+        $cacheRec = oembed_Cache::fetch("#url = '{$flickrUrl}'");
+        
+        $this->assertObjectHasAttribute('html', $cacheRec);
+        $this->assertEquals($result, $cacheRec->html);
+        $this->assertEquals('http://www.flickr.com/services/oembed/', $cacheRec->provider);
+    }
+    
+    function testEmbedVbox7()
+    {
+        $result = oembed_Plugin::getEmbedHtml('http://vbox7.com/play:7981015ce8');
+        $this->assertContains('<object', (string)$result, (string)$result, TRUE);
     }
 
     function testGetEmbedHtmlNonEmbedable()
