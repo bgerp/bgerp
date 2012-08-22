993f8ef8a0c61d046c277798f2e90847cc94b523
Dimitar Minekov <mitko@extrapack.com>
2012-08-22 18:11:02 +0300

    Работа по SETUP-a

--- core/Setup.inc.php
+++ core/Setup.inc.php
@@ -1,33 +1,50 @@
-<?php
-echo ("<h2>Първоначално установяване на основните параметри</h2>");
+<?php 
+if ($_GET['a'] == 'blank') exit;
 
-/**
- * Масив съдържащ всички активни php модули, необходими за правилна работа на ситемата
- */
+echo ("<h2>Задаване на основните параметри</h2>");
+
+/**********************************
+ * Проверка за конфигурационен файл
+ **********************************/
+
+$filename = EF_ROOT_PATH . '/conf/' . EF_APP_NAME . '.cfg.php';
+
+if(file_exists($filename)) {
+	include($filename);
+} else {
+   	echo "<li style='color: red;'>Грешка: Липсва конфигурационен файл $filename</li>";
+}
+
+/**********************************
+ * Проверка за връзка към MySql-a
+ **********************************/
+
+if (defined('EF_DB_USER') && defined('EF_DB_HOST') && defined('EF_DB_PASS')) {
+	if (FALSE !== mysql_connect(EF_DB_HOST, EF_DB_USER, EF_DB_PASS)) {
+		echo("<li style='color: green;'>Успешна връзка с базата данни</li>");
+	} else {
+		echo("<li style='color: red;'>Неуспешна връзка с базата данни</li>");
+	}
+} else {
+	echo("<li style='color: red;'>Недефинирани константи за връзка с базата данни</li>");
+}
+
+/**********************************
+ * Проверка за необходимите модули на PHP-то
+ **********************************/
+
+// Масив съдържащ всички активни php модули, необходими за правилна работа на ситемата
+// за сега периодично се попълва ръчно
 $requiredPhpModules = array('calendar', 'Core', 'ctype', 'date', 'ereg',
 							'exif', 'filter', 'ftp', 'gd', 'iconv', 'json',
 							'mbstring', 'mysql', 'pcre', 'session', 'SimpleXML',
 							'SPL', 'standard', 'tokenizer', 'xml', 'zlib', 'soap');
 
-/**
- * Масив съдържащ всички активни apach модули, необходими за правилна работа на ситемата
- */
-$requiredApacheModules = array('core', 'mod_headers', 'mod_mime', 'mod_php5', 'mod_rewrite');
-
-/**
- * Масив с липсващите php и/или apache модули, 
- * без които системата не работи коректно
- */
-$error = array();
-/**
- * Масив с всички активни php модули
- */
 $activePhpModules = get_loaded_extensions();
 
 echo ("<h3>Проверка за PHP модули ...</h3>");
 
-foreach($requiredPhpModules as $required){
-	
+foreach($requiredPhpModules as $required) {
 	if(!in_array($required, $activePhpModules)){
 		echo ("<li style='color: red;'> модул: $required - не е инсталиран!</li>");
 		flush();
@@ -37,9 +54,14 @@ foreach($requiredPhpModules as $required){
 	}
 }
 
-/**
- * Масив с всички активни apache модули
- */
+/**********************************
+ * Проверка за необходимите модули на PHP-то
+ **********************************/
+
+// Масив съдържащ всички активни apache модули, необходими за правилна работа на ситемата
+
+$requiredApacheModules = array('core', 'mod_headers', 'mod_mime', 'mod_php5', 'mod_rewrite');
+
 $activeApacheModules = apache_get_modules();
 
 echo ("<h3>Проверка за Apache модули ...</h3>");
@@ -54,38 +76,13 @@ foreach($requiredApacheModules as $requiredA){
 		flush();
 	}
 }
+?>
+<input type="button" onclick="test.location.href='<?php "http://".$_SERVER['SERVER_NAME']."/?SETUP"?>';document.getElementById('test').frameBorder=1" value="Провери">
+<input type="button" onclick="test.location.href='<?php "http://".$_SERVER['SERVER_NAME']."/?SETUP&a=blank"?>';document.getElementById('test').frameBorder=0" value="Clear">
+<iframe src='<?php "http://".$_SERVER['SERVER_NAME']."/?SETUP&a=blank"?>' frameborder="1" name="test" id="test" width=800 height=400></iframe>
 
-/**
- * Път до конфигурационния файл
- */
-
-$filename = EF_ROOT_PATH . '/conf/' . EF_APP_NAME . '.cfg.php';
-
-
-if(file_exists($filename)){
-	
-	echo "<li style='color: green;'> Конфигурационния файл $filename съществува.</li>";
-	
-	$str = file_get_contents($filename);
-	
-	if(strpos($str, '[#') === FALSE){
-		
-		echo "<li style='color: green;'> All constants are defined.</li>";
-		
-	} else {
-		
-		echo "<li style='color: red;'>Some constants are not defined.</li>";
-		
-	}
-	
-} else {
-	
-   		echo "<li style='color: red;'>Error: Missing configuration file $filename</li>";
-    
-	}
-	
-
-
+<?php
+/*
 if (count($error) > 0){
 	
 	print_r($error)."\n";
@@ -98,3 +95,4 @@ if (count($error) > 0){
     die('Everything is OK');
     
 }
+*/
