96aa81bf8e5cabb710efbc070a34f3c51a7b1993
Dimitar Minekov <mitko@extrapack.com>
2012-08-22 10:26:37 +0300

    Добавени драйвери за машини D8-16

--- sens/driver/TCW121D8.class.php
+++ sens/driver/TCW121D8.class.php
@@ -30,8 +30,7 @@ class sens_driver_TCW121 extends sens_driver_IpDevice
         'InA1' => array('unit'=>'InA1', 'param'=>'Аналогов вход 1', 'details'=>'V', 'xmlPath'=>'/Entry[3]/Value[1]'),
         'InA2' => array('unit'=>'InA2', 'param'=>'Аналогов вход 2', 'details'=>'V', 'xmlPath'=>'/Entry[4]/Value[1]'),
         'RPM' => array('unit'=>'RPM', 'param'=>'Удари в минута', 'details'=>'rpm'),
-        'P' => array('unit'=>'P', 'param'=>'Налягане', 'details'=>'BAR'),
-    // Oписваме и изходите за да можем да ги следим в логовете
+        // Oписваме и изходите за да можем да ги следим в логовете
         'Out1' => array('unit'=>'Out1', 'param'=>'Състояние изход 1', 'details'=>'(ON,OFF)', 'xmlPath'=>'/Entry[9]/Value[1]'),
         'Out2' => array('unit'=>'Out2', 'param'=>'Състояние изход 2', 'details'=>'(ON,OFF)', 'xmlPath'=>'/Entry[10]/Value[1]')
     
@@ -133,10 +132,7 @@ class sens_driver_TCW121 extends sens_driver_IpDevice
             }
         }
         
-        $state['RPM'] = round(($state['InA1']/4.2)*100); // За D19
-        if ($state['InA2'] >= 1) {
-        	$state['P'] = (($state['InA2'] - 1)/0.2)*0.5;
-        }
+        $state['RPM'] = round(($state['InA1']/4)*100);
         
         $this->stateArr = $state;
         
