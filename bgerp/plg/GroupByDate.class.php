ab476665b58d198e24de3141e5aa2eea1b7d5ad5
Milen Georgiev <milen@experta.bg>
2012-09-08 15:03:04 +0300

    Добавен плъгина bgerp_plg_GroupByDate за групиране на листовия изглед

---
+++ bgerp/plg/GroupByDate.class.php
@@ -0,0 +1,71 @@
+<?php
+
+
+
+/**
+ * Плъгин за групиране на редовете от листовия изглед, според датата
+ *
+ *
+ * @category  bgerp
+ * @package   bgerp
+ * @author    Milen Georgiev <milen@experta.bg>
+ * @copyright 2006 - 2012 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ * @title     Групиране на редовете от листовия изглед, според датата
+ */
+class bgerp_plg_GroupByDate extends core_Plugin
+{
+    
+    /**
+     * Изпълнява се след подготовката на редовете за листовия изглед
+     */
+    static function on_AfterPrepareListRows($mvc, &$res, $data)
+    {   
+
+        setIfNot($data->groupByDateField, $mvc->groupByDateField);
+
+        if(!($field = $data->groupByDateField)) return;
+
+        $columns = count(arr::make($data->listFields));
+
+        if(!count($data->recs)) return;
+
+        $exDate = '0000-00-00';
+        
+        foreach($data->recs as $id => $rec) {
+
+            list($d, $t) = explode(' ', $rec->{$field});
+
+            if($d != $exDate) {
+
+                $day = dt::getRelativeDayName($rec->{$field});
+                if($day) $day .= ', ';
+                $day .= dt::mysql2verbal($rec->{$field}, 'd F-YEAR, l');
+
+                $color = dt::getColorByTime(time() - dt::mysql2timestamp($rec->{$field}));
+
+                $style = $data->rows[$id]->ROW_ATTR['style'];
+
+                $rows[$id . ' '] = ht::createElement('tr', 
+                    $data->rows[$id]->ROW_ATTR, 
+                    new ET("<td style='padding-top:9px;padding-left:5px;' colspan='{$columns}'><i style='color:#{$color};'>" . $day . "</i></td>")); 
+                    
+                $exDate = $d;
+            }
+
+            $rows[$id] = $data->rows[$id];
+                        
+            if(trim($t) && ($t != '00:00:00')) {
+                list($h, $m, $s) = explode(':', $t);
+                $color = dt::getColorByTime(time() - dt::mysql2timestamp($rec->{$field}));
+                $rows[$id]->{$field} = "<span style='color:#{$color}'>{$h}:{$m}</span>";
+            } else {
+                $rows[$id]->{$field} = '';
+            }
+        }
+
+        $data->rows = $rows;
+    }
+
+}
