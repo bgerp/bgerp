61ffee3a5c733582df9a79065ea5647dd0a78492
Milen Georgiev <milen@experta.bg>
2012-09-09 15:18:28 +0300

    Рефакторинг на type_Combodate

--- type/Combodate.class.php
+++ type/Combodate.class.php
@@ -163,16 +163,16 @@ class type_Combodate extends type_Varchar {
      */
     static function create($y, $m, $d)
     {
-        $d = str_pad($d, 2, '0', STR_PAD_LEFT);
-        $m = str_pad($m, 2, '0', STR_PAD_LEFT);
-        $y = str_pad($y, 4, '0', STR_PAD_LEFT);
+        static::padParts($y, $m, $d);
+
+        $u = self::UNKNOWN;
 
         $div = self::DIV;
 
         $date = "{$y}{$div}{$m}{$div}{$d}";
         
         // Очакваме, че датата е получена във вътрешния ни формат
-        expect(preg_match("/^[\?0-9]{4}{$div}[\?0-9]{2}{$div}[\?0-9]{2}$/", $date), $date);
+        expect(preg_match("/^[\{$u}0-9]{4}{$div}[\{$u}0-9]{2}{$div}[\{$u}0-9]{2}$/", $date), $date);
 
         return $date;
     }
@@ -199,6 +199,17 @@ class type_Combodate extends type_Varchar {
             $y = $t;
         }
         
+        static::padParts($y, $m, $d);
+
+        return array($y, $m, $d);
+    }
+
+
+    /**
+     * Форматира частите на комбодатата
+     */
+    static function padParts(&$y, &$m, &$d)
+    {
         if($d>0) {
             $d = str_pad($d, 2, '0', STR_PAD_LEFT);
         } else {
@@ -217,7 +228,6 @@ class type_Combodate extends type_Varchar {
             $y = str_pad('', 4, self::UNKNOWN, STR_PAD_LEFT);
         }
 
-        return array($y, $m, $d);
     }
 
 
@@ -254,15 +264,19 @@ class type_Combodate extends type_Varchar {
         if($this->days) {
             return;
         }
+        
+        $y = 0; $m = 0; $d = 0;
+
+        static::padParts($y, $m, $d);
 
         // Подготовка на дните
-        $this->days = array('??' => '');
+        $this->days = array($d => '');
         for($i = 1; $i <= 31; $i++) {
             $this->days[$i] = $i;
         }
         
         // Подготовка на месеците
-        $this->months = array('??' => '') + dt::getMonthOptions('FM');
+        $this->months = array($m => '') + dt::getMonthOptions('FM');
         
         // Подготовка на годините
         $min = $this->params['minYear'] ? $this->params['minYear'] : 1900;
@@ -271,7 +285,7 @@ class type_Combodate extends type_Varchar {
         for($i = $max; $i > $cur; $i--) {
             $this->years[$i] = $i;
         }
-        $this->years['????'] = '';
+        $this->years[$y] = '';
         for($i = $cur; $i >= $min; $i--) {
             $this->years[$i] = $i;
         }
