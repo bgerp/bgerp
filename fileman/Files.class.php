fa1096463a6ae097dc1db1984bb234a5de82af2d
Yusein Yuseinov <yyuseinov@gmail.com>
2012-09-12 15:04:23 +0300

    fileman_webdrv - Клас за визуализиране на gif файловете.

--- fileman/Files.class.php
+++ fileman/Files.class.php
@@ -651,43 +651,8 @@ class fileman_Files extends core_Master
         $ext = self::getExt($data->rec->name);
 
         // Проверяваме дали разширението, предлага preview на файла
-        if (in_array($ext, array('gif'))) {
+        if (in_array($ext, array(''))) {
 
-            //Вземема конфигурационните константи
-            $conf = core_Packs::getConfig('fileman');
-            
-            // В зависимост от широчината на екрана вземаме размерите на thumbnail изображението
-            if (mode::is('screenMode', 'narrow')) {
-                $thumbWidth = $conf->FILEMAN_PREVIEW_WIDTH_NARROW;
-                $thumbHeight = $conf->FILEMAN_PREVIEW_HEIGHT_NARROW;
-            } else {
-                $thumbWidth = $conf->FILEMAN_PREVIEW_WIDTH;
-                $thumbHeight = $conf->FILEMAN_PREVIEW_HEIGHT;
-            }
-            
-            // Атрибути на thumbnail изображението
-            $attr = array('baseName' => 'Preview', 'isAbsolute' => FALSE, 'qt' => '');
-                
-            //Размера на thumbnail изображението
-            $size = array($thumbWidth, $thumbHeight);
-            
-            //Създаваме тумбнаил с параметрите
-            $thumbnailImg = thumbnail_Thumbnail::getImg($fh, $size, $attr);
-            
-            if ($thumbnailImg) {
-                
-                // Background' а на preview' то
-                $bgImg = sbf('fileman/img/Preview_background.jpg');
-            
-                // Създаваме шаблон за preview на изображението
-                $preview = new ET("<fieldset style='max-width:900px;'><legend>Преглед</legend><div style='background-image:url(" . $bgImg . "); padding:10px 0; '><div style='margin: 0 auto; display:table;'>[#THUMB_IMAGE#]</div></div></fieldset>");
-                
-                // Добавяме към preview' то генерираното изображение
-                $preview->append($thumbnailImg, 'THUMB_IMAGE');
-                
-                 
-            }
-        
         } elseif( in_array($ext, array('html', 'htm')) ) { 
             $dUrl = fileman_Download::getDownloadUrl($data->rec->fileHnd);
             $preview = new ET("<fieldset style='max-width:900px;'><legend>Преглед</legend><iframe src='{$dUrl}' frameBorder='0' ALLOWTRANSPARENCY='true' style='width:100%; min-height:600px;border:solid 0px transparent;'></iframe></fieldset>");
