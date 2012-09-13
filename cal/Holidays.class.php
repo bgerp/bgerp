8b7535dd8b0d5fa04e9012917b71fe13abf7946e
GPetrova <gab4eto@gmail.com>
2012-09-13 17:39:46 +0300

    Проба за reachtext

--- cal/Holidays.class.php
+++ cal/Holidays.class.php
@@ -246,7 +246,7 @@ class cal_Holidays extends core_Master
 										ZA=ЮАР,
 										JM=Ямайка,
 										JP=Япония)', 'caption=Празник->Тип,export');
-        $this->FLD('info', 'text', 'caption=Празник->Данни,export');
+        $this->FLD('info', 'reachtext', 'caption=Празник->Данни,export');
         
         $this->FLD('nameday', 'text', 'caption=Именници,export');
         
