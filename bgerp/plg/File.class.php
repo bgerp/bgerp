b346a119f3557744e5184be484fd272ed4b6dcf7
Yusein Yuseinov <yyuseinov@gmail.com>
2012-09-26 17:17:21 +0300

    Плъгин за прихващане на генерирането на линк за сваляне на файл.

---
+++ bgerp/plg/File.class.php
@@ -0,0 +1,35 @@
+<?php
+
+
+/**
+ * Плъгин за работа с файлове
+ *
+ * @category  bgerp
+ * @package   bgerp
+ * @author    Yusein Yuseinov <yyuseinov@gmail.com>
+ * @copyright 2006 - 2012 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class bgerp_plg_File extends core_Plugin
+{
+    
+    
+    /**
+     * Прихващаме генерирането на имейл.
+     * Ако мода е xhtml, тогава сработва и прекъсва по нататъшното изпълнение на функцията.
+     */
+    function on_BeforeGenerateUrl($mvc, &$res, $fh)
+    {
+        // Ако не се изпраща имейла, да не сработва
+        if (!Mode::is('text', 'xhtml')) return ;
+
+        // Името на файла
+        $name = fileman_Files::fetchByFh($fh, 'name');
+
+        //Генерираме връзката 
+        $res = toUrl(array('F', 'S', doc_DocumentPlg::getMidPlace(), 'n' => $name), TRUE);
+
+        return FALSE;
+    }
+}
