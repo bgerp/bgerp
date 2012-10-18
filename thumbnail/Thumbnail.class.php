1800d36882d8570c89501149e0534cdfd2043c5c
Yusein Yuseinov <yyuseinov@gmail.com>
2012-10-18 16:21:32 +0300

    thumbnail_Thumbnail - Ако няма isAbsolute, тогава вземаме от mode

--- thumbnail/Thumbnail.class.php
+++ thumbnail/Thumbnail.class.php
@@ -67,6 +67,10 @@ class thumbnail_Thumbnail extends core_Manager {
         // Очакваме да е от познатите разширения за растерни файлове
         expect($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif' || $ext == 'bmp', $ext);
         
+        // Ако не са зададени параметрите
+        if (!isset($attr['qt'])) $attr['qt'] = '"';
+        if (!isset($attr['isAbsolute'])) $attr['isAbsolute'] = Mode::is('text', 'xhtml') || Mode::is('printing');
+        
         if(is_array($size)) {
             $thumbFilePath = THUMBNAIL_FOLDER . "/" . $baseName . "-" . $fh . "-" . $size[0] . "-" . $size[1] . "." . $ext;
             $thumbFileUrl = sbf(THUMBNAIL_URL . "/" . $baseName . "-" . $fh . "-" . $size[0] . "-" . $size[1] . "." . $ext, $attr['qt'], $attr['isAbsolute']);
