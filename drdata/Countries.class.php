1789d4093f38e0c62432b4d69d583f6d6d1325df
Milen Georgiev <milen@experta.bg>
2012-10-21 12:28:52 +0300

    Добавяне на говоримия език към drdata_Countries

--- drdata/Countries.class.php
+++ drdata/Countries.class.php
@@ -34,7 +34,13 @@ class drdata_Countries extends core_Manager {
      */
     var $loadList = 'drdata_Wrapper,plg_Sorting';
     
-    
+
+    /**
+     * Масив за съхранение на съответствието Държава => Езици
+     */
+    static $countryToLanguages = array();
+
+
     /**
      * Описание на модела (таблицата)
      */
@@ -45,13 +51,14 @@ class drdata_Countries extends core_Manager {
         $this->FLD('type', 'varchar', 'caption=Тип');
         $this->FLD('sovereignty', 'varchar', 'caption=Суверинитет');
         $this->FLD('capital', 'varchar', 'caption=Столица');
-        $this->FLD('currencyCode', 'varchar(3)', 'caption=валута->код');
-        $this->FLD('currencyName', 'varchar', 'caption=валута->име');
-        $this->FLD('telCode', 'varchar(3)', 'caption=ITU-T телефонен код');
-        $this->FLD('letterCode2', 'varchar(3)', 'caption=2,rem=ISO 3166-1 2 буквен код');
-        $this->FLD('letterCode3', 'varchar(3)', 'caption=3, rem=ISO 3166-1 3 буквен код');
-        $this->FLD('isoNumber', 'int', 'caption=3, rem=ISO Номер');
-        $this->FLD('domain', 'varchar(4)', 'caption=разширение, rem=IANA Country Code TLD');
+        $this->FLD('currencyCode', 'varchar(3)', 'caption=Валута->Код');
+        $this->FLD('currencyName', 'varchar', 'caption=Валута->Име');
+        $this->FLD('telCode', 'varchar(3)', 'caption=Tел. код');
+        $this->FLD('letterCode2', 'varchar(3)', 'caption=ISO 3166-1->2,rem=ISO 3166-1 2 буквен код');
+        $this->FLD('letterCode3', 'varchar(3)', 'caption=ISO 3166-1->3, rem=ISO 3166-1 3 буквен код');
+        $this->FLD('isoNumber', 'int', 'caption=ISO 3166-1->N, rem=ISO Номер');
+        $this->FLD('domain', 'varchar(4)', 'caption=TLD, rem=IANA Country Code TLD');
+        $this->FNC('languages', 'varchar(2)', 'caption=Езици');
         $this->load('plg_RowTools');
         
         $this->setDbUnique('commonName');
@@ -59,6 +66,34 @@ class drdata_Countries extends core_Manager {
         $this->setDbIndex('letterCode3');
     }
     
+
+    /**
+     * Попълва езиците, които се говорят в дадена страна
+     */
+    function on_CalcLanguages($mvc, &$rec)
+    {
+        if(!self::$countryToLanguages) {
+            self::$countryToLanguages = arr::make(  
+               'af=fa|ps,ax=sv,al=sq,dz=ar,as=sm,ad=ca,ao=pt,ai=en,aq=en|fr|es|no|nn,ag=en,ar=es,am=hy,aw=nl,ac=en,au=en,at=de,az=az,bs=en,
+               bh=ar,bd=bn,bb=en,by=be,be=de|fr|nl|wa,bz=en,bj=fr,bm=en,bt=dz,bo=es,ba=bs,bw=en,br=pt,io=en,bn=ms,bg=bg,bf=fr,bi=fr,kh=km,
+               cm=en|fr,ca=en|fr,cv=pt,ky=en,cf=fr,td=fr|ar,cl=es,cn=zh,cx=en,cc=ms|en,co=es,km=ar|fr,cg=fr,cd=fr,ck=en|mi,cr=es,ci=fr,
+               hr=hr,cu=es,cy=el|tr,cz=cs,dk=da|en,dj=fr|ar,dm=en,do=es,ec=es,eg=ar,sv=es,gq=es|fr,er=ti,ee=et,et=am|ti,fk=en,fo=fo,
+               fj=en|fj,fi=fi|sv,fr=br|fr|oc,gf=fr,pf=fr,tf=fr,ga=fr,gm=en|wo|ff,ge=ka,de=de,gh=en,gi=en|es|it|pt,gr=el,gl=kl,gd=en,
+               gp=fr,gu=en|ch,gt=es,gg=en|fr,gn=fr,gw=pt,gy=en,ht=fr,va=it|la,hn=es,hk=en|zh,hu=hu,is=is,in=ar|bn|en|hi|mr|ta|te,id=id,
+               ir=fa,iq=ar,ie=en|ga,im=en|gv,il=he,it=it,jm=en,jp=ja,je=en|pt,jo=ar,kz=kk|ru,ke=en,ki=en,kp=ko,kr=ko,kw=ar,kg=ky|ru,la=lo,
+               lv=lv,lb=ar,ls=st|en|zu|xh,lr=en,ly=ar,li=de,lt=lt,lu=de|fr,mo=zh,mk=mk,mg=fr|mg,mw=ny,my=ms,mv=dv,ml=fr|bm,mt=mt,mh=mh,
+               mq=fr,mr=ar,mu=en|fr,yt=fr,mx=es,fm=en,md=mo|ru,mc=fr|it|en,mn=mn,me=sr,ms=en,ma=ar,mz=pt,mm=my,na=en|af|de,nr=na|en,
+               np=ne|en,nl=nl,an=nl,nc=fr,nz=en|mi,ni=es,ne=fr,ng=en|ha|yo|ig|ff,nu=en,nf=en,mp=ch|en,no=nn|no|se,om=ar,pk=ur,pw=en,
+               ps=ar,pa=es,pg=en,py=es,pe=es,ph=en|tl,pn=en,pl=pl,pt=pt,pr=es,qa=ar,re=fr,ro=ro,ru=ru|tt,rw=rw,bl=fr,sh=en,kn=en,lc=en,
+               pm=fr,vc=en,ws=sm|en,sm=it,st=pt,sa=ar,sn=fr|wo,rs=sr,sc=en,sl=en,sg=en|sg|zh,sk=sk,si=sl,sb=en,so=so,
+               za=en|af|zu|xh|tn|st|ss|nr,gs=en,es=ca|es|eu|gl,lk=si|ta,sd=ar,sr=nl,sj=no|ru,sz=en|ss,se=sv,ch=de|fr|it,sy=ar,tw=zh,
+               tj=tg,tz=sw,th=th,tl=pt,tg=fr|ee,tk=en,to=to|en,tt=en,tn=ar,tr=tr,tm=tk,tc=en,tv=tv,ug=en,ua=uk,ae=ar,gb=cy|en|gd|gv|kw,
+               us=en,um=en,uy=es,uz=uz,vu=bi|en,ve=es,vn=vi,vg=en,vi=en,wf=fr,eh=ar,ye=ar,yu=mk|sl|hr,zm=en,zw=en');
+        }
+
+        $rec->languages = str_replace('|', ',', self::$countryToLanguages[strtolower($rec->letterCode2)]);
+    }
+
     
     /**
      * Връща id-то на държавата от която посоченото или текущото ip
