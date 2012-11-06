1fb66687f3456776e6efd6b82603b8cd7c63cf55
Milen Georgiev <milen@experta.bg>
2012-11-06 11:32:07 +0200

    Поправката на папките не се притеснява от липса на класове - корици

--- doc/Folders.class.php
+++ doc/Folders.class.php
@@ -652,7 +652,7 @@ class doc_Folders extends core_Master
                 $projectName = "LaF " . $rec->title;
             } else {
             
-                if(!($className = core_Classes::fetchField("#id = {$rec->coverClass} && #state = 'active'", 'name'))) {
+                if(!($cls =  cls::load($rec->coverClass, TRUE)) {
                     $err[$rec->id] .= 'Not exists coverClass; ';
                     $projectName = "LaF " . $rec->title;
                 } else {
