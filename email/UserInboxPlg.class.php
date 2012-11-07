b64a623e40aab3e87d298449200715e5bf4dffe1
Milen Georgiev <milen@experta.bg>
2012-11-07 12:09:55 +0200

    Refact

--- email/UserInboxPlg.class.php
+++ email/UserInboxPlg.class.php
@@ -197,12 +197,12 @@ class email_UserInboxPlg extends core_Plugin
 
 
     /**
-     *
+     * Определяне на правата за действия над потребителите
      */
     function on_AfterGetRequiredRoles($mvc, &$roles, $action, $uRec, $user = NULL)
     {
         if($action == 'delete') {
-            if(is_object($uRec) && $uRec->state != 'draft') {
+            if(is_object($uRec) && ($uRec->state != 'draft')) {
                 $roles = 'no_one';
             }
         }
