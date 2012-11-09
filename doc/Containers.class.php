b22eaf096f8326c4b75e3f986c864bd7096e132d
Yusein Yuseinov <yyuseinov@gmail.com>
2012-11-09 17:48:28 +0200

    bugFix при определяне на правата в getVerbalLink

--- doc/Containers.class.php
+++ doc/Containers.class.php
@@ -750,7 +750,7 @@ class doc_Containers extends core_Manager
             expect($field = $ctrInst->rowToolsSingleField);
 
             // Очакваме да имаме права за съответния екшън
-            expect($ctrInst->haveRightFor($params['Act'], $rec), 'Нямате права за разглеждане');
+            expect($ctrInst->haveRightFor('single', $rec), 'Нямате права за разглеждане');
         } catch (core_exception_Expect $e) {
             
             // Ако възникне някаква греша
