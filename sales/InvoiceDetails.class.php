4d0597e287ad70c539492a9ee93bb93102ab6daf
Stefan Stefanov <stefan.bg@gmail.com>
2012-12-05 18:37:59 +0200

    Детайли на фактура: валидиране на формата за добавяне / промяна

--- sales/InvoiceDetails.class.php
+++ sales/InvoiceDetails.class.php
@@ -25,7 +25,7 @@ class sales_InvoiceDetails extends core_Detail
     /**
      * Плъгини за зареждане
      */
-    var $loadList = 'plg_RowTools, plg_Created, sales_Wrapper';
+    var $loadList = 'plg_RowTools, plg_Created, sales_Wrapper, plg_RowNumbering';
     
     
     /**
@@ -43,7 +43,7 @@ class sales_InvoiceDetails extends core_Detail
     /**
      * Полета, които ще се показват в листов изглед
      */
-    var $listFields = 'invoiceId, actionType, invPeraId, orderId, note,  productId, unit, quantity, price, amount, tools=Пулт';
+    var $listFields = 'itemId, quantity, unit, price, amount, note, tools=Пулт';
     
     
     /**
@@ -69,18 +69,20 @@ class sales_InvoiceDetails extends core_Detail
      */
     function description()
     {
-        $this->FLD('invoiceId', 'key(mvc=sales_Invoices)', 'caption=Поръчка, input=hidden, silent');
-        $this->FLD('actionType', 'enum(sale,downpayment,deduct,discount)', 'caption=Тип');
-        $this->FLD('itemId', 'acc_type_Item', 'caption=Пера');
-        $this->FLD('orderId', 'int', 'caption=Поръчка');
-        $this->FLD('note', 'text', 'caption=Пояснение');
-        $this->FLD('productId', 'key(mvc=cat_Products, select=name)', 'caption=Продукт');
-        $this->FLD('unit', 'key(mvc=cat_UoM, select=name)', 'caption=Мярка');
-        $this->FLD('quantity', 'double(decimals=4)', 'caption=Количество');
-        $this->FLD('price', 'double(decimals=2)', 'caption=Ед. цена');
-        $this->FNC('amount', 'double(decimals=2)', 'caption=Сума, column');
+        $this->FLD('invoiceId', 'key(mvc=sales_Invoices)', 'caption=Фактура, input=hidden, silent');
+        $this->FLD('itemId', 'acc_type_Item', 'caption=Перо,mandatory');
+        $this->FLD('itemType', 'enum(standard, custom)', 'caption=Тип, input=hidden, silent');
+        $this->FLD('quantity', 'double(decimals=4)', 'caption=Количество,mandatory');
         
-        $this->setDbUnique('invoiceId, productId');
+        $this->FLD('priceType', 'enum(policy=По ценова политика, history=По предишна цена, input=Въведена цена)', 'caption=Ценообразуване, input=none');
+        $this->FLD('price', 'double(decimals=2)', 'caption=Ед. цена, input=none');
+        
+        $this->FLD('note', 'varchar(64)', 'caption=@Пояснение', array('attr'=>array('rows'=>2)));
+        
+        $this->FNC('amount', 'double(decimals=2)', 'caption=Сума, column');
+        $this->FNC('unit', 'varchar', 'caption=Мярка');
+        
+        $this->setDbUnique('invoiceId, itemId');
     }
     
     
@@ -92,21 +94,98 @@ class sales_InvoiceDetails extends core_Detail
      */
     static function on_CalcAmount($mvc, $rec)
     {
-        $rec->amount = round($rec->priceForOne * $rec->quantity, 2);
+        $rec->amount = round($rec->price * $rec->quantity, 2);
     }
     
+    
     /**
+     * Изчислява полето 'unit' (мярка на перото)
      *
+     * @param core_Mvc $mvc
+     * @param stdClass $rec
      */
-    function on_AfterPrepareDetail1($mvc, &$res, $data)
-    { 
-       $query = $this->getQuery();
-
-       while($rec = $query->fetch("#invoiceId = {$data->masterId}")) {
-           $data->recs[$rec->id] = $rec;
-       }
+    static function on_CalcUnit($mvc, $rec)
+    {
+        if ($rec->itemId) {
+            $itemRec = acc_Items::fetch($rec->itemId);
+            $rec->unit = acc_Items::getVerbal($itemRec, 'uomId');
+        }
+    }
+    
+    
+    /**
+     * Подготовка на бутоните за добавяне на нови редове на фактурата 
+     * 
+     * @param core_Mvc $mvc
+     * @param stdClass $data
+     */
+    public static function on_AfterPrepareListToolbar($mvc, $data)
+    {
+        $data->toolbar->removeBtn('*');
+        
+        $addUrl = array($mvc, 'add', $mvc->masterKey=>$data->masterId, 'ret_url'=>true);
+        
+        $data->toolbar->addBtn('Стандартен продукт', 
+            $addUrl + array('itemType'=>'standard'), 
+            array('class'=>'btn-add')
+        );
+    }
+    
+    
+    public static function on_AfterPrepareEditForm($mvc, $data)
+    {
+        /* @var $form core_Form */
+        $form = $data->form;
+        
+        switch ($form->rec->itemType)
+        {
+            case 'standard':
+                $itemField = $form->getField('itemId');
+                $itemField->type->params['lists'] = 'standardProducts|goods';
+                
+                $form->setField('priceType, price', 'input=input');
+                
+                break;
+            case 'custom':
+            default:
+                bp('TODO ...');
+        }
     }
 
+
+    public static function on_AfterInputEditForm(core_Mvc $mvc, core_Form $form)
+    {
+        if (!$form->isSubmitted()) {
+            return;
+        }
+        
+        $mvc::validatePrice($form);
+    }
+    
+    public static function validatePrice(core_Form $form)
+    {
+        $rec = $form->rec;
+        
+        if ($rec->priceType != 'input' && trim($rec->price) != '') {
+            $form->setWarning('price',
+                'Цената ще бъде изчислена автоматично. Въведената цена 
+                ще бъде игнорирана!'
+            );
+        }
+        
+        if ($rec->priceType == 'input' && !$rec->price) {
+            $form->setError('price',
+                'Полето е задължително'
+            );
+        }
+    }    
+    
+    
+    public static function on_BeforeSave($mvc, $id, $rec)
+    {
+        
+    }
+    
     
     /**
      * Подготвя шаблона за детайлите
@@ -115,7 +194,7 @@ class sales_InvoiceDetails extends core_Detail
      * @param stdClass $res
      * @param stdClass $data
      */
-    function on_AfterRenderDetail($mvc, &$res, $data)
+    function on_AfterRenderDetail1($mvc, &$res, $data)
     { return;
         $res = new ET("
             <table class=\"invTable\" border=\"0\" cellpadding=\"1\" cellspacing=\"0\" width=\"100%\">
