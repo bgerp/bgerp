cd1c5493a4cab08e45a4b377a2d45a5e11b75319
arsov <Arsov@EP-BAGS.com>
2012-12-05 16:35:42 +0200

    Update crm/ext/IdCards.class.php

--- crm/ext/IdCards.class.php
+++ crm/ext/IdCards.class.php
@@ -114,7 +114,7 @@ class crm_ext_IdCards extends core_Detail
         
         while($mvrRec = $mvrQuery->fetch()) {
             $mvrName = 'МВР - ';
-            $mvrName .= $mvc->getVerbal($mvrRec, 'city');
+            $mvrName .= drdata_Mvr::getVerbal($mvrRec, 'city');
             $mvrSug[$mvrName] = $mvrName;
         }
 
