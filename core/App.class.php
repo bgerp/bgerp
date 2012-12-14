d435fce8e85a0f7def7acb0906321718889b3bb2
Milen Georgiev <milen@experta.bg>
2012-12-14 13:22:51 +0200

    Update core/App.class.php

--- core/App.class.php
+++ core/App.class.php
@@ -383,7 +383,11 @@ class core_App
      * Добавя сесийния идентификатор, ако е необходимо
      */
     public static function redirect($url, $absolute = FALSE, $msg = NULL, $type = 'info')
-    { die("$msg");
+    { 
+        if($msg) {
+            print_r("$msg");
+            die;
+        }
         expect(ob_get_length() <= 3, ob_get_length(), ob_get_contents());
 
         $url = static::toUrl($url, $absolute ? 'absolute' : 'relative');
