35a17d1bc463c575ecf685e3e1257f503a5c2811
Milen Georgiev <milen@experta.bg>
2012-12-15 15:46:06 +0200

    buxFix

--- cms/Articles.class.php
+++ cms/Articles.class.php
@@ -26,7 +26,7 @@ class cms_Articles extends core_Manager
     /**
      * Плъгини за зареждане
      */
-    var $loadList = 'plg_Created, plg_State2, plg_RowTools, plg_Printing, cms_Wrapper, plg_Sorting, plg_Vid, plg_Ordering';
+    var $loadList = 'plg_Created, plg_State2, plg_RowTools, plg_Printing, cms_Wrapper, plg_Sorting, plg_Vid';
     
 
     /**
