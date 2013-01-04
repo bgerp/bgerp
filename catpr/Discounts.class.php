342ec8c74748f90cf8e9adebfd528736e38a9a0f
arsov <Arsov@EP-BAGS.com>
2013-01-04 12:16:02 +0200

    Тест за двоен ескейп

--- catpr/Discounts.class.php
+++ catpr/Discounts.class.php
@@ -153,7 +153,7 @@ class catpr_Discounts extends core_Master
             //$paramName = $mvc->getVerbal($paramRec, 'name');
             
             //Заместваме равното с ASCII стойността му
-            //$paramName = str_replace('=', '&#61;', $paramName);
+            $paramName = str_replace('=', '&#61;', $paramName);
             
             $form->FLD("value_{$paramRec->id}", $valueType, "input,caption=Отстъпки->" . $paramName);
             $form->setDefault("value_{$paramRec->id}", $val);
