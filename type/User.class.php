49398111c0b318ebb236d65e9297d96e831e2474
Stefan Stefanov <stefan.bg@gmail.com>
2013-01-11 18:53:36 +0200

    type_User позволява allowEmpty

--- type/User.class.php
+++ type/User.class.php
@@ -132,6 +132,10 @@ class type_User extends type_Key
     {
         $this->prepareOptions();
         
+        if($this->params['allowEmpty']) {
+            $this->options = array('' => '&nbsp;') + $this->options;
+        }
+        
         if(empty($value)) {
             $value = '|' . core_Users::getCurrent() . '|';
         }
