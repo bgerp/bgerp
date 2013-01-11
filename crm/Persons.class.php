79d839063d03dfabefed5728268dfe2a658869f9
Milen Georgiev <milen@experta.bg>
2013-01-11 11:39:03 +0200

    Визуално форматиране

--- crm/Persons.class.php
+++ crm/Persons.class.php
@@ -488,16 +488,16 @@ class crm_Persons extends core_Master
                     $dateType = 'Роден(а)';
                 }
             }
-            $row->nameList .= "<div style='font-size:0.8em;margin-top:5px;'>$dateType:&nbsp;{$birthday}</div>";
+            $row->nameList .= "<div style='font-size:0.8em;margin:3px;'>$dateType:&nbsp;{$birthday}</div>";
         } elseif($rec->egn) {
             $egn = $mvc->getVerbal($rec, 'egn');
             $row->title .= "&nbsp;&nbsp;<div style='float:right'>{$egn}</div>";
-            $row->nameList .= "<div style='font-size:0.8em;margin-top:5px;'>{$egn}</div>";
+            $row->nameList .= "<div style='font-size:0.8em;margin:3px;'>{$egn}</div>";
         }
 
         if($rec->buzCompanyId && crm_Companies::haveRightFor('single', $rec->buzCompanyId)) {
             $row->buzCompanyId = ht::createLink($mvc->getVerbal($rec, 'buzCompanyId'), array('crm_Companies', 'single', $rec->buzCompanyId));
-            $row->nameList .= "<div>{$row->buzCompanyId}</div>";
+            $row->nameList .= "<div style='font-size:0.8em;margin:3px;'>{$row->buzCompanyId}</div>";
         }
     }
 
