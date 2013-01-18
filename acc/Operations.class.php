afed076c4e78873392da102d9a47611b43ccd415
ivelin1012 <ivelin_pdimov@abv.bg>
2013-01-18 18:08:32 +0200

    acc_Operations филтриране на операциите

--- acc/Operations.class.php
+++ acc/Operations.class.php
@@ -4,7 +4,7 @@
 
 /**
  * Мениджър на Счетоводни Операции
- * Една "Операция" може да се обвърза само с документи които генерират
+ * Една "Операция" може да се обвързва само с документи които генерират
  * счетоводни транзакции (@see acc_TransactionSourceIntf)
  *
  * @category  bgerp
@@ -16,6 +16,8 @@
  */
 class acc_Operations extends core_Manager
 {
+    
+    
     /**
      * Заглавие на мениджъра
      */
@@ -37,7 +39,7 @@ class acc_Operations extends core_Manager
     /**
      * Полета, които ще се показват в листов изглед
      */
-    var $listFields = "tools=Пулт, name, document, debitAccount, creditAccount";
+    var $listFields = "tools=Пулт, name, document, debitAccount, creditAccount, systemId";
    
     
     /**
@@ -91,7 +93,8 @@ class acc_Operations extends core_Manager
     	$this->FLD('document', 'class(interface=acc_TransactionSourceIntf)', 'caption=Документ,mandatory');
     	$this->FLD('debitAccount', 'customKey(mvc=acc_Accounts,key=systemId, select=title)', 'caption=Дебит сметка,mandatory');
     	$this->FLD('creditAccount', 'customKey(mvc=acc_Accounts,key=systemId, select=title)', 'caption=Кредит сметка,mandatory');
-    
+    	$this->FLD('systemId', 'varchar(32)', 'caption=System ID, export,mandatory');
+    	
     	// Поставяне на уникални индекси
     	$this->setDbUnique('name');
     }
@@ -118,7 +121,7 @@ class acc_Operations extends core_Manager
     static function getPossibleOperations($name)
     {
     	// Ид-то на класа на документа
-    	$classId = core_Classes::fetchIdByName($name);
+    	expect($classId = core_Classes::fetchIdByName($name), 'Няма такъв документ !!!');
     	
     	// Извличаме онея операции, които са за този документ
     	$options = array();
@@ -131,7 +134,59 @@ class acc_Operations extends core_Manager
         if(count($options) == 0) {
         	$options[] = tr('Няма операции за този документ');
         }
+        
         return $options;
+    }
+    
+    
+    /**
+     * Функция която филтрира позволените операции на даден документ
+     * според подаден клас
+     * @param array() $options - Масив с ключове ид-та на операции
+     * @param mixed $class - Име или Ид на клас спрямо който филтрираме
+     * @return array() $options - Връщаме вече филтрираните операции
+     */
+    static function filter($options, $class)
+    {
+    	expect(is_array($options), 'Не е подаден масив !!!');
+    	if(count($options) == 0) {
+    		
+    		return;
+    	}
+    	
+    	// В кои номенклатури може да участва класа
+    	$lists = acc_Lists::getPossibleLists($class);
+    	
+    	foreach($options as $key => $value) {
+    		
+    		// за всяка операция от масива намираме сметките с които работи
+    		expect($rec = static::fetch($key), 'Няма такава операция');
+    		$dAcc = $rec->debitAccount;
+    		$cAcc = $rec->creditAccount;
+    		$occ = 0;
+    		
+    		// За всяка позволена номенклатура проверяваме дали сметките
+    		// на операцията я поддържат
+    		foreach($lists as $keyL => $valueL) {
+    			
+    			$listIntf = acc_Lists::fetchField($keyL, 'regInterfaceId');
+    			if(acc_Lists::getPosition($dAcc, $listIntf) ||
+    			   acc_Lists::getPosition($cAcc, $listIntf)
+    			)  {
+    				$occ++;
+    				break;
+    			}
+    		}
+    		
+    		// Ако сметките на операцията не поддържа нито една
+    		// позволена номенклатура ние премахваме операцията
+    		if($occ == 0) {
+    			unset($options[$key]);
+    		}
+    	}
+    	
+    	// Връщаме вече филтрираните опции
+    	return $options;
     }
     
     
@@ -142,7 +197,7 @@ class acc_Operations extends core_Manager
      */
     static function getOperationInfo($id)
     {
-    	$rec = static::fetch($id);
+    	expect($rec = static::fetch($id), 'Няма такава операция !!!');
     	
     	// Извличаме записите на сметките с подадените systemId-та
     	$debitRec = acc_Accounts::getRecBySystemId($rec->debitAccount);
@@ -152,6 +207,19 @@ class acc_Operations extends core_Manager
     	$rec->creditAccount = acc_Accounts::getAccountInfo($creditRec->id);
         
     	return $rec;
+    }
+    
+    
+    /**
+     * Връща операцията 
+     * @param int $id - ид-то на операцията
+     * @return varchar $sysId - систем ид-то на операцията
+     */
+    static function fetchSysId($id)
+    {
+    	expect($sysId = static::fetchField($id, 'systemId'), 'Няма такава операция');
+    	
+    	return $sysId;
     }
     
     
