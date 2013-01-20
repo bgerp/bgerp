ab6fe046678cef25c3720dd768c915ff4ad206a9
Milen Georgiev <milen@experta.bg>
2013-01-20 21:39:40 +0200

    Дебъг - показва времето за изпълнение на всяка операция

--- core/Debug.class.php
+++ core/Debug.class.php
@@ -93,7 +93,11 @@ class core_Debug
 
         self::init();
         
-        self::$debugTime[] = number_format((dt::getMicrotime() - self::$startMicroTime), 5) . ": " . $name;
+        $rec = new stdClass();
+        $rec->start = dt::getMicrotime() - self::$startMicroTime;
+        $rec->name  = $name;
+
+        self::$debugTime[] = $rec;
     }
     
     
@@ -117,13 +121,14 @@ class core_Debug
         if (count(self::$debugTime)) {
             self::log('End');
             $html .= "\n<div style='padding:5px; margin:10px; border:solid 1px #777; background-color:#FFFF99; display:table;color:black;'>" .
-            "\n<div style='background-color:#FFFF33; padding:5px; color:black;'>Debug log</div><ol>";
+            "\n<div style='background-color:#FFFF33; padding:5px; color:black;'>Debug log</div><ul>";
             
             foreach (self::$debugTime as $rec) {
-                $html .= "\n<li style='padding:15px 0px 15px 0px;border-top:solid 1px #cc3;'>" . htmlentities($rec, ENT_QUOTES, 'UTF-8');
+                $html .= "\n<li style='padding:15px 0px 15px 0px;border-top:solid 1px #cc3;'>" .  number_format(($rec->start - $last), 5) . ": " . htmlentities($rec->name, ENT_QUOTES, 'UTF-8');
+                $last = $rec->start;
             }
             
-            $html .= "\n</ol></div>";
+            $html .= "\n</ul></div>";
         }
         
         if (count(self::$timers)) {
