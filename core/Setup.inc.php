d6cfcc320c5142d9e06874a018aeec6f44d82a7e
Dimitar Minekov <mitko@extrapack.com>
2013-01-21 18:36:50 +0200

    Ako SetupKey е зададен и е верен - упълномощава и нелокални ИП-та да правят Сетъп

--- core/Setup.inc.php
+++ core/Setup.inc.php
@@ -232,14 +232,16 @@ href=\"data:image/icon;base64,AAABAAEAEBAAAAAAAABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIA
 </body>
 </html>";
 
+// Определяме масива с локалните IP-та
+$localIpArr = array('::1', '127.0.0.1');
+
 if (defined('BGERP_SETUP_KEY')) {
 	if($_GET['SetupKey'] != BGERP_SETUP_KEY) {
 	    halt('Wrong Setup Key!');
 	}
+	$localIpArr += array($_SERVER['REMOTE_ADDR']);
 }
 
-// Определяме масива с локалните IP-та
-$localIpArr = array('::1', '127.0.0.1');
 session_name('SID');
 session_start();
 if($ip = $_SESSION[EF_APP_NAME . 'admin_ip']) {
