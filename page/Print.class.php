1eb299221bcf149833812fb5fe8c88377790e0fd
Stefan Stefanov <stefan.bg@gmail.com>
2013-02-27 18:11:22 +0200

    Добавен шаблон за принт-страниците

---
+++ page/Print.class.php
@@ -0,0 +1,50 @@
+<?php
+
+
+
+/**
+ * Клас 'page_Print' - Шаблон за страница за печат
+ *
+ *
+ * @category  ef
+ * @package   page
+ * @author    Milen Georgiev <milen@download.bg>
+ * @copyright 2006 - 2012 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ * @link
+ */
+class page_Print extends page_Html {
+    
+    
+    /**
+     * @todo Чака за документация...
+     */
+    function page_Print()
+    {
+    	$conf = core_Packs::getConfig('core');
+    	
+        $this->page_Html();
+        $this->replace("UTF-8", 'ENCODING');
+        
+        $this->push('css/common.css','CSS');
+        $this->push('css/Application.css','CSS');
+        
+        $this->append("window.print();", 'ON_LOAD');
+        
+        $this->append("
+         * {
+             background-color: white !important;
+             DIV.background-image: none !important;
+           }
+         ", "STYLES");
+        
+        $this->push('js/efCommon.js', 'JS');
+        
+        $this->appendOnce("\n<link  rel=\"shortcut icon\" href=" . sbf("img/favicon.ico") . ">", "HEAD");
+        
+        $this->prepend($conf->EF_APP_TITLE, 'PAGE_TITLE');
+        
+        $this->replace(new ET("<div class='printing'>[#PAGE_CONTENT#]</div>"), "PAGE_CONTENT");
+    }
+}
