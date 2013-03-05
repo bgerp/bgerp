e36a3c92d196dc0408f4efbfbb66960f8b477107
ivelin1012 <ivelin_pdimov@abv.bg>
2013-03-05 16:47:21 +0200

    cat_products_Packagings - при създаването на нова продуктова опаковка трябва да е зададен поне един код (вътрешен или баркод)

--- cat/products/Packagings.class.php
+++ cat/products/Packagings.class.php
@@ -69,6 +69,11 @@ class cat_products_Packagings extends cat_products_Detail
     {
     	if ($form->isSubmitted()){
     		$rec = &$form->rec;
+    		
+    		if(empty($rec->eanCode) && empty($rec->customCode)){
+    			$form->setError('eanCode, customCode', 'Трябва да е зададен поне един уникален код');
+    		}
+    		
     		foreach(array('eanCode', 'customCode') as $code) {
     			if($rec->$code) {
     				
