fa4f36dc56dc5681361c47c307ad57c6564d9860
Yusein Yuseinov <yyuseinov@gmail.com>
2013-04-10 15:47:33 +0300

    #Tsk93 - Пакет archiv - за работа с архиви.

---
+++ archive/7z/Entry.php
@@ -0,0 +1,243 @@
+<?php
+/**
+ *
+ * This software is distributed under the GNU GPL v3.0 license.
+ *
+ * @author    Gemorroj
+ * @copyright 2012 http://wapinet.ru
+ * @license   http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link      https://github.com/Gemorroj/Archive_7z
+ * @version   0.1 alpha
+ *
+ */
+
+class Archive_7z_Entry
+{
+    /**
+     * @var string
+     */
+    var $path;
+    /**
+     * @var string
+     */
+    var $size;
+    /**
+     * @var string
+     */
+    var $packedSize;
+    /**
+     * @var string
+     */
+    var $modified;
+    /**
+     * @var string
+     */
+    var $attributes;
+    /**
+     * @var string
+     */
+    var $crc;
+    /**
+     * @var string
+     */
+    var $encrypted;
+    /**
+     * @var string
+     */
+    var $method;
+    /**
+     * @var string
+     */
+    var $block;
+
+    /**
+     * @var Archive_7z
+     */
+    var $archive;
+
+
+    /**
+     * @param Archive_7z $archive
+     * @param array      $data
+     */
+    public function __construct(Archive_7z $archive, array $data)
+    {
+        $this->archive = $archive;
+        $this->parseEntry($data);
+    }
+
+
+    /**
+     * @param array $data
+     */
+    private function parseEntry(array $data)
+    {
+        foreach ($data as $line) {
+            list($k, $v) = explode(' =', $line, 2);
+            $v = ltrim($v);
+
+            $this->setData($k, $v);
+        }
+    }
+
+
+    /**
+     * @param string $key
+     * @param string $value
+     */
+    private function setData($key, $value)
+    {
+        switch ($key) {
+        case 'Path':
+            $this->path = $value;
+            break;
+
+        case 'Size':
+            $this->size = $value;
+            break;
+
+        case 'Packed Size':
+            $this->packedSize = $value;
+            break;
+
+        case 'Modified':
+            $this->modified = $value;
+            break;
+
+        case 'Attributes':
+            $this->attributes = $value;
+            break;
+
+        case 'CRC':
+            $this->crc = $value;
+            break;
+
+        case 'Encrypted':
+            $this->encrypted = $value;
+            break;
+
+        case 'Method':
+            $this->method = $value;
+            break;
+
+        case 'Block':
+            $this->block = $value;
+            break;
+        }
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function isDirectory()
+    {
+        return ($this->attributes[0] === 'D');
+    }
+
+
+    /**
+     * @throws Archive_7z_Exception
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->archive->getContent($this->path);
+    }
+
+
+    /**
+     * @throws Archive_7z_Exception
+     */
+    public function extract()
+    {
+        $this->archive->extractEntry($this->path);
+    }
+
+
+    /**
+     * @param string $directory
+     *
+     * @throws Archive_7z_Exception
+     */
+    public function extractTo($directory = './')
+    {
+        $oldDirectory = $this->archive->getOutputDirectory();
+        $this->archive->setOutputDirectory($directory);
+        $this->archive->extractEntry($this->path);
+        $this->archive->setOutputDirectory($oldDirectory);
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlock()
+    {
+        return $this->block;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCrc()
+    {
+        return $this->crc;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEncrypted()
+    {
+        return $this->encrypted;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * @return string
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPackedSize()
+    {
+        return $this->packedSize;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+}
---
+++ archive/Adapter.class.php
@@ -0,0 +1,242 @@
+<?php
+
+
+/**
+ * Директория, в която ще се държат екстрактнатите файлове
+ */
+defIfNot('ARCHIVE_TEMP_PATH', EF_TEMP_PATH . '/archive');
+
+
+
+/**
+ * Пътя до 7z пакета
+ */
+defIfNot('ARCHIVE_7Z_PATH', '7z');
+
+
+/**
+ * Адаптер за разглеждане и разархивиране на архиви
+ *
+ * @category  vendors
+ * @package   archive
+ * @author    Yusein Yuseinov <yyuseinov@gmail.com>
+ * @copyright 2006 - 2013 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class archive_Adapter
+{
+    
+    
+    /**
+     * Инстанция на класа
+     */
+    protected $inst;
+    
+    
+    /**
+     * Пътя до временния файл на архива
+     */
+    protected $path;
+    
+    
+    /**
+     * Временната директория с временните файлове, екстрактнати от архива
+     */
+    protected $dir;
+    
+    
+    /**
+     * При създаване на инстанция на класа, инициализираме някои променливи
+     * 
+     * @param fileHandler $fh - Манипулатор на файл
+     */
+    function init($fh)
+    {
+        // Вкарваме пакета
+        require_once getFullPath("archive/7z/7z.php");
+        
+        // Пътя до файла
+        $this->path = fileman::extract($fh);
+
+        // Инстанция на архива
+        $this->inst = new Archive_7z($this->path);
+    }
+    
+    
+    /**
+     * Връща в дървовидна структура съдържанието на архива, които сочат в съответното URL
+     * 
+     * @param array $url - Масив с URL
+     */
+    function tree($url)
+    {
+        // Вземаме съдържанието
+        $entriesArr = $this->getEntries();
+        
+        // Инстанция на класа
+        $tableInst = cls::get('core_Tree');
+
+        // Обхождаме масива
+        foreach ((array)$entriesArr as $key => $entry) {
+            
+            // Пътя в архива
+            $path = $entry->getPath();
+            
+            // Заместваме разделителите за поддиректория с разделителя за дърво
+            $path = str_replace(array('/', '\\'), "->", $path);
+            
+            // Ако има размер
+            if ($entry->getSize()) {
+                
+                $urlPath = $url;
+                
+                // Индекса да е ключа
+                $urlPath['index'] = $key;
+            } else {
+                
+                // Ако няма размер
+                $urlPath = FALSE;
+            }
+            
+            // Добавяме пътя в дървото със съответното URL
+            $tableInst->addNode($path, $urlPath);
+        }
+        
+        // Името
+        $tableInst->name = 'archive';
+        
+        // Рендираме изгледа
+        $res = $tableInst->renderHtml(NULL);
+        
+        // Връщаме шаблона
+        return $res;
+    }
+    
+    
+    /**
+     * Връща масив от обекти със информацията за съдържанието на файла
+     * 
+     * @param ingeger $entry - Индекса на файла от архива
+     * 
+     * @return mixed - Ако е подаден индекс, връща обект с информация за съответния файл/папка
+     * Ако не е подаден индек, масив с всички файлове/папки, като обекти
+     */
+    public function getEntries($entry = FALSE)
+    {
+        // Вземаме информация за всички файлове/папки
+        $entriesArr = $this->inst->getEntries();
+        
+        // Ако е подаден номер на файл
+        if ($entry !== FALSE) {
+            
+            // Връщаме съответния запис
+            return $entriesArr[$entry];
+        }
+        
+        // Връщаме целия маси
+        return $entriesArr;
+    }
+    
+    
+    /**
+     * Качва в кофа файла в съответния индекс и връща манипулатора на качения файл
+     * 
+     * @param integer $index - Индекса на файла в архива
+     */
+    public function getFile($index)
+    {
+        // Вземаме обекта за съответния файл
+        $entry = $this->getEntries($index);
+        
+        // Ако няма размер
+        expect($entry->getSize(), 'Не е файл');
+        
+        // Вземаме пътя до файла в архива
+        $path = $entry->getPath();
+        
+        return $this->absorbFile($path);
+    }
+
+
+	/**
+     * Изтрива временния файл
+     */
+    public function deleteTempPath()
+    {   
+        // Изтрива временния файл
+        fileman::deleteTempPath($this->path);
+    }
+    
+    
+    /**
+     * Абсорбираме файла от архива.
+     * Качваме подадения файл от архива, в кофата 'archive'
+     * 
+     * @param string $path - Вътрешния път в архива
+     * 
+     * @param fileHandler $fh - Манипулатора на файла
+     */
+    protected function absorbFile($path)
+    {
+        // Екстрактваме файла от архива и връщаме пътя във файловата система
+        $path = $this->extractEntry($path);
+
+        // Ако е файл
+        if (is_file($path)) {
+            
+            // Абсорбираме файла
+            $fh = fileman::absorb($path, 'archive');    
+        }
+        
+        // Изтриваме временнада директория със съдържанието му
+        core_Os::deleteDir($this->dir);
+        
+        return $fh;
+    }
+    
+    
+    /**
+     * Екстрактваме файла от архива и връщаме пътя във файловата система
+     * 
+     * @param string $path - Вътрешния път в архива
+     */
+    function extractEntry($path)
+    {
+        // Вземаме директорията
+        $outputDir = $this->setOutputDirectory();
+        
+        // Екстрактваме файла
+        $this->inst->extractEntry($path);
+        
+        // Връщаме пълния път до файла
+        return $this->dir . '/' . $path;        
+    }
+    
+    
+	/**
+     * Задаваме временна директроя, където ще се разархивират файловете
+     */
+    protected function setOutputDirectory()
+    {
+        // Вземаме директорията за временните файлове
+        $dir = ARCHIVE_TEMP_PATH;
+        
+        // Сканираме директорията
+        $dirs = @scandir($dir);
+        
+        // Опитваме се да генерираме име, което не се среща в директория
+        do {
+            $newName = str::getRand();
+        }while(in_array($newName, (array)$dirs));
+        
+        // Пътя на директорията
+        $this->dir = $dir . '/' . $newName;
+        
+        // Създаваме директорията
+        expect(mkdir($this->dir, 0777, TRUE), 'Не може да се създаде директория.');
+        
+        // Инициализираме директорията
+        $this->inst->setOutputDirectory($this->dir);
+    }
+}
