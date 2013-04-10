f056fc2c635f00085177c3ebd62d0894332ca84c
Yusein Yuseinov <yyuseinov@gmail.com>
2013-04-10 17:10:51 +0300

    fileman_webdrv_Archive - по подразбиране да използва archive_Adapter

--- fileman/webdrv/Archive.class.php
+++ fileman/webdrv/Archive.class.php
@@ -50,8 +50,70 @@ class fileman_webdrv_Archive extends fileman_webdrv_Generic
 				'title'   => 'Съдържание',
 				'html'    => "<div class='webdrvTabBody' style='white-space:pre-wrap;'><fieldset class='webdrvFieldset'><legend>Съдържание</legend>{$contentStr}</fieldset></div>",
 				'order' => 7,
+				'tpl' => $contentStr,
 			);
         
         return $tabsArr;
+    }
+    
+    
+    /**
+     * Връща инстанция на адаптера за работа с архиви
+     * 
+     * @param object $fRec - Записите за файла
+     */
+    static function getArchiveInst($fRec)
+    {
+        
+        // Връщаме инстанцията
+        return cls::get('archive_Adapter', $fRec->fileHnd);
+    }
+    
+    
+    /**
+     * Връща съдържанието на архива в дървовидна структура
+     * 
+     * @param object $fRec - Записите за файла
+     */
+    static function getArchiveContent($fRec, $path = NULL) 
+    {
+        // Инстанция на класа
+        $inst = static::getArchiveInst($fRec);
+        
+        // URL' то където да сочат файловете
+        $url = array('fileman_webdrv_Archive', 'absorbFileInArchive', $fRec->fileHnd, 'index' => 1);
+        
+        // Създаваме дървото
+        $tree = $inst->tree($url);
+        
+        // Изтриваме временните файлове
+        $inst->deleteTempPath();
+        
+        // Връщаме дървото
+        return $tree;
+    }
+    
+    
+    /**
+     * Уплоадва файла от архива
+     * 
+     * @param object $fRec - Записите за файла
+     * @param integer $index - Номера на файлам, който ще се екстрактва
+     * 
+     * @return fileHandler - Манипулатор на файл
+     */
+    static function uploadFileFromArchive($fRec, $index)
+    {
+        // Вземаме инстанция на архива
+        $archive = static::getArchiveInst($fRec);
+        
+        // Качваме съответния файл
+        $fh = $archive->getFile($index);
+        
+        // Изтриваме временните файлове
+        $archive->deleteTempPath();
+        
+        // Връщаме манипулатора на файла
+        return $fh;
     }
 }
