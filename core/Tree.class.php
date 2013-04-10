5617768bd3076ec9e08fb77317a67c6fdc422c30
Yusein Yuseinov <yyuseinov@gmail.com>
2013-04-10 16:49:41 +0300

    core_Tree bugFix

--- core/Tree.class.php
+++ core/Tree.class.php
@@ -38,13 +38,15 @@ class core_Tree extends core_BaseClass
     /**
      * Добавя елемент в дървото
      */
-    function addNode($path, $url)
+    function addNode($path, $url, $onlyLastUrl = FALSE)
     {
         $nodes = explode("->", $path);
         
         $pid = -1;
         
-        foreach($nodes as $node) {
+        $nodesCnt = count($nodes);
+
+        foreach($nodes as $key => $node) {
             
             $currentPath .= ($currentPath ? "->" : "") . $node;
             
@@ -56,22 +58,16 @@ class core_Tree extends core_BaseClass
                 $n->pid = $pid;
                 $pid = $n->id;
                 $n->title = $node;
-                $n->url = toUrl($url);
-                
-                $this->nodes[$currentPath] = $n;
-            } elseif (isset($this->nodes[$currentPath]) && ($this->nodes[$currentPath]->url != $url)) {
-                
-                // Ако има такъв запис и подаденото URL не съвпада 
-                
-                // Ако има URL
-                if ($url) {
+
+                // Ако е задедено само на последния nod да се добавя URL
+                if (!$onlyLastUrl || ($onlyLastUrl && ($key == $nodesCnt-1))) {
                     
-                    // Преобразуваме го
-                    $url = toUrl($url);    
+                    if ($url) { 
+                        $n->url = toUrl($url);       
+                    }
                 }
                 
-                // Заместваме URL' то
-                $this->nodes[$currentPath]->url = $url;
+                $this->nodes[$currentPath] = $n;
             }
             
             $pid = $this->nodes[$currentPath]->id;
@@ -139,7 +135,7 @@ class core_Tree extends core_BaseClass
         if($selectedId) {
             // $tpl->append("\n{$name}.openTo({$selectedId}, true);", 'treeDesciption');
         }
-        
+
         $tpl->replace($name, 'treeName');
         
         $tpl->replace($body, 'body');
