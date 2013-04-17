3dc348bdd1fedd5812ea929ef213b5d6a7e132e3
ivelin1012 <ivelin_pdimov@abv.bg>
2013-04-17 14:06:50 +0300

    Фактури-детайли: рефакторинг

--- sales/InvoiceDetails.class.php
+++ sales/InvoiceDetails.class.php
@@ -43,7 +43,7 @@ class sales_InvoiceDetails extends core_Detail
     /**
      * Полета, които ще се показват в листов изглед
      */
-    var $listFields = 'productId, quantity, unit, price, amount, note, tools=Пулт';
+    var $listFields = 'productId, quantity, unit=Мярка, price, amount, tools=Пулт';
     
     
     /**
@@ -79,12 +79,10 @@ class sales_InvoiceDetails extends core_Detail
         $this->FLD('invoiceId', 'key(mvc=sales_Invoices)', 'caption=Фактура, input=hidden, silent');
         $this->FLD('productId', 'key(mvc=cat_Products, select=name)', 'caption=Продукт');
         $this->FLD('quantity', 'double(decimals=4)', 'caption=Количество,mandatory');
-        $this->FLD('priceType', 'enum(policy=По ценова политика, history=По предишна цена, input=Въведена цена)', 'caption=Ценообразуване, input=hidden, silent');
         $this->FLD('policyId', 'class(interface=price_PolicyIntf, select=title)', 'input=hidden,caption=Политика, silent');
-        $this->FLD('price', 'double(decimals=2)', 'caption=Ед. цена, input=none');
-        $this->FLD('note', 'varchar(64)', 'caption=@Пояснение', array('attr'=>array('rows'=>2)));
-        $this->FNC('amount', 'double(decimals=2)', 'caption=Сума, column');
-        $this->FNC('unit', 'varchar', 'caption=Мярка');
+        $this->FLD('price', 'double(decimals=2)', 'caption=Ед. цена, input');
+        $this->FLD('note', 'varchar(64)', 'caption=@Пояснение');
+        $this->FLD('amount', 'double(decimals=2)', 'caption=Сума,input=none');
         $this->setDbUnique('invoiceId, productId');
     }
     
@@ -98,21 +96,6 @@ class sales_InvoiceDetails extends core_Detail
     static function on_CalcAmount($mvc, $rec)
     {
         $rec->amount = round($rec->price * $rec->quantity, 2);
-    }
-    
-    
-    /**
-     * Изчислява полето 'unit' (мярка на перото)
-     *
-     * @param core_Mvc $mvc
-     * @param stdClass $rec
-     */
-    static function on_CalcUnit($mvc, $rec)
-    {
-        if ($rec->productId) {
-        	$productRec = cat_Products::fetch($rec->productId);
-            $rec->unit = cat_Products::getVerbal($productRec, 'measureId');
-        }
     }
     
     
@@ -133,91 +116,68 @@ class sales_InvoiceDetails extends core_Detail
                     "id=btnAdd-{$policyId},class=btn-shop");
             }
             
-            $data->toolbar->addBtn('По предишна цена', $addUrl + array('priceType' => 'history'));
-            $data->toolbar->addBtn('Въведена цена', $addUrl + array('priceType' => 'input'));
             unset($data->toolbar->buttons['btnAdd']);
         }
     }
     
     
+    /**
+     * Извиква се след подготовката на формата
+     */
     public static function on_AfterPrepareEditForm($mvc, $data)
     {
         $form = $data->form;
         $form->setOptions('productId', cat_Products::getByGroup($mvc::$productGroups));
-    	
-        if($type = Request::get('priceType', 'varchar')){
-        	$form->setField('policyId', 'input=none');
-        	$form->setReadOnly('priceType');
-        	if($type == 'input'){
-        		$form->setField('price', 'input,mandatory');
-        	}
-        } else {
-        	$form->setDefault('priceType', 'policy');
-        }
     }
 
 
+    /**
+     * Извиква се след изпращане на формата
+     */
     public static function on_AfterInputEditForm(core_Mvc $mvc, core_Form $form)
     {
         if ($form->isSubmitted()) {
            
-           // Ако няма грешки, прави опит да изчисли цената според избраната стратегия
-           $mvc::calculatePrice($form);
-        }
-    }
-    
-    
-    /**
-     * Изчислява цената на стандартен продукт според стратегията: въведена, последна или 
-     * каталожна цена.
-     * 
-     * Изчислената цена се записва във полето на формата ($form). При проблем задава 
-     * предупреждения или грешки във формата.
-     * 
-     * @param core_Form $form
-     * 
-     */
-    public static function calculatePrice(core_Form $form)
-    {
-        $rec = $form->rec;
-        
-        switch ($rec->priceType) {
-            case 'input':
-                return;
-            case 'policy':
-                
-            	$masterRec = sales_Invoices::fetch($rec->invoiceId);
-                $contragentItem = acc_Items::fetch($masterRec->contragentAccItemId);
-            	
-                $Policy = cls::get($rec->policyId);
-                $rec->price= $Policy->getPriceInfo($contragentItem->classId, $contragentItem->objectId, $rec->productId)->price;
-               
-            	break;
-            case 'history':
-            	
-                /**
-                * Последната цена, на която този продукт е фактуриран на този клиент.
-                */
-                $recentPrice = static::getRecentPriceFor($rec->productId, $rec->invoiceId);
-                
-                if ($recentPrice !== FALSE) {
-                    if ($rec->price && $rec->price != $recentPrice) {
-                        $form->setWarning('price',
-                            "Цената бе изчислена автоматично: {$recentPrice}. Въведената цена е игнорирана!"
-                        );
-                    }
-                    $rec->price = $recentPrice;
-                } elseif (!$rec->price) {
-                    $form->setError('price', 'Невъзможно е да се определи предишна цена');
-                }
-                
-                break;
+          if(!$form->rec->price){
+          	
+          	// Ако не е зададена цена, извличаме я от избраната политика
+          	$masterRec = sales_Invoices::fetch($form->rec->invoiceId);
+            $contragentItem = acc_Items::fetch($masterRec->contragentAccItemId);
+            $Policy = cls::get($form->rec->policyId);
+            $form->rec->price = $Policy->getPriceInfo($contragentItem->classId, $contragentItem->objectId, $form->rec->productId)->price;
+          
+	        if(!$form->rec->price){
+	          $form->setError('price', 'Неможе да се определи цена');
+	        }
+          }
+          
+          // Изчисляваме цената
+          $form->rec->amount = round($form->rec->price * $form->rec->quantity, 2);
         }
     }
 
+    
+    /**
+     * След преобразуване на записа в четим за хора вид.
+     */
+    public static function on_AfterRecToVerbal($mvc, &$row, $rec, $fields = array())
+    {
+    	$varchar = cls::get('type_Varchar');
+    	$row->quantity = $varchar->toVerbal(floatval($rec->quantity));
+    	
+    	if($rec->note){
+	    	$row->note = $varchar->toVerbal($rec->note);
+	    	$row->productId .= "<br/><small style='color:#555;'>{$row->note}</small>";
+    	}
+    	
+    	$productRec = cat_Products::fetch($rec->productId);
+        $row->unit = cat_Products::getVerbal($productRec, 'measureId');
+    }
 	
+    
     /**
      * Намира последната цена за даден продукт
+     * @TODO Не се използва засега
      * @param int $productId - продукт, за който проверяваме
      * @param int $invoiceId - фактура
      * @return double $recentPrice - последната цена
