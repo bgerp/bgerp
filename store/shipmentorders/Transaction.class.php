f2d9a473698cab7ddbac2dee1fc03e753ac602ce
Stefan Stefanov <stefan.bg@gmail.com>
2013-04-18 18:12:15 +0300

    Контиране на ЕН

---
+++ store/shipmentorders/Transaction.class.php
@@ -0,0 +1,207 @@
+<?php
+/**
+ * Помощен клас-имплементация на интерфейса acc_TransactionSourceIntf за класа store_ShipmentOrders
+ *
+ * @category  bgerp
+ * @package   sales
+ * @author    Stefan Stefanov <stefan.bg@gmail.com>
+ * @copyright 2006 - 2013 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ * 
+ * @see acc_TransactionSourceIntf
+ *
+ */
+class store_shipmentorders_Transaction
+{
+    /**
+     * 
+     * @var sales_Sales
+     */
+    public $class;
+    
+    
+    /**
+     * Генериране на счетоводните транзакции, породени от експедиционно нареждане.
+     * 
+     * Счетоводната транзакция, породена от експедиционно нареждане може да се раздели на две
+     * части:
+     *
+     * 1. Задължаване на с/ката на клиента
+     *
+     *    Dt: 411  - Вземания от клиенти               (Клиент, Валута)
+     *    Ct: 7011 - Приходи от продажби по Документи  (Стоки и Продукти)
+     * 
+     * 2. Експедиране на стоката от склада (в някой случаи)
+     *
+     *    Dt: 7011 - Приходи от продажби по Документи (Стоки и Продукти)
+     *    Ct: 321  - Стоки и Продукти                 (Склад, Стоки и Продукти)
+     *
+     *    Цените, по които се изписват продуктите от с/ка 321 са според зададената стратегия 
+     *
+     * @param int|object $id първичен ключ или запис на продажба
+     * @return object NULL означава, че документа няма отношение към счетоводството, няма да генерира
+     *                счетоводни транзакции
+     * @throws core_exception_Expect когато възникне грешка при генерирането на транзакция               
+     */
+    public function getTransaction($id)
+    {
+        $entries = array();
+        
+        $rec = $this->fetchShipmentData($id);
+            
+        // Всяко ЕН трябва да има поне един детайл
+        expect(count($rec->details) > 0);
+            
+        // Записите от тип 1 (вземане от клиент)
+        $entries = $this->getTakingPart($rec);
+            
+        // Записите от тип 2 (експедиция)
+        $entries = array_merge($entries, $this->getDeliveryPart($rec));
+        
+        $transaction = (object)array(
+            'reason'  => 'ЕН #' . $rec->id,
+            'valior'  => $rec->valior,
+            'entries' => $entries, 
+        );
+        
+        return $transaction;
+    }
+    
+    
+    public function finalizeTransaction($id)
+    {
+        $rec = $this->class->fetch($id);
+        
+        $rec->state = 'active';
+        
+        if ($this->class->save($rec)) {
+            $this->class->invoke('Activation', array($rec));
+        }
+    }
+    
+    
+    /**
+     * Помощен метод за извличане на данните на ЕН - мастър + детайли
+     * 
+     * Детайлите на ЕН (продуктите) са записани в полето-масив 'details' на резултата 
+     * 
+     * @param int|object $id първичен ключ или запис на ЕН
+     * @param object запис на ЕН (@see store_ShipmentOrders)
+     */
+    protected function fetchShipmentData($id)
+    {
+        $rec = $this->class->fetchRec($id);
+        
+        expect($rec->id);
+        
+        // Извличаме базовата продажба
+        expect($sale = doc_Containers::getDocument($rec->originId));
+        expect($sale->getInstance() instanceof sales_Sales);
+        
+        // Преобразуване на трибуквен ISO код на валута към първичен ключ на валута
+        $rec->currencyCode = $sale->rec('currencyId');
+        $rec->currencyId   = currency_Currencies::getIdByCode($rec->currencyCode);
+        
+        // Извличаме детайлите на продажбата
+        /* @var $detailQuery core_Query */
+        $detailQuery = store_ShipmentOrderDetails::getQuery();
+        $detailQuery->where("#shipmentId = '{$rec->id}'");
+        $rec->details  = array();
+        
+        while ($dRec = $detailQuery->fetch()) {
+            $rec->details[] = $dRec;
+        }
+        
+        return $rec;
+    }
+    
+    
+    /**
+     * Генериране на записите от тип 1 (вземане от клиент)
+     * 
+     *    Dt: 411  - Вземания от клиенти               (Клиент, Валута)
+     *    Ct: 7011 - Приходи от продажби по Документи  (Стоки и Продукти)
+     *    
+     * @param stdClass $rec
+     * @return array
+     */
+    protected function getTakingPart($rec)
+    {
+        $entries = array();
+        
+        // Изчисляваме курса на валутата на продажбата към базовата валута
+        $currencyRate = $this->getCurrencyRate($rec);
+
+        foreach ($rec->details as $detailRec) {
+            $entries[] = array(
+                'amount' => $detailRec->amount * $currencyRate, // В основна валута
+                
+                'debit' => array(
+                    '411', // Сметка "411. Вземания от клиенти"
+                        array($rec->contragentClassId, $rec->contragentId), // Перо 1 - Клиент
+                        array('currency_Currencies', $rec->currencyId),     // Перо 2 - Валута
+                    'quantity' => $detailRec->amount, // "брой пари" във валутата на продажбата
+                ),
+                
+                'credit' => array(
+                    '7011', // Сметка "7011. Приходи от продажби по Документи"
+                        array('cat_Products', $detailRec->productId), // Перо 1 - Продукт
+                    'quantity' => $detailRec->quantity, // Количество продукт в основната му мярка
+                ),
+            );
+        }
+        
+        return $entries;
+    }
+    
+    
+    /**
+     * Помощен метод - генерира доставната част от транзакцията за продажба (ако има)
+     * 
+     * Експедиране на стоката от склада (в някой случаи)
+     *
+     *    Dt: 7011 - Приходи от продажби по Документи (Стоки и Продукти)
+     *    Ct: 321  - Стоки и Продукти                 (Склад, Стоки и Продукти)
+     *    
+     * @param stdClass $rec
+     * @return array
+     */
+    protected function getDeliveryPart($rec)
+    {
+        $entries = array();
+        
+        expect($rec->storeId, 'Генериране на експедиционна част при липсващ склад!');
+            
+        foreach ($rec->details as $detailRec) {
+            $entries[] = array(
+                'debit' => array(
+                    '7011', // Сметка "7011. Приходи от продажби по Документи"
+                        array('cat_Products', $detailRec->productId), // Перо 1 - Продукт
+                    'quantity' => $detailRec->quantity, // Количество продукт в основна мярка
+                ),
+                
+                'credit' => array(
+                    '321', // Сметка "321. Стоки и Продукти"
+                        array('store_Stores', $rec->storeId), // Перо 1 - Склад
+                        array('cat_Products', $detailRec->productId), // Перо 2 - Продукт
+                    'quantity' => $detailRec->quantity, // Количество продукт в основна мярка
+                ),
+            );
+        }
+        
+        return $entries;
+    }
+    
+    
+    /**
+     * Курс на валутата на продажбата към базовата валута за периода, в който попада продажбата
+     * 
+     * @param stdClass $rec запис за продажба
+     * @return float
+     */
+    protected function getCurrencyRate($rec)
+    {
+        return currency_CurrencyRates::getRate($rec->valior, $rec->currencyCode, NULL);
+    }
+}
