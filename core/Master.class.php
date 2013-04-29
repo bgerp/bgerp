ab888ecec32f5fe080ca48967e04980c6467758b
Yusein Yuseinov <yyuseinov@gmail.com>
2013-04-29 14:04:43 +0300

    core_Master - Ако таба на детайла е disabled да не се показва като линк

--- core/Master.class.php
+++ core/Master.class.php
@@ -270,7 +270,7 @@ class core_Master extends core_Manager
 
                     $url = Url::addParams($_SERVER['REQUEST_URI'], array('Tab' => $var)) . '#detailTabs';
  
-                    $tabs->TAB($var, $data->{$var}->TabCaption ? $data->{$var}->TabCaption : $var, $url);
+                    $tabs->TAB($var, $data->{$var}->TabCaption ? $data->{$var}->TabCaption : $var, $data->{$var}->disabled ? array() : $url);
 
                     if($var == $data->Tab || (!$data->Tab && !$selected)) {
                         $selected = $var;
