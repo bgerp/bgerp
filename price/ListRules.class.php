0586ba8739b5b1bd347f6bea18d9d9a27f848cae
arsov <Arsov@EP-BAGS.com>
2013-06-05 17:16:23 +0300

    Примерни часове в полетата за време

--- price/ListRules.class.php
+++ price/ListRules.class.php
@@ -92,8 +92,8 @@ class price_ListRules extends core_Detail
         $this->FLD('groupId', 'key(mvc=price_Groups,select=title,allowEmpty)', 'caption=Група,mandatory');
         $this->FLD('price', 'double(decimals=2)', 'caption=Цена,mandatory');
         $this->FLD('discount', 'percent(decimals=2)', 'caption=Отстъпка,mandatory,placeholder=%');
-        $this->FLD('validFrom', 'datetime', 'caption=В сила->От');
-        $this->FLD('validUntil', 'datetime', 'caption=В сила->До');
+        $this->FLD('validFrom', 'datetime(timeSuggestions=00:00|04:00|08:00|09:00|10:00|11:00|12:00|13:00|14:00|15:00|16:00|17:00|18:00|22:00)', 'caption=В сила->От');
+        $this->FLD('validUntil', 'datetime(timeSuggestions=00:00|04:00|08:00|09:00|10:00|11:00|12:00|13:00|14:00|15:00|16:00|17:00|18:00|22:00)', 'caption=В сила->До');
     }
     
     
