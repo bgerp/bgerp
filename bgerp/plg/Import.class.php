72b10da87244a46c184a8d75e23e40c4d4314a4f
ivelin1012 <ivelin_pdimov@abv.bg>
2013-06-13 18:46:16 +0300

    bgerp_plg_Import: обновления, както и по интерфейса bgerp_ImportIntf

--- bgerp/plg/Import.class.php
+++ bgerp/plg/Import.class.php
@@ -3,8 +3,22 @@
 
 
 /**
- * Плъгин за импорт на данни от Бизнес навигатор
+ * Плъгин за импорт на данни. Импортирват се csv-данни с помоща на драйвър
+ * имплементиращ интерфейса bgerp_ImportIntf.
+ * Плъгина добавя бутон за импортиране към мениджъра където е закачен
+ * За да се импортират csv данни се минава през няколко стъпки с помощна
+ * на експерта (@see expert_Expert).
  * 
+ * Целта е да се уточни:
+ *   1. Кой драйвър ще се използва (клас имплементиращ bgerp_ImportIntf)
+ *   2. Как се въвеждат csv данните с ъплоуд на файл или с copy & paste
+ *   3. Какви са разделителят, ограждането и първия ред на данните
+ *   4. Кои колони от csv-to на кои полета от мениджъра отговарят.
+ * 
+ * След определянето на тези данни драйвъра се грижи за правилното импортиране
+ * 
+ * Мениджъра в който ще се импортира и кои полета от него ще бъдат попълнени
+ * се определя от драйвъра.
  *
  * @category  bgerp
  * @package   bgerp
@@ -45,51 +59,53 @@ class bgerp_plg_Import extends core_Plugin
     
     
 	/**
-   	 * Обработка на SingleToolbar-a
+   	 * Обработка на ListToolbar-a
    	 */
    	static function on_AfterPrepareListToolbar($mvc, &$data)
     {
-    	if(haveRole('admin')){
-    		$importOptions = core_Classes::getOptionsByInterface('bgerp_ImportIntf');
-    		if(count($importOptions)){
-    			$url = array($mvc, 'import', 'retUrl' => TRUE);
-    			$data->toolbar->addBtn('Импорт', $url, NULL, 'ef_icon=img/16/import16.png');
-    		}
+    	// Добавяне на бутон за импортиране, ако има инсталирани драйвъри
+    	if($mvc->haveRightFor('import')){
+    		$url = array($mvc, 'import', 'retUrl' => TRUE);
+    		$data->toolbar->addBtn('Импорт', $url, NULL, 'ef_icon=img/16/import16.png');
     	}
     }
     
     
 	/**
      * Преди всеки екшън на мениджъра-домакин
-     *
-     * @param core_Manager $mvc
-     * @param core_Et $tpl
-     * @param core_Mvc $data
      */
     public static function on_BeforeAction($mvc, &$tpl, $action)
     {
     	if($action == 'import'){
-    		$importOptions = core_Classes::getOptionsByInterface('bgerp_ImportIntf');
-    		expect(count($importOptions), 'Няма инсталирани драйвъри за импортиране');
+    		$mvc->requireRightFor('import');
     		
+    		// Подготвяме експерта
     		$exp = cls::get('expert_Expert', array('mvc' => $mvc));
     		$content = static::solveExpert($exp);
     		
         	if($content == 'SUCCESS') {
+        		
+        		// Извличаме уточнените вече стойностти на параметритр
         		$driverId = $exp->getValue("#driver");
         		$csvData = $exp->getValue("#csvData");
         		$delimiter = $exp->getValue("#delimiter");
         		$enclosure = $exp->getValue("#enclosure");
         		$firstRow = $exp->getValue("#firstRow");
         		
+        		// Намиране на коя колона от csv-то на кое поле съответства
         		$Driver = cls::get($driverId);
         		$fields = $Driver->getFields();
         		foreach($fields as $name => $caption){
         			$fields[$name] = $exp->getValue("#col{$name}");
         		}
         		
+        		// Преобразуване на csv-то в масив, по зададените параметри
         		$rows = static::getCsvRows($csvData, $delimiter, $enclosure, $firstRow, $cols);
+        		
+        		// Импортиране на данните от масива в зададените полета
         		$msg = $Driver->import($rows, $fields);
+        		
+        		// Редирект кум лист изгледа на мениджъра в който се импортира
         		return Redirect(array($Driver->getDestinationManager(), 'list'), 'FALSE', $msg);
         	} 
         	
@@ -108,7 +124,6 @@ class bgerp_plg_Import extends core_Plugin
             }
         	
         	$tpl = $mvc->renderWrapping($content);
-            
             return FALSE;
     	}
     }
@@ -116,15 +131,15 @@ class bgerp_plg_Import extends core_Plugin
     
     /**
      * Връща масив с данните от csv-то
-     * @param string $csvData -
-     * @param char $delimiter -
-     * @param char $enclosure -
-     * @param string $firstRow -
-     * @return array $rows -
+     * @param string $csvData - csv данни
+     * @param char $delimiter - разделител
+     * @param char $enclosure - ограждане
+     * @param string $firstRow - първи ред данни или имена на колони
+     * @return array $rows - масив с парсирани редовете на csv-то
      */
     private static function getCsvRows($csvData, $delimiter, $enclosure, $firstRow)
     {
-    	$textArr = explode(PHP_EOL, $csvData);
+    	$textArr = explode(PHP_EOL, trim($csvData));
     	foreach($textArr as $line){
     		$rows[] = str_getcsv($line, $delimiter, $enclosure);
     	}
@@ -150,35 +165,41 @@ class bgerp_plg_Import extends core_Plugin
    
     
     /**
-     * Enter description here ...
+     * Подготовка на експерта за импортирането (@see expert_Expert)
      * @param expert_Expert $exp
+     * @return string $res
      */
     public static function solveExpert(expert_Expert &$exp)
     {
     	$exp->functions['getfilecontentcsv'] = 'bgerp_plg_Import::getFileContent';
     	$exp->functions['getcsvcolnames'] = 'blast_ListDetails::getCsvColNames';
     		
+    	// Избиране на драйвър за импортиране
     	$exp->DEF('#driver', 'class(interface=bgerp_ImportIntf,select=title)', 'caption=Драйвър,input,mandatory');
     	$exp->question("#driver", tr("Моля, изберете драйвър") . ":", TRUE, 'title=' . tr('Какъв драйвер ще се използва') . '?');
-    		
+
+    	// Избор как ще се въведат данните с copy & paste или с ъплоуд
     	$exp->DEF('#source=Източник', 'enum(csvFile=Файл със CSV данни,csv=Copy&Paste на CSV данни)', 'maxRadio=5,columns=1,mandatory');
         $exp->ASSUME('#source', '"csvFile"');
         $exp->question("#source", tr("Моля, посочете източника на данните") . ":", TRUE, 'title=' . tr('От къде ще се импортират данните') . '?');
-    		
+
+        // Поле за ръчно въвеждане на csv данни
         $exp->DEF('#csvData=CSV данни', 'text(1000000)', 'width=100%,mandatory');
-        $exp->question("#csvData", tr("Моля, поставете данните") . ":", "#source == 'csv'", 'title=' . tr('Въвеждане на CSV данни за контакти'));
+        $exp->question("#csvData,#delimiter,#enclosure,#firstRow", tr("Моля, поставете данните,, и посочете формата на данните") . ":", "#source == 'csv'", 'title=' . tr('Въвеждане на CSV данни за контакти, и уточняване на разделителя и ограждането'));
         	
-        $exp->DEF('#csvFile=CSV файл', 'fileman_FileType(bucket=csvContacts)', 'mandatory');
-        $exp->question("#csvFile", tr("Въведете файл с контактни данни във CSV формат") . ":", "#source == 'csvFile'", 'title=' . tr('Въвеждане на данните от файл'));
+        // Поле за ъплоуд на csv файл
+        $exp->DEF('#csvFile=CSV файл', 'fileman_FileType(bucket=bnav_importCsv)', 'mandatory');
+        $exp->question("#csvFile,#delimiter,#enclosure,#firstRow", tr("Въведете файл с контактни данни във CSV формат, и посочете формата на данните") . ":", "#source == 'csvFile'", 'title=' . tr('Въвеждане на данните от файл, и уточняване на разделителя и ограждането'));
         $exp->rule("#csvData", "getFileContentCsv(#csvFile)");
-        	
+
+        // Полета за избиране на Разделител, ограждане и вида на първия ред
         $exp->DEF('#delimiter=Разделител', 'varchar(1,size=1)', array('value' => ','), 'mandatory');
         $exp->SUGGESTIONS("#delimiter", array(',' => ',', ';' => ';', ':' => ':', '|' => '|'));
         $exp->DEF('#enclosure=Ограждане', 'varchar(1,size=1)', array('value' => '"'), 'mandatory');
         $exp->SUGGESTIONS("#enclosure", array('"' => '"', '\'' => '\''));
         $exp->DEF('#firstRow=Първи ред', 'enum(columnNames=Имена на колони,data=Данни)', 'mandatory');
-        $exp->question("#delimiter,#enclosure,#firstRow", tr("Посочете формата на CSV данните") . ":", "#csvData", 'title=' . tr('Уточняване на разделителя и ограждането'));
-        
+       
+        // Проверка дали броя на колоните отговаря навсякъде
         $exp->rule("#csvColumnsCnt", "count(getCsvColNames(#csvData,#delimiter,#enclosure))");
         $exp->WARNING(tr("Възможен е проблем с формата на CSV данните, защото е открита само една колона"), '#csvColumnsCnt == 2');
         $exp->ERROR(tr("Има проблем с формата на CSV данните"). ". <br>" . tr("Моля проверете дали правилно сте въвели данните и разделителя"), '#csvColumnsCnt < 2');
@@ -188,7 +209,9 @@ class bgerp_plg_Import extends core_Plugin
         	$Driver = cls::get($driverId);
 	        $fieldsArr = $Driver->getFields();
 	        $dManager= $Driver->getDestinationManager();
-	        	
+
+	        // Поставяне на възможност да се направи мачване на 
+	        // полетата от модела и полетата от csv-то
 	    	foreach($fieldsArr as $name => $caption) {
 		        $exp->DEF("#col{$name}={$caption}", 'int', 'mandatory');
 		        $exp->OPTIONS("#col{$name}", "getCsvColNames(#csvData,#delimiter,#enclosure)");
@@ -196,29 +219,31 @@ class bgerp_plg_Import extends core_Plugin
 		            
 		        $qFields .= ($qFields ? ',' : '') . "#col{$name}";
 	        }
-	        	$exp->question($qFields, tr("Въведете съответстващите полета за \"{$dManager->className}\"") . ":", TRUE, 'title=' . tr('Съответствие между полетата на източника и списъка'));
-        		$res = $exp->solve("#driver,#source,#delimiter,#enclosure,#firstRow,{$qFields}");
+	        $exp->question($qFields, tr("Въведете съответстващите полета за \"{$dManager->className}\"") . ":", TRUE, 'title=' . tr('Съответствие между полетата на източника и списъка'));
+        	$res = $exp->solve("#driver,#source,#delimiter,#enclosure,#firstRow,{$qFields}");
         } else {
         	$res = $exp->solve("#driver,#source,#delimiter,#enclosure,#firstRow");
         }
-        	
-        	return $res;
+        
+        // Връщане на резултата
+        return $res;
     }
     
     
-	/**
-     * 
-     * Enter description here ...
+    /**
+     * Извиква се след изчисляването на необходимите роли за това действие
      */
-    private static function prepareImportForm()
+    function on_AfterGetRequiredRoles($mvc, &$res, $action, $rec = NULL, $userId = NULL)
     {
-    	$form = cls::get('core_Form');
-    	$form->title = 'Импортиране';
-    	$form->FNC('importClass', 'class(interface=bgerp_ImportIntf,select=title)', 'caption=Технолог,input,mandatory');
-        $form->FNC('text', 'text(rows=3)', 'caption=Текст,input,width=35em');
-    	$form->FNC('csvFile', 'fileman_FileType(bucket=bnav_importCsv)', 'caption=CSV Файл,input');
-       
-    	return $form;
+        if ($action == 'import') {
+        	
+        	// Неможе да се импортира ако няма инсталиран драйвър
+        	$importOptions = core_Classes::getOptionsByInterface('bgerp_ImportIntf');
+    		if(count($importOptions)){
+    			$res = 'admin';
+    		} else {
+    			$res = 'no_one';
+    		}
+        }
     }
 }
-
