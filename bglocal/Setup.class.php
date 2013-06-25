55f95f3748f52f5c33ff53b2b2a95b83954c6a40
GPetrova <gab4eto@gmail.com>
2013-06-25 17:56:18 +0300

    bglocal:Setup
    Change-Id: Ia9f561fbe4a868a0a760c508af485394c1ae5204

---
+++ bglocal/Setup.class.php
@@ -0,0 +1,83 @@
+<?php
+
+
+/**
+ * class drdata_Setup
+ *
+ * Инсталиране/Деинсталиране на
+ * доктор за адресни данни
+ *
+ *
+ * @category  vendors
+ * @package   drdata
+ * @author    Milen Georgiev <milen@download.bg>
+ * @copyright 2006 - 2012 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class bglocal_Setup extends core_Manager {
+    
+    
+    /**
+     * Версия на пакета
+     */
+    var $version = '0.15';
+    
+    
+    /**
+     * Мениджър - входна точка в пакета
+     */
+    var $startCtr = 'drdata_Countries';
+    
+    
+    /**
+     * Екшън - входна точка в пакета
+     */
+    var $startAct = 'default';
+    
+    
+    /**
+     * Описание на модула
+     */
+    var $info = "Готови данни и типове от различни области";
+
+    
+    /**
+     * Инсталиране на пакета
+     */
+    function install()
+    {
+        
+        $managers = array(
+            
+            //'bglocal_Mvr',
+          	//'bglocal_Banks',
+            //'bglocal_Address',
+       		'bglocal_NKID',
+            'bglocal_NKPD',
+            //'bglocal_DistrictCourts'
+        
+        
+        
+        );
+        
+        $instances = array();
+        
+        foreach ($managers as $manager) {
+            $instances[$manager] = &cls::get($manager);
+            $html .= $instances[$manager]->setupMVC();
+             	             
+        }
+        
+        return $html;
+    }
+    
+    
+    /**
+     * Де-инсталиране на пакета
+     */
+    function deinstall()
+    {
+        return "Пакета drdata е разкачен";
+    }
+}
