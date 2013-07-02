4546fd938d4f55bd568e0859dea58797bcb02d4b
Stefan Stefanov <stefan.bg@gmail.com>
2013-07-02 18:30:04 +0300

    admin може да експедира (canShip).

--- store/plg/Shippable.class.php
+++ store/plg/Shippable.class.php
@@ -23,6 +23,7 @@ class store_plg_Shippable extends core_Plugin
         
         $canShip = arr::make($mvc->canShip, TRUE);
         $canShip['store'] = 'store';
+        $canShip['admin'] = 'admin';
         
         $mvc->canShip = $canShip;
     }
