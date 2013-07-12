36f6af3e2a5134fe2f080cb7396f41b6a104db4b
Yusein Yuseinov <yyuseinov@gmail.com>
2013-07-12 10:41:01 +0300

    bglocal_Address - Сгрешено име на фунцкия на няколко места - getFielContent -> getFileContent

--- bglocal/Address.class.php
+++ bglocal/Address.class.php
@@ -306,19 +306,19 @@ class bglocal_Address extends core_MVC
         static $regards, $companyTypes, $companyWords, $givenNames;
         
         if(empty($regards)) {
-        	$regards = getFielContent('bglocal/data/regards.txt');
+        	$regards = getFileContent('bglocal/data/regards.txt');
         }
         
         if(empty($companyTypes)) {
-        	$companyTypes = getFielContent('bglocal/data/companyTypes.txt');
+        	$companyTypes = getFileContent('bglocal/data/companyTypes.txt');
         }
         
         if(empty($companyWords)) {
-        	$companyWords = getFielContent('bglocal/data/companyWords.txt');
+        	$companyWords = getFileContent('bglocal/data/companyWords.txt');
         }
         
         if(empty($givenNames)) {
-        	$givenNames = getFielContent('bglocal/data/givenNames.txt');
+        	$givenNames = getFileContent('bglocal/data/givenNames.txt');
         }
         
         $div = array('@NO_DIV@', '|');
@@ -417,15 +417,15 @@ class bglocal_Address extends core_MVC
         static $regards, $companyTypes, $companyWords, $givenNames;
         
         if(empty($companyTypes)) {
-        	$companyTypes = getFielContent('bglocal/data/companyTypes.txt');
+        	$companyTypes = getFileContent('bglocal/data/companyTypes.txt');
         }
         
         if(empty($companyWords)) {
-        	$companyWords = getFielContent('bglocal/data/companyWords.txt');
+        	$companyWords = getFileContent('bglocal/data/companyWords.txt');
         }
         
         if(empty($givenNames)) {
-        	$givenNames = getFielContent('bglocal/data/givenNames.txt');
+        	$givenNames = getFileContent('bglocal/data/givenNames.txt');
         }
         
         if(strpos(trim($line), '>') === 0) return;
