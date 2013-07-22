019193406046619bd739e01f237b2cbb84726416
Yusein Yuseinov <yyuseinov@gmail.com>
2013-07-22 11:59:51 +0300

    fileman_view_DialogWrapper -  #C1886 - Използване на `scss` вместо `css`

--- fileman/view/DialogWrapper.class.php
+++ fileman/view/DialogWrapper.class.php
@@ -24,11 +24,7 @@ class fileman_view_DialogWrapper extends page_Html {
         
         $this->append("<link rel=\"stylesheet\" type=\"text/css\" href=" . sbf("fileman/css/default.css") . "/>\n", "HEAD");
         
-        if(Mode::is('screenMode', 'narrow')) {
-            $this->append("<link rel=\"stylesheet\" type=\"text/css\" href=" . sbf("css/narrowCommon.css") . "/>\n", "HEAD");
-        } else {
-            $this->append("<link rel=\"stylesheet\" type=\"text/css\" href=" . sbf("css/wideCommon.css") . "/>\n", "HEAD");
-        }
+        $this->append("<link rel=\"stylesheet\" type=\"text/css\" href=" . sbf("css/common.css") . "/>\n", "HEAD");
         
         $this->append("<script type=\"text/javascript\" src=" . sbf("js/efCommon.js") . "></script>\n", "HEAD");
         
