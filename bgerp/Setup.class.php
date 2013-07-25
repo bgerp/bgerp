5dab3d6ea9c6a8f7eb160a42133a4d923541f7ea
Yusein Yuseinov <yyuseinov@gmail.com>
2013-07-25 17:22:32 +0300

    До таба Система->Файлове да имат достъп всички `powerUser` потребители

--- bgerp/Setup.class.php
+++ bgerp/Setup.class.php
@@ -195,7 +195,7 @@ class bgerp_Setup {
         
         $html .= $Menu->addItem(1.62, 'Система', 'Ядро', 'core_Packs', 'default', 'admin');
 //        $html .= $Menu->addItem(1.64, 'Система', 'bgERP', 'bgerp_Menu', 'default', 'admin');
-        $html .= $Menu->addItem(1.66, 'Система', 'Файлове', 'fileman_Files', 'default', 'admin');
+        $html .= $Menu->addItem(1.66, 'Система', 'Файлове', 'fileman_Files', 'default', 'powerUser');
 //        $html .= $Menu->addItem(1.68, 'Система', 'Данни', 'drdata_Countries', 'default', 'admin');
         
         $html .= $Menu->repair();
