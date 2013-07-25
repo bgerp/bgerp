f5210765da5c9c8df0379d914e524606acfa9d8a
Yusein Yuseinov <yyuseinov@gmail.com>
2013-07-25 17:23:33 +0300

    Имейли->Входящи - `admin` да има достъп до тях

--- email/Wrapper.class.php
+++ email/Wrapper.class.php
@@ -24,7 +24,15 @@ class email_Wrapper extends plg_ProtoWrapper
     {
         //Показва таба за постинги, само ако имаме права за листване
         $this->TAB('email_Outgoings', 'Изходящи', 'ceo, admin, user');
-        $this->TAB('email_Incomings', 'Входящи', 'ceo');
+        
+        // Ако имаме права ceo
+        // Да е първия таб
+        if (haveRole('ceo')) {
+            $this->TAB('email_Incomings', 'Входящи', 'admin, ceo');
+        } else {
+            $this->TAB('email_Returned', 'Входящи', 'admin, ceo');
+        }
+        
         $this->TAB('email_Inboxes', 'Кутии', 'ceo, admin, user');
         $this->TAB('email_Accounts', 'Сметки', 'admin');
         $this->TAB('email_Filters', 'Рутиране', 'admin, debug');
