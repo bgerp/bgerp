50200c2c7ea0dc951198bd04f19552c0b1719336
ivelin1012 <ivelin_pdimov@abv.bg>
2013-07-26 18:15:10 +0300

    plg_Current: потребителя вижда само тези обекти за които отговаря, както и само тях може да избира

--- plg/Current.class.php
+++ plg/Current.class.php
@@ -16,6 +16,15 @@
 class plg_Current extends core_Plugin
 {
     
+	/**
+     * Извиква се след описанието на модела
+     */
+    function on_AfterDescription(&$mvc)
+    {
+    	// Как ще се казва полето за отговорника на модела,
+        setIfNot($mvc->inChargeField, 'inCharge');
+    }
+    
     
     /**
      * Връща указаната част (по подразбиране - id-то) на текущия за сесията запис
@@ -30,8 +39,20 @@ class plg_Current extends core_Plugin
         if(!$res) {
             $res = Mode::get('currentPlg_' . $mvc->className)->{$part};
             
+            
             if($bForce && (!$res) && ($mvc->className != Request::get('Ctr'))) {
-                redirect(array($mvc), FALSE, "Моля, изберете текущ/а {$mvc->singleTitle}");
+            
+            	// Ако потребителя има достъп само до 1 запис, той се приема
+	            // за избран
+	            $query = $mvc->getQuery();
+	            $cu = core_Users::getCurrent();
+				$query->where("#{$mvc->inChargeField} = {$cu} || #{$mvc->inChargeField} LIKE '%|{$cu}|%'");
+	            if($query->count() == 1){
+	            	$rec = $query->fetch();
+	            	Redirect(array($mvc, 'SetCurrent', $rec->id));
+	            }
+	            
+            	redirect(array($mvc), FALSE, "Моля, изберете текущ/а {$mvc->singleTitle}");
             }
         }
     }
@@ -97,5 +118,32 @@ class plg_Current extends core_Plugin
             $row->currentPlg = ht::createBtn('Избор', array($mvc, 'SetCurrent', $rec->id), NULL, NULL, 'ef_icon = img/16/key.png');
             $row->ROW_ATTR['class'] .= ' state-closed';
         }
+    }
+    
+    
+	/**
+	 * Преди подготовка на резултатите
+	 */
+	function on_BeforePrepareListRecs($mvc, $res, $data)
+	{
+		if(!haveRole('ceo')){
+			
+			// Показват се само записите за които отговаря потребителя
+			$cu = core_Users::getCurrent();
+			$data->query->where("#{$mvc->inChargeField} = {$cu} || #{$mvc->inChargeField} LIKE '%|{$cu}|%'");
+		}
+	}
+	
+	
+	/**
+     * Изпълнява се след подготовката на ролите, които могат да изпълняват това действие.
+     */
+    public static function on_AfterGetRequiredRoles($mvc, &$res, $action, $rec = NULL, $userId = NULL)
+    {
+    	if($action == 'write' && isset($rec)){
+    		if(($rec->{$mvc->inChargeField} != $userId && strpos($rec->{$mvc->inChargeField}, "|$userId|") === FALSE) && !haveRole('ceo')){
+    			$res = 'no_one';
+    		}
+    	}
     }
 }
