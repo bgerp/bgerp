1be2cd2d9a29c55569e2b727ad773004dcc7d1e4
Milen Georgiev <milen@experta.bg>
2013-08-26 21:06:20 +0300

    type_Bigint

---
+++ type/Bigint.class.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+/**
+ * Кой символ да използваме за разделител на хилядите?
+ */
+defIfNot('EF_NUMBER_THOUSANDS_SEP', ' ');
+
+
+/**
+ * Клас  'type_Bigint' - Тип за цели числа
+ *
+ *
+ * @category  ef
+ * @package   type
+ * @author    Milen Georgiev <milen@download.bg>
+ * @copyright 2006 - 2012 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ * @link
+ */
+class type_Bigint extends type_Int {
+    
+    
+    /**
+     * MySQL тип на полето в базата данни
+     */
+    var $dbFieldType = 'bigint';
+    
+    
+    /**
+     * Дължина на полето в mySql таблица
+     */
+    var $dbFieldLen = '21';
+    
+    
+}
