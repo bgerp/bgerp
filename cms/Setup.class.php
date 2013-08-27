884940f887566fc357b2c5f3cedb2ab2d735f046
GPetrova <gab4eto@gmail.com>
2013-08-27 15:00:29 +0300

    bugFix: Неспазен стил на задаване на вербална стойност на константа

--- cms/Setup.class.php
+++ cms/Setup.class.php
@@ -65,11 +65,11 @@ class cms_Setup extends core_ProtoSetup
 	 */
 	var $configDescription = array(
 	
-			'CMS_THEME' => array ('varchar', 'caption=Тема по подразбиране'),
+			'CMS_THEME' => array ('varchar', 'caption=Тема по подразбиране->Път до темата'),
 
-            'CMS_SHARE' => array ('html', 'caption=Код за споделяне'),
+            'CMS_SHARE' => array ('html', 'caption=Код за споделяне-> HTML код'),
 	
-			'CMS_OGRAPH_IMAGE' => array ('fileman_FileType(bucket=pictures)', 'caption=Изображение за ograph-а'),
+			'CMS_OGRAPH_IMAGE' => array ('fileman_FileType(bucket=pictures)', 'caption=Изображение за ograph-а->Изображение'),
 	
 	);
 
