2d861be7c0904e423ed7fb02ac7e44e0c97a5568
Milen Georgiev <milen@experta.bg>
2013-09-03 13:10:17 +0300

    А - кратко име за статия, + шаблон за избор на език

---
+++ bgerp/A.class.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+/**
+ * CMS статии
+ *
+ * @category  bgerp
+ * @package   bgerp
+ * @author    Yusein Yuseinov <yyuseinov@gmail.com>
+ * @copyright 2006 - 2012 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class bgerp_A extends cms_Articles
+{
+    
+    function act_A()
+    {
+        return $this->act_Article();
+    }
+}
