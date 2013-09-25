3de239a99472c89037a3047bcabaf580cf072078
HeBuTy <vencho_27@abv.bg>
2013-09-25 17:05:20 +0300

    bugFix js за височина на клетките

--- sales/js/ResizeQuoteTable.js
+++ sales/js/ResizeQuoteTable.js
@@ -2,8 +2,11 @@ $(document).ready(function () {
 	$('td[id^=product-row]').each(function(){
 		 var id = $(this).attr('id');
 		 id = id.replace('product-row', '');
-		 var h = $(this).height();
+		 //взимаме височината на клетката + падинг-а 
+		 var h = $(this).outerHeight(); 
 		 var eH = h/$('td[class^=product-row'+id+']').length;
+		 //от получената височина вадим падинг-а на клетката
+		 eH = eH - 10;
 		 $('td[class^=product-row'+id+']').each(function(){
 		        $(this).height(eH);
 		 });
