45a24b30240db259f65e7bd06f234f7e0a6b9ee4
GPetrova <gab4eto@gmail.com>
2013-10-03 11:48:36 +0300

    Сигнал #Sig394

--- trz/LeavesWrapper.class.php
+++ trz/LeavesWrapper.class.php
@@ -5,9 +5,15 @@ function on_AfterRenderWrapping($mvc, &$tpl)
     {
         $tabs = cls::get('core_Tabs', array('htmlClass' => 'alphabet'));
 
-        $tabs->TAB('trz_Requests', 'Молби');
-        $tabs->TAB('trz_Orders', 'Заповеди');
-
+        // ако имаме форма за добавяне или редактирване
+        // скриваме вътрешните бутони
+        if(Request::get('Act') == 'add' || Request::get('Act') == 'edit'){
+        	 $tabs;
+        } else {
+        	 $tabs->TAB('trz_Requests', 'Молби');
+        	 $tabs->TAB('trz_Orders', 'Заповеди');
+        }
+        
         $tpl = $tabs->renderHtml($tpl, $mvc->className);
         $mvc->currentTab = 'Отпуски >> Молби';
     }
