6b65eb5ffae7946ba9610579d2d9e6e220445532
GPetrova <gab4eto@gmail.com>
2013-10-07 12:00:31 +0300

    bugFix: зарежда се csv-то от празна база винаги
    Change-Id: I78c54528524f57a4286fe44c52b23d83c3cef54d

--- bglocal/NKPD.class.php
+++ bglocal/NKPD.class.php
@@ -77,7 +77,7 @@ class bglocal_NKPD extends core_Master
     {
  		$file = "bglocal/data/nkpd.csv";
     	$fields = array(0 => "csv_key", 1 => "csv_title", 2 => "csv_position");
-    	$cntObj = csv_Lib::importOnce($mvc, $file, $fields);
+    	$cntObj = csv_Lib::importOnceFromZero($mvc, $file, $fields);
     	$res .= $cntObj->html;
     }
 }
