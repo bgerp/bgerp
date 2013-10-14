33b6e930491c8d55ac264e32e5330ab94c412744
Tester <arsov@experta.bg>
2013-10-14 18:01:36 +0300

    Revert

--- acc/Accounts.class.php
+++ acc/Accounts.class.php
@@ -84,12 +84,6 @@ class acc_Accounts extends core_Manager
     var $canEditsysdata = 'acc';
 
     
-    /**  
-     * Кой има право да променя състоянието?  
-     */  
-    var $canChangeState = 'acc, ceo';
-
-    
     /**
      * Брой записи на страница
      */
