da2e5dc57f7ca605765badcdf4d4fd4492df15f8
GPetrova <gab4eto@gmail.com>
2013-10-22 18:26:28 +0300

    Форматиране на информация за контакти

--- bgerp/plg/CommunicationFormat.class.php
+++ bgerp/plg/CommunicationFormat.class.php
@@ -14,73 +14,82 @@
  */
 class bgerp_plg_CommunicationFormat extends core_Plugin
 {
-    static $tel = array("тел", "телефон", "tel" , "telephone", "phone", "mobile", "mob",
-			    	 	"тел:", "телефон:", "tel:" , "telephone:", "phone:", "mobile:", "mob:",
-			    	 	"тел.", "телефон.", "tel." , "telephone.", "phone.", "mobile.", "mob.");
-    static $email = array("company email", "e-mail", "e-mayl", "e-meyl", "e-post", "e", 
-    				   	  "emai" , "email msn", "email to", "email", "emne", "epost", "mail to",
-    				      "mailto", "mail", "personal e-mail", "@",
-    				      "company email:", "e-mail:", "e-mayl:", "e-meyl:", "e-post:", "e:", 
-    				      "emai:" , "email msn:", "email to:", "email:", "emne:", "epost:", "mail to:",
-    				      "mailto:", "mail:", "personal e-mail:", "@:",
-    				      "company email.", "e-mail.", "e-mayl.", "e-meyl.", "e-post.", "e.", 
-    				      "emai." , "email msn.", "email to.", "email.", "emne.", "epost.", "mail to.",
-    				      "mailto.", "mail.", "personal e-mail.", "@.");
+    
 	/**
-     * 
+     * Обработваме елементите линковете, които сочат към ISQ, Scype, tel
      */
-    function on_AfterCatchRichElements1($mvc, &$html)
+    function on_AfterCatchRichElements($mvc, &$html)
     {
-     
-       $this->mvc = $mvc;
 
-       //Ако намери съвпадение на регулярния израз изпълнява функцията
-       // Обработваме елементите, който са от вида услига : или . или интервал последвана от email 
-       $html = preg_replace_callback("/([\r\n]{0,2}[a-zа-я\-]{3,32}[ .:]?)([ ]?[-_a-z0-9\'+*$^&%=~!?{}]+(?:\.[-_a-z0-9\'+*$^&%=~!?{}]+)*+@(?:(?![-.])[-a-z0-9.]+(?<![-.])\.[a-z]{2,6}|\d{1,3}(?:\.\d{1,3}){3})(?::\d++)?)/is", array($this, 'catchCommunicationFormat'), $html);
+       $this->mvc = $mvc;
        
        //Ако намери съвпадение на регулярния израз изпълнява функцията
-       // Обработваме елементите, който са всичко останало
-       $html = preg_replace_callback("/([\r\n]{0,2}[a-zа-я\-]{3,32}[ .:]{1})([ ]?[-_a-z0-9\'+*$^&%=~!?{}]++(?:\.[-_a-z0-9\'+*$^&%=~!?{}]+)*)/is", array($this, 'catchCommunicationFormat'), $html);
-
+       $html = preg_replace_callback("/^\s*((Тел|Телефон|Tel|Telephone|Phone|Mobile|Mob|AIM|YIM|MSNIM|MSN|XMPP|Jabber|Skype|ICQ)\.?\:? *)([a-zA-Z0-9_\-\@\.\+]{3,64})/umi", array($this, 'catchCommunicationFormat'), $html);
     }
     
     
     /**
+     * Обработваме всички елементи в richText-а,
+     * които са от вида на "Skype: скайп_име" или "ICQ номер на icq потребител"
+     * и започват на нов ред.
+     * Заместваме ги с линк към съответната услуга
      * 
-     * Enter description here ...
-     * @param unknown_type $match
+     * @param array $match
      */
     function catchCommunicationFormat($match)
     {
+        // намираме мястото, което ще заместваме
         $place = $this->mvc->getPlace();
-    
-        if(in_array(strtolower(trim($match[1])), static::$email)){
-        	
-        }
         
-        // Ако услугата е телефон
-        elseif(in_array(trim(strtolower($match[1])), static::$tel)){ 
-        	
-        	$this->mvc->_htmlBoard[$place] = "<div>{$match[1]}<a href='tel:{$match[2]}' title='phone'>{$match[2]}</a></div>";
+        // елемент съдържащ: телефонен номер или потребителско име/номер
+        $matchElement = trim(strtolower($match[2]));
 
-          // Ако услугата е skype
-        } elseif(trim(strtolower($match[1])) == "skype" || trim(strtolower($match[1])) == "skype." || trim(strtolower($match[1])) == "skype:") {
+        // в зависимост от услугата, правим различни линкове
+        switch ($matchElement) {
         	
-        	$skypeUser = trim($match[2]);
+        	case 'тел' :
+        	case 'телефон' :
+        	case 'tel' :
+        	case 'telephone' :
+        	case 'phone' :
+        	case 'mobile' :
+        	case 'mob' :
+        		$this->mvc->_htmlBoard[$place] = "<span class='communication'><a href='tel:{$match[3]}' title='phone'>{$match[3]}</a>";
+        	    break;
+        	    
+        	case 'msnim' :
+        	case 'msn' :
+        		$this->mvc->_htmlBoard[$place] = "<span class='communication'><a class='url' href='msnim:chat?contact={$match[3]}' title='MSN'>{$match[3]}</a>";
+        		break;
+
+        	case 'xmpp' :
+        	case 'jabber' :
+        		 $this->mvc->_htmlBoard[$place] = "<span class='communication'><a class='url' href='xmpp:{$match[3]}' title='{$match[2]}'>{$match[3]}</a>";
+        		 break;
+        		 
+	        case 'skype' :
+		        $skypeUser = trim($match[3]);
         	
-        	$this->mvc->_htmlBoard[$place] = "<div>{$match[1]}<a class='url' href='skype:{$skypeUser}?call'>{$match[2]}</a></div>";
+        		$this->mvc->_htmlBoard[$place] = "<span class='communication'><a class='url' href='skype:{$skypeUser}?call' title='Skype'>{$match[3]}</a>";
+		        break;
+		        
+	        case 'aim' : 
+		        $this->mvc->_htmlBoard[$place] = "<span class='communication'><a class='url' href='aim:goim?screenname={$match[3]}' title='AOL Instant Messenger (AIM)'>{$match[3]}</a>";
+		        break;
+		        
+	        case 'yim' :
+		        $this->mvc->_htmlBoard[$place] = "<span class='communication'><a class='url' href='ymsgr:sendIM?{$match[3]}' title='Yahoo! Messenger'>{$match[3]}</a>";
+		        break;
+		 		        
+		    case 'icq' :
+		        $this->mvc->_htmlBoard[$place] = "<span class='communication'><a class='url' type='application/x-icq' 
+		         																			href='http://www.icq.com/people/cmd.php?uin=[{$match[3]}]&action=message'>{$match[3]}</a>";
+		        break;
         }
         
-        // ако не удовлетворява другите условия връщаме тестов див
-        // за да знаем, че отговаря на регулярните изрази
-        else{
-      
-			$title = htmlentities($match[2], ENT_COMPAT, 'UTF-8');
-				        
-			$this->mvc->_htmlBoard[$place] = "<div><span class='tel'><span class='value'>$match[1]{$title}</span></span></span></div>";
-        }
-        
-    	return "[#{$place}#]";
-      
+        // посочваме мястото където ще за заменят линковете
+        $communicationFormat = str_replace($match[3], "[#{$place}#]", $match[0]);
+    	
+        return $communicationFormat;
     }
 }
