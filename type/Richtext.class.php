a0335c0e9e356c4172c0401835d4fb3ecbcc3b7b
HeBuTy <vencho_27@abv.bg>
2013-10-29 10:27:05 +0200

    bugFix #C2395: иконката за външните линкове вече е с картинка, а не с фон

--- type/Richtext.class.php
+++ type/Richtext.class.php
@@ -821,8 +821,8 @@ class type_Richtext extends type_Blob
         if($title{0} != ' ') {
             
             $bgPlace = $this->getPlace();
-            $this->_htmlBoard[$bgPlace] = "background-image:url('//www.google.com/s2/u/0/favicons?domain={$domain}');";
-            $link = "<a href=\"[#{$place}#]\" target=\"_blank\" class=\"out linkWithIcon\" style=\"[#{$bgPlace}#]\">[#{$titlePlace}#]</a>";  
+            $this->_htmlBoard[$bgPlace] = ht::createElement('img', array('src' => "//www.google.com/s2/u/0/favicons?domain={$domain}", 'class'=>'outLinkIcon'));
+            $link = "<a href=\"[#{$place}#]\" target=\"_blank\" class=\"out linkWithIcon\">[#{$bgPlace}#][#{$titlePlace}#]</a>";  
               
         } else {
             $link = "<a href=\"[#{$place}#]\" target=\"_blank\" class=\"out\">[#{$titlePlace}#]</a>";
