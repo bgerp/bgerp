2ec7943b78adb07ebd0ae214f13bd469b164768b
HeBuTy <vencho_27@abv.bg>
2013-10-29 17:29:10 +0200

    Промени по help-a: показване в активния таб

--- js/tooltipCustom.js
+++ js/tooltipCustom.js
@@ -1,54 +1,65 @@
 function tooltipCustom(){
+	if($('.tooltip-button').length){
+		var checkVisibility = $('.tooltip-text');
+		if(checkVisibility.hasClass('show-tooltip')){
+			$('.tooltip-text').css("display","block");
+		}
 	
-	var checkVisibility = $('.tooltip-text');
-	if(checkVisibility.hasClass('show-tooltip')){
-		$('.tooltip-text').css("display","block");
+		//изчислява на позицията на стрелката
+		setArrowPosition();
+		
+		//задава като max-width на тултипа разстоянието до активния таб
+		setTooltipMaxWidth();
+		
+		if (isTouchDevice()){
+			//ако сменим ориентацията на телефона, изчисляваме отново позицията на стрелката
+			$(window).resize( function() {
+				setArrowPosition();
+				setTooltipMaxWidth();
+			});
+		}
+		
+		//при клик на бутона, да се скрива и показва инфото и да се изчисли позицията на стрелката
+		 $('.tooltip-button').click(function(e) {
+		     $('.tooltip-text').fadeToggle("slow");
+		     setArrowPosition();
+		     e.stopPropagation();
+		 });
+		 
+		//при клик на тултипа, да не се скрива
+		 $('.tooltip-text').click(function(e) {
+		     e.stopPropagation();
+		 });
+		 
+		 //ако кликнем извън бутона или тултипа да се скрива инфото
+		 $(document).click(function () {
+		     $('.tooltip-text').fadeOut("slow");
+		 });
 	}
-	//изчислява на позицията на стрелката
-	setArrowPosition();
-	
-	//задава като max-width ширината на listTable
-	setTooltipMaxWidth();
-	
-	if (isTouchDevice()){
-		//ако сменим ориентацията на телефона, изчисляваме отново позицията на стрелката
-		$(window).resize( function() {
-			setArrowPosition();
-		});
-	}
-	
-	//при клик на бутона, да се скрива и показва инфото и да се изчисли позицията на стрелката
-	 $('.tooltip-button').click(function(e) {
-	     $('.tooltip-text').fadeToggle("slow");
-	     setArrowPosition();
-	     e.stopPropagation();
-	 });
-	 
-	 //ако кликнем извън бутона да се скрива инфото
-	 $(document).click(function () {
-	     $('.tooltip-text').fadeOut("slow");
-	 });
 }
 
 function setTooltipMaxWidth(){
-	var listTable = $('.tooltip-button').closest('.switching-display').find('.listTable');
-	if(listTable.length){
-		var mwidth = listTable.css('width');
-		var padding = $('.tooltip-text').css("paddingLeft");
-		mwidth = parseInt(mwidth) - 2 * parseInt(padding);
-		if(mwidth > 450 && mwidth < 950){
-			$('.tooltip-text').css('maxWidth', mwidth);
-		}
+	var tooltip = $('.tooltip-button');
+	var mwidth = tooltip.offset().left;
+	if(mwidth > 700){
+		$('.tooltip-text').css('maxWidth', mwidth);
 	}
 }
 
 function setArrowPosition(){
-	if($('.tooltip-button').length){
-		var leftOffet = $('.tooltip-button').offset().left;
-		var leftOffetBlock = $('.tooltip-text').offset().left;
-		leftOffet = parseInt(leftOffet) - parseInt(leftOffetBlock);
-		$('.tooltip-arrow').css("left",leftOffet ) ;
+	
+	var leftOffet = $('.tooltip-button').offset().left;
+	var leftOffetBlock = $('.tooltip-text').offset().left;
+	
+	//заради разликата в големината на картинката в двата изгледа
+	var offset = 2;
+	if($('body').hasClass('narrow')) {
+		offset=0;
 	}
+	
+	leftOffet = parseInt(leftOffet) - parseInt(leftOffetBlock) - offset;
+	$('.tooltip-arrow').css("left",leftOffet ) ;
+	
 }
 
 function isTouchDevice(){
