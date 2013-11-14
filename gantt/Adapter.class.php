5f7124c81dd83716f989f1d2e87fa4abdf0f14d8
HeBuTy <vencho_27@abv.bg>
2013-11-14 11:48:04 +0200

    gantt: промени по адаптера

--- gantt/Adapter.class.php
+++ gantt/Adapter.class.php
@@ -14,52 +14,27 @@ class gantt_Adapter extends core_Mvc
 {    
 
     /**
-     * начертаване на гант таблици по даден двумерен масив
-     * 
-     * @param array $ganttData - двумерен масив, от който вземаме данните за гант таблицa
-     * @param $startTime - timestamp на началото на таблицата
-   	 * @param $endTime - timestamp на края на таблицата
-     * $ganttData трябва да има следната структура:
-     * $ganttData = array(
-	 *		  	'0' => array (
-    					'taskId' => "Tsk23",
-    					'rowId' => 0,
-    					'duration' => 3600,
-    					'startTask' => 1451599199, 
-    					'color' => "#c00",
-    					'hint' => '',
-    					'url' => ''
-    			),
-	 * taskId - id на задачата
-	 * rowId - в кой ред от таблицата се намира
-     * duration - продължителност в секунди
-     * startTask - timestamp на началото на задачата
-     * color - фон на графиката й
-     * hint - hint  при ховър
-     * url - url към сингъла й
-     * 
+     * начертаване на гант таблици по дадена структура
+     * @param array $ganttData - структура, от която вземаме данните за гант таблицa
      */
-    static function render_($ganttData, $startTime, $endTime)
+    static function render_($ganttData)
     {
     	static $ganttChartCnt;
-    	
     	if(!$ganttChartCnt) $orgChartCnt = 0;
-    	
     	$ganttChartCnt++;
-    	
-    	$idChart = 'ganttTable' . $ganttChartCnt;
-    
-    	
-    
-        // тестов шаблон
-        $tpl = getTplFromFile('gantt/mockup/mockup.shtml');
+    	$idChart = 'ganttTableHolder' . $ganttChartCnt;
+          
+        $tpl = new ET();
+        $ganttHolder = ht::createElement('div',  array('id' => $idChart), $tpl);
+        $tpl->append($ganttHolder);
+        
         jquery_Jquery::enable($tpl);
          
         $tpl->push('gantt/lib/ganttCustom.css', 'CSS');
         $tpl->push('gantt/lib/ganttCustom.js', 'JS');
             
         $ganttData = json_encode($ganttData);
-        jquery_Jquery::run($tpl, "ganttRender($('#{$idChart}'),{$startTime},{$endTime},{$ganttData});");
+        jquery_Jquery::run($tpl, "ganttRender($('#{$idChart}'),{$ganttData});");
         
         return $tpl;
     }
