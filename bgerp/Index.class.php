c2da712935f4636e7f59987888b8fa8ee11b0ba4
ivelin1012 <ivelin_pdimov@abv.bg>
2013-11-20 16:36:40 +0200

    bgerp_Index: bugFix с пътя към colab_Profiles койтос е връщаше в рет урл-то

--- bgerp/Index.class.php
+++ bgerp/Index.class.php
@@ -27,15 +27,17 @@ class bgerp_Index extends core_Manager
                 
                 return new Redirect(array('bgerp_Menu', 'Show'));
             } else {
-                if(haveRole('powerUser')){
-                	
-                	return new Redirect(array('bgerp_Portal', 'Show'));
-                } else {
-                	
-                	return new Redirect(array('colab_Profiles', 'Single'));
-                }
+            	if(core_Users::getCurrent('id', FALSE)){
+            		if(haveRole('powerUser')){
+            			return new Redirect(array('bgerp_Portal', 'Show'));
+            		} else {
+            			return new Redirect(array('colab_Profiles', 'Single'));
+            		}
+            	}
+            	return new Redirect(array('bgerp_Portal', 'Show'));
             }
-        } else {
+                
+         } else {
             return new Redirect(array('cms_Content', 'Show'));
         }
     }
