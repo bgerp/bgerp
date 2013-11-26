103c1da534818704a3351a8114edaddce030193e
ivelin1012 <ivelin_pdimov@abv.bg>
2013-11-26 11:32:20 +0200

    ПДУ и ППУ: добавен minDecimals вместо decimals

--- purchase/ServicesDetails.class.php
+++ purchase/ServicesDetails.class.php
@@ -113,7 +113,7 @@ class purchase_ServicesDetails extends core_Detail
         $this->FNC('packQuantity', 'double(decimals=2)', 'caption=К-во,input=input,mandatory');
         
         // Цена за опаковка (ако има packagingId) или за единица в основна мярка (ако няма packagingId)
-        $this->FNC('packPrice', 'double(decimals=2)', 'caption=Цена,input=none');
+        $this->FNC('packPrice', 'double(minDecimals=2)', 'caption=Цена,input=none');
         
         $this->FLD('discount', 'percent', 'caption=Отстъпка,input=none');
     }
