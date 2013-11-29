52e7560720c47968f0d3eb39aad315b4d7471329
ivelin1012 <ivelin_pdimov@abv.bg>
2013-11-29 18:36:51 +0200

    Т. Линии показват и междускладови трансфери

--- trans/Lines.class.php
+++ trans/Lines.class.php
@@ -84,7 +84,7 @@ class trans_Lines extends core_Master
     /**
      * Детайла, на модела
      */
-    public $details = 'Shipments=store_ShipmentOrders,Receipts=store_Receipts';
+    public $details = 'Shipments=store_ShipmentOrders,Receipts=store_Receipts,Transfers=store_Transfers';
     
     
     /**
