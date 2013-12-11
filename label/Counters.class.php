3ccdbba5fcff32546db5632d1effa149a69ffc12
Yusein Yuseinov <yyuseinov@gmail.com>
2013-12-11 16:40:54 +0200

    label_Counters - Оттеглените броячи да не могат да се редактират

--- label/Counters.class.php
+++ label/Counters.class.php
@@ -251,4 +251,32 @@ class label_Counters extends core_Master
             }
         }
     }
+    
+    
+    /**
+     * Изпълнява се след подготовката на ролите, които могат да изпълняват това действие.
+     *
+     * @param label_Labels $mvc
+     * @param string $requiredRoles
+     * @param string $action
+     * @param stdClass $rec
+     * @param int $userId
+     */
+    public static function on_AfterGetRequiredRoles($mvc, &$requiredRoles, $action, $rec = NULL, $userId = NULL)
+    {
+        // Ако има запис
+        if ($rec) {
+            
+            // Ако редактираме
+            if ($action == 'edit') {
+                
+                // Ако е оттеглено
+                if ($rec->state == 'rejected') {
+                    
+                    // Оттеглените да не могат да се редактират
+                    $requiredRoles = 'no_one';
+                }
+            }
+        }
+    }
 }
