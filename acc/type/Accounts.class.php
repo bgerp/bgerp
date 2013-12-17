28854620185219716e7e2d850b238445e7399f9f
ivelin1012 <ivelin_pdimov@abv.bg>
2013-12-17 15:00:10 +0200

    acc_type_Accounts: възможност за показване само на сметки чиито аналитичности поддържат определени интерфейси

--- acc/type/Accounts.class.php
+++ acc/type/Accounts.class.php
@@ -18,7 +18,7 @@ class acc_type_Accounts extends type_Keylist
     
     
     /**
-     * @todo Чака за документация...
+     * Максимум предложения
      */
     const MAX_SUGGESTIONS = 1000;
     
@@ -32,6 +32,8 @@ class acc_type_Accounts extends type_Keylist
         
         setIfNot($params['params']['select'], 'title');
         setIfNot($params['params']['root'], '');
+        setIfNot($params['params']['regInterfaces'], '');
+        
         setIfNot($params['params']['maxSuggestions'], self::MAX_SUGGESTIONS);
         
         parent::init($params);
@@ -51,8 +53,63 @@ class acc_type_Accounts extends type_Keylist
         $mvc = cls::get($this->params['mvc']);
         $root = $this->params['root'];
         $select = $this->params['select'];
+        $regInterfaces = $this->params['regInterfaces'];
         
-        $this->suggestions = $mvc->makeArray4Select($select, array("#num LIKE '[#1#]%' AND state NOT IN ('closed')", $root));
+        $suggestions = $mvc->makeArray4Select($select, array("#num LIKE '[#1#]%' AND state NOT IN ('closed')", $root));
+    	
+        // Ако има зададени интерфейси на аналитичностите
+        if($regInterfaces){
+    		$this->filterSuggestions($regInterfaces, $suggestions);
+    	}
+    	
+    	$this->suggestions = $suggestions;
+    }
+    
+    
+    /**
+     * Помощна ф-я филтрираща опциите на модела, така че аналитичностите на
+     * сметките да отговарят на някакви интерфейси. Подредбата на итнерфейсите
+     * трябва да отговаря на тази на аналитичностите
+     * 
+     * @param string $list - имената на интерфейсите разделени с "|"
+     * @param array $suggestions - подадените предложения
+     */
+    private function filterSuggestions($list, &$suggestions)
+    {
+    	$arr = explode('|', $list);
+    	expect(count($arr) <= 3, 'Най-много могат да са зададени 3 итнерфейса');
+    	foreach ($arr as $index => $el){
+    		$arr[$index] = core_Interfaces::fetchField("#name = '{$el}'", 'id');
+    	}
+    	
+    	if(count($suggestions)){
+    		
+    		// За всяка сметка
+    		foreach ($suggestions as $id => $sug){
+    			
+    			// Извличане на записа на сметката
+    			$rec = acc_Accounts::fetch($id);
+    			
+    			// За всеки итнерфейс
+    			foreach ($arr as $index => $el){
+    				
+    				// Ако съответния запис няма аналитичност се премахва
+    				$fld = "groupId" . ++$index;
+    				if(!isset($rec->$fld)) {
+    					unset($suggestions[$id]);
+    					break;
+    				}
+    				
+    				// Ако има аналитичност, се извлича интерфейса, който поддържа
+    				$listIntf = acc_Lists::fetchField($rec->$fld, 'regInterfaceId');
+    				
+    				// Ако интерфейса не съвпада с подадения, записа се премахва
+    				if($listIntf != $el){
+    					unset($suggestions[$id]);
+    				}
+    			}
+    		}
+    	}
     }
     
     
