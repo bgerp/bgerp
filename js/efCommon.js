ed3a507ab8af012878dbc4857f36b3d0ae5500ef
Yusein Yuseinov <yyuseinov@gmail.com>
2014-01-10 14:48:56 +0200

    bugFix

--- js/efCommon.js
+++ js/efCommon.js
@@ -615,9 +615,11 @@ function toggleRichtextGroups(id)
 		
 		hideRichtextEditGroupsBlock();
 		
-		if (hidden != 'block' && (el.style.display == 'none' || el.style.display == '')) el.style.display='block';
-			else el.style.display='none';
-		
+		if (hidden != 'block' && (el.style.display == 'none' || el.style.display == '')) {
+			el.style.display='block';
+		} else {
+			el.style.display='none';
+		}
 	}
 	
 	return false;
