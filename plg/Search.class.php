af36854daab8805c9e7c6ec09e663a9dfdd0d9e8
Yusein Yuseinov <yyuseinov@gmail.com>
2014-02-03 16:36:38 +0200

    plg_Search bugFix при клониране

--- plg/Search.class.php
+++ plg/Search.class.php
@@ -65,11 +65,14 @@ class plg_Search extends core_Plugin
      */
     static function getKeywords($mvc, $rec)
     {
-        $cRec = clone $rec;
         $searchKeywords = '';
         
         if (!empty($mvc->searchFields)) {
             $fieldsArr = $mvc->selectFields("", $mvc->searchFields);
+            
+            if (is_object($rec)) {
+                $cRec = clone $rec;
+            }
             
             foreach($fieldsArr as $field => $fieldObj) {
                 if(get_class($fieldObj->type) == 'type_Text') {
