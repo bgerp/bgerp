83b5b5ea828b1e53ad8e6c46f68ceb5b0e81d60f
Yusein Yuseinov <yyuseinov@gmail.com>
2014-02-17 17:40:17 +0200

    page_HTML - При извикване на страницата да се записва `hitTime` в JS. Показване на новите статуси.

--- page/Html.class.php
+++ page/Html.class.php
@@ -94,5 +94,19 @@ class page_Html extends core_ET {
                 $invoker->appendOnce("\n<script type=\"text/javascript\" src=\"{$file}\"></script>", "HEAD", TRUE);
             }
         }
+        
+        $date = dt::verbal2mysql();
+        
+        $hitTime = dt::mysql2timestamp($date);
+        
+        // Добавяме в JS timestamp на извикване на страницата
+        $invoker->append("var hitTime = {$hitTime};", 'SCRIPTS');
+        
+        // Ако има такава функция - това е във vendors
+        if (method_exists('status_Messages', 'show')) {
+            
+            // Извикваме показването на статусите
+            $invoker->append(status_Messages::show($hitTime), 'STATUSES');
+        }
     }
 }
