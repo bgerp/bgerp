d1e1a5f0ac6ab0018b0b8b44e6129d5938506aa5
Yusein Yuseinov <yyuseinov@gmail.com>
2014-02-20 17:24:40 +0200

    toast_Toast - JQuery трябва да се активира по рано

--- toast/Toast.class.php
+++ toast/Toast.class.php
@@ -42,6 +42,9 @@ class toast_Toast extends core_Plugin
         $conf = core_Packs::getConfig('toast');
         $version = $conf->TOAST_MESSAGE_VERSION;
         
+        // Активираме JQuery
+        jquery_Jquery::enable($tpl);
+        
         //Добавяме JS и CSS необходими за работа на статусите
         $tpl->push("toast/{$version}/javascript/jquery.toastmessage.js", 'JS');
         $tpl->push("toast/{$version}/resources/css/jquery.toastmessage.css", 'CSS');
@@ -65,7 +68,6 @@ class toast_Toast extends core_Plugin
         $toastJS= static::getStatusesJS($hitTime);
         
         // Стартираме в JQuery
-        jquery_Jquery::enable($tpl);
         jquery_Jquery::run($tpl, $toastJS);
         
         // Ако е зададено да се абонира
