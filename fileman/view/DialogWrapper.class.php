6603d2c86049fbc1c9cba5a3e996a486232d0ac7
Yusein Yuseinov <yyuseinov@gmail.com>
2014-02-21 16:24:13 +0200

    fileman_view_DialogWrapper - bugFix да е като другите `page_` файлове

--- fileman/view/DialogWrapper.class.php
+++ fileman/view/DialogWrapper.class.php
@@ -3,31 +3,26 @@
 /**
  * Клас 'fileman_view_DialogWrapper' -
  *
- *
  * @category  vendors
  * @package   fileman
- * @author    Milen Georgiev <milen@download.bg>
- * @copyright 2006 - 2012 Experta OOD
+ * @author    Milen Georgiev <milen@download.bg> и Yusein Yuseinov <yyuseinov@gmail.com>
+ * @copyright 2006 - 2014 Experta OOD
  * @license   GPL 3
  * @since     v 0.1
- * @todo:     Да се документира този клас
  */
-class fileman_view_DialogWrapper extends page_Html {
+class fileman_view_DialogWrapper extends page_Html
+{
     
     
-    /**
-     * Изпраща към изхода съдържанието, като преди това го опакова
+	/**
+     * Конструктор
      */
-    function output($content = '', $place = 'CONTENT')
+    function fileman_view_DialogWrapper()
     {
+        $this->page_Html();
         $this->replace("UTF-8", 'ENCODING');
-        
-        $this->append("<link rel=\"stylesheet\" type=\"text/css\" href=" . sbf("fileman/css/default.css") . "/>\n", "HEAD");
-        
-        $this->append("<link rel=\"stylesheet\" type=\"text/css\" href=" . sbf("css/common.css") . "/>\n", "HEAD");
-        
-        $this->append("<script type=\"text/javascript\" src=" . sbf("js/efCommon.js") . "></script>\n", "HEAD");
-        
-        parent::output($content, 'PAGE_CONTENT');
+        $this->push('fileman/css/default.css', 'CSS');
+        $this->push('css/common.css', 'CSS');
+        $this->push('js/efCommon.js', 'JS');
     }
 }
