d91aa396fbad1788de8532f528965002ea021c26
ivelin1012 <ivelin_pdimov@abv.bg>
2014-03-05 18:20:17 +0200

    eshop_Products: генериране на запитване

--- eshop/Products.class.php
+++ eshop/Products.class.php
@@ -146,8 +146,7 @@ class eshop_Products extends core_Master
         $this->FLD('coParams', 'text(rows=5)', 'caption=Запитване->Параметри,width=100%');
         $this->FLD('coMoq', 'varchar', 'caption=Запитване->МКП,hint=Минимално количество за поръчка');
 
-
-        $this->setDbUnique('code');
+		$this->setDbUnique('code');
     }
 
 
@@ -175,8 +174,13 @@ class eshop_Products extends core_Master
         }
 
         if($rec->coDriver) {
-            $title = tr('Изпратете запитване за производство');
-            $row->coInquiry   = ht::createLink(tr('Запитване'), array(cls::get($rec->coDriver), 'Inquiry', $id), "Все още не работи...", "ef_icon=img/16/button-question-icon.png,title={$title}");
+            if(sales_Inquiries::haveRightFor('add')){
+            	
+            	$title = tr('Изпратете запитване за производство');
+            	Request::setProtected('drvId,coParams');
+            	
+            	$row->coInquiry = ht::createBtn('Запитване', array('sales_Inquiries', 'new', 'drvId' => $rec->coDriver, 'coParams' => $rec->coParams, 'ret_url' => TRUE), NULL, NULL, "ef_icon=img/16/button-question-icon.png,title={$title}");
+            }
         }
 
         if($fields['-list']) {
