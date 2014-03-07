7d8b6af9a0e15f93cd03a99527e0144414cdc50b
ivelin1012 <ivelin_pdimov@abv.bg>
2014-03-07 13:05:08 +0200

    Компании и Лица: полето name  е silent

--- crm/Persons.class.php
+++ crm/Persons.class.php
@@ -199,7 +199,7 @@ class crm_Persons extends core_Master
     {
         // Име на лицето
         $this->FLD('salutation', 'enum(,mr=Г-н,mrs=Г-жа,miss=Г-ца)', 'caption=Обръщение');
-        $this->FLD('name', 'varchar(255)', 'caption=Имена,class=contactData,mandatory,remember=info');
+        $this->FLD('name', 'varchar(255)', 'caption=Имена,class=contactData,mandatory,remember=info,silent');
         $this->FNC('nameList', 'varchar', 'sortingLike=name');
 
         // Единен Граждански Номер
