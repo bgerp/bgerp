594741d8c6a1554e8187e445b6f964f50d3f8eb7
Yusein Yuseinov <yyuseinov@gmail.com>
2014-03-07 17:13:11 +0200

    plg_RowTools - възможност и други да могат да добавят в `rowTools`. Полето винаги да е по един и същи начин - дивове

--- plg/RowTools.class.php
+++ plg/RowTools.class.php
@@ -106,17 +106,18 @@ class plg_RowTools extends core_Plugin
         		}
         	}
         }
-                
+        
+        $tpl = new ET("<div class='rowtools'><div class='l nw'>[#TOOLS#]</div><div class='r'>[#ROWTOOLS_CAPTION#]</div></div>");
+        $tpl->append($row->{$field}, 'ROWTOOLS_CAPTION');
+        
         if ($singleLink || $editLink || $deleteLink || $restoreLink) {
             // Вземаме съдържанието на полето, като шаблон
-            $tpl = new ET("<div class='rowtools'><div class='l nw'>[#TOOLS#]</div><div class='r'>[#ROWTOOLS_CAPTION#]</div></div>");
-            $tpl->append($row->{$field}, 'ROWTOOLS_CAPTION');
             $tpl->append($singleLink, 'TOOLS');
             $tpl->append($editLink, 'TOOLS');
             $tpl->append($deleteLink, 'TOOLS');
             $tpl->append($restoreLink, 'TOOLS');
-            $row->{$field} = $tpl;
         }
+        $row->{$field} = $tpl;
     }
     
     
