e845334cd8dd012e8f98ba2f36e155c50a818555
Milen Georgiev <milen@experta.bg>
2014-03-16 17:07:03 +0200

    vislog/IpNames.class.php

---
+++ vislog/IpNames.class.php
@@ -0,0 +1,79 @@
+<?php
+
+
+
+/**
+ * Клас 'vislog_IpNames' -
+ *
+ *
+ * @category  bgerp
+ * @package   vislog
+ * @author    Milen Georgiev <milen@download.bg>
+ * @copyright 2006 - 2014 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ * @todo:     Да се документира този клас
+ */
+class vislog_IpNames extends core_Manager {
+    
+    
+    /**
+     * @todo Чака за документация...
+     */
+    var $pageMenu = 'Система';
+    
+    
+    /**
+     * Заглавие
+     */
+    var $title = 'Search Log Ip-s';
+    
+    
+    /**
+     * Брой записи на страница
+     */
+    var $listItemsPerPage = 20;
+    
+    
+    /**
+     * Плъгини за зареждане
+     */
+    var $loadList = "vislog_Wrapper";
+    
+    
+    /**
+     * Кой  може да пише?
+     */
+    var $canWrite = "cms,admin,ceo";
+    
+    /**
+     * Кой може да чете?
+     */
+    var $canRead = 'cms, ceo, admin';
+    
+    
+    /**
+	 * Кой може да го разглежда?
+	 */
+	var $canList = 'ceo, admin, cms';
+
+
+	/**
+	 * Кой може да разглежда сингъла на документите?
+	 */
+	var $canSingle = 'ceo, admin, cms';
+
+    
+    /**
+     * Описание на модела (таблицата)
+     */
+    function description()
+    {
+        
+        $this->FLD('ip', 'varchar(15)', 'caption=IP');
+        $this->FLD('name', 'varchar(15)', 'caption=Име');
+        
+        $this->setDbUnique('ip');
+        $this->setDbUnique('name');
+    }
+}
