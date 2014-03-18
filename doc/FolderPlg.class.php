29d6d06eec01a7a214639f8ad8d75f1d16b836f2
ivelin1012 <ivelin_pdimov@abv.bg>
2014-03-18 10:51:07 +0200

    doc_FolderPlg: ф-я getDefaultInCharge връщаща първия админситратор, ceo за отговорник на папката

--- doc/FolderPlg.class.php
+++ doc/FolderPlg.class.php
@@ -300,8 +300,40 @@ class doc_FolderPlg extends core_Plugin
         // Подсигуряване да не се създава корица с отговорник @system или @anonym
         // в такъв случай отговорника става първия регистриран потребител в системата
         if(!$rec->inCharge || $rec->inCharge == -1){
-        	$rec->inCharge = core_Users::fetchField("#state = 'active'");
+        	
+        	// Ако няма отговорник, това става или първия admin или първия ceo
+        	// Така избягваме възможността, отговорника да е @system или @anonym
+        	$rec->inCharge = self::getDefaultInCharge();
         }
+    }
+    
+    
+    /**
+     * Връща дефолт потребителя, който може да е отговорник на папка, това е или първия
+     * администратор или първия ceo регистриран в системата
+     */
+    public static function getDefaultInCharge()
+    {
+    	// Ид на ролята "admin"
+    	$adminRoleId = core_Roles::fetchByName('admin');
+    	
+    	// Извличане на първия активен потребител с роля 'admin'
+        $query = core_Users::getQuery();
+        $query->where("#state = 'active'");
+        $query->orderBy('createdOn', 'ASC');
+        	
+        $query2 = clone $query;
+        $query->like("roles", "|{$adminRoleId}|");
+        	
+        // Ако няма такъв администратор, намираме първия 'ceo'
+        if(!$userRec = $query->fetch()){
+        	$ceoId = core_Roles::fetchByName('ceo');
+        	$query2->like("roles", "|{$ceoId}|");
+        	$userRec = $query2->fetch();
+        }
+        
+        // Връщаме ид-то на намерения потребител
+        return $userRec->id;
     }
     
     
