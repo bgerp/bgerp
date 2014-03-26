6c5eedb2e26e3b759af07cc5548947bb227eb33b
ivelin1012 <ivelin_pdimov@abv.bg>
2014-03-26 13:12:04 +0200

    cat_Products, crm_Companies & crm_Persons: от редакцията на формата неможе да се добавят/премахват в номенклатури

--- cat/Products.class.php
+++ cat/Products.class.php
@@ -221,6 +221,11 @@ class cat_Products extends core_Master {
                 }
             }
         }
+        
+    	// Неможе да се променят номенклатурите от формата
+    	if($data->form->fields['lists']){
+        	$data->form->setField('lists', 'input=none');
+        }
     }
     
     
