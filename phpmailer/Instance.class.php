e0c14afbddf53eadfbc04a8a7ccf9c38b07c449d
GPetrova <gab4eto@gmail.com>
2014-03-28 10:44:56 +0200

    Клас Setup за phpmailer
    Change-Id: Ib9849692f241dd32dadb35d31cbb6be6a85a894b

--- phpmailer/Instance.class.php
+++ phpmailer/Instance.class.php
@@ -1,64 +1,5 @@
 <?php
 
-$conf = core_Packs::getConfig('core');
-
-/**
- * Подразбиращата се кодировка на съобщенията
- */
-defIfNot('PML_CHARSET', 'utf-8');
-
-
-/**
- * Ник-а на изпращача по подразбиране
- */
-defIfNot('PML_DEF_NICK', 'support');
-
-
-/**
- * Адреса във 'From' хедър-а на съобщението
- */
-defIfNot('PML_FROM_EMAIL', PML_DEF_NICK . '@' . $_SERVER['SERVER_NAME']);
-
-
-/**
- * Името във 'From' хедър-а на съобщението
- */
-defIfNot('PML_FROM_NAME', $conf->EF_APP_TITLE . ' ' .
-    mb_convert_case(PML_DEF_NICK, MB_CASE_TITLE, PML_CHARSET));
-
-
-/**
- * Адреса на изпращача (Return-Path) на съобщението
- */
-defIfNot('PML_SENDER', PML_FROM_EMAIL);
-
-
-/**
- * Какъв да е метода за изпращане на писма?
- * ("mail", "sendmail", or "smtp")
- */
-defIfNot('PML_MAILER', 'sendmail');
-
-
-/**
- * Къде се намира Sendmail?
- */
-defIfNot('SENDMAIL_PATH', '/usr/sbin/sendmail');
-
-
-/**
- * Дефинираме пътя до кода на PHP_Mailer
- */
-defIfNot('PML_CLASS', '5.2/class.phpmailer.php');
-
-
-/**
- * Да изпраща ли по единично писмата от адресите в 'To:'
- */
-defIfNot('PML_SINGLE_TO', FALSE);
-
-// Зареждаме кода на на PHP_Mailer
-require_once(PML_CLASS);
 
 
 /**
@@ -81,24 +22,26 @@ class phpmailer_Instance extends core_BaseClass
      */
     function init($params = array())
     {
+    	$conf = core_Packs::getConfig('phpmailer');
+    	 
         // Създаваме инстанция на PHPMailerLite
         $PML = new PHPMailer();
         
         // Задаваме стойностите от конфигурацията
-        $PML->Mailer    = PML_MAILER;
-        $PML->CharSet   = PML_CHARSET;
+        $PML->Mailer    = $conf->PML_MAILER;
+        $PML->CharSet   = $conf->PML_CHARSET;
         
         //      $PML->Encoding  = PML_ENCODING;
-        $PML->From      = PML_FROM_EMAIL;
-        $PML->FromName  = PML_FROM_NAME;
-        $PML->Sendmail  = SENDMAIL_PATH;
-        $PML->SingleTo  = PML_SINGLE_TO;
-        $PML->Host      = PML_HOST;
-        $PML->Port      = PML_PORT;
-        $PML->SMTPAuth  = PML_SMTPAUTH;
-        $PML->SMTPSecure = PML_SMTPSECURE;
-        $PML->Username  = PML_USERNAME;
-        $PML->Password  = PML_PASSWORD;
+        $PML->From      = $conf->PML_FROM_EMAIL;
+        $PML->FromName  = $conf->PML_FROM_NAME;
+        $PML->Sendmail  = $conf->SENDMAIL_PATH;
+        $PML->SingleTo  = $conf->PML_SINGLE_TO;
+        $PML->Host      = $conf->PML_HOST;
+        $PML->Port      = $conf->PML_PORT;
+        $PML->SMTPAuth  = $conf->PML_SMTPAUTH;
+        $PML->SMTPSecure = $conf->PML_SMTPSECURE;
+        $PML->Username  = $conf->PML_USERNAME;
+        $PML->Password  = $conf->PML_PASSWORD;
         
         // Добавяме динамичните параметри, които могат да 
         // "препокрият" зададените конфигурационни стойности
---
+++ phpmailer/Setup.class.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ * Подразбиращата се кодировка на съобщенията
+ */
+defIfNot('PML_CHARSET', 'utf-8');
+
+
+/**
+ * Ник-а на изпращача по подразбиране
+ */
+defIfNot('PML_DEF_NICK', 'support');
+
+
+/**
+ * Адреса във 'From' хедър-а на съобщението
+ */
+defIfNot('PML_FROM_EMAIL', PML_DEF_NICK . '@' . $_SERVER['SERVER_NAME']);
+
+
+/**
+ * Името във 'From' хедър-а на съобщението
+ */
+defIfNot('PML_FROM_NAME', $conf->EF_APP_TITLE . ' ' .
+    mb_convert_case(PML_DEF_NICK, MB_CASE_TITLE, PML_CHARSET));
+
+
+/**
+ * Адреса на изпращача (Return-Path) на съобщението
+ */
+defIfNot('PML_SENDER', PML_FROM_EMAIL);
+
+
+/**
+ * Какъв да е метода за изпращане на писма?
+ * ("mail", "sendmail", or "smtp")
+ */
+defIfNot('PML_MAILER', 'sendmail');
+
+
+/**
+ * Къде се намира Sendmail?
+ */
+defIfNot('SENDMAIL_PATH', '/usr/sbin/sendmail');
+
+
+/**
+ * Дефинираме пътя до кода на PHP_Mailer
+ */
+defIfNot('PML_CLASS', '5.2/class.phpmailer.php');
+
+
+/**
+ * Да изпраща ли по единично писмата от адресите в 'To:'
+ */
+defIfNot('PML_SINGLE_TO', 'FALSE');
+
+
+/**
+ * Хоста за SMTP
+ */
+defIfNot('PML_HOST', $_SERVER['SERVER_NAME']);
+
+
+/**
+ * Порт за SMTP
+ */
+defIfNot('PML_PORT', 25);
+
+
+// Зареждаме кода на на PHP_Mailer
+require_once(PML_CLASS);
+
+/**
+ * class phpmailer_Setup
+ *
+ * Инсталиране/Деинсталиране на
+ * доктор за адресни данни
+ *
+ *
+ * @category  vendors
+ * @package   phpmailer
+ * @author    Milen Georgiev <milen@download.bg>
+ * @copyright 2006 - 2012 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class phpmailer_Setup extends core_ProtoSetup
+{
+    
+    
+    /**
+     * Версия на пакета
+     */
+    var $version = '0.1';
+    
+    
+    /**
+     * Мениджър - входна точка в пакета
+     */
+    var $startCtr = 'phpmailer_Instance';
+    
+    
+    /**
+     * Екшън - входна точка в пакета
+     */
+    var $startAct = 'default';
+    
+    
+    /**
+     * Описание на модула
+     */
+    var $info = "Aгрегиране на PHP Mailer Lite";
+    
+    
+    /**
+     * Описание на конфигурационните константи
+     */
+    var $configDescription = array(
+            'PML_CHARSET'   => array ('varchar', 'mandatory, caption=Писмо->Kодировка на съобщенията'),
+            'PML_FROM_EMAIL'   => array ('email', 'mandatory, caption=Писмо->Адреса във `От` хедър-а на съобщението'),
+            'PML_FROM_NAME'  => array ('varchar', 'mandatory, caption=Писмо->Името във `От` хедър-а на съобщението'),
+    		'PML_SINGLE_TO' => array('enum(TRUE=да, FALSE=не)', 'mandatory, caption=Писмо->Ще се изпраща ли единично писмата от адресите в `До`,row=2'),
+            'PML_MAILER' => array ('enum(mail=mail, sendmail=sendmail, smtp=smtp)', 'mandatory, caption=Писмо->Какъв да е метода за изпращане на писма?'), //"mail", "sendmail", or "smtp") 
+            'SENDMAIL_PATH'  => array ('varchar', 'caption=Sendmail->Пътя до папката на Sendmail'),
+            'PML_HOST'  => array ('varchar', 'caption=Smtp->Хост'),
+       		'PML_PORT'  => array ('int', 'caption=Smtp->Порт'),
+    		'PML_SMTPAUTH'  => array ('enum(TRUE=да, FALSE=не)', 'caption=Smtp->Оторизация'),
+    		'PML_USERNAME'  => array ('varchar', 'caption=Smtp->Потребител'),
+    		'PML_PASSWORD'  => array ('varchar', 'caption=Smtp->Парола'),
+    		'PML_SMTPSECURE'  => array ('enum(tls=TLS, ssl=SSL, 0=няма)', 'caption=Smtp->Криптографски протокол'),
+            
+        );
+
+        
+    /**
+     * Списък с мениджърите, които съдържа пакета
+     */
+    /*var $managers = array(
+            'phpmailer_Instance',
+            
+        );*/
+    
+    
+    /**
+     * Де-инсталиране на пакета
+     */
+    function deinstall()
+    {
+         // Изтриване на пакета от менюто
+        $res .= bgerp_Menu::remove($this);
+        
+        return $res;
+    }
+}
