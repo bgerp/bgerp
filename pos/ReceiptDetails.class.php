76f6744d1ab39ba303cfba646a39b42f8eabccc6
HeBuTy <vencho_27@abv.bg>
2014-04-29 16:09:28 +0300

    сменена картинка

--- pos/ReceiptDetails.class.php
+++ pos/ReceiptDetails.class.php
@@ -451,7 +451,7 @@ class pos_ReceiptDetails extends core_Detail {
     	
     	if($mvc->haveRightFor('delete', $rec)){
     		$delUrl = toUrl(array($mvc->className, 'deleteRec'), 'local');
-    		$row->DEL_BTN = ht::createElement('img', array('src' => sbf('img/16/delete.png', ''), 
+    		$row->DEL_BTN = ht::createElement('img', array('src' => sbf('img/16/deletered.png', ''), 
     													   'class' => 'pos-del-btn', 'data-recId' => $rec->id, 
     													   'data-warning' => tr('Наистина ли искате да изтриете записа?'), 
     													   'data-url' => $delUrl));
