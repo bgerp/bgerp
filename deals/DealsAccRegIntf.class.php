7ec65547507bc27419138a20312007e2b82c4b1d
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-05-10 17:18:35 +0300

    Интерфейс за номенклатура Финансови сделки

---
+++ deals/DealsAccRegIntf.class.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+/**
+ * Интерфейс за пера - Финансови сделки
+ *
+ *
+ * @category  bgerp
+ * @package   deals
+ * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
+ * @copyright 2006 - 2014 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ * @title     Интерфейс за дълготрайни активи
+ */
+class deals_DealsAccRegIntf extends acc_RegisterIntf
+{
+}
