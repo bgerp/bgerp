2e60203d502105a951ebbba7bf7c55f6e1d62554
Yusein Yuseinov <yyuseinov@gmail.com>
2014-05-13 11:30:05 +0300

    #C3669 type_Varchar revert на commit

--- type/Varchar.class.php
+++ type/Varchar.class.php
@@ -83,21 +83,4 @@ class type_Varchar extends core_Type {
 
         return $value;
     }
-    
-    
-    /**
-     * В plain режим декодва HTML ентитлите
-     * 
-     * @see core_Type::toVerbal_()
-     */
-    function toVerbal_($value)
-    {
-        $value = parent::toVerbal_($value);
-        
-        if (Mode::is('text', 'plain')) {
-            $value = html_entity_decode($value, ENT_QUOTES, 'UTF-8');
-        }
-        
-        return $value;
-    }
 }
