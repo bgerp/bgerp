4427e9dcd594ace1fa1325c7953c6674f30ad208
Yusein Yuseinov <yyuseinov@gmail.com>
2014-06-05 17:55:41 +0300

    #Tsk229 Модел `core_LoginLog` за логово за логвания

---
+++ core/LoginLog.class.php
@@ -0,0 +1,150 @@
+<?php
+
+
+/**
+ * 
+ *
+ * @category  ef
+ * @package   core
+ * @author    Yusein Yuseinov <yyuseinov@gmail.com>
+ * @copyright 2006 - 2014 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class core_LoginLog extends core_Manager
+{
+    
+    
+    /**
+     * Заглавие на таблицата
+     */
+    var $title = "Логин лог на потребителите";
+    
+    
+    /**
+     * 
+     */
+    var $canSingle = 'admin';
+    
+    
+    /**
+     * Кой има право да чете?
+     */
+    var $canRead = 'admin';
+    
+    
+    /**
+     * Кой има право да променя?
+     */
+    var $canEdit = 'no_one';
+    
+    
+    /**
+     * Кой има право да добавя?
+     */
+    var $canAdd = 'no_one';
+    
+    
+    /**
+     * Кой има право да го види?
+     */
+    var $canView = 'admin';
+    
+    
+    /**
+     * Кой може да го разглежда?
+     */
+    var $canList = 'admin';
+    
+    
+    /**
+     * Необходими роли за оттегляне на документа
+     */
+    var $canReject = 'no_one';
+    
+    
+    /**
+     * Кой има право да го изтрие?
+     */
+    var $canDelete = 'no_one';
+    
+    
+    /**
+     * Плъгини за зареждане
+     */
+    var $loadList = 'plg_SystemWrapper, plg_Created';
+    
+    
+    /**
+     * 
+     */
+    var $listFields = 'userId, status, ip, brid, time';
+    
+    
+    /**
+     * Описание на модела
+     */
+    function description()
+    {
+        $this->FLD('userId', 'user(select=nick)', 'caption=Потребител');
+        $this->FLD('ip', 'ip', 'caption=IP');
+        $this->FLD('brid', 'varchar', 'caption=BRID');
+        $this->FLD('status', 'enum(success, error, block, reject, draft, missing_password, wrong_password, pass_ret, pass_chg, user_reg)', 'caption=Статус');
+        $this->FLD('time', 'datetime()', 'caption=Време, input=none');
+    }
+    
+    
+    /**
+     * Записва в лога опитите за логване
+     * 
+     * @param integer $userId
+     * @param string $status
+     * @param time $time
+     */
+    static function add($userId, $status, $time=NULL)
+    {
+        $rec = new stdClass();
+        $rec->userId = $userId;
+        $rec->ip = core_Users::getRealIpAddr();
+        $rec->status = $status;
+        $rec->brid = core_Browser::getBrid();
+        
+        if ($time) {
+            $rec->time = dt::timestamp2Mysql($time);
+        }
+        
+        static::save($rec);
+        
+        return $rec->id;
+    }
+    
+    
+    /**
+     * 
+     * 
+     * @param core_LoginLog $mvc
+     * @param object $row
+     * @param object $rec
+     * @param array $fields
+     */
+    public static function on_AfterRecToVerbal($mvc, &$row, $rec, $fields = array())
+    {
+        if ($rec->ip){
+    	    $row->ip = vislog_History::decorateIp($rec->ip, $rec->createdOn);
+    	}
+    }
+    
+    
+    /**
+     * 
+     *
+     * @param core_Mvc $mvc
+     * @param StdClass $res
+     * @param StdClass $data
+     */
+    static function on_AfterPrepareListFilter($mvc, &$data)
+    {
+        // Сортиране на записите по num
+        $data->query->orderBy('createdOn', 'DESC');
+    }
+}
