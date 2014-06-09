6c10781e4364ab914d28f018cf8635b5cffa066e
Yusein Yuseinov <yyuseinov@gmail.com>
2014-06-09 13:39:04 +0300

    type_Richtext - Сгрешена променлива

--- type/Richtext.class.php
+++ type/Richtext.class.php
@@ -320,7 +320,7 @@ class type_Richtext extends type_Blob
             $html = str_replace(array('<b></b>', '<i></i>', '<u></u>'), array('', '', ''), $out);
         }
     
-        $html = str_replace('<nbsp>', '&nbsp;', $out);
+        $html = str_replace('<nbsp>', '&nbsp;', $html);
 
         if(!Mode::is('text', 'plain')) {
             $html =  new ET("<div class=\"richtext\">{$html}</div>");
