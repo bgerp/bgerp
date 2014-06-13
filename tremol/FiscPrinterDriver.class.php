4702f519fbaf1d26299a9ab2ee93e2d110ce6f59
mitko <mitko@extrapack.com>
2014-06-13 17:51:38 +0300

    Добавен драйвера за ФП + скрипта за печатане

--- tremol/FiscPrinterDriver.class.php
+++ tremol/FiscPrinterDriver.class.php
@@ -8,7 +8,7 @@
  * @copyright 2006 - 2014 Experta OOD
  * @license   GPL 3
  * @since     v 0.1
- * 
+ *
  * @see pos_FiscalPrinterIntf
  *
  */
@@ -31,16 +31,16 @@ class tremol_FiscPrinterDriver extends core_Manager {
      * Шаблона на бележката
      */
     private static $tpl = '<?xml version="1.0" encoding="utf-8" ?>
-    					   <TremolFpServer Command="Receipt" Description="*** Описание в дневникa ***">
-	    					   [#ITEMS#]
-		    					   <!--ET_BEGIN ITEM-->
-		    	  						[#ITEM#]<Item Description=\'[#name#]\' Price="[#price#]" Quantity="[#quantity#]" VatInfo="2" UnitName="[#measureId#]" <!--ET_BEGIN discount-->Discount="[#discount#]"<!--ET_END discount--> />
-		    					   <!--ET_END ITEM-->
-	    					   [#PAYMENTS#]
-	    					   <!--ET_BEGIN PAY-->
-	    						 	[#PAY#]<Payment Type="[#type#]" Amount="[#amount#]" />
-	    					   <!--ET_END PAY-->
-    					   </TremolFpServer>';
+<TremolFpServer Command="Receipt" Description="*** Описание в дневникa ***">
+                                [#ITEMS#]
+                                <!--ET_BEGIN ITEM-->
+                                    [#ITEM#]<Item Description=\'[#name#]\' Price="[#price#]" Quantity="[#quantity#]" VatInfo="2" UnitName="[#measureId#]" <!--ET_BEGIN discount-->Discount="[#discount#]"<!--ET_END discount--> />
+                                <!--ET_END ITEM-->
+                                [#PAYMENTS#]
+                                <!--ET_BEGIN PAY-->
+                                    [#PAY#]<Payment Type="[#type#]" Amount="[#amount#]" />
+                                <!--ET_END PAY-->
+</TremolFpServer>';
     
     
    /*
@@ -56,54 +56,54 @@ class tremol_FiscPrinterDriver extends core_Manager {
      */
     private function makeFileContent($id)
     {
-    	// Взимаме шаблона
-    	$contentTpl = new ET(static::$tpl);
-    	
-    	// Извличаме детайлите на бележката
-    	$payments = $products = array();
-    	$query = pos_ReceiptDetails::getQuery();
-    	$query->where("#receiptId = '{$id}'");
-    	
-    	// Разделяме детайлите на плащания и продажби
-    	while($rec = $query->fetch()){
-    		if(strpos($rec->action, 'sale') !== false){
-    			$products[] = $rec;
-    		} elseif(strpos($rec->action, 'payment') !== false) {
-    			$payments[] = $rec;
-    		}
-    	}
-    	
-    	// Добавяме към шаблона всеки един продаден продукт
-    	$itemBlock = $contentTpl->getBlock('ITEM');
-    	foreach ($products as $p){
-    		$block = clone $itemBlock;
-    		$block->placeObject($this->getRow($p, 'sale'));
-    		$block->removeBlocks();
-    		$contentTpl->append($block, 'ITEMS');
-    	}
-    	
-    	/*$newPayments = array();
-    	foreach ($payments as $p){
-    		list(, $type) = explode('|', $p->action);
-    		$code = pos_Payments::fetchField($type, 'code');
-    		if(empty($newPayments[$code])){
-    			$newPayments[$code] = (object)array('code' => $code);
-    		}
-    		$newPayments[$code]->amount += $p->amount;
-    	}
-    	bp($newPayments,$payments);*/
-    	
-    	// Добавяме към шаблона направените плащания
-    	$itemBlock = $contentTpl->getBlock('PAY');
-    	foreach ($payments as $p){
-    		$block = clone $itemBlock;
-    		$block->placeObject($this->getRow($p, 'payment'));
-    		$block->removeBlocks();
-    		$contentTpl->append($block, 'PAYMENTS');
-    	}
-    	
-    	// Връщаме чистото съдържание
-    	return $contentTpl->getContent();
+        // Взимаме шаблона
+        $contentTpl = new ET(static::$tpl);
+        
+        // Извличаме детайлите на бележката
+        $payments = $products = array();
+        $query = pos_ReceiptDetails::getQuery();
+        $query->where("#receiptId = '{$id}'");
+        
+        // Разделяме детайлите на плащания и продажби
+        while($rec = $query->fetch()){
+            if(strpos($rec->action, 'sale') !== false){
+                $products[] = $rec;
+            } elseif(strpos($rec->action, 'payment') !== false) {
+                $payments[] = $rec;
+            }
+        }
+        
+        // Добавяме към шаблона всеки един продаден продукт
+        $itemBlock = $contentTpl->getBlock('ITEM');
+        foreach ($products as $p){
+            $block = clone $itemBlock;
+            $block->placeObject($this->getRow($p, 'sale'));
+            $block->removeBlocks();
+            $contentTpl->append($block, 'ITEMS');
+        }
+        
+        /*$newPayments = array();
+        foreach ($payments as $p){
+            list(, $type) = explode('|', $p->action);
+            $code = pos_Payments::fetchField($type, 'code');
+            if(empty($newPayments[$code])){
+                $newPayments[$code] = (object)array('code' => $code);
+            }
+            $newPayments[$code]->amount += $p->amount;
+        }
+        bp($newPayments,$payments);*/
+        
+        // Добавяме към шаблона направените плащания
+        $itemBlock = $contentTpl->getBlock('PAY');
+        foreach ($payments as $p){
+            $block = clone $itemBlock;
+            $block->placeObject($this->getRow($p, 'payment'));
+            $block->removeBlocks();
+            $contentTpl->append($block, 'PAYMENTS');
+        }
+        
+        // Връщаме чистото съдържание
+        return $contentTpl->getContent();
     }
     
     
@@ -116,26 +116,26 @@ class tremol_FiscPrinterDriver extends core_Manager {
      */
     private function getRow($rec, $type)
     {
-    	$row = new stdClass();
-    	
-    	if($type == 'sale'){
-    		$row->price = round($rec->price * (1 + $rec->param), 2);
-    		if($rec->discountPercent){
-    			$row->discount = (round($rec->discountPercent, 2) * 100) . "%";
-    		}
-    		$pRec = cat_Products::fetch($rec->productId);
-    		$row->name = cat_Products::getVerbal($pRec, 'name');
-    		
-    		$row->quantity = $rec->quantity;
-    		$row->measureId = ($rec->value) ? cat_Packagings::getTitleById($rec->value) : cat_UoM::getShortName($pRec->measureId);
-    		
-    	} elseif($type == 'payment'){
-    		list(, $type) = explode('|', $rec->action);
-    		$row->type = pos_Payments::fetchField($type, 'code');
-    		$row->amount = round($rec->amount, 2);
-    	}
-    	
-    	return $row;
+        $row = new stdClass();
+        
+        if($type == 'sale'){
+            $row->price = round($rec->price * (1 + $rec->param), 2);
+            if($rec->discountPercent){
+                $row->discount = (round($rec->discountPercent, 2) * 100) . "%";
+            }
+            $pRec = cat_Products::fetch($rec->productId);
+            $row->name = cat_Products::getVerbal($pRec, 'name');
+            
+            $row->quantity = $rec->quantity;
+            $row->measureId = ($rec->value) ? cat_Packagings::getTitleById($rec->value) : cat_UoM::getShortName($pRec->measureId);
+            
+        } elseif($type == 'payment'){
+            list(, $type) = explode('|', $rec->action);
+            $row->type = pos_Payments::fetchField($type, 'code');
+            $row->amount = round($rec->amount, 2);
+        }
+        
+        return $row;
     }
     
     
@@ -147,22 +147,22 @@ class tremol_FiscPrinterDriver extends core_Manager {
      */
     public function createFile($id)
     {
-    	// Създаваме съдържанието на файла
-    	$content = $this->makeFileContent($id);
-    	
-    	// Задаваме нужните хедъри за форсиране на изтегляне от браузъра
-    	header('Content-Description: File Transfer');
-	    header('Content-Type: application/xhtml+xml');
-	    header("Content-Disposition: attachment; filename=receipt{$id}.xml");
-	    header('Expires: 0');
-	    header('Cache-Control: must-revalidate');
-	    header('Pragma: public');
-	    
-	    // Аутпут на съдържанието
-	    echo $content;
-	    
-	    // Сприраме изпълнението на скрипта
-	    shutdown();
+        // Създаваме съдържанието на файла
+        $content = $this->makeFileContent($id);
+        
+        // Задаваме нужните хедъри за форсиране на изтегляне от браузъра
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/xhtml+xml');
+        header("Content-Disposition: attachment; filename=receipt{$id}.xml");
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        
+        // Аутпут на съдържанието
+        echo $content;
+        
+        // Сприраме изпълнението на скрипта
+        shutdown();
     }
     
     
@@ -170,7 +170,7 @@ class tremol_FiscPrinterDriver extends core_Manager {
      * @TODO ТЕСТОВО
      */
     public function act_test(){
-    	$id = '158';
-    	$this->createFile($id);
+        $id = '158';
+        $this->createFile($id);
     }
 }
