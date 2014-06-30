2ece8e522ddb13d102b110f83d4b9155f5de0905
HeBuTy <vencho_27@abv.bg>
2014-06-30 11:16:32 +0300

    #Sig624: bugFix по адреса на получателя в оферта

--- sales/Quotations.class.php
+++ sales/Quotations.class.php
@@ -355,11 +355,10 @@ class sales_Quotations extends core_Master
 	    	$row->number = $mvc->getHandle($rec->id);
 			$row->username = core_Users::recToVerbal(core_Users::fetch($rec->createdBy), 'names')->names;
 			
+			$row->contragentAdress = trim(sprintf("%s, %s %s",$row->country, $row->pCode, $row->place)); 
 			if($row->address){
-				$row->contragentAdress = $row->address . ",";
+				$row->contragentAdress .= ", " . $row->address ;
 			}
-			$row->contragentAdress .= trim(sprintf(" <br />%s %s<br />%s",$row->pcode, $row->place, $row->country)); 
-			
 			if($rec->currencyRate == 1){
 				unset($row->currencyRate);
 			}
