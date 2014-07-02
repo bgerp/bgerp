d424339f79f993fe805769a06c2dcc5cc9cd02dc
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-07-02 10:51:51 +0300

    bgerp_iface_DealAggregator промени вече е по общ

--- bgerp/iface/DealAggregator.class.php
+++ bgerp/iface/DealAggregator.class.php
@@ -15,287 +15,62 @@
  */
 class bgerp_iface_DealAggregator
 {
-	const TYPE_PURCHASE = 'purchase';
-	const TYPE_SALE     = 'sale';
-	const TYPE_DEAL     = 'deal';
-	
-	
 	/**
-	 * Вид на сделката
+	 * Задава стойност на пропърти, ако няма
+	 * 
+	 * @param string $name - име на пропърти
+	 * @param string $value - стойност
+	 * @return void
 	 */
-	public $dealType;
-	
-	
-	/**
-	 * Договорената сума по сделката
-	 */
-	public $amount;
-	
-	
-	/**
-	 * Платено до момента
-	 */
-	public $amountPaid;
-	
-	
-	/**
-	 * Сума на доставеното
-	 */
-	public $deliveryAmount;
-	
-	
-	/**
-	 * Датата на сключване на договора
-	 */
-	public $agreedValior;
-	
-	
-	/**
-	 * Дата на първото експедиране
-	 */
-	public $shippedValior;
-	
-	
-	/**
-	 * Дата на първото фактуриране
-	 */
-	public $invoicedValior;
-	
-	
-	/**
-	 * Валута на сделката
-	 */
-	public $currency;
-	
-	
-	/**
-	 * Валутен курс на сделката
-	 */
-	public $rate;
-	
-	
-	/**
-	 * Тип ддс
-	 */
-	public $vatType;
-	
-	
-	/**
-	 * Направено авансово плащане в основната валута за периода
-	 */
-	public $downpayment;
-	
-	
-	/**
-	 * Очаквано авансово плащане в основната валута
-	 */
-	public $agreedDownpayment;
-	
-	
-	/**
-	 * Приспаднато ддс на авансово плащане
-	 */
-	public $downpaymentDeducted;
-	
-	
-	/**
-	 * Общата сума на фактурираното
-	 */
-	public $amountInvoiced;
-	
-	
-	/**
-	 * Условие на доставка
-	 */
-	public $deliveryTerm;
-	
-	
-	/**
-	 * Място на доставка
-	 */
-	public $deliveryLocation;
-	
-	
-	/**
-	 * Дата на доставка
-	 */
-	public $deliveryTime;
-	
-	
-	/**
-	 * Склад
-	 */
-	public $storeId;
-	
-	
-	/**
-	 * Наша банкова сметка
-	 */
-	public $bankAccountId;
-	
-	
-	/**
-	 * Каса
-	 */
-	public $caseId;
-	
-	
-	/**
-	 * Начин на плащане
-	 */
-	public $paymentMethodId;
-	
-	
-	/**
-	 * Позволени операции за плащане
-	 */
-	public $allowedPaymentOperations = array();
-	
-	
-	/**
-	 * Позволени операции за експедиране
-	 */
-	public $allowedShipmentOperations = array();
-	
-	
-	/**
-	 * Засегнати контрагенти
-	 */
-	public $involvedContragents = array();
-	
-	
-	/**
-	 * Договорени продукти от сделката
-	 */
-	public $products = array();
-	
-	
-	/**
-	 * Експедирани продукти
-	 */
-	public $shippedProducts = array();
-	
-	
-	/**
-	 * Магически метод, позволява да се извикват функции 'setIfNot' и 'get'
-	 */
-	public function __call($name, $args)
+	public function setIfNot($name, $value)
 	{
-		// Ако метода е 'setIfNot', задава веднъж стойност на пропърти от класа, ако няма
-		if($name == 'setIfNot'){
-			
-			// Трябва да са подадени точно два параметъра
-			expect(count($args) == 2);
-			
-			// В обекта трябва да го има зададеното пропърти
-			expect(property_exists($this, $args[0]));
-			expect(!is_array($this->$args[0]));
-			
-			// Ако няма стойност пропъртито, задаваме му първата стойност
-			if(empty($this->$args[0])){
-				$this->$args[0] = $args[1];
-			} 
+		// Ако няма стойност пропъртито, задаваме му първата стойност
+		if(empty($this->$args[0])){
+			$this->$name = $value;
+		}
+	}
+	
+	
+	/**
+	 * Задава стойност на пропърти
+	 * 
+	 * @param string $name - име на пропърти
+	 * @param string $value - стойност
+	 * @return void
+	 */
+	public function set($name, $value)
+	{
+		$this->$name = $value;
+	}
+	
+	
+	/**
+	 * Връща стойност на пропърти
+	 * 
+	 * @param string $name - име на пропърти
+	 * @return mixed
+	 */
+	public function get($name)
+	{
+		// Връщаме стойността на пропъртито
+		return $this->$name;
+	}
+	
+	
+	/**
+	 * Пушва стойност към масив
+	 * 
+	 * @param string $name - име на пропърти
+	 * @param mixed $array - масив, обект или скалар който да се добавя към масива
+	 */
+	public function push($name, $array)
+	{
+		// Ако няма такова пропърти, създаваме го
+		if(!isset($this->$name)){
+			$this->$name = array();
 		}
 		
-		// Ако метода е 'get'
-		if($name == 'get'){
-			
-			// Трябва да е подаден точно един параметър
-			expect(count($args) == 1);
-			
-			// Трябва да го има въпросното пропърти
-			expect(property_exists($this, $args[0]));
-			
-			// Връщаме стойността на пропъртито
-			return $this->$args[0];
-		}
-	}
-	
-	
-	/**
-	 * Задава позволените операции за експедиране
-	 */
-	public function setShippmentOperations($array)
-	{
-		expect(is_array($array));
-		$this->allowedShipmentOperations = $array;
-	}
-	
-	
-	/**
-	 * Задава контрагенти, замесени в сделката
-	 */
-	public function setContragents($array)
-	{
-		expect(is_array($array));
-		$this->involvedContragents = $array;
-	}
-	
-	
-	/**
-	 * Задава позволените операции за плащане
-	 */
-	public function setPayOperations($array)
-	{
-		expect(is_array($array));
-		$this->allowedPaymentOperations = $array;
-	}
-	
-	
-	/**
-	 * Добавя продукт към договорените
-	 */
-	public function addProduct(bgerp_iface_DealProduct $product)
-	{
-		$this->products[] = $product;
-	}
-	
-	
-	/**
-	 * Добавя експедиран продукт
-	 */
-	public function addShippedProduct(bgerp_iface_DealProduct $product)
-	{
-		$this->shippedProducts[] = $product;
-	}
-	
-	
-	/**
-	 * Връща продуктите оставащи за експедиране
-	 */
-	public function getRemainingToShip()
-	{
-		$remaining = $this->products;
-		if(count($this->shippedProducts)){
-			foreach ($this->shippedProducts as $p) {
-				$found = $this->findProduct($p->productId, $p->getClassId(), $p->packagingId);
-				if ($found) {
-					$found->quantity -= $p->quantity;
-				} else {
-					$q = $p->quantity;
-					$p = clone $p;
-					$p->quantity = -$q;
-					$remaining[] = $p;
-				}
-			}
-		}
-		
-		return $remaining;
-	}
-	
-	
-	/**
-	 * Намира договорен продукт по негови уникални данни
-	 */
-	public function findProduct($productId, $classId, $packagingId)
-	{
-		/* @var $p bgerp_iface_DealProduct */
-		foreach ($this->products as $i=>$p) {
-			if ($p->isIdentifiedBy($productId, $classId, $packagingId)) {
-				return $p;
-			}
-		}
-	
-		return NULL;
+		// Долепяме в края на масива
+		array_push($this->$name, $array);
 	}
 }
