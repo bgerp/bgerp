d6f9d6dd75f59d340097c0ab694f43731bc5b73c
HeBuTy <vencho_27@abv.bg>
2014-07-08 13:33:25 +0300

    BugFix: разтояние между отделните страници при показване на документ

--- fileman/webdrv/Generic.class.php
+++ fileman/webdrv/Generic.class.php
@@ -174,7 +174,7 @@ class fileman_webdrv_Generic extends core_Manager
             $thumbWidthAndHeightArr = static::getPreviewWidthAndHeight();
             
             // Атрибути на thumbnail изображението
-            $attr = array('baseName' => 'Preview', 'isAbsolute' => FALSE, 'qt' => '', 'class' => 'webdrv-preview', 'style' => 'margin: 0 auto; display: block;');
+            $attr = array('baseName' => 'Preview', 'isAbsolute' => FALSE, 'qt' => '', 'class' => 'webdrv-preview', 'style' => 'margin: 0 auto 5px auto; display: block;');
             
             // Background' а на preview' то
             $bgImg = sbf('fileman/img/Preview_background.jpg');
