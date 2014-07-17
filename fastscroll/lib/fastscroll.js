34c4c04ffcb151c951fb3dd9041f5b7ca68708d8
HeBuTy <vencho_27@abv.bg>
2014-07-17 15:52:59 +0300

    fastscroll: js и css

---
+++ fastscroll/lib/fastscroll.js
@@ -0,0 +1,47 @@
+/**
+ * Скролиране надолу и нагоре
+ */
+function fastScroll(hideAfterSec, activateRatio)
+{
+	var bodyHeight = $(document).height();
+	var screenHeight = screen.height;
+    var hideAfterMilisec = hideAfterSec * 1000;
+	if(bodyHeight / screenHeight > activateRatio){
+		$("#maincontent").append('<div class="scroll-btn-container"><div id="scroll-to-top"></div><div id="scroll-to-bottom"></div></div>');
+		$("#scroll-to-top").click(function(){
+			$("html, body").animate({ 
+				scrollTop: 0 
+				}, 500);
+			return false;
+	     });
+		
+		$('#scroll-to-bottom').click(function(){
+			$("html,body").animate({ 
+				scrollTop: $(document).height()  
+				}, 500 );
+			return false;
+	     });
+		
+	     var scrollTimeout;
+	     $(window).scroll(function() {
+	    	  if(scrollTimeout) clearTimeout(scrollTimeout);
+
+		      scrollTimeout = setTimeout(function(){
+				  $('#scroll-to-bottom').fadeOut('slow');
+			      $('#scroll-to-top').fadeOut('slow');
+			  }, hideAfterMilisec);
+		   
+		      if($(this).scrollTop() > 200) {
+		          $('#scroll-to-top').fadeIn('slow');
+		      } else {
+		          $('#scroll-to-top').fadeOut('slow');
+		      }
+
+			  if($(this).scrollTop() > bodyHeight - screenHeight) {
+		         $('#scroll-to-bottom').fadeOut('slow');
+		      } else {
+		         $('#scroll-to-bottom').fadeIn('slow');
+		      }
+		 });   
+	}
+}
