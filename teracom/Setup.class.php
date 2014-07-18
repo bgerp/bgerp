89c52f88e757427b82ba3078c410da5885d35f82
Milen Georgiev <milen@experta.bg>
2014-07-18 22:52:19 +0300

    Пакет за драйвери за устройствата на Тераком ООД

---
+++ teracom/Setup.class.php
@@ -0,0 +1,57 @@
+<?php
+
+
+/**
+ * class teracom_Setup
+ *
+ * Инсталиране/Деинсталиране на драйвери за устройствата на Тераком ООД - Русе 
+ *
+ * @category  vendors
+ * @package   teracom
+ * @author    Milen Georgiev <milen@download.bg>
+ * @copyright 2006 - 2014 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class teracom_Setup extends core_ProtoSetup
+{
+    
+    
+    /**
+     * Версия на пакета
+     */
+    var $version = '0.1';
+    
+    
+    /**
+     * От кои други пакети зависи
+     */
+    var $depends = '';
+    
+      
+    /**
+     * Описание на модула
+     */
+    var $info = "Драйвери за контролери на Тераком ООД";
+    
+            
+    /**
+     * Инсталиране на пакета
+     */
+    function install()
+    {
+        $html = parent::install();
+                                 
+        // Добавяме наличните драйвери
+        $drivers = array(
+            'teracom_TCW122B',
+        );
+        
+        foreach ($drivers as $drvClass) {
+            $html .= core_Classes::add($drvClass);
+        }
+         
+        return $html;
+    }
+    
+}
