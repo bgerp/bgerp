27d227f9c29f564734d1c3ee3a368de7108a695e
Yusein Yuseinov <yyuseinov@gmail.com>
2014-07-18 10:55:46 +0300

    doc_RichTextPlg - Никовете след autocomplete да не преминават през обработка за никове

--- doc/RichTextPlg.class.php
+++ doc/RichTextPlg.class.php
@@ -348,7 +348,8 @@ class doc_RichTextPlg extends core_Plugin
         $place = $this->mvc->getPlace();
         
         // За ника използваме и префикса от стринга
-        $nick = $match['pre'] . core_Users::prepareNick($match['nick']);
+//        $nick = $match['pre'] . core_Users::prepareNick($match['nick']);
+        $nick = $match['pre'] . $match['nick'];
         
         $this->mvc->_htmlBoard[$place] = crm_Profiles::createLink($id, $nick);
         
