a2a45932376101db0e1a1e223b79092fb4ac6664
HeBuTy <vencho_27@abv.bg>
2014-07-24 17:01:34 +0300

    Добавени setup класове

---
+++ orgchart/Setup.class.php
@@ -0,0 +1,27 @@
+<?php
+
+
+/** Oрганизациони структурии
+ * 
+ * @category  vendors
+ * @package   orgchart
+ * @author    Nevena Georgieva <nevena.georgieva89@gmail.com>
+ * @copyright 2006 - 2013 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class orgchart_Setup extends core_ProtoSetup
+{
+    
+	/**
+	 * Път до js файла
+	 */
+	var $commonJS = 'orgchart/lib/jquery.orgchart.js';
+	
+	
+	/**
+	 * Път до css файла
+	 */
+	var $commonCSS = 'orgchart/lib/jquery.orgchart.css';
+}
+
