f0c1336c73f0cdcf445de54fd7e3a0720a8dcbfe
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-07-30 17:45:05 +0300

    price_ListRules:след желаната цена да се показва валутата от мастъра

--- price/ListRules.class.php
+++ price/ListRules.class.php
@@ -300,8 +300,10 @@ class price_ListRules extends core_Detail
             case 'discount' :
                 $form->setField('groupId,price,currency,vat', 'input=none');
                 $title = "Правило за марж в ценова политика|* \"$masterTitle\"";
-                $unit = ($masterRec->vat == 'yes') ? "с ДДС" : "без ДДС";
-                $form->setField('targetPrice', "unit = {$unit}");
+                
+                $form->getField('targetPrice')->unit = "|*" . $masterRec->currency . ", ";
+                $form->getField('targetPrice')->unit .= ($masterRec->vat == 'yes') ? "|с ДДС|*" : "|без ДДС|*";
+                
                 break;
             case 'value' :
                 $form->setField('groupId,discount,calculation,targetPrice', 'input=none');
