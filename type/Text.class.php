2d392c6b70de1c6b07805de330277e5ad95b2044
Yusein Yuseinov <yyuseinov@gmail.com>
2014-08-08 16:57:16 +0300

    type_Text - Възможност за задаване на suggestions в type_Text

--- type/Text.class.php
+++ type/Text.class.php
@@ -164,4 +164,34 @@ class type_Text extends core_Type {
         
         return $chunk;
     }
+    
+    
+    /**
+     * Връща масив със всички предложения за този списък
+     */
+    public function getSuggestions()
+    {
+        if(!$this->suggestions) {
+            $this->prepareSuggestions();
+        }
+
+        return $this->suggestions;
+    }
+    
+    
+    /**
+     * Подготвя предложенията за списъка
+     */
+    private function prepareSuggestions()
+    {
+        $this->suggestions = arr::make($this->suggestions);
+        
+        if ($this->invoke('BeforePrepareSuggestions', array(&$this->suggestions, $this)) === FALSE) return ;
+        
+        // Добавяме 
+        $suggestionsStr = str_replace('|', ',', $this->params['suggestions']);
+        $this->suggestions = arr::make($suggestionsStr, TRUE);
+        
+        $this->invoke('AfterPrepareSuggestions', array(&$this->suggestions, $this));
+    }
 }
