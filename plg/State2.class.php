6dc00b5bd871f096a6fc5ba7a7eb5a86fd1c4f0a
Milen Georgiev <milen@experta.bg>
2014-08-23 15:38:19 +0300

    plg_State2 разпознава активното и затвореното състояние от множеството опции на полето

--- plg/State2.class.php
+++ plg/State2.class.php
@@ -16,7 +16,29 @@
  */
 class plg_State2 extends core_Plugin
 {
+
+    /**
+     * Наименование на активното състояние
+     */
+    private $activeState;
     
+    /**
+     * Наименование на затвореното състояние
+     */
+    private $closedState;
+
+
+    /**
+     * Кои състояния се кастват до Активно
+     */
+    private $castToActive = array('active', 'opened', 'free');
+
+
+    /**
+     * Кои състояния се кастват до затворено
+     */
+    private $castToClosed = array('closed', 'stopped', 'rejected');
+
     
     /**
      * Добавя полето за състояние, ако то липсва
@@ -27,7 +49,38 @@ class plg_State2 extends core_Plugin
             $mvc->FLD('state',
                 'enum(active=Активен,closed=Затворен)',
                 'caption=Видимост,input=none,notSorting,notNull,value=active');
+            $this->activeState = 'active';
+            $this->closedState = 'closed';
+        } 
+    }
+
+
+    /**
+     * Определя активното и затвореното състояние
+     */
+    function getActiveAndClosedState($mvc)
+    {
+        if($this->activeState && $this->closedState) {
+
+            return;
         }
+        $opt = $mvc->getFieldType('state')->options;
+            
+        foreach($this->castToActive as $state) {
+            if($opt[$state]) {
+                $this->activeState = $state;
+                break;
+            }
+        }
+
+        foreach($this->castToClosed as $state) {
+            if($opt[$state]) {
+                $this->closedState = $state;
+                break;
+            }
+        }
+
+        expect($this->activeState && $this->closedState);
     }
     
     
@@ -44,9 +97,10 @@ class plg_State2 extends core_Plugin
      * Гарантира, че новите записи ще имат state по подразбиране - 'active'
      */
     function on_BeforeSave(&$invoker, &$id, &$rec, $fields = NULL)
-    {
+    {   
         if (!$rec->state) {
-            $rec->state = 'active';
+            $this->getActiveAndClosedState($invoker);
+            $rec->state = $this->activeState;
         }
     }
     
@@ -64,15 +118,20 @@ class plg_State2 extends core_Plugin
         
         if ($mvc->haveRightFor('changeState', $rec)) {
             
+            $this->getActiveAndClosedState($mvc);
+
             $add = "<img src=" . sbf("img/16/lightbulb_off.png") . " width='16' height='16'>";
             $cancel = "<img src=" . sbf("img/16/lightbulb.png") . " width='16' height='16'>";
             
-            $row->state = ht::createLink($rec->state == 'active' ? $cancel : $add ,
-                array($mvc, 'changeState', $rec->id, 'ret_url' => TRUE),
-                NULL,
-                array('title' => $rec->state == 'active' ? 'Скриване' : 'Показване'));
-            $row->state = ht::createElement('div',
-                array('style' => "text-align:center;"), $row->state);
+            if($rec->state == $this->activeState || $rec->state == $this->closedState) {
+                $row->state = ht::createLink($rec->state == $this->activeState ? $cancel : $add ,
+                    array($mvc, 'changeState', $rec->id, 'ret_url' => TRUE),
+                    NULL,
+                    array('title' => $rec->state == $this->activeState ? 'Скриване' : 'Показване'));
+            
+                $row->state = ht::createElement('div',
+                    array('style' => "text-align:center;"), $row->state);
+            }
         }
     }
     
@@ -94,9 +153,14 @@ class plg_State2 extends core_Plugin
         
         $mvc->requireRightFor($action, $rec, NULL, $retUrl);
         
-        $rec->state = ($rec->state == 'active' ? 'closed' : 'active');
-        
-        $mvc->save($rec, 'state');
+        $this->getActiveAndClosedState($mvc);
+
+        if($rec->state == $this->activeState || $rec->state == $this->closedState) {
+
+            $rec->state = ($rec->state == $this->activeState ? $this->closedState : $this->activeState);
+            
+            $mvc->save($rec, 'state');
+        }
         
         $content = new Redirect($retUrl);
         
