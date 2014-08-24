1653936ccce934e9af4b77e246964eecb107051f
Milen Georgiev <milen@experta.bg>
2014-08-24 22:31:00 +0300

    bugFix

--- crm/Setup.class.php
+++ crm/Setup.class.php
@@ -162,7 +162,10 @@ class crm_Setup extends core_ProtoSetup
      * Премества персонализационните данни за потребителя от crm_Personalization в core_Users
      */
     static function movePersonalizationData()
-    {
+    { 
+        // Функцията изглежда недовършена, затова е байпасната
+        return;
+
         $query = crm_Personalization::getQuery();
         $query->where('1=1');
         while($rec = $query->fetch()) {
