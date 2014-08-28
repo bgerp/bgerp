01a22214b1aeb5a83b5ba82cc0e49a521ce6c8e3
Yusein Yuseinov <yyuseinov@gmail.com>
2014-08-28 10:21:48 +0300

    core_Manger - тест за качество

--- core/Manager.class.php
+++ core/Manager.class.php
@@ -14,8 +14,6 @@
  * @since     v 0.1
  * @link
  * 
- * @method on_AfterRenderWrapping(core_Manager $mvc, core_ET &$res, core_ET &$tpl=NULL, $data = NULL)
- * @method on_BeforeRenderWrapping($mvc, &$res, &$tpl=NULL, $data = NULL)
  * @method renderWrapping(core_ET|string|null &$tpl=NULL, $data = NULL)
  */
 class core_Manager extends core_Mvc
