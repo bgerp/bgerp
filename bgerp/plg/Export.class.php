377e70fd6913af588a4b8d466bff52955c391dc9
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-08-29 16:10:17 +0300

    Плъгин за експортиране: bgerp_plg_Export

---
+++ bgerp/plg/Export.class.php
@@ -0,0 +1,122 @@
+<?php
+
+
+
+/**
+ * Плъгин за експортиране на данни
+ *
+ * @category  bgerp
+ * @package   bgerp
+ * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
+ * @copyright 2006 - 2014 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class bgerp_plg_Export extends core_Plugin
+{
+	
+	
+	/**
+	 * След дефиниране на полетата на модела
+	 *
+	 * @param core_Mvc $mvc
+	 */
+	public static function on_AfterDescription(core_Mvc $mvc)
+	{
+		defIfNot($mvc->canExport, 'ceo');
+	}
+	
+	
+	/**
+	 * Извиква се след подготовката на toolbar-а за табличния изглед
+	 */
+	static function on_AfterPrepareListToolbar($mvc, &$data)
+	{
+		if($mvc->haveRightFor('export')){
+			$data->toolbar->addBtn('Експорт', array($mvc, 'export', 'ret_url' => TRUE), 'ef_icon=img/16/export.png');
+		}
+	}
+    
+	
+    /**
+     * Функция връщаща опции с всички драйвери които могат да се прикачват
+     * към мениджъра
+     * 
+     * @return array $options - масив с възможни драйвъри
+     */
+    public static function getImportDrivers($mvc)
+    {
+    	$options = array();
+    	$drivers = core_Classes::getOptionsByInterface('bgerp_ExportIntf');
+    	
+    	foreach ($drivers as $id => $driver){
+    		$Driver = cls::get($id);
+    		if($Driver->isApplicable($mvc)){
+    			$options[$id] = $Driver->title;
+    		}
+    	}
+    	
+    	return $options;
+    }
+    
+    
+    /**
+     * Преди всеки екшън на мениджъра-домакин
+     */
+    public static function on_BeforeAction($mvc, &$tpl, $action)
+    {
+    	if($action == 'export'){
+    		
+    		// Проверка за права
+    		$mvc->requireRightFor('export');
+    		 
+    		// Трябва да има инсталиран поне един драйвър за експорт
+    		$options = self::getImportDrivers($mvc);
+    		
+    		// Подготвяме формата
+    		$form = cls::get('core_Form');
+    		$form->method = 'GET';
+    		$form->title = "Експортиране на {$mvc->title}";
+    		$form->FNC('driver', 'class(interface=bgerp_ExportIntf,allowEmpty,select=title)', 'input,caption=Драйвър,mandatory,silent', array('attr' => array('onchange' => "addCmdRefresh(this.form);this.form.submit()")));
+    		
+    		if(count($options)){
+    			$form->setOptions('driver', $options);
+    		} else {
+    			$form->setreadOnly('driver');
+    		}
+    		
+    		$form->input(NULL, 'silent');
+    		
+    		if($form->rec->driver){
+    			$Driver = cls::get($form->rec->driver);
+    			$Driver->prepareExportForm($form);
+    		}
+    		
+    		$form->input();
+    		
+    		if($Driver){
+    			$Driver->checkExportForm($form);
+    		}
+    		
+    		if($form->isSubmitted()){
+    			$Driver = cls::get($form->rec->driver);
+    			$Driver->export($form->rec);
+    			followRetUrl();
+    		}
+    		 
+    		// Добавяне на туулбара
+    		if(count($options)){
+    			$form->toolbar->addSbBtn('Експорт', 'default', array('class' => 'btn-next'), 'ef_icon = img/16/export.png');
+    		} else {
+    			$form->toolbar->addBtn('Експорт', array(), 'error=Няма налични драйвъри за експорт');
+    		}
+    		
+    		$form->toolbar->addBtn('Отказ', array($this, 'list'), 'ef_icon = img/16/close16.png');
+    		$form = $form->renderHtml();
+    		
+    		$tpl = $mvc->renderWrapping($form);
+    		
+    		return FALSE;
+    	}
+    }
+}
