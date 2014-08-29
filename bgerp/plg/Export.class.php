2e19677601b32b4abb44f9b0c513b6ec68296b1d
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-08-29 16:18:03 +0300

    bgerp_plg_Export: добавени коментари

--- bgerp/plg/Export.class.php
+++ bgerp/plg/Export.class.php
@@ -30,7 +30,7 @@ class bgerp_plg_Export extends core_Plugin
 	/**
 	 * Извиква се след подготовката на toolbar-а за табличния изглед
 	 */
-	static function on_AfterPrepareListToolbar($mvc, &$data)
+	static function on_AfterPrepareListToolbar(core_Mvc $mvc, &$data)
 	{
 		if($mvc->haveRightFor('export')){
 			$data->toolbar->addBtn('Експорт', array($mvc, 'export', 'ret_url' => TRUE), 'ef_icon=img/16/export.png');
@@ -44,7 +44,7 @@ class bgerp_plg_Export extends core_Plugin
      * 
      * @return array $options - масив с възможни драйвъри
      */
-    public static function getImportDrivers($mvc)
+    public static function getImportDrivers(core_Mvc $mvc)
     {
     	$options = array();
     	$drivers = core_Classes::getOptionsByInterface('bgerp_ExportIntf');
@@ -63,7 +63,7 @@ class bgerp_plg_Export extends core_Plugin
     /**
      * Преди всеки екшън на мениджъра-домакин
      */
-    public static function on_BeforeAction($mvc, &$tpl, $action)
+    public static function on_BeforeAction(core_Mvc $mvc, &$tpl, $action)
     {
     	if($action == 'export'){
     		
@@ -79,25 +79,31 @@ class bgerp_plg_Export extends core_Plugin
     		$form->title = "Експортиране на {$mvc->title}";
     		$form->FNC('driver', 'class(interface=bgerp_ExportIntf,allowEmpty,select=title)', 'input,caption=Драйвър,mandatory,silent', array('attr' => array('onchange' => "addCmdRefresh(this.form);this.form.submit()")));
     		
+    		// Ако има опции за избор на драйвър слагаме ги, иначе правим полето readOnly
     		if(count($options)){
     			$form->setOptions('driver', $options);
     		} else {
     			$form->setreadOnly('driver');
     		}
     		
+    		// Инпутваме тихите полета
     		$form->input(NULL, 'silent');
     		
+    		// Ако е избран драйвър, той добавя полета към формата
     		if($form->rec->driver){
     			$Driver = cls::get($form->rec->driver);
     			$Driver->prepareExportForm($form);
     		}
     		
+    		// Инпут на формата
     		$form->input();
     		
+    		// Драйвера проверява формата
     		if($Driver){
     			$Driver->checkExportForm($form);
     		}
     		
+    		// Ако формата е събмитната
     		if($form->isSubmitted()){
     			$Driver = cls::get($form->rec->driver);
     			$Driver->export($form->rec);
