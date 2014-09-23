66c442ae4d745bbfbf27a1befa1a187e4192b940
Milen Georgiev <milen@experta.bg>
2014-09-23 21:37:59 +0300

    Installing package thumb on setup

--- bgerp/Setup.class.php
+++ bgerp/Setup.class.php
@@ -111,7 +111,7 @@ class bgerp_Setup extends core_ProtoSetup {
         $isFirstSetup = ($Packs->count() == 0);
         
         // Списък на основните модули на bgERP
-        $packs = "core,fileman,drdata,bglocal,editwatch,recently,thumbnail,custom,doc,acc,currency,cms,
+        $packs = "core,fileman,drdata,bglocal,editwatch,recently,thumb,custom,doc,acc,currency,cms,
                   email,crm, cat, trans, price, blast,rfid,hr,trz,lab,sales,mp,marketing,store,cond,cash,bank,
                   budget,purchase,accda,sens,cams,frame,cal,fconv,log,fconv,cms,blogm,forum,deals,findeals,
                   vislog,docoffice,incoming,support,survey,pos,change,sass,techno,
