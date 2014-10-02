45db718033af03b9c28af86ac85d7c21bc52047f
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-10-02 17:22:09 +0300

    Покупка и рподажба:bugFix върнато местоположение на плъгините

--- purchase/Purchases.class.php
+++ purchase/Purchases.class.php
@@ -36,7 +36,7 @@ class purchase_Purchases extends deals_DealMaster
     /**
      * Плъгини за зареждане
      */
-    public $loadList = 'plg_RowTools, purchase_Wrapper, plg_Sorting, doc_plg_MultiPrint, doc_plg_TplManager, acc_plg_Contable, doc_DocumentPlg, plg_Printing,
+    public $loadList = 'plg_RowTools, purchase_Wrapper, plg_Sorting, doc_plg_MultiPrint, doc_plg_TplManager, doc_DocumentPlg, acc_plg_Contable, plg_Printing,
 				        plg_ExportCsv, cond_plg_DefaultValues, doc_plg_HidePrices, doc_SharablePlg, plg_Clone,
 				        doc_EmailCreatePlg, bgerp_plg_Blank, doc_plg_BusinessDoc, acc_plg_DocumentSummary, plg_Search';
     
