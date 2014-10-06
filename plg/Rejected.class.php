503b83b9589a26ed8c35859f92d8344d22d98f98
Milen Georgiev <milen@experta.bg>
2014-10-06 15:50:26 +0300

    Да не се показва коша, ако не можем да реджекваме или възстановяваме

--- plg/Rejected.class.php
+++ plg/Rejected.class.php
@@ -78,7 +78,10 @@ class plg_Rejected extends core_Plugin
      * Добавя бутон за показване на оттеглените записи
      */
     function on_AfterPrepareListToolbar($mvc, &$res, $data)
-    {
+    {   
+        if(!$mvc->haveRightFor('restore') && !$mvc->haveRightFor('reject')) {
+            return;
+        }
         if(Request::get('Rejected')) {
             $data->toolbar->removeBtn('*', 'with_selected');
             $data->toolbar->addBtn('Всички', array($mvc), 'id=listBtn', "ef_icon = img/16/application_view_list.png,title=Всички " . mb_strtolower($mvc->title));
