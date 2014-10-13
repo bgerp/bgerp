8663804d43682a3a18206479943797eefe602c3b
Yusein Yuseinov <yyuseinov@gmail.com>
2014-10-13 11:16:25 +0300

    blast_Wrapper bugFix

--- blast/Wrapper.class.php
+++ blast/Wrapper.class.php
@@ -22,8 +22,8 @@ class blast_Wrapper extends plg_ProtoWrapper
      */
     function description()
     {
-        $this->TAB('blast_Lists', 'Списъци', 'blast,ceo,admin');
         $this->TAB('blast_ListDetails', 'Списъци', 'blast,ceo,admin');
+        $this->TAB('blast_Lists', 'Списъци', 'blast,ceo,admin');
         $this->TAB('blast_Emails', 'Имейли', 'ceo, blast');
         $this->TAB('blast_Sms', 'SMS-и');
         $this->TAB('blast_Faxes', 'Факсове');
