7b6df51b74e0a85a4ac410f04101d155bd714936
Yusein Yuseinov <yyuseinov@gmail.com>
2014-10-13 11:27:14 +0300

    email_FaxSent - Премахнато TODO

--- email/FaxSent.class.php
+++ email/FaxSent.class.php
@@ -244,8 +244,8 @@ class email_FaxSent extends core_Manager
         //Всички факс номера
         $faxToArr = static::faxToArray($options->faxTo);
         
-//        $emailCss = file_get_contents(sbf('css/email.css', "", TRUE)); //TODO
-        $success  = $failure = array(); // списъци с изпратени и проблемни получатели
+        // Списъци с изпратени и проблемни получатели
+        $success  = $failure = array();
         
         // Инстанция на log_Documents за да работи on_Shutdown
         cls::get('log_Documents');
@@ -276,13 +276,9 @@ class email_FaxSent extends core_Manager
             // Подготовка на текста на писмото (HTML & plain text)
             $rec->__mid = NULL;
             
-            //HTML частта на факса
-//            $faxHtml = $Email->getEmailHtml($rec, $lg);
-            
             //Текстовата част на факса
             $faxText = core_ET::unEscape($Email->getEmailText($rec, $lg));
             
-//            $rec->html = $faxHtml //TODO не е нужно, защото HTML частта се добавя като прикачен файл
             $rec->text = $faxText;
             
             // Генериране на прикачените документи
