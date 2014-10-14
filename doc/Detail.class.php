2505865290dc17dd82ff04d12864b15dc84bf0c5
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-10-14 10:30:47 +0300

    Нов клас: doc_Detail абстрактен клас за детайли на документи

---
+++ doc/Detail.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Клас 'doc_Detail'
+ *
+ * абстрактен клас за детайл на документи
+ *
+ *
+ * @category  bgerp
+ * @package   doc
+ * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
+ * @copyright 2006 - 2014 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+abstract class doc_Detail extends core_Detail
+{
+	
+}
