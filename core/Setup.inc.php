89cea7909f22c0c78db85356659a1cf3219f5db7
mitko <mitko@extrapack.com>
2014-10-20 18:33:47 +0300

    setup: не се следи и лог-а

--- core/Setup.inc.php
+++ core/Setup.inc.php
@@ -857,8 +857,9 @@ if ($step == 'setup') {
             $logModified = FALSE;
         }
 //    } while ($numRows < $totalRecords && $numTables <= $totalTables && $numColumns <= $totalColumns || !empty($setupLog) || $logModified);
-    } while ($numRows < $totalRecords && $numTables <= $totalTables && $numColumns <= $totalColumns || !empty($setupLog));
-    
+//    } while ($numRows < $totalRecords && $numTables <= $totalTables && $numColumns <= $totalColumns || !empty($setupLog));
+    } while ($numRows < $totalRecords && $numTables <= $totalTables && $numColumns <= $totalColumns);
+     
     if ($percents < 100) {
         $percents = 100;
         $width = 4.5*$percents;
