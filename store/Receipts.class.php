9c707bf10e87531f1b44f7201d3e881eebc4426c
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-10-28 16:58:26 +0200

    Вх. фактура и СР: BugFix По кепшъните в формата

--- store/Receipts.class.php
+++ store/Receipts.class.php
@@ -145,6 +145,7 @@ class store_Receipts extends store_DocumentMaster
     public function description()
     {
         parent::setDocFields($this);
+        $this->setField('storeId', 'caption=В склад');
     }
     
     
