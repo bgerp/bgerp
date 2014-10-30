5d70aca0e30f5c0424e1e8b50ed6cd417a7d3274
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-10-30 10:50:59 +0200

    Входяща Ф-ри:bugFix махнати полетата за ддс

--- purchase/Purchases.class.php
+++ purchase/Purchases.class.php
@@ -218,6 +218,7 @@ class purchase_Purchases extends deals_DealMaster
     {
     	parent::setDealFields($this);
     	$this->FLD('bankAccountId', 'iban_Type(64)', 'caption=Плащане->Към банк. сметка,after=currencyRate');
+    	$this->setField('dealerId', 'caption=Наш персонал->Търговец');
     }
     
     
