97f124bfbb0cc5feb2ed05b66684e84d0fa8280b
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-11-03 14:41:59 +0200

    frame_BaseDriver: абстрактен клас за наследяване на драйвърите на отчетите

---
+++ frame/BaseDriver.class.php
@@ -0,0 +1,75 @@
+<?php
+
+
+
+/**
+ * Базов клас за наследяване от другите драйвери
+ *
+ *
+ * @category  bgerp
+ * @package   acc
+ * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
+ * @copyright 2006 - 2014 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+abstract class frame_BaseDriver extends core_BaseClass
+{
+	
+	
+	/**
+	 * След активация на репорта
+	 */
+	public static function on_AfterActivation($mvc, &$is, $innerForm, &$rec)
+	{
+		$is = $mvc->prepareInnerState($innerForm);
+		frame_Reports::save($rec);
+	}
+	
+	
+	/**
+	 * След оттегляне на репорта
+	 */
+	public static function on_AfterReject($mvc, &$is, $innerForm, &$rec)
+	{
+		$is = $mvc->prepareInnerState($innerForm);
+		frame_Reports::save($rec);
+	}
+	
+	
+	/**
+	 * След възстановяване на репорта
+	 */
+	public static function on_AfterRestore($mvc, &$is, $innerForm, &$rec)
+	{
+		if($rec->state == 'draft'){
+			unset($rec->data);
+			frame_Reports::save($rec);
+		}
+	}
+	
+	
+	/**
+	 * Можели вградения обект да се избере
+	 */
+	public function canSelectInnerObject($userId = NULL)
+	{
+		return core_Users::haveRole($this->canSelectSource, $userId);
+	}
+
+
+	/**
+	 * Подготвя данните необходими за показването на вградения обект
+	 *
+	 * @param core_Form $innerForm
+	 * @param stdClass $innerState
+	 */
+	public function prepareEmbeddedData($innerForm, &$innerState)
+	{
+		if(!empty($innerState)){
+			return $innerState;
+		}
+		 
+		return $this->prepareInnerState($innerForm);
+	}
+}
