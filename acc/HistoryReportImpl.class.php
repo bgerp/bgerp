aa18429d77fe01666496eb9368b62759e7415347
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-11-04 12:29:20 +0200

    Драйвъра за сч. справка на хронологията е отделен клас

---
+++ acc/HistoryReportImpl.class.php
@@ -0,0 +1,161 @@
+<?php
+
+
+
+/**
+ * Имплементация на 'frame_ReportSourceIntf' за направата на справка на баланса
+ *
+ *
+ * @category  bgerp
+ * @package   acc
+ * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
+ * @copyright 2006 - 2014 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class acc_HistoryReportImpl extends frame_BaseDriver
+{
+	
+	/**
+	 * Кой може да избира драйвъра
+	 */
+	public $canSelectSource = 'ceo, acc';
+	
+	
+	/**
+	 * Кои интерфейси имплементира
+	 */
+	public $interfaces = 'frame_ReportSourceIntf';
+	
+	
+	/**
+	 * Плъгини за зареждане
+	 */
+	public $loadList = 'History=acc_BalanceHistory';
+	
+	
+	/**
+	 * Заглавие
+	 */
+	public $title = 'Хронологична справка на аналитична сметка';
+	
+	
+	/**
+	 * Добавя полетата на вътрешния обект
+	 *
+	 * @param core_Fieldset $fieldset
+	 */
+	public function addEmbeddedFields(core_Form &$form)
+	{
+		$form->FLD('accountId', 'acc_type_Account(allowEmpty)', 'input,caption=Сметка,silent,mandatory', array('attr' => array('onchange' => "addCmdRefresh(this.form);this.form.submit()")));
+		$form->FLD('fromDate', 'date(allowEmpty)', 'caption=От,input,mandatory');
+		$form->FLD('toDate', 'date(allowEmpty)', 'caption=До,input,mandatory');
+	}
+	
+	
+	/**
+	 * Подготвя формата за въвеждане на данни за вътрешния обект
+	 *
+	 * @param core_Form $form
+	 */
+	public function prepareEmbeddedForm(core_Form &$form)
+	{
+		$op = $this->History->getBalancePeriods();
+		 
+		$form->setSuggestions('fromDate', array('' => '') + $op->fromOptions);
+		$form->setSuggestions('toDate', array('' => '') + $op->toOptions);
+		 
+		if($form instanceof core_Form){
+			$form->input();
+		}
+		 
+		if(isset($form->rec->accountId)){
+			if($form->rec->id){
+				if(frame_Reports::fetchField($form->rec->id, 'filter')->accountId != $form->rec->filter->accountId){
+					unset($form->rec->ent1Id, $form->rec->ent2Id, $form->rec->ent3Id);
+					Request::push(array('ent1Id' => NULL, 'ent2Id' => NULL, 'ent3Id' => NULL));
+				}
+			}
+			 
+			$accInfo = acc_Accounts::getAccountInfo($form->rec->accountId);
+			 
+			foreach (range(1, 3) as $i){
+				if(isset($accInfo->groups[$i])){
+					$gr = $accInfo->groups[$i];
+					$form->FNC("ent{$i}Id", "acc_type_Item(lists={$gr->rec->num}, allowEmpty)", "caption=Избор на пера->{$gr->rec->name},input,mandatory");
+				} else {
+					$form->FNC("ent{$i}Id", "int", "");
+					$form->rec->{"ent{$i}Id"} = NULL;
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * Проверява въведените данни
+	 *
+	 * @param core_Form $form
+	 */
+	public function checkEmbeddedForm(core_Form &$form)
+	{
+		if($form->isSubmitted()){
+			if($form->rec->toDate < $form->rec->fromDate){
+				$form->setError('to, from', 'Началната дата трябва да е по малка от крайната');
+			}
+		}
+	}
+
+
+	/**
+	 * Подготвя вътрешното състояние, на база въведените данни
+	 *
+	 * @param core_Form $innerForm
+	 */
+	public function prepareInnerState(&$innerForm)
+	{
+		// Подготвяне на данните
+		$filter = $innerForm;
+		$data = new stdClass();
+		$accNum = acc_Accounts::fetchField($filter->accountId, 'num');
+		 
+		$data->rec = new stdClass();
+		$data->rec->accountId = $filter->accountId;
+		$data->rec->ent1Id = $filter->ent1Id;
+		$data->rec->ent2Id = $filter->ent2Id;
+		$data->rec->ent3Id = $filter->ent3Id;
+		$data->rec->accountNum = $accNum;
+		 
+		acc_BalanceDetails::requireRightFor('history', $data->rec);
+		 
+		$balanceRec = $this->History->getBalanceBetween($filter->fromDate, $filter->toDate);
+		 
+		$data->balanceRec = $balanceRec;
+		$data->fromDate = $filter->fromDate;
+		$data->toDate = $filter->toDate;
+		 
+		$this->History->prepareHistory($data);
+		 
+		return $data;
+	}
+	
+	
+	/**
+	 * След подготовката на показването на информацията
+	 */
+	public function on_AfterPrepareEmbeddedData($mvc, &$res, $innerForm, &$innerState)
+	{
+		$this->History->prepareRows($res);
+	}
+	
+	
+	/**
+	 * Рендира вградения обект
+	 *
+	 * @param stdClass $data
+	 */
+	public function renderEmbeddedData($data)
+	{
+		return $this->History->renderHistory($data);
+	}
+}
