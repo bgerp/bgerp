52c12e080c88af1492be5f5e285555447e4b3a36
GPetrova <gab4eto@gmail.com>
2014-11-11 16:17:50 +0200

    hr: Променено заглавие на интерфейс

--- hr/DepartmentAccRegIntf.class.php
+++ hr/DepartmentAccRegIntf.class.php
@@ -3,7 +3,7 @@
 
 
 /**
- * Интерфейс за пера - трудови договори
+ * Интерфейс за пера - организационна структура
  *
  *
  * @category  bgerp
@@ -12,7 +12,7 @@
  * @copyright 2006 - 2012 Experta OOD
  * @license   GPL 3
  * @since     v 0.1
- * @title     Интерфейс за пера - трудови договори
+ * @title     Интерфейс за пера - организационна структура
  */
 class hr_DepartmentAccRegIntf extends acc_RegisterIntf
 {
