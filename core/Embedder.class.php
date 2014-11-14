67a455e121152372b2e265feabcb650bb9466129
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-11-14 15:44:48 +0200

    core_Embedder:getDriver bugFix При подаване на информацията за ембедъра

--- core/Embedder.class.php
+++ core/Embedder.class.php
@@ -105,7 +105,7 @@ class core_Embedder extends core_Master
 		
 		if(empty(self::$Drivers[$rec->id])){
 			$Driver = cls::get($rec->{$this->innerClassField});
-			$Driver->ProductRec = new core_ObjectReference($this, $rec);
+			$Driver->ProductRec = new core_ObjectReference($this, $rec->id);
 			self::$Drivers[$rec->id] = $Driver;
 		}
 		
