fad2011b24d0e68008946d1e0c26e7e16dcda8cd
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-11-18 11:24:05 +0200

    Сделки:дилъра е отговорника на папката, влиза като свойство

--- purchase/Purchases.class.php
+++ purchase/Purchases.class.php
@@ -218,7 +218,7 @@ class purchase_Purchases extends deals_DealMaster
     {
     	parent::setDealFields($this);
     	$this->FLD('bankAccountId', 'iban_Type(64)', 'caption=Плащане->Към банк. сметка,after=currencyRate');
-    	$this->setField('dealerId', 'caption=Наш персонал->Търговец');
+    	$this->setField('dealerId', 'caption=Наш персонал->Закупчик');
     	$this->setField('shipmentStoreId', 'caption=Доставка->В склад');
     }
     
