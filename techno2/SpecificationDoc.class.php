1af1e888a9e9adff9a2b5b11b6047243fe937df9
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-11-21 12:27:02 +0200

    Спецификации2: добавен бутон към филтрираната история на изгледа на спецификацията

--- techno2/SpecificationDoc.class.php
+++ techno2/SpecificationDoc.class.php
@@ -334,6 +334,10 @@ class techno2_SpecificationDoc extends core_Embedder
     			$data->toolbar->addBtn("Оферта", array('sales_Quotations', 'add', 'originId' => $data->rec->containerId), 'ef_icon = img/16/document_quote.png,title=Нова оферта за спецификацията');
     		}
     	}
+    	
+    	if(techno2_SpecTplCache::haveRightFor('read')){
+    		$data->toolbar->addBtn("История", array('techno2_SpecTplCache', 'list', 'docId' => $data->rec->id), 'target=_blank,ef_icon = img/16/view.png,title=Минали изгледи на спецификации');
+    	}
     }
     
     
