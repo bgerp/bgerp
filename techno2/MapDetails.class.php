10e7f325c1b12a6d0c1180d9d24d9c23a308e346
Ivelin Dimov <ivelin_pdimov@abv.bg>
2014-12-17 17:51:19 +0200

    Т карти:plg_LastUsedKeys

--- techno2/MapDetails.class.php
+++ techno2/MapDetails.class.php
@@ -38,7 +38,13 @@ class techno2_MapDetails extends doc_Detail
     /**
      * Плъгини за зареждане
      */
-    var $loadList = 'plg_Created, plg_RowTools, techno2_Wrapper, plg_Sorting, plg_RowNumbering, plg_StyleNumbers, plg_AlignDecimals';
+    var $loadList = 'plg_Created, plg_RowTools, techno2_Wrapper, plg_LastUsedKeys, plg_Sorting, plg_RowNumbering, plg_StyleNumbers, plg_AlignDecimals';
+    
+    
+    /**
+     * Кои ключове да се тракват, кога за последно са използвани
+     */
+    var $lastUsedKeys = 'stageId,resourceId';
     
     
     /**
