b8494756fc00f256485012c262956b64d44f5473
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-01-06 12:45:41 +0200

    МО: транзакционните методи са изнесени в отделен клас

--- acc/Articles.class.php
+++ acc/Articles.class.php
@@ -25,7 +25,7 @@ class acc_Articles extends core_Master
     /**
      * Какви интерфейси поддържа този мениджър
      */
-    var $interfaces = 'acc_TransactionSourceIntf';
+    var $interfaces = 'acc_TransactionSourceIntf=acc_transaction_Article';
     
     
     /**
@@ -289,100 +289,6 @@ class acc_Articles extends core_Master
         }
     }
     
-    /*******************************************************************************************
-     * 
-     *     Имплементация на интерфейса `acc_TransactionSourceIntf`
-     * 
-     ******************************************************************************************/
-    
-    
-    /**
-     * @param int $id
-     * @return stdClass
-     * @see acc_TransactionSourceIntf::getTransaction
-     */
-    public static function getTransaction($id)
-    {
-        // Извличане на мастър-записа
-        expect($rec = self::fetchRec($id));
-        
-        $result = (object)array(
-            'reason' => $rec->reason,
-            'valior' => $rec->valior,
-            'totalAmount' => 0,
-            'entries' => array()
-        );
-        
-        $totalAmount = 0;
-        
-        if (!empty($rec->id)) {
-            // Извличаме детайл-записите на документа. В случая просто копираме полетата, тъй-като
-            // детайл-записите на мемориалните ордери имат същата структура, каквато е и на 
-            // детайлите на журнала.
-            $query = acc_ArticleDetails::getQuery();
-            
-            while ($entry = $query->fetch("#articleId = {$rec->id}")) {
-                $debitRec = acc_Accounts::fetch($entry->debitAccId);
-                $creditRec = acc_Accounts::fetch($entry->creditAccId);
-                
-                $result->entries[] = array(
-                    'amount' => round($entry->amount, 2),
-                    
-                    'debit' => array(
-                        $debitRec->systemId,
-                        $entry->debitEnt1, // Перо 1
-                        $entry->debitEnt2, // Перо 2
-                        $entry->debitEnt3, // Перо 3
-                        'quantity' => $entry->debitQuantity,
-                    ),
-                    
-                    'credit' => array(
-                        $creditRec->systemId,
-                        $entry->creditEnt1, // Перо 1
-                        $entry->creditEnt2, // Перо 2
-                        $entry->creditEnt3, // Перо 3
-                        'quantity' => $entry->creditQuantity,
-                    ),
-                );
-                
-                if(!empty($entry->reason)){
-                	$result->entries[count($result->entries) - 1]['reason'] = $entry->reason;
-                }
-                
-                // Проверка дали трябва да се сума на движението
-                $quantityOnly = ($debitRec->type == 'passive' && $debitRec->strategy) ||
-                ($creditRec->type == 'active' && $creditRec->strategy);
-                
-                // Ако трябва да е само количество, премахваме нулевата сума
-                if($quantityOnly){
-                    unset($result->entries[count($result->entries) - 1]['amount']);
-                }
-                
-                //Добавяме сумата (ако я има) към общото
-                if(isset($result->entries[count($result->entries) - 1]['amount'])){
-                    $totalAmount += $result->entries[count($result->entries) - 1]['amount'];
-                }
-            }
-        }
-        
-        $result->totalAmount = $totalAmount;
-        
-        return $result;
-    }
-    
-    
-    /**
-     * @param int $id
-     * @return stdClass
-     * @see acc_TransactionSourceIntf::getTransaction
-     */
-    public static function finalizeTransaction($id)
-    {
-        $rec = self::fetchRec($id);
-        $rec->state = 'active';
-        
-        return self::save($rec, 'state');
-    }
     
     /****************************************************************************************
      *                                                                                      *
