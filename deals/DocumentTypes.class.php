f7e24c358265d2443d6101e68e648003cc815fbc
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-01-07 14:30:33 +0200

    Помощен клас за типове документи

---
+++ deals/DocumentTypes.class.php
@@ -0,0 +1,40 @@
+<?php
+
+
+/**
+ * Помощен клас за видове типовете документи
+ *
+ *
+ * @category  bgerp
+ * @package   deals
+ * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
+ * @copyright 2006 - 2015 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class deals_DocumentTypes
+{
+	
+	/**
+	 * Документа е вид договор
+	 */
+	const CONTRACT = 'contract';
+	
+	
+	/**
+	 * Документа е вид запитване
+	 */
+	const INQUIRY = 'inquiry';
+	
+	
+	/**
+	 * Документа е вид фактура
+	 */
+	const INVOICE = 'invoice';
+	
+	
+	/**
+	 * Стандартен документ
+	 */
+	const STANDART = 'standart';
+}
