b2ba8765c468a88c9607c762ba31af323247eece
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-01-07 18:06:48 +0200

    Спецификации:bugFix Могат да са само начало на нишка

--- techno2/SpecificationDoc.class.php
+++ techno2/SpecificationDoc.class.php
@@ -164,6 +164,12 @@ class techno2_SpecificationDoc extends core_Embedder
     
     
     /**
+     * Дали може да бъде само в началото на нишка
+     */
+    public $onlyFirstInThread = TRUE;
+    
+    
+    /**
      * Описание на модела
      */
     function description()
