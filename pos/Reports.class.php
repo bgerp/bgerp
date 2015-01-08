38a2c12e7bbcba0ed53294e8470d1732dd6f084e
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-01-08 11:22:25 +0200

    pos_Reports: bugFix, грешен редирект и име на бутон

--- pos/Reports.class.php
+++ pos/Reports.class.php
@@ -216,7 +216,7 @@ class pos_Reports extends core_Master {
     public static function on_AfterInputEditForm($mvc, &$form)
     {
     	if($form->isSubmitted()) {
-    		$draftRec = static::fetch("#state='draft' AND #pointId={$form->rec->pointId} AND #cashier={$form->rec->cashier}");
+    		$draftRec = static::fetch("#state='draft' AND #pointId={$form->rec->pointId} AND #cashier={$form->rec->cashier} AND #id != '{$form->rec->id}'");
     		if($draftRec) {
     			
     			// Ако има вече чернова с тези параметри директно я отваряме
@@ -461,6 +461,10 @@ class pos_Reports extends core_Master {
     		if(!$rec->details){
     			$mvc->extractData($rec);
     		}
+    	}
+    	
+    	if(empty($rec->id)){
+    		$rec->isContable = 'yes';
     	}
     }
     
