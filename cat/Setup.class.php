7b880af45102ae676f367b4812fa6ea481e69545
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-01-23 14:27:21 +0200

    Revert "cat_Setup: тестова миграция"
    This reverts commit 28a986732e4b46f0218e4bbf4b3f143238241a5b.

--- cat/Setup.class.php
+++ cat/Setup.class.php
@@ -58,7 +58,6 @@ class cat_Setup extends core_ProtoSetup
             'cat_Params',
             'cat_Packagings',
     		'migrate::updateProducts',
-    		'migrate::updateProducts2',
         );
 
         
@@ -146,28 +145,6 @@ class cat_Setup extends core_ProtoSetup
     	while($pRec = $pQuery->fetch()){
     		$pRec->classId = $cId;
     		cat_products_Params::save($pRec);
-    	}
-    }
-    
-    
-    /**
-     * Миграция за продуктовите драйвъри
-     */
-    function updateProducts2()
-    {
-    	$pQuery = cat_Products::getQuery();
-    	while($rec = $pQuery->fetch()){
-    		if($rec->innerClass){
-    			try{
-    				$pRec->innerForm->photo = $rec->photo;
-    				$pRec->innerState->photo = $rec->photo;
-    				 
-    				cat_Products::save($pRec);
-    			} catch(core_exception_Expect $e){
-    				
-    			}
-    			
-    		}
     	}
     }
 }
