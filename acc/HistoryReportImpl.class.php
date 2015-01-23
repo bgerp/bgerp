12d06324cf2a44474094560f854b2c42163fd4e3
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-01-23 18:05:38 +0200

    Сч. отчети: в опциите за филтриране на поле '->' заменено с '»'

--- acc/HistoryReportImpl.class.php
+++ acc/HistoryReportImpl.class.php
@@ -71,7 +71,7 @@ class acc_HistoryReportImpl extends frame_BaseDriver
 		$form->FLD('isGrouped', 'varchar', 'caption=Групиране');
 		$form->setOptions('isGrouped', array('' => '', 'yes' => 'Да', 'no' => 'Не'));
 		
-		$orderFields = ",valior=Вальор,docId=Документ,debitQuantity=Дебит->К-во,debitAmount=Дебит->Сума,creditQuantity=Кредит->К-во,creditAmount=Кредит->Сума,blQuantity=Остатък->К-во,blAmount=Остатък->Сума";
+		$orderFields = ",valior=Вальор,docId=Документ,debitQuantity=Дебит»К-во,debitAmount=Дебит»Сума,creditQuantity=Кредит»К-во,creditAmount=Кредит»Сума,blQuantity=Остатък»К-во,blAmount=Остатък»Сума";
 		
 		$form->FLD('orderField', "enum({$orderFields})", 'caption=Подредба->По,formOrder=110000');
 		$form->FLD('orderBy', 'enum(,asc=Въздходящ,desc=Низходящ)', 'caption=Подредба->Тип,formOrder=110001');
