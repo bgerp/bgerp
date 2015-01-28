c11890dddefb6219f27f1bd321ab755241b00c08
Yusein Yuseinov <yyuseinov@gmail.com>
2015-01-28 11:49:23 +0200

    needhelp_Setup - Премахване на инсталиране на мениджър и на неизползвани константи

--- needhelp/Setup.class.php
+++ needhelp/Setup.class.php
@@ -13,20 +13,6 @@ defIfNot('NEEDHELP_TYPEID', 28);
 defIfNot('NEEDHELP_INACTIVE_SECS', 15);
 
 
-/**
- * Брой показвания за един потребител
- */
-defIfNot('NEEDHELP_SHOW_LIMIT', 3);
-
-
-/**
- * След колко време да не се вземат предвид показванията
- * 6 месеца
- */
-defIfNot('NEEDHELP_SHOW_LIMIT_DATE', 15778476);
-
-
-
 /** Въпроси
  * 
  * @category  bgerp
@@ -46,18 +32,8 @@ class  needhelp_Setup extends core_ProtoSetup
 	public $configDescription = array(
 		'NEEDHELP_TYPEID' => array('int', 'caption=Номер на системата'),
 		'NEEDHELP_INACTIVE_SECS' => array('int', 'caption=След колко време трябва да се покаже прозореца за помощ->Секунди'),
-		'NEEDHELP_SHOW_LIMIT' => array('int', 'caption=Брой показвания на един потребител->Брой'),
-		'NEEDHELP_SHOW_LIMIT_DATE' => array('time(suggestions=3 месецa|6 месеца|9 месеца|1 година|2 години)', 'caption=След колко време да не се вземат предвид показванията->Време'),
 	);
-	
     
-    /**
-     * Списък с мениджърите, които съдържа пакета
-     */
-    public $managers = array(
-        'needhelp_Log',
-    );
-        
         
 	/**
 	 * Инсталиране на пакета
