cbe446086cdb55072a20a9f8be20ff0b05b05e7e
Yusein Yuseinov <yyuseinov@gmail.com>
2015-01-28 11:47:55 +0200

    needhelp_Plugin - да работи с `help_log` и да се стартира само при добавяне/редакция, ако са изпълнени и другите условия

--- needhelp/Plugin.class.php
+++ needhelp/Plugin.class.php
@@ -12,20 +12,47 @@
  */
 class needhelp_Plugin extends core_Plugin
 {    
+
     
-	public static function on_AfterRenderWrapping($mvc, &$tpl)
+    
+    /**
+     * Преди показване на форма за добавяне/промяна.
+     *
+     * @param core_Manager $mvc
+     * @param stdClass $data
+     */
+    public static function on_AfterPrepareEditForm($mvc, &$data)
     {
+        if (Request::get('ajax_mode')) return ;
+        
         $currUserId = core_Users::getCurrent();
         
         if ($currUserId <= 0) return ;
         
-        if (needhelp_Log::isShowLimitReached($currUserId)) return ;
+        if (help_Log::getDisplayMode('-1', $currUserId, FALSE) != 'open') return ;
+        
+        $data->__needHelp = TRUE;
+    }
+    
+    
+    /**
+     * 
+     * 
+     * @param core_Manager $mvc
+     * @param core_ET $res
+     * @param core_ET $tpl
+     * @param object $data
+     */
+	public static function on_AfterRenderWrapping($mvc, &$res, $tpl, $data)
+    {
+        // Ако е зададено да не се показва
+        if (!$data->__needHelp) return ;
         
     	cls::get(page_InternalFooter);
     	$baseUrl = BGERP_SUPPORT_URL;
     	$conf = core_Packs::getConfig('needhelp');
-    	$id = $conf->NEEDHELP_TYPEID;
-    	$url = core_Url::addParams($baseUrl, array('typeId' => $id));
+    	$typeId = $conf->NEEDHELP_TYPEID;
+    	$url = core_Url::addParams($baseUrl, array('typeId' => $typeId));
     	
     	if(defined('BGERP_SUPPORT_URL') && strpos(BGERP_SUPPORT_URL, '//') !== FALSE) {
     		$email = email_Inboxes::getUserEmail();
@@ -35,18 +62,21 @@ class needhelp_Plugin extends core_Plugin
     		list($user, $domain) = explode('@', $email);
     		$name = core_Users::getCurrent('names');
     		$form = new ET("<form class='needHelpForm' style='display:inline' method='post' target='_blank' onSubmit=\"prepareBugReport(this, '{$user}', '{$domain}', '{$name}');\" action='" . $url . "'></form>");
-    		$tpl->append($form);
+    		$res->append($form);
     	}
-    	$tpl->push('needhelp/lib/style.css', 'CSS');
-    	$tpl->push('needhelp/lib/script.js', 'JS');
+    	$res->push('needhelp/lib/style.css', 'CSS');
+    	$res->push('needhelp/lib/script.js', 'JS');
 
     	$inactiveTime = $conf->NEEDHELP_INACTIVE_SECS;
     	
     	$text = tr('Имате ли въпроси за') . ' <span class="logo">bgERP</span>?';
     	
-    	$url = toUrl(array('needhelp_Log', 'addToLog'), 'local');
-    	$url = urlencode($url);
+    	$closeUrl = toUrl(array('help_log', 'closeInfo', '-1'), 'local');
+    	$closeUrl = urlencode($closeUrl);
     	
-    	jquery_Jquery::run($tpl, "needHelpActions('{$text}', $inactiveTime, '{$url}');", TRUE);;
+    	$seeUrl = toUrl(array('help_log', 'see', '-1'), 'local');
+    	$seeUrl = urlencode($seeUrl);
+    	
+    	jquery_Jquery::run($res, "needHelpActions('{$text}', $inactiveTime, '{$closeUrl}', '{$seeUrl}');", TRUE);;
     }
 }
