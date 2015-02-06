9f02c33813364ca886bb61c6ad5c048beb4e73c0
Milen Georgiev <milen@experta.bg>
2015-02-06 23:46:41 +0200

    Поле defailtType на support_Systems

--- support/Systems.class.php
+++ support/Systems.class.php
@@ -149,10 +149,11 @@ class support_Systems extends core_Master
     function description()
     {
         $this->FLD('name', 'varchar', "caption=Наименование,mandatory, width=100%");
-        $this->FLD('allowedTypes', 'keylist(mvc=support_IssueTypes, select=type)', 'caption=Позволени типове, width=100%, maxColumns=3');
         $this->FLD('prototype', 'key(mvc=support_Systems, select=name, allowEmpty)', "caption=Прототип, width=100%");
         $this->FLD('description', 'richtext(rows=10,bucket=Support)', "caption=Описание");
-        
+        $this->FLD('allowedTypes', 'keylist(mvc=support_IssueTypes, select=type)', 'caption=Сигнали->Използвани, width=100%, maxColumns=3');
+        $this->FLD('defaultType', 'key(mvc=support_IssueTypes, select=type)', 'caption=Сигнали->По подразбиране');
+
         $this->setDbUnique('name');
     }
     
