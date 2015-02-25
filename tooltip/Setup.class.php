3f6ca7368035680d26aa20b222fda1d8d19d2b83
gpetrova <gpetrova@experta.bg>
2015-02-25 18:00:10 +0200

    tooltip, type, unit, zbar: Добавяне на info и version на пакетите

--- tooltip/Setup.class.php
+++ tooltip/Setup.class.php
@@ -17,6 +17,19 @@
  */
 class tooltip_Setup extends core_ProtoSetup
 {
+	
+	
+	/**
+	 * Версия на пакета
+	 */
+	public $version = '0.1';
+	
+	
+	/**
+	 * Описание на модула
+	 */
+	public $info = "Пакет за работа с tooltip - подсказки";
+	
 
 	/**
 	 * Пакет без инсталация
