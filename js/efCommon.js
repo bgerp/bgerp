a690305ab093ddaac4253ec0f87945d56a6845a4
HeBuTy <nevena@experta.bg>
2015-02-26 14:44:39 +0200

    Добавен css файл, с който ще пренаписваме стиловете на външни пакети

--- js/efCommon.js
+++ js/efCommon.js
@@ -56,6 +56,7 @@ function showTooltip(){
 };
 
 
+
 // Функция за лесно селектиране на елементи
 function get$() {
     var elements = new Array();
@@ -1230,6 +1231,11 @@ function setMinHeight() {
             pw.style.minHeight = h;
         }
     }
+    $('select').change(function(){
+	   
+	    $("#width_tmp").html($('.portal-select option:selected').text());	
+	    $(this).width($("#width_tmp").width() + 20); // 30 : the size of the down arrow of the select box
+    });
 }
 
 
