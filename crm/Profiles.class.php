eaa6079e0fb7110761a114cc26d8b233713f145d
Milen Georgiev <milen@experta.bg>
2015-03-02 13:04:30 +0200

    bugFix и добавяне на "модифицирано на" в профили

--- crm/Profiles.class.php
+++ crm/Profiles.class.php
@@ -116,10 +116,10 @@ class crm_Profiles extends core_Master
     {
         $this->FLD('userId', 'key(mvc=core_Users, select=nick)', 'caption=Потребител,mandatory,notNull');
         $this->FLD('personId', 'key(mvc=crm_Persons, select=name, group=users)', 'input=hidden,silent,caption=Визитка,mandatory,notNull');
-        $this->EXT('lastLoginTime',  'core_Users', 'externalKey=userId');
-        $this->EXT('state',  'core_Users', 'externalKey=userId');
-        $this->EXT('exState',  'core_Users', 'externalKey=userId');
-        $this->EXT('lastUsedOn',  'core_Users', 'externalKey=userId');
+        $this->EXT('lastLoginTime',  'core_Users', 'externalKey=userId,input=none');
+        $this->EXT('state',  'core_Users', 'externalKey=userId,input=none');
+        $this->EXT('exState',  'core_Users', 'externalKey=userId,input=none');
+        $this->EXT('lastUsedOn',  'core_Users', 'externalKey=userId,input=none');
 
         $this->setDbUnique('userId');
         $this->setDbUnique('personId');
@@ -156,6 +156,16 @@ class crm_Profiles extends core_Master
             // Подготвяме сингъла
             crm_Persons::prepareSingle($data->Person);
             
+            // Ако няма служебен имейл - показваме личния
+            if(!isset($data->Person->row->buzEmail) && isset($data->Person->row->email)) {
+                $data->Person->row->buzEmail = $data->Person->row->email;
+            }
+            
+            // Ако няма служебен телефон - показваме личния
+            if(!isset($data->Person->row->buzTel) && isset($data->Person->row->tel)) {
+                $data->Person->row->buzTel = $data->Person->row->tel;
+            }
+
             // Ако има права за сингъл
             if (crm_Persons::haveRightFor('single', $data->Person->rec)) {
                 
@@ -191,21 +201,22 @@ class crm_Profiles extends core_Master
                 $data->User->row->password = str_repeat('*', 7) . " " . $changePasswordLink;
                 
                 // Ако има роля admin
-                if (haveRole('admin')) {
-                    
-                    // Иконата за редактиране
-                    $img = "<img src=" . sbf('img/16/edit.png') . " width='16' height='16'>";
-                    
-                    // URL за промяна
-                    $url = array('core_Users', 'edit', $data->rec->userId, 'ret_url' => TRUE);
-                    
-                    // Създаме линка
-                    $data->User->row->editLink = ht::createLink($img, $url, FALSE, 'title=' . tr('Редактиране на потребителски данни'));  
-                }
             } else {
 
                 // Премахваме информацията, която не трябва да се вижда от другите
                 unset($data->User->row->password);
+            }
+
+            if (haveRole('admin')) {
+                    
+                // Иконата за редактиране
+                $img = "<img src=" . sbf('img/16/edit.png') . " width='16' height='16'>";
+                   
+                // URL за промяна
+                $url = array('core_Users', 'edit', $data->rec->userId, 'ret_url' => TRUE);
+                    
+                // Създаме линка
+                $data->User->row->editLink = ht::createLink($img, $url, FALSE, 'title=' . tr('Редактиране на потребителски данни'));  
             }
             
             if($data->User->rec->state != 'active') {
@@ -492,7 +503,7 @@ class crm_Profiles extends core_Master
         $addUserUrl = array(
             'core_Users', 
             'add', 
-            'personId'=>Request::get('personId', 'int'), 
+            'personId'=>Request::get('personId', 'key(mvc=core_Users)'), 
             'ret_url'=>getRetUrl()
         );
 
