5ed2a6217c81dea0af4f1d0019ee03e8ee248c85
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-03-10 11:20:01 +0200

    Качен интерфейс за дефолт цена за счетоводни операции

---
+++ acc/RegistryDefaultCostIntf.class.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+/**
+ * Интерфейс за регистри на пера, които задължително трябва да имат цена при контировката
+ *
+ *
+ * @category  bgerp
+ * @package   acc
+ * @author    Ivelin Dimov <ivelin_pdimov@abv.com>
+ * @copyright 2006 - 2015 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ * @title     Интерфейс за регистри на пера, които задължително трябва да имат цена при контировката
+ */
+class acc_RegistryDefaultCostIntf
+{
+    
+    /**
+     * Инстанция на мениджъра имащ интерфейса
+     */
+    public $class;
+    
+    
+    /**
+     * Връща дефолтната цена отговаряща на количеството
+     * 
+     * @param mixed $id - ид/запис на обекта
+     */
+    function getDefaultCost($id)
+    {
+    	$this->class->getDefaultCost($id);
+    }
+}
