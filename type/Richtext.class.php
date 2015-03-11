77c7db59fc35dcb036d66ac077ead54907cabd71
HeBuTy <nevena@experta.bg>
2015-03-11 11:03:49 +0200

    Бутон more в имейлите

--- type/Richtext.class.php
+++ type/Richtext.class.php
@@ -1029,8 +1029,8 @@ class type_Richtext extends type_Blob
 
         $id = 'hide' . rand(1, 1000000);
         
-        $html = "<a href=\"javascript:toggleDisplay('{$id}')\"  class= 'more-btn' style=\"font-weight:bold; background-image:url(" . sbf('img/16/plus.png', "'") . ");\" 
-                   class=\"linkWithIcon\">{$title}</a><div class='clearfix21 richtextHide' id='{$id}'>";
+        $html = "<a href=\"javascript:toggleDisplay('{$id}')\"  class= 'more-btn linkWithIcon' style=\"font-weight:bold; background-image:url(" . sbf('img/16/plus.png', "'") . ");\" 
+                   >{$title}</a><div class='clearfix21 richtextHide' id='{$id}'>";
         
         $this->_htmlBoard[$place] =  $html;
         
