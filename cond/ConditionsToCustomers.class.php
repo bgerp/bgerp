c43ca33be2556e40c407b77e656b097a5f88ea73
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-03-31 17:00:56 +0300

    Търговски условия: на платежните методи да се показва описанието

--- cond/ConditionsToCustomers.class.php
+++ cond/ConditionsToCustomers.class.php
@@ -104,7 +104,7 @@ class cond_ConditionsToCustomers extends core_Manager
     		if($condType == 'delCond'){
     			$form->fields['value']->type = cls::get('type_Key',array('params' => array('mvc' => 'cond_DeliveryTerms', 'select' => 'codeName', 'allowEmpty' => 'allowEmpty')));
     		} elseif($condType == 'payMethod'){
-    			$form->fields['value']->type = cls::get('type_Key', array('params' => array('mvc' => 'cond_paymentMethods', 'select' => 'name', 'allowEmpty' => 'allowEmpty')));
+    			$form->fields['value']->type = cls::get('type_Key', array('params' => array('mvc' => 'cond_paymentMethods', 'select' => 'description', 'allowEmpty' => 'allowEmpty')));
     		} else {
     			$form->fields['value']->type = cat_Params::getParamTypeClass($form->rec->conditionId, 'cond_Parameters');
     		}
