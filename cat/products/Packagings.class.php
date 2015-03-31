c6fdec8827714fe47811592cf5a7aafb1c2c90f7
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-03-31 16:19:10 +0300

    Продуктови опаковки: да не се въвеждат неположителни количества

--- cat/products/Packagings.class.php
+++ cat/products/Packagings.class.php
@@ -73,7 +73,7 @@ class cat_products_Packagings extends cat_products_Detail
     {
         $this->FLD('productId', 'key(mvc=cat_Products,select=name)', 'input=hidden, silent');
         $this->FLD('packagingId', 'key(mvc=cat_Packagings,select=name,allowEmpty)', 'input,caption=Опаковка,mandatory,width=7em');
-        $this->FLD('quantity', 'double', 'input,caption=Количество,mandatory');
+        $this->FLD('quantity', 'double(Min=0)', 'input,caption=Количество,mandatory');
         $this->FLD('isBase', 'enum(yes=Да,no=Не)', 'caption=Основна,mandatory,maxRadio=2');
         $this->FLD('netWeight', 'cat_type_Weight', 'caption=Тегло->Нето');
         $this->FLD('tareWeight', 'cat_type_Weight', 'caption=Тегло->Тара');
