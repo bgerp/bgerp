26be042171d455f7b4547d27834b4ac766c99045
gpetrova <gpetrova@experta.bg>
2015-04-06 11:37:14 +0300

    #C7000 - промяна на името от vislog_IpResources на vislog_ResourcesReport

--- vislog/ResourcesReport.class.php
+++ vislog/ResourcesReport.class.php
@@ -13,7 +13,7 @@
  * @license   GPL 3
  * @since     v 0.1
  */
-class vislog_IpResources extends frame_BaseDriver
+class vislog_ResourcesReport extends frame_BaseDriver
 {                  
     
 	
