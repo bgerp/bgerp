a46619cf817cf137decd6b4860938cf08db211c5
gpetrova <gpetrova@experta.bg>
2015-04-16 18:09:27 +0300

    #Tsk395:Задължения към доставчици

---
+++ acc/OweRrovidersReport.class.php
@@ -0,0 +1,118 @@
+<?php
+
+
+
+/**
+ * Мениджър на отчети от Задължения към доставчици
+ * Имплементация на 'frame_ReportSourceIntf' за направата на справка на баланса
+ *
+ *
+ * @category  bgerp
+ * @package   acc
+ * @author    Gabriela Petrova <gab4eto@gmail.com>
+ * @copyright 2006 - 2015 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class acc_OweRrovidersReport extends acc_BalanceReportImpl
+{
+
+
+    /**
+     * Кой може да избира драйвъра
+     */
+    public $canSelectSource = 'ceo, acc';
+
+
+    /**
+     * Заглавие
+     */
+    public $title = 'Счетоводство->Задължения към доставчици';
+
+
+    /**
+     * Дефолт сметка
+     */
+    public $accountSysId = '401';
+
+
+    /**
+     * След подготовката на ембеднатата форма
+     */
+    public static function on_AfterAddEmbeddedFields($mvc, core_Form &$form)
+    {
+
+        // Искаме да покажим оборотната ведомост за сметката на касите
+        $accId = acc_Accounts::getRecBySystemId($mvc->accountSysId)->id;
+        $form->setDefault('accountId', $accId);
+        $form->setHidden('accountId');
+
+        // Дефолт периода е текущия ден
+        $today = dt::today();
+
+        $form->setDefault('from',date('Y-m-01', strtotime("-1 months", dt::mysql2timestamp(dt::now()))));
+        $form->setDefault('to', $today);
+
+        // Задаваме че ще филтрираме по перо
+        $form->setDefault('action', 'group');
+        $form->setHidden('orderField');
+        $form->setHidden('orderBy');
+    }
+
+
+    /**
+     * След подготовката на ембеднатата форма
+     */
+    public static function on_AfterPrepareEmbeddedForm($mvc, core_Form &$form)
+    {
+        $form->setHidden('action');
+
+        foreach (range(1, 3) as $i) {
+
+            $form->setHidden("feat{$i}");
+            $form->setHidden("grouping{$i}");
+
+        }
+
+        $articlePositionId = acc_Lists::getPosition($mvc->accountSysId, 'crm_ContragentAccRegIntf');
+
+        $form->setDefault("feat{$articlePositionId}", "*");
+    }
+
+
+    public static function on_AfterGetReportLayout($mvc, &$tpl)
+    {
+        $tpl->removeBlock('action');
+    }
+
+
+    public static function on_AfterPrepareListFields($mvc, &$res, &$data)
+    {
+
+    }
+
+
+    /**
+     * Скрива полетата, които потребител с ниски права не може да вижда
+     *
+     * @param stdClass $data
+     */
+    public function hidePriceFields()
+    {
+        $innerState = &$this->innerState;
+
+        unset($innerState->recs);
+    }
+
+
+    /**
+     * Коя е най-ранната дата на която може да се активира документа
+     */
+    public function getEarlyActivation()
+    {
+        $activateOn = "{$this->innerForm->to} 23:59:59";
+
+        return $activateOn;
+    }
+
+}
