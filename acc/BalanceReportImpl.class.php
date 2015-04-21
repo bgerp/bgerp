11f5faa0bd2d42783d90c8951a2d2641a2ea9f2c
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-04-21 15:03:45 +0300

    Отчет за обоиротна ведомост: предефиниране на функция

--- acc/BalanceReportImpl.class.php
+++ acc/BalanceReportImpl.class.php
@@ -129,6 +129,24 @@ class acc_BalanceReportImpl extends frame_BaseDriver
     
     
     /**
+     * Рендира вътрешната форма като статична форма в подадения шаблон
+     *
+     * @param core_ET $tpl - шаблон
+     * @param string $placeholder - плейсхолдър
+     */
+    protected function prependStaticForm(core_ET &$tpl, $placeholder = NULL)
+    {
+    	$form = cls::get('core_Form');
+    
+    	$this->addEmbeddedFields($form);
+    	$form->rec = $this->innerForm;
+    	$form->class = 'simpleForm';
+    		
+    	$tpl->prepend($form->renderStaticHtml(), $placeholder);
+    }
+    
+    
+    /**
      * Проверява въведените данни
      *
      * @param core_Form $form
