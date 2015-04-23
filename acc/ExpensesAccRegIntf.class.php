3c4e82a09641c386b53e5255886b46a8c4368a99
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-04-23 14:04:55 +0300

    Добавен интерфейс за пера които моигат да бъдат разходи

---
+++ acc/ExpensesAccRegIntf.class.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+/**
+ * Интерфейс за пера - Разходи
+ *
+ *
+ * @category  bgerp
+ * @package   acc
+ * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
+ * @copyright 2006 - 2015 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ * @title     Интерфейс за пера разходи
+ */
+class acc_ExpensesAccRegIntf extends acc_RegisterIntf
+{
+}
