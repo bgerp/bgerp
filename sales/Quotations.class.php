59b00e3313b2c9d94c24feed5ffb5e02d5274623
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-05-11 16:15:48 +0300

    Оферти:BugFix

--- sales/Quotations.class.php
+++ sales/Quotations.class.php
@@ -413,6 +413,8 @@ class sales_Quotations extends core_Master
     			
     		$row->number = $mvc->getHandle($rec->id);
     		$row->username = core_Users::recToVerbal(core_Users::fetch($rec->createdBy), 'names')->names;
+    		$row->username = tr(core_Lg::transliterate($row->username));
+    		
     		$profRec = crm_Profiles::fetchRec("#userId = {$rec->createdBy}");
     		if($position = crm_Persons::fetchField($profRec->personId, 'buzPosition')){
     			$row->position = cls::get('type_Varchar')->toVerbal($position);
@@ -422,7 +424,8 @@ class sales_Quotations extends core_Master
     			
     		$Varchar = cls::get('type_Varchar');
     		$row->MyCompany = $Varchar->toVerbal($ownCompanyData->company);
-    			
+    		$row->MyCompany = tr(core_Lg::transliterate($row->MyCompany));
+    		
     		$contragent = new core_ObjectReference($rec->contragentClassId, $rec->contragentId);
     		$cData = $contragent->getContragentData();
     			
