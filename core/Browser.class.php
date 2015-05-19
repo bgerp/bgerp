7afbb353c8b4b52bf6a75b60c9b7a743eb841856
Yusein Yuseinov <yyuseinov@gmail.com>
2015-05-19 15:00:49 +0300

    core_Browser bugFix `brid` кукито да е валиден за целия домейн

--- core/Browser.class.php
+++ core/Browser.class.php
@@ -375,7 +375,7 @@ class core_Browser extends core_Master
         
         // Добавяме хеш към brid и записваме в кукитата
         $bridHash = str::addHash($brid, self::HASH_LENGTH, $bridSalt);
-        setcookie(self::BRID_NAME, $bridHash, time() + $conf->CORE_COOKIE_LIFETIME);
+        setcookie(self::BRID_NAME, $bridHash, time() + $conf->CORE_COOKIE_LIFETIME, '/');
     }
     
     
