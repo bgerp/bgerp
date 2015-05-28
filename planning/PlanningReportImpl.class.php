8f2feda6dbabc2c6f35b1179c24fdcdccf2208f9
gpetrova <gpetrova@experta.bg>
2015-05-28 17:05:36 +0300

    Ако всичко е доставено или произведено, то записа не е интересен за отчета

--- planning/PlanningReportImpl.class.php
+++ planning/PlanningReportImpl.class.php
@@ -58,9 +58,6 @@ class planning_PlanningReportImpl extends frame_BaseDriver
     	$form->FLD('from', 'date', 'caption=Начало');
     	$form->FLD('to', 'date', 'caption=Край');
     
-    	//$form->FLD('orderField', "enum(,ent1Id=Перо 1,ent2Id=Перо 2,ent3Id=Перо 3,baseQuantity=К-во»Начално,baseAmount=Сума»Начална,debitQuantity=К-во»Дебит,debitAmount=Сума»Дебит,creditQuantity=К-во»Кредит,creditAmount=Сума»Кредит,blQuantity=К-во»Крайно,blAmount=Сума»Крайна)", 'caption=Подредба->По,formOrder=110000');
-    	//$form->FLD('orderBy', 'enum(,asc=Въздходящ,desc=Низходящ)', 'caption=Подредба->Тип,formOrder=110001');
-    
     	$this->invoke('AfterAddEmbeddedFields', array($form));
     }
     
@@ -112,8 +109,6 @@ class planning_PlanningReportImpl extends frame_BaseDriver
         
         $this->prepareListFields($data);
         
-        //$query->where("#state = 'active' AND (#createdOn >= '{$this->innerForm->from}' AND #createdOn <= '{$this->innerForm->to}')");
-        //$queryJob->where("#state = 'active' AND (#createdOn >= '{$this->innerForm->from}' AND #createdOn <= '{$this->innerForm->to}')");
         $query->where("#state = 'active' ");
         $queryJob->where("#state = 'active'");
         
@@ -123,7 +118,7 @@ class planning_PlanningReportImpl extends frame_BaseDriver
         	//$origin = doc_Threads::getFirstDocument($rec->threadId);
         	// взимаме информация за сделките
         	//$dealInfo = $origin->getAggregateDealInfo();
-        	//bp($rec);
+
         	if ($rec->deliveryTime) {
         		$date = $rec->deliveryTime;
         	} else {
@@ -146,7 +141,6 @@ class planning_PlanningReportImpl extends frame_BaseDriver
         if(is_array($products)){
 	    	foreach($products as $product) {
 	    		// правим индекс "класа на продукта|ид на продукта"
-	        	//$index = "$product->classId" .'|'. "$product->productId";
 	        	$index = $product->productId;
 	        		
 	        	if($product->deliveryTime) {
@@ -154,6 +148,9 @@ class planning_PlanningReportImpl extends frame_BaseDriver
 	        	} else {
 	        		$date = $rec->valior;
 	        	}
+	        	
+	        	if ((abs($product->quantityDelivered - $product->quantity) == 0)) continue;
+	        	
 		        	
 	        	// ако нямаме такъв запис,
 	        	// го добавяме в масив
@@ -182,6 +179,7 @@ class planning_PlanningReportImpl extends frame_BaseDriver
         while ($recJobs = $queryJob->fetch()) {
         	$indexJ = $recJobs->productId;
         	 
+        	if ((abs($recJobs->quantityProduced - $recJobs->quantity) == 0)) continue;
         	// ако нямаме такъв запис,
         	// го добавяме в масив
         	if(!array_key_exists($indexJ, $data->recs)){
@@ -224,13 +222,10 @@ class planning_PlanningReportImpl extends frame_BaseDriver
             $count = 0;
             
             foreach ($data->recs as $id => $rec){
-            	//bp($rec);
-                if ((abs($rec->quantityDelivered - $rec->quantity) != 0) || (abs($rec->quantityProduced - $rec->quantityJob) != 0)) {
-                if(!$pager->isOnPage()) continue;
+				if(!$pager->isOnPage()) continue;
                 
                 $row = $mvc->getVerbal($rec);
                 $data->rows[$id] = $row;
-                }
             }
         }
         
