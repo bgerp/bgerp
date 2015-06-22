600df545009f203e6700117a4d9b9d0e556a168e
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-06-22 11:40:04 +0300

    Локации и маршрути: BugFix #Sig1167 с форсирането на папката на контрагента

--- crm/Locations.class.php
+++ crm/Locations.class.php
@@ -323,10 +323,25 @@ class crm_Locations extends core_Master {
     		}
     		
     		if(sales_Sales::haveRightFor('add')){
-    			$folderId = cls::get($rec->contragentCls)->forceCoverAndFolder($rec->contragentId);
-    			$data->toolbar->addBtn('Продажба', array('sales_Sales', 'add', 'folderId' => $folderId, 'deliveryLocationId' => $rec->id), 'ef_icon=img/16/cart_go.png,title=Създаване на нова продажба');
+    			$data->toolbar->addBtn('Продажба', array($mvc, 'createSale', $rec->id), 'ef_icon=img/16/cart_go.png,title=Създаване на нова продажба');
     		}
     	}
+    }
+    
+    
+    /**
+     * Екшън за създаване на нова продажба
+     */
+    function act_CreateSale()
+    {
+    	sales_Sales::requireRightFor('add');
+    	$id = Request::get('id', 'key(mvc=crm_Locations)');
+    	$rec = $this->fetch($id);
+    	
+    	// Форсираме папката на контрагента
+    	$folderId = cls::get($rec->contragentCls)->forceCoverAndFolder($rec->contragentId);
+    	
+    	redirect(array('sales_Sales', 'add', 'folderId' => $folderId, 'deliveryLocationId' => $id));
     }
     
     
