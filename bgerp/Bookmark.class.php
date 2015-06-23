54c1e1a58c3b42aa55ba8074ee7b2bc86b407c77
nevena <nevena@experta.bg>
2015-06-23 13:43:05 +0300

    bugFix: големина на иконка

--- bgerp/Bookmark.class.php
+++ bgerp/Bookmark.class.php
@@ -123,7 +123,7 @@ class bgerp_Bookmark extends core_Manager
             $attr = array();
             $attr['onclick'] = "addParamsToBookmarkBtn(this, '{$sUrl}', '{$localUrl}'); return ;";
 
-            $img =  ht::createElement('img', array('src' => sbf('img/bookmark-add.png', ''), 'title' => tr('Добавяне'), 'class' => 'bookmarkLink'));
+            $img =  ht::createElement('img', array('src' => sbf('img/bookmark-add.png', ''), 'title' => tr('Добавяне'), 'class' => 'bookmarkLink', 'width' => 16, 'height' => 16));
             $tpl = ht::createLink($img, $url, FALSE, $attr);
         }
         
