650ff9426ab3afbbdbf8e74b116f100a8b1e538c
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-06-24 17:14:23 +0300

    type_Set:bugFix да се превеждат стойностите

--- type/Set.class.php
+++ type/Set.class.php
@@ -41,7 +41,7 @@ class type_Set extends core_Type {
         
         foreach($vals as $v) {
             if($v) {
-                $res .= ($res ? ", " : '') . $this->getVerbal($v);
+                $res .= ($res ? ", " : '') . tr($this->getVerbal($v));
             }
         }
         
