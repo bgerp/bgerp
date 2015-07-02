ac11e0a21b182ce05966941e1430b422876bcb54
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-07-02 16:11:25 +0300

    cat_Wrapper: таба кеш да се показва само на админ и Ceo

--- cat/Wrapper.class.php
+++ cat/Wrapper.class.php
@@ -32,7 +32,7 @@ class cat_Wrapper extends plg_ProtoWrapper
         $this->TAB('cat_Packagings', 'Опаковки', 'ceo,user');
         $this->TAB('cat_Params', 'Параметри', 'ceo,user');
         $this->TAB('cat_UoM', 'Мерки', 'ceo,user');
-        $this->TAB('cat_ProductTplCache', 'Кеш', 'ceo,cat');
+        $this->TAB('cat_ProductTplCache', 'Кеш', 'ceo,admin');
         
         $this->title = 'Продукти';
     }
