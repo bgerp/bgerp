7e58337d20c75d7d03502c65758091c63c043d4c
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-07-08 10:45:03 +0300

    cat_Setup: добавено начало на миграционна функция

--- cat/Setup.class.php
+++ cat/Setup.class.php
@@ -272,4 +272,163 @@ class cat_Setup extends core_ProtoSetup
     		}
     	}
     }
+    
+    
+    public function replaceResources()
+    {
+    	if(!cat_Products::count()) return;
+    	
+    	$pClassId = cat_Products::getClassId();
+    	$rClass = planning_Resources::getClassId();
+    	
+    	$oQuery = planning_ObjectResources::getQuery();
+    	$oQuery->groupBy('resourceId');
+    	$oQuery->where("#classId = {$pClassId}");
+    	$map = array();
+    	while($oRec = $oQuery->fetch()){
+    		$map[$oRec->resourceId] = $oRec->objectId;
+    	}
+    	
+    	if(!count($map)) return;
+    	
+    	$bomQuery = cat_BomDetails::getQuery();
+    	while ($bomRec = $bomQuery->fetch()){
+    		$bomRec->resourceId = $map[$bomRec->resourceId];
+    		//cat_BomDetails::save($bomRec, 'resourceId');
+    		
+    		
+    		
+    	}
+    	
+    	
+    	$itemsQuery = acc_Items::getQuery();
+    	$itemsQuery->where("#classId = {$rClass}");
+    	$itemMap = array();
+    	
+    	while($iRec = $itemsQuery->fetch()){
+    		if(isset($map[$iRec->objectId])){
+    			if($productItem = acc_Items::fetchItem($pClassId, $map[$iRec->objectId])){
+    				$itemMap[$iRec->id] = $productItem->id;
+    			}
+    		} else {
+    			$sysId = planning_Resources::fetchField($iRec->objectId, 'systemId');
+    			unset($pId);
+    			switch($sysId){
+    				case 'commonLabor':
+    					$pId = cat_Products::fetchField("#code = 'labor'", 'id');
+    					break;
+    				case 'commonMaterial':
+    					$pId = cat_Products::fetchField("#code = 'materials'", 'id');
+    					break;
+    				case 'commonService':
+    					$pId = cat_Products::fetchField("#code = 'services'", 'id');
+    					break;
+    				case 'commonEquipment':
+    					$pId = cat_Products::fetchField("#code = 'fixedAssets'", 'id');
+    					break;
+    			}
+    			$itemMap[$iRec->id] = acc_Items::fetchItem($pClassId, $pId)->id;
+    		}
+    		
+    		if($iRec->state != 'closed'){
+    			$iRec->state = 'closed';
+    			acc_Items::save($iRec);
+    		}
+    	}
+    	
+    	$replaceIds = array_keys($itemMap);
+    	
+    	$bQuery = acc_BalanceDetails::getQuery();
+    	$bQuery->in('ent1Id', $replaceIds);
+    	$bQuery->in('ent2Id', $replaceIds, FALSE, TRUE);
+    	$bQuery->in('ent3Id', $replaceIds, FALSE, TRUE);
+    	
+    	while($bRec = $bQuery->fetch()){
+    		foreach (array('ent1Id', 'ent2Id', 'ent3Id') as $fld){
+    			if(isset($itemMap[$bRec->$fld])){
+    				$bRec->$fld = $itemMap[$bRec->$fld];
+    			}
+    		}
+    		
+    		try{
+    			acc_BalanceDetails::save($bRec);
+    		} catch(core_exception_Expect $e){
+    			
+    		}
+    	}
+    	
+    	$jQuery = acc_JournalDetails::getQuery();
+    	
+    	$jQuery->in('debitItem1', $replaceIds);
+    	$jQuery->in('debitItem2', $replaceIds, FALSE, TRUE);
+    	$jQuery->in('debitItem3', $replaceIds, FALSE, TRUE);
+    	$jQuery->in('creditItem1', $replaceIds, FALSE, TRUE);
+    	$jQuery->in('creditItem2', $replaceIds, FALSE, TRUE);
+    	$jQuery->in('creditItem3', $replaceIds, FALSE, TRUE);
+    	
+    	while($jRec = $jQuery->fetch()){
+    		foreach (array('debitItem1', 'debitItem2', 'debitItem3', 'creditItem1', 'creditItem2', 'creditItem3') as $fld){
+    			if(isset($itemMap[$jRec->$fld])){
+    				$jRec->$fld = $itemMap[$jRec->$fld];
+    			}
+    		}
+    		
+    		try{
+    			acc_JournalDetails::save($jRec);
+    		} catch(core_exception_Expect $e){
+    			
+    		}
+    	}
+    	
+    	$mQuery = acc_ArticleDetails::getQuery();
+    	$mQuery->in('debitEnt1', $replaceIds);
+    	$mQuery->in('debitEnt2', $replaceIds, FALSE, TRUE);
+    	$mQuery->in('debitEnt3', $replaceIds, FALSE, TRUE);
+    	$mQuery->in('creditEnt1', $replaceIds, FALSE, TRUE);
+    	$mQuery->in('creditEnt2', $replaceIds, FALSE, TRUE);
+    	$mQuery->in('creditEnt3', $replaceIds, FALSE, TRUE);
+    	
+    	while($mRec = $mQuery->fetch()){
+    		foreach (array('debitEnt1', 'debitEnt2', 'debitEnt3', 'creditEnt1', 'creditEnt2', 'creditEnt3') as $fld){
+    			if(isset($itemMap[$mRec->$fld])){
+    				$mRec->$fld = $itemMap[$mRec->$fld];
+    			}
+    		}
+    		
+    		try{
+    			acc_ArticleDetails::save($mRec);
+    		} catch(core_exception_Expect $e){
+    			
+    		}
+    	}
+    	
+    	$tQuery = acc_BalanceTransfers::getQuery();
+    	$tQuery->in('fromEnt1Id', $replaceIds);
+    	$tQuery->in('fromEnt2Id', $replaceIds, FALSE, TRUE);
+    	$tQuery->in('fromEnt3Id', $replaceIds, FALSE, TRUE);
+    	$tQuery->in('toEnt1Id', $replaceIds, FALSE, TRUE);
+    	$tQuery->in('toEnt2Id', $replaceIds, FALSE, TRUE);
+    	$tQuery->in('toEnt3Id', $replaceIds, FALSE, TRUE);
+    	
+    	while($tRec = $tQuery->fetch()){
+    		foreach (array('fromEnt1Id', 'fromEnt2Id', 'fromEnt3Id', 'toEnt1Id', 'toEnt3Id', 'toEnt3Id') as $fld){
+    			if(isset($itemMap[$tRec->$fld])){
+    				$tRec->$fld = $itemMap[$tRec->$fld];
+    			}
+    		}
+    	
+    		try{
+    			acc_BalanceTransfers::save($tRec);
+    		} catch(core_exception_Expect $e){
+    			
+    		}
+    	}
+    	bp($tQuery->fetchAll());
+    	
+    	//bp($jQuery->fetchAll());
+    	
+    	bp($replaceIds,$bQuery->fetchAll());
+    	
+    	bp($itemMap,acc_items::fetch(5687), acc_Items::fetch(138));
+    }
 }
