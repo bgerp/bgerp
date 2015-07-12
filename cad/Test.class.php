43a6bedab45477442bf37435a3c54cd0a4017ed7
Milen Georgiev <milen@experta.bg>
2015-07-12 21:30:29 +0300

    SVG cad testove

--- cad/Test.class.php
+++ cad/Test.class.php
@@ -27,6 +27,7 @@ class cad_Test {
         $form->FLD('y', 'float', 'caption=Y');
         $form->FLD('w', 'float', 'caption=Широчина');
         $form->FLD('h', 'float', 'caption=Височина');
+        $form->FLD('g', 'float', 'caption=Фалт');
 
         $form->FLD('stroke', 'color_Type', 'caption=Молив->Цвят');
         $form->FLD('strokeWidth', 'float', 'caption=Молив->Размер,suggestions=0.1|0.2|0.3|0.4|0.5|0.6|0.7|0.8|0.9|1');
@@ -50,14 +51,29 @@ class cad_Test {
             'stroke-width' => $strokeWidth, 
             'fill-opacity' => $opacity)
             );
+        
+        if(!$g) {
+            $g = round($w * 2/3.5) / 2 - 10;
+        }
+
+        $r = 20;
 
         $canvas->moveTo($x, $y, TRUE);
         
-        cad_Rectangle::draw($canvas, $p);
-        $p['r'] = min($p['w'], $p['h']) / 4;
-        $p['fill'] = '#cfd';
-        //$p['notStartNewPath'] = TRUE;
-        cad_Circle::draw($canvas, $p);
+        $canvas->lineTo($g, 0);
+
+        $canvas->lineTo(0, 150 - $r);
+        $canvas->roundTo(0, $r, $r, $r, $r);
+
+        $canvas->lineTo($w - 2 * $g - $r, 0);
+        $canvas->lineTo(0, -150);
+        
+
+        $canvas->lineTo($g, 0);
+        $canvas->lineTo(0, $h);
+        $canvas->lineTo(-$w, 0);
+        $canvas->lineTo(0, -$h);
+
     }
     
 }
