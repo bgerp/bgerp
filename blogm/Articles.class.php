f283bdfe4935e47296f2d78b913c9509e536af01
Milen Georgiev <milen@experta.bg>
2015-07-14 21:36:44 +0300

    Надпис: Все още няма статии в тази категория

--- blogm/Articles.class.php
+++ blogm/Articles.class.php
@@ -702,8 +702,9 @@ class blogm_Articles extends core_Master {
 
    			$data->title = tr('Статии в') .  '&nbsp;"<b>' . blogm_Categories::getVerbal($catRec, 'title') . '</b>"';
             $data->descr = blogm_Categories::getVerbal($catRec, 'description');
-
-
+            if(!count($data->rows)) {
+                $data->descr .= "<p><b style='color:#666;'>" . tr('Все още няма статии в тази категория') . '</b></p>';
+            }
         } else {
             $data->title = cms_Content::getLang() == 'bg' ? 'Всички статии в блога' : 'All Articles in the Blog';
             if(!count($data->rows)) {
