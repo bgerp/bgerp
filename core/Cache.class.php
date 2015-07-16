373f3cfdd7436d88a2e14ecc86444e6802f3dab1
Yusein Yuseinov <yyuseinov@gmail.com>
2015-07-16 10:03:27 +0300

    core_Cache - използване на новия лог

--- core/Cache.class.php
+++ core/Cache.class.php
@@ -110,7 +110,7 @@ class core_Cache extends core_Manager
                 if($data->dHash != $dHash) {
                     $Cache->deleteData($key);
                     
-                    Debug::log("Cache::get $type, $handler - other models are changed, no success");
+                    self::logDebug("get $type, $handler - other models are changed, no success");
                     
                     return FALSE;
                 }
@@ -118,12 +118,12 @@ class core_Cache extends core_Manager
             
             // Увеличаваме времето на валидността на данните ????
             
-            Debug::log("Cache::get $type, $handler - success");
+            self::logDebug("get $type, $handler - success");
             
             return $data->value;
         }
         
-        Debug::log("Cache::get $type, $handler - no exists");
+        self::logDebug("get $type, $handler - no exists");
         
         return FALSE;
     }
@@ -136,7 +136,7 @@ class core_Cache extends core_Manager
     {
         $Cache = cls::get('core_Cache');
         
-        Debug::log("Cache::set $type, $handler");
+        self::logDebug("set $type, $handler");
         
         if (!$handler) {
             $handler = md5(json_encode($value));
