cb00b5cb03d9cf27d124b3eaa0bda30386713e11
Milen Georgiev <milen@experta.bg>
2015-07-26 23:45:23 +0300

    Показане на паролите и в sens

--- sens/driver/TCW122B.class.php
+++ sens/driver/TCW122B.class.php
@@ -67,7 +67,7 @@ class sens_driver_TCW122B extends sens_driver_IpDevice
         $form->FNC('ip', 'ip', 'caption=IP,hint=Въведете IP адреса на устройството, input, mandatory');
         $form->FNC('port', 'int(5)', 'caption=Port,hint=Порт, input, mandatory,value=80');
         $form->FNC('user', 'varchar(10)', 'caption=User,hint=Потребител, input, mandatory, value=admin, notNull');
-        $form->FNC('password', 'password(allowEmpty,autocomplete=off)', 'caption=Password,hint=Парола, input, value=admin, notNull');
+        $form->FNC('password', 'password(show)', 'caption=Password,hint=Парола, input, value=admin, notNull');
         
         // Добавя и стандартните параметри
         $this->getSettingsForm($form);
