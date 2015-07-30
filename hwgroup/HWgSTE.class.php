6693beb52732c03b5cc1e4c9cffce0c841f447d3
mitko <mitko@experta.bg>
2015-07-30 15:22:12 +0300

    hwgroup driver bugfix

--- hwgroup/HWgSTE.class.php
+++ hwgroup/HWgSTE.class.php
@@ -77,7 +77,7 @@ class hwgroup_HWgSTE extends sens2_ProtoDriver
         
         $parsed = $res = array();
         
-        core_XML::toArrayFlat(simplexml_load_string($xml), $parsed);
+        core_Xml::toArrayFlat(simplexml_load_string($xml), $parsed);
         
         foreach ($this->params as $param => $details) {
             $res[$param] = $parsed[$details['xmlPath']];
