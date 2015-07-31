381447bbf7058f4b89c55fc077cdbf4efa82b33e
Donyka <donyka@experta.bg>
2015-07-31 11:27:14 +0300

    изтрити патерни за залепяне

--- cad/Lib.class.php
+++ cad/Lib.class.php
@@ -68,9 +68,6 @@ class cad_Lib {
     
     /**
      * Създава молив - пунктир
-     * 
-     * @param unknown $canvas
-     * @param array $p
      */
     static function getInnerlinePen($canvas, $p)
     {
@@ -92,6 +89,10 @@ class cad_Lib {
     	);
     }
     
+    
+    /**
+     * Създава молив - патерн за залепяне
+     */
     static function getPatternLine($canvas, $p)
     {
     	extract($p);
@@ -113,6 +114,9 @@ class cad_Lib {
     }
     
     
+    /**
+     * Създава молив - прегъване
+     */
     static function getFoldingLine($canvas, $p)
     {
     	extract($p);
@@ -133,6 +137,9 @@ class cad_Lib {
     }
     
     
+    /**
+     * Създава молив - измерителни линии
+     */
     static function getMeasureLine($canvas, $p)
     {
     	extract ($p);
@@ -146,119 +153,5 @@ class cad_Lib {
     					'stroke' => $strokeColor,
     					'stroke-width' => $strokeWidth)
     	);
-    }
-    
-    
-    /**
-     * Изчертава тип за залепяне - стрела
-     * 
-     * @param unknown $canvas
-     * @param array $p
-     * @param float $handleH
-     * @param float $handleW
-     * @param float $handleS
-     */
-    static function drawArrow($canvas, $p = array())
-    {
-    	extract($p);
-    /*	const EP_HANDLE_HEIGHT = 80; // от горна външна линия до ръба на торбичката
-    	const EP_HANDLE_WIDTH = 140; // от лява външна линия до дясна външна линия
-    	const EP_HANDLE_SIZE = 30; */
-    	
-    	$handleH = 80;
-    	$handleW= 140;
-    	$handleS = 30;
-    	
-    	$innerH = self::EP_HANDLE_INNER_HEIGHT;
-    	$innerSpace = self::EP_INNER_SPACE;
-    	$arrowH = self::EP_ARROW_HEIGHT;
-    	$arrowW = self::EP_ARROW_WIDTH;
-    	$arrowS = self::EP_ARROW_SIZE;
-    	
-    	self::getPatternLine($canvas, $p);
-    	
-    	$br=0;
-    	for ($i=0; $i<3; $i++)
-    	{
-    		$canvas->moveTo($x+$w/2-$handleW/2+$handleS/2-$arrowW/2, $y+$handleH+$innerSpace+$arrowH, TRUE);
-    		$canvas->lineTo($arrowW/2-$arrowS, -$arrowH+$arrowS);
-    		
-    		
-    		$canvas->roundTo($arrowS, -$arrowS/2, 2*$arrowS, 0, $arrowS/2);
-    		$canvas->lineTo($arrowW/2-$arrowS, $arrowH-$arrowS);
-    		
-    		
-    		$canvas->lineTo(0, $arrowS);
-    		$canvas->lineTo(-($arrowW/2-$arrowS), -($arrowH-$arrowS));
-    		$canvas->roundTo(-$arrowS, -$arrowS/2, -2*$arrowS, 0, $arrowS/2);
-    		
-    		$canvas->lineTo(-($arrowW/2-$arrowS), ($arrowH-$arrowS));
-    		
-    		$canvas->closePath();
-    		
-    		$y += $arrowH;
-    		
-    	}
-    	
-    }
-    
-    
-    /**
-     * Чертае тип залепяне - овал
-     * 
-     * @param unknown $canvas
-     * @param array $p
-     * @param float $handleH
-     * @param float $handleW
-     * @param float $handleS
-     */
-    static function drawOval($canvas, $p = array())
-    {
-    	extract($p);
-    	 
-
-    	$handleH = 80;
-    	$handleW= 140;
-    	$handleS = 30;
-    	
-    	$innerH = self::EP_HANDLE_INNER_HEIGHT;
-    	$innerSpace = self::EP_INNER_SPACE;
-    	$ovalH = self::EP_OVAL_HEIGHT;
-    	$ovalW = self::EP_OVAL_WIDTH;
-    	$ovalInnerH = self::EP_OVAL_INNER_HEIGHT;
-    	$ovalInnerW = self::EP_OVAL_INNER_WIDTH;
-    	
-    	
-    	self:: getPatternLine($canvas, $p);
-    	
-    	$canvas->moveTo($x+$w/2-$handleW/2+$handleS/2-$ovalW/2, $y+$handleH+$innerSpace+$ovalH, TRUE);
-    	$canvas->lineTo(0, -($ovalH/2-$ovalW/2));
-    	$canvas->roundTo(0, -$ovalW/2, $ovalW/2, -$ovalW/2, $ovalW/2);
-    	$canvas->roundTo($ovalW/2, 0, $ovalW/2, $ovalW/2, $ovalW/2);
-    	$canvas->lineTo(0, $ovalH/2-$ovalW/2);
-    	$canvas->roundTo(0, $ovalW/2, -$ovalW/2, $ovalW/2, $ovalW/2);
-    	$canvas->roundTo(-$ovalW/2, 0, -$ovalW/2, -$ovalW/2, $ovalW/2);
-    	
-    	$canvas->moveTo($x+$w/2-$handleW/2+$handleS/2-$ovalInnerW/2, $y+$handleH+$innerSpace+$ovalH, TRUE);
-    	$canvas->lineTo(0, -($ovalInnerH/2-$ovalInnerW/2));
-    	
-    	
-    	/*
-    	$canvas->roundTo($ovalW/2, 0, $ovalW/2, $ovalW/2, $ovalW/2);    	
-    	$canvas->roundTo($ovalW/2, 0, $ovalW/2, $ovalW/2, $ovalW/2);
-    	
-    	
-    	$canvas->roundTo(-$ovalW/2, 0, -$ovalW/2, -$ovalW/2, $ovalW/2);
-    	$canvas->lineTo(0, -($ovalH/2-$ovalW/2));
-    	
-    	$canvas->moveTo($x+$w/2-$handleW/2+$handleS/2-$ovalInnerW/2, $y+$handleH+$innerSpace+$ovalInnerH, TRUE);
-    	$canvas->lineTo(0, -($ovalInnerH/2-$ovalInnerW/2));
-    	$canvas->roundTo(0, -$ovalInnerW/2, $ovalInnerW/2, -$ovalInnerW/2, $ovalInnerW/2);
-    	$canvas->roundTo($ovalInnerW/2, 0, $ovalInnerW/2, $ovalInnerW/2, $ovalInnerW/2);
-    	$canvas->lineTo(0, $ovalInnerH-2*$ovalInnerW);
-    	$canvas->roundTo(0, $ovalInnerW/2, -$ovalInnerW/2, $ovalInnerW/2, $ovalInnerW/2);
-    	$canvas->roundTo(-$ovalInnerW/2, 0, -$ovalInnerW/2, -$ovalInnerW/2, $ovalInnerW/2);
-    	$canvas->lineTo(0, -($ovalInnerH/2-$ovalInnerW/2));
-    */
     }
 }
