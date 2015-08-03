4e68143e728f604a533714f68ccf3a08850bcd4e
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-08-03 12:55:32 +0300

    Задания: премахната излишна проверка

--- planning/Jobs.class.php
+++ planning/Jobs.class.php
@@ -295,11 +295,9 @@ class planning_Jobs extends core_Master
     	}
 
     	// Бутон за добавяне на документ за бързо производство
-    	if($rec->state != 'draft' && $rec->state != 'rejected'){
-    		 if(planning_DirectProductionNote::haveRightFor('add', (object)array('originId' => $rec->containerId))){
-    			 $pUrl = array('planning_DirectProductionNote', 'add', 'originId' => $rec->containerId, 'ret_url' => TRUE);
-    			 $data->toolbar->addBtn("Производство", $pUrl, 'ef_icon = img/16/page_paste.png,title=Създаване на протокол за бързо производство от заданието');
-    		}
+    	if(planning_DirectProductionNote::haveRightFor('add', (object)array('originId' => $rec->containerId))){
+    		 $pUrl = array('planning_DirectProductionNote', 'add', 'originId' => $rec->containerId, 'ret_url' => TRUE);
+    		 $data->toolbar->addBtn("Производство", $pUrl, 'ef_icon = img/16/page_paste.png,title=Създаване на протокол за бързо производство от заданието');
     	}
     }
     
