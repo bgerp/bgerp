54abf2a44371f4141a6a21560263d57216aa2b73
Yusein Yuseinov <yyuseinov@gmail.com>
2015-08-05 11:29:06 +0300

    fileman_webdrv_Svg - възможност за конвертиране към `svg`

--- fileman/webdrv/Svg.class.php
+++ fileman/webdrv/Svg.class.php
@@ -14,4 +14,53 @@
 class fileman_webdrv_Svg extends fileman_webdrv_Inkscape
 {
     
+    
+    /**
+     * Преобразува подадения файл в PDF
+     * 
+     * @param string $file
+     * 
+     * @return string - манипулатор на новия файл
+     */
+    public static function toSvg($file)
+    {
+        if (!$file) return ;
+        
+        cls::load('fileman_Files');
+        
+        if ((strlen($file) == FILEMAN_HANDLER_LEN) && (strpos($file, '/') === FALSE)) {
+            $fRec = fileman_Files::fetchByFh($file);
+            
+            expect($fRec);
+    	}
+        
+        // Инстанция на класа
+        $Script = cls::get('fconv_Script');
+        
+        // Вземаме името на файла без разширението
+        $name = fileman_Files::getFileNameWithoutExt($file);
+        
+        // Пътя до изходния файла
+        $outFilePath = $Script->tempDir . $name . '_to.svg';
+        
+        $Script->setFile('INPUTF', $file);
+        $Script->setFile('OUTPUTF', $outFilePath);
+        
+        $Script->setProgram('inkscape', INKSCAPE_PATH);
+        
+        // Скрипта, който ще конвертира файла в .svg формат
+        $Script->lineExec("inkscape [#INPUTF#] --export-plain-svg=[#OUTPUTF#] --export-area-drawing");
+        
+        // Стартираме скрипта синхронно
+        $Script->run(FALSE);
+        
+        $resFileHnd = fileman::absorb($outFilePath, 'fileIndex');
+        
+        if ($Script->tempDir) {
+            // Изтриваме временната директория с всички файлове вътре
+            core_Os::deleteDir($Script->tempDir);
+        }
+        
+        return $resFileHnd;
+    }
 }
