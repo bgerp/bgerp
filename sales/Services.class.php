7a9fe870d051c793db4c4c602b78d6cd142fadc6
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-08-19 13:06:54 +0300

    Премахнат плъгин Plg_CsvRExport от някой документи

--- sales/Services.class.php
+++ sales/Services.class.php
@@ -36,7 +36,7 @@ class sales_Services extends deals_ServiceMaster
      * Плъгини за зареждане
      */
     public $loadList = 'plg_RowTools, sales_Wrapper, plg_Sorting, acc_plg_Contable, doc_DocumentPlg, plg_Printing,
-                    plg_ExportCsv, acc_plg_DocumentSummary, plg_Search,
+                    acc_plg_DocumentSummary, plg_Search,
 					doc_EmailCreatePlg, bgerp_plg_Blank, cond_plg_DefaultValues, doc_plg_TplManager, doc_plg_HidePrices, plg_LastUsedKeys';
 
     
