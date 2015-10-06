2f3c04f6678602f3c2983e5ba6796839c6bcb87f
Yusein Yuseinov <yyuseinov@gmail.com>
2015-10-06 13:48:37 +0300

    core_Sbf - bugFix - ttf и подобни файлове да се стартират статично

--- core/Sbf.class.php
+++ core/Sbf.class.php
@@ -166,7 +166,7 @@ class core_Sbf extends core_Mvc
     public static function serveStaticFile($name)
     {
         $file = getFullPath($name);
- 
+        
         // Грешка. Файла липсва
         if (!$file || !($toSave = $content = @file_get_contents($file))) {
             
@@ -202,11 +202,15 @@ class core_Sbf extends core_Mvc
                 'jpeg' => 'image/jpeg',
                 'jpg'  => 'image/jpeg',
                 'gif'  => 'image/gif',
-                'ico'  => 'image/vnd.microsoft.icon'
+                'ico'  => 'image/vnd.microsoft.icon',
+                
+                // Икони
+                'ttf' => 'application/x-font-ttf',
+                'eot' => 'application/vnd.ms-fontobject',
+                'woff' => 'application/octet-stream',
+                'woff2' => 'application/octet-stream',
             );
-
-            $ctype = $mimeTypes[$fileExt];
-
+            
             if (!$ctype) {
                 if (isDebug()) {
                     error_log("Warning: Unsuported file extention: {$file}");
