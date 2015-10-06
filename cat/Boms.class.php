7b213cc62b9b45ab6021b810deaf2d7e09e3c5be
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-10-06 18:06:17 +0300

    Рецепти:отново се кешира изгледа им

--- cat/Boms.class.php
+++ cat/Boms.class.php
@@ -119,12 +119,6 @@ class cat_Boms extends core_Master
     
     
     /**
-     * Да се забрани ли кеширането на документа
-     */
-    //public $preventCache = TRUE;
-    
-    
-    /**
      * Файл с шаблон за единичен изглед на статия
      */
     var $singleLayoutFile = 'cat/tpl/SingleLayoutBom.shtml';
