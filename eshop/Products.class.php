af86a59a079c44e02a887fdd6e3a1455fda80c40
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-10-13 13:23:32 +0300

    Запитвания:bugFixes

--- eshop/Products.class.php
+++ eshop/Products.class.php
@@ -494,10 +494,6 @@ class eshop_Products extends core_Master
     		$newArr['title'] .= ($rec->code) ? " (" . $this->getVerbal($rec, 'code'). ")" : "";
     	}
     	
-    	if(isset($newArr['measureId'])){
-    		$newArr['measureId'] = cat_UoM::fetchBySinonim($newArr['measureId'])->id;
-    	}
-    	
     	return $newArr;
     }
 
