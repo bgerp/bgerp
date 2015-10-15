67f94cfb76947f85af0b00279ed9b14721826b09
Yusein Yuseinov <yyuseinov@gmail.com>
2015-10-15 10:06:48 +0300

    bugFix

--- email/Incomings.class.php
+++ email/Incomings.class.php
@@ -406,7 +406,7 @@ class email_Incomings extends core_Master
         } catch (core_exception_Expect $exp) {
             // Обща грешка
             $status = 'error';
-            reportException($e, NULL, TRUE);
+            reportException($exp, NULL, TRUE);
         }
         
         // Записваме в отпечатъка на това писмо, както и статуса му на сваляне
