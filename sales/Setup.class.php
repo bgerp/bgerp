0339470e2086fe53a761adc111bd2b0c4c42ac71
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-10-15 10:53:39 +0300

    фактури: основанията за неначисляване на ДДС да са уеб константи

--- sales/Setup.class.php
+++ sales/Setup.class.php
@@ -62,6 +62,18 @@ defIfNot('SALE_FISC_PRINTER_DRIVER', '');
 
 
 /**
+ * Основание за неначисляване на ДДС за контрагент контрагент от държава в ЕС (без България)
+ */
+defIfNot('SALES_VAT_REASON_IN_EU', 'чл.53 от ЗДДС – вътреобщностна доставка на стоки');
+
+
+/**
+ * Основание за неначисляване на ДДС за контрагент извън ЕС
+ */
+defIfNot('SALES_VAT_REASON_OUTSIDE_EU', 'чл.28 от ЗДДС – износ на стоки извън ЕС');
+
+
+/**
  * Кой да е по подразбиране драйвера за фискален принтер
  */
 defIfNot('SALE_INV_VAT_DISPLAY', 'no');
@@ -158,6 +170,9 @@ class sales_Setup extends core_ProtoSetup
 	
 			'SALE_INVOICE_DEF_TPL_BG'     => array('key(mvc=doc_TplManager,allowEmpty)', 'caption=Фактура основен шаблон->Български,optionsFunc=sales_Invoices::getTemplateBgOptions'),
 			'SALE_INVOICE_DEF_TPL_EN'     => array('key(mvc=doc_TplManager,allowEmpty)', 'caption=Фактура основен шаблон->Английски,optionsFunc=sales_Invoices::getTemplateEnOptions'),
+			
+			'SALES_VAT_REASON_OUTSIDE_EU'  => array('varchar', 'caption=Основание за ненаничляване на ДДС за контрагент->Извън ЕС'),
+			'SALES_VAT_REASON_IN_EU'       => array('varchar', 'caption=Основание за ненаничляване на ДДС за контрагент->От ЕС'),
 	);
 	
 	
