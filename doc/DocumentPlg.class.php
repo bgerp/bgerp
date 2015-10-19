f13c7588f822c153d59bbcf1b7f319368bd2f08c
Yusein Yuseinov <yyuseinov@gmail.com>
2015-10-19 10:23:13 +0300

    doc_DocumentPlg #Sig1390 - да не се показват излишни полета при отпечатване

--- doc/DocumentPlg.class.php
+++ doc/DocumentPlg.class.php
@@ -2521,7 +2521,7 @@ class doc_DocumentPlg extends core_Plugin
         $res = getTplFromFile('/doc/tpl/LetterHeadTpl.shtml');
         
         // Ако има повече от една версия
-        if ($row->LastVersion != 0.1) {
+        if (isset($row->LastVersion) && ($row->LastVersion != 0.1)) {
             // Полета, които ще се показват
             $headerRes = change_Plugin::getDateAndVersionRow();
         }
