fb30b6a9be4a06ee2d7f25103fdb83a63500f891
Milen Georgiev <milen@experta.bg>
2015-10-22 13:32:52 +0300

    Валутата преди сумата в каси и сметки

--- bank/OwnAccounts.class.php
+++ bank/OwnAccounts.class.php
@@ -277,7 +277,7 @@ class bank_OwnAccounts extends core_Master {
         $currencyId = acc_Periods::getBaseCurrencyCode();
         $state = (Request::get('Rejected', 'int')) ? 'rejected' : 'closed';
         $colspan = count($data->listFields) - 1;
-        $lastRow = new ET("<tr style='text-align:right' class='state-{$state}'><td colspan='{$colspan}'>[#caption#]: &nbsp;<b>[#total#]</b> <span class='cCode'>{$currencyId}</span></td><td>&nbsp;</td></tr>");
+        $lastRow = new ET("<tr style='text-align:right' class='state-{$state}'><td colspan='{$colspan}'>[#caption#]:&nbsp;<span class='cCode'>{$currencyId}</span>&nbsp;<b>[#total#]</b></td><td>&nbsp;</td></tr>");
         $lastRow->replace(tr("Общо"), 'caption');
         $lastRow->replace($total, 'total');
         
