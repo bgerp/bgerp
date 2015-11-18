2f458e7a9092c245894d312a9aa3196999a22a49
Yusein Yuseinov <yyuseinov@gmail.com>
2015-11-18 10:12:06 +0200

    doc_DocumentPlg - бутона история да не се показва на контракторите

--- doc/DocumentPlg.class.php
+++ doc/DocumentPlg.class.php
@@ -249,11 +249,13 @@ class doc_DocumentPlg extends core_Plugin
 
         }
         
-        $historyCnt = log_Data::getObjectCnt($mvc, $data->rec->id);
-        
-        if ($historyCnt) {
-            $data->toolbar->addBtn("История|* ({$historyCnt})", doclog_Documents::getLinkToSingle($data->rec->containerId, doclog_Documents::ACTION_HISTORY),
-            "id=btnHistory{$data->rec->containerId}, row=2, order=19.5,title=" . tr('История на документа'),  'ef_icon = img/16/book_open.png');
+        if($mvc->haveRightFor('single') && !core_Users::isContractor()){
+            $historyCnt = log_Data::getObjectCnt($mvc, $data->rec->id);
+            
+            if ($historyCnt) {
+                $data->toolbar->addBtn("История|* ({$historyCnt})", doclog_Documents::getLinkToSingle($data->rec->containerId, doclog_Documents::ACTION_HISTORY),
+                "id=btnHistory{$data->rec->containerId}, row=2, order=19.5,title=" . tr('История на документа'),  'ef_icon = img/16/book_open.png');
+            }
         }
     }
     
