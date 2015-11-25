a88033356b337516dfe323dcd2c7310980bb183e
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-11-25 11:13:56 +0200

    store_Transfers:bugFix

--- store/Transfers.class.php
+++ store/Transfers.class.php
@@ -335,7 +335,6 @@ class store_Transfers extends core_Master
     	$dQuery->EXT('state', 'store_Transfers', 'externalKey=transferId');
     	$dQuery->where("#transferId = '{$id}'");
     	while($dRec = $dQuery->fetch()){
-    		$sProd = store_Products::fetch($dRec->newProductId);
     		$cid = cat_Products::fetchField($dRec->newProductId, 'containerId');
     		$res[$cid] = $cid;
     	}
