675f1f97e2955312c86b1de17931226ba7cf4d3b
Yusein Yuseinov <yyuseinov@gmail.com>
2015-12-02 13:34:18 +0200

    bugFix

--- core/Detail.class.php
+++ core/Detail.class.php
@@ -457,5 +457,7 @@ class core_Detail extends core_Manager
             
             return $inst->Master->getLinkForObject($masterId);
         }
+        
+        return parent::getLinkForObject($objId);
     }
 }
