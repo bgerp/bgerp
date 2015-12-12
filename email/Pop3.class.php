6caa23724f796df4384e10c1781b22074d05bfa4
Yusein Yuseinov <yyuseinov@gmail.com>
2015-12-12 17:11:11 +0200

    email - Използване на системния лог

--- email/Pop3.class.php
+++ email/Pop3.class.php
@@ -78,8 +78,8 @@ class email_Pop3
         @$this->connection = fsockopen($this->host, $this->port, $this->err['no'], $this->err['str'], $conf->EMAIL_POP3_TIMEOUT);
         
         if ($this->connection === false) {
-            log_Data::add('err', "Не може да се установи връзка с пощенската кутия на: 
-                            \"{$this->user}\". Грешка: " . $this->err['no'] . " - " . $this->err['str'], get_called_class());
+            log_System::add(get_called_class(), "Не може да се установи връзка с пощенската кутия на: 
+                            \"{$this->user}\". Грешка: " . $this->err['no'] . " - " . $this->err['str'], NULL, 'err');
             
             return FALSE;
         }
@@ -91,8 +91,8 @@ class email_Pop3
         $this->login();
         
         if (!$this->logged) {
-            log_Data::add('err', "Не може да се установи връзка с пощенската кутия на: 
-                            \"{$this->user}\". Потребителското име и/или паролата са грешни.", get_called_class());
+            log_System::add(get_called_class(), "Не може да се установи връзка с пощенската кутия на: 
+                            \"{$this->user}\". Потребителското име и/или паролата са грешни.", NULL, 'err');
         }
         
         return TRUE;
