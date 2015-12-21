cdaf91fe2fcbb034f8e8a761c179f60f587e3fb5
Ivelin Dimov <ivelin_pdimov@abv.bg>
2015-12-21 17:51:34 +0200

    Производствени документи: добавено поле за партида

--- deals/ManifactureDetail.class.php
+++ deals/ManifactureDetail.class.php
@@ -29,6 +29,7 @@ abstract class deals_ManifactureDetail extends doc_Detail
 	public function setDetailFields($mvc)
 	{
 		$mvc->FLD('productId', 'key(mvc=cat_Products,select=name)', 'caption=Продукт,mandatory', 'tdClass=leftCol wrap,silent,removeAndRefreshForm=quantity|measureId|packagingId|packQuantity');
+		$mvc->FLD('batch', 'varchar(128)', 'input=none,caption=Партида,after=productId,forceField');
 		$mvc->FLD('packagingId', 'key(mvc=cat_UoM, select=shortName, select2MinItems=0)', 'caption=Мярка','smartCenter,mandatory');
 		$mvc->FNC('packQuantity', 'double(Min=0)', 'caption=К-во,input=input,mandatory');
 		$mvc->FLD('quantityInPack', 'double(smartRound)', 'input=none,notNull,value=1');
@@ -159,6 +160,11 @@ abstract class deals_ManifactureDetail extends doc_Detail
 		
 		// Показваме подробната информация за опаковката при нужда
 		deals_Helper::getPackInfo($row->packagingId, $rec->productId, $rec->packagingId, $rec->quantityInPack);
+	
+		if($rec->batch){
+			$batch = $mvc->getFieldType('batch')->toVerbal($rec->batch);
+			$row->productId .= "<div class='small'>lot: {$batch}</div>";
+		}
 	}
 }
  	
