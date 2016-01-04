b273d8f310c7618af154991be3abd4aad02a5f01
Ivelin Dimov <ivelin_pdimov@abv.bg>
2016-01-04 16:08:40 +0200

    plg_ALignDecimals2: метод alignDecimals

--- plg/AlignDecimals2.class.php
+++ plg/AlignDecimals2.class.php
@@ -18,6 +18,58 @@ class plg_AlignDecimals2 extends core_Plugin
 	
 	
 	/**
+	 * Метод за подравняване на десетичните числа
+	 * 
+	 * @param core_Mvc $mvc - модел
+	 * @param array $recs - записите във вътрешен вид
+	 * @param array $rows - записите във вербален вид
+	 * @return void
+	 */
+	public static function alignDecimals(core_Mvc $mvc, $recs, &$rows)
+	{
+		// Намираме всички полета, които се показват от типа Double
+		$decFields = array();
+		foreach ($mvc->fields as $name => $field){
+			if($field->type instanceof type_Double && $filed->input != 'none' && !($field->type instanceof type_Percent)){
+				$decFields[] = $name;
+			}
+		}
+		 
+		if(!count($decFields)) return;
+		 
+		// тука определяме най-дългата дробна част, без да записваме числото
+		foreach ($recs as $id => $rec){
+			foreach ($decFields as $fName){
+				core_Math::roundNumber($rec->$fName, ${"{$fName}FracLen"});
+			}
+		}
+		
+		// Закръгляме сумата и я обръщаме във вербален вид
+		foreach ($recs as $id => &$rec){
+			foreach ($decFields as $col => $fName){
+		
+				if(isset($rows[$id]->$fName)){
+					$Type = $mvc->fields[$fName]->type;
+					setIfNot($Type->params['minDecimals'], 0);
+					setIfNot($Type->params['maxDecimals'], 6);
+						
+					$optDecimals = min(
+							$Type->params['maxDecimals'],
+							max($Type->params['minDecimals'], ${"{$fName}FracLen"})
+					);
+					$Type->params['decimals'] = $optDecimals;
+						
+					// Вербализираме числово само ако наистина е число
+					if(is_numeric($rec->$fName)){
+						$rows[$id]->$fName = $Type->toVerbal($rec->$fName);
+					}
+				}
+			}
+		}
+	}
+	
+	
+	/**
      * Преди рендиране на таблицата
      */
     public static function on_BeforeRenderListTable($mvc, &$tpl, $data)
@@ -27,44 +79,6 @@ class plg_AlignDecimals2 extends core_Plugin
     	
     	if(!count($recs)) return;
     	
-    	// Намираме всички полета, които се показват от типа Double
-    	$decFields = array();
-    	foreach ($mvc->fields as $name => $field){
-    		if($field->type instanceof type_Double && $filed->input != 'none' && !($field->type instanceof type_Percent)){
-    			$decFields[] = $name;
-    		}
-    	}
-    	
-    	if(!count($decFields)) return;
-    	
-    	// тука определяме най-дългата дробна част, без да записваме числото 
-		foreach ($recs as $id => $rec){
-			foreach ($decFields as $fName){
-				core_Math::roundNumber($rec->$fName, ${"{$fName}FracLen"});
-			}
-		}
-		
-		// Закръгляме сумата и я обръщаме във вербален вид
-    	foreach ($recs as $id => &$rec){ 
-			foreach ($decFields as $col => $fName){
-				
-				if(isset($rows[$id]->$fName)){
-					$Type = $mvc->fields[$fName]->type;
-					setIfNot($Type->params['minDecimals'], 0);
-	                setIfNot($Type->params['maxDecimals'], 6);
-					
-	                $optDecimals = min(
-	                    $Type->params['maxDecimals'],
-	                    max($Type->params['minDecimals'], ${"{$fName}FracLen"})
-	                );
-					$Type->params['decimals'] = $optDecimals;
-					
-					// Вербализираме числово само ако наистина е число
-					if(is_numeric($rec->$fName)){
-						$rows[$id]->$fName = $Type->toVerbal($rec->$fName);
-					}
-				}
-			}
-		}
+    	self::alignDecimals($mvc, $recs, $rows);
     }
 }
