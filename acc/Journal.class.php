30e7c891a873929905307e39c8679829546bfbd7
HeBuTy <nevena@experta.bg>
2016-01-04 16:19:21 +0200

    Правописни грешки

--- acc/Journal.class.php
+++ acc/Journal.class.php
@@ -822,12 +822,12 @@ class acc_Journal extends core_Master
     	acc_JournalDetails::filterQuery($dQuery, $from, $to);
     	
     	if($debitSysId){
-    		expect($debitAccId = acc_Accounts::fetchField(array("#systemId = '[#1#]'", $debitSysId), 'id'), "Няма сметка с систем ид {$debitAccId}");
+    		expect($debitAccId = acc_Accounts::fetchField(array("#systemId = '[#1#]'", $debitSysId), 'id'), "Няма сметка със систем ид {$debitAccId}");
     		$dQuery->where("#debitAccId = {$debitAccId}");
     	}
     	
     	if($creditSysId){
-    		expect($creditAccId = acc_Accounts::fetchField(array("#systemId = '[#1#]'", $creditSysId), 'id'), "Няма сметка с систем ид {$creditSysId}");
+    		expect($creditAccId = acc_Accounts::fetchField(array("#systemId = '[#1#]'", $creditSysId), 'id'), "Няма сметка със систем ид {$creditSysId}");
     		$dQuery->where("#creditAccId = {$creditAccId}");
     	}
     	
