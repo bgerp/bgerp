afadaa07548db31915b20436a9362e7008e4a49d
Yusein Yuseinov <yyuseinov@gmail.com>
2016-01-04 17:12:21 +0200

    bugFix

--- core/Setup.inc.php
+++ core/Setup.inc.php
@@ -662,14 +662,14 @@ if($step == 3) {
     
     // Необходими програми на сървъра
     $log[] = 'h:Проверка за необходимите програми на сървъра:';
-
+    
     $requiredPrograms = array('wget');
     
     foreach($requiredPrograms as $program){
         if (exec('which ' . escapeshellcmd($program))){
             $log[] = "inf:Налична програма: <b>`$program`</b>";
         } else {
-            $log[] = "err:Липсващ програма: <b>`$module`</b>";
+            $log[] = "err:Липсващ програма: <b>`$program`</b>";
         }
     }
     
