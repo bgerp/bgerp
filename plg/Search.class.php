421996bc1503338f384f6c97e0f20d2bbdb3e492
Yusein Yuseinov <yyuseinov@gmail.com>
2016-01-07 15:52:47 +0200

    plg_Search #C10527 - използване на is_int вместо is_numeric

--- plg/Search.class.php
+++ plg/Search.class.php
@@ -88,7 +88,7 @@ class plg_Search extends core_Plugin
                     }
                     
                 }
-            } elseif (is_numeric($rec)) {
+            } elseif (is_int($rec)) {
                 $cRec = $mvc->fetch($rec);
             }
             
@@ -135,7 +135,7 @@ class plg_Search extends core_Plugin
             static::applySearch($filterRec->{$mvc->searchInputField}, $data->query);
             
             // Ако ключовата дума е число, търсим и по ид
-            if(is_numeric($filterRec->{$mvc->searchInputField})){
+            if (is_int($filterRec->{$mvc->searchInputField})){
             	$data->query->where(array("#id = [#1#]", $filterRec->{$mvc->searchInputField}), TRUE);
             }
         }
