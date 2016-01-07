a4e6c0065c17b3a39c99a8e8c8f7fd29b980dd67
Ivelin Dimov <ivelin_pdimov@abv.bg>
2016-01-07 13:11:57 +0200

    Оферти:bugFix да използват plg_AlignDecimals2::alignDecimals

--- sales/QuotationsDetails.class.php
+++ sales/QuotationsDetails.class.php
@@ -63,7 +63,7 @@ class sales_QuotationsDetails extends doc_Detail {
     /**
      * Плъгини за зареждане
      */
-    public $loadList = 'plg_RowTools, sales_Wrapper, plg_AlignDecimals, doc_plg_HidePrices, plg_SaveAndNew, LastPricePolicy=sales_SalesLastPricePolicy';
+    public $loadList = 'plg_RowTools, sales_Wrapper, doc_plg_HidePrices, plg_SaveAndNew, LastPricePolicy=sales_SalesLastPricePolicy';
     
     
     /**
@@ -485,7 +485,29 @@ class sales_QuotationsDetails extends doc_Detail {
     		$data->addOptionalBtn = ht::createBtn('Опционален артикул',  array($this, 'add', 'quotationId' => $data->masterId, 'optional' => 'yes', 'ret_url' => TRUE),  FALSE, FALSE, "{$error} ef_icon = img/16/shopping.png, title=Добавяне на опционален артикул към офертата");
     	}
     	
+    	// Ако няма записи не правим нищо
     	if(!$data->rows) return;
+    	
+    	// Заределяме рековете и роуовете на опционални и неопционални
+    	$optionalRows = $notOptionalRows = $optionalRecs = $notOptionalRecs = array();
+    	foreach($data->recs as $ind => $r){
+    		if($r->optional == 'no'){
+    			$notOptionalRecs[$ind] = $r;
+    			$notOptionalRows[$ind] = $data->rows[$ind];
+    		} else {
+    			$optionalRecs[$ind] = $r;
+    			$optionalRows[$ind] = $data->rows[$ind];
+    		}
+    	}
+    	
+    	// Подравняваме ги спрямо едни други
+    	plg_AlignDecimals2::alignDecimals($this, $optionalRecs, $optionalRows);
+    	plg_AlignDecimals2::alignDecimals($this, $notOptionalRecs, $notOptionalRows);
+    	
+    	// Подменяме записите за показване с подравнените
+    	$data->rows = $notOptionalRows + $optionalRows;
+    	
+    	// Групираме записите за по-лесно показване
     	foreach($data->rows as $i => $row){
     		$pId = $data->recs[$i]->productId;
     		$optional = $data->recs[$i]->optional;
@@ -555,7 +577,7 @@ class sales_QuotationsDetails extends doc_Detail {
     	$oTpl = getTplFromFile($optionalTemplateFile);
     	$oTpl->removeBlock("totalPlace");
     	$oCount = $dCount = 1;
-    	
+   
     	// Променливи за определяне да се скриват ли някои колони
     	$hasQuantityColOpt = FALSE;
     	if($data->rows){
