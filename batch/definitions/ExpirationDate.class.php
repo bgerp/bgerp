6dc947a046a558d08f79ac1c01c7f8e8b0efa107
Ivelin Dimov <ivelin_pdimov@abv.bg>
2016-01-18 17:55:16 +0200

    Партиди срок на годност:правилна проверка да валидност на датата

--- batch/definitions/ExpirationDate.class.php
+++ batch/definitions/ExpirationDate.class.php
@@ -2,13 +2,13 @@
 
 
 /**
- * Базов драйвер за вид партида 'дата на годност'
+ * Базов драйвер за партиден клас 'дата на годност'
  *
  *
  * @category  bgerp
  * @package   batch
  * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
- * @copyright 2006 - 2015 Experta OOD
+ * @copyright 2006 - 2016 Experta OOD
  * @license   GPL 3
  * @since     v 0.1
  * @title Дата на годност
@@ -18,9 +18,9 @@ class batch_definitions_ExpirationDate extends batch_definitions_Proto
 	
 	
 	/**
-	 * Предложения за формати
+	 * Позволени формати
 	 */
-	private $formatSuggestions = 'm/d/y,m.d.y,d.m.Y,m/d/Y,d/m/Y,Ymd';
+	private $formatSuggestions = 'm/d/y,m.d.y,d.m.Y,m/d/Y,d/m/Y,Ymd,Ydm,Y-m-d,dmY,ymd,ydm';
 	
 	
 	/**
@@ -33,7 +33,7 @@ class batch_definitions_ExpirationDate extends batch_definitions_Proto
 		$fieldset->FLD('format', 'varchar(20)', 'caption=Формат,mandatory');
 		$fieldset->FLD('time', 'time(suggestions=1 ден|2 дена|1 седмица|1 месец)', 'caption=Колко дни след текущата дата');
 		
-		$fieldset->setSuggestions('format', array('' => '') + arr::make($this->formatSuggestions, TRUE));
+		$fieldset->setOptions('format', array('' => '') + arr::make($this->formatSuggestions, TRUE));
 	}
 	
 	
@@ -67,20 +67,35 @@ class batch_definitions_ExpirationDate extends batch_definitions_Proto
 	 */
 	public function isValid($value, $quantity, &$msg)
 	{
-		$check = strtotime($value);
-		if(!$check) {
+		// Карта
+		$map = array();
+		$map['m'] = "(?'month'[0-9]{2})";
+		$map['d'] = "(?'day'[0-9]{2})";
+		$map['y'] = "(?'yearShort'[0-9]{2})";
+		$map['Y'] = "(?'year'[0-9]{4})";
+		
+		// Генерираме регулярен израз спрямо картата
+		$expr = $this->rec->format;
+		$expr = preg_quote($expr, '/');
+		$expr = strtr($expr, $map);
+		
+		// Проверяваме дали датата отговаря на формата
+		if(!preg_match("/^{$expr}$/", $value, $matches)){
 			$msg = "|Партидата трябва да е във формат за дата|* <b>{$this->rec->format}</b>";
 			return FALSE;
 		}
 		
-		$check = dt::timestamp2Mysql($check);
-		$check = dt::mysql2verbal($check, $this->rec->format);
+		if(isset($matches['yearShort'])){
+			$matches['year'] = "20{$matches['yearShort']}";
+		}
 		
-		if($check !== $value){
+		// Проверяваме дали датата е възможна
+		if(!checkdate($matches['month'], $matches['day'], $matches['year'])){
 			$msg = "|Партидата трябва да е във формат за дата|* <b>{$this->rec->format}</b>";
 			return FALSE;
 		}
 		
+		// Връщаме истина, ако не са се получили грешки
 		return TRUE;
 	}
 }
