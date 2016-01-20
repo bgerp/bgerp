d9ba558655da29b4ed9c0e5926ed2ba323bca03a
Ivelin Dimov <ivelin_pdimov@abv.bg>
2016-01-20 16:07:54 +0200

    Задачи за производство:при показване като детайл да се скрива колоната за очаквано начало ако е празна

--- tasks/Tasks.class.php
+++ tasks/Tasks.class.php
@@ -823,10 +823,10 @@ class tasks_Tasks extends embed_Manager
     {
     	$tpl = new ET("");
     		
-    	// Ако няма намерени записи, не се реднира нищо
+    	// Ако няма намерени записи, не се рендира нищо
     	// Рендираме таблицата с намерените задачи
     	$table = cls::get('core_TableView', array('mvc' => $this));
-    	$table->setFieldsToHideIfEmptyColumn('timeStart,timeDuration,timeEnd');
+    	$table->setFieldsToHideIfEmptyColumn('timeStart,timeDuration,timeEnd,expectedTimeStart');
     	$tpl = $table->get($data->rows, 'tools=Пулт,progress=Прогрес,name=Документ,title=Заглавие,expectedTimeStart=Очаквано начало, timeDuration=Продължителност, timeEnd=Край, modified=Модифицирано');
     
     	// Имали бутони за добавяне
