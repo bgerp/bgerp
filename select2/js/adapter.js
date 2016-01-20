9c3ee58561d56d2df69d09737bc6c1a781c63fdb
Yusein Yuseinov <yyuseinov@gmail.com>
2016-01-20 16:22:24 +0200

    select2 - ако се избира цвят да се показва и при избраните

--- select2/js/adapter.js
+++ select2/js/adapter.js
@@ -18,7 +18,6 @@ function formatSelect2Data(data)
 	if (data.element && (color = data.element.getAttribute('data-color'))) {
 		text = "<div class='color-preview' style='background-color:" + color + " !important;'> </div>&nbsp;" + text;
 	}
-
 	
 	if (data.loading) return text;
 	
@@ -43,8 +42,15 @@ function formatSelect2Data(data)
  */
 function formatSelect2DataSelection(data)
 {
+	var text = data.text;
 	
-	return data.text;
+	if (data.element && (color = data.element.getAttribute('data-color'))) {
+		text = "<span><div class='color-preview' style='background-color:" + color + " !important; margin-bottom: 2px;'> </div>&nbsp;" + text + "</span>";
+		
+		text = $(text);
+	}
+	
+	return text;
 }
 
 
