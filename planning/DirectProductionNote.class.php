90536bd4b2b32718ee6eda79a7470f8922a188ae
Ivelin Dimov <ivelin_pdimov@abv.bg>
2016-01-22 18:00:45 +0200

    Протоколи за бързо производство: по хубав кепшън

--- planning/DirectProductionNote.class.php
+++ planning/DirectProductionNote.class.php
@@ -152,7 +152,7 @@ class planning_DirectProductionNote extends planning_ProductionDocument
 		$this->FLD('productId', 'key(mvc=cat_Products,select=name)', 'caption=Артикул,mandatory,before=storeId');
 		$this->FLD('batch', 'text', 'input=none,caption=Партида,after=productId,forceField');
 		$this->FLD('jobQuantity', 'double(smartRound)', 'caption=Задание,input=hidden,mandatory,after=productId');
-		$this->FLD('quantity', 'double(smartRound,Min=0)', 'caption=За,mandatory,after=jobQuantity');
+		$this->FLD('quantity', 'double(smartRound,Min=0)', 'caption=Количество,mandatory,after=jobQuantity');
 		$this->FLD('expenses', 'percent', 'caption=Режийни разходи,after=quantity');
 		
 		$this->setDbIndex('productId');
@@ -188,7 +188,7 @@ class planning_DirectProductionNote extends planning_ProductionDocument
 		if(isset($bomRec->expenses)){
 			$form->setDefault('expenses', $bomRec->expenses);
 		}
-		
+		dt::getLastDayOfMonth()
 		$curStore = store_Stores::getCurrent('id', FALSE);
 		$data->form->setDefault('inputStoreId', $curStore);
 	}
