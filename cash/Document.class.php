2311296d997120a5924dc2cef67f2a00deaf30f5
ivelin1012 <ivelin_pdimov@abv.bg>
2016-02-11 18:46:28 +0200

    Банкови и касови ордери: предупреждение при разминаването на сумите

--- cash/Document.class.php
+++ cash/Document.class.php
@@ -193,6 +193,11 @@ abstract class cash_Document extends core_Master
     		if($rec->currencyId == $rec->dealCurrencyId){
     			$rec->amount = $rec->amountDeal;
     		}
+    		
+    		$dealCurrencyCode = currency_Currencies::getCodeById($rec->dealCurrencyId);
+    		if($msg = currency_CurrencyRates::checkAmounts($rec->amount, $rec->amountDeal, $rec->valior, $currencyCode, $dealCurrencyCode)){
+    			$form->setWarning('amount', $msg);
+    		}
     	}
     	
     	acc_Periods::checkDocumentDate($form, 'valior');
