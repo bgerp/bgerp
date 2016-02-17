73fde7072a35d6a2ac8ddfdcbf22d0f609f9cd93
ivelin1012 <ivelin_pdimov@abv.bg>
2016-02-17 18:37:53 +0200

    По хубав хинт

--- planning/Tasks.class.php
+++ planning/Tasks.class.php
@@ -229,7 +229,7 @@ class planning_Tasks extends tasks_Tasks
 				$error = ($tQuery->fetch()) ? '' : ",error=Няма наличен шаблон за етикети от задачи за производство";
 				
 				core_Request::setProtected('class,objectId');
-				$data->toolbar->addBtn('Етикетиране', array('label_Labels', 'selectTemplate', 'class' => $mvc->className, 'objectId' => $data->rec->id, 'ret_url' => TRUE), NULL, "target=_blank,ef_icon = img/16/price_tag_label.png,title=Разпечатване на етикети{$error}");
+				$data->toolbar->addBtn('Етикетиране', array('label_Labels', 'selectTemplate', 'class' => $mvc->className, 'objectId' => $data->rec->id, 'ret_url' => TRUE), NULL, "target=_blank,ef_icon = img/16/price_tag_label.png,title=Разпечатване на етикети от задачата за производство{$error}");
 			}
 		}
 	}
