85f59729e72e56a32938e2b3de01fc6bf29ec9fb
Milen Georgiev <milen@experta.bg>
2016-02-23 22:51:32 +0200

    Съобщения за грешки в plg_UserReg + опит за изпращане на писма и без наличие на конфигурация за това

--- phpmailer/Instance.class.php
+++ phpmailer/Instance.class.php
@@ -45,6 +45,18 @@ class phpmailer_Instance extends core_BaseClass
         $PML->SMTPSecure = $conf->PML_SMTPSECURE;
         $PML->Username  = $conf->PML_USERNAME;
         $PML->Password  = $conf->PML_PASSWORD;
+
+        if($params['emailTo']) {
+            list($user, $domain) = explode('@', $params['emailTo']);
+            if($domain && getmxrr($domain, $mxhosts, $mx_weight)) {
+                if(count($mxhosts) && ! $params['Host']) {
+                    $params['Host'] = $mxhosts[0];
+                    $params['SMTPAuth'] = FALSE;
+                    $params['SMTPSecure'] = FALSE;
+                }
+            }
+            unset($params['emailTo']);
+        }
         
         // Добавяме динамичните параметри, които могат да 
         // "препокрият" зададените конфигурационни стойности
@@ -53,7 +65,7 @@ class phpmailer_Instance extends core_BaseClass
                 $PML->{$name} = $value;
             }
         }
-        
+ 
         // Връщаме създадения обект
         return $PML;
     }
