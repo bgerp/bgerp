779b83e0c00763a1716d6f0ac220bacf2fb35648
mitko <mitko@experta.bg>
2016-03-12 17:23:00 +0200

    инвалидиране opt_cache след промяна на конф. файл

--- core/Setup.inc.php
+++ core/Setup.inc.php
@@ -752,6 +752,9 @@ if($step == 3) {
                     $src = str_replace('USER_PASSWORD_FOR_DB', $passwordDB, $src);
                     @file_put_contents($paths['config'], $src);
                     $log[] = "inf: Паролата на root на mysql-a е сменена";
+                    if (function_exists(opcache_invalidate())) {
+                        opcache_invalidate("bgerp.inc.php", TRUE);
+                    }
                 } else {
                     $log[] = "wrn: Паролата на root на mysql-a не е сменена - използвате шаблонна парола, която се разпространява с имиджите на bgERP";
                 }
