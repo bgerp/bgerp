e36e2879b682b3d865b88f09ee42aa3429cdac53
GPetrova <gab4eto@gmail.com>
2016-03-17 14:31:59 +0200

    Корекция html - csv

--- acc/reports/HistoryImpl.class.php
+++ acc/reports/HistoryImpl.class.php
@@ -322,7 +322,7 @@ class acc_reports_HistoryImpl extends frame_BaseDriver
 	    unset($this->innerState->allRecs['zero']);
 	    unset($this->innerState->allRecs['last']);
 	    
-	    foreach ($this->innerState->allRecs as $id => $rec) {
+	    foreach ($this->innerState->allRecs as $id => &$rec) {
 	       
 	        $dataRec[] = cls::get('acc_BalanceHistory')->getVerbalHistoryRow($rec);
 
@@ -331,16 +331,14 @@ class acc_reports_HistoryImpl extends frame_BaseDriver
 	               $dataRec[$id]->$fld = $rec[$fld];
 	           }
 	        }
-	        
-	        if (!is_null($dataRec[$id]->docId)) {
-	            //$dataRecs[$id]->docId = str_replace("&nbsp;", '', $dataRec[$id]->docId);
-	            $dataRecs[$id]->docId = trim(html_entity_decode(strip_tags($dataRec[$id]->docId)));
-	        }
-	         
-	        if (!is_null($dataRec[$id]->reason)) {
-	            //$dataRecs[$id]->reason = str_replace("&nbsp;", '', $dataRec[$id]->docId);
-	            $dataRecs[$id]->reason = trim(html_entity_decode(strip_tags($dataRec[$id]->docId)));
-	        }
+
+    	    if (!is_null($dataRec[$id]->docId)) {
+    	       $dataRec[$id]->docId = trim(html_entity_decode(strip_tags($dataRec[$id]->docId)));
+    	    }
+    	         
+    	    if (!is_null($dataRec[$id]->reason)) {
+    	       $dataRec[$id]->reason = trim(html_entity_decode(strip_tags($dataRec[$id]->docId)));
+    	    }
 	    }
 
 	    $csv = csv_Lib::createCsv($dataRec, $fields, $exportFields);
