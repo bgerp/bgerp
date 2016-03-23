7f894c7d4617da35ad17b4ed950fe9a862812e86
Milen Georgiev <milen@experta.bg>
2016-03-24 00:04:00 +0200

    Самопоправяне на таблици от базата данни
    Conflicts:
    	core/boot.inc.php
    	core/exception/Db.class.php

--- core/exception/Db.class.php
+++ core/exception/Db.class.php
@@ -34,4 +34,26 @@ class core_exception_Db extends core_exception_Expect
         return $res;
     }
 
+
+    /**
+     * Опит за самопоправка на DB
+     */
+    public function repairDB($link)
+    {
+        if(isset($this->dump['mysqlErrCode']) && ($this->dump['mysqlErrCode'] ==  1062)) {
+            $parts = explode('`', $this->dump['query']);
+            $table = $parts[1];
+            $query = "SELECT max(id) as m FROM `{$table}`";
+            $dbRes = $link->query($query);  
+            $res = $dbRes->fetch_object();
+            $link->query("ALTER TABLE `{$table}` AUTO_INCREMENT = {$res->m}+10");
+        }
+        
+        if(isset($this->dump['mysqlErrCode']) && in_array($this->dump['mysqlErrCode'], array(126, 127, 132, 134, 141, 144, 145)) ) {
+            $parts = explode('`', $this->dump['query']);
+            $table = $parts[1];
+            $query = "REPAIR TABLE `{$table}`";
+            $dbRes = $link->query($query);  
+        }
+    }
 }
