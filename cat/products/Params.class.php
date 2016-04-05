39db92f8740a1b1a279ebe316c2cc3cb87470bae
ivelin1012 <ivelin_pdimov@abv.bg>
2016-04-05 16:16:16 +0300

    продуктови параметри:bugFix

--- cat/products/Params.class.php
+++ cat/products/Params.class.php
@@ -308,17 +308,19 @@ class cat_products_Params extends doc_Detail
     {
         if($requiredRoles == 'no_one') return;
     	
-        if (($action == 'add' || $action == 'delete') && isset($rec->productId)) {
+        if (($action == 'add' || $action == 'delete' || $action == 'edit') && isset($rec->productId)) {
         	$pRec = cat_Products::fetch($rec->productId);
         	
         	// Ако няма оставащи параметри или състоянието е оттеглено, не може да се добавят параметри
-        	if (!count($mvc::getRemainingOptions($rec->classId, $rec->productId))) {
-                $requiredRoles = 'no_one';
-            } elseif($pRec->innerClass != cat_GeneralProductDriver::getClassId()) {
-            	
-            	// Добавянето е разрешено само ако драйвера на артикула е универсалния артикул
-            	$requiredRoles = 'no_one';
-            }
+        	if($action != 'edit'){
+        		if (!count($mvc::getRemainingOptions($rec->classId, $rec->productId))) {
+        			$requiredRoles = 'no_one';
+        		} elseif($pRec->innerClass != cat_GeneralProductDriver::getClassId()) {
+        			 
+        			// Добавянето е разрешено само ако драйвера на артикула е универсалния артикул
+        			$requiredRoles = 'no_one';
+        		}
+        	}
             
             if($pRec->state != 'active' && $pRec->state != 'draft'){
             	$requiredRoles = 'no_one';
