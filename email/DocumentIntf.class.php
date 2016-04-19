fbdde6a1047436a298d898bb19aabb8ae1c62ee1
ivelin1012 <ivelin_pdimov@abv.bg>
2016-04-19 10:51:55 +0300

    email_DocumentIntf:bugFix ъпдейтнато описанието на функцията getDocumentBody

--- email/DocumentIntf.class.php
+++ email/DocumentIntf.class.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 /**
  * Интерфейс на документ който може да бъде изпращан по електронна поща
  *
@@ -9,18 +8,23 @@
  * @category  bgerp
  * @package   email
  * @author    Stefan Stefanov <stefan.bg@gmail.com>
- * @copyright 2006 - 2012 Experta OOD
+ * @copyright 2006 - 2016 Experta OOD
  * @license   GPL 3
  * @since     v 0.1
  * @title     Интерфейс на документ който може да бъде изпращан по електронна поща
  */
 class email_DocumentIntf extends doc_DocumentIntf
 {
+	
+	
 	/**
-     * Връща тялото по подразбиране на имейл-а
-     */
-    function getDefaultEmailBody($originId, $forward = FALSE)
+	 * Връща тялото на имейла генериран от документа
+	 * 
+	 * @param int $id - ид на документа
+	 * @return string - тялото на имейла
+	 */
+    function getDefaultEmailBody($id)
     {
-        return $this->class->getDefaultEmailBody($originId, $forward);
+        return $this->class->getDefaultEmailBody($id);
     }
 }
