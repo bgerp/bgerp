0b56437ddd8bc7a9fa0e3b11654d1ba6635c6591
Yuseinov <yuseinov@experta.bg>
2016-04-27 10:14:54 +0300

    zbar_Setup #C12126 - проверка дали е инсталирана zbarimg

--- zbar/Setup.class.php
+++ zbar/Setup.class.php
@@ -35,6 +35,32 @@ class zbar_Setup extends core_ProtoSetup
 	 * Пакет без инсталация
 	 */
 	public $noInstall = TRUE;
+    
+    
+	/**
+	 * Проверява дали програмата е инсталирана в сървъра
+	 *
+	 * @return NULL|string
+	 */
+	function checkConfig()
+	{
+	    $program = 'zbarimg';
+	    if (core_Os::isWindows()) {
+	        $res = exec("{$program} --help", $output, $code);
+	        if ($code != 0) {
+	            $haveError = TRUE;
+	        }
+	    } else {
+	        $res = exec("which {$program}", $output, $code);
+	        if (!$res) {
+	            $haveError = TRUE;
+	        }
+	    }
 	
+	    if ($haveError) {
+		
+	        return "Програмата '{$program}' не е инсталирана.";
+	    }
+	}
 }
 
