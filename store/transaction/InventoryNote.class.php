2ee969f979dc611a63726fb34833e18a1744ffe9
ivelin1012 <ivelin_pdimov@abv.bg>
2016-05-16 18:40:24 +0300

    Протокол за инвентаризация: добавена контировка

--- store/transaction/InventoryNote.class.php
+++ store/transaction/InventoryNote.class.php
@@ -39,8 +39,92 @@ class store_transaction_InventoryNote extends acc_DocumentTransactionSource
 			if(Mode::get('saveTransaction')){
 				$this->class->sync($rec);
 			}
+			
+			$result->entries = $this->getEntries($rec, $result->totalAmount);
 		}
 		
 		return $result;
+	}
+	
+	
+	
+	/**
+	 * Връща записите на транзакцията
+	 * 
+	 * Във всички случаи на констатирани излишъци (т.е. превишение на намереното над очакваното):
+	 * 
+	 * Dt 321. Суровини, материали, продукция, стоки         (Складове, Артикули)
+	 * Ct 799. Други извънредни приходи
+	 * 
+	 * В случаите на констатирани липси (т.е. превишение на очакваното над намереното), само когато НЕ Е чекнато 
+	 * "Начет МОЛ" (т.е. решено е, че липсите ще са за сметка на фирмата/собственика):
+	 * 
+	 * Dr 699. Други извънредни разходи
+	 * Ct 321. Суровини, материали, продукция, стоки         (Складове, Артикули)
+	 */
+	private function getEntries($rec, &$total)
+	{
+		$entries = array();
+		$errorArr = array();
+		
+		// Намираме тези редове, които няма да се начисляват към МОЛ
+		$dQuery = store_InventoryNoteSummary::getQuery();
+		$dQuery->where("#noteId = {$rec->id}");
+		$dQuery->where("#charge IS NULL");
+		while($dRec = $dQuery->fetch()){
+			
+			// Ако разликата е положителна, тоест имаме излишък
+			if($dRec->delta > 0){
+				// Артикулът трябва да има себестойност
+    			$amount = cat_Products::getWacAmountInStore($dRec->delta, $dRec->productId, $rec->valior, $rec->storeId);
+    			if(!$amount){
+    				$amount = $dRec->delta * cat_Products::getSelfValue($dRec->productId, NULL, $dRec->delta, $rec->valior);
+    			}
+    			//$amount = 0;
+    			
+    			if(!$amount){
+    				$errorArr[$dRec->productId] = cat_Products::getTitleById($dRec->productId);
+    			}
+    			
+    			$amount = round($amount, 2);
+    			$total += $amount;
+    			
+				$entries[] = array(
+						'amount' => $amount,
+						'debit' => array('321', array('store_Stores', $rec->storeId), 
+												array('cat_Products', $dRec->productId),
+												'quantity' => $dRec->delta),
+						'credit' => array('799'),
+						'reason' => 'Заприходени излишъци на стоково-материални запаси',
+				);
+				
+				// Ако разликата е отрицателна, имаме липса
+			} elseif($dRec->delta < 0){
+				$delta = abs($dRec->delta);
+				
+				$entries[] = array(
+						'debit' => array('699'),
+						'credit' => array('321', array('store_Stores', $rec->storeId),
+												 array('cat_Products', $dRec->productId),
+												'quantity' => $delta),
+						
+						'reason' => 'Отписани липси на стоково-материални запаси',
+				);
+			}
+		}
+		
+		if(Mode::get('saveTransaction')){
+			
+			// Ако има грешки, при контиране прекъсваме
+			if(count($errorArr)){
+				$str = implode(', ', $errorArr);
+				
+				$errorArr = implode(', ', $errorArr);
+				$message = "{$errorArr} |нямат себестойност|*";
+				acc_journal_RejectRedirect::expect(FALSE, $message);
+			}
+		}
+		
+		return $entries;
 	}
 }
