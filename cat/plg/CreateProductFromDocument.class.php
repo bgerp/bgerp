1796adb711b27579f2a6cad5b86fda0880fbd10c
ivelin1012 <ivelin_pdimov@abv.bg>
2016-06-06 16:33:15 +0300

    По правилен bugFix

--- cat/plg/CreateProductFromDocument.class.php
+++ cat/plg/CreateProductFromDocument.class.php
@@ -45,14 +45,14 @@ class cat_plg_CreateProductFromDocument extends core_Plugin
 	 */
 	public static function on_AfterGetRequiredRoles($mvc, &$requiredRoles, $action, $rec = NULL, $userId = NULL)
 	{
-		if($action == 'createproduct' && isset($rec)){
-			$masterRec = $mvc->Master->fetch($rec->{$mvc->masterKey});
-			if($masterRec->state == 'active' || $masterRec->state == 'active'){
-				$requiredRoles = 'no_one';
-				return;
-			}
-			
+		if($action == 'createproduct'){
 			if(isset($rec)){
+				$masterRec = $mvc->Master->fetch($rec->{$mvc->masterKey});
+				if($masterRec->state == 'active' || $masterRec->state == 'active'){
+					$requiredRoles = 'no_one';
+					return;
+				}
+				
 				$options = self::getProtoOptions($mvc->filterProtoByMeta, 1);
 				
 				if(isset($rec->cloneId)){
