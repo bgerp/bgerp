596b3fcd814147122799ae05b11175aef56b8682
GPetrova <gab4eto@gmail.com>
2016-07-15 16:26:14 +0300

    #C13088 т.1 Да се добавя всяко напомняне

--- cal/Reminders.class.php
+++ cal/Reminders.class.php
@@ -231,10 +231,6 @@ class cal_Reminders extends core_Master
         
         // Изпратена ли е нотификация?
         $this->FLD('notifySent', 'enum(no,yes)', 'caption=Изпратена нотификация,notNull,input=none');
-        
-        // Да влезе ли в календара?
-        $this->FLD('calNotify', 'enum(yes=Да, no=Не)', 'caption=Календар->Добавяне,notNull');
-
     }
 
 
@@ -666,7 +662,7 @@ class cal_Reminders extends core_Master
         $prefix = "RЕМ-{$id}";
         
         // Подготвяме запис за началната дата
-        if($rec->timeStart && $rec->timeStart >= $fromDate && $rec->timeStart <= $toDate && ($rec->state == 'active') && ($rec->calNotify = 'no' || $rec->calNotify = NULL)) {
+        if($rec->timeStart && $rec->timeStart >= $fromDate && $rec->timeStart <= $toDate && ($rec->state == 'active')) {
             
             if ($d > 0 && $m > 0) {
                 
@@ -696,7 +692,7 @@ class cal_Reminders extends core_Master
             }
         }
            
-        if ($rec->nextStartTime && $rec->nextStartTime >= $fromDate && $rec->nextStartTime <= $toDate && ($rec->state == 'active') && ($rec->calNotify = 'no' || $rec->calNotify = NULL)){
+        if ($rec->nextStartTime && $rec->nextStartTime >= $fromDate && $rec->nextStartTime <= $toDate && ($rec->state == 'active')){
         
             $calRec = new stdClass();
                
