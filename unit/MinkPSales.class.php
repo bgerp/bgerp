fd1e18508c316fe60b92d56b68bc08dca2353348
pavlina <pdainovska@experta.bg>
2016-07-18 17:41:35 +0300

    Тест - авансово плащане и приспадане

--- unit/MinkPSales.class.php
+++ unit/MinkPSales.class.php
@@ -675,5 +675,170 @@ class unit_MinkPSales extends core_Manager {
         }
         
         return $browser->getHtml();
-    }            
+    }  
+    
+    /**
+     * Продажба - схема с авансово плащане
+     * Проверка състояние чакащо плащане - не (платено)
+     */
+     
+    //http://localhost/unit_MinkPSales/CreateSaleAdvancePayment/
+    
+    function act_CreateSaleAdvancePayment()
+    {
+    
+        // Логваме се
+        $browser = $this->SetUp();
+    
+        //Отваряме папката на фирмата
+        $browser->click('Визитник');
+        $browser->click('F');
+        $Company = 'Фирма bgErp';
+        $browser->click($Company);
+        $browser->press('Папка');
+    
+        // нова продажба - проверка има ли бутон
+        if(strpos($browser->gettext(), 'Продажба')) {
+            $browser->press('Продажба');
+        } else {
+            $browser->press('Нов...');
+            $browser->press('Продажба');
+        }
+         
+        //$browser->hasText('Създаване на продажба');
+        $endhour=strtotime("+5 hours");
+        $enddate=strtotime("+1 Day");
+        $browser->setValue('deliveryTime[d]', date('d-m-Y', $enddate));
+        $browser->setValue('deliveryTime[t]', '10:30');
+    
+        $browser->setValue('reff', 'MinkP');
+        $browser->setValue('bankAccountId', '');
+        $browser->setValue('note', 'MinkPAdvancePayment');
+        $browser->setValue('paymentMethodId', "20% авансово и 80% преди експедиция");
+        $browser->setValue('chargeVat', "Включено ДДС в цените");
+         
+        // Записваме черновата на продажбата
+        $browser->press('Чернова');
+    
+        // Добавяме нов артикул
+        // За да смята добре с водещи нули - апостроф '023+045*03', '013+091*02'
+        $browser->press('Артикул');
+        $browser->setValue('productId', 'Чувал голям 50 L');
+        $browser->refresh('Запис');
+        $browser->setValue('packQuantity', '010+03*08');//34
+        $browser->setValue('packPrice', '01+3*0,8');//3.4
+        $browser->setValue('discount', 3);
+        $browser->press('Запис и Нов');
+         
+        // Записваме артикула и добавяме нов
+        $browser->setValue('productId', 'Плик 7 л');
+        $browser->refresh('Запис');
+        $browser->setValue('packQuantity', '03*048-0123');//21
+        $browser->setValue('packPrice', '010.20+0.3*08');//12.6
+        $browser->setValue('discount', 2);
+        $browser->press('Запис и Нов');
+        // Записваме артикула и добавяме нов
+    
+        $browser->setValue('productId', 'Други стоки');
+        $browser->refresh('Запис');
+        $browser->setValue('packQuantity', '023 + 017*02');//57
+        $browser->setValue('packPrice', '091 - 013*02');//65
+        $browser->setValue('discount', 3);
+    
+        // Записваме артикула и добавяме нов - услуга
+        $browser->press('Запис и Нов');
+        $browser->setValue('productId', 'Други услуги');
+        $browser->refresh('Запис');
+        $browser->setValue('packQuantity', 114);
+        $browser->setValue('packPrice', 1.1124);
+        $browser->setValue('discount', 1);
+        // Записваме артикула и добавяме нов - услуга
+        $browser->press('Запис и Нов');
+        $browser->setValue('productId', 'Транспорт');
+        $browser->refresh('Запис');
+        $browser->setValue('packQuantity', '160 / 05-03*08');//8
+        $browser->setValue('packPrice', '100/05+3*08');//44
+        $browser->setValue('discount', 1);
+    
+        // Записваме артикула
+        $browser->press('Запис');
+        // активираме продажбата
+        $browser->press('Активиране');
+        //return  $browser->getHtml();
+        //$browser->press('Активиране/Контиране');
+         
+        if(strpos($browser->gettext(), 'BGN 887,86')) {
+        } else {
+            return "Грешно авансово плащане";
+        }
+    
+        if(strpos($browser->gettext(), 'Четири хиляди четиристотин тридесет и девет BGN и 0,32')) {
+        } else {
+            return "Грешна обща сума";
+        }
+       
+        // ПБД
+        $browser->press('ПБД');
+        $browser->setValue('ownAccount', '#BG11CREX92603114548401');
+        $browser->setValue('amountDeal', '887,86');
+        $browser->press('Чернова');
+        $browser->press('Контиране');
+        
+        // Фактура
+        $browser->press('Фактура');
+        $browser->press('Чернова');
+        //return 'paymentType';
+        //$browser->setValue('paymentType', 'По банков път');
+        $browser->press('Контиране');
+        
+        // ПБД
+        $browser->press('ПБД');
+        $browser->setValue('ownAccount', '#BG11CREX92603114548401');
+        $browser->setValue('amountDeal', '3551,46');
+        $browser->press('Чернова');
+        //return $browser->getHtml();
+        $browser->press('Контиране');
+        
+        // експедиционно нареждане
+        $browser->press('Експедиране');
+        $browser->setValue('storeId', 'Склад 1');
+        $browser->setValue('template', 'Експедиционно нареждане с цени');
+        $browser->press('Чернова');
+        $browser->press('Контиране');
+        //if(strpos($browser->gettext(), 'Контиране')) {
+        //}
+        if(strpos($browser->gettext(), 'Три хиляди деветстотин шестдесет и пет BGN и 0,30')) {
+            // връща грешка, ако не е избрано ЕН с цени
+        } else {
+            return "Грешна сума в ЕН";
+        }
+         
+        // протокол
+        $browser->press('Пр. услуги');
+        $browser->press('Чернова');
+        $browser->press('Контиране');
+        //if(strpos($browser->gettext(), 'Контиране')) {
+        //  $browser->press('Контиране');
+        //}
+    
+        // Фактура
+        $browser->press('Фактура');
+        $browser->press('Чернова');
+        $browser->press('Контиране');
+        if(strpos($browser->gettext(), '-739,88')) {
+        } else {
+            return "Грешна сума за приспадане";
+        }
+     
+       // Приключване
+        $browser->press('Приключване');
+        $browser->setValue('valiorStrategy', 'Най-голям вальор в нишката');
+        $browser->press('Чернова');
+        //return  $browser->getHtml();
+        $browser->press('Контиране');
+        if(strpos($browser->gettext(), 'Чакащо плащане: Не')) {
+        } else {
+            return "Грешно чакащо плащане";
+        }
+    }
 }
