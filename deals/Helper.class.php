e82b90162848470a7dbbb0a89734358eb73703d6
HeBuTy <nevena@experta.bg>
2016-07-25 16:25:15 +0300

    Излишен интервал между числото и знака за процент

--- deals/Helper.class.php
+++ deals/Helper.class.php
@@ -229,7 +229,7 @@ abstract class deals_Helper
 				foreach ($values['vats'] as $percent => $vi){
 					if(is_object($vi)){
 						$index = str_replace('.', '', $percent);
-						$arr["vat{$index}"] = $percent * 100 . " %";
+						$arr["vat{$index}"] = $percent * 100 . "%";
 						$arr["vat{$index}Amount"] = $vi->amount * (($invoice) ? $currencyRate : 1);
 						$arr["vat{$index}AmountCurrencyId"] = ($invoice) ? $baseCurrency : $currencyId;
 							
