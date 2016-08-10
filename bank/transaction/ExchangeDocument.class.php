0bf790e8fb8e9ddc640063a64e298d4954ef433b
ivelin1012 <ivelin_pdimov@abv.bg>
2016-08-10 17:49:28 +0300

    Касови и банкова обмяна на валути:revert

--- bank/transaction/ExchangeDocument.class.php
+++ bank/transaction/ExchangeDocument.class.php
@@ -7,7 +7,7 @@
  * @category  bgerp
  * @package   bank
  * @author    Ivelin Dimov <ivelin_pdimov@abv.com>
- * @copyright 2006 - 2014 Experta OOD
+ * @copyright 2006 - 2016 Experta OOD
  * @license   GPL 3
  * @since     v 0.1
  * 
@@ -29,8 +29,18 @@ class bank_transaction_ExchangeDocument extends acc_DocumentTransactionSource
      * Имплементиране на интерфейсен метод (@see acc_TransactionSourceIntf)
      * Създава транзакция която се записва в Журнала, при контирането
      *
-     * Dt: 503. Разплащателни сметки             (Банкова сметка, Валута)
-     * Ct: 503. Разплащателни сметки             (Банкова сметка, Валута)
+     * Ако избраната валута е в основна валута
+     * 
+     * Dt: 503. Разплащателни сметки             (Банкови сметки, Валути)
+     * Ct: 503. Разплащателни сметки             (Банкови сметки, Валути)
+     * 
+     * Ако е в друга валута различна от основната
+     * 
+     * Dt: 503. Разплащателни сметки             (Банкови сметки, Валути)
+     * Ct: 481. Разчети по курсови разлики       (Валути)
+     * 
+     * Dt: 481. Разчети по курсови разлики       (Валути)
+     * Ct: 503. Разплащателни сметки             (Банкови сметки, Валути)
      */
     public function getTransaction($id)
     {
@@ -40,15 +50,27 @@ class bank_transaction_ExchangeDocument extends acc_DocumentTransactionSource
     	$cOwnAcc = bank_OwnAccounts::getOwnAccountInfo($rec->peroFrom, 'currencyId');
     	$dOwnAcc = bank_OwnAccounts::getOwnAccountInfo($rec->peroTo);
     
-    	$entry = array('debit' => array('503',
-					    			array('bank_OwnAccounts', $rec->peroTo),
-					    			array('currency_Currencies', $dOwnAcc->currencyId),
-					    			'quantity' => $rec->debitQuantity), 
-    				   'credit' => array('503',
-					    			array('bank_OwnAccounts', $rec->peroFrom),
-					    			array('currency_Currencies', $cOwnAcc->currencyId),
-					    			'quantity' => $rec->creditQuantity));
-    	$entry = array($entry);
+    	$toBank = array('503',
+    			array('bank_OwnAccounts', $rec->peroTo),
+    			array('currency_Currencies', $dOwnAcc->currencyId),
+    			'quantity' => $rec->debitQuantity);
+		
+		$fromBank = array('503',
+				array('bank_OwnAccounts', $rec->peroFrom),
+				array('currency_Currencies', $cOwnAcc->currencyId),
+				'quantity' => $rec->creditQuantity);
+
+		if($dOwnAcc->currencyId == acc_Periods::getBaseCurrencyId($rec->valior)){
+			$entry = array();
+			$entry[] = array('amount' => $rec->debitQuantity,
+					         'debit' => $toBank,
+							 'credit' => array('481', array('currency_Currencies', $cOwnAcc->currencyId), 'quantity' => $rec->creditQuantity));
+			$entry[] = array('debit' => array('481', array('currency_Currencies', $cOwnAcc->currencyId), 'quantity' => $rec->creditQuantity),
+    				    	 'credit' => $fromBank);
+		} else {
+			$entry = array('debit' => $toBank, 'credit' => $fromBank);
+			$entry = array($entry);
+		}
     
     	// Подготвяме информацията която ще записваме в Журнала
     	$result = (object)array(
