0cb5bce7c7df8569baa4b66d26b46371cc01945d
GPetrova <gab4eto@gmail.com>
2016-08-10 18:04:25 +0300

    Показване на графика на департамента в корицата на служителя.

--- crm/PersonsDetails.class.php
+++ crm/PersonsDetails.class.php
@@ -31,15 +31,28 @@ class crm_PersonsDetails extends core_Manager
 			$data->TabCaption = 'HR';
 		}
 		
-		$data->Cards = cls::get('crm_ext_IdCards');
-		if(isset($data->Codes)){
-			$data->Codes->prepareData($data);
-			if(crm_ext_Employees::haveRightFor('add', (object)array('personId' => $data->masterId))){
-				$data->addResourceUrl = array('crm_ext_Employees', 'add', 'personId' => $data->masterId, 'ret_url' => TRUE);
-			}
+		$eQuery = hr_EmployeeContracts::getQuery();
+		$eQuery->where("#personId = '{$data->masterId}'"); 
+		
+		$dQuery = hr_Departments::getQuery();
+		while($eRec = $eQuery->fetch()){
+		    $data->masterId = $eRec->departmentId;
+		    $data->Cycles = cls::get('hr_WorkingCycles');
 		}
 		
+		$data->Cards = cls::get('crm_ext_IdCards');
 		$data->Cards->prepareIdCard($data);
+		
+		if(isset($data->Cycles)){
+			$data->Cycles->prepareGrafic($data);
+		}
+		
+		if(isset($data->Codes)){
+		    $data->Codes->prepareData($data);
+		    if(crm_ext_Employees::haveRightFor('add', (object)array('personId' => $data->masterId))){
+		        $data->addResourceUrl = array('crm_ext_Employees', 'add', 'personId' => $data->masterId, 'ret_url' => TRUE);
+		    }
+		}
 	}
 	
 	
@@ -60,6 +73,13 @@ class crm_PersonsDetails extends core_Manager
 			$tpl->append($resTpl, 'CODE');
 		}
 		
+		if(isset($data->Cycles)){
+		    $resTpl = $data->Cycles->renderGrafic($data);
+		    $resTpl->removeBlock('legend');
+		    $resTpl->removeBlocks();
+		    $tpl->append($resTpl, 'CYCLES');
+		}
+
 		return $tpl;
 	}
 }
