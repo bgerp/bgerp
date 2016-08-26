bcd13ad61982e7ddb61f071e75144353305d92c7
mitko <mitko@experta.bg>
2016-08-26 11:16:56 +0300

    дублирана проверка на пакети

--- core/Setup.inc.php
+++ core/Setup.inc.php
@@ -634,7 +634,7 @@ if($step == 3) {
 
     // Необходими модули на PHP
     $log[] = 'h:Проверка за необходимите PHP модули:';
-    $requiredPhpModules = array('imap', 'mbstring', 'gd', 'calendar', 'Core', 'ctype', 'date',
+    $requiredPhpModules = array('imap', 'calendar', 'Core', 'ctype', 'date',
                                 'exif', 'filter', 'ftp', 'gd', 'iconv', 'json',
                                 'mbstring', 'mysqli', 'pcre', 'session', 'SimpleXML',
                                 'SPL', 'standard', 'tokenizer', 'xml', 'zlib', 'soap', 'curl');
