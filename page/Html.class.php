8588e9ef003aeef8996f3ccccbc5b3beae542699
Yuseinov <yuseinov@experta.bg>
2016-08-26 11:59:39 +0300

    page_Html bugFix да не дава warning

--- page/Html.class.php
+++ page/Html.class.php
@@ -124,8 +124,11 @@ class page_Html extends core_ET {
         jquery_Jquery::run($tpl, 'smartCenter();');
         jquery_Jquery::run($tpl, 'showTooltip();');
         jquery_Jquery::run($tpl, 'makeTooltipFromTitle();');
-
-        $url = json_encode(toUrl(array('bgerp_A', 'wp', 'pUrl' => getCurrentUrl()), 'local'));
+        
+        $pUrl = toUrl(getCurrentUrl(), TRUE);
+        $pUrl = urlencode($pUrl);
+        $url = json_encode(toUrl(array('bgerp_A', 'wp', 'pUrl' => $pUrl), 'local'));
+        
         jquery_Jquery::run($tpl, "detectScrollAndWp({$url});");
 
         
