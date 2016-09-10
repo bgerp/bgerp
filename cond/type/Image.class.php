5cb7cb91d500468a47f4e492b3edeece7d84fc9b
ivelin1012 <ivelin_pdimov@abv.bg>
2016-09-10 12:16:14 +0300

    Клас за параметър: изображение

---
+++ cond/type/Image.class.php
@@ -0,0 +1,32 @@
+<?php
+
+
+/**
+ * Тип за параметър 'Изображение'
+ *
+ *
+ * @category  bgerp
+ * @package   cond
+ * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
+ * @copyright 2006 - 2015 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ * @title     Изображение
+ */
+class cond_type_Image extends cond_type_Proto
+{
+	
+	
+	/**
+	 * Връща инстанция на типа
+	 *
+	 * @param int $paramId - ид на параметър
+	 * @return core_Type - готовия тип
+	 */
+	public function getType($rec)
+	{
+		$Type = core_Type::getByName('fileman_FileType(bucket=pictures)');
+	
+		return $Type;
+	}
+}
