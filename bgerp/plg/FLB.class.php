4e51f2b5a3c4d5a81f77d82ce319da0ee5c38395
ivelin1012 <ivelin_pdimov@abv.bg>
2016-09-13 13:39:09 +0300

    bugFix

--- bgerp/plg/FLB.class.php
+++ bgerp/plg/FLB.class.php
@@ -35,7 +35,7 @@ class bgerp_plg_FLB extends core_Plugin
 		
 		// Поле, в които се указват ролите, които могат да контират документи с обекта
 		if(!$mvc->getField($mvc->canActivateRoleFld, FALSE)){
-			$mvc->FLD($mvc->canActivateRoleFld, 'keylist(mvc=core_Roles,select=role,groupBy=type)', "caption=Контиране на документи->Роли,after={$mvc->canActivateUserFld}");
+			$mvc->FLD($mvc->canActivateRoleFld, 'keylist(mvc=core_Roles,select=role,groupBy=type)', "caption=Контиране на документи->Екипи,after={$mvc->canActivateUserFld}");
 		}
 		
 		// Поле, в които се указват потребителите, които могат да контират документи с обекта
@@ -45,7 +45,7 @@ class bgerp_plg_FLB extends core_Plugin
 		
 		// Поле, в които се указват rolite, които могат да избират обекта в документи
 		if(!$mvc->getField($mvc->canSelectRoleFld, FALSE)){
-			$mvc->FLD($mvc->canSelectRoleFld, 'keylist(mvc=core_Roles,select=role,groupBy=type)', "caption=Използване в документи->Роли,after={$mvc->canSelectUserFld}");
+			$mvc->FLD($mvc->canSelectRoleFld, 'keylist(mvc=core_Roles,select=role,groupBy=type)', "caption=Използване в документи->Екипи,after={$mvc->canSelectUserFld}");
 		}
 		
 		// Трябва да е към корица
@@ -66,10 +66,6 @@ class bgerp_plg_FLB extends core_Plugin
 		$rQuery->where("#type = 'team'");
 		while($rRec = $rQuery->fetch()){
 			$roles[$rRec->id] = $rRec->role;
-		}
-		
-		if(count($roles)){
-			$roles = array('x' => (object)array('group' => TRUE, 'title' => tr('Екип'))) + $roles;
 		}
 		
 		$form->setSuggestions($mvc->canSelectRoleFld, $roles);
