e061a52eadf598f95733dadb348aff272c8891cf
HeBuTy <nevena@experta.bg>
2016-10-12 14:24:59 +0300

    Нова версия на chart.js

--- chartjs/Adapter.class.php
+++ chartjs/Adapter.class.php
@@ -38,24 +38,22 @@ class chartjs_Adapter extends  core_Mvc
      */
     static function prepare_($data, $chartType)
     {
-
         $tpl = new ET();
         static $chartCnt;
         if(!$chartCnt) $orgChartCnt = 0;
         $chartCnt++;
         $idChart = 'myChart' . $chartCnt;
 
-        $chart = ht::createElement('canvas',  array('id' => $idChart, "width" => "300", 'height' => '270'), $tpl);
+        $chart = ht::createElement('canvas',  array('id' => $idChart, "width" => "700", 'height' => '570'), $tpl);
         $tpl->append("<div class='chartHolder chart-$chartType'>" . $chart . "</div>");
 
         $tpl->push('chartjs/lib/preparechart.js', 'JS');
-        $tpl->push('chartjs/1.0.2/Chart.min.js', 'JS');
-        $tpl->push('chartjs/lib/numeral.min.js', 'JS');
+        $tpl->push('chartjs/' . chartjs_Setup::get('VERSION') . '/Chart.min.js', 'JS');
         $tpl->push('chartjs/lib/Chart.css', 'CSS');
 
         $data = json_encode($data);
 
-        jquery_Jquery::run($tpl, "prepareChart('$idChart', $data, '$chartType');");
+        jquery_Jquery::run($tpl, "prepareChart('$idChart', $data, '{$chartType}');");
 
         return $tpl;
 	}
--- chartjs/lib/preparechart.js
+++ chartjs/lib/preparechart.js
@@ -1,5 +1,5 @@
 function prepareChart( idElem, chartData, charType) {
-
+    var ctx = document.getElementById(idElem);
     if (!$(chartData).length) return;
 
     // цветове за графиките
@@ -9,105 +9,94 @@ function prepareChart( idElem, chartData, charType) {
     var fillColor = ["rgba(10,161,255,0.5)", "rgba(252,162,53,0.5)", "rgba(112,193,15,0.5)","rgba(255,33,23,0.5)", "rgba(51,254,224,0.5)",
         "rgba(200,200,200,0.5)" ,"rgba(34,101,10,0.5)", "rgba(141,12,201,0.5)","rgba(247,247,17,0.5)", "rgba(50,35,183,0.5)","rgba(99,41,22,0.5)", "rgba(227,113,196,0.5)", "rgba(203,246,144,0.5)", "rgba(203,144,,246,0.5)"];
 
+
     // ако чертаем торта
     if (charType == "pie") {
-        var data = [];
-        // от получените данни генериране необходимата структура
 
         var title = chartData.legendTitle;
         var suffix = chartData.suffix;
-        if(!title) title = "";
-        if(!suffix) suffix = "";
-        var i = 0;
+        if (!title) title = "";
+        if (!suffix) suffix = "";
+        var value = [];
+        var label = [];
+
         jQuery.each(chartData.info, function (key, val) {
-            var temp = {};
-            temp.value = val;
-            temp.label = key;
-            temp.color = colors[i];
-            data.push(temp);
-            i++;
+            label.push(key);
+            value.push(val);
         });
 
-        // чертаем тортата с легенда към нея
-        var pie = $('#' + idElem).get(0).getContext('2d');
-        var myNewChart = new Chart(pie).Pie(data, {
-            responsive: true, animation: false,
-            tooltipTemplate:  "<%=label%>: <%= numeral(value).format('(00[.]00)') %>" + suffix + " (<%= numeral(circumference / 6.283).format('(0[.][00]%)') %>)"  ,
-            legendTemplate: "<ul class=\"chart-legend <%=name.toLowerCase()%>-legend\"><li class=\"legend-title\">" + title + "</li>" +
-            "<% for (var i=0; i<segments.length; i++){%><li>" +
-            "<span style=\"background-color:<%=segments[i].fillColor%> !important\"></span>" +
-            "<%if(segments[i].label){%><%=segments[i].label%><%}%>: <%if(segments[i].value){%><%= numeral(segments[i].value).format('(00[.]00)') %>"+ suffix + " (<%= numeral(segments[i].circumference / 6.283).format('(0[.][00]%)') %>)<%}%></li><%}%></ul>"
+        new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: label,
+                datasets: [{
+                    data: value,
+                    backgroundColor: fillColor,
+                    borderColor: colors,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                legend: {
+                    display: true,
+                    position: 'right',
+                },
+            }
         });
-        // добавяме легендата
-        $('#' + idElem).parent().append(myNewChart.generateLegend());
-
     } else if(charType == "bar" || charType == "line") {
         var res = {};
+
         res.labels = chartData.labels;
         res.datasets = [];
         var title = chartData.legendTitle;
         var len = Object.keys(chartData.values).length;
 
-        for(var i=0; i < len; i++) {
-            if(!res.datasets[i])
-                res.datasets[i]= {};
+        for (var i = 0; i < 3; i++) {
+            if (!res.datasets[i])
+                res.datasets[i] = {};
 
-            if(!res.datasets[i].label)
+            if (!res.datasets[i].label)
                 res.datasets[i].label = [];
 
-            if(!res.datasets[i].pointColor)
+            if (!res.datasets[i].pointColor)
                 res.datasets[i].pointColor = [];
 
-            if(!res.datasets[i].strokeColor)
+            if (!res.datasets[i].strokeColor)
                 res.datasets[i].strokeColor = [];
 
-            if(!res.datasets[i].fillColor)
+            if (!res.datasets[i].fillColor)
                 res.datasets[i].fillColor = [];
 
-            if(!res.datasets[i].data)
+            if (!res.datasets[i].data)
                 res.datasets[i].data = [];
         }
         i = 0;
+
         // от получените данни генериране необходимата структура
         jQuery.each(chartData.values, function (key, value) {
-                res.datasets[i].label = key;
-                res.datasets[i].data = value;
-                res.datasets[i].pointColor = colors[i];
-                res.datasets[i].strokeColor = colors[i];
+            res.datasets[i].label = key;
+            res.datasets[i].data = value;
+            res.datasets[i].borderColor = colors[i];
+            res.datasets[i].backgroundColor = fillColor[i];
+            res.datasets[i].borderWidth = 1;
 
-                // ако графиката е тип Барове, ни трябва и фон
-                if(charType == 'bar'){
-                    res.datasets[i].fillColor = fillColor[i];
-                }
+            // ако графиката е тип Барове, ни трябва и фон
+            if (charType == 'bar') {
+                res.datasets[i].fillColor = fillColor[i];
+            }
             i++;
         });
 
-        // чертаем графиката, в зависимост от типа
-        var chart = $('#' + idElem).get(0).getContext('2d');
-        if(charType == "bar" ){
-            var myNewChart = new Chart(chart).Bar(res, {
-                responsive: true, animation: false,
-                tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
-                multiTooltipTemplate: "<%= datasetLabel %> - <%= value %>",
-                legendTemplate: "<ul class=\" chart-legend <%=name.toLowerCase()%>-legend\"><li class=\"legend-title\">" + title + "</li>" +
-                "<% for (var i=0; i<datasets.length; i++){%><li>" +
-                "<span style=\"background-color:<%=datasets[i].fillColor%> !important; border: 2px solid <%=datasets[i].strokeColor%> !important\"></span>" +
-                "<%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-
-            });
-
-        } else {
-            var myNewChart = new Chart(chart).Line(res, {
-                responsive: true, animation: false, datasetFill: false,
-                tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
-                multiTooltipTemplate: "<%= datasetLabel %> - <%= value %>",
-                legendTemplate: "<ul class=\" chart-legend <%=name.toLowerCase()%>-legend\"><li class=\"legend-title\">" + title + "</li>" +
-                "<% for (var i=0; i<datasets.length; i++){%><li>" +
-                "<span style=\"background-color:<%=datasets[i].strokeColor%> !important;\"></span>" +
-                "<%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-            });
-        }
-
-        $('#' + idElem).parent().append(myNewChart.generateLegend());
+        new Chart(ctx, {
+            type: charType,
+            data: res,
+            options: {
+                scales: {
+                    xAxes: [{
+                        display: true
+                    }]
+                }
+            }
+        });
     }
 }
