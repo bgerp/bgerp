4e42f86e19e5a991850babf0d824a7a962782a98
HeBuTy <nevena@experta.bg>
2016-10-14 10:36:07 +0300

    bugFix цветове при колаборатор

--- cms/DefaultTheme.class.php
+++ cms/DefaultTheme.class.php
@@ -200,7 +200,7 @@ class cms_DefaultTheme extends core_ProtoInner {
 
         // Цвятове за линковете и h2 заглавията
         $css .= "\n    #cmsNavigation .nav_item a { color: #{$fontColor};}";
-        $css .= "\n    #all #maincontent a:visited{ color: #{$visitedFontColor} !important;}";
+        $css .= "\n    #all #maincontent .richtext a:visited, #all #maincontent .articles-menu a:visited, #all #maincontent .blogm-categories a:visited{ color: #{$visitedFontColor};}";
         $css .= "\n    #cmsNavigation .sel_page a, #cmsNavigation a:hover {background-color: #{$bgcolorActive} !important; border: 1px solid #{$linkBorder} !important; color: #{$fontColor}}";
         $css .= "\n    a:hover, .eshop-group-button:hover .eshop-group-button-title a {color: #{$fontColor};}";
         $css .= "\n    h2 {background-color:#{$bgcolorActive} !important; padding: 5px 10px;border:none !important}";
