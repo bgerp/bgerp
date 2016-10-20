8932a299d440a4d880454f4adaef7f7bf41125a6
ivelin1012 <ivelin_pdimov@abv.bg>
2016-10-20 14:35:00 +0300

    продажби: да могат чакащите да стават на чернови

--- deals/DealMaster.class.php
+++ deals/DealMaster.class.php
@@ -1606,6 +1606,8 @@ abstract class deals_DealMaster extends deals_DealBase
     			$Detail = cls::get($mvc->mainDetail);
     			if(!$Detail->fetch("#{$Detail->masterKey} = {$rec->id}")){
     				$res = 'no_one';
+    			} elseif(core_Users::isPowerUser($userId) && $rec->state == 'draft'){
+    				$res = 'no_one';
     			}
     		}
     	}
