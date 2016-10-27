5b12d8d8063ba8cf51eade572b6dd8cb0c6eeb05
GPetrova <gab4eto@gmail.com>
2016-10-27 18:19:25 +0300

    Ако обикновен потребител създава документа в папката си, то трябва задължително да сподели ceo или trz

--- trz/Sickdays.class.php
+++ trz/Sickdays.class.php
@@ -230,6 +230,8 @@ class trz_Sickdays extends core_Master
         if ($rec->folderId && $folderClass == 'crm_Persons') {
 	        $rec->personId = doc_Folders::fetchCoverId($rec->folderId);
 	        $data->form->setReadonly('personId');
+	        
+	        $data->form->fields['sharedUsers']->mandatory = 'mandatory';
         } 
     }
     
