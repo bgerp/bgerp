052f8f56b820b7aad8450c286fe4cb643b3a5e66
Yuseinov <yuseinov@experta.bg>
2016-12-06 15:04:59 +0200

    doc_Threads - при поправка да се променя броя, ако има стойност

--- doc/Threads.class.php
+++ doc/Threads.class.php
@@ -443,8 +443,9 @@ class doc_Threads extends core_Manager
                 
                 // Ако се различава броя на документите
                 $cCnt = $cQuery->count();
-                if ($cCnt != $rec->allDocCnt) {
+                if ($cCnt && ($cCnt != $rec->allDocCnt)) {
                     self::logNotice("Променен брой на документите от {$rec->allDocCnt} на {$cCnt}", $rec->id);
+                    self::updateThread($rec->id);
                     $resArr['allDocCnt']++;
                 }
                 
