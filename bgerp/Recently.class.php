5cb97e8104a91f77470c21f44d95d24558638511
GPetrova <gab4eto@gmail.com>
2016-12-06 15:31:47 +0200

    bgerp: Премахване на plg_RowTools2 зарди портала

--- bgerp/Recently.class.php
+++ bgerp/Recently.class.php
@@ -33,7 +33,7 @@ class bgerp_Recently extends core_Manager
     /**
      * Необходими мениджъри
      */
-    var $loadList = 'bgerp_Wrapper, plg_RowTools2, plg_GroupByDate, plg_Search, bgerp_RefreshRowsPlg';
+    var $loadList = 'bgerp_Wrapper, plg_RowTools, plg_GroupByDate, plg_Search, bgerp_RefreshRowsPlg';
     
     
     /**
