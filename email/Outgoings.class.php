636fecf6379e75773c5b3d9eb17eb800b5234215
GPetrova <gab4eto@gmail.com>
2016-12-07 15:36:49 +0200

    email: Преминаване от plg_RowTools към plg_RowTools2

--- email/Outgoings.class.php
+++ email/Outgoings.class.php
@@ -122,7 +122,7 @@ class email_Outgoings extends core_Master
     /**
      * Плъгини за зареждане
      */
-    var $loadList = 'email_Wrapper, doc_DocumentPlg, plg_RowTools, 
+    var $loadList = 'email_Wrapper, doc_DocumentPlg, plg_RowTools2, 
         plg_Printing, email_plg_Document, doc_ActivatePlg, 
         bgerp_plg_Blank,  plg_Search, recently_Plugin, plg_Clone';
     
