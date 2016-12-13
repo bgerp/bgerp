859905f5be64d538b5b78f4d28406418e607543e
ivelin1012 <ivelin_pdimov@abv.bg>
2016-12-13 18:01:59 +0200

    sales_Setup:bugFix

--- sales/Setup.class.php
+++ sales/Setup.class.php
@@ -326,9 +326,12 @@ class sales_Setup extends core_ProtoSetup
     		core_Packs::setConfig('sales', array('SALE_INVOICE_DEF_TPL_EN' => $key));
     	}
     	
+    	
+    	$defRoles = (core_Packs::isInstalled('colab')) ? 'sales,ceo,distributor' : 'sales,ceo';
+    	
     	// Добавяне на дефолтни роли за бутоните
     	foreach (array('SALES_ADD_BY_PRODUCT_BTN', 'SALES_ADD_BY_CREATE_BTN', 'SALES_ADD_BY_LIST_BTN', 'SALES_ADD_BY_IMPORT_BTN') as $const){
-    		$roles = ($const == 'SALES_ADD_BY_IMPORT_BTN') ? 'sales,ceo' : 'sales,ceo,distributor';
+    		$roles = ($const == 'SALES_ADD_BY_IMPORT_BTN') ? 'sales,ceo' : $defRoles;
     		if(strlen($config->{$const}) === 0){
     			$keylist = core_Roles::getRolesAsKeylist($roles);
     			core_Packs::setConfig('sales', array($const => $keylist));
