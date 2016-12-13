1d851852300c21b6fe6726e5f63b60f6221b289a
Milen Georgiev <milen@experta.bg>
2016-12-13 23:59:06 +0200

    Махнат wrapper при първоначалната регистрация

--- core/Users.class.php
+++ core/Users.class.php
@@ -622,6 +622,7 @@ class core_Users extends core_Manager
     		$data->form->title = 'Първоначална регистрация на администратор';
             cls::load('crm_Setup');
             $data->form->setDefault('country', drdata_Countries::getIdByName(BGERP_OWN_COMPANY_COUNTRY));
+            unset($mvc->_plugins['plg_SystemWrapper']);
     	}
     }
     
