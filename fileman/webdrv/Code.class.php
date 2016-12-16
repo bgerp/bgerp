0c3c43eeb1f34313431c01dd188dfca6429506d9
Yuseinov <yuseinov@experta.bg>
2016-12-16 16:47:13 +0200

    fileman_webdrv_Code #C15348 - да може да работи с пътища

--- fileman/webdrv/Code.class.php
+++ fileman/webdrv/Code.class.php
@@ -89,9 +89,12 @@ class fileman_webdrv_Code extends fileman_webdrv_Generic
      * Извлича текстовата част от файла
      * 
      * @param object $fRec - Записите за файла
+     * 
+     * @return NULL|string
      */
     static function extractText($fRec)
     {
-        fileman_webdrv_Text::extractText($fRec);
+        
+        return fileman_webdrv_Text::extractText($fRec);
     }
 }
