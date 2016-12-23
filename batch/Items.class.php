e0fae0e7804c6c6a4f91e375416bd2b893e138c6
ivelin1012 <ivelin_pdimov@abv.bg>
2016-12-23 14:24:29 +0200

    batch_Items:bugFix

--- batch/Items.class.php
+++ batch/Items.class.php
@@ -538,6 +538,9 @@ class batch_Items extends core_Master {
     	$date = (isset($date)) ? $date : dt::today();
     	$res = array();
     	
+    	$def = batch_Defs::getBatchDef($productId);
+    	if(!$def) return $res;
+    	
     	$date = dt::addDays(-1, $date);
     	$date = dt::verbal2mysql($date, FALSE);
     	
@@ -567,7 +570,6 @@ class batch_Items extends core_Master {
     	}
     	
     	// Намерените партиди се подават на партидната дефиниция, ако иска да ги преподреди
-    	$def = batch_Defs::getBatchDef($productId);
     	$def->orderBatchesInStore($res, $storeId, $date);
     	
     	// Връщане на намерените партиди
