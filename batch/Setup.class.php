9708aadcf1b858b2e32eb18313dc288234434d74
ivelin1012 <ivelin_pdimov@abv.bg>
2017-01-16 17:29:34 +0200

    Протокол за производство bugFix

--- batch/Setup.class.php
+++ batch/Setup.class.php
@@ -145,6 +145,7 @@ class batch_Setup extends core_ProtoSetup
         $html .= $Plugins->installPlugin('Партидни движения на протоколите за отговорно пазене', 'batch_plg_DocumentMovement', 'store_ConsignmentProtocols', 'private');
         
         $html .= $Plugins->installPlugin('Партидни движения на протокола за инвентаризация', 'batch_plg_InventoryNotes', 'store_InventoryNoteDetails', 'private');
+        $html .= $Plugins->installPlugin('Партидни движения на протокола за производство', 'batch_plg_DocumentMovementDetail', 'planning_DirectProductionNote', 'private');
         
         // Обновяване на протокола за инвентаризация да мус е сетъпне модела
         $Notes = cls::get('store_InventoryNotes');
--- planning/DirectProductionNote.class.php
+++ planning/DirectProductionNote.class.php
@@ -42,7 +42,7 @@ class planning_DirectProductionNote extends planning_ProductionDocument
 	 * , acc_plg_Contable
 	 */
 	public $loadList = 'plg_RowTools2, planning_Wrapper, acc_plg_DocumentSummary, acc_plg_Contable,
-                    doc_DocumentPlg, plg_Printing, plg_Clone, plg_Search, bgerp_plg_Blank, batch_plg_DocumentMovementDetail';
+                    doc_DocumentPlg, plg_Printing, plg_Clone, plg_Search, bgerp_plg_Blank';
 	
 	
 	/**
