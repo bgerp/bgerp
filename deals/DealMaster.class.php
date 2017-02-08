d6f86b12bb7b99e255f6ffd746b5ad7dae56e256
ivelin1012 <ivelin_pdimov@abv.bg>
2017-02-08 13:00:18 +0200

    Ф-ри махната спорна автоматизация + бутона за налкиване от договора да трие и редовете

--- deals/DealMaster.class.php
+++ deals/DealMaster.class.php
@@ -1753,36 +1753,6 @@ abstract class deals_DealMaster extends deals_DealBase
     		$details[] = $dRec;
     	}
     	
-    	if(is_array($info->dealProducts)){
-    		$replaced = array();
-    		
-    		foreach ($details as $index => $d1){
-    			$totalSum = $totalQuantity = 0;
-    			$pId = $d1->productId;
-    			
-    			// Сумиране на договорените артикули
-    			$res = array_filter($info->dealProducts, function (&$e) use ($pId, &$totalSum, &$totalQuantity) {
-    				if($e->productId == $pId){
-    					$totalSum += ($e->quantity * $e->price * (1 - $e->discount));
-    					$totalQuantity += $e->quantity;
-    					$e->quantity /= $e->quantityInPack;
-    					return TRUE;
-    				}
-    				
-    				return FALSE;
-    			});
-    			
-    			// Ако сумата и к-во им отговарят точно на това от счетоводството подменят се
-    			if(round($totalSum, 2) == round($d1->amount, 2) && round($totalQuantity, 5) == round($d1->quantity, 5)){
-    				$replaced = array_merge($replaced, $res);
-    			} else {
-    				$replaced[] = $d1;
-    			}
-    		}
-    		
-    		return $replaced;
-    	}
-    	
     	return $details;
     }
     
