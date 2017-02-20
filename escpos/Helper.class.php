fa7cac81ec56479ef6b8eaa5850ef0a1d2a7496a
ivelin1012 <ivelin_pdimov@abv.bg>
2017-02-20 17:55:07 +0200

    Печат на продажба за принтер

--- escpos/Helper.class.php
+++ escpos/Helper.class.php
@@ -38,6 +38,49 @@ class escpos_Helper
     }
     
     
+    private static function getSalePreview($id, $data)
+    {
+    	$tpl = getTplFromFile('sales/tpl/sales/SalePrint.shtml');
+    	$row = $data->row;
+    	
+    	foreach (array('contragentName', 'MyCompany', 'MyAddress', 'closedDocuments', 'caseId', 'deliveryLocationId', 'bankAccountId') as $fld){
+    		if(!empty($data->rec->{$fld}) && $row->{$fld} instanceof core_ET){
+    			$row->{$fld} = $row->{$fld}->getContent();
+    		}
+    		
+    		if(!empty($row->{$fld})){
+				$row->{$fld} = strip_tags($row->{$fld});
+			}
+		}
+		
+    	$tpl->placeObject($row);
+    	
+    	$count = 0;
+    	$detailRecs = $data->sales_SalesDetails->recs;
+    	$detailRows = $data->sales_SalesDetails->rows;
+    	$Double = core_Type::getByName('double(decimals=2)');
+    	$DoubleQ = core_Type::getByName('double(decimals=3)');
+    	
+    	$block = $tpl->getBlock('PRODUCT_BLOCK');
+    	foreach ($detailRows as $id => $dRow){
+    		$dRec = $detailRecs[$id];
+    		$dRow->numb += 1;
+    		$dRow->productId = cat_Products::getTitleById($dRec->productId);
+    		$dRow->packQuantity = $DoubleQ->toVerbal($dRec->packQuantity);
+    		$dRow->packPrice = $Double->toVerbal($dRec->packPrice);
+    		$dRow->amount = $Double->toVerbal($dRec->amount);
+    		
+    		$b = clone $block;
+    		$b->placeObject($dRow);
+    		$b->removeBlocks();
+    		$b->removePlaces();
+    		$b->append2Master();
+    	}
+    	
+    	return $tpl;
+    }
+    
+    
     
     /**
      * Подготвя данните за отпечатване
@@ -49,21 +92,26 @@ class escpos_Helper
      */
     protected static function preparePrintView($clsInst, $id)
     {
-        // TODO - тестово
-        $str = "<c F b>{$clsInst->singleTitle} №{$id}/28.02.17" .
-                        "<p><r32 =>" .
-                        "<p b>1.<l3 b>Кисело мляко" .
-                        "<p><l4>2.00<l12>х 0.80<r32>= 1.60" .
-                        "<p b>2.<l3 b>Хляб \"Добруджа\"" . "<l f> | годност: 03.03" .
-                        "<p><l4>2.00<l12>х 0.80<r32>= 1.60" .
-                        "<p b>3.<l3 b>Минерална вода" .
-                        "<p><l4>2.00<l12>х 0.80<r32>= 1.60" .
-                        "<p><r32 =>" .
-                        "<p><r29 F b>Общо: 34.23 лв.";
-        
-        // @todo -  Трябва да се направи за $clsInst и съответното $id
-        
-        return $str;
+    	expect($Inst = cls::get($clsInst));
+    	
+    	Mode::push('text', 'php');
+    	$data = Request::forward(array('Ctr' => $Inst->className, 'Act' => 'single', 'id' => $id));
+    	Mode::pop('text');
+    	expect($data);
+    	
+    	switch($Inst){
+    		case $Inst instanceof sales_Sales:
+    			$str = self::getSalePreview($id, $data);
+    			break;
+    		case $Inst instanceof store_ShipmentOrders:
+    			$str = self::getSoPreview($id, $data);
+    			break;
+    		case $Inst instanceof sales_Invoices:
+    			$str = self::getInvPreview($id, $data);
+    			break;
+    	}
+    	
+    	return $str;
     }
     
     
