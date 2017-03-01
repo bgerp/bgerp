2acac4fdd3d1d555801d7dd5d2725914c18f285b
ivelin1012 <ivelin_pdimov@abv.bg>
2017-03-01 17:41:33 +0200

    касови, банкови и складови документи:филтър по състояние

--- store/DocumentMaster.class.php
+++ store/DocumentMaster.class.php
@@ -622,4 +622,28 @@ abstract class store_DocumentMaster extends core_Master
     		$rec->originId = doc_Threads::getFirstContainerId($rec->threadId);
     	}
     }
+    
+    
+    /**
+     *  Подготовка на филтър формата
+     */
+    public static function on_AfterPrepareListFilter($mvc, $data)
+    {
+    	if(!Request::get('Rejected', 'int')){
+    		$data->listFilter->FNC('dState', 'enum(all=Всички, pending=Заявка, draft=Чернова, active=Контиран)', 'caption=Състояние,input,silent');
+    		$data->listFilter->showFields .= ',dState';
+    		$data->listFilter->input();
+    		$data->listFilter->setDefault('dState', 'all');
+    		 
+    		if($rec = $data->listFilter->rec){
+    
+    			// Филтър по състояние
+    			if($rec->dState){
+    				if($rec->dState != 'all'){
+    					$data->query->where("#state = '{$rec->dState}'");
+    				}
+    			}
+    		}
+    	}
+    }
 }
