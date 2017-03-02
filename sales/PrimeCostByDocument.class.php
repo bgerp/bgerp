b5ce84b4436c18230d016349de1dfa8f3a513978
ivelin1012 <ivelin_pdimov@abv.bg>
2017-03-02 14:39:58 +0200

    Индикатори за делти: рефакторинг

--- sales/PrimeCostByDocument.class.php
+++ sales/PrimeCostByDocument.class.php
@@ -65,6 +65,12 @@ class sales_PrimeCostByDocument extends core_Manager
 	
 	
     /**
+     * Работен кеш
+     */
+    public static $cache = array();
+    
+    
+    /**
      * Описание на модела (таблицата)
      */
     function description()
@@ -140,36 +146,49 @@ class sales_PrimeCostByDocument extends core_Manager
 	
 	
 	/**
-	 * Ако в документите не е зададен, кой е дилъра на сделката
-	 * се опитваме да го намерим, от папката
+	 * Намиране на дилъра и инициатора на документа, данните се кешират
 	 * 
-	 * @param stdClass $rec
-	 * @return int $dealerId
+	 * @param int $docClass  - ид на клас
+	 * @param int $docId     - ид на документ
+	 * @return array
+	 * 			['dealerId']    - Ид на визитката на дилъра
+	 * 			['initiatorId'] - Ид на визитката на инициатора
 	 */
-	public static function getDealerId($rec)
+	public static function getDealerAndInitiatorId($docClass, $docId)
 	{
-	    
-	    $Class = cls::get($rec->detailClassId);
-	     
-	    $dRec = $Class->fetch($rec->detailRecId);
-	     
-	    $masterRec = $Class->Master->fetch($dRec->{$Class->masterKey});
-	    
-	    $firstDoc = doc_Threads::getFirstDocument($masterRec->threadId);
-
-	    $dealerId = $firstDoc->rec()->dealerId;
-	    
-	    // Ако няма, но отговорника на папката е търговец - него
-	    if(empty($dealerId)){
-	        $inCharge = doc_Folders::fetchField($firstDoc->rec()->folderId, 'inCharge');
-	        if(core_Users::haveRole('sales', $inCharge)){
-	            $dealerId = $inCharge;
-	        }
-	    } 
-	    
-	    $dealerId = crm_Profiles::fetchField("#userId = {$dealerId}", "personId");
-
-	    return $dealerId;
+		// Ако няма кеширани данни
+		if(!isset(static::$cache[$docClass][$docId])){
+			
+			// Кой е първия документ в нишката
+			$Class = cls::get($docClass);
+			$threadId = $Class->fetchField($docId, 'threadId');
+			$firstDoc = doc_Threads::getFirstDocument($threadId);
+			$firstDocRec = $firstDoc->fetch('dealerId, initiatorId, folderId');
+			
+			// Ако няма дилър това е отговорника на папката ако има права `sales`
+			if(empty($firstDocRec->dealerId)){
+				$inCharge = doc_Folders::fetchField($firstDocRec->folderId, 'inCharge');
+	        	if(core_Users::haveRole('sales', $inCharge)){
+	           		 $firstDocRec->dealerId = $inCharge;
+	        	}
+			}
+			
+			// Ид-та на визитките на дилъра и инициатора
+			$dealerId = $initiatorId = NULL;
+			if(isset($firstDocRec->dealerId)){
+				$dealerId = crm_Profiles::fetchField("#userId = {$firstDocRec->dealerId}", "personId");
+			}
+			
+			if(isset($firstDocRec->initiatorId)){
+				$initiatorId = crm_Profiles::fetchField("#userId = {$firstDocRec->initiatorId}", "personId");
+			}
+			
+			// Кеширане на дилъра и инициатора на документа
+			static::$cache[$docClass][$docId] = array('dealerId' => $dealerId, 'initiatorId' => $initiatorId);
+		}
+		
+		// Връщане на кешираните данни
+		return static::$cache[$docClass][$docId];
 	}
 	
 	
@@ -181,8 +200,94 @@ class sales_PrimeCostByDocument extends core_Manager
 	 */
 	public static function getIndicatorValues($timeline)
 	{
-        // Чака за имплементация
-        return array();
+		$iQuery = self::getQuery();
+		
+		// Кои документи ще се проверяват дали са променяни
+		$documents = array('sales_Sales' => 'sales_SalesDetails', 'sales_Services' => 'sales_ServicesDetails', 'purchase_Services' => 'purchase_ServicesDetails', 'store_ShipmentOrders' => 'store_ShipmentOrderDetails', 'store_Receipts' => 'store_ReceiptDetails');
+		$or = FALSE;
+		
+		$masters = array();
+		foreach ($documents as $Master => $Detail){
+			$masterClasId = $Master::getClassId();
+			$Detail = cls::get($Detail);
+			$detailClassId = $Detail->getClassId();
+			
+			// За всеки документ, му извличаме детайлите ако той е променян след $timeline
+			$dQuery = $Detail->getQuery();
+			$dQuery->EXT('state', $Master, "externalName=state,externalKey={$Detail->masterKey}");
+			$dQuery->EXT('modifiedOn', $Master, "externalName=modifiedOn,externalKey={$Detail->masterKey}");
+			$dQuery->where("#modifiedOn >= '{$timeline}'");
+			$dQuery->where("#state != 'draft' AND #state != 'pending'");
+			
+			$fields = "modifiedOn,{$Detail->masterKey},state";
+			if($Master != 'sales_Sales'){
+				$dQuery->EXT('isReverse', $Master, "externalName=isReverse,externalKey={$Detail->masterKey}");
+				$fields .= ",isReverse";
+			}
+			
+			$ids = array();
+			$dQuery->show($fields);
+			
+			// Извличане на ид-та на детайлите му и кеш на мастър данните
+			while($dRec = $dQuery->fetch()){
+				$masters[$detailClassId][$dRec->id] = array($masterClasId, $dRec->{$Detail->masterKey}, $dRec->state, $dRec->isReverse);
+				$ids[$dRec->id] = $dRec->id;
+			}
+			
+			// Ако има детайли от модела ще търсим точно записите от детайли на документи променяни след timeline
+			if(count($ids)){
+				$ids = implode(',', $ids);
+				$iQuery->where("#detailClassId = {$Detail->getClassId()} AND #detailRecId IN ($ids)", $or);
+				$or = TRUE;
+			}
+		}
+		
+		// Ако не е намерен променен документ, връща се празен масив
+		$wh = $iQuery->getWhereAndHaving();
+		if(empty($wh->w)) return array();
+		
+		$result = array();
+		
+		// За всички записи
+		while($rec = $iQuery->fetch()){
+			
+			// Намиране на дилъра, инициатора и взимане на данните на мастъра на детайла
+			$master = $masters[$rec->detailClassId][$rec->detailRecId];
+			$persons = self::getDealerAndInitiatorId($master[0], $master[1]);
+			
+			// За дилъра и инициатора, ако има ще се подават делтите
+			foreach (array('dealerId', 'initiatorId') as $personFld){
+				if(isset($persons[$personFld])){
+					
+					// Ключа по който ще събираме е лицето, документа и вальора
+					$key = "{$persons[$personFld]}|{$master[0]}|{$master[1]}|{$rec->valior}";
+					
+					// Ако документа е обратен 
+					$sign = ($master[3] == 'yes') ? -1 : 1;
+					$delta = $sign * $rec->delta;
+					
+					// Ако няма данни, добавят се
+					if(!array_key_exists($key, $result)){
+						$result[$key] = (object)array('date'        => $rec->valior,
+								                      'personId'    => $persons[$personFld],
+								                      'docId'       => $master[1],
+								                      'docClass'    => $master[0],
+								                      'indicatorId' => ($personFld == 'dealerId') ? 1 : 2,
+								                      'value'       => $delta,
+								                      'isRejected'  => ($master[2] == 'rejected'),
+						);
+					} else {
+						
+						// Ако има вече се сумират
+						$ref = &$result[$key];
+						$ref->value += $delta;
+					}
+				}
+			}
+		}
+		
+		// Връщане на намерените резултати
+        return $result;
 	}
 
     
@@ -193,7 +298,7 @@ class sales_PrimeCostByDocument extends core_Manager
      */
     public static function getIndicatorNames()
     {
-        return array(1 => 'Делта');
+        return array(1 => 'Делта', 2 => 'Делта на инициатора');
     }
 
 }
