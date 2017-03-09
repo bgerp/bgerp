c9669ea512956947078eb97ebbd9d44a83bed45d
ivelin1012 <ivelin_pdimov@abv.bg>
2017-03-09 17:24:47 +0200

    Създаване на артикули към сделки:BugFix

--- cat/plg/CreateProductFromDocument.class.php
+++ cat/plg/CreateProductFromDocument.class.php
@@ -108,6 +108,8 @@ class cat_plg_CreateProductFromDocument extends core_Plugin
 			
 			// Поле за прототип
 			$form->FLD('innerClass', "class(interface=cat_ProductDriverIntf, allowEmpty, select=title)", "caption=Вид,mandatory,silent,before=proto,removeAndRefreshForm=proto|packPrice|discount|packagingId|tolerance,mandatory");
+			$form->setOptions('innerClass', cat_Products::getAvailableDriverOptions());
+			
 			$form->FLD('proto', "key(mvc=cat_Products,allowEmpty,select=name)", "caption=Шаблон,input=hidden,silent,refreshForm,placeholder=Популярни продукти,before=packagingId");
 			if(!($mvc instanceof sales_QuotationsDetails)){
 				$form->setField('packPrice', 'mandatory');
