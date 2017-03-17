2caa9599667685307c997702a5defbc77e52df89
Yuseinov <yuseinov@experta.bg>
2017-03-17 18:10:31 +0200

    social_Sharings https://github.com/bgerp/bgerp/commit/67f0539efce146f8a86fe8f1604ce230324281dc

--- social/SharingCnts.class.php
+++ social/SharingCnts.class.php
@@ -120,7 +120,7 @@ class social_SharingCnts extends core_Master
      * @param stdClass $rec
      * @param int $userId
      */
-    public static function on_AfterGetRequiredRoles($mvc, &$requiredRoles, $action, $rec = NULL, $userId)
+    public static function on_AfterGetRequiredRoles($mvc, &$requiredRoles, $action, $rec = NULL, $userId=NULL)
     {
     	
     	if ($action == 'edit' || $action == 'add') {
