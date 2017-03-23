a5f33b88184359e9ebb3da3318be4091c567e339
Yuseinov <yuseinov@experta.bg>
2017-03-23 17:51:16 +0200

    cal_TaskDocuments - ако има само един документ и той е оттеглен да се показва и да може да се възстанови

--- cal/TaskDocuments.class.php
+++ cal/TaskDocuments.class.php
@@ -381,7 +381,10 @@ class cal_TaskDocuments extends core_Detail
 	    $res = parent::prepareDetail_($data);
 		
 		if (empty($data->recs)) {
-		    $data->disabled = TRUE;
+		    
+		    if (!self::fetch("#state = 'rejected' && #taskId = '{$data->masterData->rec->id}'")) {
+		        $data->disabled = TRUE;
+		    }
 		}
 		
 		return $res;
@@ -395,7 +398,7 @@ class cal_TaskDocuments extends core_Detail
 	 */
 	public function renderDetail_($data)
 	{
-	    if (empty($data->recs)) return ;
+	    if ($data->disabled) return ;
 		
 		return parent::renderDetail_($data);
 	}
