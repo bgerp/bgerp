f0e2b9bfbe0416078c6878fd3863b1c455fe9c5c
ivelin1012 <ivelin_pdimov@abv.bg>
2017-03-24 10:34:22 +0200

    Пакет:frame2 начало

---
+++ frame2/ReportIntf.class.php
@@ -0,0 +1,23 @@
+<?php
+
+
+/**
+ * Интерфейс за създаване на справки във системата
+ *
+ *
+ * @category  bgerp
+ * @package   frame
+ * @author    Milen Georgiev <milen@download.bg>
+ * @copyright 2006 - 2017 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class frame2_ReportIntf extends embed_DriverIntf
+{
+	
+	
+	/**
+	 * Инстанция на класа имплементиращ интерфейса
+	 */
+	public $class;
+}
