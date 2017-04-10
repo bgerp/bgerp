4d400f0b681720f2fe192bc78dd6f7444ea75faf
Yuseinov <yuseinov@experta.bg>
2017-04-10 18:47:51 +0300

    select2 #Sig3205 - подобрения по търсене, когато не сработва AJAX - наша функция

--- select2/js/adapter.js
+++ select2/js/adapter.js
@@ -104,3 +104,93 @@ $.fn.alternativeFind = function(selector) {
 	
 	return this.find(selector);
 }
+
+
+/**
+ * 
+ * 
+ * @param params
+ * @param data
+ * 
+ * @see https://github.com/select2/select2/issues/3034
+ * 
+ * @returns
+ */
+function modelMatcher (params, data)
+{
+    data.parentText = data.parentText || "";
+
+    // Always return the object if there is nothing to compare
+    if ($.trim(params.term) === '') {
+    	
+    	return data;
+    }
+
+	// Do a recursive check for options with children
+	if (data.children && data.children.length > 0) {
+		// Clone the data object if there are children
+		// This is required as we modify the object to remove any non-matches
+		var match = $.extend(true, {}, data);
+		
+		// Check each child of the option
+		for (var c = data.children.length - 1; c >= 0; c--) {
+			var child = data.children[c];
+			child.parentText += data.parentText + " " + data.text;
+			
+			var matches = modelMatcher(params, child);
+			
+			// If there wasn't a match, remove the object in the array
+			if (matches == null) {
+				match.children.splice(c, 1);
+			}
+		}
+		
+		  // If any children matched, return the new object
+		if (match.children.length > 0) {
+			
+			return match;
+		}
+		
+		// If there were no matching children, check just the plain object
+		return modelMatcher(params, match);
+	}
+
+    // If the typed-in term matches the text of this term, or the text from any
+    // parent term, then it's a match.
+    var original = (data.parentText + ' ' + data.text).toUpperCase();
+    var term = params.term.toUpperCase();
+    
+    have = true;
+    
+    // Търсене във всяка дума
+    var termArr = term.split(" ");
+    termArr.forEach(function(element) {
+    	if (!element || !element.length) return true;
+    	
+    	if (original.indexOf(element) == -1) {
+    		have = false;
+    	}
+    });
+    
+    // Търсене с OR
+    if (!have) {
+    	var termArr = term.split("|");
+        termArr.forEach(function(element) {
+        	if (have) return true;
+        	
+        	if (!element || !element.length) return true;
+        	
+        	if (original.indexOf(element) > -1) {
+        		have = true;
+        	}
+        });
+    }
+    
+    if (have) {
+    	
+    	return data;
+    }
+    
+    // If it doesn't contain the term, don't return anything
+    return null;
+}
