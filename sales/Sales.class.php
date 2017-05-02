f60aaedc0341104578e1317b408655a021508dbd
ivelin1012 <ivelin_pdimov@abv.bg>
2017-05-02 15:42:30 +0300

    Продажба: изтриване на кеш за готовноста

--- sales/Sales.class.php
+++ sales/Sales.class.php
@@ -1209,4 +1209,13 @@ class sales_Sales extends deals_DealMaster
     	
     	return $products;
     }
+    
+    
+    /**
+     * След промяна в журнала със свързаното перо
+     */
+    public static function on_AfterJournalItemAffect($mvc, $rec, $item)
+    {
+    	core_Cache::remove('sales_reports_ShipmentReadiness', "c{$rec->containerId}");
+    }
 }
