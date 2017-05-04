e8995c9e63f877bbf9eb24472df2d1f76fbe8823
HeBuTy <nevena@experta.bg>
2017-05-04 17:43:10 +0300

    цветове в селектите - добавен скрипт

--- uiext/js/Label.js
+++ uiext/js/Label.js
@@ -1,4 +1,8 @@
 function labelActions() {
+	$('.transparentSelect').each(function(){
+		changeColor(this);
+	});
+
 	$(document.body).on('change', ".selectLabel", function (e) {
 		
 		var url = $(this).attr("data-url");
@@ -11,5 +15,22 @@ function labelActions() {
 		resObj['url'] = url;
 		
 		getEfae().process(resObj, data);
+
+		changeColor(this);
 	});
+}
+
+function changeColor(element) {
+	var color = $(element).find(":selected").attr('data-color');
+
+	if (color) {
+		$(element).closest('td').css('background-color', color);
+		$(element).addClass('no-border-select');
+	} else {
+		color = "#fff";
+		$(element).closest('td').css('background-color', 'transparent');
+		$(element).removeClass('no-border-select');
+	}
+
+	$(element).attr('style', 'background-color:' + color + ' !important');
 }
