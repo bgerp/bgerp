b85c89cefdd355aa2248791a91e980dbd687afae
ivelin1012 <ivelin_pdimov@abv.bg>
2017-05-04 17:14:31 +0300

    uiext:bugFix

--- uiext/DocumentLabels.class.php
+++ uiext/DocumentLabels.class.php
@@ -113,10 +113,15 @@ class uiext_DocumentLabels extends core_Manager
 			return status_Messages::returnStatusesArray();
 		}
 		
+		$delete = FALSE;
 		$label = Request::get('label', 'int');
-		if(!uiext_Labels::fetch($label)){
-			core_Statuses::newStatus('|Няма такъв етикет|*!', 'error');
-			return status_Messages::returnStatusesArray();
+		if(!$label){
+			$delete = TRUE;
+		} else {
+			if(!uiext_Labels::fetch($label)){
+				core_Statuses::newStatus('|Няма такъв таг|*!', 'error');
+				return status_Messages::returnStatusesArray();
+			}
 		}
 		
 		$rec = (object)array('containerId' => $containerId, 'hash' => $hash);
@@ -125,21 +130,25 @@ class uiext_DocumentLabels extends core_Manager
 			$rec->id = $exRec->id;
 		}
 		
-		// Ако сме в AJAX режим
-		if($this->save($rec)){
-			if(Request::get('ajax_mode')) {
-			
-				// Заместваме клетката по AJAX за да визуализираме промяната
-				$resObj = new stdClass();
-				$resObj->func = "html";
-					
-				$k = "{$containerId}|{$classId}|{$hash}";
-				$resObj->arg = array('id' => "charge{$k}", 'html' => uiext_Labels::renderLabel($containerId, $classId, $hash), 'replace' => TRUE);
-			
-				$res = array_merge(array($resObj));
-			
-				return $res;
-			}
+		if($delete === TRUE){
+			self::delete($exRec->id);
+			core_Statuses::newStatus('|Премахнат таг|*!', 'nitice');
+		} else {
+			$this->save($rec);
+		}
+		
+		if(Request::get('ajax_mode')) {
+				
+			// Заместваме клетката по AJAX за да визуализираме промяната
+			$resObj = new stdClass();
+			$resObj->func = "html";
+				
+			$k = "{$containerId}|{$classId}|{$hash}";
+			$resObj->arg = array('id' => "charge{$k}", 'html' => uiext_Labels::renderLabel($containerId, $classId, $hash), 'replace' => TRUE);
+				
+			$res = array_merge(array($resObj));
+				
+			return $res;
 		}
 		
 		$document = doc_Containers::getDocument($containerId);
