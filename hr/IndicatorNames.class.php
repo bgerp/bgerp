3d83d3903f65bc343a689083b90b5936d1bf9299
ivelin1012 <ivelin_pdimov@abv.bg>
2017-05-26 12:51:32 +0300

    Индикатори рефакторинг + миграция

--- hr/IndicatorNames.class.php
+++ hr/IndicatorNames.class.php
@@ -3,7 +3,7 @@
 
 
 /**
- * Мениджър на Имената на индикаторите
+ * Мениджър на Имената на показателите
  *
  *
  * @category  bgerp
@@ -12,7 +12,7 @@
  * @copyright 2006 - 2017 Experta OOD
  * @license   GPL 3
  * @since     v 0.1
- * @title     Имена на индикатори
+ * @title     Имена на показатели
  */
 class hr_IndicatorNames extends core_Manager
 {
@@ -21,19 +21,13 @@ class hr_IndicatorNames extends core_Manager
     /**
      * Заглавие
      */
-    public $title = 'Имена на индикатори';
+    public $title = 'Имена на показатели';
     
     
     /**
      * Заглавие в единично число
      */
-    public $singleTitle = 'Индикатор';
-    
-    
-    /**
-     * Плъгини за зареждане
-     */
-    public $loadList = 'plg_Created';
+    public $singleTitle = 'Име на показател';
     
     
     /**
@@ -57,9 +51,15 @@ class hr_IndicatorNames extends core_Manager
     /**
 	 * Кой може да го разглежда?
 	 */
-	public $canList = 'admin,debug';
+	public $canList = 'debug,admin';
 
     
+	/**
+	 * Плъгини за зареждане
+	 */
+	public $loadList = 'hr_Wrapper';
+	
+	
     /**
      * Описание на модела (таблицата)
      */
@@ -67,7 +67,7 @@ class hr_IndicatorNames extends core_Manager
     {
         $this->FLD('name', 'varchar', 'caption=Наименование,mandatory');
         $this->FLD('uniqueId', 'int', 'caption=Обект,mandatory');
-        $this->FLD('classId', 'class', 'caption=Клас,mandatory');
+        $this->FLD('classId', 'class(interface=hr_IndicatorsSourceIntf,select=title)', 'caption=Клас,mandatory');
         
         $this->setDbUnique('uniqueId,classId');
     }
@@ -76,9 +76,10 @@ class hr_IndicatorNames extends core_Manager
     /**
      * Връща id-то на дадения индикатор. Ако липсва - добавя го.
      *
-     * @param string $name - заглавие на индикатора
-     * @param mixed $class - клас на индикатора
-     * @param int $uniqueId - уникален номер
+     * @param string $name   - заглавие на индикатора
+     * @param mixed $class   - клас на индикатора
+     * @param int $uniqueId  - уникален номер
+     * @return stdClass $rec - форсирания запис
      */
     public static function force($name, $class, $uniqueId)
     {
@@ -88,20 +89,25 @@ class hr_IndicatorNames extends core_Manager
     	$name = self::normalizeName($name);
     	
     	if(!$rec){
-    		$id = self::save((object)array('name' => $name, 'classId' => $class->getClassId(), 'uniqueId' => $uniqueId));
+    		$rec = (object)array('name' => $name, 'classId' => $class->getClassId(), 'uniqueId' => $uniqueId);
+    		self::save($rec);
     	} else{
     		if($rec->name != $name){
     			$rec->name = $name;
     			self::save($rec, 'name');
     		}
-    		
-    		$id = $rec->id;
     	}
     	
-    	return $id;
+    	return $rec;
     }
     
     
+    /**
+     * Нормализира името на индикатора
+     * 
+     * @param string $name
+     * @return string $name
+     */
     public static function normalizeName($name)
     {
     	$name = preg_replace('/\s+/', ' ', $name);
@@ -109,5 +115,12 @@ class hr_IndicatorNames extends core_Manager
     	
     	return $name;
     }
+    
+    
+    function act_Test()
+    {
+    	$this->truncate();
+    	hr_Indicators::truncate();
+    	hr_Payroll::truncate();
+    }
 }
-
