3311cc9990b74461053d8a0bd78cceda23caa77a
Yuseinov <yuseinov@experta.bg>
2017-05-29 14:09:59 +0300

    fileman_webdrv_Log - драйвер за работс с .log файлове

---
+++ fileman/webdrv/Log.class.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * Драйвер за работа с .log файлове.
+ * 
+ * @category  vendors
+ * @package   fileman
+ * @author    Yusein Yuseinov <yyuseinov@gmail.com>
+ * @copyright 2006 - 2017 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class fileman_webdrv_Log extends fileman_webdrv_Code
+{
+    
+}
