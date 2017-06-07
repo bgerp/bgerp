685eccb1e1bb72f59bd7399a616cfe8ac36c2801
ivelin1012 <ivelin_pdimov@abv.bg>
2017-06-07 14:25:56 +0300

    batch_Defs:bugFix

--- batch/Defs.class.php
+++ batch/Defs.class.php
@@ -25,7 +25,7 @@ class batch_Defs extends core_Manager {
     /**
      * Плъгини за зареждане
      */
-    public $loadList = 'plg_RowTools2, batch_Wrapper, plg_Modified, plg_Search, plg_Sorting, plg_SaveAndNew';
+    public $loadList = 'plg_RowTools2, batch_Wrapper, plg_Created, plg_Modified, plg_Search, plg_Sorting, plg_SaveAndNew';
     
     
     /**
