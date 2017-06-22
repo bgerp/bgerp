f7ad769ae561cde246a63d6246b737c1c236957f
ivelin1012 <ivelin_pdimov@abv.bg>
2017-06-22 11:02:52 +0300

    Протокол за влагане да не се създава от лист изгледа

--- planning/ConsumptionNotes.class.php
+++ planning/ConsumptionNotes.class.php
@@ -46,7 +46,7 @@ class planning_ConsumptionNotes extends deals_ManifactureMaster
 	 * Плъгини за зареждане
 	 */
 	public $loadList = 'plg_RowTools2, store_plg_StoreFilter, planning_Wrapper, acc_plg_DocumentSummary, acc_plg_Contable,
-                    doc_DocumentPlg, plg_Printing, plg_Clone, doc_plg_BusinessDoc, plg_Search, bgerp_plg_Blank';
+                    doc_DocumentPlg, plg_Printing, plg_Clone, plg_Search, bgerp_plg_Blank';
 	
 	
 	/**
