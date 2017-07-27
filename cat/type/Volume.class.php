f34010c380da696646d00044e137245926994cf7
ivelin1012 <ivelin_pdimov@abv.bg>
2017-07-27 13:45:11 +0300

    cat_type_UoM:да може да се задават мин и макс

--- cat/type/Volume.class.php
+++ cat/type/Volume.class.php
@@ -24,7 +24,9 @@ class cat_type_Volume extends cat_type_Uom {
 	function init($params = array())
     {
     	// Основната мярка на типа е кубични метра
-    	$this->params['unit'] = 'cub.m';
+    	$this->params['unit'] = 'kg';
+    	$this->params = array_merge($this->params, $params['params']);
+    	
     	parent::init($this->params);
     }
 }
