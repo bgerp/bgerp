5ac526cc6a1b40ef8cfb3389ea46f149a60ea29e
Yuseinov <yuseinov@experta.bg>
2017-08-16 18:15:02 +0300

    core_Form - премахнат debug

--- core/Form.class.php
+++ core/Form.class.php
@@ -241,7 +241,6 @@ class core_Form extends core_FieldSet
                     $value = $type->fromVerbal($value);
                 }
             } else {
-                if ($name == 'type') wp($value, Request::get($name), Request::$vars, Request::$protected);
                 $value = $type->fromVerbal($value);
                 
                 // Вдигаме грешка, ако стойността от Request 
