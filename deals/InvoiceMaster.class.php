8c75366939d5a381bcf22035fae1dcb09eef89a8
Yuseinov <yuseinov@experta.bg>
2017-08-16 16:16:14 +0300

    debug информация

--- deals/InvoiceMaster.class.php
+++ deals/InvoiceMaster.class.php
@@ -585,6 +585,8 @@ abstract class deals_InvoiceMaster extends core_Master
     	}
     
     	$type = Request::get('type');
+    	wp($type, Request::$vars);
+    	
     	if(empty($type)){
     		$type = 'invoice';
     	}
