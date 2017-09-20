2139616ced6d58f74046afbcad90411b91a6f3c4
Yuseinov <yuseinov@experta.bg>
2017-09-20 17:15:43 +0300

    fingerprints - добавени нови полета

--- email/Fingerprints.class.php
+++ email/Fingerprints.class.php
@@ -79,6 +79,8 @@ class email_Fingerprints extends core_Manager
         $this->FLD('accountId', 'key(mvc=email_Accounts,select=email,allowEmpty)', 'caption=Сметка, autoFilter');
         $this->FLD('uid', 'int', 'caption=Имейл UID');
         $this->FLD('status', 'enum(returned,receipt,spam,incoming,misformatted)', 'caption=Статус,notNull');
+        $this->FLD('downloadedOn', 'datetime', 'caption=Свалено на,notNull');
+        $this->FLD('deleted', 'enum(no=Не, yes=Да)', 'caption=Изтрито,notNull');
 
         $this->setDbUnique('hash');
         $this->setDbIndex('accountId,uid');
@@ -200,6 +202,10 @@ class email_Fingerprints extends core_Manager
         $rec->uid = $uid;
 
         $rec->status = $status;
+        
+        $rec->downloadedOn = dt::now();
+        
+        $rec->deleted = 'no';
 
         self::save($rec);
 
