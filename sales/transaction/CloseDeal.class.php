346e801eb4b951b08be19f8a6842b8bc084cea45
ivelin1012 <ivelin_pdimov@abv.bg>
2017-10-02 18:11:44 +0300

    Бъгфикс на превод

--- sales/transaction/CloseDeal.class.php
+++ sales/transaction/CloseDeal.class.php
@@ -49,7 +49,7 @@ class sales_transaction_CloseDeal extends deals_ClosedDealTransaction
     	$dealItem = acc_Items::fetchItem('sales_Sales', $firstDoc->that);
     	
     	if(Mode::get('saveTransaction')){
-    		acc_journal_RejectRedirect::expect(!doc_Containers::fetchField("#threadId = {$rec->threadId} AND #state = 'pending'"), tr("Към продажбата има документ в състояние \'Заявка\'|*"));
+    		acc_journal_RejectRedirect::expect(!doc_Containers::fetchField("#threadId = {$rec->threadId} AND #state = 'pending'"), tr("Към продажбата има документ в състояние|* '|Заявка|*'"));
     	}
     	
     	// Създаване на обекта за транзакция
