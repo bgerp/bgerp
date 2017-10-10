45f0ed097a12c31b3920ca1257bbdd1462a1800b
ivelin1012 <ivelin_pdimov@abv.bg>
2017-10-10 12:50:09 +0300

    change_Plugin:само активни и съдбудени документи да могат да се променят

--- change/Plugin.class.php
+++ change/Plugin.class.php
@@ -670,8 +670,7 @@ class change_Plugin extends core_Plugin
     public static function on_AfterCanChangeRec($mvc, &$res, $rec)
     {
         // Чернова и затворени документи не могат да се променят
-        if ($res !== FALSE && $rec->state != 'draft' && $rec->state != 'closed') {
-            
+        if ($res !== FALSE && $rec->state == 'active' && $rec->state == 'wakeup') {
             $res = TRUE;
         } 
     }
