0acf69f79206c74782b330269a18b2aaa4740191
ivelin1012 <ivelin_pdimov@abv.bg>
2017-10-16 11:47:39 +0300

    change_Plugin: само оттеглените/черновите и чакащите да не могат да се променят

--- change/Plugin.class.php
+++ change/Plugin.class.php
@@ -670,7 +670,7 @@ class change_Plugin extends core_Plugin
     public static function on_AfterCanChangeRec($mvc, &$res, $rec)
     {
         // Чернова и затворени документи не могат да се променят
-        if ($res !== FALSE && ($rec->state == 'active' || $rec->state == 'wakeup' || $rec->state == 'template')) {
+        if ($res !== FALSE && (!in_array($rec->state, array('rejected', 'draft', 'pending')))) {
             $res = TRUE;
         } 
     }
