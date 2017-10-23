b4ec88a0ff1f394ee17b2c929c6009a59769c9c4
ivelin1012 <ivelin_pdimov@abv.bg>
2017-10-23 15:59:29 +0300

    cat_Boms::showInProduct bugFix

--- cat/Boms.class.php
+++ cat/Boms.class.php
@@ -186,13 +186,11 @@ class cat_Boms extends core_Master
     public static function showInProduct($id)
     {
     	$rec = self::fetchRec($id);
-    	if(!empty($rec->showInProduct)) {
-    		if($rec->showInProduct == 'auto') return (cat_Products::fetchField($rec->productId, 'fixedAsset') == 'yes'); 
-    		
-    		return ($rec->showInProduct == 'yes') ? TRUE : FALSE;
-    	}
+    	$showInProduct = !empty($rec->showInProduct) ? $rec->showInProduct : cat_Setup::get('SHOW_BOM_IN_PRODUCT');
     	
-    	return cat_Setup::get('SHOW_BOM_IN_PRODUCT');
+    	if($showInProduct == 'auto') return (cat_Products::fetchField($rec->productId, 'fixedAsset') == 'yes'); 
+    		
+    	return ($showInProduct == 'yes') ? TRUE : FALSE;
     }
     
     
