d22f42be912c563c2b3fca151995e246586d26e8
ivelin1012 <ivelin_pdimov@abv.bg>
2017-10-30 11:25:10 +0200

    Серийни номера да няма аутокомплийт

--- planning/ProductionTaskDetails.class.php
+++ planning/ProductionTaskDetails.class.php
@@ -116,7 +116,7 @@ class planning_ProductionTaskDetails extends core_Detail
     	$this->FLD("taskId", 'key(mvc=planning_Tasks)', 'input=hidden,silent,mandatory,caption=Операция');
     	$this->FLD("productId", 'key(mvc=cat_Products,select=name)', 'silent,mandatory,caption=Артикул,removeAndRefreshForm=packagingId|serial,tdClass=productCell leftCol wrap');
     	$this->FLD('type', 'enum(input=Влагане,production=Произв.,waste=Отпадък)', 'input=hidden,silent,tdClass=small-field nowrap');
-    	$this->FLD('serial', 'varchar(32)', 'caption=Сер. №,smartCenter,focus');
+    	$this->FLD('serial', 'varchar(32)', 'caption=Сер. №,smartCenter,focus,autocomplete=off');
     	$this->FLD('quantity', 'double(Min=0)', 'caption=Количество');
     	$this->FLD('scrappedQuantity', 'double(Min=0)', 'caption=Брак,input=none');
     	$this->FLD('weight', 'double', 'caption=Тегло,smartCenter,unit=кг');
