472e3714de350d720452cb0d4334987c02de4e1b
ivelin1012 <ivelin_pdimov@abv.bg>
2017-10-31 16:54:05 +0200

    planning: махнати остарели миграции

--- planning/Setup.class.php
+++ planning/Setup.class.php
@@ -8,12 +8,6 @@ defIfNot('PLANNING_TASK_SERIAL_COUNTER', 1000);
 
 
 /**
- * Ротация на баркода
-*/
-defIfNot('PLANNING_TASK_LABEL_ROTATION', 'yes');
-
-
-/**
  * Широчина на превюто на артикула в етикета
  */
 defIfNot('PLANNING_TASK_LABEL_PREVIEW_WIDTH', 90);
@@ -114,11 +108,6 @@ class planning_Setup extends core_ProtoSetup
     		'planning_TaskSerials',
     		'planning_AssetGroups',
     		'planning_AssetResourcesNorms',
-    		'migrate::updateNotes',
-    		'migrate::updateStoreIds',
-    		'migrate::migrateJobs',
-    		'migrate::addPackToNotes',
-    		'migrate::addPackToJobs',
     		'migrate::deleteTaskCronUpdate',
         );
 
@@ -159,157 +148,6 @@ class planning_Setup extends core_ProtoSetup
         $res = bgerp_Menu::remove($this);
         
         return $res;
-    }
-    
-    
-    /**
-     * Миграция на старите задачи
-     */
-    function updateNotes()
-    {
-    	if(!planning_DirectProductionNote::count()) return;
-    	
-    	$query = planning_DirectProductionNote::getQuery();
-    	$query->where('#inputStoreId IS NULL');
-    	
-    	while($rec = $query->fetch()){
-    		$rec->inputStoreId = $rec->storeId;
-    		cls::get('planning_DirectProductionNote')->save_($rec);
-    	}
-    }
-    
-    
-    /**
-     * Миграция на протоколите за производство
-     */
-    function updateStoreIds()
-    {
-    	core_App::setTimeLimit(300);
-    	$Details = cls::get('planning_DirectProductNoteDetails');
-    	$Details->setupMvc();
-    	
-    	$query = planning_DirectProductNoteDetails::getQuery();
-    	$query->EXT('inputStoreId', 'planning_DirectProductionNote', 'externalName=inputStoreId,externalKey=noteId');
-    	$query->EXT('canStore', 'cat_Products', 'externalName=canStore,externalKey=productId');
-    	$query->where("#inputStoreId IS NOT NULL");
-    	$query->where("#storeId IS NULL");
-    	$query->where("#canStore = 'yes'");
-    	
-    	while($dRec = $query->fetch()){
-    		$dRec->storeId = $dRec->inputStoreId;
-    		try{
-    			$Details->save_($dRec, 'storeId');
-    		} catch(core_exception_Expect $e){
-    			reportException($e);
-    		}
-    	}
-    }
-    
-    
-    /**
-     * Миграция на департаментите
-     */
-    function migrateJobs()
-    {
-    	$Jobs = cls::get('planning_Jobs');
-    	$Jobs->setupMvc();
-    	
-    	if(!planning_Jobs::count()) return;
-    	
-    	$emptyId = hr_Departments::fetch("#systemId = 'emptyCenter'")->id;
-    	if(!$emptyId) return;
-    	
-    	$defFolderId = hr_Departments::forceCoverAndFolder($emptyId);
-    	
-    	$query = $Jobs->getQuery();
-    	$query->FLD('departments', 'keylist(mvc=hr_Departments,select=name,makeLinks)');
-    	
-    	while($rec = $query->fetch()){
-    		
-    		try{
-    			$newFolderId = $defFolderId;
-    			
-    			if(isset($rec->departments)){
-    				$departments = keylist::toArray($rec->departments);
-    				if(count($departments)){
-    					$departmentId = key($departments);
-    					if($departmentId){
-    						$rec->department = $departmentId;
-    						$newFolderId = hr_Departments::forceCoverAndFolder($departmentId);
-    							
-    						$Jobs->save_($rec, 'department');
-    					}
-    				}
-    			}
-    			
-    			doc_Threads::move($rec->threadId, $newFolderId);
-    			doc_ThreadUsers::addShared($rec->threadId, $rec->containerId, $rec->createdBy);
-    		} catch (core_exception_Expect $e){
-    			reportException($e);
-    		}
-    	}
-    }
-    
-    
-    /**
-     * Добавя опаковки на протокола за производство
-     */
-    public function addPackToNotes()
-    {
-    	$Notes = cls::get('planning_DirectProductionNote');
-    	$Notes->setupMvc();
-    	
-    	if(!$Notes->count()) return;
-    	core_App::setTimeLimit(300);
-    	
-    	$toSave = array();
-    	$query = planning_DirectProductionNote::getQuery();
-    	$query->where("#packagingId IS NULL");
-    	
-    	while($rec = $query->fetch()){
-    		try{
-    			$rec->packagingId = cat_Products::fetchField($rec->productId, 'measureId');
-    			$rec->quantityInPack = 1;
-    			$toSave[] = $rec;
-    		} catch(core_exception_Expect $e){
-    			reportException($e);
-    		}
-    	}
-    	
-    	if(count($toSave)){
-    		$Notes->saveArray($toSave, 'id,packagingId,quantityInPack');
-    	}
-    }
-    
-    
-    /**
-     * Добавя опаковки на протокола за производство
-     */
-    public function addPackToJobs()
-    {
-    	$Job = cls::get('planning_Jobs');
-    	$Job->setupMvc();
-    	 
-    	if(!$Job->count()) return;
-    	core_App::setTimeLimit(300);
-    	 
-    	$toSave = array();
-    	$query = planning_Jobs::getQuery();
-    	$query->where("#packagingId IS NULL");
-    	 
-    	while($rec = $query->fetch()){
-    		try{
-    			$rec->packagingId = cat_Products::fetchField($rec->productId, 'measureId');
-    			$rec->quantityInPack = 1;
-    			$toSave[] = $rec;
-    		} catch(core_exception_Expect $e){
-    			reportException($e);
-    		}
-    	}
-    	 
-    	if(count($toSave)){
-    		$Job->saveArray($toSave, 'id,packagingId,quantityInPack');
-    	}
     }
     
     
