accae67ca653c1936b68767155a49d0181dab9da
ivelin1012 <ivelin_pdimov@abv.bg>
2017-11-13 10:57:00 +0200

    Задания:козметична промяна

--- planning/Jobs.class.php
+++ planning/Jobs.class.php
@@ -1124,7 +1124,9 @@ class planning_Jobs extends core_Master
     	$form = cls::get('core_Form');
     	$form->title = 'Създаване на производствена операция към|* <b>' . self::getHyperlink($jobRec->id, TRUE) . "</b>";
     	$form->FLD('select', 'varchar', 'caption=Избор,mandatory');
-    	$form->setOptions('select',$this->getTaskOptions($jobRec));
+    	
+    	$options = $this->getTaskOptions($jobRec);
+    	$form->setOptions('select',$options);
     	$form->setDefault('select', 'new');
     	$form->input();
     	if($form->isSubmitted()){
