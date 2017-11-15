01ef92592cb00d3c05c2aa0b9370eb1a0ba4de48
ivelin1012 <ivelin_pdimov@abv.bg>
2017-11-15 12:55:26 +0200

    ЕН и СР: възможност за заявено к-во

--- store/ReceiptDetails.class.php
+++ store/ReceiptDetails.class.php
@@ -39,7 +39,7 @@ class store_ReceiptDetails extends deals_DeliveryDocumentDetail
     /**
      * Плъгини за зареждане
      */
-    public $loadList = 'plg_RowTools2, plg_Created, store_Wrapper, plg_SaveAndNew, plg_RowNumbering,Policy=purchase_PurchaseLastPricePolicy, 
+    public $loadList = 'plg_RowTools2, plg_Created, store_Wrapper, plg_SaveAndNew, plg_RowNumbering,store_plg_RequestDetail,Policy=purchase_PurchaseLastPricePolicy, 
                         plg_AlignDecimals2, plg_Sorting, doc_plg_HidePrices, ReverseLastPricePolicy=sales_SalesLastPricePolicy, 
                         Policy=purchase_PurchaseLastPricePolicy, plg_PrevAndNext,deals_plg_ImportDealDetailProduct,cat_plg_ShowCodes,store_plg_TransportDataDetail';
     
