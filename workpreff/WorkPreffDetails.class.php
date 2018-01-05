c9f9153905ba02932f17d1c56540b0701af1eb36
atrifonoff <angel.trifonoff@gmail.com>
2018-01-05 18:02:40 +0200

    rename

--- workpreff/WorkPreffDetails.class.php
+++ workpreff/WorkPreffDetails.class.php
@@ -3,7 +3,7 @@
 
 
 /**
- * Клас 'workpreff_WorkpreffDetails'
+ * Клас 'workpreff_WorkPreffDetails'
  *
  * Детайли на мениджър за създаване на Форма за CV 
  *
@@ -13,20 +13,58 @@
  * @copyright 2006 - 2017 Experta OOD
  * @license   GPL 3
  * @since     v 0.1
- * @title     Избор на данни на Форма за CV
  */
 
-class workpreff_WorkpreffDetails extends core_Detail
+class workpreff_WorkPreffDetails extends core_Detail
 {
+	
+	/**
+	 * Заглавие
+	 *
+	 * @var string
+	 */
+	public $title = 'Детайли на форма за CV';
+	
+	
+	/**
+	 * Заглавие в единствено число
+	 *
+	 * @var string
+	 */
+	public $singleTitle = 'Избор';
+	
+	
+	/**
+	 * Плъгини за зареждане
+	 *
+	 * var string|array
+	 */
+	public $loadList = 'plg_RowTools2, plg_Created, sales_Wrapper, plg_RowNumbering, plg_SaveAndNew, plg_PrevAndNext';
+			
+            //            plg_AlignDecimals2, plg_Sorting, deals_plg_ImportDealDetailProduct, doc_plg_HidePrices, LastPricePolicy=sales_SalesLastPricePolicy,
+			  //          cat_plg_CreateProductFromDocument,doc_plg_HideMeasureAndQuantityColumns,cat_plg_ShowCodes';
+	
+	
+
+	/**
+	 * Име на поле от модела, външен ключ към мастър записа
+	 */
+	public $masterKey = 'choiceId';
 
 
 
 
 
 
-
-
-
+	/**
+	 * Описание на модела (таблицата)
+	 */
+	public function description()
+	{
+		$this->FLD('choiceId', 'key(mvc=workpreff_WorkPreff)', 'column=none,notNull,silent,hidden,mandatory');
+	
+		
+	}
 
 
 
