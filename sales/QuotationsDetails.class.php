410c3b9297b6096984d8839a3140fe2ac7c08d5b
ivelin1012 <ivelin_pdimov@abv.bg>
2018-01-15 16:48:50 +0200

    правописна грешка

--- sales/QuotationsDetails.class.php
+++ sales/QuotationsDetails.class.php
@@ -936,7 +936,7 @@ class sales_QuotationsDetails extends doc_Detail {
     	// Показване на теглото при определени условия
     	if($rec->showMode == 'detailed' || ($rec->showMode == 'auto' && cat_Products::fetchField($rec->productId, 'isPublic') == 'no')){
     		
-    		// Показва се теглото, само ако мярката не е прозиводна на килограм
+    		// Показва се теглото, само ако мярката не е производна на килограм
     		$kgMeasures = cat_UoM::getSameTypeMeasures(cat_UoM::fetchBySysId('kg')->id);
     		if(!array_key_exists($rec->packagingId, $kgMeasures)){
     			$row->weight = deals_Helper::getWeightRow($rec->productId, $rec->packagingId, $rec->quantity, $rec->weight);
