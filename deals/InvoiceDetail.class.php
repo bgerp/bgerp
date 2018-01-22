68aeb6c3f072afb93f9281e3f067a43493ab3f2a
arsov <arsov@experta.bg>
2018-01-22 15:49:56 +0200

    Редактиране детайл на известие - "цена" вместо "сума"

--- deals/InvoiceDetail.class.php
+++ deals/InvoiceDetail.class.php
@@ -100,7 +100,7 @@ abstract class deals_InvoiceDetail extends doc_Detail
 		}
 		
 		if($masterRec->type === 'dc_note'){
-			$data->form->info = tr('|*<div style="color:#333;margin-top:3px;margin-bottom:12px">|Моля въведете крайното количество|* <b>|или|*</b> |сума след промяната|* <br><small>( |системата автоматично ще изчисли и попълни разликата в известието|* )</small></div>');
+			$data->form->info = tr('|*<div style="color:#333;margin-top:3px;margin-bottom:12px">|Моля, въведете крайното количество|* <b>|или|*</b> |цена след промяната|* <br><small>( |системата автоматично ще изчисли и попълни разликата в известието|* )</small></div>');
 			$data->form->setField('quantity', 'caption=|Крайни|* (|след известието|*)->К-во');
 			$data->form->setField('packPrice', 'caption=|Крайни|* (|след известието|*)->Цена');
 			
