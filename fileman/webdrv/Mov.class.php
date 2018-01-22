751a4c1ec859214294f2fed1fc1fbd31637d03d6
Yuseinov <yuseinov@experta.bg>
2018-01-22 18:30:42 +0200

    fileman_webdrv_Mov - драйвер за mov файлове

---
+++ fileman/webdrv/Mov.class.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * Драйвер за работа с .mov файлове.
+ * 
+ * @category  vendors
+ * @package   fileman
+ * @author    Yusein Yuseinov <yyuseinov@gmail.com>
+ * @copyright 2006 - 2018 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class fileman_webdrv_Mov extends fileman_webdrv_Video
+{
+    
+}
