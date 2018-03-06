e4ce192e0b54a02a509f4c25217889ebc3ad577f
ivelin1012 <ivelin_pdimov@abv.bg>
2018-03-06 16:45:26 +0200

    ЧМР: добавени класове за цветове за печат

--- trans/Cmrs.class.php
+++ trans/Cmrs.class.php
@@ -118,7 +118,7 @@ class trans_Cmrs extends core_Master
     /**
      * Брой копия при печат
      */
-    public $copiesOnPrint = 4;
+    public $copiesOnPrint = 5;
     
     
     /**
@@ -610,8 +610,10 @@ class trans_Cmrs extends core_Master
      */
     protected static function on_AfterRenderPrintCopy($mvc, &$copyTpl, $copyNum, $rec)
     {
-    	$head = array(1 => 'Copy for sender', 2 => 'Copy for receiver', 3 => 'Copy for carrier', 4 => 'Copy for second carrier');
+    	$head = array(1 => 'Copy for sender', 2 => 'Copy for consignee', 3 => 'Copy for carrier', 4 => 'Copy for second carrier', 5 => 'Copy for sender');
+    	$colorClass = array(1 => 'cmr-red', 2 => 'cmr-blue', 3 => 'cmr-green');
     	$copyTpl->append($copyNum, 'copyNum');
     	$copyTpl->append($head[$copyNum], 'copyTitle');
+    	$copyTpl->append($colorClass[$copyNum], 'colorClass');
     }
 }
