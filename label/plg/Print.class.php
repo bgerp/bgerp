ad61261f2c0f89a51dca53dfc0df9db501891661
ivelin1012 <ivelin_pdimov@abv.bg>
2018-03-06 14:10:22 +0200

    label_plg_print: да показва бутона и в rowToolbar-а в листовия изглед

--- label/plg/Print.class.php
+++ label/plg/Print.class.php
@@ -1,13 +1,14 @@
 <?php
 
 
+
 /**
- * Плъгин даващ възможност да се печатат етикети от документ
+ * Плъгин даващ възможност да се печатат етикети от обект
  * 
  * @category  bgerp
  * @package   label
  * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
- * @copyright 2006 - 2017 Experta OOD
+ * @copyright 2006 - 2018 Experta OOD
  * @license   GPL 3
  * @since     v 0.1
  */
@@ -27,6 +28,55 @@ class label_plg_Print extends core_Plugin
 	
 	
 	/**
+	 * След преобразуване на записа в четим за хора вид.
+	 *
+	 * @param core_Mvc $mvc
+	 * @param stdClass $row Това ще се покаже
+	 * @param stdClass $rec Това е записа в машинно представяне
+	 */
+	public static function on_AfterRecToVerbal($mvc, &$row, $rec, $fields = array())
+	{
+		if(isset($fields['-list']) && $mvc->hasPlugin('plg_RowTools2')){
+			$btnParams = self::getLabelBtnParams($mvc, $rec);
+			if(!empty($btnParams['url'])){
+				$row->_rowTools->addLink('Етикетиране', $btnParams['url'], $btnParams['attr']);
+			}
+		}
+	}
+	
+	
+	/**
+	 * Параметрите на бутона за етикетиране
+	 * 
+	 * @param core_mvc $mvc
+	 * @param stdClass $rec
+	 * @return array $res -
+	 * 			['url'] - урл, ако има права
+	 * 			['attr] - атрибути
+	 */
+	private static function getLabelBtnParams($mvc, $rec)
+	{
+		$res = array('url' => NULL, 'attr' => '');
+	
+		if($mvc->haveRightFor('printlabel', $rec)){
+			$templates = label_Templates::getTemplatesByDocument($mvc, $rec->id, TRUE);
+			$error = (!count($templates)) ? ",error=Няма наличен шаблон за етикети от \"{$mvc->title}\"" : '';
+			
+			if(label_Prints::haveRightFor('add', (object)array('classId' => $mvc->getClassId(), 'objectId' => $rec))) {
+				core_Request::setProtected(array('classId, objectId'));
+				$res['url'] = array('label_Prints', 'add', 'classId' => $mvc->getClassId(), 'objectId' => $rec->id, 'ret_url' => TRUE);
+				$res['url'] = toUrl($res['url']);
+				core_Request::removeProtected('classId,objectId');
+				
+				$res['attr'] = "target=_blank,ef_icon = img/16/price_tag_label.png,title=Разпечатване на етикети от|* |{$mvc->title}|* №{$rec->id}{$error}";
+			}
+		}
+		
+		return $res;
+	}
+	
+	
+	/**
 	 * След подготовка на тулбара на единичен изглед.
 	 *
 	 * @param core_Mvc $mvc
@@ -34,19 +84,9 @@ class label_plg_Print extends core_Plugin
 	 */
 	public static function on_AfterPrepareSingleToolbar($mvc, &$data)
 	{
-		if($mvc->haveRightFor('printlabel', $data->rec)){
-			$templates = label_Templates::getTemplatesByDocument($mvc, $data->rec->id, TRUE);
-			$error = '';
-			if(!count($templates)){
-				$error = ",error=Няма наличен шаблон за етикети от \"{$mvc->title}\"";
-			}
-			
-			if (label_Prints::haveRightFor('add', (object)array('classId' => $mvc->getClassId(), 'objectId' => $data->rec->id))) {
-			    core_Request::setProtected(array('classId, objectId'));
-			    $url = array('label_Prints', 'add', 'classId' => $mvc->getClassId(), 'objectId' => $data->rec->id, 'ret_url' => TRUE);
-			    $data->toolbar->addBtn('Етикетиране', toUrl($url), NULL, "target=_blank,ef_icon = img/16/price_tag_label.png,title=Разпечатване на етикети от|* |{$mvc->title}|* №{$data->rec->id}{$error}");
-			    core_Request::removeProtected('class,objectId');
-			}
+		$btnParams = self::getLabelBtnParams($mvc, $data->rec);
+		if(!empty($btnParams['url'])){
+			$data->toolbar->addBtn('Етикетиране', $btnParams['url'], NULL, $btnParams['attr']);
 		}
 	}
 	
