ba90ab40cb10ac14b485493e07a31c4bc5536646
atrifonoff <angel.trifonoff@gmail.com>
2018-03-16 17:26:16 +0200

    корекции

--- lab/TestDetails.class.php
+++ lab/TestDetails.class.php
@@ -39,7 +39,7 @@ class lab_TestDetails extends core_Detail
     /**
      * Полета, които ще се показват в листов изглед
      */
-    var $listFields = 'methodId,paramName,value,error';
+    var $listFields = 'methodId,paramName,value,refValue,error';
 
     
     /**
@@ -63,8 +63,9 @@ class lab_TestDetails extends core_Detail
         $this->FNC('paramName', 'varchar(255)', 'caption=Параметър, notSorting,smartCenter');
         $this->FLD('methodId', 'key(mvc=lab_Methods, select=name)', 'caption=Метод, notSorting,mandatory,smartCenter');
         $this->FLD('value', 'varchar(64)', 'caption=Стойност, notSorting, input=none,smartCenter');
+        $this->FLD('refValue', 'varchar(64)', 'caption=Реф.Стойност, notSorting, input=none,smartCenter');
         $this->FLD('error', 'percent(decimals=2)', 'caption=Грешка, notSorting,input=none,smartCenter');
-        $this->FLD('comment', 'text', 'caption=Коментари, notSorting,after=results, column=none,class=" w50, rows= 1"');
+        $this->FLD('comment', 'varchar', 'caption=Коментари, notSorting,after=results, column=none,class=" w50, rows= 1"');
       
         $this->FLD('better', 'enum(up=По-големия,down=По-малкия)',
             'caption=По-добрия резултат е,after=title,maxRadio=2,columns=2');
@@ -191,7 +192,12 @@ class lab_TestDetails extends core_Detail
         // Подготовка на масива за резултатите ($rec->results)
         $rec->results = str_replace("\r\n", "\n", $rec->results);
         $rec->results = str_replace("\n\r", "\n", $rec->results);
-        $resultsArr = explode("\n", $rec->results);
+       
+        
+        //$resultsArr = explode("\n", $rec->results);
+        $resultsArr = json_decode($rec->results)->value;
+        
+       
         
         // trim array elements
         foreach ($resultsArr as $k => $v) {
