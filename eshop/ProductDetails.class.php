2db6a129edd1f56ed33715db1c4a2d94cc46392f
ivelin1012 <ivelin_pdimov@abv.bg>
2018-03-29 17:22:54 +0300

    Детайли на ешопа възможност за задаване на к-во

--- eshop/ProductDetails.class.php
+++ eshop/ProductDetails.class.php
@@ -26,7 +26,7 @@ class eshop_ProductDetails extends core_Detail
 	/**
 	 * Плъгини за зареждане
 	 */
-	public $loadList = 'eshop_Wrapper, plg_Created, plg_Modified, plg_SaveAndNew, plg_RowTools2';
+	public $loadList = 'eshop_Wrapper, plg_Created, plg_Modified, plg_SaveAndNew, plg_RowTools2, plg_Select, plg_AlignDecimals2';
 	
 	
 	/**
@@ -44,7 +44,7 @@ class eshop_ProductDetails extends core_Detail
 	/**
 	 * Кои полета да се показват в листовия изглед
 	 */
-	public $listFields = 'eshopProductId=Артикул в е-мага,productId,packagingId,createdOn,createdBy,modifiedOn,modifiedBy';
+	public $listFields = 'eshopProductId=Артикул в е-мага,productId,packagingId,packQuantity,createdOn,createdBy,modifiedOn,modifiedBy';
 	
 	
 	/**
@@ -78,9 +78,26 @@ class eshop_ProductDetails extends core_Detail
 	{
 		$this->FLD('eshopProductId', 'key(mvc=eshop_Products,select=name)', 'caption=Ешоп артикул,mandatory,input=hidden,silent');
 		$this->FLD('productId', 'key2(mvc=cat_Products,select=name,allowEmpty,selectSourceArr=eshop_ProductDetails::getSellableProducts)', 'caption=Артикул,silent,removeAndRefreshForm=packagingId,mandatory');
-		$this->FLD('packagingId', 'key(mvc=cat_UoM,select=name)', 'caption=Опаковка,input=hidden,mandatory,smartCenter');
-		
-		$this->setDbUnique('eshopProductId,productId,packagingId');
+		$this->FLD('packagingId', 'key(mvc=cat_UoM,select=name)', 'caption=Мярка,input=hidden,mandatory,smartCenter,removeAndRefreshForm=quantity|quantityInPack');
+		$this->FLD('quantity', 'double', 'caption=Количество,input=none');
+    	$this->FLD('quantityInPack', 'double', 'input=none');
+    	$this->FNC('packQuantity', 'double(Min=0)', 'caption=Количество,input=none,smartCenter');
+    	
+		$this->setDbUnique('eshopProductId,productId,packagingId,quantity');
+	}
+	
+	
+	/**
+	 * Изчисляване на количеството на реда в брой опаковки
+	 *
+	 * @param core_Mvc $mvc
+	 * @param stdClass $rec
+	 */
+	protected static function on_CalcPackQuantity(core_Mvc $mvc, $rec)
+	{
+		if (empty($rec->quantity) || empty($rec->quantityInPack)) return;
+	
+		$rec->packQuantity = $rec->quantity / $rec->quantityInPack;
 	}
 	
 	
@@ -146,16 +163,45 @@ class eshop_ProductDetails extends core_Detail
 	 * @param core_Manager $mvc
 	 * @param stdClass $data
 	 */
-	public static function on_AfterPrepareEditForm($mvc, &$data)
+	protected static function on_AfterPrepareEditForm($mvc, &$data)
 	{
 		$form = &$data->form;
 		$rec = $form->rec;
 		
 		if(isset($rec->productId)){
 			$form->setField('packagingId', 'input');
+			$form->setField('packQuantity', 'input');
 			$packs = cat_Products::getPacks($rec->productId);
 			$form->setOptions('packagingId', $packs);
 			$form->setDefault('packagingId', key($packs));
+			$form->setDefault('packQuantity', 1);
+		}
+	}
+	
+	
+	/**
+	 * Извиква се след въвеждането на данните от Request във формата ($form->rec)
+	 *
+	 * @param core_Mvc $mvc
+	 * @param core_Form $form
+	 */
+	protected static function on_AfterInputEditForm($mvc, &$form)
+	{
+		$rec = $form->rec;
+		
+		if($form->isSubmitted()){
+			
+			// Проверка на к-то
+			if(!deals_Helper::checkQuantity($rec->packagingId, $rec->packQuantity, $warning)){
+				$form->setError('packQuantity', $warning);
+			}
+			
+			// Ако артикула няма опаковка к-то в опаковка е 1, ако има и вече не е свързана към него е това каквото е било досега, ако още я има опаковката обновяваме к-то в опаковка
+			if(!$form->gotErrors()){
+				$productInfo = cat_Products::getProductInfo($rec->productId);
+				$rec->quantityInPack = ($productInfo->packagings[$rec->packagingId]) ? $productInfo->packagings[$rec->packagingId]->quantity : 1;
+				$rec->quantity = $rec->packQuantity * $rec->quantityInPack;
+			}
 		}
 	}
 	
@@ -167,11 +213,13 @@ class eshop_ProductDetails extends core_Detail
 	 * @param stdClass $row Това ще се покаже
 	 * @param stdClass $rec Това е записа в машинно представяне
 	 */
-	public static function on_AfterRecToVerbal($mvc, &$row, $rec, $fields = array())
+	protected static function on_AfterRecToVerbal($mvc, &$row, $rec, $fields = array())
 	{
 		if(isset($fields['-list'])){
 			$row->productId = cat_Products::getHyperlink($rec->productId, TRUE);
 			$row->eshopProductId = eshop_Products::getHyperlink($rec->eshopProductId, TRUE);
+			
+			deals_Helper::getPackInfo($row->packagingId, $rec->productId, $rec->packagingId, $rec->quantityInPack);
 		}
 	}
 	
@@ -179,7 +227,7 @@ class eshop_ProductDetails extends core_Detail
 	/**
 	 * Преди подготовката на полетата за листовия изглед
 	 */
-	public static function on_AfterPrepareListFields($mvc, &$res, &$data)
+	protected static function on_AfterPrepareListFields($mvc, &$res, &$data)
 	{
 		if(isset($data->masterMvc)){
 			unset($data->listFields['eshopProductId']);
