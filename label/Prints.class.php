884b4ec28513a1c2f4225c89a45fab93c05cd9fe
Yuseinov <yuseinov@experta.bg>
2018-04-16 11:43:31 +0300

    label Not935136 - подобрения при избор на шаблон на en

--- label/Prints.class.php
+++ label/Prints.class.php
@@ -218,6 +218,10 @@ class label_Prints extends core_Master
         
         $labelDataArr = array();
         
+        $form->input('templateId');
+        
+        $oLang = core_Lg::getCurrent();
+        
         if(isset($classId) && isset($objId)){
         	$intfInst = cls::getInterface('label_SequenceIntf', $classId);
         	
@@ -228,6 +232,7 @@ class label_Prints extends core_Master
         	core_Mode::push('prepareLabel', TRUE);
         	if ($lang) {
         	    core_Lg::push($lang);
+        	    $oLang = $lang;
         	}
             $labelDataArr = $intfInst->getLabelPlaceholders($objId);
             if ($lang) {
@@ -310,6 +315,11 @@ class label_Prints extends core_Master
             $lang = label_Templates::fetchField($rec->templateId, 'lang');
             
             core_Lg::push($lang);
+            
+			// Ако е променен езика, вземаме данните пак
+            if ($lang != $oLang) {
+                $labelDataArr = $intfInst->getLabelPlaceholders($objId);
+            }
             
 			// При редакция да се попълват стойностите
             if ($rec->id) {
