044591b949c4d2243fbf23c7231736e06c16648b
ivelin1012 <ivelin_pdimov@abv.bg>
2018-04-27 15:13:37 +0300

    Артикули: бутон за преглед на е-артикул

--- cat/Products.class.php
+++ cat/Products.class.php
@@ -2047,6 +2047,10 @@ class cat_Products extends embed_Manager {
     		if(eshop_Products::haveRightFor('linktoeshop', (object)array('productId' => $data->rec->id))){
     			$data->toolbar->addBtn("E-маг", array('eshop_Products', 'linktoeshop', 'productId' => $data->rec->id, 'ret_url' => TRUE), 'ef_icon = img/16/cart_go.png,title=Свързване в Е-маг');
     		}
+    		
+    		if(eshop_Products::haveRightFor('vieweproduct', (object)array('productId' => $data->rec->id))){
+    			$data->toolbar->addBtn("E-артикул", array('eshop_Products', 'vieweproduct', 'productId' => $data->rec->id, 'ret_url' => TRUE), 'ef_icon = img/16/cart_go.png,title=Към е-артикула');
+    		}
     	}
     }
     
