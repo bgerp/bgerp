7b647c8fe69becc54b4ff9ef6800a75f78b1b3ba
atrifonoff <angel.trifonoff@gmail.com>
2018-05-18 10:27:44 +0300

    escape на името на контрагента

--- acc/reports/UnpaidInvoices.class.php
+++ acc/reports/UnpaidInvoices.class.php
@@ -553,7 +553,7 @@ class acc_reports_UnpaidInvoices extends frame2_driver_TableData
                                 </fieldset><!--ET_END BLOCK-->"));
         
         if (isset($data->rec->contragent)) {
-            $fieldTpl->append(doc_Folders::fetch($data->rec->contragent)->title, 'contragent');
+            $fieldTpl->append(type_Varchar::escape(doc_Folders::fetch($data->rec->contragent)->title), 'contragent');
         } else {
             $fieldTpl->append('Всички', 'contragent');
         }
