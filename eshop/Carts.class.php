fcbd7be4d4333e7f1f124411b6ca4109e288c3a6
ivelin1012 <ivelin_pdimov@abv.bg>
2018-05-29 18:49:44 +0300

    Количка:бъгфикс с типа за телефон

--- eshop/Carts.class.php
+++ eshop/Carts.class.php
@@ -118,7 +118,7 @@ class eshop_Carts extends core_Master
     	
     	$this->FLD('personNames', 'varchar(255)', 'caption=Контактни данни->Имена,class=contactData,hint=Вашето име||Your name,mandatory');
     	$this->FLD('email', 'email(valid=drdata_Emails->validate)', 'caption=Контактни данни->Имейл,hint=Вашият имейл||Your email,mandatory');
-    	$this->FLD('tel', 'drdata_PhoneType', 'caption=Контактни данни->Тел,hint=Вашият телефон,mandatory');
+    	$this->FLD('tel', 'drdata_PhoneType(type=tel)', 'caption=Контактни данни->Тел,hint=Вашият телефон,mandatory');
     	
     	$this->FLD('termId', 'key(mvc=cond_DeliveryTerms,select=codeName,allowEmpty)', 'caption=Доставка->Начин,mandatory,removeAndRefreshForm,silent');
     	$this->FLD('deliveryData', 'blob(serialize, compress)', 'input=none');
