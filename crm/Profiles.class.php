696fee41d3e74b572c8409201c70eded20cb27b5
ivelin1012 <ivelin_pdimov@abv.bg>
2018-06-12 13:27:23 +0300

    Профили:бъгфикс

--- crm/Profiles.class.php
+++ crm/Profiles.class.php
@@ -932,7 +932,7 @@ class crm_Profiles extends core_Master
           
             if(core_Packs::isInstalled('colab') && core_Users::isContractor($user)){
             	$privateFolderId = crm_Persons::forceCoverAndFolder($person->id);
-            	if(!colab_FolderToPartners::fetch("#folderId = {$privateFolderId} AND #contractorId = {$person->id}")){
+            	if(!colab_FolderToPartners::fetch("#folderId = {$privateFolderId} AND #contractorId = {$user->id}")){
             		colab_FolderToPartners::save((object)array('folderId' => $privateFolderId, 'contractorId' => $user->id));
             	}
             }
