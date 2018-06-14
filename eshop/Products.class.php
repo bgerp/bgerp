c1bbc5c87cb9da7a59a37f04ea4dd1d1ecf6a1b6
HeBuTy <nevena@experta.bg>
2018-06-14 14:30:03 +0300

    Сменен title  на бутон

--- eshop/Products.class.php
+++ eshop/Products.class.php
@@ -356,7 +356,7 @@ class eshop_Products extends core_Master
             			$pRow->measureId = cat_UoM::getVerbal($productRec->measureId, 'name');
             			
             			$addUrl = toUrl(array('eshop_Carts', 'addtocart'), 'local');
-            			$pRow->addBtn = ht::createFnBtn('Купи', NULL, FALSE, array('ef_icon' => "img/16/cart_go.png", 'title'=> 'Добавяне на артикул към кошницата', 'data-url' => $addUrl, 'data-productid' => $dRec->productId, 'data-packagingid' => $productRec->measureId, 'data-eshopproductpd' => $pRec->id, 'class' => 'eshop-btn productBtn'));
+            			$pRow->addBtn = ht::createFnBtn('Купи', NULL, FALSE, array('ef_icon' => "img/16/cart_go.png", 'title'=> 'Добавяне на артикул', 'data-url' => $addUrl, 'data-productid' => $dRec->productId, 'data-packagingid' => $productRec->measureId, 'data-eshopproductpd' => $pRec->id, 'class' => 'eshop-btn productBtn'));
 					}
             	}
             }
