c96f450bddd15caf237ca4c3a47e14a9e10ce74a
ivelin1012 <ivelin_pdimov@abv.bg>
2018-06-14 12:37:05 +0300

    бъгфикс

--- eshop/Products.class.php
+++ eshop/Products.class.php
@@ -356,7 +356,7 @@ class eshop_Products extends core_Master
             			$pRow->measureId = cat_UoM::getVerbal($productRec->measureId, 'name');
             			
             			$addUrl = toUrl(array('eshop_Carts', 'addtocart'), 'local');
-            			$pRow->addBtn = ht::createFnBtn('', NULL, FALSE, FALSE, array('ef_icon' => "img/16/cart_go.png", 'title'=> 'Добавяне на артикул', 'data-url' => $addUrl, 'data-productid' => $dRec->productId, 'data-packagingid' => $productRec->measureId, 'data-eshopproductpd' => $pRec->id, 'class' => 'eshop-btn eshop-group-list-btn'));
+            			$pRow->addBtn = ht::createFnBtn('', NULL, FALSE, array('ef_icon' => "img/16/cart_go.png", 'title'=> 'Добавяне на артикул', 'data-url' => $addUrl, 'data-productid' => $dRec->productId, 'data-packagingid' => $productRec->measureId, 'data-eshopproductpd' => $pRec->id, 'class' => 'eshop-btn eshop-group-list-btn'));
 					}
             	}
             }
