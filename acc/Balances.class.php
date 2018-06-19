341f0ce49caa01a624f78082084b1e0d30a104d5
arsov <arsov@experta.bg>
2018-06-19 15:35:42 +0300

    Doc

--- acc/Balances.class.php
+++ acc/Balances.class.php
@@ -439,12 +439,17 @@ class acc_Balances extends core_Master
     		$rec->fromDate = $pRec->start;
     		$rec->toDate = $pRec->end;
     		$rec->periodId = $pRec->id;
-            
+            	
+		// Преизчисляваме баланса няколко пъти, за да подаде верни данни на следващите ...
+		
             	$j = 0;
 	        do {
     		    self::forceCalc($rec);
       		    self::logDebug("After Calc: {$rec->lastCalculateChange}; j = {$j}");
             	} while($rec->lastCalculateChange != 'no' && $j++ < 10 && $rc);
+		
+		// ... но само първият (най-старият; този в който има нови данни)
+		
 		$rc = FALSE;
     	}
     	
