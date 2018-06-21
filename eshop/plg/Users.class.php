1ed92e052263218147e459b13355498e7f1255d0
ivelin1012 <ivelin_pdimov@abv.bg>
2018-06-21 17:37:49 +0300

    ЕШОП: при логване да се обединяват количките

--- eshop/plg/Users.class.php
+++ eshop/plg/Users.class.php
@@ -24,6 +24,8 @@ class eshop_plg_Users extends core_Plugin
 	 */
 	public static function on_AfterLogin($mvc, $userRec, $inputs, $refresh)
 	{
+		$Carts = cls::get('eshop_Carts');
+		
 		// За всеки домейн
 		$brid = log_Browsers::getBrid();
 		$dQuery = cms_Domains::getQuery();
@@ -32,13 +34,37 @@ class eshop_plg_Users extends core_Plugin
 			// Проверка има ли чернова на количка без потребител от същия брид, ако има присвоява се на логнатия потребител
 			if($dCart = eshop_Carts::fetch("#domainId = {$dRec->id} AND #state = 'draft' AND #brid = '{$brid}' AND #userId IS NULL")){
 				
-				// Ако потребителя има количка, домейна се пропуска
+				// Артикулите в текущата кошница
+				$arr = array();
+				$dQuery = eshop_CartDetails::getQuery();
+				$dQuery->where("#cartId = {$dCart->id}");
+				while($cRec = $dQuery->fetch()){
+					$arr["{$cRec->productId}|{$cRec->packagingId}"] = $cRec;
+				}
+				
+				// Ако логнатия потребител има чернова количка
 				if($exId = eshop_Carts::force($dRec->id, $userRec->id, FALSE)){
+					
+					// Извличат се артикулит от нея
+					$eQuery = eshop_CartDetails::getQuery();
+					$eQuery->where("#cartId = {$exId}");
+					while($eRec = $eQuery->fetch()){
+						
+						// Тези, които не присъстват в новата се прехвърлят в нея
+						if(!array_key_exists("{$eRec->productId}|{$eRec->packagingId}", $arr)){
+							$eRec->cartId = $dCart->id;
+							eshop_CartDetails::save($eRec, 'cartId');
+						}
+					}
+					
+					// Старата количка се изтрива
 					eshop_Carts::delete($exId);
 				}
 				
+				// Последната количка се присвоява на потребителя
 				$dCart->userId = $userRec->id;
-				eshop_Carts::save($dCart, 'userId');
+				$Carts->save($dCart);
+				$Carts->updateMaster($dCart);
 			}
 		}
 	}
