351839bee93b161478feccb5bacf2941dbff7673
Milen Georgiev <milen@experta.bg>
2018-07-09 14:54:29 +0300

    Пропуснат интерфейс

---
+++ sens2/ioport/Intf.class.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Интерфейс за порт към IoT контролер
+ *
+ *
+ * @category  bgerp
+ * @package   sens2
+ * @author    Milen Georgiev <milen@experta.bg>
+ * @copyright 2006 - 2018 Experta OOD
+ * @license   GPL 3
+ * @since     v 0.1
+ */
+class sens2_ioport_Intf extends embed_DriverIntf
+{
+    
+    /**
+     * Инстанция на класа имплементиращ интерфейса
+     */
+    public $class;
+    
+    
+    /**
+     * Добавя полетата на драйвера към Fieldset
+     *
+     * @param core_Fieldset $fieldset
+     */
+    public function addFields(core_Fieldset &$fieldset)
+    {
+        
+        return $this->class->addFields($fieldset);
+    }
+    
+    
+    /**
+     * Може ли вградения обект да се избере
+     */
+    public function canSelectDriver($userId = null)
+    {
+        
+        return $this->class->canSelectDriver($userId = null);
+    }
+}
