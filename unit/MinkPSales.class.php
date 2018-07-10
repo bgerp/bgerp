899bea50edbec02717a1d7e1720b6a32efcc2628
pavlina <pdainovska@experta.bg>
2018-07-10 18:14:24 +0300

    Предупреждение за отрицателно количество

--- unit/MinkPSales.class.php
+++ unit/MinkPSales.class.php
@@ -541,12 +541,12 @@ class unit_MinkPSales extends core_Manager
         $browser->press('Активиране/Контиране');
         if (strpos($browser->gettext(), 'Контирането на документа ще доведе до отрицателни количества')) {
             $browser->setValue('Ignore', '1');
-           
+            $browser->press('Активиране/Контиране');
         } else {
             
             return unit_MinkPbgERP::reportErr('Не дава грешка за отрицателно количество', 'warning');
         }
-        
+       
         // Когато няма автом. избиране - ЕН
         
         // Фактура с днешна дата
@@ -554,7 +554,7 @@ class unit_MinkPSales extends core_Manager
         $browser->setValue('additionalInfo', 'за оттегляне и контрол да не може да се възстанови');
         $browser->press('Чернова');
         $browser->press('Контиране');
-        
+         return $browser->getHtml();
         // Фактура с вчерашна дата - контрол, грешка и отказ
         $browser->press('Фактура');
         $dateInv = strtotime('-1 Day');
@@ -2486,7 +2486,7 @@ class unit_MinkPSales extends core_Manager
         $browser->press('Запис и Нов');
         $browser->setValue('productId', 'Други стоки');
         $browser->refresh('Запис');
-        $browser->setValue('packQuantity', '1');
+        $browser->setValue('packQuantity', '1000');
         $browser->setValue('packPrice', '1');//
         $browser->setValue('discount', 20);
         $browser->press('Запис и Нов');
@@ -2508,20 +2508,28 @@ class unit_MinkPSales extends core_Manager
         // Контиране, ако не е в бъдещ период
         if (strpos($browser->gettext(), 'Активиране/Контиране')) {
             $browser->press('Активиране/Контиране');
-            if (strpos($browser->gettext(), 'ДДС 20%: BGN 12,96')) {
-            } else {
+            if (strpos($browser->gettext(), 'Контирането на документа ще доведе до отрицателни количества')) {
+            $browser->setValue('Ignore', '1');
+            $browser->press('Активиране/Контиране');
+        } else {
+        
+            return unit_MinkPbgERP::reportErr('Не дава грешка за отрицателно количество', 'warning');
+        }
+            
+        if (strpos($browser->gettext(), 'ДДС 20%: BGN 12,96')) {
+        } else {
                 
-                return unit_MinkPbgERP::reportErr('Грешно ДДС 20%', 'warning');
-            }
-            if (strpos($browser->gettext(), 'ДДС 9%: BGN 6,63')) {
-            } else {
+            return unit_MinkPbgERP::reportErr('Грешно ДДС 20%', 'warning');
+        }
+        if (strpos($browser->gettext(), 'ДДС 9%: BGN 6,63')) {
+        } else {
                 
-                return unit_MinkPbgERP::reportErr('Грешно ДДС 9%', 'warning');
-            }
-            if (strpos($browser->gettext(), 'Сто петдесет и осем BGN и 0,10')) {
-            } else {
+            return unit_MinkPbgERP::reportErr('Грешно ДДС 9%', 'warning');
+        }
+        if (strpos($browser->gettext(), 'Сто петдесет и осем BGN и 0,10')) {
+        } else {
                 
-                return unit_MinkPbgERP::reportErr('Грешна обща сума', 'warning');
+            return unit_MinkPbgERP::reportErr('Грешна обща сума', 'warning');
             }
         }
     }
