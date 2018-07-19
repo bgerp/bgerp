320489bca2497d3cf8c5ca13172b2089ec7a3457
atrifonoff <angel.trifonoff@gmail.com>
2018-07-19 18:28:27 +0300

    korekcii

--- sales/reports/SoldProductsRep.class.php
+++ sales/reports/SoldProductsRep.class.php
@@ -216,7 +216,7 @@ class sales_reports_SoldProductsRep extends frame2_driver_TableData
         if (isset($rec->dealers)) {
             
             if ((min(array_keys(keylist::toArray($rec->dealers))) >= 1)) {
-               
+                
                 $dealers = keylist::toArray($rec->dealers);
                 
                 $query->in('dealerId', $dealers);
