62ca167ba8537ed8691668183327282a63a302bf
arsov <arsov@experta.bg>
2018-08-09 15:47:06 +0300

    Revert "bugfix"
    This reverts commit 8b23e74502ce2b1ea4611c4336b82a376cdbe49a.

--- acc/Balances.class.php
+++ acc/Balances.class.php
@@ -420,7 +420,7 @@ class acc_Balances extends core_Master
     /**
      * Рекалкулира баланса
      */
-    public function recalc()
+    public function recalc($rc)
     {
         $lockKey = 'RecalcBalances';
         
