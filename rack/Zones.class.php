f562d375cd77487e91936a68513812aa2e2422eb
ivelin1012 <ivelin_pdimov@abv.bg>
2018-08-15 13:16:11 +0300

    Зони: добавен клас на commonRow-а

--- rack/Zones.class.php
+++ rack/Zones.class.php
@@ -450,4 +450,13 @@ class rack_Zones extends core_Master
         
         return $num;
     }
+    
+    
+    /**
+     * Преди рендиране на таблицата
+     */
+    protected static function on_BeforeRenderListTable($mvc, &$tpl, $data)
+    {
+        $data->listTableMvc->commonRowClass = 'zonesCommonRow';
+    }
 }
