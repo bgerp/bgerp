6735e0ec40b1acc6cff680f86ffd90b97ff73c08
pavlina <pdainovska@experta.bg>
2018-08-20 12:03:09 +0300

    Корекция на правописна грешка.

--- bank/Document.class.php
+++ bank/Document.class.php
@@ -351,7 +351,7 @@ abstract class bank_Document extends deals_PaymentDocument
         
         // Ако не е избрана сметка, показваме бутона за контиране но с грешка
         if (($rec->state == 'draft' || $rec->state == 'pending') && !isset($rec->ownAccount) && $mvc->haveRightFor('conto')) {
-            $data->toolbar->addBtn('Контиране', array(), array('id' => 'btnConto', 'error' => 'Документа не може да бъде контиран, докато няма посочена банкова сметка|*!'), 'ef_icon = img/16/tick-circle-frame.png,title=Контиране на документа');
+            $data->toolbar->addBtn('Контиране', array(), array('id' => 'btnConto', 'error' => 'Документът не може да бъде контиран, докато няма посочена банкова сметка|*!'), 'ef_icon = img/16/tick-circle-frame.png,title=Контиране на документа');
         }
     }
     
