46873e10cd8a4e55e38257459703a86436f270a4
ivelin1012 <ivelin_pdimov@abv.bg>
2018-08-30 13:56:58 +0300

    pos_Reports:козметична промяна

--- pos/Reports.class.php
+++ pos/Reports.class.php
@@ -404,7 +404,7 @@ class pos_Reports extends core_Master
             // Ако детайла е плащане
             $row->pack = $currencyCode;
             $value = ($obj->value != -1) ? cond_Payments::getTitleById($obj->value) : tr('В брой');
-            $row->value = tr('Плащания') . ": &nbsp;<i>{$value}</i>";
+            $row->value = "<b>" . tr('Плащане') . "</b>: &nbsp;<i>{$value}</i>";
             $row->ROW_ATTR['class'] = 'report-payment';
             unset($row->quantity);
         }
