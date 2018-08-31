b31102e8162e787b8503e5ba420d9d63f3b0823a
ivelin1012 <ivelin_pdimov@abv.bg>
2018-08-31 15:30:53 +0300

    Начало на интерфейс на онлайн плащане

---
+++ cond/OnlinePaymentIntf.class.php
@@ -0,0 +1,47 @@
+<?php
+
+
+/**
+ * Интерфейс за онлайн плащане
+ *
+ *
+ * @category  bgerp
+ * @package   cond
+ *
+ * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
+ * @copyright 2006 - 2018 Experta OOD
+ * @license   GPL 3
+ *
+ * @since     v 0.1
+ */
+class cond_OnlinePaymentIntf extends embed_DriverIntf
+{
+    /**
+     * Инстанция на класа имплементиращ интерфейса
+     */
+    public $class;
+    
+    
+    /**
+     * Генериране на бутон за онлайн плащане
+     * 
+     * @param int $paymentId         - начин на плащане
+     * @param double $amount         - сума на плащане
+     * @param string $currency       - валута на плащане
+     * @param string $okUrl          - урл при потвърждение
+     * @param string $cancelUrl      - урл при отказ
+     * @param mixed $initiatorClass  - класа инициатор
+     * @param int $initiatorId       - ид на инициатор
+     * @param array $soldItems
+     *                  [sysId]      - системен номер на артикула
+     *                  [name]       - име на артикула
+     *                  [quantity]   - продадено количество
+     *                  [price]      - цена на артикула
+     * 
+     * @return string $button        - бутон за онлайн плащане
+     */
+    public function getPaymentUrl($paymentId, $amount, $currency, $okUrl, $cancelUrl, $initiatorClass, $initiatorId, $soldItems = array())
+    {
+        return $this->class->getPaymentUrl($paymentId, $amount, $currency, $okUrl, $cancelUrl, $initiatorClass, $initiatorId, $soldItems);
+    }
+}
