3eff1ed1224531f82abd94dee944b84c631017bd
Yuseinov <yuseinov@experta.bg>
2018-09-10 17:17:06 +0300

    core fix - преместена променлива за цикъла

--- core/QueryCnts.class.php
+++ core/QueryCnts.class.php
@@ -109,11 +109,11 @@ class core_QueryCnts extends core_Manager
      */
     public function on_Shutdown()
     {
-        $slowQueryDelimiter = 3; // По подразбиране да ги разделя на 3 групи
         $divorceSearchMin = 100000; // Когато id-то е над 100К - тогава да сработва
         $divorceSearchMax = 1000000; // Ако записите са над 1М - да ги разделя спрямо бройката, като мин е 3
         
         foreach ($this->queries as $hash => $qCnt) {
+            $slowQueryDelimiter = 3; // По подразбиране да ги разделя на 3 групи
             $lastRec = self::getFromChache($hash, null);
             $cnt = false;
             
