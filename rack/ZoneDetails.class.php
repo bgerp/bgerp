9835a55bfbde43e5de9e02d0770b662be30eef96
ivelin1012 <ivelin_pdimov@abv.bg>
2018-09-11 17:19:58 +0300

    Зони: призрачните записи да не се показват в лист записа

--- rack/ZoneDetails.class.php
+++ rack/ZoneDetails.class.php
@@ -137,10 +137,15 @@ class rack_ZoneDetails extends core_Detail
             
             // Ако няма движения и к-та са 0, реда се маркира
             if(empty($rec->movementQuantity) && empty($rec->documentQuantity) && empty($rec->_movements)){
-                $row->ROW_ATTR['class'] = 'state-rejected';
+                if($data->masterData->rec->_isSingle === true){
+                    $row->ROW_ATTR['class'] = 'state-rejected';
+                } else {
+                    unset($data->rows[$id]);
+                }
             }
         }
     }
+    
     
     /**
      * Записва движение в зоната
