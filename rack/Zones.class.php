ff05bf19cb48a3bfdabd385f10b2cfbad84b4aa3
HeBuTy <nevena@experta.bg>
2018-09-20 16:36:18 +0300

    По-светли вложени нива в зоните

--- rack/Zones.class.php
+++ rack/Zones.class.php
@@ -653,7 +653,7 @@ class rack_Zones extends core_Master
      */
     protected static function on_BeforeRenderListTable($mvc, &$tpl, $data)
     {
-        $data->listTableMvc->commonRowClass = 'zonesCommonRow';
+        $data->listTableMvc->commonRowClass = 'zonesCommonRow zoneLighter';
         $data->listTableMvc->setFieldType('num', 'varchar');
     }
     
