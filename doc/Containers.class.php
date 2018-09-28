1e9b7bab842f73f83a4aa9ee5e6771719a5566e0
Yuseinov <yuseinov@experta.bg>
2018-09-28 17:29:38 +0300

    Revert "Revert "doc_Containers #C24423 - добавен индекс за originId""
    This reverts commit f1712f3572748c1e0cde70c42d72947e9493ca47.

--- doc/Containers.class.php
+++ doc/Containers.class.php
@@ -127,6 +127,7 @@ class doc_Containers extends core_Manager
         $this->setDbIndex('createdBy');
         $this->setDbIndex('createdOn');
         $this->setDbIndex('modifiedOn');
+        $this->setDbIndex('originId');
         $this->setDbUnique('docClass, docId');
         $this->setDbIndex('searchKeywords', null, 'FULLTEXT');
     }
