3fb36fc6132f4d15a491a521540334f6058a3eab
angel <angel.trifonoff@gmail.com>
2018-10-05 18:19:50 +0300

    bugfix

--- sales/reports/SoldProductsRep.class.php
+++ sales/reports/SoldProductsRep.class.php
@@ -497,16 +497,12 @@ class sales_reports_SoldProductsRep extends frame2_driver_TableData
         }
      
         if (!is_null($recs)) {
-            if ($rec->grouping == 'no' && $rec->group){
-                arr::sortObjects($recs, 'code', 'аsc', 'native');
-            }
-            
-            if ($rec->grouping == 'no' && !$rec->group){
-                arr::sortObjects($recs, 'code', 'аsc', 'native');
+            if ($rec->grouping == 'no'){
+                arr::sortObjects($recs, 'code', 'аsc', 'stri');
             }
             
             if ($rec->grouping == 'yes'){
-                arr::sortObjects($recs, 'code', 'аsc', 'native');
+                arr::sortObjects($recs, 'primeCost', 'desc', 'native');
             }
         }
        
