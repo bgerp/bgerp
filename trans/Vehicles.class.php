e61b805ae2391605f848f495da907732b43ef338
ivelin1012 <ivelin_pdimov@abv.bg>
2018-10-19 15:24:02 +0300

    Т средства: добавено canSingle

--- trans/Vehicles.class.php
+++ trans/Vehicles.class.php
@@ -2,7 +2,7 @@
 
 
 /**
- * Клас 'trans_vehicles'
+ * Клас 'trans_Vehicles'
  *
  * Мениджър за транспортни средства
  *
@@ -10,7 +10,7 @@
  * @package   trans
  *
  * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
- * @copyright 2006 - 2017 Experta OOD
+ * @copyright 2006 - 2018 Experta OOD
  * @license   GPL 3
  *
  * @since     v 0.1
@@ -39,6 +39,12 @@ class trans_Vehicles extends core_Master
      * Кой има право да променя?
      */
     public $canEdit = 'ceo, trans';
+    
+    
+    /**
+     * Кой има право да разглежда?
+     */
+    public $canSingle = 'ceo, trans';
     
     
     /**
