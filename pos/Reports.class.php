ccad9304a7d922ef710797defe5da332228b4269
ivelin1012 <ivelin_pdimov@abv.bg>
2018-11-07 16:21:22 +0200

    pos_reports: по-хубава маска на периода

--- pos/Reports.class.php
+++ pos/Reports.class.php
@@ -183,8 +183,8 @@ class pos_Reports extends core_Master
         $row->title = $mvc->getLink($rec->id, 0);
         $row->pointId = pos_Points::getHyperLink($rec->pointId, true);
         
-        $row->from = dt::mysql2verbal($rec->details['receipts'][0]->createdOn);
-        $row->to = dt::mysql2verbal($rec->details['receipts'][count($rec->details['receipts']) - 1]->createdOn);
+        $row->from = dt::mysql2verbal($rec->details['receipts'][0]->createdOn, 'd.m.Y H:i');
+        $row->to = dt::mysql2verbal($rec->details['receipts'][count($rec->details['receipts']) - 1]->createdOn, 'd.m.Y H:i');
         
         if ($fields['-single']) {
             $pointRec = pos_Points::fetch($rec->pointId);
