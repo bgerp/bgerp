30405c6a5a2b2e5805d8523a5052bdc7fa972f8a
Milen Georgiev <milen@experta.bg>
2018-11-08 13:41:53 +0200

    Очаква на EF_UPLOADS_PATH . '/wrfiles' да може да се пише

--- core/Webroot.class.php
+++ core/Webroot.class.php
@@ -98,6 +98,7 @@ class core_Webroot
     {
         if (!file_exists(WEBROOT_FILES_PATH)) {
             mkdir(WEBROOT_FILES_PATH, 0777, true);
+            expect(is_writable(WEBROOT_FILES_PATH));
         }
 
         if(is_numeric($domain)) {
