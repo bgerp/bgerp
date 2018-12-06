2738711b42449f46faf6cfcd557213f97101d742
atrifonoff <angel.trifonoff@gmail.com>
2018-12-06 16:10:34 +0200

    правописни грешки

--- tcost/reports/ComparisonOfTransportCosts.class.php
+++ tcost/reports/ComparisonOfTransportCosts.class.php
@@ -12,7 +12,7 @@
  * @license   GPL 3
  *
  * @since     v 0.1
- * @title     Логистика » Сравнение на очкваните и реалните траспортни разходи
+ * @title     Логистика » Сравнение на очакваните и реалните транспортни разходи
  */
 class tcost_reports_ComparisonOfTransportCosts extends frame2_driver_TableData
 {
