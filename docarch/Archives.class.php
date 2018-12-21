c731a817140698cac4fed939b313f8de0c3f187f
atrifonoff <angel.trifonoff@gmail.com>
2018-12-21 16:47:45 +0200

    working

--- docarch/Archives.class.php
+++ docarch/Archives.class.php
@@ -13,25 +13,24 @@
  * @since     v 0.1
  * @title     Архиви
  */
-
 class docarch_Archives extends core_Master
 {
     public $title = 'Архив';
     
-    public $loadList = 'plg_Created,plg_Modified';
+    public $loadList = 'plg_Created, plg_RowTools2,plg_Modified';
     
     public $listFields = 'id,name,modifiedOn=Модифициране';
     
     protected function description()
     {
         //Наименование на архива
-        $this->FLD('name', 'varchar(32)', 'caption=Наименование на архива');
+        $this->FLD('name', 'varchar(32)', 'caption=Наименование');
         
         //Видове томове/обеми/контейнери за съхранение
-        $this->FLD('volType', 'set(folder=Папка,box=Кутия, case=Нещоси, pallet=Палет, warehouse=Склад)', 'caption=Видове томове');
+        $this->FLD('volType', 'set(folder=Папка,box=Кутия, case=Кашон, pallet=Палет, warehouse=Склад)', 'caption=Видове томове');
         
         //Какъв тип документи ще се съхраняват в този архив
-        $this->FLD('documents', 'keylist(mvc=core_Classes, select=title,allowEmpty)', 'caption=Документи');
+        $this->FLD('documents', 'keylist(mvc=core_Classes, select=title,allowEmpty)', 'caption=Документи,placeholder=Всички');
         
         //Кой може да добавя документи в този архив
         $this->FLD('sharedUsers', 'userList(rolesForAll=sales|ceo,allowEmpty,roles=ceo|sales)', 'caption=Потребители');
@@ -39,8 +38,36 @@ class docarch_Archives extends core_Master
         
         //Срок за съхранение
         $this->FLD('storageTime', 'time(suggestions=1 година|2 години|3 години|4 години|5 години|10 години)', 'caption=Срок');
+    }
+    
+    
+    /**
+     * Преди показване на форма за добавяне/промяна.
+     *
+     * @param frame2_driver_Proto $Driver
+     *                                      $Driver
+     * @param embed_Manager       $Embedder
+     * @param stdClass            $data
+     */
+    protected static function on_AfterPrepareEditForm($mvc, &$data)
+    {
+        $form = $data->form;
+        $rec = $form->rec;
         
-        //$this->setDbUnique('path');
+        
+        $docClasses = core_Classes::getOptionsByInterface('doc_DocumentIntf');
+        
+        $docClasses = array_keys($docClasses);
+        
+        $temp = array();
+        
+        foreach ($docClasses as $k => $v) {
+            $temp[$v] = core_Classes::getTitleById($v);
+        }
+        
+        $docClasses = $temp;
+        
+        $form->setSuggestions('documents', $docClasses);
     }
     
     
@@ -66,8 +93,6 @@ class docarch_Archives extends core_Master
          */
         requireRole('admin');
         
-        
         return 'Action';
     }
-   
 }
--- docarch/Volumes.class.php
+++ docarch/Volumes.class.php
@@ -1,1 +1,114 @@
 <?php
+/**
+ * Мениджър Видове томове и контейнери в архива
+ *
+ *
+ * @category  bgerp
+ * @package   docart
+ *
+ * @author    Angel Trifonov angel.trifonoff@gmail.com
+ * @copyright 2006 - 2018 Experta OOD
+ * @license   GPL 3
+ *
+ * @since     v 0.1
+ * @title     Томове и контейнери
+ */
+class docarch_Volumes extends core_Master
+{
+    public $title = 'Томове и контейнери';
+    
+    public $loadList = 'plg_Created, plg_RowTools2,plg_Modified';
+    
+    public $listFields = 'number,type,inCharge,archive,createdOn=Създаден,modifiedOn=Модифициране';
+    
+    protected function description()
+    {
+        //Определя в кой архив се съхранява конкретния том
+        $this->FLD('archive', 'key(mvc=docarch_Archives,allowEmpty)', 'caption=В архив');
+        
+        //В какъв тип контейнер/том от избрания архив се съхранява документа
+        $this->FLD('type', 'enum(folder=Папка,box=Кутия, case=Нещоси, pallet=Палет, warehouse=Склад)', 'caption=Тип');
+        
+        //Това е номера на дадения вид том в дадения архив
+        $this->FLD('number', 'int', 'caption=Номер');
+        
+        //Отговорник на този том/контейнер
+        $this->FLD('inCharge', 'key(mvc=core_Users)', 'caption=Отговорник');
+        
+        //Съдържа ли документи
+        $this->FLD('isForDocuments', 'enum(yes,no)', 'caption=Съдържа ли документи,input=none');
+        
+        //Показва в кой по-голям том/контейнер е включен
+        $this->FLD('includeIn', 'key(mvc=docarch_Vlolumes)', 'caption=По-големия том,input=hidden');
+        $this->FLD('position', 'varchar(32)', 'caption=Позиция в по-големия том,input=hidden');
+        
+        //Състояние
+        $this->FLD('state', 'enum(active=Активен,rejected=Изтрит,closed=Приключен)', 'caption=Статус,input=none,notSorting');
+        
+        //Оща информация
+        $this->FLD('firstDocDate', 'date', 'caption=Дата на първия документ в тома,input=none');
+        $this->FLD('lastDocDate', 'date', 'caption=Дата на последния документ в тома,input=none');
+        $this->FLD('docCnt', 'int', 'caption=Дата на първия документ,input=none');
+        
+        
+        $this->setDbUnique('archive,type,number');
+    }
+    
+    
+    /**
+     * Преди показване на листовия тулбар
+     *
+     * @param core_Manager $mvc
+     * @param stdClass     $data
+     */
+    public static function on_AfterPrepareListToolbar($mvc, &$res, $data)
+    {
+        $data->toolbar->addBtn('Бутон', array($mvc, 'Action'));
+    }
+    
+    
+    /**
+     * Преди показване на форма за добавяне/промяна.
+     *
+     * @param frame2_driver_Proto $Driver
+     *                                      $Driver
+     * @param embed_Manager       $Embedder
+     * @param stdClass            $data
+     */
+    protected static function on_AfterPrepareEditForm($mvc, &$data)
+    {
+        $form = $data->form;
+        $rec = $form->rec;
+        
+        if ($rec->id) {
+            $form->setReadOnly('archive');
+            $form->setReadOnly('type');
+        }
+    }
+    
+    
+    /**
+     * Филтър
+     *
+     * @param core_Mvc $mvc
+     * @param stdClass $data
+     */
+    public static function on_AfterPrepareListFilter($mvc, &$res, $data)
+    {
+    }
+    
+    
+    /**
+     * @return string
+     */
+    public function act_Action()
+    {
+        /**
+         * Установява необходима роля за да се стартира екшъна
+         */
+        requireRole('admin');
+        
+        $a = docarch_Volumes::getQuery()->fetchAll();
+        bp($a);
+    }
+}
