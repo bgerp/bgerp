67831f29cb684fd6fc011157eb0c9082a9eceb27
pavlina <pdainovska@experta.bg>
2019-01-11 15:36:46 +0200

    (ivo) Палети:бъгфикс

--- rack/Pallets.class.php
+++ rack/Pallets.class.php
@@ -123,7 +123,11 @@ class rack_Pallets extends core_Manager
         $query->where("#productId = {$productId} AND #storeId = {$storeId} AND #state != 'closed'");
         $query->show('quantity,position');
         if(!is_null($batch)){
-            $query->where("#batch = '{$batch}'");
+            if(empty($batch)){
+                $query->where("#batch = '{$batch}' OR #batch IS NULL");
+            } else {
+                $query->where("#batch = '{$batch}'");
+            }
         }
         
         $query->orderBy('createdOn', 'ASC');
