2529ac9133dc7297ac0b3448a9efc103d09156e4
Ivelin Dimov <ivelin_pdimov@abv.bg>
2019-03-01 13:17:03 +0200

    продуктови параметри:бъгфикс

--- planning/Wrapper.class.php
+++ planning/Wrapper.class.php
@@ -33,6 +33,7 @@ class planning_Wrapper extends plg_ProtoWrapper
         $this->TAB(array('planning_AssetResources', 'type' => 'nonMaterial'), 'Ресурси->Нематериални', 'ceo,planning');
         $this->TAB('planning_Hr', 'Ресурси->Хора', 'ceo,planning');
         $this->TAB('planning_AssetGroups', 'Ресурси->Групи', 'ceo,planning');
+        $this->TAB('planning_Stages', 'Ресурси->Етапи', 'ceo,planning');
         
         $this->TAB('planning_FoldersWithResources', 'Настройки->Папки с ресурси', 'ceo,planning');
         $this->TAB('planning_AssetResourcesNorms', 'Настройки->Норми', 'ceo,planning');
