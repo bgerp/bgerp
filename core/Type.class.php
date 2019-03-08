ebe436620545d2007feac400799a43c30c948111
Ivelin Dimov <ivelin_pdimov@abv.bg>
2019-03-08 15:02:47 +0200

    core_Type: възможност за задаване на `warningMax` и `warningMin`

--- core/Type.class.php
+++ core/Type.class.php
@@ -312,6 +312,20 @@ class core_Type extends core_BaseClass
                 }
             }
             
+            if (!$res['error'] && isset($this->params['warningMax'])) {
+                if ($value > $this->params['Max']) {
+                    $res['warning'] = 'Стойността е над' . "|* - '" .
+                    $this->toVerbal($this->params['warningMax']) . "'";
+                }
+            }
+            
+            if (!$res['error'] && isset($this->params['warningMin'])) {
+                if ($value < $this->params['Max']) {
+                    $res['warning'] = 'Стойността е под' . "|* - '" .
+                        $this->toVerbal($this->params['warningMax']) . "'";
+                }
+            }
+            
             if ($res['error']) {
                 $this->error = $res['error'];
             }
