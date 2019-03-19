95a54dae924aa210e4931c19ef344f99c8d8b8e5
Ivelin Dimov <ivelin_pdimov@abv.bg>
2019-03-19 15:35:45 +0200

    палети: по хубаво изтриване при миграция

--- rack/Setup.class.php
+++ rack/Setup.class.php
@@ -200,11 +200,11 @@ class rack_Setup extends core_ProtoSetup
             $Zones->saveArray($toSave, 'id,batch');
         }
         
+        $deleteArr = array();
         $query2 = $Zones->getQuery();
         $query2->where("#batch = ''");
         $query2->orderBy('id', 'ASC');
         while($rec2 = $query2->fetch()){
-            
             $exRec = rack_ZoneDetails::fetch("#id != '{$rec2->id}' AND #zoneId = {$rec2->zoneId} AND #productId = {$rec2->productId} AND #packagingId = {$rec2->packagingId} AND #batch = ''");
             if(!$exRec) continue;
             
@@ -212,7 +212,11 @@ class rack_Setup extends core_ProtoSetup
             $rec2->documentQuantity = !empty($rec2->documentQuantity) ? $rec2->documentQuantity : $exRec->documentQuantity;
             
             $Zones->save($rec2, 'movementQuantity,documentQuantity');
-            rack_ZoneDetails::delete($exRec->id);
+            $deleteArr[$exRec->id] = $exRec->id;
+        }
+        
+        foreach ($deleteArr as $delId) {
+            rack_ZoneDetails::delete($delId);
         }
     }
 }
