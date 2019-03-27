749ba4812111332a1097fe1ce9b68a532eb6abf3
HeBuTy <nevena@experta.bg>
2019-03-27 13:18:15 +0200

    terminal пропуснати файлове

--- planning/tpl/terminal/scripts.js
+++ planning/tpl/terminal/scripts.js
@@ -2,7 +2,7 @@ function planningActions() {
 	$("input[name=serial]").focus();
 	
 	// Използване на числата за въвеждане на суми за плащания
-	$(document.body).on('click', "#planning-terminal-send-btn", function(e){
+	$(document.body).on('click', "#sendBtn", function(e){
 		var url = $(this).attr("data-url");
 		if(!url) return;
 		
@@ -11,7 +11,7 @@ function planningActions() {
 		
 		var serial = $("input[name=serial]").val();
 		var type = $("#typeSelect").is('[readonly]') ?  $("input[name=type]").val() : $("#typeSelect").val();
-		var productId = $("input[name=productId]").is('[readonly]') ? $("#productIdSelect").val() : $("input[name=productId]").val();
+		var productId = $("#productIdSelect").is('[readonly]') ? $("input[name=productId]").val() :  $("#productIdSelect").val() ;
 		var quantity = $("input[name=quantity]").val();
 		var employees = $("#employeeSelect").val();
 		var fixedAsset = $("#fixedAssetSelect").val();
@@ -33,5 +33,16 @@ function planningActions() {
 		resObj = new Object();
 		resObj['url'] = url;
 		getEfae().process(resObj);
+	});
+
+	var currentTab = $('.tabs-holder li.active a').attr('href');
+	$('.tabContent' + currentTab).addClass('active');
+
+	// Скриване на табовете
+	$(document.body).on('click', ".tabs-holder li a ", function(e){
+		var currentAttrValue= $(this).attr('href');
+		$('.tabContent' + currentAttrValue).show().siblings().hide();
+		$(this).parent('li').addClass('active').siblings().removeClass('active');
+		e.preventDefault();
 	});
 }
