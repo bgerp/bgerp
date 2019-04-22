44df6f0e7a1aa2abf67081f72496f97388444308
Milen Georgiev <milen@experta.bg>
2019-04-22 10:55:13 +0300

    sao - без видими колони

--- plg/StructureAndOrder.class.php
+++ plg/StructureAndOrder.class.php
@@ -33,10 +33,9 @@ class plg_StructureAndOrder extends core_Plugin
     {
         $mvc->FNC('saoPosition', 'enum(next=След,prev=Преди,subLevel=Под ниво)', 'caption=Структура и подредба->Положение,input=none,column=none,order=100000,maxRadio=3,columns=3');
         $mvc->FNC('saoRelative', 'int', 'caption=Структура и подредба->Спрямо,input=none,column=none,order=100000,class=w100');
-        
-        $mvc->FLD('saoParentId', 'int', 'caption=Структура и подредба->Родител,input=none,column1=none,order=100000');
-        $mvc->FLD('saoOrder', 'double', 'caption=Структура и подредба->Подредба,input=none,column1=none,order=100000');
-        $mvc->FLD('saoLevel', 'int', 'caption=Структура и подредба->Ниво,input=none,column1=none,order=100000');
+        $mvc->FLD('saoParentId', 'int', 'caption=Структура и подредба->Родител,input=none,column=none,order=100000');
+        $mvc->FLD('saoOrder', 'double', 'caption=Структура и подредба->Подредба,input=none,column=none,order=100000');
+        $mvc->FLD('saoLevel', 'int', 'caption=Структура и подредба->Ниво,input=none,column=none,order=100000');
         
         $mvc->listItemsPerPage = max($mvc->listItemsPerPage, 1000);
         
