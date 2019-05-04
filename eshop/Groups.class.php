de463f28cc6076cba54314d9ac931caec5f79c53
Milen Georgiev <milen@experta.bg>
2019-05-04 13:30:29 +0300

    Без показване на групите, които нямат картинки в общия блок

--- eshop/Groups.class.php
+++ eshop/Groups.class.php
@@ -177,6 +177,10 @@ class eshop_Groups extends core_Master
         if ($form->rec->menuId && !$opt[$form->rec->menuId]) {
             $form->rec->menuId = key($opt);
         }
+
+        if(count($opt) && !$form->isSubmitted()) { 
+            $form->rec->menuId = key($opt);
+        }
         
         if ($form->rec->menuId) {
             $data->query->where(array("#menuId = '[#1#]'", $form->rec->menuId));
@@ -196,9 +200,7 @@ class eshop_Groups extends core_Master
             $row->name = $mvc->getHyperlink($rec, true);
             
             $row->name = $mvc->saoGetTitle($rec, $row->name, '&nbsp;&nbsp;');
-            
-            //      if($rec->saoLevel > 1) bp($row->name);
-            
+                        
             if (haveRole('powerUser') && $rec->state != 'closed') {
                 core_RowToolbar::createIfNotExists($row->_rowTools);
                 $row->_rowTools->addLink('Преглед', self::getUrl($rec), 'alwaysShow,ef_icon=img/16/monitor.png,title=Преглед във външната част');
@@ -434,6 +436,8 @@ class eshop_Groups extends core_Master
                 if ($rec->icon) {
                     $img = new thumb_Img($rec->icon, 400, 300, 'fileman');
                     $tpl->replace(ht::createLink($img->createImg(), $rec->url), 'img');
+                } else {
+                    continue;
                 }
                 $name = ht::createLink($this->getVerbal($rec, 'name'), $rec->url);
                 $tpl->replace($name, 'name');
