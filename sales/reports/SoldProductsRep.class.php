273f1311d604f6fec7f8d146ff1880ea437e6afc
Yuseinov <yuseinov@experta.bg>
2019-05-07 14:35:13 +0300

    test

--- sales/reports/SoldProductsRep.class.php
+++ sales/reports/SoldProductsRep.class.php
@@ -74,7 +74,7 @@ class sales_reports_SoldProductsRep extends frame2_driver_TableData
         $fieldset->FLD('crmGroup', 'keylist(mvc=crm_Groups,select=name)', 'caption=Контрагенти->Група контрагенти,after=contragent,single=none');
       
         $fieldset->FLD('group', 'keylist(mvc=cat_Groups,select=name)', 'caption=Артикули->Група артикули,after=crmGroup,single=none');
-        $fieldset->FLD('products', 'key2(mvc=cat_Products,select=name,selectSourceArr=cat_Products::getProductOptions,allowEmpty,maxSuggestions=100,forceAjax)', 'caption=Артикули->Артикули,after=group,single=none');
+        $fieldset->FLD('products', 'key(mvc=cat_Products,select=name)', 'caption=Артикули->Артикули,after=group,single=none');
         $fieldset->FLD('articleType', 'enum(yes=Стандартни,no=Нестандартни,all=Всички)', 'caption=Артикули->Тип артикули,maxRadio=3,columns=3,after=productId,single=none');
         
         //Покаване на резултата
