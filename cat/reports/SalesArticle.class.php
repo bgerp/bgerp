70152b4125e349a020e30166a2293b1b582a51cc
atrifonoff <angel.trifonoff@gmail.com>
2019-05-08 17:25:24 +0300

    deprecat

--- cat/reports/SalesArticle.class.php
+++ cat/reports/SalesArticle.class.php
@@ -12,9 +12,10 @@
  * @author    Gabriela Petrova <gab4eto@gmail.com>
  * @copyright 2006 - 2015 Experta OOD
  * @license   GPL 3
- * @deprecated
- *
+ * 
  * @since     v 0.1
+ * 
+ * @deprecated
  */
 class cat_reports_SalesArticle extends frame_BaseDriver
 {
