e0d1bbf817f8d84c7dd3eb8df3a753499bdc217c
Ivelin Dimov <ivelin_pdimov@abv.bg>
2019-05-09 14:53:09 +0300

    Производство: размяна

--- planning/Wrapper.class.php
+++ planning/Wrapper.class.php
@@ -28,16 +28,13 @@ class planning_Wrapper extends plg_ProtoWrapper
         $this->TAB('planning_Tasks', 'Операции->Списък', 'ceo,taskWorker');
         $this->TAB('planning_ProductionTaskDetails', 'Операции->Прогрес', 'ceo,taskWorker');
         $this->TAB('planning_Centers', 'Центрове', 'ceo, planning');
-        
         $this->TAB(array('planning_AssetResources', 'type' => 'material'), 'Ресурси->Оборудване', 'ceo,planning');
         $this->TAB(array('planning_AssetResources', 'type' => 'nonMaterial'), 'Ресурси->Нематериални', 'ceo,planning');
         $this->TAB('planning_Hr', 'Ресурси->Хора', 'ceo,planning');
-        $this->TAB('planning_AssetGroups', 'Ресурси->Групи', 'ceo,planning');
         $this->TAB('planning_Stages', 'Ресурси->Етапи', 'ceo,planning');
-        
+        $this->TAB('planning_AssetGroups', 'Ресурси->Групи', 'ceo,planning');
         $this->TAB('planning_FoldersWithResources', 'Настройки->Папки с ресурси', 'ceo,planning');
         $this->TAB('planning_AssetResourcesNorms', 'Настройки->Норми', 'ceo,planning');
-        
         $this->TAB('planning_WorkCards', 'Раб. карти', 'ceo,planning,admin');
         
         $this->title = 'Планиране';
