2b2d5e5ad09720cb0879dea567c1242ff34ef792
Milen Georgiev <milen@experta.bg>
2019-06-17 09:06:04 +0300

    bugFix

--- log/Ips.class.php
+++ log/Ips.class.php
@@ -332,9 +332,10 @@ class log_Ips extends core_Manager
     public function cron_UpdateIpInfo()
     {
         $query = self::getQuery();
-        $before1day = dt::addDays(-1);
+        $before1day = dt::addDays(-3);
+        $query->orderBy('createdOn', 'DESC');
         $query->limit(10);
-        $query->where("#createdOn < '{$before1day}' AND (#host IS NULL OR #country2 IS NULL)");
+        $query->where("#createdOn > '{$before1day}' AND (#host IS NULL OR #country2 IS NULL)");
         while($rec = $query->fetch()) {
             self::decorateIp($rec->ip);
         }
