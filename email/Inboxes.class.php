a7f5d9de4485b97e721af97df4eb3fcffaa07a64
Yuseinov <yuseinov@experta.bg>
2019-07-09 17:06:46 +0300

    email_Inboxes #Tsk2197 - добавено име за "From:" хедъра

--- email/Inboxes.class.php
+++ email/Inboxes.class.php
@@ -171,10 +171,27 @@ class email_Inboxes extends core_Master
     public function description()
     {
         $this->FLD('email', 'email(link=no)', 'caption=Имейл, mandatory, silent');
+        $this->FLD('name', 'varchar(128)', 'caption=Име, silent');
         $this->FLD('accountId', 'key(mvc=email_Accounts, select=email)', 'caption=Сметка, refreshForm, mandatory, notNull, silent');
         $this->FLD('notifyForEmail', 'enum(yes=Винаги,no=Стандартно за системата)', 'caption=Нотифициране на отговорниците за получен имейл->Избор, notNull');
         
         $this->setDbUnique('email');
+    }
+    
+    /**
+     * Връща името за "From:" хедъра
+     * 
+     * @param integer $id
+     * 
+     * @return string
+     */
+    public static function getFromName($id)
+    {
+        $res = '';
+        
+        $res = self::fetchField($id, 'name');
+        
+        return $res;
     }
     
     
