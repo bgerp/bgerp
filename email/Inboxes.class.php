dc91c291f462cee8a9443af1f193359c4d33081b
Yuseinov <yuseinov@experta.bg>
2019-07-10 17:06:26 +0300

    email #C27785 - име на потребителия и фирмата във `from`

--- email/Inboxes.class.php
+++ email/Inboxes.class.php
@@ -178,6 +178,7 @@ class email_Inboxes extends core_Master
         $this->setDbUnique('email');
     }
     
+    
     /**
      * Връща името за "From:" хедъра
      * 
@@ -189,7 +190,44 @@ class email_Inboxes extends core_Master
     {
         $res = '';
         
-        $res = self::fetchField($id, 'name');
+        $rec = self::fetch($id);
+        
+        if (!$rec) {
+            
+            return $res;
+        }
+        
+        $res = trim($rec->name);
+        
+        if (!$res) {
+            $email = mb_strtolower(trim($rec->email));
+            
+            $cEmailArr = email_Accounts::getCommonAndCorporate();
+            
+            $ourCRec = crm_Companies::fetchOurCompany('name');
+            
+            if ($cEmailArr[$email]) {
+                $res = $ourCRec->name;
+            } else {
+                if (!email_Accounts::fetch(array("LOWER(#email) = '[#1#]' AND #state = 'active'", $email))) {
+                    if ($rec->inCharge > 0) {
+                        $names = core_Users::fetchField($rec->inCharge, 'names');
+                        $names = core_Users::prepareUserNames($names);
+                        
+                        if ($names) {
+                            $res .= $names;
+                        }
+                        $res .= $res ? ', ' : '';
+                        
+                        if (defined('OUR_COMPANY_SHORT_NAME')) {
+                            $res .= OUR_COMPANY_SHORT_NAME;
+                        } else {
+                            $res .= $ourCRec->name;
+                        }
+                    }
+                }
+            }
+        }
         
         return (string) $res;
     }
