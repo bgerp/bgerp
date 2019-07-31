f29a6fee7eea280cdca9c642260b4d0bd242e4fb
Ivelin Dimov <ivelin_pdimov@abv.bg>
2019-07-31 16:13:40 +0300

    протоколи за производство и влагане промяна

--- planning/Tasks.class.php
+++ planning/Tasks.class.php
@@ -1275,5 +1275,11 @@ class planning_Tasks extends core_Master
             $pUrl = array('planning_DirectProductionNote', 'add', 'originId' => $rec->containerId, 'ret_url' => true);
             $data->toolbar->addBtn('Произвеждане', $pUrl, 'ef_icon = img/16/page_paste.png,title=Създаване на протокол за производство от операцията');
         }
+        
+        // Бутон за добавяне на документ за производство
+        if (planning_ConsumptionNotes::haveRightFor('add', (object) array('originId' => $rec->containerId))) {
+            $pUrl = array('planning_ConsumptionNotes', 'add', 'originId' => $rec->containerId, 'ret_url' => true);
+            $data->toolbar->addBtn('Влагане', $pUrl, 'ef_icon = img/16/produce_in.png,title=Създаване на протокол за влагане от операцията');
+        }
     }
 }
