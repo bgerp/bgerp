08cf88ecd28f68db305678b5d56d49003689c824
Yuseinov <yuseinov@experta.bg>
2019-08-20 13:57:39 +0300

    change_Log - да се инвалидира кеша при избор на друга версия

--- change/Log.class.php
+++ change/Log.class.php
@@ -325,6 +325,9 @@ class change_Log extends core_Manager
         // Очакваме да имаме права до сингъла или до треда
         expect($class->haveRightFor('single', $docId) || doc_Threads::haveRightFor('single', $cRec->threadId));
         
+        // Инвалидираме кеша на контейнера
+        doc_DocumentCache::cacheInvalidation($cRec->containerId);
+        
         // Масив с всички избрани версии за съответния документ
         $dataArr = static::getSelectedVersionsArr($classId, $docId);
         
