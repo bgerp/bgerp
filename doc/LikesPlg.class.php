9ec0bb916051f1545c57bc5cea52239e32e1d641
Yuseinov <yuseinov@experta.bg>
2019-08-23 14:13:12 +0300

    викане на makeTooltipFromTitle след рефреш по ajax

--- doc/LikesPlg.class.php
+++ doc/LikesPlg.class.php
@@ -481,6 +481,7 @@ class doc_LikesPlg extends core_Plugin
                 jquery_Jquery::runAfterAjax($row->DocumentSettingsLeft, 'smartCenter');
                 jquery_Jquery::runAfterAjax($row->DocumentSettingsLeft, 'setThreadElemWidth');
                 jquery_Jquery::runAfterAjax($row->DocumentSettingsLeft, 'getContextMenuFromAjax');
+                jquery_Jquery::runAfterAjax($row->DocumentSettingsLeft, 'makeTooltipFromTitle');
             }
         }
     }
