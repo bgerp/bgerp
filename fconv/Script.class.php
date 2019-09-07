d9f7ca4d8066d040683a92ac3d478cee2bac6015
Milen Georgiev <milen@experta.bg>
2019-09-07 16:22:26 +0300

    Кеширане в хита на липсващите програми

--- fconv/Script.class.php
+++ fconv/Script.class.php
@@ -448,6 +448,9 @@ class fconv_Script
      */
     public function run($asynch = true, $time = 2, $timeoutCallback = '')
     {
+        // Кеш за липсващи програми
+        static $missing = array();
+
         // Ако е зададена програма, може да се пусне скрипта отдалечено, на друг сървър
         // и да се чака резултат от там
         if (!$this->stopRemote) {
@@ -485,6 +488,7 @@ class fconv_Script
         }
         if (!empty($checkProgramsArr)) {
             foreach ($checkProgramsArr as $program) {
+                if($missing[$program]) return false;
                 if (isset($this->programs[$program])) {
                     $path = $this->programs[$program];
                 } else {
@@ -493,7 +497,7 @@ class fconv_Script
                 
                 if (!(is_executable($path) || exec("{$which} {$path}"))) {
                     log_System::add('fconv_Remote', 'Липсва програма: ' . $path, $rRec->id, 'warning');
-                    
+                    $missing[$program] = true;
                     return false;
                 }
             }
