6fff27c5a54946f228d6a79ce0a01aef26a0cd8a
HeBuTy <nevena@experta.bg>
2019-09-20 16:29:24 +0300

    Начален вариант на пакета ztm

---
+++ ztm/Adapter.class.php
@@ -0,0 +1,42 @@
+<?php
+
+
+/**
+ * Клас 'ztm_Adapter'
+ *
+ * Табло с настройки за състояния
+ *
+ *
+ * @author    Nevena Georgieva <nevena.georgieva89@gmail.com>
+ * @copyright 2006 - 2019 Experta OOD
+ * @license   GPL 3
+ *
+ * @since     v 0.1
+ */
+class ztm_Adapter extends core_Mvc
+{
+    public function act_Default()
+    {
+        Mode::set('wrapper', 'page_PureHtml');
+
+
+        $tpl = new ET(getTplFromFile('ztm/tpl/dashboard.shtml'));
+
+
+        $tpl->push('ztm/css/bootstrap.min.css', 'CSS');
+        $tpl->push('ztm/css/bootstrap-grid.css', 'CSS');
+        $tpl->push('ztm/css/font-awesome.min.css', 'CSS');
+        $tpl->push('ztm/css/rangeslider.css', 'CSS');
+        $tpl->push('ztm/css/layout.css', 'CSS');
+
+
+        $tpl->push('ztm/js/jquery-3.1.1.min.js', 'JS');
+        $tpl->push('ztm/js/popper.min.js', 'JS');
+        $tpl->push('ztm/js/bootstrap.min.js', 'JS');
+        $tpl->push('ztm/js/skycons.js', 'JS');
+        $tpl->push('ztm/js/rangeslider.js', 'JS');
+        $tpl->push('ztm/js/custom.js', 'JS');
+
+        return $tpl;
+    }
+}
