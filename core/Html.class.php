607164563699d257d135c9a24795c8894e85c60c
Ivelin Dimov <ivelin_pdimov@abv.bg>
2019-10-24 14:38:41 +0300

    ht::createHint подсигуряване че винаги връща шаблон

--- core/Html.class.php
+++ core/Html.class.php
@@ -1034,13 +1034,9 @@ class core_Html
      */
     public static function createHint($body, $hint, $type = 'notice', $appendToEnd = true, $iconAttr = array(), $elementArr = array())
     {
-        if (empty($hint)) {
+        if (empty($hint) || Mode::is('printing') || Mode::is('text', 'xhtml') || Mode::is('pdf')) {
             
-            return $body;
-        }
-        if (Mode::is('printing') || Mode::is('text', 'xhtml') || Mode::is('pdf')) {
-            
-            return $body;
+            return new core_ET($body);
         }
         
         $hint = strip_tags(tr($hint));
