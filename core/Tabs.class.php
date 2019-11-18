23215fd1d253751e970730c87f8a8b7054e63c47
dminko <mitko@experta.bg>
2019-11-18 12:15:27 +0200

    getallheaders() - дефинирана за phpFPM < 7.3
    Conflicts:
    	core/Tabs.class.php

--- core/Tabs.class.php
+++ core/Tabs.class.php
@@ -107,6 +107,23 @@ class core_Tabs extends core_BaseClass
             core_Settings::setValues('TABS::' . $this->tabGroup, array('DEFAULT_TABS' => $selectedTab));
 
         }
+
+        if (!function_exists('getallheaders'))
+        {
+            function getallheaders()
+            {
+                $headers = [];
+                foreach ($_SERVER as $name => $value)
+                {
+                    if (substr($name, 0, 5) == 'HTTP_')
+                    {
+                        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                    }
+                }
+                return $headers;
+            }
+        }
+        
         $headers = getallheaders();
         
         $head = '';
