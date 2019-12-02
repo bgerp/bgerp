1bd52243af02218dc185e49d47fbb255584b94c4
Ivelin Dimov <ivelin_pdimov@abv.bg>
2019-12-02 14:42:48 +0200

    количка: добавена допълнителна проверка в екшъна за промяна на к-то

--- eshop/CartDetails.class.php
+++ eshop/CartDetails.class.php
@@ -562,6 +562,7 @@ class eshop_CartDetails extends core_Detail
         $this->requireRightFor('updatecart', (object) array('cartId' => $cartId));
         $quantity = rtrim($quantity, '.');
         $quantity = rtrim($quantity, ',');
+        expect($quantity && $quantity > 0, 'Количеството трябва да е положително');
         
         $rec = self::fetch($id);
         $rec->quantity = $quantity * $rec->quantityInPack;
