44c28b3a526040b9e991b32433eea0dfb5a09d7c
HeBuTy <nevena@experta.bg>
2019-12-03 13:35:44 +0200

    POS: махане на излишен код

--- pos/js/scripts.js
+++ pos/js/scripts.js
@@ -28,12 +28,6 @@ function posActions() {
 		});
 	} 
 
-	// Засветяване на избрания ред и запис в хидън поле
-	$(document.body).on('mouseover', ".pos-sale", function(e){
-		$(this).css( 'cursor', 'pointer' );
-	});
-	
-	
 	// Използване на числата за въвеждане в пулта
 	$(document.body).on('click', "#tools-holder .numPad", function(e){
 		var val = $(this).val();
@@ -73,73 +67,7 @@ function posActions() {
 		getEfae().process(resObj, data);
 		$("input[name=paysum]").val("");
 	});
-	
-	// Използване на числата за въвеждане на суми за плащания
-	$(document.body).on('click', "#tools-payment .numPad", function(e){
-		var val = $(this).val();
-		var inpVal = $("input[name=paysum]").val();
-		if(val == '.'){
-			if(inpVal.length == 0){
-				inpVal = 0;
-			}
-			
-			if(inpVal.indexOf(".")  != -1){
-				return;
-			}
-		}
-		
-		inpVal += val;
-		$("input[name=paysum]").val(inpVal);
-		if($('body').hasClass('wide')){
-			$("input[name=paysum]").focus();
-		}
-	});
-	
-	
-	// Триене на числа в пулта
-	$(document.body).on('click', "#tools-form .numBack", function(e){
-		var inpValLength = $("input[name=ean]").val().length;
-		var newVal = $("input[name=ean]").val().substr(0, inpValLength-1);
-		
-		$("input[name=ean]").val(newVal);
-		if($('body').hasClass('wide')){
-			$("input[name=ean]").focus();
-		}
-	});
-	
-	
-	// Триене на числа при плащанията
-	$(document.body).on('click', "#tools-payment .numBack", function(e){
-		var inpValLength = $("input[name=paysum]").val().length;
-		var newVal = $("input[name=paysum]").val().substr(0, inpValLength-1);
-		
-		$("input[name=paysum]").val(newVal);
-		if($('body').hasClass('wide')){
-			$("input[name=paysum]").focus();
-		}
-	});
-	
-	
-	// Модифициране на количество
-	$(document.body).on('click', ".tools-modify", function(e){
-		var inpVal = $("input[name=ean]").val();
-		var rowVal = $("input[name=rowId]").val();
-		
-		var url = $(this).attr("data-url");
-		
-		if(!url){
-			return;
-		}
-		
-		var data = {recId:rowVal, amount:inpVal};
-		
-		resObj = new Object();
-		resObj['url'] = url;
-		
-		getEfae().process(resObj, data);
-		$("input[name=ean]").val("");
-	});
-	
+
 	
 	// При натискане на бутон от клавиатурата, ако е 'ENTER'
 	$(document).keypress(function(e) {
@@ -152,16 +80,11 @@ function posActions() {
 	    	var element = $(".navigable:focus");
 	    	
 	    	if(element.length){
-	    		//console.log(element);
-	    		
-	    		//return;
 	    		// Намира първия елемент с data-url
 	    		var elementDataUrl = element.attr("data-url");
 	    		var hrefUrl = element.attr("href");
 	    		elementDataUrl = (elementDataUrl) ? elementDataUrl : ((hrefUrl) ? hrefUrl : elementDataUrl);
-	    		
-	    		console.log(elementDataUrl);
-	    		
+
 	    		if(elementDataUrl == undefined){
 	    			var child = element.find('[data-url]');
 	    			
@@ -177,8 +100,7 @@ function posActions() {
 	    				element = child;
 	    			}
 	    		}
-	    		
-	    		
+
 	    		if(element != undefined){
 	    			// Вика се клик
 		    		var event = jQuery.Event("click");
@@ -191,50 +113,28 @@ function posActions() {
 	    	if(!url){
 	    		return;
 	    	}
-	    	
-	    	console.log(url, value, 'ddddd');
+
 	    	resObj = new Object();
 			resObj['url'] = url;
 			
 			getEfae().process(resObj, {string:value});
 	    }
 	});
-	
-	
-	// Добавя продукт при събмит на формата
-	$(document.body).on("click", '#addProductBtn', function(event){
-	    var url = $(this).attr("data-url");
-	    if(!url){
-			return;
-		}
-	    
-		var code = $("input[name=ean]").val();
-		var data = {ean:code};
-		
-		resObj = new Object();
-		resObj['url'] = url;
-		
-		getEfae().process(resObj, data);
-	
-		$("input[name=ean]").val("");
-	    return false; 
-	});
-	
+
 	// Направата на плащане след натискане на бутон
 	$(document.body).on('click', ".payment", function(e){
 		if(!$(this).hasClass( "disabledBtn")){
 			var url = $(this).attr("data-url");
-			
+
 			var type = $(this).attr("data-type");
-			
-			
+
+
 			type = (!type) ? '-1' : type;
-			
+
 			doPayment(url, type);
 		}
 	});
-	
-	
+
 	// Бутоните за приключване приключват бележката
 	$(document.body).on('click', ".closeBtns", function(e){
 		var url = $(this).attr("data-url");
@@ -302,7 +202,7 @@ function posActions() {
 		}
 	});
 	
-	// При клик на бутон изтрива запис от бележката
+	// При клик на бутон добавя отстъпка
 	$(document.body).on('click', ".discountBtn", function(e){
 		var url = $(this).attr("data-url");
 		
@@ -314,24 +214,7 @@ function posActions() {
 		
 		getEfae().process(resObj, {recId:selectedRecId});
 	});
-	
-	var tabContent = $('#tools-wide-tabs li.active a').attr('href');
-	$('.tab-content.active').find('input[type=text]').focus();
-	$(tabContent).addClass('active');
 
-	// Скриване на табовете
-	$(document.body).on('click', ".pos-tabs a ", function(e){
-		var currentAttrValue= $(this).attr('href');
-		$('.tab-content' + currentAttrValue).show().siblings().hide();
-		
-		$(this).parent('li').addClass('active').siblings().removeClass('active');
-		$('.tab-content' + currentAttrValue).find('input[type=text]').focus();
-		if($('body').hasClass('wide')) {
-			calculateWidth();
-		}
-		e.preventDefault();
-	}); 
-	
 	
 	// Смяна на текущата клавиатура
 	$(document.body).on('click', ".keyboard-change-btn", function(e){
@@ -393,13 +276,14 @@ function posActions() {
 			pageUp();
 		}
 	});
+
 	if($('.navigable').length) {
 		naviBoard.setNavigation("result-holder");
 	}
 
 
 	// Триене на символи от формата за търсене
-	$(document.body).on('click', ".keyboard-back-btn-tools, .keyboard-back-btn-payment", function(e){
+	$(document.body).on('click', ".keyboard-back-btn-tools", function(e){
 		var inpValLength = $(".large-field").val().length;
 		var newVal = $(".large-field").val().substr(0, inpValLength-1);
 		$(".large-field").val(newVal);
@@ -413,34 +297,8 @@ function posActions() {
 	// Време за изчакване
 	var timeout;
 	
-	
-	$(document.body).on('keyup', "input[name=ean]", function(e){
-		// След всяко натискане на бутон изчистваме времето на изчакване
-		clearTimeout(timeout);
-		
-		var url = $(this).attr("data-keyupurl");
-		if(!url){
-			return;
-		}
-		
-		var inpVal = $(this).val();
-		var operation = getSelectedOperation();
-		
-		var selectedElement = $(".highlighted");
-		var selectedRecId = selectedElement.attr("data-id");
-		
-		// Правим Ajax заявката като изтече време за изчакване
-		timeout = setTimeout(function(){
-			resObj = new Object();
-			resObj['url'] = url;
-			
-			getEfae().process(resObj, {operation:operation,search:inpVal,recId:selectedRecId});
 
-		}, 700);
-
-	});
-	
-	
+	// действие на бутоните за действията
 	$(document.body).on('click', ".operationBtn", function(e){
 		clearTimeout(timeout);
 		var operation = $(this).attr("data-value");
@@ -500,18 +358,6 @@ function posActions() {
 		resObj['url'] = url;
 		getEfae().process(resObj);
 	});
-	
-	// При натискане на бутон със знак добавя знака в полето за търсене на код
-	$(document.body).on('click', ".tools-sign", function(e){
-		var sign = $(this).val();
-		
-		var inpVal = $("input[name=ean]").val();
-		inpVal += sign;
-		
-		$("input[name=ean]").val(inpVal);
-		$("input[name=ean]").focus();
-	});
-
 
 	// сменяне на селектирания ред от бележките при клик
 	$(document.body).on('click', "#receipt-table .receiptRow td", function(e){
@@ -522,61 +368,8 @@ function posActions() {
 
 	// Време за изчакване
 	var timeout1;
-	
-	// Търсене на контрагенти след освобождаване на клавиш
-	$("input[name=input-search-contragent]").keyup(function() {
-		var url = $(this).attr("data-url");
-		var receiptId = $("input[name=receiptId]").val();
-		var inpVal = $("input[name=input-search-contragent]").val();
-		
-		// След всяко натискане на бутон изчистваме времето на изчакване
-		clearTimeout(timeout1);
-		
-		// Правим Ajax заявката като изтече време за изчакване
-		timeout1 = setTimeout(function(){
-			resObj = new Object();
-			resObj['url'] = url;
-			getEfae().process(resObj, {receiptId:receiptId,searchString:inpVal});
-			calculateWidth();
-		}, 3000);
-	});
-	
-	// Търсене на контрагенти след натискане на ENTER
-	$("input[name=input-search-contragent]").keypress(function(e) {
-		if(e.which == 13) {
-			var url = $(this).attr("data-url");
-			var receiptId = $("input[name=receiptId]").val();
-			var inpVal = $("input[name=input-search-contragent]").val();
-			
-			resObj = new Object();
-			resObj['url'] = url;
-			getEfae().process(resObj, {receiptId:receiptId,searchString:inpVal});
-			calculateWidth();
-	    }
-	});
-	
-	// Търсене на контрагенти
-	$(document.body).on('click', ".pos-search-contragent-btn", function(e){
-		var searchStr = $("input[name=input-search-contragent]").val();
-		var receiptId = $("input[name=receiptId]").val();
-		
-		var url2 = $(this).attr("data-url");
-		
-		if(!url2) return;
-		
-		resObj = new Object();
-		resObj['url'] = url2;
-		
-		if (typeof timeout1 != 'undefined') {
-			clearTimeout(timeout1);
-		}
-		
-		getEfae().process(resObj, {receiptId:receiptId,searchString:searchStr});
-		calculateWidth();
-	});
-	
-	
-	
+
+
 	$(document.body).on('click', "div.resultPrice", function(e){
 		var url = $(this).attr("data-url");
 		if(!url) return;
@@ -632,17 +425,12 @@ function posActions() {
 		//return;
 		dialog = $("#productInfo").dialog({
 			autoOpen: false,
-			height: 600,
-			width: 900,
+			height: 700,
+			width: 1000,
 			modal: true
 		});
 
 		dialog.dialog( "open" );
-
-
-
-		console.log(operation, productId);
-
 	});
 	
 }
@@ -677,8 +465,6 @@ function calculateWidth(){
 		$('#result-holder').css('width', maxColWidth);
 	}
 
-
-	
 	//височина за таблицата с резултатите
 	var searchTopHeight = parseInt($('.search-top-holder').height());
 	$('#pos-search-result-table').css('maxHeight', winHeight - searchTopHeight - 120);
@@ -716,14 +502,14 @@ function doPayment(url, type){
 // Показване на определени любими бутони
 function showFavouriteButtons(element, value){
 	element.addClass('active').siblings().removeClass('active');
-	
+
 	if(value) {
 		var nValue = "|" + value + "|";
-		
+
 		$("div.pos-product[data-cat != '"+nValue+"']").each(function() {
 			$(this).hide();
 		});
-		
+
 		$("div.pos-product[data-cat *= '"+nValue+"']").each(function() {
 			$(this).show();
 		});
@@ -732,12 +518,6 @@ function showFavouriteButtons(element, value){
 			$(this).show();
 		});
 	}
-}
-
-// Рендира fancybox-а
-function render_fancybox()
-{
-	$('a.fancybox').fancybox();
 }
 
 function pageUp(){
@@ -786,14 +566,6 @@ function arrowLeft(){
 	console.log('arrowLeft')
 }
 
-function enter(){
-	if($('.pos-product:focus').length) {
-		$('.pos-product:focus').click();
-	}
-	if($('.pos-search-result-table .rowBlock.active').length) {
-		$('.pos-search-result-table .rowBlock.active .pos-add-res-btn').click();
-	}
-}
 
 function getSelectedOperation()
 {
