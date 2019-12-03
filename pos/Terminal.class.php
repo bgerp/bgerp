ea0a1c2e836d16a49c91592bbefd4473657dc136
HeBuTy <nevena@experta.bg>
2019-12-03 11:45:11 +0200

    Корекции по диалоговия прозорец

--- pos/Terminal.class.php
+++ pos/Terminal.class.php
@@ -232,7 +232,7 @@ class pos_Terminal extends peripheral_Terminal
             }
         }
         
-        $enlargeBtn = ht::createFnBtn('', '', '', array('data-url' => toUrl(array('cat_Products', 'single'), 'local'), 'class' => 'enlargeProductBtn', 'ef_icon' => 'img/32/search.png'));
+        $enlargeBtn = ht::createFnBtn(' ', '', '', array('data-url' => toUrl(array('cat_Products', 'single'), 'local'), 'class' => 'operationBtn enlargeProductBtn', 'ef_icon' => 'img/32/search.png'));
         $block->append($enlargeBtn, 'INPUT_FLD');
         $block->append(ht::createElement('input', $params), 'INPUT_FLD');
         $block->append($this->renderKeyboard('tools'), 'KEYBOARDS');
