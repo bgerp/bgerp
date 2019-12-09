195be4332019d564be0b9a11f568e35e0a1c1f57
dminko <mitko@experta.bg>
2019-12-09 11:40:29 +0200

    Избягване от недефинирана константа

--- core/boot.inc.php
+++ core/boot.inc.php
@@ -797,7 +797,7 @@ function getTplFromFile($file)
 function setupKey($efSalt = null, $i = 0)
 {
     // Сетъп ключ, ако не е зададен
-    $salt = ($efSalt)?($efSalt):(EF_SALT);
+    $salt = ($efSalt)?($efSalt):('*9fbaknc');
     
     $key = md5($salt . '*9fbaknc');
     
