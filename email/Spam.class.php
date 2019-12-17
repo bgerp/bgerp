428afc89b8bdd48a1e0cf01ef12f210b5ff2df31
Yuseinov <yuseinov@experta.bg>
2019-12-17 17:12:38 +0200

    email_Spam - закръгляне

--- email/Spam.class.php
+++ email/Spam.class.php
@@ -42,7 +42,7 @@ class email_Spam extends email_ServiceEmails
     public function description()
     {
         $this->addFields();
-        $this->FLD('spamScore', 'double', 'caption=Спам рейтинг');
+        $this->FLD('spamScore', 'double(smartRound)', 'caption=Спам рейтинг');
     }
     
     
