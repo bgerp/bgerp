26a919eb25e40cb238ffe1ac249466b1e7fda42c
Ivelin Dimov <ivelin_pdimov@abv.bg>
2019-12-31 14:28:21 +0200

    deals_Setup: #C30677

--- deals/Setup.class.php
+++ deals/Setup.class.php
@@ -8,10 +8,27 @@ defIfNot('DEALS_BALANCE_TOLERANCE', '0.01');
 
 
 /**
+ * Напомняне за неконтиран документ със стар вальор първо
+ */
+defIfNot('DEALS_OVERDUE_PENDING_DAYS_1', '1');
+
+
+/**
+ * Напомняне за неконтиран документ със стар вальор второ
+ */
+defIfNot('DEALS_OVERDUE_PENDING_DAYS_2', '5');
+
+
+/**
+ * Напомняне за неконтиран документ със стар вальор трето
+ */
+defIfNot('DEALS_OVERDUE_PENDING_DAYS_3', '14');
+
+
+/**
  * Кой потребител да излиза като съставител на документите
  */
 defIfNot('DEALS_ISSUER', 'activatedBy');
-
 
 /**
  * class deals_Setup
@@ -66,7 +83,9 @@ class deals_Setup extends core_ProtoSetup
     public $configDescription = array(
         'DEALS_BALANCE_TOLERANCE' => array('percent(min=0)', 'caption=Процент за допустимо разминаване в салдото според сумата->Процент'),
         'DEALS_ISSUER' => array('enum(createdBy=Създателят,activatedBy=Активиралият)', 'caption=Съставител на бизнес документи->Избор'),
-    
+        'DEALS_OVERDUE_PENDING_DAYS_1' => array('int(Min=0)', 'caption=Напомняне за неконтиран документ със стар вальор->Първо след,unit=дни'),
+        'DEALS_OVERDUE_PENDING_DAYS_2' => array('int(Min=0)', 'caption=Напомняне за неконтиран документ със стар вальор->Второ след,unit=дни'),
+        'DEALS_OVERDUE_PENDING_DAYS_3' => array('int(Min=0)', 'caption=Напомняне за неконтиран документ със стар вальор->Трето след,unit=дни'),
     );
     
     
@@ -116,21 +135,26 @@ class deals_Setup extends core_ProtoSetup
             while($dRec = $dQuery->fetch()){
                 
                 // На коя дата се очаква да има направено плащане, ако не е посочена е 1 месец от създаването
-                $expectedDate = empty($dRec->{$Class->termDateFld}) ? dt::addMonths(1, $dRec->modifiedOn, false) : $dRec->{$Class->termDateFld};
+                $expectedDate = empty($dRec->{$Class->termDateFld}) ? dt::addMonths(1, $dRec->modifiedOn, false) : dt::verbal2mysql($dRec->{$Class->termDateFld}, false);
                 
-                // Ако датата е просрочена да бие нотификация, на създателя на документа и на отговорника на папката
-                if($expectedDate < $today){
-                    
-                    // Подготовка на текста на нотификацията
-                    $amountVerbal = core_Type::getByName('double(smartRound)')->toVerbal($dRec->amount);
-                    $amountVerbal = currency_Currencies::decorate($amountVerbal, $dRec->currencyId);
-                    $amountVerbal = str_replace('&nbsp;', ' ', $amountVerbal);
-                    $contragentName = cls::get($dRec->contragentClassId)->getVerbal($dRec->contragentId, 'name');
-                    $msg = "Просрочен вальор на|* #{$Class->getHandle($dRec->id)} |от|* {$contragentName} |за|* {$amountVerbal}";
-                    
-                    bgerp_Notifications::add($msg, array($Class, 'single', $dRec->id), $dRec->createdBy);
-                    if($dRec->createdBy != $dRec->inCharge){
-                        bgerp_Notifications::add($msg, array($Class, 'single', $dRec->id), $dRec->inCharge);
+                // Изпращане на първо/второ или трето напомняне
+                foreach (array('1' => 'първо', '2' => 'второ', '3' => 'трето') as $i => $iVerbal){
+                    $days = static::get("OVERDUE_PENDING_DAYS_{$i}");
+                    $overdueDate = dt::addDays($days, $expectedDate, false);
+                    if($overdueDate == $today){
+                        
+                        // Подготовка на текста на нотификацията
+                        $amountVerbal = core_Type::getByName('double(smartRound)')->toVerbal($dRec->amount);
+                        $amountVerbal = currency_Currencies::decorate($amountVerbal, $dRec->currencyId);
+                        $amountVerbal = str_replace('&nbsp;', ' ', $amountVerbal);
+                        $contragentName = cls::get($dRec->contragentClassId)->getVerbal($dRec->contragentId, 'name');
+                        $msg = "Просрочен вальор на|* #{$Class->getHandle($dRec->id)} |от|* {$contragentName} |за|* {$amountVerbal} (|{$iVerbal} напомняне|*)";
+                       
+                        bgerp_Notifications::add($msg, array($Class, 'single', $dRec->id), $dRec->createdBy);
+                        if($dRec->createdBy != $dRec->inCharge){
+                            bgerp_Notifications::add($msg, array($Class, 'single', $dRec->id), $dRec->inCharge);
+                        }
+                        break;
                     }
                 }
             }
