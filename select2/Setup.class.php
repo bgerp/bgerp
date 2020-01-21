566b1d237fdc956bebefd3f3a26b05d1918d709e
HeBuTy <nevena@experta.bg>
2020-01-21 16:31:35 +0200

    Махната версия на select2 - не работи с показването на дървовидната структура

--- select2/Setup.class.php
+++ select2/Setup.class.php
@@ -72,7 +72,7 @@ class select2_Setup extends core_ProtoSetup
         // Минималния брой елементи, за които няма да сработи SELECT2
         'SELECT2_WIDE_MIN_SEARCH_ITEMS_CNT' => array('int', 'caption=Минимален брой опции за да сработи търсенето в Select2->За широк екран, suggestions=5|10|20|50|100'),
         'SELECT2_NARROW_MIN_SEARCH_ITEMS_CNT' => array('int', 'caption=Минимален брой опции за да сработи търсенето в Select2->За тесен екран, suggestions=5|10|20|50|100'),
-        'SELECT2_VERSION' => array('enum(4.0rc2, 4.0, 4.0.1, 4.0.7)', 'caption=Версия на Select2->Версия'),
+        'SELECT2_VERSION' => array('enum(4.0rc2, 4.0.1, 4.0.7)', 'caption=Версия на Select2->Версия'),
         'SELECT2_TOTREE_VERSION' => array('enum(19.03.26)', 'caption=Версия на Select2ToTree->Версия'),
     );
     
