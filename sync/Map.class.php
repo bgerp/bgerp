ed8bc7550ad0c40ee3568540be37a74ce7d7edaf
Yuseinov <yuseinov@experta.bg>
2020-01-22 17:10:54 +0200

    sync_Map fix

--- sync/Map.class.php
+++ sync/Map.class.php
@@ -291,11 +291,7 @@ class sync_Map extends core_Manager
                     $exRec->{$name} = keylist::merge($exRec->{$name}, $rec->{$name});
                 }
                 
-                if (is_object($rec->{$name})) {
-                    bp($rec, $name, $class, $id, $res, $controller);
-                }
-                
-                if (empty($exRec->{$name}) && (is_array($rec->{$name}) || strlen($rec->{$name}))) {
+                if (empty($exRec->{$name}) && (is_array($rec->{$name}) || strlen($rec->{$name}) || is_object($rec->{$name}))) {
                     $exRec->{$name} = $rec->{$name};
                 }
             }
