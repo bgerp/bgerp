106ed571431f277cf259bec6eb6b5895b0fe1b35
Yuseinov <yuseinov@experta.bg>
2020-01-22 16:33:49 +0200

    sync - възможност за синхронизиране на е-магазин

--- sync/Companies.class.php
+++ sync/Companies.class.php
@@ -15,86 +15,41 @@
  * @since     v 0.1
  * @title     Синхронизиране на фирми между bgERP системи
  */
-class sync_Companies extends core_Manager
+class sync_Companies extends sync_Helper
 {
-    
     /**
      * Какво друго да експортираме?
      */
     public $exportAlso = array(
-        'crm_Companies' => array(
-            array('crm_Locations' => 'contragentCls|contragentId'),
-            array('bank_Accounts' => 'contragentCls|contragentId'),
-            array('cond_ConditionsToCustomers' => 'cClass|cId'),
-            array('price_ListToCustomers' => 'cClass|cId'),
-            array('crm_ext_Cards' => 'contragentClassId|contragentId'),
-        ),
-
-        'crm_Locations' => array(
-            array('sales_Routes' => 'locationId'),
-        ),
-        'price_Lists' => array(
-            array('price_ListRules' => 'listId'),
-        ),
+            'crm_Companies' => array(
+                    array('crm_Locations' => 'contragentCls|contragentId'),
+                    array('bank_Accounts' => 'contragentCls|contragentId'),
+                    array('cond_ConditionsToCustomers' => 'cClass|cId'),
+                    array('price_ListToCustomers' => 'cClass|cId'),
+                    array('crm_ext_Cards' => 'contragentClassId|contragentId'),
+            ),
+            
+            'crm_Locations' => array(
+                    array('sales_Routes' => 'locationId'),
+            ),
+            'price_Lists' => array(
+                    array('price_ListRules' => 'listId'),
+            ),
     );
-
-
-    /**
-     * Глобални уникални ключове
-     */
-    public $globalUniqKeys = array(
-        'drdata_Countries' => 'letterCode2',
-        'core_Roles' => 'role',
-        'core_Classes' => 'name',
-        'currency_Currencies' => 'code',
-    );
-
-
-    /**
-     * Полета от моделите, които не трябва да се експортират
-     */
-    public $fixedExport = array(
-        '*::createdOn' => null,
-        '*::createdBy' => null,
-        '*::modifiedOn' => null,
-        '*::modifiedBy' => null,
-        '*::searchKeywords' => null,
-        '*::folderId' => null,
-        '*::containerId' => null,
-        '*::threadId' => null,
-        '*::ps5Enc' => null,
-        '*::exSysId' => null,
-        '*::lastLoginTime' => null,
-        '*::lastLoginTime' => null,
-        '*::lastLoginIp' => null,
-        '*::lastActivityTime' => null,
-        '*::lastUsedOn' => null,
-        '*::id' => null,
-    );
-
-
  
     /**
      *  Връща Json-a на филтрираните обекти
      */
     public function act_Export()
     {
-        if (haveRole('user')) {
-            requireRole('admin');
-        } else {
-            expect($remoteAddr = $_SERVER['REMOTE_ADDR']);
-            
-            if (defined('SYNC_EXPORT_ADDR')) {
-                expect(SYNC_EXPORT_ADDR == $remoteAddr);
-            } else {
-                expect(core_Url::isPrivate($remoteAddr));
-            }
-        }
+        self::requireRight();
         
         core_App::setTimeLimit(1000);
         
         $groupId = sync_Setup::get('COMPANY_GROUP');
-   
+        
+        expect($groupId);
+        
         $res = array();
         
         core_Users::forceSystemUser();
@@ -106,20 +61,7 @@ class sync_Companies extends core_Manager
         
         core_Users::cancelSystemUser();
         
-        $res = array_reverse($res, true);
-        
-        if ($res['core_Users'] && (countR($res) > 1)) {
-            $res2 = array();
-            $res2['core_Users'] = $res['core_Users'];
-            unset($res['core_Users']);
-            $res2 += $res;
-            $res = $res2;
-        }
-        
-        $res = gzcompress(serialize($res));
-        
-        echo $res;
-        die;
+        return self::outputRes($res);
     }
 
 
@@ -128,29 +70,22 @@ class sync_Companies extends core_Manager
      */
     public function act_Import()
     {
-        requireRole('admin');
-        
-        $url = sync_Setup::get('EXPORT_URL');
-        
-        ini_set('default_socket_timeout', 600);
+        self::requireRight('import');
         
         core_App::setTimeLimit(1000);
         
-        $res = file_get_contents($url);
-        $res = unserialize(gzuncompress($res));
+        $resArr = self::getDataFromUrl(get_called_class());
         
         core_Users::forceSystemUser();
         
         Mode::set('preventNotifications', true);
         
-        foreach ($res as $class => $objArr) {
+        foreach ($resArr as $class => $objArr) {
             foreach ($objArr as $id => $rec) {
-                sync_Map::importRec($class, $id, $res, $this);
+                sync_Map::importRec($class, $id, $resArr, $this);
             }
         }
         
         crm_Groups::updateGroupsCnt('crm_Persons', 'personsCnt');
-        
-        core_Users::cancelSystemUser();
     }
 }
