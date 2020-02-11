7051962d2ddeaa6a841aed5968161a6004ec2471
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-02-11 11:18:47 +0200

    входящи фактури: възможност за експпорт на csv

--- sales/Invoices.class.php
+++ sales/Invoices.class.php
@@ -175,14 +175,6 @@ class sales_Invoices extends deals_InvoiceMaster
     
     
     /**
-     * Кои полета да могат да се експортират в CSV формат
-     *
-     * @see bgerp_plg_CsvExport
-     */
-    public $exportableCsvFields = 'date,number,contragentName,contragentVatNo,uicNo,dealValue=Сума фактура,valueNoVat=Данъчна основа,vatAmount=Сума ДДС,currencyId,accountId,state';
-    
-    
-    /**
      * Кой може да променя активирани записи
      *
      * @see change_Plugin
@@ -641,41 +633,6 @@ class sales_Invoices extends deals_InvoiceMaster
         }
         
         $copyTpl->replace($inv_status, 'INV_STATUS');
-    }
-    
-    
-    /**
-     * Преди експортиране като CSV
-     */
-    public static function on_BeforeExportCsv($mvc, &$recs)
-    {
-        if (!$recs) {
-            
-            return ;
-        }
-        
-        $fields = $mvc->selectFields();
-        $fields['-list'] = true;
-        foreach ($recs as &$rec) {
-            $rec->number = str_pad($rec->number, '10', '0', STR_PAD_LEFT);
-            
-            $row = new stdClass();
-            parent::getVerbalInvoice($mvc, $rec, $row, $fields);
-            $rec->dealValue = strip_tags(str_replace('&nbsp;', '', $row->dealValue));
-            $rec->valueNoVat = strip_tags(str_replace('&nbsp;', '', $row->valueNoVat));
-            $rec->vatAmount = strip_tags(str_replace('&nbsp;', '', $row->vatAmount));
-        }
-    }
-    
-    
-    /**
-     * След подготвяне на заявката за експорт
-     */
-    public static function on_AfterPrepareExportQuery($mvc, &$query)
-    {
-        // Искаме освен фактурите показващи се в лист изгледа да излизат и тези,
-        // които са били активни, но сега са оттеглени
-        $query->where("#state != 'draft' OR (#state = 'rejected' AND #brState = 'active')");
     }
     
     
