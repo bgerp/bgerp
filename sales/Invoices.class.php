5c66e375e8f9f822fa877439b33b31c2f5dccfb5
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-02-12 11:21:13 +0200

    Изходящи ф-ри и паднатия номер да влиза в ключовите думи

--- sales/Invoices.class.php
+++ sales/Invoices.class.php
@@ -448,7 +448,9 @@ class sales_Invoices extends deals_InvoiceMaster
         if ($rec->state == 'active') {
             if (empty($rec->number)) {
                 $rec->number = self::getNextNumber($rec);
-                $rec->searchKeywords .= ' ' . plg_Search::normalizeText($rec->number);
+                $numberPadded = str_pad($rec->number, '10', '0', STR_PAD_LEFT);
+                
+                $rec->searchKeywords .= ' ' . plg_Search::normalizeText($rec->number) . ' ' . plg_Search::normalizeText($numberPadded);
             }
         }
     }
