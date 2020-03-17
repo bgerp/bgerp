c52d148bc9024dd832ab57c77c499b542160c32f
gabi <gabi@gabi-developer>
2020-03-17 10:51:44 +0200

    bugFix; заявка към db

--- acc/reports/MovementArtRep.class.php
+++ acc/reports/MovementArtRep.class.php
@@ -172,7 +172,7 @@ class acc_reports_MovementArtRep extends frame2_driver_TableData
         //327 wry6a
         
         //Производство
-        $jRecs2 = $jQuery->where("#docType = 325 OR #docType = 323");
+        $jRecs2 = $jQuery->where("#docType = 325 AND #docType = 323");
         $jRecs2 = $jQuery->fetchAll();
 
         $recs = array();
