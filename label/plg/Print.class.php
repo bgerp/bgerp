d1ebb9eb99f27403456cfd234e77a0ad6646b806
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-03-31 18:54:05 +0300

    печат на бързи етикети: бъгфикс

--- label/plg/Print.class.php
+++ label/plg/Print.class.php
@@ -81,7 +81,7 @@ class label_plg_Print extends core_Plugin
             $source = $mvc->getLabelSource($rec);
             $interface = cls::getInterface('label_SequenceIntf', $source['class']);
             expect($peripheralTemplateId = $interface->getDefaultFastLabel($source['id'], $deviceRec));
-            $labelContent = $interface->getDefaultLabelWithData($rec, $peripheralTemplateId);
+            $labelContent = $interface->getDefaultLabelWithData($rec->id, $peripheralTemplateId);
             
             Request::setProtected('hash');
             $hash = str::addHash('fastlabel', 4);
