2235fc0693546491cab904137cafbdb784877545
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-04-03 14:52:07 +0300

    Сделки:вашия реф да се клонира

--- deals/DealMaster.class.php
+++ deals/DealMaster.class.php
@@ -59,7 +59,7 @@ abstract class deals_DealMaster extends deals_DealBase
      *
      * @see plg_Clone
      */
-    public $fieldsNotToClone = 'reff,valior,contoActions,amountDelivered,amountBl,amountPaid,amountInvoiced,sharedViews,closedDocuments,paymentState,deliveryTime,currencyRate,contragentClassId,contragentId,state,deliveryTermTime,closedOn';
+    public $fieldsNotToClone = 'valior,contoActions,amountDelivered,amountBl,amountPaid,amountInvoiced,sharedViews,closedDocuments,paymentState,deliveryTime,currencyRate,contragentClassId,contragentId,state,deliveryTermTime,closedOn';
     
     
     /**
