3521e95c87cf1f98e6cee89b45ce39504d45ae8a
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-04-21 15:06:55 +0300

    работа с локации в онлайн магазина: подобрения

--- tcost/FeeZones.class.php
+++ tcost/FeeZones.class.php
@@ -374,7 +374,7 @@ class tcost_FeeZones extends core_Master
         $Document = cls::get($document);
         if($Document instanceof eshop_Carts){
             foreach (array('deliveryCountry', 'deliveryPCode', 'deliveryPlace', 'deliveryAddress') as $fld){
-                if(!$form->getFieldTypeParam($fld, 'isReadOnly')){
+                if(!$form->getFieldTypeParam($fld, 'isReadOnly') && $form->getFieldParam($fld, 'input') != 'hidden'){
                     $form->setField($fld, 'mandatory');
                 }
             }
