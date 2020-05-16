d401cc4394d7fe43dc501169ab86232eeb35efec
Yuseinov <yuseinov@experta.bg>
2020-05-16 09:57:15 +0300

    fix

--- plg/Search.class.php
+++ plg/Search.class.php
@@ -721,7 +721,7 @@ class plg_Search extends core_Plugin
             }
             
             if ($isFirst) {
-                $clsInst->logDebug("Регенериране на ключови думи за {$rec->id}");
+                $clsInst->logDebug("Регенериране на ключови думи от {$rec->id}");
                 $isFirst = false;
             }
             
