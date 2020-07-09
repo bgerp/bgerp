8aa99fec4b77e20d881c2e831fb5a9175458b4e1
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-07-09 11:49:53 +0300

    пос:бъгфикс с отстъпките от бутона

--- pos/ReceiptDetails.class.php
+++ pos/ReceiptDetails.class.php
@@ -280,9 +280,8 @@ class pos_ReceiptDetails extends core_Detail
                    
                    if(isset($discount)){
                        expect($discount >= -1 && $discount <= 1, 'Отстъпката трябва да е между -100% и 100%|*!');
-                      
                        if($discount != 0){
-                           if(strpos($string, '%') == 0){
+                           if(strpos($string, '%') === 0){
                                $discount = -1 * $discount;
                             }
                        } else {
