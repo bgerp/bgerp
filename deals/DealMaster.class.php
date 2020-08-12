351b03c69d41ead6c33bf2ea9bd9fb13345e5e7b
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-08-12 14:50:39 +0300

    логистичен адрес:при лице бъгфикс

--- deals/DealMaster.class.php
+++ deals/DealMaster.class.php
@@ -2179,7 +2179,7 @@ abstract class deals_DealMaster extends deals_DealBase
         } else {
             $res["{$contrPart}PCode"] = !empty($contragentData->pCode) ? $contragentData->pCode : null;
             $res["{$contrPart}Place"] = !empty($contragentData->place) ? $contragentData->place : null;
-            $res["{$contrPart}Address"] = !empty($contragentData->address) ? $contragentData->address : null;
+            $res["{$contrPart}Address"] = !empty($contragentData->pAddress) ? $contragentData->pAddress : (($contragentData->address) ? $contragentData->address : null);
             $res["{$contrPart}Person"] = !empty($contragentData->person) ? $contragentData->person : null;
         }
         
