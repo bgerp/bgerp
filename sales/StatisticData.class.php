2eac81fdde5f4fd5c607ced18fbb9ae470fc8341
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-08-14 14:43:38 +0300

    модел за статистически данни промени

--- sales/StatisticData.class.php
+++ sales/StatisticData.class.php
@@ -57,7 +57,7 @@ class sales_StatisticData extends core_Manager
     /**
      * Полета, които се виждат
      */
-    public $listFields = 'productId,storeId,storeId,count,quantity,amount,type';
+    public $listFields = 'productId,key,count,quantity,amount';
     
     
     /**
@@ -65,15 +65,14 @@ class sales_StatisticData extends core_Manager
      */
     public function description()
     {
-        $this->FLD('type', 'enum(,sales=Продажби,pos=ПОС,eshop=Е-шоп)', 'mandatory,caption=Източник');
         $this->FLD('productId', 'key2(mvc=cat_Products,select=name,selectSourceArr=cat_Products::getProductOptions,allowEmpty,maxSuggestions=100,forceAjax)', 'caption=Артикул,silent,class=ajaxSelect');
-        $this->FLD('storeId', 'key(mvc=store_Stores,select=name,allowEmpty)', 'mandatory,caption=Склад,silent');
+        $this->FLD('key', 'varchar', 'mandatory,caption=Ключ,silent');
         $this->FLD('count', 'int', 'mandatory,caption=Брой');
         $this->FLD('quantity', 'double', 'mandatory,caption=Количество');
         $this->FLD('amount', 'double', 'mandatory,caption=Сума (без ДДС)');
         
-        $this->setDbIndex('type');
-        $this->setDbUnique('type,productId,storeId');
+        $this->setDbIndex('key');
+        $this->setDbUnique('key,productId');
     }
     
     
@@ -82,10 +81,10 @@ class sales_StatisticData extends core_Manager
      */
     protected static function on_AfterPrepareListFilter($mvc, &$data)
     {
-        $data->listFilter->showFields = 'productId,storeId,type';
+        $data->listFilter->showFields = 'productId';
         $data->listFilter->view = 'horizontal';
         $data->listFilter->toolbar->addSbBtn('Филтрирай', array($mvc, 'list'), 'id=filter', 'ef_icon = img/16/funnel.png');
-        $data->listFilter->input('productId,storeId,type');
+        $data->listFilter->input('productId');
         
         if($rec = $data->listFilter->rec){
             if(!empty($rec->productId)){
@@ -127,14 +126,14 @@ class sales_StatisticData extends core_Manager
         $newRecs = array();
         $saleRecs = self::getSaleStatistic();
         foreach ($saleRecs as $saleRec){
-            $newRecs["sales|{$saleRec->productId}|{$saleRec->storeId}"] = (object)array('type' => 'sales', 'productId' => $saleRec->productId, 'storeId' => $saleRec->storeId, 'count' => $saleRec->count, 'quantity' => $saleRec->sumQuantity, 'amount' => $saleRec->sumAmount);
+            $newRecs["sales|{$saleRec->productId}|{$saleRec->storeId}"] = (object)array("key" => "sales|{$saleRec->storeId}", 'productId' => $saleRec->productId, 'count' => $saleRec->count, 'quantity' => $saleRec->sumQuantity, 'amount' => $saleRec->sumAmount);
         }
         
         if(core_Packs::isInstalled('pos')){
             $posRecs = array();
             $posDataRecs = pos_SellableProductsCache::getPosStatisticData();
             foreach($posDataRecs as $posRec){
-                $posRecs["pos|{$posRec->productId}|{$posRec->storeId}"] = (object)array('type' => 'pos', 'productId' => $posRec->productId, 'storeId' => $posRec->storeId, 'count' => $posRec->count, 'quantity' => $posRec->sumQuantity, 'amount' => $posRec->sumAmount);
+                $posRecs["pos|{$posRec->productId}|{$posRec->storeId}"] = (object)array("key" => "pos|{$posRec->storeId}", 'productId' => $posRec->productId, 'count' => $posRec->count, 'quantity' => $posRec->sumQuantity, 'amount' => $posRec->sumAmount);
             }
             
             $newRecs += $posRecs;
@@ -144,14 +143,17 @@ class sales_StatisticData extends core_Manager
             $saleEshopRecs = self::getSaleStatistic(true);
             $eshopRecs = array();
             foreach ($saleEshopRecs as $saleEshopRec){
-              $eshopRecs["eshop|{$saleEshopRec->productId}|{$saleEshopRec->storeId}"] = (object)array('type' => 'eshop', 'productId' => $saleEshopRec->productId, 'storeId' => $saleEshopRec->storeId, 'count' => $saleEshopRec->count, 'quantity' => $saleEshopRec->sumQuantity, 'amount' => $saleEshopRec->sumAmount);
+                $eshopRecs["eshop|{$saleEshopRec->productId}|{$saleEshopRec->storeId}"] = (object)array("key" => "eshop|{$saleEshopRec->storeId}", 'productId' => $saleEshopRec->productId, 'count' => $saleEshopRec->count, 'quantity' => $saleEshopRec->sumQuantity, 'amount' => $saleEshopRec->sumAmount);
             }
             $newRecs += $eshopRecs;
         }
         
-        $res = arr::syncArrays($newRecs, $exRecs, 'type,productId,storeId', 'storeId,count,quantity,amount');
+        
+        
+        //bp($newRecs);
+        $res = arr::syncArrays($newRecs, $exRecs, 'key,productId', 'count,quantity,amount');
         $this->saveArray($res['insert']);
-        $this->saveArray($res['update'], 'id,storeId,count,quantity,amount');
+        $this->saveArray($res['update'], 'id,count,quantity,amount');
         
         if(countR($res['delete'])){
             foreach ($res['delete'] as $deleteId){
@@ -183,6 +185,7 @@ class sales_StatisticData extends core_Manager
         $deltaQuery->where("#sellCost IS NOT NULL AND (#state = 'active' OR #state = 'closed') AND #isPublic = 'yes'");
         $deltaQuery->groupBy('productId,storeId');
         $deltaQuery->orderBy("count", 'DESC');
+        $deltaQuery->limit(200);
         $deltaQuery->show('productId,storeId,sumQuantity,sumAmount,count');
         
         if($onlyOnlineSales){
