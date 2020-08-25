74661a33abe45650596ac59007f9d8493420fb26
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-08-25 11:07:41 +0300

    декларации:разместен плъгин

--- dec/Declarations.class.php
+++ dec/Declarations.class.php
@@ -48,8 +48,8 @@ class dec_Declarations extends core_Master
     /**
      * Плъгини за зареждане
      */
-    public $loadList = 'sales_Wrapper, bgerp_plg_Blank, recently_Plugin, doc_ActivatePlg, plg_Printing, cond_plg_DefaultValues, 
-    				 plg_RowTools2, doc_DocumentIntf, doc_DocumentPlg, doc_EmailCreatePlg ,doc_plg_TplManager';
+    public $loadList = 'sales_Wrapper, doc_plg_TplManager, bgerp_plg_Blank, recently_Plugin, doc_ActivatePlg, plg_Printing, cond_plg_DefaultValues, 
+    				 plg_RowTools2, doc_DocumentIntf, doc_DocumentPlg, doc_EmailCreatePlg';
     
     
     /**
