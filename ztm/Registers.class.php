0ce0507be53ddfb3e6a3a2dac6d07fc79eaa86ac
Yuseinov <yuseinov@experta.bg>
2020-09-01 17:34:37 +0300

    ztm #C35802 - подобрения при импорт на `csv`

--- ztm/Registers.class.php
+++ ztm/Registers.class.php
@@ -11,7 +11,7 @@
  * @license   GPL 3
  *
  * @since     v 0.1
- 
+ *
  * @title     Дефинирани регистри в Zontromat
  */
 class ztm_Registers extends core_Master
@@ -22,7 +22,7 @@ class ztm_Registers extends core_Master
     /**
      * Кой има право да променя?
      */
-    public $canEdit = 'ztm, ceo';
+    public $canEdit = 'no_one';
     
     
     /**
@@ -36,6 +36,8 @@ class ztm_Registers extends core_Master
      */
     public $canList = 'ztm, ceo';
     public $canSingle = 'ztm, ceo';
+    
+    
     /**
      * Кой има право да го изтрие?
      */
@@ -43,26 +45,32 @@ class ztm_Registers extends core_Master
     
     
     /**
+     * Активните регистри след импортиране
+     */
+    protected $activeRegistersArr;
+    
+    
+    /**
      * Кой може да променя състоянието на документите
      *
      * @see plg_State2
      */
-    public $canChangestate = 'ztm, ceo';
+    public $canChangestate = 'no_one';
     
     
     /**
      * Плъгини за зареждане
      */
-    public $loadList = 'ztm_Wrapper, plg_Rejected, plg_Created, plg_State2, plg_RowTools2, plg_Modified, plg_Sorting';
+    public $loadList = 'ztm_Wrapper, plg_Created, plg_State2, plg_RowTools2, plg_Modified, plg_Sorting';
     
     
     /**
      *
      * @var string
      */
-     public $listFields = 'id, name, type, range, plugin, priority, default, description';
+    public $listFields = 'id, name, type, range, plugin, priority, default, description';
     
-     
+    
     /**
      * Описание на модела (таблицата)
      */
@@ -97,7 +105,7 @@ class ztm_Registers extends core_Master
             6 => 'description',
         );
         
-        $cntObj = csv_Lib::importOnceFromZero($this, $file, $fields);
+        $cntObj = csv_Lib::importOnce($this, $file, $fields);
         $res = $cntObj->html;
         
         return $res;
@@ -106,16 +114,16 @@ class ztm_Registers extends core_Master
     
     /**
      * Какъв наш тип отговаря на техния
-     * 
-     * @param int $registerId
-     * @param boolean $forForm
-     * 
+     *
+     * @param int  $registerId
+     * @param bool $forForm
+     *
      * @return core_Type
      */
     public static function getOurType($registerId)
     {
         $type = ztm_Registers::fetchField($registerId, 'type');
-        switch($type){
+        switch ($type) {
             case 'int':
                 $ourType = 'Int';
                 break;
@@ -139,22 +147,22 @@ class ztm_Registers extends core_Master
     
     /**
      * Добавя функционално поле за въвеждане на допустима стойност
-     * 
-     * @param core_Form $form
+     *
+     * @param core_Form   $form
      * @param string|null $registerFld
-     * @param string|null $valueFld 
-     * 
+     * @param string|null $valueFld
+     *
      * @return void
      */
     public static function extendAddForm($form, $registerFld = 'registerId', $valueFld = 'value')
     {
         $rec = &$form->rec;
         
-        if(isset($rec->{$registerFld})){
+        if (isset($rec->{$registerFld})) {
             $form->FLD('extValue', ztm_Registers::getOurType($rec->{$registerFld}), 'caption=Стойност,mandatory');
             $form->rec->_type = ztm_Registers::fetchField($rec->{$registerFld}, 'type');
             
-            if(!empty($rec->{$valueFld})){
+            if (!empty($rec->{$valueFld})) {
                 $value = ztm_LongValues::getValueByHash($rec->{$valueFld});
                 $form->setDefault('extValue', $value);
             }
@@ -165,32 +173,31 @@ class ztm_Registers extends core_Master
     /**
      * Обработва стойността, ако е от нескаларен тип записва я в помощен модел
      * подменяйки я с нейния хеш
-     * 
-     * @param int $registerId
+     *
+     * @param int   $registerId
      * @param mixed $extValue
-     * 
+     *
      * @return mixed
      */
     public static function recordValue($registerId, $extValue)
     {
         $type = ztm_Registers::fetchField($registerId, 'type');
-      
-        if($type == 'json'){
+        
+        if ($type == 'json') {
             
             // Ако типа е json, но стойността не е подсигуряваме се да е json
             $extValue = (str::isJson($extValue)) ? $extValue : json_encode($extValue);
-        } elseif(in_array($type, array('int', 'float'))){
+        } elseif (in_array($type, array('int', 'float'))) {
             
             // Ако е число, проверка дали е подадена валидна числова стойност
             $Double = core_Type::getByName('double');
-            if($Double->fromVerbal($extValue) === false){
-                
+            if ($Double->fromVerbal($extValue) === false) {
                 throw new core_exception_Expect("Въведената стойност '{$extValue}' трябва да е число|*!", 'Несъответствие');
             }
         }
         
         // Не бива до тук да стигат нескаларни стойностти
-        if(!is_scalar($extValue)) {
+        if (!is_scalar($extValue)) {
             wp($extValue, $registerId, $type);
             $extValue = serialize($extValue);
             
@@ -198,13 +205,13 @@ class ztm_Registers extends core_Master
         }
         
         // Записва стойността в помощния модел при нужда
-        if($type == 'json' || ($type == 'str' && strlen($extValue) > 32)){
+        if ($type == 'json' || ($type == 'str' && strlen($extValue) > 32)) {
             $hash = md5(serialize($extValue));
             $value = $hash;
             
             $existingValue = ztm_LongValues::fetchField("#hash = '{$hash}'", 'value');
-            if(!isset($existingValue)){
-                $longRec = (object)array('hash' => $hash, 'value' => $extValue);
+            if (!isset($existingValue)) {
+                $longRec = (object) array('hash' => $hash, 'value' => $extValue);
                 
                 ztm_LongValues::save($longRec);
             }
@@ -217,14 +224,51 @@ class ztm_Registers extends core_Master
     
     
     /**
-     * След изтриване на запис
+     * Преди импортиране на запис
+     * 
+     * @param ztm_Registers $mvc
+     * @param stdClass $rec
      */
-    protected static function on_AfterDelete($mvc, &$numDelRows, $query, $cond)
+    public static function on_BeforeImportRec($mvc, &$rec)
     {
-        // Ако изтриваме етап, изтриваме всичките редове от този етап
-        foreach ($query->getDeletedRecs() as $rec) {
-            ztm_RegisterValues::delete("#registerId = {$rec->id}");
-            ztm_ProfileDetails::delete("#registerId = {$rec->id}");
+        $rec->state = 'active';
+    }
+    
+    
+    /**
+     * След импортиране на запис
+     * 
+     * @param ztm_Registers $mvc
+     * @param stdClass $rec
+     */
+    public function on_AfterImportRec($mvc, $rec)
+    {
+        setIfNot($mvc->activeRegistersArr, array());
+        $mvc->activeRegistersArr[$rec->id] = $rec->id;
+    }
+    
+    
+    /**
+     * След приключване на процесите
+     * 
+     * @param ztm_Registers $mvc
+     */
+    public static function on_Shutdown($mvc)
+    {
+        parent::on_Shutdown($mvc);
+        
+        // Деактивираме всички регистри, които не се добавят при импорт на `csv`
+        if (is_array($mvc->activeRegistersArr)) {
+            $query = $mvc->getQuery();
+            $query->notIn('id', $mvc->activeRegistersArr);
+            $query->where("#state = 'active'");
+            
+            $query->show('state');
+            while ($rec = $query->fetch()) {
+                $rec->state = 'closed';
+                
+                $mvc->save($rec, 'state');
+            }
         }
     }
 }
