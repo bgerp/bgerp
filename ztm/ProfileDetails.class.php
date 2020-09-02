bb2b54d672bf1b44c4ec688b02c7a588c87d84b3
Yuseinov <yuseinov@experta.bg>
2020-09-02 18:37:16 +0300

    ztm #C35802 - да не може да се добавят регистри от устройството в профила

--- ztm/ProfileDetails.class.php
+++ ztm/ProfileDetails.class.php
@@ -92,7 +92,7 @@ class ztm_ProfileDetails extends core_Detail
     protected function description()
     {
         $this->FLD('profileId','key(mvc=ztm_Profiles, select=name)','caption=Профил,mandatory,smartCenter');
-        $this->FLD('registerId','key(mvc=ztm_Registers, select=name, allowEmpty)','caption=Регистър,removeAndRefreshForm=value|extValue,silent,mandatory');
+        $this->FLD('registerId','key(mvc=ztm_Registers, select=name, allowEmpty, where=#priority !\\= \\\'device\\\')','caption=Регистър,removeAndRefreshForm=value|extValue,silent,mandatory');
         $this->FLD('value', 'varchar(32)', 'caption=Стойност,input=none');
         
         $this->setDbUnique('profileId, registerId');
@@ -252,7 +252,7 @@ class ztm_ProfileDetails extends core_Detail
         if ($rec && ($action == 'edit')) {
             
             $rRec = ztm_Registers::fetch($rec->registerId);
-            if ($rRec->state != 'active') {
+            if (($rRec->state != 'active') || ($rRec->priority == 'device')) {
                 $requiredRoles = 'no_one';
             }
         }
