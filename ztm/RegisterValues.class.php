efca0ea98c952b00a5c94636d10066006450864b
Yuseinov <yuseinov@experta.bg>
2020-09-04 11:14:24 +0300

    ztm - рефрешване на филтъра

--- ztm/RegisterValues.class.php
+++ ztm/RegisterValues.class.php
@@ -65,7 +65,7 @@ class ztm_RegisterValues extends core_Manager
     public function description()
     {
         $this->FLD('deviceId', 'key(mvc=ztm_Devices, select=name)','caption=Устройство,mandatory');
-        $this->FLD('registerId', 'key(mvc=ztm_Registers, select=name,allowEmpty)','caption=Регистър,mandatory,removeAndRefreshForm=value|extValue,silent');
+        $this->FLD('registerId', 'key(mvc=ztm_Registers, select=name,allowEmpty)','caption=Регистър,mandatory,removeAndRefreshForm=value|extValue,silent, refreshForm');
         $this->FLD('value', 'varchar(32)','caption=Стойност,input=none');
         $this->FLD('updatedOn', 'datetime(format=smartTime)','caption=Обновено на,input=none');
         
