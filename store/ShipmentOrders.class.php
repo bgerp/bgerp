64d2e0c4a5f95daa0368df4f488a7707b6d54136
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-09-04 17:11:44 +0300

    бутон чмр бъгфикс

--- store/ShipmentOrders.class.php
+++ store/ShipmentOrders.class.php
@@ -612,11 +612,11 @@ class store_ShipmentOrders extends store_DocumentMaster
                 // Само ако условието на доставка позволява ЧМР да се добавя към документа
                 $firstDoc = doc_Threads::getFirstDocument($rec->threadId);
                 $deliveryTermId = $firstDoc->fetchField('deliveryTermId');
-                $cmrRow = 1;
-                if ((isset($deliveryTermId) && strpos(cond_DeliveryTerms::fetchField($deliveryTermId, 'properties'), 'cmr') !== FALSE) || trans_Setup::get('CMR_SHOW_BTN') == 'yes') {
-                    $cmrRow = 2;
-                }
                 
+                $cmrRow = 2;
+                if ((isset($deliveryTermId) && strpos(cond_DeliveryTerms::fetchField($deliveryTermId, 'properties'), 'cmr') !== FALSE) || trans_Setup::get('CMR_SHOW_BTN') == 'yes') {
+                    $cmrRow = 1;
+                }
                 $data->toolbar->addBtn('ЧМР', array('trans_Cmrs', 'add', 'originId' => $rec->containerId, 'ret_url' => true), "title=Създаване на ЧМР към експедиционното нареждане,ef_icon=img/16/lorry_add.png,row={$cmrRow}");
             }
         }
