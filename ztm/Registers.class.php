aaa2bd73108cf1d6031ca258371512e28393e24e
Yuseinov <yuseinov@experta.bg>
2020-09-10 14:37:59 +0300

    ztm - свалянето на csv да е само за dev

--- ztm/Registers.class.php
+++ ztm/Registers.class.php
@@ -93,14 +93,16 @@ class ztm_Registers extends core_Master
      */
     public function loadSetupData()
     {
-        $csv = @file_get_contents('https://raw.githubusercontent.com/bgerp/ztm/master/registers.csv');
-        
-        if (trim($csv)) {
-            if ($fPath = getFullPath('ztm/csv/Registri.csv')) {
-                if (md5($csv) != md5(@file_get_contents($fPath))) {
-                    @file_put_contents($fPath, $csv);
-                    
-                    sleep(1);
+        if (BGERP_GIT_BRANCH == 'dev') {
+            $csv = @file_get_contents('https://raw.githubusercontent.com/bgerp/ztm/master/registers.csv');
+            
+            if (trim($csv)) {
+                if ($fPath = getFullPath('ztm/csv/Registri.csv')) {
+                    if (md5($csv) != md5(@file_get_contents($fPath))) {
+                        @file_put_contents($fPath, $csv);
+                        
+                        sleep(1);
+                    }
                 }
             }
         }
