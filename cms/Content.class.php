710a63395aef560a1dd3586bfc1e418301bd1246
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-09-15 11:25:29 +0300

    eshop:показване на резултати лека промяна по показването

--- cms/Content.class.php
+++ cms/Content.class.php
@@ -800,19 +800,19 @@ class cms_Content extends core_Manager
             $domainId = cms_Domains::getPublicDomain('id');
         }
         
-        $query->where("(#domainId = {$domainId} OR #sharedDomains LIKE '%|{$domainId}|%') AND #id != {$menuId}");
-        
+        $query->where("(#domainId = {$domainId} OR #sharedDomains LIKE '%|{$domainId}|%') AND #id != {$menuId} AND #source IS NOT NULL");
+        $query->orderBy('id', 'ASC');
         $html = '';
         
         do {
-            if (!$rec->source || !cls::load($rec->source, true)) {
+            if (!cls::load($rec->source, true)) {
                 continue;
             }
             
             $cls = cls::get($rec->source);
-            
             if (cls::existsMethod($cls, 'getSearchResults')) {
                 $res = $cls->getSearchResults($rec->id, $q);
+                
                 if (countR($res)) {
                     $domainName = '';
                     if ($rec->domainId != $domainId) {
@@ -825,22 +825,25 @@ class cms_Content extends core_Manager
                     }
                     
                     $html .= "<h2><strong style='color:green'>" . type_Varchar::escape($rec->title ? $rec->title : $rec->menu) . $domainName . '</strong></h2>';
-                    $elements = "";
-
+                    $itemsInTable = $itemsInUl = '';
+                    
                     foreach ($res as $o) {
-                        if (isset($o->img)) {
-                            $prefix = "<table class='searchResult'>";
-                            $suffix = "</table>";
+                        if (isset($o->img) && $o->img instanceof thumb_Img) {
                             $img = $o->img->createImg(array('class' => 'eshop-product-image'));
-                            $elements .= "<tr><td class='searchImg'>" . ht::createLink($img, $o->url) . "</td><td class='searchName'>" . ht::createLink($o->title, $o->url, false, array('class'=>"searchName")) . '</td> </tr>';
-
+                            $itemsInTable .= "<tr><td class='searchImg'>" . ht::createLink($img, $o->url) . "</td><td class='searchName'>" . ht::createLink($o->title, $o->url, false, array('class'=>"searchName")) . '</td> </tr>';
                         } else {
-                            $prefix = "<ul>";
-                            $suffix = "</ul>";
-                            $elements .= "<li style='font-size:1.2em; margin:5px;' >" . ht::createLink($o->title, $o->url) . '</li>';
+                            $itemsInUl .= "<li style='font-size:1.2em; margin:5px;' >" . ht::createLink($o->title, $o->url) . '</li>';
                         }
                     }
-                    $html .= $prefix . $elements . $suffix;
+                    
+                    if(!empty($itemsInUl)){
+                        $html .= "<ul>{$itemsInUl}</ul>";
+                    }
+                    
+                    if(!empty($itemsInTable)){
+                        $html .= "<table class='searchResult'>{$itemsInTable}</table>";
+                    }
+                    
                     if ($rec->domainId != $domainId) {
                         Mode::pop('BGERP_CURRENT_DOMAIN');
                     }
