29ba7ab650d98c2d62322d72347e0e3df0826023
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-10-07 09:31:01 +0300

    добавен клас в количката

--- eshop/Carts.class.php
+++ eshop/Carts.class.php
@@ -1629,7 +1629,7 @@ class eshop_Carts extends core_Master
         if (eshop_Carts::haveRightFor('finalize', $rec)) {
             $finBtn = ht::createBtn('Завършване', array('eshop_Carts', 'finalize', $rec->id), 'Сигурни ли сте, че искате да направите поръчката|*!', null, 'title=Завършване на поръчката,class=order-btn eshop-btn,rel=nofollow');
         } elseif(eshop_CartDetails::fetchField("#finalPrice IS NULL")){
-            $finBtn = ht::createErrBtn('Завършване', 'Някои от артикулите, вече са спряни от продажба!', 'title=Завършване на поръчката,class=order-btn eshop-btn,rel=nofollow');
+            $finBtn = ht::createErrBtn('Завършване', 'Някои от артикулите, вече са спряни от продажба!', 'title=Завършване на поръчката,class=order-btn eshop-btn eshop-errorBtn,rel=nofollow,ef_icon=none');
         }
         
         if(!empty($finBtn) && !empty($rec->personNames) && !empty($rec->productCount)){
