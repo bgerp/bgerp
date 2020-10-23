f843b6bb40b36d2883b13ab85274004dfe76a5a2
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-10-23 14:03:05 +0300

    бъгфикс

--- price/ProductCosts.class.php
+++ price/ProductCosts.class.php
@@ -311,7 +311,7 @@ class price_ProductCosts extends core_Manager
         // Изтриване на несрещнатите себестойностти
         if (countR($res['delete'])) {
             $avgDeliveruPolicyId = price_interface_AverageCostPricePolicyImpl::getClassId();
-            $avgStorePolicyId = price_interface_AverageCostPricePolicyImpl::getClassId();
+            $avgStorePolicyId = price_interface_AverageCostStorePricePolicyImpl::getClassId();
             $skipDeleteArr = array($avgDeliveruPolicyId, $avgStorePolicyId);
             
             $query = self::getQuery();
