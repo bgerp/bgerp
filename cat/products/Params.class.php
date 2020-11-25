43055c8029f93baf743ef00cb86f57f79da6ba6f
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-11-25 10:01:14 +0200

    продуктови параметри: стойността в базата да е текст

--- cat/products/Params.class.php
+++ cat/products/Params.class.php
@@ -103,7 +103,7 @@ class cat_products_Params extends doc_Detail
         $this->FLD('classId', 'class', 'input=hidden,silent');
         $this->FLD('productId', 'int', 'input=hidden,silent');
         $this->FLD('paramId', 'key(mvc=cat_Params,select=typeExt)', 'input,caption=Параметър,mandatory,silent');
-        $this->FLD('paramValue', 'varchar(255)', 'input=none,caption=Стойност,mandatory');
+        $this->FLD('paramValue', 'text', 'input=none,caption=Стойност,mandatory');
         
         $this->setDbUnique('classId,productId,paramId');
         $this->setDbIndex('classId,productId');
