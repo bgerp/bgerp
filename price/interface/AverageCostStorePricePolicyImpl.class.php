41e27eac87904f37ff083471d822a40df3fbc9e9
Yuseinov <yuseinov@experta.bg>
2020-12-01 09:51:54 +0200

    AverageCostStorePricePolicyImpl - docFix

--- price/interface/AverageCostStorePricePolicyImpl.class.php
+++ price/interface/AverageCostStorePricePolicyImpl.class.php
@@ -45,7 +45,7 @@ class price_interface_AverageCostStorePricePolicyImpl extends price_interface_Ba
      *
      * @param array $affectedTargetedProducts
      *
-     * @return $res
+     * @return array
      *              ['classId']       - клас ид на политиката
      *              ['productId']     - ид на артикул
      *              ['quantity']      - количество
