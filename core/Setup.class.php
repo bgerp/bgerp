b87fef1292e163fba43a4dd1c94fa94d55defe5d
Yuseinov <yuseinov@experta.bg>
2020-12-03 10:45:28 +0200

    core_Setup - нова версия

--- core/Setup.class.php
+++ core/Setup.class.php
@@ -217,7 +217,7 @@ define('CORE_LAST_DB_VERSION', '18.25-Shabran');
  * Тази константа не трябва да се ползва с core_Setup::getConfig(),
  * а само с: core_setup::CURRENT_VERSION
  */
-define('CORE_CODE_VERSION', '19.51-Vezhen');
+define('CORE_CODE_VERSION', '20.49-Sinanitsa');
 
 
 /**
