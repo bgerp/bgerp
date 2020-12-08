4c41acbe340b985fda2f309ca525b3f402f58b25
Ivelin Dimov <ivelin_pdimov@abv.bg>
2020-12-08 18:58:21 +0200

    количка: рефреша да пушва домейна във външната част

--- eshop/Carts.class.php
+++ eshop/Carts.class.php
@@ -1278,7 +1278,9 @@ class eshop_Carts extends core_Master
                 $exHaveProductsWithExpectedDelivery = Request::get('haveProductsWithExpectedDelivery', 'enum(yes,no)');
                 
                 $url = array();
-                $currentRec = self::fetch($id, 'total,state,haveProductsWithExpectedDelivery');
+                $currentRec = self::fetch($id, 'total,state,haveProductsWithExpectedDelivery,domainId');
+                cms_Domains::setPublicDomain($currentRec->domainId);
+                
                 if($currentRec->state != $exState) {
                     $url = cls::get('eshop_Groups')->getUrlByMenuId(null);
                 } elseif(trim($exTotal) != trim($currentRec->total) || $exHaveProductsWithExpectedDelivery != $currentRec->haveProductsWithExpectedDelivery){
