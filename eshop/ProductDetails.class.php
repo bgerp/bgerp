d4164dcf5f5f73ad153823f034c1c57a0b510b09
HeBuTy <nevena@experta.bg>
2020-12-09 16:58:30 +0200

    Височина на бутоните купи и запитване

--- eshop/ProductDetails.class.php
+++ eshop/ProductDetails.class.php
@@ -463,7 +463,7 @@ class eshop_ProductDetails extends core_Detail
         
         // Подготовка на бутона за купуване
         if($showCartBtn){
-            $row->btn = ht::createFnBtn($settings->addToCartBtn, null, false, array('title' => 'Добавяне в|* ' . mb_strtolower(eshop_Carts::getCartDisplayName()), 'ef_icon' => 'img/16/cart_go.png', 'data-url' => $addUrl, 'data-productid' => $rec->productId, 'data-packagingid' => $rec->packagingId, 'data-eshopproductpd' => $rec->eshopProductId, 'class' => 'eshop-btn addToCard', 'rel' => 'nofollow'));
+            $row->btn = ht::createFnBtn($settings->addToCartBtn, null, false, array('title' => 'Добавяне в|* ' . mb_strtolower(eshop_Carts::getCartDisplayName()), 'ef_icon' => 'img/16/cart_go.png', 'data-url' => $addUrl, 'data-productid' => $rec->productId, 'data-packagingid' => $rec->packagingId, 'data-eshopproductpd' => $rec->eshopProductId, 'class' => 'productBtn addToCard', 'rel' => 'nofollow'));
         }
         
         if(in_array($rec->action, array('inquiry', 'both'))){
