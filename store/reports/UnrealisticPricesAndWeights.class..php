b6a072232d4f33127d3f5d2042f3e1194c887e24
angel <angel.trifonoff@gmail.com>
2020-12-16 19:57:22 +0200

    first commit

---
+++ store/reports/UnrealisticPricesAndWeights.class..php
@@ -0,0 +1,282 @@
+<?php
+
+
+/**
+ * Мениджър на отчети за стоки с нереални цени и тегла
+ *
+ *
+ * @category  bgerp
+ * @package   store
+ *
+ * @author    Angel Trifonov angel.trifonoff@gmail.com
+ * @copyright 2006 - 2020 Experta OOD
+ * @license   GPL 3
+ *
+ * @since     v 0.1
+ * @title     Склад » Стоки с нереални цени и тегла
+ */
+class store_reports_UnrealisticPricesAndWeights extends frame2_driver_TableData
+{
+    /**
+     * Кой може да избира драйвъра
+     */
+    public $canSelectDriver = 'ceo, debug';
+
+
+    /**
+     * Кои полета от листовия изглед да може да се сортират
+     *
+     * @var int
+     */
+    protected $sortableListFields;
+
+
+    /**
+     * По-кое поле да се групират листовите данни
+     */
+    protected $groupByField;
+
+
+    /**
+     * Кои полета може да се променят от потребител споделен към справката, но нямащ права за нея
+     */
+    protected $changeableFields;
+
+
+    /**
+     * Добавя полетата на драйвера към Fieldset
+     *
+     * @param core_Fieldset $fieldset
+     */
+    public function addFields(core_Fieldset &$fieldset)
+    {
+        $fieldset->FLD('date', 'date', 'caption=Продажби след ,after=title,single=none');
+
+        $fieldset->FLD('minSellingPrice', 'varchar', 'notNull,caption=Минимална продажна цена,after=date,single=none');
+        $fieldset->FLD('maxSellingPrice', 'varchar', 'notNull,caption=Максимална продажна цена,after=minSellingPrice,single=none');
+
+        $fieldset->FLD('minVolWeight', 'varchar', 'notNull,caption=Минималнo тегло на куб. дециметър,after=date,single=none');
+        $fieldset->FLD('maxVolWeight', 'varchar', 'notNull,caption=Максималнo тегло на куб. дециметър,after=minSellingPrice,single=none');
+
+    }
+
+
+    /**
+     * След рендиране на единичния изглед
+     *
+     * @param cat_ProductDriver $Driver
+     * @param embed_Manager $Embedder
+     * @param core_Form $form
+     * @param stdClass $data
+     */
+    protected static function on_AfterInputEditForm(frame2_driver_Proto $Driver, embed_Manager $Embedder, &$form)
+    {
+        if ($form->isSubmitted()) {
+        }
+    }
+
+
+    /**
+     * Преди показване на форма за добавяне/промяна.
+     *
+     * @param frame2_driver_Proto $Driver
+     * @param embed_Manager $Embedder
+     * @param stdClass $data
+     */
+    protected static function on_AfterPrepareEditForm(frame2_driver_Proto $Driver, embed_Manager $Embedder, &$data)
+    {
+        $form = $data->form;
+        $rec = $form->rec;
+
+
+    }
+
+
+    /**
+     * Кои записи ще се показват в таблицата
+     *
+     * @param stdClass $rec
+     * @param stdClass $data
+     *
+     * @return array
+     */
+    protected function prepareRecs($rec, &$data = null)
+    {
+
+        $recs = array();
+
+        $pQuery = store_ShipmentOrderDetails::getQuery();
+
+        bp();
+
+        $pQuery->EXT('isPublic', 'cat_Products', 'externalName=isPublic,externalKey=productId');
+
+
+        return $recs;
+    }
+
+
+    /**
+     * Връща фийлдсета на таблицата, която ще се рендира
+     *
+     * @param stdClass $rec - записа
+     * @param bool $export - таблицата за експорт ли е
+     *
+     * @return core_FieldSet - полетата
+     */
+    protected function getTableFieldSet($rec, $export = false)
+    {
+        $fld = cls::get('core_FieldSet');
+
+        $fld->FLD('code', 'varchar', 'caption=Код,tdClass=centered');
+        $fld->FLD('productName', 'varchar', 'caption=Артикул');
+        $fld->FLD('measure', 'varchar', 'caption=Мярка,tdClass=centered');
+
+
+        return $fld;
+    }
+
+
+    /**
+     * Вербализиране на редовете, които ще се показват на текущата страница в отчета
+     *
+     * @param stdClass $rec
+     *                       - записа
+     * @param stdClass $dRec
+     *                       - чистия запис
+     *
+     * @return stdClass $row - вербалния запис
+     */
+    protected function detailRecToVerbal($rec, &$dRec)
+    {
+        $Double = cls::get('type_Double');
+        $Double->params['decimals'] = 2;
+
+        $row = new stdClass();
+        if (isset($dRec->code)) {
+            $row->code = $dRec->code;
+        }
+
+
+        if (isset($dRec->productName)) {
+            $row->productName = cat_Products::getLinkToSingle($dRec->productId, 'name');
+        }
+
+        $row->measure = cat_UoM::fetchField(cat_Products::fetch($dRec->productId)->measureId, 'shortName');
+
+
+        return $row;
+    }
+
+
+    /**
+     * След рендиране на единичния изглед
+     *
+     * @param frame2_driver_Proto $Driver
+     * @param embed_Manager $Embedder
+     * @param core_ET $tpl
+     * @param stdClass $data
+     */
+    protected static function on_AfterRecToVerbal(frame2_driver_Proto $Driver, embed_Manager $Embedder, $row, $rec, $fields = array())
+    {
+    }
+
+
+    /**
+     * След рендиране на единичния изглед
+     *
+     * @param cat_ProductDriver $Driver
+     * @param embed_Manager $Embedder
+     * @param core_ET $tpl
+     * @param stdClass $data
+     */
+    protected static function on_AfterRenderSingle(frame2_driver_Proto $Driver, embed_Manager $Embedder, &$tpl, $data)
+    {
+        $Date = cls::get('type_Date');
+        $Double = cls::get('type_Double');
+        $Double->params['decimals'] = 2;
+
+
+        $fieldTpl = new core_ET(tr("|*<!--ET_BEGIN BLOCK-->[#BLOCK#]
+								<fieldset class='detail-info'><legend class='groupTitle'><small><b>|Филтър|*</b></small></legend>
+                                <small><div><!--ET_BEGIN date-->|Към дата|*: [#date#]<!--ET_END date--></div></small>
+                                <small><div><!--ET_BEGIN storeId-->|Склад|*: [#storeId#]<!--ET_END storeId--></div></small>
+                                <small><div><!--ET_BEGIN group-->|Групи|*: [#group#]<!--ET_END group--></div></small>
+                                <small><div><!--ET_BEGIN products-->|Артикул|*: [#products#]<!--ET_END products--></div></small>
+                                <small><div><!--ET_BEGIN availability-->|Наличност|*: [#availability#]<!--ET_END availability--></div></small>
+                                <small><div><!--ET_BEGIN totalProducts-->|Брой артикули|*: [#totalProducts#]<!--ET_END totalProducts--></div></small>
+                                </fieldset><!--ET_END BLOCK-->"));
+
+        $date = (is_null($data->rec->date)) ? dt::today() : $data->rec->date;
+
+        $fieldTpl->append('<b>' . $Date->toVerbal($date) . '</b>', 'date');
+
+
+        if (isset($data->rec->group)) {
+            $marker = 0;
+            $groupVerb = '';
+            foreach (type_Keylist::toArray($data->rec->group) as $group) {
+                $marker++;
+
+                $groupVerb .= (cat_Groups::getTitleById($group));
+
+                if ((countR((type_Keylist::toArray($data->rec->group))) - $marker) != 0) {
+                    $groupVerb .= ', ';
+                }
+            }
+
+            $fieldTpl->append('<b>' . $groupVerb . '</b>', 'group');
+        }
+
+
+        if (isset($data->rec->storeId)) {
+            $storeIdVerb = '';
+            foreach (type_Keylist::toArray($data->rec->storeId) as $store) {
+                $marker++;
+
+                $storeIdVerb .= (store_Stores::getTitleById($store));
+
+                if ((countR(type_Keylist::toArray($data->rec->storeId))) - $marker != 0) {
+                    $storeIdVerb .= ', ';
+                }
+            }
+
+            $fieldTpl->append('<b>' . $storeIdVerb . '</b>', 'storeId');
+        } else {
+            $fieldTpl->append('<b>' . 'Всички' . '</b>', 'storeId');
+        }
+
+        if ((isset($data->rec->products))) {
+            $fieldTpl->append('<b>' . cat_Products::getTitleById($data->rec->products) . '</b>', 'products');
+        }
+
+        if ((isset($data->rec->availability))) {
+            $fieldTpl->append('<b>' . ($data->rec->availability) . '</b>', 'availability');
+        }
+
+        if ((isset($data->rec->totalProducts))) {
+            $fieldTpl->append('<b>' . ($data->rec->totalProducts) . '</b>', 'totalProducts');
+        }
+
+        $tpl->append($fieldTpl, 'DRIVER_FIELDS');
+    }
+
+
+    /**
+     * След подготовка на реда за експорт
+     *
+     * @param frame2_driver_Proto $Driver
+     * @param stdClass $res
+     * @param stdClass $rec
+     * @param stdClass $dRec
+     */
+    protected static function on_AfterGetExportRec(frame2_driver_Proto $Driver, &$res, $rec, $dRec, $ExportClass)
+    {
+        $Double = cls::get('type_Double');
+        $Double->params['decimals'] = 2;
+
+        $res->quantyti = $dRec->blQuantity;
+
+        $res->measure = cat_UoM::fetch(cat_Products::fetch($dRec->productId)->measureId)->shortName;
+    }
+}
