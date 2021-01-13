48d6f8eb25911bba227004dafa983859aabeec16
Yuseinov <yuseinov@experta.bg>
2021-01-13 11:08:47 +0200

    dec_Statements #C39780 - по-голямо ограничение на полето

--- dec/Statements.class.php
+++ dec/Statements.class.php
@@ -68,7 +68,7 @@ class dec_Statements extends core_Master
      */
     public function description()
     {
-        $this->FLD('title', 'varchar(32)', 'caption=Заглавие, width=100%');
+        $this->FLD('title', 'varchar(64)', 'caption=Заглавие, width=100%');
         $this->FLD('text', 'richtext(bucket=Notes)', 'caption=Текст');
         
         $this->setDbUnique('title');
