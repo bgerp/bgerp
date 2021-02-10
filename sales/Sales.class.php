c028cfb2566f380d84939b5e165d5eb5a96c5219
ivelin1012 <ivelin_pdimov@abv.bg>
2021-02-10 09:34:51 +0200

    забравено бп

--- sales/Sales.class.php
+++ sales/Sales.class.php
@@ -1657,7 +1657,7 @@ class sales_Sales extends deals_DealMaster
                         }
                     }
 
-                    if($addProductToCheck){ bp($detailsToCheck);
+                    if($addProductToCheck){
                         $detailsToCheck[] = $dRec;
                     }
                 }
