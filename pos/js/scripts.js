3d531805d4e93d309347efefae53396acd895c60
ivelin1012 <ivelin_pdimov@abv.bg>
2021-03-02 16:39:01 +0200

    пос иконката за изтриване да тригърва търсенето

--- pos/js/scripts.js
+++ pos/js/scripts.js
@@ -52,10 +52,12 @@ function posActions() {
 		processUrl(url, params);
 	});
 
-
 	//  ресет бутона на изтрива текста в инпута
 	$(document.body).on('click', ".close-icon", function(e){
 		$('.large-field').val("");
+
+		// След изтриване, се тригърва, че все едно ръчно е изтрито
+		triggerSearchInput($(".large-field"), 0, true);
 	});
 
 	/**
