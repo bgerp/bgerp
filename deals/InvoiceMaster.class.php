b71c67dd03b7354fc9cf55a2a109631c549b6f23
ivelin1012 <ivelin_pdimov@abv.bg>
2021-03-02 19:47:21 +0200

    ф-ри:бъгфикс

--- deals/InvoiceMaster.class.php
+++ deals/InvoiceMaster.class.php
@@ -729,8 +729,8 @@ abstract class deals_InvoiceMaster extends core_Master
                 $paymentType = ($aggregateInfo->get('paymentType')) ? $aggregateInfo->get('paymentType') : cond_PaymentMethods::fetchField($paymentMethodId, 'type');
                 $form->setDefault('paymentType', $paymentType);
             }
-            
-            $form->rec->deliveryId = $aggregateInfo->get('deliveryTerm');
+
+            $form->setDefault('deliveryId', $aggregateInfo->get('deliveryTerm'));
             if ($aggregateInfo->get('deliveryLocation')) {
                 $form->setDefault('deliveryPlaceId', $aggregateInfo->get('deliveryLocation'));
             }
