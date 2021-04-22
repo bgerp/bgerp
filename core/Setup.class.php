ed063d6eee38aedbe8a7aadcd77a7c866e07df1e
Yuseinov <yuseinov@experta.bg>
2021-04-22 13:42:17 +0300

    core_Setup #C43117 - добавене версия

--- core/Setup.class.php
+++ core/Setup.class.php
@@ -217,7 +217,7 @@ define('CORE_LAST_DB_VERSION', '18.25-Shabran');
  * Тази константа не трябва да се ползва с core_Setup::getConfig(),
  * а само с: core_setup::CURRENT_VERSION
  */
-define('CORE_CODE_VERSION', '20.49-Sinanitsa');
+define('CORE_CODE_VERSION', '21.16-Bezbog');
 
 
 /**
