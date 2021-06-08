83f9bd7c641584853db13aeb6b6b87795bb7c23d
Dimitar Minekov <mitko@experta.bg>
2021-06-08 14:08:46 +0300

    намален таймаут при лисващ search сървър

--- doc/Search.class.php
+++ doc/Search.class.php
@@ -74,19 +74,17 @@ class doc_Search extends core_Manager
         $this->dbIndexes = $DC->dbIndexes;
 
         if (defined('SEARCH_DB_NAME') && defined('SEARCH_DB_USER') && defined('SEARCH_DB_PASS') && defined('SEARCH_DB_HOST')) {
-            $conn = @mysqli_connect(SEARCH_DB_HOST, SEARCH_DB_USER, SEARCH_DB_PASS);
-            if (mysqli_connect_error()) {
-                $this->logWarning("Дефинирани, но не работещи константи за SEARCH");
+            $conn = mysqli_init();
+            $conn->options(MYSQLI_OPT_CONNECT_TIMEOUT, 1);
+            if (!@$conn->real_connect(SEARCH_DB_HOST, SEARCH_DB_USER, SEARCH_DB_PASS, SEARCH_DB_NAME)) {
+                if (rand(1, 100)%7 == 0) {
+                    $this->logWarning("Дефинирани, но не работещи константи за SEARCH");
+                }
             } else {
-                $db_selected = $conn->select_db(SEARCH_DB_NAME);
-                if (!$db_selected) {
-                    $this->logWarning("Невалидна база данни за SEARCH");
-                } else {
                     $this->db->dbName = SEARCH_DB_NAME;
                     $this->db->dbPass = SEARCH_DB_PASS;
                     $this->db->dbUser = SEARCH_DB_USER;
                     $this->db->host = SEARCH_DB_HOST;
-                }
             }
         }
     }
