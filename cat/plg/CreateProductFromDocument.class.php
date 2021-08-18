443a25096af1d334debbd3fd3e91f119d6546d70
ivelin1012 <ivelin_pdimov@abv.bg>
2021-08-18 17:32:15 +0300

    форсиране на разходен обект при създаване на поръчка за транспорт по дефолт да разпределя по стойност върху всички артикули

--- cat/plg/CreateProductFromDocument.class.php
+++ cat/plg/CreateProductFromDocument.class.php
@@ -489,7 +489,7 @@ class cat_plg_CreateProductFromDocument extends core_Plugin
                     // Разпределяне на разходи при нужда
                     if (isset($d->costItemId)) {
                         acc_CostAllocations::delete("#detailClassId = {$mvc->getClassId()} AND #detailRecId = {$dRec->id} AND #productId = {$productId}");
-                        $saveRec = (object) array('detailClassId' => $mvc->getClassId(), 'detailRecId' => $dRec->id, 'productId' => $productId, 'expenseItemId' => $d->costItemId, 'containerId' => $masterRec->containerId, 'quantity' => $dRec->quantity, 'allocationBy' => 'value');
+                        $saveRec = (object) array('detailClassId' => $mvc->getClassId(), 'detailRecId' => $dRec->id, 'productId' => $productId, 'expenseItemId' => $d->costItemId, 'containerId' => $masterRec->containerId, 'quantity' => $dRec->quantity, 'allocationBy' => 'auto');
 
                         $itemRec = acc_Items::fetch($d->costItemId, 'classId,objectId');
                         $origin = new core_ObjectReference($itemRec->classId, $itemRec->objectId);
