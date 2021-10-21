264ec9c2d1a2a2aaf38cd670d8afa1af2e20989e
ivelin1012 <ivelin_pdimov@abv.bg>
2021-10-21 12:50:22 +0300

    Продажба и покупка: еднократната доставка да може да се променя и на активиран договор

--- sales/Sales.class.php
+++ sales/Sales.class.php
@@ -62,7 +62,7 @@ class sales_Sales extends deals_DealMaster
     /**
      * Полетата, които могат да се променят с change_Plugin
      */
-    public $changableFields = 'dealerId,initiatorId';
+    public $changableFields = 'dealerId,initiatorId,oneTimeDelivery';
     
     
     /**
