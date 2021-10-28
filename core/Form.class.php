54ce7760e591ccd6704b7fccb2b39eca8d7f6ef2
Yuseinov <yuseinov@experta.bg>
2021-10-28 11:19:52 +0300

    core_Form #C46128 - fix2  премахване на forceOpen, ако има грешка при субмитване

--- core/Form.class.php
+++ core/Form.class.php
@@ -291,7 +291,7 @@ class core_Form extends core_FieldSet
             }
         }
 
-        if ($this->gotErrors()) {
+        if ($this->gotErrors() || $this->isSubmitted()) {
             foreach ((array) $this->fields as &$fObj) {
                 unset($fObj->type->params['forceOpen']);
             }
