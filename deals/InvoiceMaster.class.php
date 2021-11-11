d446683ee3252a420074f0a386c0b00d914b200e
ivelin1012 <ivelin_pdimov@abv.bg>
2021-11-11 16:21:34 +0200

    КИ и ДИ: опит за бъгфикс

--- deals/InvoiceMaster.class.php
+++ deals/InvoiceMaster.class.php
@@ -1392,53 +1392,47 @@ abstract class deals_InvoiceMaster extends core_Master
     {
         expect($document = doc_Containers::getDocument($containerId));
         expect($document->isInstanceOf($this));
-        
-        if (!isset($this->cache[$containerId])) {
-            $cache = $vats = $cacheIds = array();
-            $Detail = $this->mainDetail;
-            $query = $Detail::getQuery();
-            $vatRate = $document->fetchField('vatRate');
-            $dpAmount = $document->fetch('dpAmount');
 
-            $query->where("#{$this->{$Detail}->masterKey} = '{$document->that}'");
-            $query->orderBy('id', 'ASC');
+        $cache = $vats = $cacheIds = array();
+        $Detail = $this->mainDetail;
+        $query = $Detail::getQuery();
+        $vatRate = $document->fetchField('vatRate');
+        $dpAmount = $document->fetch('dpAmount');
 
-            while ($dRec = $query->fetch()) {
+        $query->where("#{$this->{$Detail}->masterKey} = '{$document->that}'");
+        $query->orderBy('id', 'ASC');
 
-                if($applyDiscount){
-                    $price = empty($dRec->discount) ? $dRec->packPrice : ($dRec->packPrice * (1 - $dRec->discount));
-                } else {
-                    $price = $dRec->packPrice;
-                }
-                $price = round($price, 5);
-                $key1 = "{$dRec->productId}|{$dRec->packagingId}|{$dRec->quantityInPack}|{$dRec->batches}|{$dRec->notes}|Q{$dRec->quantity}";
-                $key2 = "{$dRec->productId}|{$dRec->packagingId}|{$dRec->quantityInPack}|{$dRec->batches}|{$dRec->notes}|P{$price}";
-
-                $cache[$key1] = array('quantity' => $dRec->quantity, 'price' => $price);
-                $cache[$key2] = array('quantity' => $dRec->quantity, 'price' => $price);
-                $cacheIds[$dRec->id] = array('quantity' => $dRec->quantity, 'price' => $price);
-
-                if ($vatRate != 'no' && $vatRate != 'exempt') {
-                    $v = cat_Products::getVat($dRec->productId, $document->fetchField('date'));
-                }
-                $vats[$v] = $v;
+        while ($dRec = $query->fetch()) {
+            if($applyDiscount){
+                $price = empty($dRec->discount) ? $dRec->packPrice : ($dRec->packPrice * (1 - $dRec->discount));
+            } else {
+                $price = $dRec->packPrice;
             }
 
+            $price = round($price, 5);
+            $key1 = "{$dRec->productId}|{$dRec->packagingId}|{$dRec->quantityInPack}|{$dRec->batches}|{$dRec->notes}|Q{$dRec->quantity}";
+            $key2 = "{$dRec->productId}|{$dRec->packagingId}|{$dRec->quantityInPack}|{$dRec->batches}|{$dRec->notes}|P{$price}";
 
-            if (!countR($cache)) {
-                if (isset($dpAmount)) {
-                    $v = ($vatRate == 'yes' || $vatRate == 'separate') ? 0.2 : 0;
-                    $vats["{$v}"] = $v;
-                }
+            $cache[$key1] = array('quantity' => $dRec->quantity, 'price' => $price);
+            $cache[$key2] = array('quantity' => $dRec->quantity, 'price' => $price);
+            $cacheIds[$dRec->id] = array('quantity' => $dRec->quantity, 'price' => $price);
+            if ($vatRate != 'no' && $vatRate != 'exempt') {
+                $v = cat_Products::getVat($dRec->productId, $document->fetchField('date'));
             }
 
-
-
-
-            $this->cache[$containerId] = (object) array('recs' => $cache, 'vats' => $vats, 'recWithIds' => $cacheIds);
+            $vats[$v] = $v;
         }
 
-        return $this->cache[$containerId];
+        if (!countR($cache)) {
+            if (isset($dpAmount)) {
+                $v = ($vatRate == 'yes' || $vatRate == 'separate') ? 0.2 : 0;
+                $vats["{$v}"] = $v;
+            }
+        }
+
+        $res = (object) array('recs' => $cache, 'vats' => $vats, 'recWithIds' => $cacheIds);
+
+        return $res;
     }
 
 
