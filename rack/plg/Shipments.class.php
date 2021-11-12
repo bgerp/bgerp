5f2be97b9033de45ae6d859db52222fb040c2ab7
ivelin1012 <ivelin_pdimov@abv.bg>
2021-11-12 15:04:26 +0200

    палетен склад: бъгфикс с много дребни числа

--- rack/plg/Shipments.class.php
+++ rack/plg/Shipments.class.php
@@ -113,15 +113,17 @@ class rack_plg_Shipments extends core_Plugin
             $res = array();
             if(isset($mvc->detailToPlaceInZones)){
                 $Detail = cls::get($mvc->detailToPlaceInZones);
+
+
                 $dQuery = $Detail->getQuery();
                 $dQuery->EXT('canStore', 'cat_Products', "externalName=canStore,externalKey={$Detail->productFld}");
                 $dQuery->where("#{$Detail->masterKey} = {$rec->id} AND #canStore = 'yes'");
-                
+
                 while($dRec = $dQuery->fetch()){
                     
                     $key = "{$dRec->{$Detail->productFld}}|{$dRec->packagingId}";
                     $rest = $dRec->{$Detail->quantityFld};
-                    
+
                     $Def = batch_Defs::getBatchDef($dRec->productId);
                     if (is_object($Def)) {
                         
@@ -141,8 +143,8 @@ class rack_plg_Shipments extends core_Plugin
                             }
                         }
                     }
-                    
-                    if(round($rest, 2) > 0){
+
+                    if(round($rest, 5) > 0){
                         $key3 = "{$key}|{$k}||";
                         if(!array_key_exists($key3, $res)){
                             $res[$key3] = (object)array('productId' => $dRec->{$Detail->productFld}, 'packagingId' => $dRec->packagingId, 'batch' => '');
