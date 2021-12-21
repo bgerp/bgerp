c0665af74529f136840d41ee69d4ce06669d7245
Dimitar Minekov <mitko@experta.bg>
2021-12-21 17:22:34 +0200

    Регистрацията и валидацията в отделни екшъни

--- phpsignal/Client.class.php
+++ phpsignal/Client.class.php
@@ -31,11 +31,8 @@ class phpsignal_Client extends core_Manager
         if (core_Composer::isInUse()) {
             try {
                 // Инстанция на класа
-                $client = new Signal(
-                    phpsignal_Setup::get('SIGNAL_BIN_PATH'), // Binary Path
-                    phpsignal_Setup::get('SIGNAL_NUMBER'), // Username/Number including Country Code with '+'
-                    Signal::FORMAT_JSON // Format
-                    );
+                $binPath = phpsignal_Setup::get('SIGNAL_PATH') . '/signal-cli-' . phpsignal_Setup::get('SIGNAL_VERSION') . '/bin/signal-cli';
+                $client = new Signal($binPath, phpsignal_Setup::get('SIGNAL_NUMBER'), Signal::FORMAT_JSON);
             } catch (Exception $e) {
                 reportException($e);
             }
@@ -63,7 +60,7 @@ class phpsignal_Client extends core_Manager
             
             
             
-            $msg = 'Регистриран код за signal-cli|*';
+            $msg = 'Валидиран код за signal-cli|*';
             
             return new Redirect($retUrl, $msg);
             
@@ -109,5 +106,47 @@ class phpsignal_Client extends core_Manager
             
     }
     
-
+    /**
+     * Регистрира signal клиента
+     */
+    public function act_Register()
+    {
+        requireRole('admin');
+        $form = cls::get('core_Form');
+        $form->title = 'Регистрация клиент signal';
+        $form->FLD('captcha', 'varchar(512)', 'caption=Кепча от: https://signalcaptchas.org/staging/challenge/generate.html->Кепча');
+        $form->FLD('validationMethod', 'enum(voice,sms)', 'caption=Получаване на код валидиране->VOICE/SMS');
+        
+        $form->input('captcha,validationMethod', true);
+        
+        if ($form->isSubmitted()) {
+            $retUrl = getRetUrl();
+            
+            // Инстанция на класа
+            $binPath = phpsignal_Setup::get('SIGNAL_PATH') . '/signal-cli-' . phpsignal_Setup::get('SIGNAL_VERSION') . '/bin/signal-cli';
+            $client = new Signal($binPath, phpsignal_Setup::get('SIGNAL_NUMBER'), Signal::FORMAT_JSON);
+            
+            $signalNumber = phpsignal_Setup::get('SIGNAL_NUMBER');
+            
+            if (false !== strpos($client->getUserStatus([$signalNumber]), 'true')) {
+                $msg = 'Регистриран отпреди номер.|*';
+            } else {
+                $validationMethod = ($form->rec->validationMethod == 'voice') ? true : false;
+                if ($client->Register($validationMethod, $signalNumber)) {
+                    $msg = "Успешно регистриран номер.";
+                }
+            }
+            
+            
+            return new Redirect($retUrl, $msg);
+            
+        }
+        $form->toolbar->addSbBtn('Регистрирай', 'save', 'ef_icon = img/16/disk.png, title = Регистрирай');
+        $form->toolbar->addBtn('Отказ', getRetUrl(), 'ef_icon = img/16/close-red.png, title=Прекратяване на действията');
+        
+        $tpl = $this->renderWrapping($form->renderHtml());
+        
+        return $tpl;
+    }
+    
 }
