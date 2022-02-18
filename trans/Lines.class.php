126d80c9e0938507ac70e17146aa506b80af5f83
arsov <arsov@experta.bg>
2022-02-19 00:31:48 +0200

    Разкрасяване заглавията на ТЛ

--- trans/Lines.class.php
+++ trans/Lines.class.php
@@ -235,7 +235,7 @@ class trans_Lines extends core_Master
 
         $titleArr[] = str::limitLen($rec->title, 32);
         $titleArr[] = static::getHandle($rec->id);
-        $recTitle = implode('/', $titleArr);
+        $recTitle = implode(' » ', $titleArr);
 
         if ($escaped) {
             $recTitle = type_Varchar::escape($recTitle);
