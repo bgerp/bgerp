16a5489e4095f8d0f0883980225520cb43f0662e
angel <angel.trifonoff@gmail.com>
2022-04-14 11:40:53 +0300

    za test

--- sales/reports/SoldProductsRep.class.php
+++ sales/reports/SoldProductsRep.class.php
@@ -1215,9 +1215,10 @@ class sales_reports_SoldProductsRep extends frame2_driver_TableData
 
         $res = array();
         $originId = doc_Containers::getDocument($dcRec->originId)->that;
-        $originDetRec = sales_InvoiceDetails::fetch("#invoiceId = ${originId} AND #productId = {$dcRec->productId} AND
-                                                           #packagingId = {$dcRec->packagingId} AND 
-                                                           (#quantity != {$dcRec->quantity} OR #price != {$dcRec->price})");
+
+        $originDetRec = sales_InvoiceDetails::fetch("#invoiceId = $originId AND #productId = $dcRec->productId AND
+                                                           #packagingId = $dcRec->packagingId 
+                                                           AND (#quantity != $dcRec->quantity OR #price != $dcRec->price)");
 
         $originQuantity = $originDetRec->quantity * $originDetRec->quantityInPack;
         $changeQuatity = $dcRec->quantity * $dcRec->quantityInPack - $originQuantity;
