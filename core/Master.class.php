b04f15df859bb4c2afc6deafd913eba0baa2a53b
arsov <arsov@experta.bg>
2022-05-04 13:15:37 +0300

    Филтър по Състояние на документа

--- core/Master.class.php
+++ core/Master.class.php
@@ -953,4 +953,27 @@ class core_Master extends core_Manager
         
         return $masterTitle;
     }
+
+    
+    /**
+     *  Подготовка на филтър формата
+     */
+    protected static function on_AfterPrepareListFilter($mvc, $data)
+    {
+        if (!Request::get('Rejected', 'int')) {
+            $data->listFilter->FNC('dState', 'enum(all=Всички, pending=Заявка, draft=Чернова, active=Контиран)', 'caption=Състояние,input,silent');
+            $data->listFilter->showFields .= ',dState';
+            $data->listFilter->input();
+            $data->listFilter->setDefault('dState', 'all');
+        }
+        
+        if ($rec = $data->listFilter->rec) {
+            if ($rec->dState) {
+                if ($rec->dState != 'all') {
+                    $data->query->where("#state = '{$rec->dState}'");
+                }
+            }
+        }
+    }
+
 }
