a5182c3ce08cdc41926f8e536651e0b1d160b0bf
angel <angel.trifonoff@gmail.com>
2022-05-18 17:18:54 +0300

    bugfix

--- store/reports/UnfulfilledQuantities.class.php
+++ store/reports/UnfulfilledQuantities.class.php
@@ -20,30 +20,30 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
      * Кой може да избира драйвъра
      */
     public $canSelectDriver = 'ceo,manager,store,planning,purchase';
-    
-    
+
+
     /**
      * Брой записи на страница
      *
      * @var int
      */
     protected $listItemsPerPage = 30;
-    
-    
+
+
     /**
      * Коя комбинация от полета от $data->recs да се следи, ако има промяна в последната версия
      *
      * @var string
      */
     protected $newFieldsToCheck;
-    
-    
+
+
     /**
      * Кои полета може да се променят от потребител споделен към справката, но нямащ права за нея
      */
-    protected $changeableFields ;
-    
-    
+    protected $changeableFields;
+
+
     /**
      * Добавя полетата на драйвера към Fieldset
      *
@@ -59,63 +59,63 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
         $fieldset->FLD('group', 'key(mvc=cat_Groups,select=name,allowEmpty)', 'caption=Група артикули,placeholder=Всички,after=storeId,single=none');
         $fieldset->FLD('tolerance', 'double', 'caption=Толеранс,after=group,unit = %,single=none,mandatory');
     }
-    
-    
+
+
     /**
      * Преди показване на форма за добавяне/промяна.
      *
      * @param frame2_driver_Proto $Driver
      *                                      $Driver
-     * @param embed_Manager       $Embedder
-     * @param stdClass            $data
+     * @param embed_Manager $Embedder
+     * @param stdClass $data
      */
     protected static function on_AfterPrepareEditForm(frame2_driver_Proto $Driver, embed_Manager $Embedder, &$data)
     {
         $form = $data->form;
         $rec = $form->rec;
-        
+
         $form->setDefault('tolerance', 5);
-        
+
         $salesQuery = sales_Sales::getQuery();
-        
+
         $salesQuery->EXT('folderTitle', 'doc_Folders', 'externalName=title,externalKey=folderId');
-        
+
         $salesQuery->groupBy('folderId');
-        
+
         $salesQuery->show('folderId, contragentId, folderTitle');
-        
+
         while ($contragent = $salesQuery->fetch()) {
             if (!is_null($contragent->contragentId)) {
                 $suggestions[$contragent->folderId] = $contragent->folderTitle;
             }
         }
-        
+
         asort($suggestions);
-        
+
         $form->setSuggestions('contragent', $suggestions);
     }
-    
-    
+
+
     /**
      * След рендиране на единичния изглед
      *
      * @param cat_ProductDriver $Driver
-     * @param embed_Manager     $Embedder
-     * @param core_Form         $form
-     * @param stdClass          $data
+     * @param embed_Manager $Embedder
+     * @param core_Form $form
+     * @param stdClass $data
      */
     protected static function on_AfterInputEditForm(frame2_driver_Proto $Driver, embed_Manager $Embedder, &$form)
     {
         $rec = $form->rec;
-        
+
         if ($form->isSubmitted()) {
             if ($rec->tolerance < 0) {
                 $form->setError('tolerance', ' Толераса трябва да е положително число. ');
             }
         }
     }
-    
-    
+
+
     /**
      * Кои записи ще се показват в таблицата
      *
@@ -129,7 +129,7 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
         $saleDetRecs = array();
         $shipDetRecs = array();
         $recs = array();
-        
+
         //Продажби
         $querySaleDetails = sales_SalesDetails::getQuery();
 
@@ -138,31 +138,31 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
         $querySaleDetails->EXT('threadId', 'sales_Sales', 'externalName=threadId,externalKey=saleId');
 
         $querySaleDetails->EXT('closedOn', 'sales_Sales', 'externalName=closedOn,externalKey=saleId');
-        
+
         $querySaleDetails->EXT('folderId', 'sales_Sales', 'externalName=folderId,externalKey=saleId');
-        
+
         $querySaleDetails->EXT('state', 'sales_Sales', 'externalName=state,externalKey=saleId');
-        
+
         $querySaleDetails->EXT('contragentClassId', 'sales_Sales', 'externalName=contragentClassId,externalKey=saleId');
-        
+
         $querySaleDetails->EXT('contragentId', 'sales_Sales', 'externalName=contragentId,externalKey=saleId');
 
         $querySaleDetails->EXT('contoActions', 'sales_Sales', 'externalName=contoActions,externalKey=saleId');
-        
+
         $querySaleDetails->where("#state = 'closed'");
 
-        $querySaleDetails->where(array("#closedOn >= '[#1#]' AND #closedOn <= '[#2#]'",$rec->from . ' 00:00:00',$rec->to . ' 23:59:59'));
+        $querySaleDetails->where(array("#closedOn >= '[#1#]' AND #closedOn <= '[#2#]'", $rec->from . ' 00:00:00', $rec->to . ' 23:59:59'));
 
-        $querySaleDetails->like('contoActions', 'ship',false);
+        $querySaleDetails->like('contoActions', 'ship', false);
 
         if (!is_null($rec->contragent)) {
             $checkedContragents = keylist::toArray($rec->contragent);
-            
-            $querySaleDetails-> in('folderId', $checkedContragents);
+
+            $querySaleDetails->in('folderId', $checkedContragents);
         }
-        
+
         $querySaleDetails->where("#isPublic = 'yes'");
-        
+
         $querySaleDetails->show('id,saleId,contragentClassId,contragentId,productId,threadId,folderId,quantity,createdOn');
 
         // Синхронизира таймлимита с броя записи
@@ -174,12 +174,12 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
 
         while ($saleArt = $querySaleDetails->fetch()) {
             $saleThreadsIds[] = $saleArt->threadId;
-            $saleKey = $saleArt->threadId.'|'.$saleArt->productId;
-            
+            $saleKey = $saleArt->threadId . '|' . $saleArt->productId;
+
             // добавяме в масива
             if (!array_key_exists($saleKey, $saleDetRecs)) {
-                $saleDetRecs[$saleKey] = (object) array(
-                    
+                $saleDetRecs[$saleKey] = (object)array(
+
                     'productId' => $saleArt->productId,
                     'requestQuantity' => $saleArt->quantity,
                     'saleId' => $saleArt->saleId,
@@ -187,62 +187,62 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
                     'contragentId' => $saleArt->contragentId,
                     'threadId' => $saleArt->threadId,
                     'folderId' => $saleArt->folderId
-                
+
                 );
             } else {
                 $obj = &$saleDetRecs[$saleKey];
                 $obj->requestQuantity += $saleArt->quantity;
             }
         }
-        
-        
+
+
         //Експедиционни нареждания
         $queryShipmentOrderDetails = store_ShipmentOrderDetails::getQuery();
-        
+
         $queryShipmentOrderDetails->EXT('groups', 'cat_Products', 'externalName=groups,externalKey=productId');
-        
+
         $queryShipmentOrderDetails->EXT('threadId', 'store_ShipmentOrders', 'externalName=threadId,externalKey=shipmentId');
-        
+
         $queryShipmentOrderDetails->EXT('shipmentOrderActivatedOn', 'store_ShipmentOrders', 'externalName=activatedOn,externalKey=shipmentId');
-        
+
         $queryShipmentOrderDetails->EXT('state', 'store_ShipmentOrders', 'externalName=state,externalKey=shipmentId');
-        
+
         $queryShipmentOrderDetails->EXT('storeId', 'store_ShipmentOrders', 'externalName=storeId,externalKey=shipmentId');
-        
+
         $queryShipmentOrderDetails->where("#state = 'active'");
-        
-        $queryShipmentOrderDetails->where(array("#shipmentOrderActivatedOn >= '[#1#]' AND #shipmentOrderActivatedOn <= '[#2#]'",$rec->from . ' 00:00:01',$rec->to . ' 23:59:59'));
-        
+
+        $queryShipmentOrderDetails->where(array("#shipmentOrderActivatedOn >= '[#1#]' AND #shipmentOrderActivatedOn <= '[#2#]'", $rec->from . ' 00:00:01', $rec->to . ' 23:59:59'));
+
         //филтър по склад
         if ($rec->storeId) {
             $queryShipmentOrderDetails->where("#storeId = {$rec->storeId}");
         }
-        
+
         //Филтър по групи артикули
         if (isset($rec->group)) {
             $queryShipmentOrderDetails->where('#groups IS NOT NULL');
             $queryShipmentOrderDetails->likeKeylist('groups', $rec->group);
         }
-        
+
         $queryShipmentOrderDetails->show('id,shipmentId,productId,threadId,quantity,createdOn,shipmentOrderActivatedOn');
-        
+
         while ($shipmentDet = $queryShipmentOrderDetails->fetch()) {
             $threadId = $shipmentDet->threadId;
-            
+
             $saleIdShip = doc_Threads::getFirstDocument($threadId)->that;
-            
+
             $firstDocumentName = doc_Threads::getFirstDocument($threadId)->className;
-            
+
             if ($firstDocumentName != 'sales_Sales') {
                 continue;
             }
-            
-            $shipKey = $shipmentDet->threadId.'|'.$shipmentDet->productId;
-            
+
+            $shipKey = $shipmentDet->threadId . '|' . $shipmentDet->productId;
+
             // добавяме в масива
             if (!array_key_exists($shipKey, $shipDetRecs)) {
-                $shipDetRecs[$shipKey] = (object) array(
-                    
+                $shipDetRecs[$shipKey] = (object)array(
+
                     'id' => $shipmentDet->id,
                     'productId' => $shipmentDet->productId,
                     'shipedQuantity' => $shipmentDet->quantity,
@@ -250,7 +250,7 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
                     'firstDocumentName' => $firstDocumentName,
                     'shipmentId' => $shipmentDet->shipmentId,
                     'threadIdShip' => $shipmentDet->threadId
-                
+
                 );
             } else {
                 $obj = &$shipDetRecs[$shipKey];
@@ -258,20 +258,22 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
             }
         }
 
-        foreach ($saleDetRecs as $saleKey  => $sale) {
-            foreach ($shipDetRecs as $shipKey =>$ship ) {
+        $salesWithShipments = (arr::extractValuesFromArray($shipDetRecs, 'saleIdShip'));
+
+        foreach ($saleDetRecs as $saleKey => $sale) {
+            foreach ($shipDetRecs as $shipKey => $ship) {
                 expect($ship->firstDocumentName == 'sales_Sales');
-                
+
                 if ($shipKey == $saleKey) {
                     expect($sale->saleId == $ship->saleIdShip);
-                    
+
                     $tolerance = (100 - $rec->tolerance) / 100;
 
-                    $shipedQuantity = ($ship->shipedQuantity) ? : 0;
-                    if ($sale->saleId == 1563)bp($sale,$ship,$shipedQuantity,$ship->shipedQuantity < ($sale->requestQuantity * $tolerance));
-                    if ($ship->shipedQuantity < ($sale->requestQuantity * $tolerance)) {
-                        $recs[$saleKey] = (object) array(
-                            
+                    $shipedQuantity = $ship->shipedQuantity;
+
+                    if ($shipedQuantity < ($sale->requestQuantity * $tolerance)) {
+                        $recs[$saleKey] = (object)array(
+
                             'saleId' => $sale->saleId,
                             'productId' => $sale->productId,
                             'measure' => cat_Products::getProductInfo($sale->productId)->productRec->measureId,
@@ -282,34 +284,34 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
                             'contragentId' => $sale->contragentId
                         );
                     }
-                }else{
+                } else {
+                    if (!in_array($sale->saleId, $salesWithShipments)) {
 
-                    $recs[$saleKey] = (object) array(
-
-                        'saleId' => $sale->saleId,
-                        'productId' => $sale->productId,
-                        'measure' => cat_Products::getProductInfo($sale->productId)->productRec->measureId,
-                        'shipmentId' => '',
-                        'shipedQuantity' => 0,
-                        'requestQuantity' => $sale->requestQuantity,
-                        'contragentClassId' => $sale->contragentClassId,
-                        'contragentId' => $sale->contragentId
-                    );
-
+                        $recs[$saleKey] = (object)array(
+                            'saleId' => $sale->saleId,
+                            'productId' => $sale->productId,
+                            'measure' => cat_Products::getProductInfo($sale->productId)->productRec->measureId,
+                            'shipmentId' => '',
+                            'shipedQuantity' => 0,
+                            'requestQuantity' => $sale->requestQuantity,
+                            'contragentClassId' => $sale->contragentClassId,
+                            'contragentId' => $sale->contragentId
+                        );
+                    }
                 }
             }
         }
-        
+
         return $recs;
     }
-    
-    
+
+
     /**
      * Връща фийлдсета на таблицата, която ще се рендира
      *
      * @param stdClass $rec
      *                         - записа
-     * @param bool     $export
+     * @param bool $export
      *                         - таблицата за експорт ли е
      *
      * @return core_FieldSet - полетата
@@ -317,7 +319,7 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
     protected function getTableFieldSet($rec, $export = false)
     {
         $fld = cls::get('core_FieldSet');
-        
+
         $fld->FLD('saleId', 'varchar', 'caption=Продажба,tdClass=centered');
         $fld->FLD('productId', 'key(mvc=cat_Products,select=name)', 'caption=Артикул');
         $fld->FLD('contragent', 'varchar', 'caption=Контрагент,tdClass=centered');
@@ -328,8 +330,8 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
 
         return $fld;
     }
-    
-    
+
+
     /**
      * Вербализиране на редовете, които ще се показват на текущата страница в отчета
      *
@@ -346,40 +348,40 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
         $Date = cls::get('type_Date');
         $Double = cls::get('type_Double');
         $Double->params['decimals'] = 2;
-        
+
         $row = new stdClass();
-        
+
         $row->saleId = sales_Sales::getLinkToSingle_($dRec->saleId, 'id');
-        
+
         //$row->productId = cat_Products::getLinkToSingle_($dRec->productId, 'name');
-        
+
         $row->productId = cat_Products::getShortHyperlink($dRec->productId);
-        
+
         $contragentClassName = cls::getClassName($dRec->contragentClassId);
-        
+
         $row->contragent = $contragentClassName::getTitleById($dRec->contragentId);
-        
+
         if (isset($dRec->measure)) {
             $row->measure = cat_UoM::fetchField($dRec->measure, 'shortName');
         }
-        
+
         $row->requestQuantity = core_Type::getByName('double(decimals=2)')->toVerbal($dRec->requestQuantity);
-        
+
         $row->shipedQuantity = core_Type::getByName('double(decimals=2)')->toVerbal($dRec->shipedQuantity);
-        
-        $row->quantity = "<span class = 'red'>".'<b>'. core_Type::getByName('double(decimals=2)')->toVerbal($dRec->requestQuantity - $dRec->shipedQuantity).'</b>'.'</span>';
-        
+
+        $row->quantity = "<span class = 'red'>" . '<b>' . core_Type::getByName('double(decimals=2)')->toVerbal($dRec->requestQuantity - $dRec->shipedQuantity) . '</b>' . '</span>';
+
         return $row;
     }
-    
-    
+
+
     /**
      * След рендиране на единичния изглед
      *
      * @param cat_ProductDriver $Driver
-     * @param embed_Manager     $Embedder
-     * @param core_ET           $tpl
-     * @param stdClass          $data
+     * @param embed_Manager $Embedder
+     * @param core_ET $tpl
+     * @param stdClass $data
      */
     protected static function on_AfterRenderSingle(frame2_driver_Proto $Driver, embed_Manager $Embedder, &$tpl, $data)
     {
@@ -393,53 +395,53 @@ class store_reports_UnfulfilledQuantities extends frame2_driver_TableData
                                         <!--ET_BEGIN storeId--><div>|Склад|*: [#storeId#]</div><!--ET_END storeId-->
                                     </div>
                                 </fieldset><!--ET_END BLOCK-->"));
-        
+
 
         if (isset($data->rec->from)) {
             $fieldTpl->append('<b>' . $data->rec->from . '</b>', 'from');
         }
-        
+
         if (isset($data->rec->to)) {
             $fieldTpl->append('<b>' . $data->rec->to . '</b>', 'to');
         }
-        
-        
+
+
         if (isset($data->rec->contragent)) {
             $marker = 0;
             foreach (type_Keylist::toArray($data->rec->contragent) as $contragent) {
                 $marker++;
-                
+
                 $contragentVerb .= (doc_Folders::getTitleById($contragent));
-                
+
                 if ((countR(type_Keylist::toArray($data->rec->contragent))) - $marker != 0) {
                     $contragentVerb .= ', ';
                 }
             }
-            
+
             $fieldTpl->append('<b>' . $contragentVerb . '</b>', 'contragent');
         } else {
             $fieldTpl->append('<b>' . 'Всички' . '</b>', 'contragent');
         }
-        
+
         if (isset($data->rec->group)) {
             $fieldTpl->append('<b>' . cat_Groups::getVerbal($data->rec->group, 'name') . '</b>', 'group');
         }
-        
+
         if (isset($data->rec->storeId)) {
             $fieldTpl->append('<b>' . store_Stores::getTitleById($data->rec->storeId) . '</b>', 'storeId');
         }
-        
+
         $tpl->append($fieldTpl, 'DRIVER_FIELDS');
     }
-    
-    
+
+
     /**
      * След подготовка на реда за експорт
      *
      * @param frame2_driver_Proto $Driver
-     * @param stdClass            $res
-     * @param stdClass            $rec
-     * @param stdClass            $dRec
+     * @param stdClass $res
+     * @param stdClass $rec
+     * @param stdClass $dRec
      */
     protected static function on_AfterGetCsvRec(frame2_driver_Proto $Driver, &$res, $rec, $dRec)
     {
