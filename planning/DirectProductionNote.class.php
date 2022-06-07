2f1b4383c6f6865eaef99896269ad4fd99e2361c
ivelin1012 <ivelin_pdimov@abv.bg>
2022-06-07 17:51:28 +0300

    По-хубаво съобщение

--- planning/DirectProductionNote.class.php
+++ planning/DirectProductionNote.class.php
@@ -1466,7 +1466,7 @@ class planning_DirectProductionNote extends planning_ProductionDocument
     {
         $rec = $mvc->fetchRec($id);
         if (planning_DirectProductNoteDetails::fetchField("#productId = {$rec->productId}")) {
-            core_Statuses::newStatus('Протокола не може да се контира, защото произвеждания артикул е посочен, като материал за влагане|*!', 'error');
+            core_Statuses::newStatus('Протокола не може да се контира, защото произвеждания артикул е в детайла|*!', 'error');
 
             return false;
         }
