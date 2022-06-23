67b13421a2fc5d9263ad4b651d807fdb75d3a9a6
ivelin1012 <ivelin_pdimov@abv.bg>
2022-06-23 13:02:10 +0300

    ПО:опит за бъгфикс

--- planning/ProductionTaskDetails.class.php
+++ planning/ProductionTaskDetails.class.php
@@ -427,10 +427,7 @@ class planning_ProductionTaskDetails extends doc_Detail
                 if($rec->type == 'production'){
                     $productMeasureId = cat_Products::fetchField($rec->productId, 'measureId');
                     $similarMeasures = cat_UoM::getSameTypeMeasures($productMeasureId);
-
-                    if(isset($masterRec->labelPackagingId) && array_key_exists($masterRec->labelPackagingId, $similarMeasures) && $masterRec->measureId != $productMeasureId){
-                        $rec->quantity = cat_UoM::convertValue($rec->quantity, $masterRec->measureId, $productMeasureId);
-                    }
+                    $rec->quantity *= $masterRec->quantityInPack;
                 }
 
                 if($rec->_isKgMeasureId){
