bde1be1fcfa33c554abe172401db32cc15efd094
arsov <arsov@experta.bg>
2022-07-22 18:38:58 +0300

    Параметри да са преди Нормиране
    По искане от Плановиците

--- cat/products/Params.class.php
+++ cat/products/Params.class.php
@@ -680,7 +680,7 @@ class cat_products_Params extends doc_Detail
                 $groupName = cat_Params::getVerbal($paramRec, 'group');
                 $name = "{$groupName}: {$name}";
             }
-            $form->FLD("paramcat{$pId}", 'double', "caption=Параметри за планиране на:|* <b>{$plannedProductName}</b>->|{$name}|*,before=description");
+            $form->FLD("paramcat{$pId}", 'double', "caption=Параметри за планиране на:|* <b>{$plannedProductName}</b>->|{$name}|*,before=indTime");
             $ParamType = cat_Params::getTypeInstance($pId, $class->getClassId(), $objectId);
             $form->setFieldType("paramcat{$pId}", $ParamType);
 
