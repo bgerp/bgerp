4a6e889cba5d75a8e7e33d39d84f04651f9f59a1
arsov <arsov@experta.bg>
2022-08-25 10:52:21 +0300

    bugFix

--- store/reports/ProductsInStock.class.php
+++ store/reports/ProductsInStock.class.php
@@ -549,7 +549,7 @@ class store_reports_ProductsInStock extends frame2_driver_TableData
 
         if (is_numeric($dRec->groupOne)){
 
-            $row->groupOne = cat_Groups::getVerbal($dRec->groupOne, 'name').' : '.'Стойност '.$Double->toVerbal($rec->sumByGroup[$dRec->groupOne]->amount). {acc_Periods::getBaseCurrencyCode($rec->date)}.
+            $row->groupOne = cat_Groups::getVerbal($dRec->groupOne, 'name').' : '.'Стойност '.$Double->toVerbal($rec->sumByGroup[$dRec->groupOne]->amount). acc_Periods::getBaseCurrencyCode($rec->date).
                              '; Количества: ';
 	    $bm==0
             foreach ($rec->sumByGroup['quantities'] as $val){
