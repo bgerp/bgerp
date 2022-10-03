d7d6872dbdd49280be2e3bb75845d3abf2771bbc
angel <angel.trifonoff@gmail.com>
2022-10-03 18:30:25 +0300

    Промнена дата на datetime

--- sales/reports/PricesOfNonstandardProducts.class.php
+++ sales/reports/PricesOfNonstandardProducts.class.php
@@ -35,19 +35,19 @@ class sales_reports_PricesOfNonstandardProducts extends frame2_driver_TableData
      *
      * @var string
      */
-    protected $newFieldsToCheck ;
+    protected $newFieldsToCheck;
 
 
     /**
      * По-кое поле да се групират листовите данни
      */
-    protected $groupByField ;
+    protected $groupByField;
 
 
     /**
      * Кои полета може да се променят от потребител споделен към справката, но нямащ права за нея
      */
-    protected $changeableFields ;
+    protected $changeableFields;
 
 
     /**
@@ -70,14 +70,13 @@ class sales_reports_PricesOfNonstandardProducts extends frame2_driver_TableData
      *
      * @param frame2_driver_Proto $Driver
      *                                      $Driver
-     * @param embed_Manager       $Embedder
-     * @param stdClass            $data
+     * @param embed_Manager $Embedder
+     * @param stdClass $data
      */
     protected static function on_AfterPrepareEditForm(frame2_driver_Proto $Driver, embed_Manager $Embedder, &$data)
     {
         $form = $data->form;
         $rec = $form->rec;
-
 
 
     }
@@ -113,26 +112,26 @@ class sales_reports_PricesOfNonstandardProducts extends frame2_driver_TableData
      */
     protected function prepareRecs($rec, &$data = null)
     {
-        $recs  = array();
+        $recs = array();
 
         $fRec = fileman_Files::fetchByFh($rec->products);
         expect($fRec, 'Липсва файл за импортиране');
 
         $path = fileman_Data::getGoodFilePath($fRec->dataId);
 
-        $prodArr =  file($path);
+        $prodArr = file($path);
 
 
         //Ако няма артикули в папката на контрагента връща празен масив
 
         if (!$prodArr) return $recs;
 
-        foreach ($prodArr as $product){
+        foreach ($prodArr as $product) {
 
-        if ($product == '')continue;
+            if ($product == '') continue;
 
-        $productArr = explode(',',$product);
-        $code =  ltrim($productArr[0], '#');
+            $productArr = explode(',', $product);
+            $code = ltrim($productArr[0], '#');
 
 
             //Артикул
@@ -140,45 +139,45 @@ class sales_reports_PricesOfNonstandardProducts extends frame2_driver_TableData
 
             $prodRec = cat_Products::fetch($productId);
 
-            $quantity =$productArr[1];
+            $quantity = $productArr[1];
 
 
             //Намиране на цената за съответното количество
             $contragent = doc_Folders::fetch($rec->folderId);
 
-            $date = dt::today();
+            $datetime = dt::now();
 
             $Policy = cls::get('price_ListToCustomers');
 
-            $listId = price_ListToCustomers::getListForCustomer($contragent->coverClass,$contragent->coverId,$date);
+            $listId = price_ListToCustomers::getListForCustomer($contragent->coverClass, $contragent->coverId, $datetime);
 
-            $currencyCode  = currency_Currencies::getCodeById($rec->currencyId);
+            $currencyCode = currency_Currencies::getCodeById($rec->currencyId);
 
-            $currencyRate = currency_CurrencyRates::getRate(dt::today(),$currencyCode,null);
+            $currencyRate = currency_CurrencyRates::getRate(dt::today(), $currencyCode, null);
 
-            $policyInfo = $Policy->getPriceInfo($contragent->coverClass, $contragent->coverId, $productId, null, $quantity,null, $currencyRate,null, $listId);
+            $policyInfo = $Policy->getPriceInfo($contragent->coverClass, $contragent->coverId, $productId, null, $quantity, null, $currencyRate, null, $listId);
 
-            $defoltTransport = sales_TransportValues::calcDefaultTransportToClient($productId,$quantity,$contragent->coverClass,$contragent->coverId);
+            $defoltTransport = sales_TransportValues::calcDefaultTransportToClient($productId, $quantity, $contragent->coverClass, $contragent->coverId);
 
             $price = $policyInfo->price + $defoltTransport['singleFee'];
 
-            $id = $productId.'|'.$quantity;
+            $id = $productId . '|' . $quantity;
 
-          // добавяме в масива
-          if (!array_key_exists($id, $recs)) {
-              $recs[$id] = (object)array(
+            // добавяме в масива
+            if (!array_key_exists($id, $recs)) {
+                $recs[$id] = (object)array(
 
-                  'id' => $id,
-                  'product' => $productId,
-                  'prodName' => $prodRec->name,
-                  'measureId' => $prodRec->measureId,
-                  'quantity'=> $quantity,
-                  'price'=> $price,
-              );
-          }
+                    'id' => $id,
+                    'product' => $productId,
+                    'prodName' => $prodRec->name,
+                    'measureId' => $prodRec->measureId,
+                    'quantity' => $quantity,
+                    'price' => $price,
+                );
+            }
 
         }
-//bp($recs);
+
         return $recs;
     }
 
@@ -188,7 +187,7 @@ class sales_reports_PricesOfNonstandardProducts extends frame2_driver_TableData
      *
      * @param stdClass $rec
      *                         - записа
-     * @param bool     $export
+     * @param bool $export
      *                         - таблицата за експорт ли е
      *
      * @return core_FieldSet - полетата
@@ -202,7 +201,7 @@ class sales_reports_PricesOfNonstandardProducts extends frame2_driver_TableData
             $fld->FLD('measureId', 'key(mvc=cat_UoM,select=name)', 'caption=Мярка,tdClass=centered');
             $fld->FLD('quantity', 'double(smartRound,decimals=2)', 'caption=Количество,smartCenter');
             $fld->FLD('price', 'double(decimals=2)', 'caption=Цена,smartCenter');
-        }else{
+        } else {
             $fld->FLD('product', 'key(mvc=cat_Products,select=name)', 'caption=Артикул,tdClass=productCell leftCol wrap');
             $fld->FLD('measureId', 'key(mvc=cat_UoM,select=name)', 'caption=Мярка,tdClass=centered');
             $fld->FLD('quantity', 'double(smartRound,decimals=2)', 'caption=Количество,smartCenter');
@@ -217,7 +216,7 @@ class sales_reports_PricesOfNonstandardProducts extends frame2_driver_TableData
     /**
      * Вербализиране на редовете, които ще се показват на текущата страница в отчета
      *
-     * @param stdClass $rec  - записа
+     * @param stdClass $rec - записа
      * @param stdClass $dRec - чистия запис
      *
      * @return stdClass $row - вербалния запис
@@ -248,11 +247,11 @@ class sales_reports_PricesOfNonstandardProducts extends frame2_driver_TableData
     /**
      * След подготовка на реда за експорт
      *
-     * @param frame2_driver_Proto $Driver      - драйвер
-     * @param stdClass            $res         - резултатен запис
-     * @param stdClass            $rec         - запис на справката
-     * @param stdClass            $dRec        - запис на реда
-     * @param core_BaseClass      $ExportClass - клас за експорт (@see export_ExportTypeIntf)
+     * @param frame2_driver_Proto $Driver - драйвер
+     * @param stdClass $res - резултатен запис
+     * @param stdClass $rec - запис на справката
+     * @param stdClass $dRec - запис на реда
+     * @param core_BaseClass $ExportClass - клас за експорт (@see export_ExportTypeIntf)
      */
     protected static function on_AfterGetExportRec(frame2_driver_Proto $Driver, &$res, $rec, $dRec, $ExportClass)
     {
@@ -281,7 +280,6 @@ class sales_reports_PricesOfNonstandardProducts extends frame2_driver_TableData
                                  </fieldset><!--ET_END BLOCK-->"));
 
 
-
         if (isset($data->rec->currencyId)) {
             $fieldTpl->append('<b>' . currency_Currencies::fetch($data->rec->currencyId)->code . '</b>', 'currencyId');
         }
@@ -294,123 +292,5 @@ class sales_reports_PricesOfNonstandardProducts extends frame2_driver_TableData
         $tpl->append($fieldTpl, 'DRIVER_FIELDS');
     }
 
-    /**
-     * Промяна на стойностите min и max
-     *
-     */
-    public function act_EditQuantity()
-    {
 
-        expect($recId = Request::get('recId', 'int'));
-        expect($productId = Request::get('productId', 'int'));
-        expect($id = Request::get('rowId'));
-
-        $rec = frame2_Reports::fetch($recId);
-
-        $details = $rec->prodQuantities;
-
-
-if ($details){
-
-    $quantity = $details[$id]['quantity'];
-    $price = $details[$id]['price'];
-
-}else{
-    $quantity = 0;
-    $price = 0;
-}
-
-        $form = cls::get('core_Form');
-
-        $form->title = "Редактиране на  |* ' " . ' ' . cat_Products::getHyperlink($productId) . "' ||*";
-
-        $volOldquantity = $quantity;
-        $volOldprice = $price;
-
-        $form->FLD('volNewQuantity', 'double', 'caption=Въведи количество,input,silent');
-
-        $form->FLD('volNewPrice', 'double', 'caption=Въведи max,input=none,silent');
-
-        $form->setDefault('volNewQuantity', $volOldquantity);
-        $form->setDefault('volNewPrice', $volOldprice);
-
-        $mRec = $form->input();
-
-        $form->toolbar->addSbBtn('Запис', 'save', 'ef_icon = img/16/disk.png');
-
-        $form->toolbar->addBtn('Отказ', getRetUrl(), 'ef_icon = img/16/close-red.png');
-
-        if ($form->isSubmitted()) {
-
-            //Намиране на цената за съответното количество
-            $contragent = doc_Folders::fetch($rec->folderId);
-
-            $date = dt::today();
-
-            $Policy = cls::get('price_ListToCustomers');
-
-            $listId = price_ListToCustomers::getListForCustomer($contragent->coverClass,$contragent->coverId,$date);
-
-            $policyInfo = $Policy->getPriceInfo($contragent->coverClass, $contragent->coverId, $productId, null, $form->rec->volNewQuantity,null, 1,null, $listId);
-            $defoltTransport = sales_TransportValues::calcDefaultTransportToClient($productId,$form->rec->volNewQuantity,$contragent->coverClass,$contragent->coverId);
-
-            $volNewPrice = $policyInfo->price + $defoltTransport['singleFee'];
-
-            $form->rec->volNewPrice = $volNewPrice;
-
-            $idNew = $productId.'|'.$form->rec->volNewQuantity;
-
-
-            $details[$idNew]['quantity'] = $mRec->volNewQuantity;
-            $details[$idNew]['price'] = $mRec->$volNewPrice;
-
-            $rec->prodQuantities = $details;
-
-            $oldRowRec = $rec->data->recs[$id];
-
-            $oldRowRec->id = $idNew;
-            $oldRowRec->quantity = $form->rec->volNewQuantity;
-            $oldRowRec->price = $form->rec->volNewPrice;
-
-
-
-            $rec->data->recs[$idNew] = $oldRowRec;
-
-
-
-            if (array_key_exists($id,$details)){
-                unset($details[$id]);
-            }
-
-            unset($rec->data->recs[$id]);
-
-
-
-            frame2_Reports::save($rec);
-
-
-            frame2_Reports::refresh($rec);
-
-            return new Redirect(getRetUrl());
-        }
-
-        return $form->renderHtml();
-
-
-    }
-
-    /**
-     * Кои полета да са скрити във вътрешното показване
-     *
-     * @param core_Master $mvc
-     * @param NULL|array  $res
-     * @param object      $rec
-     * @param object      $row
-     */
-    public static function on_AfterGetHideArrForLetterHead(frame2_driver_Proto $Driver, embed_Manager $Embedd, &$res, $rec, $row)
-    {
-        $res = arr::make($res);
-
-      //  $res['external']['selfPriceTolerance'] = true;
-    }
 }
