4c8d6339536ea7c968d35d681d02a0fb753beb0b
Yuseinov <yuseinov@experta.bg>
2022-10-14 11:33:44 +0300

    white-list-extensions b#C4348268 - добавен ppf

--- fileman/data/white-list-extensions.inc.php
+++ fileman/data/white-list-extensions.inc.php
@@ -9,4 +9,4 @@ const WHITE_LIST_EXTENSIONS = array('pdf', 'dwg', 'dxf', 'doc', 'docx', 'ods', '
                                     'xls', 'xlsm', 'xlsx', 'xlt', 'xlw', 'aai', 'art', 'bmp',
                                     'jpg', 'png', 'ogv', 'mp4', 'webm', 'flv', 'wav', 'mp3',
                                     'oga', 'ogg', 'aac', 'txt', '7z', 'gz', 'gzip', 'rar',
-                                    'tar', 'tgz', 'zip', 'dae', 'csv', 'svg', 'jpeg');
+                                    'tar', 'tgz', 'zip', 'dae', 'csv', 'svg', 'jpeg', 'ppf');
