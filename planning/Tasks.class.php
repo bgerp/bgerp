6d354984a89fb000077981ee0ceced12335a04da
ivelin1012 <ivelin_pdimov@abv.bg>
2022-10-25 14:29:38 +0300

    ПО:бъгфикс

--- planning/Tasks.class.php
+++ planning/Tasks.class.php
@@ -883,8 +883,8 @@ class planning_Tasks extends core_Master
         unset($resArr['createdBy']);
         unset($resArr['createdOn']);
 
-        $display = (in_array($rec->state, array('pending', 'draft', 'waiting', 'rejected', 'stopped')) && haveRole('task')) ? 'block' : 'none';
-        $toggleClass = (in_array($rec->state, array('pending', 'draft', 'waiting', 'rejected', 'stopped')) && haveRole('task')) ? 'show-btn' : '';
+        $display = (in_array($rec->state, array('pending', 'draft', 'waiting', 'rejected', 'stopped')) || haveRole('task')) ? 'block' : 'none';
+        $toggleClass = (in_array($rec->state, array('pending', 'draft', 'waiting', 'rejected', 'stopped')) || haveRole('task')) ? 'show-btn' : '';
 
         if(Mode::is('printing')){
             $display = true;
