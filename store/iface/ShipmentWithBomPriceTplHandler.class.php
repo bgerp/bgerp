a64bd6c4ebaffb6d71941e4e222f605eb1a9c74d
ivelin1012 <ivelin_pdimov@abv.bg>
2022-10-26 14:56:13 +0300

    Обработвач за сб на цени с рецепта

--- store/iface/ShipmentWithBomPriceTplHandler.class.php
+++ store/iface/ShipmentWithBomPriceTplHandler.class.php
@@ -63,31 +63,31 @@ class store_iface_ShipmentWithBomPriceTplHandler extends doc_TplScript
 
 
     /**
-     * Преди рендиране на шаблона на детайла
+     * Метод който подава данните на детайла на мастъра, за обработка на скрипта
      *
-     * @param core_Mvc $detail
-     * @param core_ET  $tpl
-     * @param stdClass $data
+     * @param core_Mvc $detail - Детайл на документа
+     * @param stdClass $data   - данни
+     *
+     * @return void
      */
-    public function beforeRenderListTable(core_Mvc $detail, &$tpl, &$data)
+    public function modifyDetailData(core_Mvc $detail, &$data)
     {
         // Ако няма редове или не е във вътрешен режим
         if (!countR($data->rows)) return;
         if(Mode::is('printing') || (Mode::is('text', 'xhtml') && !Mode::is('docView'))) return;
 
-        // Добавяне на колонката за цена по рецепта
-        $data->listTableMvc->FNC('_amountBom', 'double(smartRound)');
-        if( array_key_exists('amount', $data->listFields)){
-            arr::placeInAssocArray($data->listFields, '_amountBom=Рецепта', 'amount');
-        } else {
-            arr::placeInAssocArray($data->listFields, '_amountBom=Рецепта', null, 'packQuantity');
-        }
-
         $date = isset($data->masterData->rec->valior) ? $data->masterData->rec->valior : dt::today();
 
         // За всеки запис
         foreach ($data->rows as $id => &$row) {
+            $Double = core_Type::getByName('double(smartRound)');
             $rec = $data->recs[$id];
+
+            $rec->_amountBase = currency_CurrencyRates::convertAmount($rec->price * $rec->quantity, $date, null, $baseCurrencyId);
+            $row->_amountBase = $Double->toVerbal(core_Math::roundNumber($rec->_amountBase));
+            if(isset($rec->discount)){
+                $rec->_amountBase *= 1 - $rec->discount;
+            }
 
             // Ако е производим
             $canManifacture = cat_Products::fetchField($rec->productId, 'canManifacture');
@@ -102,9 +102,9 @@ class store_iface_ShipmentWithBomPriceTplHandler extends doc_TplScript
                     $rec->_amountBom = $cachedRec->amount;
                     $hint = null;
                     if(isset($cachedRec->price)){
-                        $hintPrice = $data->listTableMvc->getFieldType('_amountBom')->toVerbal(core_Math::roundNumber($cachedRec->price * $rec->quantityInPack));
+                        $hintPrice = $Double->toVerbal(core_Math::roundNumber($cachedRec->price * $rec->quantityInPack));
                         $hint = "ед. сб-ст|*: {$hintPrice}. ";
-                        $row->_amountBom = $data->listTableMvc->getFieldType('_amountBom')->toVerbal(core_Math::roundNumber($cachedRec->amount));
+                        $row->_amountBom = $Double->toVerbal(core_Math::roundNumber($cachedRec->amount));
                     } else {
                         $row->_amountBom = "<span class='red'>???</span>";
                     }
@@ -127,6 +127,61 @@ class store_iface_ShipmentWithBomPriceTplHandler extends doc_TplScript
                 }
             }
         }
+
+    }
+
+
+    /**
+     * Преди рендиране на шаблона на детайла
+     *
+     * @param core_Mvc $detail
+     * @param core_ET  $tpl
+     * @param stdClass $data
+     */
+    public function beforeRenderListTable(core_Mvc $detail, &$tpl, &$data)
+    {
+        // Ако няма редове или не е във вътрешен режим
+        if (!countR($data->rows)) return;
+        if(Mode::is('printing') || (Mode::is('text', 'xhtml') && !Mode::is('docView'))) return;
+
+        // Добавяне на колонката за цена по рецепта
+        $date = isset($data->masterData->rec->valior) ? $data->masterData->rec->valior : dt::today();
+        $baseCurrencyId = acc_Periods::getBaseCurrencyCode($date);
+        $data->listTableMvc->FNC('_amountBom', 'double(smartRound)');
+        if( array_key_exists('amount', $data->listFields)){
+            arr::placeInAssocArray($data->listFields, array('_amountBom' => "Рецепта||Bom|* <span class='quiet small'>({$baseCurrencyId})</span>"), 'amount');
+        } else {
+            arr::placeInAssocArray($data->listFields, array('_amountBom' => "Рецепта||Bom|* <span class='quiet small'>({$baseCurrencyId})</span>"), null, 'packQuantity');
+        }
+
+        if($data->masterData->rec->currencyId != $baseCurrencyId){
+            arr::placeInAssocArray($data->listFields, array('_amountBase' => "Сума||Amount|* <span class='quiet small'>({$baseCurrencyId})</span>"), null, '_amountBom');
+        }
+    }
+
+
+    /**
+     * Метод който подава данните на мастъра за обработка на скрипта
+     *
+     * @param core_Mvc $mvc  - мастър на документа
+     * @param stdClass $data - данни
+     *
+     * @return void
+     */
+    public function modifyMasterData(core_Mvc $mvc, &$data)
+    {
+        // Ако няма редове или не е във вътрешен режим
+        if (!countR($data->store_ShipmentOrderDetails->recs)) return;
+        if(Mode::is('printing') || (Mode::is('text', 'xhtml') && !Mode::is('docView'))) return;
+
+        $amountBoms = arr::sumValuesArray($data->store_ShipmentOrderDetails->recs, '_amountBom');
+        $amountBase = arr::sumValuesArray($data->store_ShipmentOrderDetails->recs, '_amountBase');
+
+        $date = isset($data->masterData->rec->valior) ? $data->masterData->rec->valior : dt::today();
+        $baseCurrencyId = acc_Periods::getBaseCurrencyCode($date);
+        $data->row->totalAmountBom = core_Type::getByName('double(decimals=2)')->toVerbal($amountBoms);
+        $data->row->totalAmountBase = core_Type::getByName('double(decimals=2)')->toVerbal($amountBase);
+        $data->row->bomCurrencyId = $data->row->baseCurrencyId = $baseCurrencyId;
     }
 
 
