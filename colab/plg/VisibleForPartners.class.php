fd3ffc99aaa80379d190d802663faa411afbdfcf
Yuseinov <yuseinov@experta.bg>
2022-12-13 17:10:30 +0200

    plg_VisibleForPartners #Tsk5596 - автоматично споделяне на колаборатори, при изпиване с @

--- colab/plg/VisibleForPartners.class.php
+++ colab/plg/VisibleForPartners.class.php
@@ -16,6 +16,8 @@
  */
 class colab_plg_VisibleForPartners extends core_Plugin
 {
+
+
     /**
      * След дефиниране на полетата на модела
      *
@@ -85,6 +87,82 @@ class colab_plg_VisibleForPartners extends core_Plugin
         if (core_Users::haveRole('partner')) {
             $mvc->currentTab = 'Нишка';
             plg_ProtoWrapper::changeWrapper($mvc, 'cms_ExternalWrapper');
+        }
+    }
+
+
+    /**
+     *
+     * @param core_Mvc $mvc
+     * @param core_Form $form
+     */
+    public static function on_AfterInputEditForm($mvc, &$form)
+    {
+        // Ако е споделен към колаборатор, който има достъп до папката, да може да вижда документа
+        if ($form->rec->visibleForPartners == 'no') {
+            if ($form->isSubmitted()) {
+                $allSharedUsersArr = array();
+
+                $rec = &$form->rec;
+
+                $sharedUsersArrAll = array();
+
+                $fName = '';
+
+                // Обхождаме всички полета от модела, за да разберем кои са ричтекст
+                foreach ((array) $mvc->fields as $name => $field) {
+                    if ($field->type instanceof type_Richtext) {
+                        if ($field->type->params['nickToLink'] == 'no') {
+                            continue;
+                        }
+
+                        // Вземаме споделените потребители
+                        $sharedUsersArr = rtac_Plugin::getNicksArr($rec->$name);
+                        if (empty($sharedUsersArr)) {
+                            continue;
+                        }
+
+                        // Обединяваме всички потребители от споделянията
+                        $sharedUsersArrAll = array_merge($sharedUsersArrAll, $sharedUsersArr);
+                        if (!empty($sharedUsersArrAll)) {
+                            $fName = $fName? $fName : $field->name;
+                        }
+                    }
+                }
+
+                // Ако има споделяния
+                if (!empty($sharedUsersArrAll)) {
+                    // Добавяме id-тата на споделените потребители
+                    foreach ((array) $sharedUsersArrAll as $nick) {
+                        $nick = strtolower($nick);
+                        $id = core_Users::fetchField(array("LOWER(#nick) = '[#1#]'", $nick), 'id');
+
+                        if (!core_Users::haveRole('partner', $id)) {
+                            continue;
+                        }
+
+                        $allSharedUsersArr[$id] = $id;
+                    }
+                }
+
+                $errArray = array();
+
+                if (!empty($allSharedUsersArr)) {
+                    foreach ($allSharedUsersArr as $uId) {
+                        $cRec = colab_FolderToPartners::fetchField(array("#folderId = '[#1#]' AND #contractorId = '[#2#]'", $form->rec->folderId, $uId));
+
+                        if (!$cRec) {
+                            $errArray[$uId] = core_Users::getNick($uId);
+                        }
+                    }
+
+                    if (!empty($errArray)) {
+                        $form->setError($fName, '|Документът не може да бъде споделен към|*: ' . implode(', ', $errArray) . '<br>|*Контракторът не е споделен към папката');
+                    } else {
+                        $form->rec->visibleForPartners = 'yes';
+                    }
+                }
+            }
         }
     }
     
