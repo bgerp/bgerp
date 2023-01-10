9d5393554a297f5eb1a11a0da0e408e870e1fcac
Yuseinov <yuseinov@experta.bg>
2023-01-10 14:31:12 +0200

    fileman_Data #C54384 - добавен индекс

--- fileman/Data.class.php
+++ fileman/Data.class.php
@@ -92,6 +92,7 @@ class fileman_Data extends core_Manager
         
         $this->FLD('processed', 'enum(no,yes)', 'caption=Извличане на ключови думи,column=none,single=none,input=none');
         
+        $this->setDbIndex('processed');
         $this->setDbUnique('fileLen,md5', 'DNA');
     }
     
