b52a95e4fb353fd93b3ba5915c87fde7e94bd4ea
Yuseinov <yuseinov@experta.bg>
2023-01-18 15:35:53 +0200

    fix

--- fileman/webdrv/Xlsb.class.php
+++ fileman/webdrv/Xlsb.class.php
@@ -8,7 +8,7 @@
  * @package   fileman
  *
  * @author    Yusein Yuseinov <yyuseinov@gmail.com>
- * @copyright 2006 - 2012 Experta OOD
+ * @copyright 2006 - 2023 Experta OOD
  * @license   GPL 3
  *
  * @since     v 0.1
