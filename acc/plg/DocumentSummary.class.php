a21a5ce5ecbb7f6e2e6d98772989e945c66813d1
Yuseinov <yuseinov@experta.bg>
2023-01-26 12:59:01 +0200

    acc/plg/DocumentSummary #C54858 - оправено

--- acc/plg/DocumentSummary.class.php
+++ acc/plg/DocumentSummary.class.php
@@ -279,10 +279,11 @@ class acc_plg_DocumentSummary extends core_Plugin
             $data->listFilter->FNC('folder', 'key2(mvc=doc_Folders,allowEmpty)', 'caption=Папка,silent,after=users');
             $data->listFilter->showFields .= ',folder';
             
-            $cKey = $mvc->className . core_Users::getCurrent();
+            $cKey = $mvc->className . '|' . core_Users::getCurrent();
             $haveUsers = false;
             
             if ($lastUsers = core_Permanent::get('userFilter' . $cKey)) {
+                $usedUsers = $lastUsers;
                 $type = $data->listFilter->getField('users')->type;
                 $type->prepareOptions();
                 foreach ($type->options as $key => $optObj) {
