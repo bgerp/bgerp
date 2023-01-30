a8c4009e4eda341098fc6a41f4cd7746dd303a3d
ivelin1012 <ivelin_pdimov@abv.bg>
2023-01-30 16:03:19 +0200

    ПО:production и consumption да могат да

--- planning/Tasks.class.php
+++ planning/Tasks.class.php
@@ -3400,7 +3400,7 @@ class planning_Tasks extends core_Master
                 $productionCount = planning_ProductionTaskProducts::count("#type = 'production' AND #taskId = {$taskId}");
                 $allowedCount = ($masterRec->isFinal == 'yes') ? 1 : 0;
                 if($productionCount != $allowedCount){
-                    if(!haveRole('taskPostProduction,ceo', $userId)){
+                    if(!haveRole('taskPostProduction,ceo,production,consumption', $userId)){
                         return false;
                     }
                 } else {
@@ -3409,7 +3409,7 @@ class planning_Tasks extends core_Master
             }
 
             // Ако е преди първия хоризонт се изисква роля за пост продукция
-        } elseif(!haveRole('taskPostProduction,ceo', $userId)){
+        } elseif(!haveRole('taskPostProduction,ceo,production,consumption', $userId)){
             return false;
         }
 
