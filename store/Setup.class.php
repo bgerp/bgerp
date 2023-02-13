20079fcfbd0ae7e192b964d2804f4a95209953c6
ivelin1012 <ivelin_pdimov@abv.bg>
2023-02-13 15:57:52 +0200

    Склад: инвалидиране на миграцията

--- store/Setup.class.php
+++ store/Setup.class.php
@@ -105,7 +105,7 @@ class store_Setup extends core_ProtoSetup
         'store_InventoryNoteSummary',
         'store_InventoryNoteDetails',
         'store_StockPlanning',
-        'migrate::updateShipmentNegativeRoles2313',
+        'migrate::updateShipmentNegativeRoles23131',
     );
     
     
@@ -250,11 +250,11 @@ class store_Setup extends core_ProtoSetup
     /**
      * Миграция на ролите за изписване от склада на минус
      */
-    public function updateShipmentNegativeRoles2313()
+    public function updateShipmentNegativeRoles23131()
     {
         $config = core_Packs::getConfig('store');
         if(isset($config->_data['STORE_ALLOW_NEGATIVE_SHIPMENT'])){
-            if($config->_data['STORE_ALLOW_NEGATIVE_SHIPMENT'] == 'yes' || empty($config->_data['STORE_ALLOW_NEGATIVE_SHIPMENT'])){
+            if($config->_data['STORE_ALLOW_NEGATIVE_SHIPMENT'] !== 'no'){
                 core_Packs::setConfig('store', array('STORE_ALLOW_NEGATIVE_SHIPMENT_ROLES' => core_Roles::getRolesAsKeylist('powerUser')));
             }
         }
