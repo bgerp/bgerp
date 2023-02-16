b44488760b2a16c531ba8b674873c92be76e4d51
ivelin1012 <ivelin_pdimov@abv.bg>
2023-02-16 12:01:32 +0200

    Хинт за запазващите к-ва документи да има линк към всички

--- store/Products.class.php
+++ store/Products.class.php
@@ -94,6 +94,12 @@ class store_Products extends core_Detail
 
 
     /**
+     * По колко максимум документа да се показват в хинта за запазващите документи
+     */
+    const SHOW_DOCUMENTS_IN_PLANNED_STOCK_HINT = 15;
+
+
+    /**
      * Описание на модела (таблицата)
      */
     public function description()
@@ -761,6 +767,8 @@ class store_Products extends core_Detail
         $toDate = Request::get('date', 'date');
         $today = dt::today();
         $recs = store_StockPlanning::getRecs($productId, $stores, $toDate, $field);
+        $totalCount = countR($recs);
+        $recs = array_splice($recs, 0, static::SHOW_DOCUMENTS_IN_PLANNED_STOCK_HINT);
 
         $links = '';
         foreach($recs as $dRec){
@@ -797,6 +805,12 @@ class store_Products extends core_Detail
             $link = new core_ET("<div style='float:left;padding-bottom:2px;padding-top: 2px;'>[#link#]<!--ET_BEGIN date--> | [#date#]<!--ET_END date-->| [#createdBy#]</div>");
             $link->placeObject($row);
             $links .= $link->getContent();
+        }
+
+        if($totalCount > static::SHOW_DOCUMENTS_IN_PLANNED_STOCK_HINT){
+            $storeId = (countR($stores) == 1) ? key($stores) : null;
+            $linkToFilter = ht::createLink(tr('Още|*....'), array('store_StockPlanning', 'list', 'storeId' => $storeId, 'productId' => $productId))->getContent();
+            $links .= "<div style='float:left;padding-bottom:2px;padding-top: 2px;'>{$linkToFilter}</div>";
         }
 
         $tpl = new core_ET($links);
