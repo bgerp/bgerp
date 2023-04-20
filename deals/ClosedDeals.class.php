211b2b9d8d127422f6f8819b2836cc245b4dedf2
ivelin1012 <ivelin_pdimov@abv.bg>
2023-04-20 15:47:53 +0300

    Revert "Приклюване на сделки: при търсене на най-голям вальор в сделката да пропускам курсовите разлики"
    This reverts commit aef98e2dc7d52d1797b81e737e09fe580aedea2f.

--- deals/ClosedDeals.class.php
+++ deals/ClosedDeals.class.php
@@ -707,12 +707,9 @@ abstract class deals_ClosedDeals extends core_Master
      */
     protected function getBiggestValiorInDeal($rec)
     {
-        // Намира се най-големия вальор от документите свързани към сделката (с изключение на корекцията на курсови разлики)
+        // Намира се най-големия вальор от документите свързани към сделката
         $firstDoc =  doc_Threads::getFirstDocument($rec->threadId);
         $jRecs = acc_Journal::getEntries(array($firstDoc->className, $firstDoc->that));
-        $skipClassId = acc_RatesDifferences::getClassId();
-        $jRecs = array_filter($jRecs, function($a) use ($skipClassId) {return $a->docType != $skipClassId;});
-
         $valiors = arr::extractValuesFromArray($jRecs, 'valior');
         if($firstDocValior = $firstDoc->fetchField($firstDoc->valiorFld)){
             $valiors[$firstDocValior] = $firstDocValior;
