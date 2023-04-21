82dda7ff9fc75d66f80f2339f5578cf19929ac02
arsov <arsov@experta.bg>
2023-04-21 14:57:29 +0300

    "Запис и Нов" при добавяне на Заместващи Артикули

--- planning/GenericMapper.class.php
+++ planning/GenericMapper.class.php
@@ -25,7 +25,7 @@ class planning_GenericMapper extends core_Manager
     /**
      * Плъгини за зареждане
      */
-    public $loadList = 'plg_RowTools2, plg_Created, planning_Wrapper';
+    public $loadList = 'plg_RowTools2, plg_Created, planning_Wrapper, plg_SaveAndNew';
     
     
     /**
@@ -107,6 +107,10 @@ class planning_GenericMapper extends core_Manager
         }
         
         $data->form->title = core_Detail::getEditTitle('cat_Products', $productId, $mvc->singleTitle, $rec->id);
+		
+		if (empty($rec->genericProductId)) {
+				$data->form->toolbar->removeBtn('saveAndNew');
+			}
     }
     
     
