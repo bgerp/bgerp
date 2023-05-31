f490f48feb96dffe5b6dc8402cdda36ad0edc10a
ivelin1012 <ivelin_pdimov@abv.bg>
2023-05-31 13:15:47 +0300

    откоментиран погрешно закоментиран код

--- cat/products/Params.class.php
+++ cat/products/Params.class.php
@@ -469,7 +469,7 @@ class cat_products_Params extends doc_Detail
                         $requiredRoles = 'catEdit,ceo';
                     }
                 } elseif ($rec->classId == cat_BomDetails::getClassId()) {
-                    //$requiredRoles = cat_BomDetails::getRequiredRoles($action, $rec->productId, $userId);
+                    $requiredRoles = cat_BomDetails::getRequiredRoles($action, $rec->productId, $userId);
                 }
             }
         }
@@ -481,16 +481,16 @@ class cat_products_Params extends doc_Detail
                 if ($action == 'add') {
                     $InnerClass = cls::get($pRec->innerClass);
                     if (!($InnerClass instanceof cat_GeneralProductDriver)) {
-                       // $requiredRoles = 'no_one';
+                        $requiredRoles = 'no_one';
                     }
                 }
 
                 if ($pRec->state != 'active' && $pRec->state != 'draft' && $pRec->state != 'template') {
-                   // $requiredRoles = 'no_one';
+                    $requiredRoles = 'no_one';
                 }
 
                 if (!$Class->haveRightFor('single', $rec->productId)) {
-                    //$requiredRoles = 'no_one';
+                    $requiredRoles = 'no_one';
                 }
             }
         }
@@ -498,8 +498,8 @@ class cat_products_Params extends doc_Detail
         // Ако има указани роли за параметъра, потребителя трябва да ги има за редакция/изтриване
         if (($action == 'edit' || $action == 'delete') && $requiredRoles != 'no_one' && isset($rec)) {
             $pRec = cat_Params::fetch($rec->paramId, 'roles,valueType');
-            if (!empty($roles) && !haveRole($roles, $userId)) {
-                //$requiredRoles = 'no_one';
+            if (!empty($pRec->roles) && !haveRole($pRec->roles, $userId)) {
+                $requiredRoles = 'no_one';
             }
         }
     }
