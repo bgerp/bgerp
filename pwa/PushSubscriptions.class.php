25a838cebf5f630fbba71e93396b38ad706b21e6
Yuseinov <yuseinov@experta.bg>
2023-06-21 17:22:24 +0300

    pwa - подобрение в дебъга

--- pwa/PushSubscriptions.class.php
+++ pwa/PushSubscriptions.class.php
@@ -245,9 +245,9 @@ class pwa_PushSubscriptions extends core_Manager
             $resArr[$rec->id] =  $statusData;
 
             if (!$statusData->isSuccess) {
-                self::logDebug("Грешка при изпращане на PUSH известие: {$reason}", $rec->id, 7);
+                self::logDebug("Грешка при изпращане на PUSH известие - '{$reason}'", $rec->id, 7);
             } else {
-                self::logDebug("Успешно изпратено PUSH известие - {$data->text}", $rec->id, 3);
+                self::logDebug("Успешно изпратено PUSH известие - '{$data->text}'", $rec->id, 3);
             }
         }
 
@@ -522,7 +522,7 @@ class pwa_PushSubscriptions extends core_Manager
                         // Проверяваме дали преди това има изпратено известие
                         $showUrlHash = md5($msgObj->url . '|' . $userId . '|' . $brid);
                         if (core_Permanent::get($showUrlHash)) {
-                            self::logDebug("Прескочено изпращане на PUSH известие поради дублиране на URL: {$msgObj->url}", $uRec->id, 7);
+                            self::logDebug("Прескочено изпращане на PUSH известие поради дублиране на URL - '{$msgObj->url}'", $uRec->id, 7);
 
                             continue;
                         }
@@ -637,7 +637,7 @@ class pwa_PushSubscriptions extends core_Manager
                                 $lifetime = 24 * 60; // 24 часа за повторно изпращане, ако няма грешка
                             }
 
-                            self::logDebug("{$resStatusMsg} изпращане на известие: '{$msgTitle}': '{$msg}'", $uRec->id, 7);
+                            self::logDebug("{$resStatusMsg} изпращане на известие - '{$msgTitle}': '{$msg}'", $uRec->id, 7);
                         }
 
                         core_Permanent::set($showUrlHash, 1, $lifetime);
