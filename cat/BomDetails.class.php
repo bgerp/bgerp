4a495f9d569eabde822c6870194e7d47f64053c5
ivelin1012 <ivelin_pdimov@abv.bg>
2023-07-10 14:00:09 +0300

    Рецепта: втори бутон за свиване/разпъване в колонката

--- cat/BomDetails.class.php
+++ cat/BomDetails.class.php
@@ -196,6 +196,7 @@ class cat_BomDetails extends doc_Detail
         $baseCurrencyCode = acc_Periods::getBaseCurrencyCode($data->masterData->rec->modifiedOn);
         if(cat_BomDetails::count("#bomId = {$data->masterId} AND #type = 'stage'")){
             $data->listFields['resourceId'] .= "|* <a href=\"javascript:clickAllClasses('bomResourceColName{$data->masterData->rec->id}','bomDetailStepDescription{$data->masterData->rec->id}')\"  style=\"background-image:url(" . sbf('img/16/toggle1.png', "'") . ");\" class=' plus-icon more-btn' id='bomResourceColName{$data->masterData->rec->id}'> </a>";
+            $data->listFields['position'] .= "|* <span  class='newIconStyle' title='Показване/Скриване на всички детайли'> </span>";
         }
         $data->listFields['propQuantity'] = "|К-во влагане за|* {$data->masterData->row->quantity}->|Формула|*";
         $data->listFields['rowQuantity'] = "|К-во влагане за|* {$data->masterData->row->quantity}->|Количество|*";
