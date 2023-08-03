91d2b9058578846968669847bcf53385ff33d1e9
Yuseinov <yuseinov@experta.bg>
2023-08-03 11:50:49 +0300

    fileman - името на файла да не се показва в селект

--- fileman/Files.class.php
+++ fileman/Files.class.php
@@ -2241,38 +2241,13 @@ class fileman_Files extends core_Master
         }
 
         $fileNavArr = Mode::get('fileNavArr');
-        
-        // Показваме селект с всички файлове
-        if (!$dangerFileClass && $fileNavArr[$rec->fileHnd]['allFilesArr'] && countR($fileNavArr[$rec->fileHnd]['allFilesArr']) > 1) {
-            $form = cls::get('core_Form');
-            $form->fnc('selectFile', 'enum()', 'input=input');
-            
-            $form->addAttr('selectFile', array('onchange' => 'document.location = this.options[this.selectedIndex].value;'));
-            
-            $form->view = 'horizontal';
-            
-            $form->layout = "<form [#FORM_ATTR#] >[#FORM_FIELDS#][#FORM_TOOLBAR#][#FORM_HIDDEN#]</form>\n";
-            
-            foreach ($fileNavArr[$rec->fileHnd]['allFilesArr'] as $fUrl => $fName) {
-                if ($fileNavArr[$rec->fileHnd]['current'] == $fUrl) {
-                    $fName = type_Varchar::escape($data->rec->name);
-                }
-                $eArr[$fUrl] = str::limitLen($fName, 32);
-            }
-            
-            $form->setOptions('selectFile', $eArr);
-            $form->setDefault('selectFile', $fileNavArr[$rec->fileHnd]['current']);
-            
-            $row->fileName = $form->renderHtml();
-        } else {
-            // Вербалното име на файла
-            $row->fileName = "<span class='linkWithIcon{$dangerFileClass}' style=\"margin-left:-7px; " . ht::getIconStyle($icon) . '">';
-            if ($dangerFileClass) {
-                $row->fileName .= tr('Файл с вирус|*: ');
-            }
-            $row->fileName .= $mvc->getVerbal($rec, 'name') . '</span>';
+
+        // Вербалното име на файла
+        $row->fileName = "<span class='linkWithIcon{$dangerFileClass}' style=\"margin-left:-7px; " . ht::getIconStyle($icon) . '">';
+        if ($dangerFileClass) {
+            $row->fileName .= tr('Файл с вирус|*: ');
         }
-        
+        $row->fileName .= $mvc->getVerbal($rec, 'name') . '</span>';
 
         // Показваме и източника на файла
         if ($fileNavArr[$rec->fileHnd]['src']) {
