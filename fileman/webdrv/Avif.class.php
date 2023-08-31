a185a0ac0a84a07457c9ed04bb431fe9694a47ce
Yuseinov <yuseinov@experta.bg>
2023-08-31 13:34:53 +0300

    avif #C59671 - да показва файла без обработки

--- fileman/webdrv/Avif.class.php
+++ fileman/webdrv/Avif.class.php
@@ -13,6 +13,54 @@
  *
  * @since     v 0.1
  */
-class fileman_webdrv_Avif extends fileman_webdrv_ImageT
+class fileman_webdrv_Avif extends fileman_webdrv_Image
 {
+
+
+    /**
+     * Екшън за показване превю
+     */
+    public function act_Preview()
+    {
+        // Очакваме да има права за виждане
+        $this->requireRightFor('view');
+
+        // Манипулатора на файла
+        $fileHnd = Request::get('id');
+
+        if (!$fileHnd) {
+            $fileHnd = Request::get('fileHnd');
+        }
+
+        expect($fileHnd);
+
+        // Вземаме записа за файла
+        $fRec = fileman_Files::fetchByFh($fileHnd);
+
+        expect($fRec);
+
+        // Очакваме да има права за разглеждане на записа
+        $this->requireRightFor('view', $fRec);
+
+        // Сменяма wrapper'а да е празна страница
+        Mode::set('wrapper', 'page_Empty');
+
+        // Вземаме височината и широчината
+        $thumbWidthAndHeightArr = static::getPreviewWidthAndHeight();
+
+        // Атрибути на thumbnail изображението
+        $attr = array('class' => 'webdrv-preview', 'style' => 'margin: 0 auto 5px auto; display: block;');
+
+        // Background' а на preview' то
+        $bgImg = sbf('fileman/img/Preview_background.jpg');
+
+        $style = Mode::is('screenMode', 'wide') ? "display: table-cell; vertical-align: middle;" : "";
+
+        // Създаваме шаблон за preview на изображението
+        $preview = new ET("<div id='imgBg' style='background-image:url(" . $bgImg . "); padding: 8px; height: 598px; display: table;width: 100%;'><div  style='margin: 0 auto;" . $style . "'>[#THUMB_IMAGE#]</div></div>");
+
+        $preview->append(ht::createImg(array('src' => fileman_Download::getDownloadUrl($fileHnd))), 'THUMB_IMAGE');
+
+        return $preview;
+    }
 }
