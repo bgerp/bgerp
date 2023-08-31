990ec00cd038da77588cc0cf6aedc9ec700b748c
Yuseinov <yuseinov@experta.bg>
2023-08-31 11:04:56 +0300

    avif #C59664 - показване на avif файлвое

---
+++ fileman/webdrv/Avif.class.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/**
+ * Драйвер за работа с .avif файлове.
+ *
+ * @category  vendors
+ * @package   fileman
+ *
+ * @author    Yusein Yuseinov <yyuseinov@gmail.com>
+ * @copyright 2006 - 2023 Experta OOD
+ * @license   GPL 3
+ *
+ * @since     v 0.1
+ */
+class fileman_webdrv_Avif extends fileman_webdrv_ImageT
+{
+}
