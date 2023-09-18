e5c0c67b93e1b6e5e39ceddd661077edeadb90d5
ivelin1012 <ivelin_pdimov@abv.bg>
2023-09-18 14:46:37 +0300

    тест

--- sales/Invoices.class.php
+++ sales/Invoices.class.php
@@ -1053,6 +1053,8 @@ class sales_Invoices extends deals_InvoiceMaster
                     $notUpdated[$dRec->id] = array('number' => $dRec->number, 'count' => $count, 'rec' => $dRec, 'recs' => $cached, 'all' => $invoiceArr['recs']);
                 }
             }
+
+            bp($update, $notUpdated, $cached);
         }
 
         $Details = cls::get('sales_InvoiceDetails');
