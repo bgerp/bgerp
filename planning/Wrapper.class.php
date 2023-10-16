fdf11bcbfa5e4aa80353f9e7efb0ea2aaf29d4fe
ivelin1012 <ivelin_pdimov@abv.bg>
2023-10-16 14:35:38 +0300

    Готови сигнали: ъпдейтнато име на клас в обвивката

--- planning/Wrapper.class.php
+++ planning/Wrapper.class.php
@@ -39,7 +39,7 @@ class planning_Wrapper extends plg_ProtoWrapper
         $this->TAB('planning_StepConditions', 'Етапи->Зависимости', 'ceo,planning');
         $this->TAB('planning_FoldersWithResources', 'Настройки->Папки с ресурси', 'ceo,planning');
         $this->TAB('planning_AssetResourcesNorms', 'Настройки->Норми', 'ceo,planning');
-        $this->TAB('planning_AssetGroupTemplates', 'Настройки->Готови сигнали', 'ceo,planning');
+        $this->TAB('planning_AssetGroupIssueTemplates', 'Настройки->Готови сигнали', 'ceo,planning');
         $this->TAB('planning_GenericMapper', 'Настройки->Генерични', 'ceo,planning');
         $this->TAB('planning_Points', 'Дебъг->Точки', 'debug');
         $this->TAB('planning_GenericProductPerDocuments', 'Дебъг->По документи', 'debug');
