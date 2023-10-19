10b9da6e8d9542766f18fb64827393064560f86f
HeBuTy <nevena@experta.bg>
2023-10-19 11:07:26 +0300

    баркод скенер: ако намери задна камера, да пуска нея (няма записана в бисквитка)

--- barcode/js/html5.js
+++ barcode/js/html5.js
@@ -19,30 +19,38 @@ function barcodeActions() {
 
             // Ако има Front/back в името на камерите, ги записваме F1,F2,B1,B2 .. или с пореден номер
            if (camera.label.indexOf("front") != -1 ) {
+               cameraButton.className = "f" + frontCounter;
                cameraButton.textContent = "F" + frontCounter++;
            } else if(camera.label.indexOf("back") != -1 || camera.label.indexOf("rare") != -1) {
+               cameraButton.className = "b" + + backCounter;
                cameraButton.textContent = "B" + backCounter++;
            } else {
+               cameraButton.className = "device" + index + 1;
                cameraButton.textContent = index + 1;
            }
 
             cameraButton.value = camera.id;
-            cameraButton.className = "cameraSource";
+            cameraButton.className += " cameraSource";
 
             // при натискане на бутона да сменим камерата
             cameraButton.addEventListener("click", () => {
                 switchCamera(camera.id, html5Qrcode);
             });
+            cameraButtonsContainer.appendChild(cameraButton);
 
             // клас active на селектираната камера
-            if (camera.id === cameraId && cameraId) {
+            if (camera.id === cameraId && cameraId ) {
                 cameraButton.classList.add("active");
                 startScanning(cameraId, html5Qrcode);
-            } else {
-                $('.cameraSource').first().click();
             }
-            cameraButtonsContainer.appendChild(cameraButton);
+
         });
+
+        if( $('.cameraSource.active').length == 0 &&  $('.cameraSource.b1').length == 1) {
+            $('.cameraSource.b1').click();
+        } else {
+            $('.cameraSource').first().click();
+        }
     }).catch(err => {
         console.error("Error while getting cameras:", err);
     });
