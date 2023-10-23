2e7e3b1ff603f78f1889312b985fcb8062ac4b67
HeBuTy <nevena@experta.bg>
2023-10-23 14:35:09 +0300

    Изместване на функции за работа с бисквитки в основния js

--- js/efCommon.js
+++ js/efCommon.js
@@ -1186,6 +1186,25 @@ function prepareContextMenu() {
 }
 
 /**
+ * Създава бисквитка
+ */
+function setCookie(key, value) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString() + "; path=/";
+}
+
+
+/**
+ * Чете информацията от дадена бисквитка
+ */
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+}
+
+
+/**
  * Запазване на текущия таб
  * @param lastNotifyTime
  */
