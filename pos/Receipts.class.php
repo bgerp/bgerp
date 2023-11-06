17e76cc0db7df77b84e1ffe8ebafcc32ae9b0605
HeBuTy <nevena@experta.bg>
2023-11-06 15:25:35 +0200

    ПОС: По-голяма картинка на аватара

--- pos/Receipts.class.php
+++ pos/Receipts.class.php
@@ -991,7 +991,7 @@ class pos_Receipts extends core_Master
 
         if(Request::get('autoSelect')){
             if($rec->contragentClass == crm_Persons::getClassId()){
-                $avatar = crm_Persons::getPersonAvatarImg($rec->contragentObjectId, 80, 80);
+                $avatar = crm_Persons::getPersonAvatarImg($rec->contragentObjectId, 130, 130);
                 $personName = crm_Persons::fetchField($rec->contragentObjectId, 'name');
                 core_Statuses::newStatus("|*{$avatar->getContent()}<br>{$personName}", 'notice', null, 120);
             }
