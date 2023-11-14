1287d2ce7a8042773fd82c1400da772d17c1887b
ivelin1012 <ivelin_pdimov@abv.bg>
2023-11-14 10:40:19 +0200

    наново регенериране на ключовите думи на ПО

--- planning/Tasks.class.php
+++ planning/Tasks.class.php
@@ -2328,8 +2328,12 @@ class planning_Tasks extends core_Master
     protected static function on_AfterGetSearchKeywords($mvc, &$res, $rec)
     {
         // Ако ПО е към задание по продажба - добавя се хендлъра на продажбата в ключовите думи
-        if($jobSaleId = planning_Jobs::fetchField("#containerId = '{$rec->originId}'", 'saleId')){
-            $res .= ' ' . plg_Search::normalizeText(sales_Sales::getHandle($jobSaleId));
+        if($jobRec = planning_Jobs::fetch("#containerId = '{$rec->originId}'", 'saleId,productId')){
+            $res .= ' ' . plg_Search::normalizeText(sales_Sales::getHandle($jobRec->saleId));
+
+            // Добавяне на драйвера на артикула в ключовите думи
+            $productDriverClass = cat_Products::getVerbal($jobRec->productId, 'innerClass');
+            $res .= ' ' . plg_Search::normalizeText($productDriverClass);
         }
 
         // Добавяне на всички ключови думи от прогреса
@@ -2344,9 +2348,7 @@ class planning_Tasks extends core_Master
             }
         }
 
-        // Добавяне на драйвера на артикула в ключовите думи
-        $productDriverClass = cat_Products::getVerbal($rec->productId, 'innerClass');
-        $res .= ' ' . plg_Search::normalizeText($productDriverClass);
+
     }
 
 
