b6ca16e09bfbad58dd1b14c32bf097932bf35c8d
ivelin1012 <ivelin_pdimov@abv.bg>
2023-12-14 17:43:51 +0200

    пос бележка:бъгфикс с опаковките

--- pos/Receipts.class.php
+++ pos/Receipts.class.php
@@ -987,9 +987,11 @@ class pos_Receipts extends core_Master
             $price = $Policy->getPriceInfo($rec->contragentClass, $rec->contragentObjectId, $dRec->productId, $dRec->value, 1, $now, 1, 'no', $listId, false);
             if(empty($price->price)) continue;
             $oldPrice = (!empty($dRec->discountPercent)) ? ($dRec->price * (1 - $dRec->discountPercent)) : $dRec->price;
+
             $finalPrice = (!empty($price->discount)) ? ($price->price * (1 - $price->discount)) : $price->price;
             $finalPrice *= $perPack;
 
+            core_Statuses::newStatus("OLD:{$oldPrice} - NEW: {$finalPrice}", 'warning');
             if($isDefaultContragent || round($oldPrice, 5) > round($finalPrice, 5)){
                 $discount = $price->discount;
                 $price = $price->price / $perPack;
@@ -1006,7 +1008,7 @@ class pos_Receipts extends core_Master
                         if ($discountCalced > 0.01) {
                             // Подменяме цената за да може като се приспадне отстъпката и, да се получи толкова колкото тя е била
                             $discount = round(-1 * $disc, 3);
-                            $price = $comparePrice;
+                            $price = $comparePrice / $perPack;
                         }
                     }
                 }
