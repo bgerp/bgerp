d545d2783d4ad110faa914d7eb94c61f10bb43a5
ivelin1012 <ivelin_pdimov@abv.bg>
2024-01-05 15:17:26 +0200

    Справки: възможност за скриване на антетката

--- price/reports/PriceList.class.php
+++ price/reports/PriceList.class.php
@@ -55,8 +55,14 @@ class price_reports_PriceList extends frame2_driver_TableData
      * Какъв да е класа на групирания ред
      */
     protected $groupByFieldClass = 'pricelist-group-label';
-    
-    
+
+
+    /**
+     * Да се показва ли антектката при изпращане и при печат
+     */
+    protected $hideLetterHeadWhenSendingOrPrinting = true;
+
+
     /**
      * Добавя полетата на драйвера към Fieldset
      *
@@ -582,7 +588,7 @@ class price_reports_PriceList extends frame2_driver_TableData
      */
     protected static function on_AfterRenderSingle(frame2_driver_Proto $Driver, embed_Manager $Embedder, &$tpl, $data)
     {
-        if (Mode::is('printing')) return;
+        if (Mode::is('printing') || Mode::is('text', 'xhtml')) return;
 
         $fieldTpl = new core_ET(tr("|*<fieldset class='detail-info'>
                                             <legend class='groupTitle'><small><b>|Филтър|*</b></small></legend>
