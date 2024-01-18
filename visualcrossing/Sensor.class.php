fed58d5e302dc3367f349bc287e5cf29f6e0227f
angel <angel.trifonoff@gmail.com>
2024-01-18 17:50:49 +0200

    Драйвер за сензор на visualcrossing

---
+++ visualcrossing/Sensor.class.php
@@ -0,0 +1,91 @@
+<?php
+
+
+/**
+ * Сензор за ...............
+ *
+ * @see
+ *
+ * @category  bgerp
+ * @package   visualcrossing
+ *
+ * @author    Angel Trifonov angel.trifonoff@gmail.com
+ * @copyright 2006 - 2023 Experta OOD
+ * @license   GPL 3
+ * @title     Visualcrossing
+ *
+ * @since     v 0.1
+ */
+class visualcrossing_Sensor extends sens2_ProtoDriver
+{
+    /**
+     * Заглавие на драйвера
+     */
+    public $title = 'Visualcrossing';
+
+
+    /**
+     * Описание на входовете
+     */
+    public $inputs = array(
+        'currentTemp' => array('caption' => 'Текуща температура', 'uom' => 'C'),
+        'tempAfter6Hour' => array('caption' => 'Температура след 6ч.', 'uom' => 'C'),
+        'windAfter2Uour' => array('caption' => 'Вятър след 2ч.', 'uom' => 'Km/h'),
+
+    );
+
+    /**
+     * Интерфейси, поддържани от всички наследници
+     */
+    public $interfaces = 'sens2_ControllerIntf';
+
+
+    /**
+     * Описания на изходите
+     */
+    public $outputs = array(
+    );
+
+
+    /**
+     * Подготвя форма с настройки на контролера, като добавя полета с $form->FLD(....)
+     *
+     * @see  sens2_ControllerIntf
+     *
+     * @param core_Form
+     */
+    public function prepareConfigForm($form)
+    {
+    }
+
+
+    /**
+     * Прочита стойностите от сензорните входове
+     *
+     * @see  sens2_ControllerIntf
+     *
+     * @param array $inputs
+     * @param array $config
+     * @param array $persistentState
+     *
+     * @return mixed
+     */
+    public function readInputs($inputs, $config, &$persistentState)
+    {
+        $res = array();
+
+
+//        $time = dt::mysql2timestamp(dt::now());
+//        $h = date('G', $time);
+//        $h = $h .'-' . ($h+1);
+//        if(date('I', $time ) == 1 && date('I', $time + 3600) == 0) {
+//            $h = date('G', $time - 3600) . '-' . date('G', $time) . 'a';
+//        }
+      //  bp($time,$h);
+        $date = dt::now(false);
+
+       // $res['HOUR'] = ibex_Register::fetchField("#date = '{$date}' AND #kind = '{$h}'", 'price');
+
+        return $res;
+    }
+}
