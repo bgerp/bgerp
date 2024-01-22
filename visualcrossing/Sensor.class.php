a0f7afdc8766feb3dea0f57184ffe37d608507d4
angel <angel.trifonoff@gmail.com>
2024-01-22 18:05:30 +0200

    Подава данни

--- visualcrossing/Sensor.class.php
+++ visualcrossing/Sensor.class.php
@@ -28,8 +28,10 @@ class visualcrossing_Sensor extends sens2_ProtoDriver
      * Описание на входовете
      */
     public $inputs = array(
-        'currentTemp' => array('caption' => 'Текуща температура', 'uom' => 'C'),
-        'tempAfter6Hour' => array('caption' => 'Температура след 6ч.', 'uom' => 'C'),
+        'currentTempMin' => array('caption' => 'Текуща температура[min]', 'uom' => 'C'),
+        'currentTempMax' => array('caption' => 'Текуща температура[max]', 'uom' => 'C'),
+        'tempAfter6HourMin' => array('caption' => 'Температура след 6ч[min]', 'uom' => 'C'),
+        'tempAfter6HourMax' => array('caption' => 'Температура след 6ч[max]', 'uom' => 'C'),
         'windAfter2Uour' => array('caption' => 'Вятър след 2ч.', 'uom' => 'Km/h'),
 
     );
@@ -43,16 +45,15 @@ class visualcrossing_Sensor extends sens2_ProtoDriver
     /**
      * Описания на изходите
      */
-    public $outputs = array(
-    );
+    public $outputs = array();
 
 
     /**
      * Подготвя форма с настройки на контролера, като добавя полета с $form->FLD(....)
      *
+     * @param core_Form
      * @see  sens2_ControllerIntf
      *
-     * @param core_Form
      */
     public function prepareConfigForm($form)
     {
@@ -62,46 +63,51 @@ class visualcrossing_Sensor extends sens2_ProtoDriver
     /**
      * Прочита стойностите от сензорните входове
      *
-     * @see  sens2_ControllerIntf
-     *
      * @param array $inputs
      * @param array $config
      * @param array $persistentState
      *
      * @return mixed
+     * @see  sens2_ControllerIntf
+     *
      */
     public function readInputs($inputs, $config, &$persistentState)
     {
         $res = array();
 
-
         $time = dt::mysql2timestamp(dt::now());
-        $h = date('G', $time);
-        //$h = $h .'-' . ($h+1);
-        //$h = $h+2;
 
-//        if(date('I', $time ) == 1 && date('I', $time + 3600) == 0) {
-//            $h = date('G', $time - 3600) . '-' . date('G', $time) . 'a';
-//        }
-     //   bp($time,$h);
         $date = dt::now(false);
+        $tomorrow = dt::addDays(2, $date);
 
-        $query =visualcrossing_Forecasts::getQuery();
-        $query->where("#date = '{$date}'");
-        //$query->where("#time = '{$h}'");
-bp(visualcrossing_Forecasts::getForecast($date));
-        $forRec = $query->fetchAll();
-        foreach ($forRec as $fRec => $val ){
-            if(!$val->time ){bp($val);
-                $currentTemp = $val->low;
+        $query = visualcrossing_Forecasts::getQuery();
+        $query->where("#date >= '{$date}' AND #date < '{$tomorrow}' ");
+
+        $currentTempMin = $currentTempMax = $tempAfter6HourMin = $tempAfter6HourMax = $windAfter2Uour = null;
+        while ($fRec = $query->fetch()) {
+
+
+            if ($fRec->date == date('Y-m-d', $time) && ($fRec->time == date('G', $time))) {
+                $currentTempMin = $fRec->low;
+                $currentTempMax = $fRec->high;
+            }
+
+            if ($fRec->date == date('Y-m-d', $time) && ($fRec->time == date('G', $time + 6 * 3600))) {
+                $tempAfter6HourMin = $fRec->low;
+                $tempAfter6HourMax = $fRec->high;
+            }
+
+            if ($fRec->date == date('Y-m-d', $time) && ($fRec->time == date('G', $time + 2 * 3600))) {
+                $windAfter2Uour = $fRec->wind;
             }
 
         }
 
-
-        $res['currentTemp'] = '12345';
-        $res['tempAfter6Hour'] = '98765';
-        $res['windAfter2Uour'] = '23432';
+        $res['currentTempMin'] = $currentTempMin;
+        $res['currentTempMax'] = $currentTempMax;
+        $res['tempAfter6HourMin'] = $tempAfter6HourMin;
+        $res['tempAfter6HourMax'] = $tempAfter6HourMax;
+        $res['windAfter2Uour'] = $windAfter2Uour;
 
         return $res;
     }
