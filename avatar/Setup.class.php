79b14623b6f7ef8af8f51f27ac465be6f1a09526
Yuseinov <yuseinov@experta.bg>
2024-02-19 11:49:29 +0200

    avatar - още повече избор

--- avatar/Setup.class.php
+++ avatar/Setup.class.php
@@ -2,7 +2,7 @@
 
 
 /**
- * Икона по подразбиране в garavatar - identicon, monsterid, wavatar, retro, blank
+ * Икона по подразбиране в garavatar - identicon, monsterid, wavatar, retro, blank, mysteryperson, robohash
  */
 defIfNot('AVATAR_DEFAULT_ICON_TYPE', 'wavatar');
 
@@ -61,7 +61,7 @@ class avatar_Setup extends core_ProtoSetup
      * Описание на конфигурационните константи
      */
     public $configDescription = array(
-        'AVATAR_DEFAULT_ICON_TYPE' => array('enum(wavatar, identicon, monsterid, retro, blank)', 'caption=Икона по подразбиране в garavatar->Избор'),
+        'AVATAR_DEFAULT_ICON_TYPE' => array('enum(wavatar, identicon, mysteryperson, robohash, monsterid, retro, blank)', 'caption=Икона по подразбиране в garavatar->Избор'),
     );
 
     
