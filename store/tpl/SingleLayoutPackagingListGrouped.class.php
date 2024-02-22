e031252a59a16cfda857109f02ab3a3096e1f35f
ivelin1012 <ivelin_pdimov@abv.bg>
2024-02-22 12:43:44 +0200

    packin list за митница козметична промяна

--- store/tpl/SingleLayoutPackagingListGrouped.class.php
+++ store/tpl/SingleLayoutPackagingListGrouped.class.php
@@ -250,7 +250,7 @@ class store_tpl_SingleLayoutPackagingListGrouped extends doc_TplScript
                     $detail->Master->pushTemplateLg($masterRec->template);
                 }
             }
-            $columns= $columns - (in_array($masterRec->state, array('draft', 'pending')) ? 3 : 4);
+            $columns= $columns - (in_array($masterRec->state, array('draft')) ? 3 : 4);
             $element = ht::createElement('tr', $rowAttr, new ET("<td style='background: #eee;padding-top:9px;padding-left:5px; border-right: none !important;' colspan='{$columns}'>" . $groupVerbal .'</td><td class="tariffCodeModifyBtn aright" style="vertical-align: middle !important; border-left: none !important;background: #eee; ">'.  $modifyBtn->getContent() .'</td>'));
             $rows['|' . $tariffNumber] = $element;
             
