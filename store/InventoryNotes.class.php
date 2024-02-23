da95a4d89f7b08206d7b65cffdcdd5b917d99a88
ivelin1012 <ivelin_pdimov@abv.bg>
2024-02-23 14:06:59 +0200

    Протокол за инвентаризация: бъгфикс #Tsk7121

--- store/InventoryNotes.class.php
+++ store/InventoryNotes.class.php
@@ -204,6 +204,7 @@ class store_InventoryNotes extends core_Master
         $this->FLD('hideOthers', 'enum(yes=Да,no=Не)', 'caption=Показване само на избраните групи->Избор, mandatory, notNull,value=no,maxRadio=2');
         $this->FLD('cache', 'blob', 'input=none');
         $this->FLD('expandByBatches', 'enum(no=Само ако има въведени,yes=Винаги)', 'caption=Разпъване по партиди при показване->Избор');
+        $this->FLD('orderProductBy', 'enum(auto=Автоматично,name=Име,code=Код)', 'caption=Сортиране на артикулите в групите->Избор,notNull,value=auto');
 
         // Ако потребителя има роля 'accMaster', може да контира/оотегля/възстановява МО с приключени права
         if (haveRole('accMaster,ceo')) {
