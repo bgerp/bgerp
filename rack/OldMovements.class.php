c6111a4c0c46d5cc43739593a2a690aeb8b82972
arsov <arsov@experta.bg>
2024-03-19 18:38:13 +0200

    Добавяне "С избраните" - за лесно изтриване на неизпълнени чакащи движения

--- rack/OldMovements.class.php
+++ rack/OldMovements.class.php
@@ -31,7 +31,7 @@ class rack_OldMovements extends rack_MovementAbstract
     /**
      * Плъгини за зареждане
      */
-    public $loadList = 'plg_RowTools2, plg_Created, rack_Wrapper, plg_State, plg_Sorting,plg_SelectPeriod,plg_Search,plg_AlignDecimals2,plg_Modified';
+    public $loadList = 'plg_RowTools2, plg_Created, rack_Wrapper, plg_State, plg_Sorting,plg_SelectPeriod,plg_Select,plg_Search,plg_AlignDecimals2,plg_Modified';
 
 
     /**
