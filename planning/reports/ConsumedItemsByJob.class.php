6bddc434962bffeb52083809e55bc713cf0202ce
atrifonoff <angel.trifonoff@gmail.com>
2024-03-21 15:27:10 +0200

    Коригиран експорта

--- planning/reports/ConsumedItemsByJob.class.php
+++ planning/reports/ConsumedItemsByJob.class.php
@@ -505,18 +505,18 @@ class planning_reports_ConsumedItemsByJob extends frame2_driver_TableData
             $fld->FLD('code', 'varchar', 'caption=Код,tdClass=centered');
             $fld->FLD('name', 'varchar', 'caption=Артикул');
             $fld->FLD('measure', 'varchar', 'caption=Мярка,tdClass=centered');
-            $fld->FLD('consumedQuantity', 'double(smartRound,decimals=2)', 'smartCenter,caption=Вложено->Количество');
+            $fld->FLD('consumedQuantity', 'double(decimals=2)', 'smartCenter,caption=Вложено->Количество');
             if (!is_null($rec->seeAmount)) {
-                $fld->FLD('consumedAmount', 'double(smartRound,decimals=2)', 'smartCenter,caption=Вложено->Стойност');
+                $fld->FLD('consumedAmount', 'double(decimals=2)', 'smartCenter,caption=Вложено->Стойност');
             }
-            $fld->FLD('returnedQuantity', 'double(smartRound,decimals=2)', 'smartCenter,caption=Върнато->Количество');
+            $fld->FLD('returnedQuantity', 'double(decimals=2)', 'smartCenter,caption=Върнато->Количество');
             if (!is_null($rec->seeAmount)) {
-                $fld->FLD('returnedAmount', 'double(smartRound,decimals=2)', 'smartCenter,caption=Върнато->Стойност');
+                $fld->FLD('returnedAmount', 'double(decimals=2)', 'smartCenter,caption=Върнато->Стойност');
             }
 
-            $fld->FLD('totalQuantity', 'double(smartRound,decimals=2)', 'smartCenter,caption=Резултат->Количество');
+            $fld->FLD('totalQuantity', 'double(decimals=2)', 'smartCenter,caption=Резултат->Количество');
             if (!is_null($rec->seeAmount)) {
-                $fld->FLD('totalAmount', 'double(smartRound,decimals=2)', 'caption=Резултат->Стойност');
+                $fld->FLD('totalAmount', 'double(decimals=2)', 'caption=Резултат->Стойност');
             }
 
         }
