0b6553f8b7d3b49a1ebb9ec34d52b9ec68694445
ivelin1012 <ivelin_pdimov@abv.bg>
2024-04-23 13:10:30 +0300

    ЕН за митница: бъгфикс с проверката за валидност на теглата

--- deals/Helper.class.php
+++ deals/Helper.class.php
@@ -2914,7 +2914,9 @@ abstract class deals_Helper
         }
 
         if(isset($weight) && isset($netWeight) && isset($tareWeight)){
-            if(round($weight - $netWeight) != round($tareWeight)){
+            $calcedTare = $weight - $netWeight;
+            $tareDiff = abs(round($calcedTare - $tareWeight, 3));
+            if($tareDiff > 0.3) {
                 $res['errors'][] = array('fields' => "{$weightFieldName},{$netWeightFieldName},{$tareWeightFieldName}", 'text' => 'Разликата между бруто и нето не отговаря на тарата');
             }
         }
