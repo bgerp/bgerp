7e205633883008f21b83053b6ddc029fede1bb98
Yuseinov <yuseinov@experta.bg>
2024-06-26 15:06:00 +0300

    js #Tsk7433 - fix за маркиране на текст при глобалното търсене

--- js/modernTheme.js
+++ js/modernTheme.js
@@ -243,7 +243,7 @@ function changePinIcon(){
 function userMenuActions() {
 	$('body').on('click', function(e){
     	if($(e.target).is('.menu-options') || $(e.target).is('.menu-options > img') ) {
-			var selText = getSelText().toString().trim();
+			var selText = getSelText().trim();
 			if (selText) {
 				$('.search-input-modern').val(selText.substring(0,32));
 			}
