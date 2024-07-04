82cb5593713c6f7eb8dd60c58de352ace455fe93
iveta <ivetamosheva@gmail.com>
2024-07-04 12:41:16 +0300

    new class

--- eventhub/Venues.class.php
+++ eventhub/Venues.class.php
@@ -14,7 +14,7 @@
  *
  * @since     v 0.1
  */
-class eventhub_Venues extends core_Manager
+class eventhub_Venues extends core_Master
 {
     /**
      * Заглавие на страницата
@@ -32,7 +32,7 @@ class eventhub_Venues extends core_Manager
     /**
      * Полета за листов изглед
      */
-    public $listFields = 'id, title, createdOn=Създаване||Created->На, createdBy=Създаване||Created->От||By, modifiedOn=Модифицирано||Modified->На, modifiedBy=Модифицирано||Modified->От||By,state';
+    public $listFields = 'id, title, type, district, city, address, createdOn=Създаване||Created->На, createdBy=Създаване||Created->От||By, modifiedOn=Модифицирано||Modified->На, modifiedBy=Модифицирано||Modified->От||By,state';
 
 
     /**
@@ -66,19 +66,25 @@ class eventhub_Venues extends core_Manager
 
 
     /**
+     * Нов темплейт за показване
+     */
+    public $singleLayoutFile = 'eventhub/tpl/SingleLayoutVenues.shtml';
+
+
+    /**
      * Описание на модела
      */
     public function description()
     {
         $this->FLD('title', 'varchar(128)', 'caption=Наименование, mandatory');
         $this->setDbUnique('title');
-        $this->FLD('type', 'enum(hall=Зала, stadium=Стадион, square=Площад, building=Сграда)', 'caption=Вид');
-        $this->FLD('district', 'enum(1=Благоевградска, 2=Бурсгаска, 3=Варненска, 4=Великотърновска, 5=Видинска, 6=Врачанска, 7=Габровска,
+        $this->FLD('type', 'enum(,hall=Зала, stadium=Стадион, square=Площад, building=Сграда)', 'caption=Вид,mandatory, placeholder=Изберете');
+        $this->FLD('district', 'enum(,1=Благоевградска, 2=Бурсгаска, 3=Варненска, 4=Великотърновска, 5=Видинска, 6=Врачанска, 7=Габровска,
         8=Добричка, 9=Кърджалийска, 10=Кюстендилска, 11=Ловешка, 12=Монтанска, 13=Пазарджишка, 14=Пернишка, 15=Плевенска, 16=Пловдивска, 
         17=Разградска, 18=Русенска, 19=Силистренска, 20=Сливенска, 21=Смолянска, 22=София-град, 23=Софийска, 24=Старозагорска, 25=Търговищка, 
-        26=Хасковска, 27=Шуменска, 28=Ямболска)', 'caption=Област');
+        26=Хасковска, 27=Шуменска, 28=Ямболска)', 'caption=Област,mandatory, placeholder=Изберете');
         $this->FLD('city', 'varchar(64)', 'caption=Нас. място, mandatory');
         $this->FLD('address', 'varchar(128)', 'caption=Адрес, mandatory');
-        $this->FLD('geolocation', 'location_Type(geolocation=mobile)', 'caption=Адрес');
+        $this->FLD('geolocation', 'location_Type(geolocation=mobile)', 'caption=Геолокация');
     }
 }
