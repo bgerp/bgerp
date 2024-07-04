53d380a70c21988f41a9c77616b44b3fbaa1f6e6
iveta <ivetamosheva@gmail.com>
2024-07-04 11:03:41 +0300

    качена нова версия на модел на eventhub

--- eventhub/Venues.class.php
+++ eventhub/Venues.class.php
@@ -73,7 +73,10 @@ class eventhub_Venues extends core_Manager
         $this->FLD('title', 'varchar(128)', 'caption=Наименование, mandatory');
         $this->setDbUnique('title');
         $this->FLD('type', 'enum(hall=Зала, stadium=Стадион, square=Площад, building=Сграда)', 'caption=Вид');
-        $this->FLD('district', 'enum(1=Благоевградска, 2=)', 'caption=Област');
+        $this->FLD('district', 'enum(1=Благоевградска, 2=Бурсгаска, 3=Варненска, 4=Великотърновска, 5=Видинска, 6=Врачанска, 7=Габровска,
+        8=Добричка, 9=Кърджалийска, 10=Кюстендилска, 11=Ловешка, 12=Монтанска, 13=Пазарджишка, 14=Пернишка, 15=Плевенска, 16=Пловдивска, 
+        17=Разградска, 18=Русенска, 19=Силистренска, 20=Сливенска, 21=Смолянска, 22=София-град, 23=Софийска, 24=Старозагорска, 25=Търговищка, 
+        26=Хасковска, 27=Шуменска, 28=Ямболска)', 'caption=Област');
         $this->FLD('city', 'varchar(64)', 'caption=Нас. място, mandatory');
         $this->FLD('address', 'varchar(128)', 'caption=Адрес, mandatory');
         $this->FLD('geolocation', 'location_Type(geolocation=mobile)', 'caption=Адрес');
