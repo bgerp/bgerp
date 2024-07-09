2765fcb4c5d26f901d287c29cf472b0fb25f65ef
Svetlozar <svetlozartrifonov60@gmail.com>
2024-07-09 11:02:41 +0300

    Validation logic added, duration need attention

--- eventhub/Events.class.php
+++ eventhub/Events.class.php
@@ -1,107 +1,108 @@
 <?php
-
-
 /**
- * Събития
- *
- *
- * @category  bgerp
- * @package   eventhub
- *
- * @author    Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
- * @copyright 2006 - 2021 Experta OOD
- * @license   GPL 3
- *
- * @since     v 0.1
- */
+* Събития
+*
+*
+* @category  bgerp
+* @package   eventhub
+*
+*
+* @since     v 0.1
+*/
 class eventhub_Events extends core_Master
 {
-    /**
-     * Заглавие на страницата
-     */
-    public $title = 'Събития';
+/**
+* Заглавие на страницата
+*/
+public $title = 'Събития';
 
+/**
+* Зареждане на необходимите плъгини
+*/
+public $loadList = 'plg_RowTools2, plg_State2, plg_Printing,
+plg_Search, plg_Created, plg_Modified, plg_Sorting, eventhub_Wrapper';
 
-    /**
-     * Зареждане на необходимите плъгини
-     */
-    public $loadList = 'plg_RowTools2, plg_State2, plg_Printing, 
-        plg_Search, plg_Created, plg_Modified, plg_Sorting, eventhub_Wrapper';
+/**
+* Полета за листов изглед
+*/
+public $listFields = 'id, title, format, description, poster, startDate, startTime, duration, place, participants, organizer, tickets, magnitude, createdOn=Създаване||Created->На, createdBy=Създаване||Created->От||By, modifiedOn=Модифицирано||Modified->На, modifiedBy=Модифицирано||Modified->От||By,state';
 
+/**
+*  Полета по които ще се търси
+*/
+public $searchFields = 'title';
 
-    /**
-     * Полета за листов изглед
-     */
-    public $listFields = 'id, title, format, description, poster, startDate, startTime, duration, place, participants, organizer, tickets, magnitude, createdOn=Създаване||Created->На, createdBy=Създаване||Created->От||By, modifiedOn=Модифицирано||Modified->На, modifiedBy=Модифицирано||Modified->От||By,state';
+/**
+* Кой може да го разглежда?
+*/
+public $canList = 'ceo, admin';
 
+/**
+* Кой може да разглежда сингъла на документите?
+*/
+public $canSingle = 'ceo, admin';
 
-    /**
-     *  Полета по които ще се търси
-     */
-    public $searchFields = 'title';
+/**
+* Кой може да добявя,редактира или изтрива статия
+*/
+public $canWrite = 'ceo, admin';
 
+/**
+* Единично заглавие на документа
+*/
+public $singleTitle = 'Събития';
 
-    /**
-     * Кой може да го разглежда?
-     */
-    public $canList = 'ceo, admin';
+/**
+* Описание на модела
+*/
+public function description()
+{
+$this->FLD('title', 'varchar(128)', 'caption=Наименование, mandatory');
+$this->FLD('series', 'key(mvc=eventhub_Series,select=title)', 'caption=Поредица, mandatory');
+$this->FLD('categories', 'keylist(mvc=eventhub_Categories, select=title)', 'caption=Категория, mandatory');
+$this->FLD('formId', 'key(mvc=eventhub_Forms, select=title)', 'caption=Формат, mandatory');
+$this->FLD('description', 'richtext', 'caption=Описание');
+$this->FLD('poster', 'fileman_FileType(bucket=pictures)', 'caption=Плакат');
+$this->FLD('startDate', 'date', 'caption=Начало, mandatory');
+$this->FLD('openingTime', 'hour', 'caption=Отваряне, mandatory');
+$this->FLD('startTime', 'hour', 'caption=Започва, mandatory');
+$this->FLD('duration', 'time', 'caption=Продължителност');
+$this->FLD('place', 'key(mvc=eventhub_Venues,select=title)', 'caption=Място, mandatory');
+$this->FLD('participants', 'keylist(mvc=crm_Persons,select=name, allowEmpty)', 'caption=Участват');
+$this->FLD('organizers', 'keylist(mvc=crm_Persons,select=name, allowEmpty)', 'caption=Организатор, mandatory');
+$this->FLD('tickets', 'type_Urls', 'caption=Билети');
+$this->FLD('magnitude', 'enum(1=локално, 2=регионално, 3=национално, 4=международно)', 'caption=Магнитуд');
+}
 
+/**
+* Извиква се след въвеждането на данните от Request във формата ($form->rec)
+*/
+protected static function on_AfterInputEditForm($mvc, &$form)
+{
+if ($form->isSubmitted()) {
+// проверява дали 'startdate' е записана и не е в миналото
+if ($form->rec->startDate) {
+if ($form->rec->startDate < dt::today()) {
+$form->setError('startDate', 'Invalid start date: Event cannot start in the past.');
+}
+}
 
-    /**
-     * Кой може да разглежда сингъла на документите?
-     */
-    public $canSingle = 'ceo, admin';
+// Validate openingTime and startTime
+if ($form->rec->openingTime && $form->rec->startTime) {
+$openingTime = strtotime($form->rec->openingTime);
+$startTime = strtotime($form->rec->startTime);
+//проверява дали обявеното време, в което отваря, е преди часът за започване на събитието ( ако е нужно )
+if ($openingTime >= $startTime) {
+$form->setError('openingTime, startTime', 'Opening time must be before the start time.');
+}
+}
 
-
-    /**
-     * Кой може да добявя,редактира или изтрива статия
-     */
-    public $canWrite = 'ceo, admin';
-
-
-    /**
-     * Единично заглавие на документа
-     */
-    public $singleTitle = 'Събития';
-
-    /**
-     * Нов темплейт за показване
-     */
-    public $singleLayoutFile = 'eventhub/tpl/SingleLayoutEvents.shtml';
-
-    /**
-     * Описание на модела
-     */
-    public function description()
-    {
-        $this->FLD('title', 'varchar(128)', 'caption=Наименование, mandatory');
-
-        $this->FLD('series', 'key(mvc=eventhub_Series,select=title)', 'caption=Поредица, mandatory');
-
-        $this->FLD('categories', 'keylist(mvc=eventhub_Categories, select=title)', 'caption=Категория, mandatory');
-
-        $this->FLD('formId', 'key(mvc=eventhub_Forms, select=title)', 'caption=Формат, mandatory');
-
-        $this->FLD('description', 'richtext', 'caption=Описание');
-
-        $this->FLD('poster', 'fileman_FileType(bucket=pictures)', 'caption=Плакат');
-
-        $this->FLD('startDate', 'date', 'caption=Начало, mandatory');
-
-        $this->FLD('openingTime', 'hour', 'caption=Отваряне, mandatory');
-
-        $this->FLD('startTime', 'hour', 'caption=Започва, mandatory');
-
-        $this->FLD('duration', 'time', 'caption=Продължителност');
-
-        $this->FLD('place', 'key(mvc=eventhub_Venues,select=title)', 'caption=Място, mandatory');
-
-        $this->FLD('participants', 'keylist(mvc=crm_Persons,select=name, allowEmpty)', 'caption=Участват');
-
-        $this->FLD('organizers', 'keylist(mvc=crm_Persons,select=name, allowEmpty)', 'caption=Организатор, mandatory');
-
-        $this->FLD('tickets', 'type_Urls', 'caption=Билети');
-
-        $this->FLD('magnitude', 'enum(1=локално, 2=регионално, 3=национално, 4=международно)', 'caption=Магнитуд');
-    }
+/**
+ * Продължителността не е задължително поле за попълване, но ако бъде попълнено не може да е <= 0
+ */
+if($form->rec->duration<=0){
+    $form->setError('duration', 'Duration cannot be negative.');
+}
+}
+}
 }
