af67be5124836395e8c93edf9aa88944e1cbee70
Yuseinov <yuseinov@experta.bg>
2024-07-12 13:42:03 +0300

    zxcvbn #C66383 - подобрение при логване с лоша парола

--- zxcvbn/Setup.class.php
+++ zxcvbn/Setup.class.php
@@ -56,7 +56,7 @@ class zxcvbn_Setup extends core_ProtoSetup
      */
     public $configDescription = array(
         'ZXCVBN_MIN_SCORE' => array('int(min=0,max=4)', 'caption=Сложност на паролата->Точки'),
-        'ZXCVBN_CHECK_ON_LOGIN' => array('enum(no=Не,yes=Да)', 'caption=Дали да се проверява паролата при логване->Избор'),
+        'ZXCVBN_CHECK_ON_LOGIN' => array('enum(no=Не,yes=Да,stop=Спиране на логване)', 'caption=Дали да се проверява паролата при логване->Избор'),
     );
     
     /**
