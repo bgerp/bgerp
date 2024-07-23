44d3eb8d139efe064ceb330f3ab569561153906c
ivelin1012 <ivelin_pdimov@abv.bg>
2024-07-23 17:30:25 +0300

    ешоп:ваучери бъгфикс

--- crm/ext/Cards.class.php
+++ crm/ext/Cards.class.php
@@ -504,6 +504,8 @@ class crm_ext_Cards extends core_Manager
                     $cartRec = eshop_Carts::fetch($cartId);
                     $cartRec->voucherId = $voucherInfo['id'];
                     eshop_Carts::save($cartRec);
+                    voucher_Cards::mark($cartRec->voucherId, true, eshop_Carts::getClassId(), $cartRec->id);
+                    followRetUrl(null, 'Ваучерът е добавен');
                 }
             }
         }
