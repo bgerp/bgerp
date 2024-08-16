febd5ecd2aaf05e23399066d6b5302b668bfb5af
ivelin1012 <ivelin_pdimov@abv.bg>
2024-08-16 17:29:03 +0300

    Ресурси:да може да се избира проект от формата

--- doc/FolderResources.class.php
+++ doc/FolderResources.class.php
@@ -111,7 +111,7 @@ class doc_FolderResources extends core_Manager
             $query->EXT('state', 'crm_Persons', 'externalName=state,externalKey=personId');
             $query->EXT('name', 'crm_Persons', 'externalName=name,externalKey=personId');
         }
-        $query->orderBy('name=ASC,state=ASC');
+        $query->orderBy('code=ASC,state=ASC');
         
         // Подготовка на пейджъра
         $data->Pager = cls::get('core_Pager', array('itemsPerPage' => $data->itemsPerPage));
@@ -130,7 +130,7 @@ class doc_FolderResources extends core_Manager
             
             $data->rows[$dRec->id] = $row;
         }
-        
+
         // Подготовка на полетата за показване
         $listFields = ($DetailName == 'planning_Hr') ? 'code=Код,personId=Оператор,users=Потребители,created=Създаване' : 'code=Код,name=Оборудване,groupId=Група,users=Потребители,created=Създаване';
         $data->listFields = arr::make($listFields, true);
