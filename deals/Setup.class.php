6e8a1f1d2aac7f601aad4b08dc746eb76b51cfe9
arsov <arsov@experta.bg>
2024-09-26 10:57:26 +0300

    Малко по-голям избор от конкретни потребители за "Съставил"

--- deals/Setup.class.php
+++ deals/Setup.class.php
@@ -120,7 +120,7 @@ class deals_Setup extends core_ProtoSetup
      */
     public $configDescription = array(
         'DEALS_BALANCE_TOLERANCE' => array('percent(min=0)', 'caption=Сделката да не се показва като просрочена при салдо (неплатено)->Под,unit= от доставеното'),
-        'DEALS_ISSUER_USER' => array('user(roles=ceo|salesMaster,allowEmpty)', 'caption=Съставител на бизнес документи->Конкретен потребител,customizeBy=ceo|sales|purchase,invoicer'),
+        'DEALS_ISSUER_USER' => array('user(roles=ceo|sales,allowEmpty)', 'caption=Съставител на бизнес документи->Конкретен потребител,customizeBy=ceo|sales|purchase,invoicer'),
         'DEALS_ISSUER' => array('enum(createdBy=Създателят,activatedBy=Активиралият)', 'caption=Съставител на бизнес документи->Или,customizeBy=ceo|sales|purchase,invoicer'),
         'DEALS_OVERDUE_PENDING_DAYS_1' => array('int(Min=0)', 'caption=Напомняне за неконтиран документ с минал падеж/вальор->Първо след,unit=дни'),
         'DEALS_OVERDUE_PENDING_DAYS_2' => array('int(Min=0)', 'caption=Напомняне за неконтиран документ с минал падеж/вальор->Второ след,unit=дни'),
