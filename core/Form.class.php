0723d95c1c0b44d341abfda20f9805da32a9c2ba
ivelin <ivo@experta.bg>
2024-09-30 16:43:13 +0300

    core_Form:да се маха празната опция само ако полето не с позволена празна опция

--- core/Form.class.php
+++ core/Form.class.php
@@ -928,7 +928,7 @@ class core_Form extends core_FieldSet
                         // ако ще се рендират опциите като радио-бутони маха се празната опция
                         if(isset($maxRadio) && countR($options) <= $maxRadio){
                             if(isset($options['']) && (empty($options['']) || (is_object($options['']) && empty(trim($options['']->title)))) && countR($options) >= 2){
-                                if(countR($options) != 2){
+                                if(!$type->params['allowEmpty']){
                                     unset($options['']);
                                 }
                             }
