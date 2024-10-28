152a2860fcd8c4cc07a8eaf762be8d9068fe8006
ivelin1012 <ivelin_pdimov@abv.bg>
2024-10-28 17:16:13 +0200

    качен таймер

--- planning/Tasks.class.php
+++ planning/Tasks.class.php
@@ -324,7 +324,7 @@ class planning_Tasks extends core_Master
         $this->FLD('labelQuantityInPack', 'double(smartRound,Min=0)', 'caption=Етикиране->В опаковка,tdClass=small-field nowrap,input=hidden,oldFieldName=packagingQuantityInPack');
         $this->FLD('labelType', 'enum(print=Генериране,scan=Въвеждане,both=Комбинирано,autoPrint=Генериране и Печат)', 'caption=Етикиране->Производ. №,tdClass=small-field nowrap,notNull,value=both,input=hidden');
         $this->FLD('labelTemplate', 'key(mvc=label_Templates,select=title)', 'caption=Етикиране->Шаблон,tdClass=small-field nowrap,input=hidden');
-        $this->FLD('timeStart', 'datetime(timeSuggestions=08:00|09:00|10:00|11:00|12:00|13:00|14:00|15:00|16:00|17:00|18:00,format=smartTime)', 'caption=Целеви времена->Начало, changable, tdClass=leftColImportant');
+        $this->FLD('timeStart', 'datetime(timegetAssetTaskOptionsSuggestions=08:00|09:00|10:00|11:00|12:00|13:00|14:00|15:00|16:00|17:00|18:00,format=smartTime)', 'caption=Целеви времена->Начало, changable, tdClass=leftColImportant');
         $this->FLD('timeDuration', 'time', 'caption=Целеви времена->Продължителност,changable');
         $this->FLD('calcedDuration', 'time', 'caption=Целеви времена->Нетна продължителност,input=none');
         $this->FLD('calcedCurrentDuration', 'time', 'caption=Целеви времена->Изчислена продължителност,input=none');
@@ -1988,9 +1988,12 @@ class planning_Tasks extends core_Master
                 $assetTasks = planning_AssetResources::getAssetTaskOptions($rec->assetId, true);
                 unset($assetTasks[$rec->id]);
                 $taskOptions = array();
+
+                core_Debug::startTimer('GET_TASK_OPTIONS_TITLE');
                 foreach ($assetTasks as $tRec) {
                     $taskOptions[$tRec->id] = $mvc->getAlternativeTitle($tRec);
                 }
+                core_Debug::stopTimer('GET_TASK_OPTIONS_TITLE');
 
                 $form->setField('startAfter', 'input');
                 if (countR($taskOptions)) {
