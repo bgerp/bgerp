3028b2270790548b75e0171bf3f82ee3f94a56c2
ivelin1012 <ivelin_pdimov@abv.bg>
2024-11-08 10:53:03 +0200

    ПОС:дебъг

--- pos/Terminal.class.php
+++ pos/Terminal.class.php
@@ -2266,13 +2266,18 @@ class pos_Terminal extends peripheral_Terminal
      */
     private function getPosProductPreview($productId, $width, $height, $settings = array())
     {
+        core_Debug::startTimer('RENDER_RESULT_GET_PREVIEW_PARAM');
         $photo = cat_Products::getParams($productId, 'preview');
+        core_Debug::stopTimer('RENDER_RESULT_GET_PREVIEW_PARAM');
         if($settings->productBtnTpl == 'pictureAndText' && empty($photo)) return;
 
         $arr = array();
+        core_Debug::startTimer('RENDER_RESULT_GET_PREVIEW_THUMB');
         $thumb = (!empty($photo)) ? new thumb_Img(array($photo, $height, $width, 'fileman')) : new thumb_Img(getFullPath('pos/img/default-image.jpg'), $width, $height, 'path');
+        $res = $thumb->createImg($arr);
+        core_Debug::stopTimer('RENDER_RESULT_GET_PREVIEW_THUMB');
 
-        return $thumb->createImg($arr);
+        return $res;
     }
     
     
