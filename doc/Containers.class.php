2bbde8d240783850a8e945c7e1f8372ff65aa1ca
ivelin1012 <ivelin_pdimov@abv.bg>
2024-11-21 10:32:26 +0200

    махнат току що добавен индекс

--- doc/Containers.class.php
+++ doc/Containers.class.php
@@ -149,7 +149,6 @@ class doc_Containers extends core_Manager
         $this->setDbIndex('createdOn');
         $this->setDbIndex('modifiedOn');
         $this->setDbIndex('originId');
-        $this->setDbIndex('docClass');
         $this->setDbUnique('docClass, docId');
         $this->setDbIndex('searchKeywords', null, 'FULLTEXT');
     }
