8df36ddf30e1442a31ea6cb90100a38053e2db89
MilenGG <milen@experta.bg>
2024-12-15 22:39:24 +0200

    Изчисляване типа на цената на енергийната борса

--- ibex/Sensor.class.php
+++ ibex/Sensor.class.php
@@ -27,6 +27,8 @@ class ibex_Sensor extends sens2_ProtoDriver
      */
     public $inputs = array(
         'HOUR' => array('caption' => 'Текуща цена', 'uom' => 'BGN'),
+        'TYPE' => array('caption' => 'Вид', 'uom' => ''),
+
         //'BASE' => array('caption' => 'Дневна цена', 'uom' => 'BGN'),
 
     );
@@ -48,6 +50,8 @@ class ibex_Sensor extends sens2_ProtoDriver
      */
     public function prepareConfigForm($form)
     {
+        $form->FLD('minIndex', 'int', 'caption=Индекс на часа->Евтин,value=6');
+        $form->FLD('maxIndex', 'int', 'caption=Индекс на часа->Скъп,value=18');
     }
     
     
@@ -75,6 +79,31 @@ class ibex_Sensor extends sens2_ProtoDriver
         $date = dt::now(false);
  
         $res['HOUR'] = ibex_Register::fetchField("#date = '{$date}' AND #kind = '{$h}'", 'price');
+
+        $iQuery = ibex_Register::getQuery();
+
+        while($iRec = $iQuery->fetch("#date = '{$date}'")) {
+            if($iRec->kind == '00-24') continue;
+            $prices[(int) $iRec->kind] = (float) $iRec->price;
+        }
+$h = '04-05';
+        asort($prices);
+        $prices = array_keys($prices);
+
+        $pos = array_search($h, $prices);
+        $minIndex = $config->minIndex ? $config->minIndex : 6;
+        $maxIndex = $config->maxIndex ? $config->maxIndex : 6;
+        
+        $h = (int) $h;
+
+        if($pos < 6) {
+            $res['TYPE'] = -1;
+        } elseif($pos >= 18) {
+            $res['TYPE'] = 1;
+        } else {
+            $res['TYPE'] = 0;
+        }
+
  
         return $res;
     }
