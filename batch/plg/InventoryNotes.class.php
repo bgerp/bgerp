b96adf20fc2d79a042270b05a67c13ca83446719
ivelin1012 <ivelin_pdimov@abv.bg>
2024-12-16 13:58:34 +0200

    Инвентаризация: добавен плейсхолдър

--- batch/plg/InventoryNotes.class.php
+++ batch/plg/InventoryNotes.class.php
@@ -69,7 +69,7 @@ class batch_plg_InventoryNotes extends core_Plugin
             }
 
             // Добавяне на поле за избор на съществуваща партида
-            $form->FNC('batchEx', 'varchar', 'caption=Партида,maxRadio=1');
+            $form->FNC('batchEx', 'varchar', 'caption=Партида,maxRadio=1,placeholder=Без партида');
             $autohide = countR($quantities) ? 'autohide' : '';
             $caption = ($Def->getFieldCaption()) ? $Def->getFieldCaption() : 'Партида';
             $form->FNC('batchNew', 'varchar', "caption=Установена нова партида->{$caption},input,placeholder={$Def->placeholder}");
