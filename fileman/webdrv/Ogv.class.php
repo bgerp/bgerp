18c2e4461824b6b1c8963818781f166dc81d8ac7
Yusein <yyuseinov@gmail.com>
2024-12-30 16:32:54 +0200

    fileman/webdrv #Tsk7858 - конвертиране към mp4

--- fileman/webdrv/Ogv.class.php
+++ fileman/webdrv/Ogv.class.php
@@ -8,11 +8,19 @@
  * @package   fileman
  *
  * @author    Yusein Yuseinov <yyuseinov@gmail.com>
- * @copyright 2006 - 2013 Experta OOD
+ * @copyright 2006 - 2025 Experta OOD
  * @license   GPL 3
  *
  * @since     v 0.1
  */
 class fileman_webdrv_Ogv extends fileman_webdrv_Video
 {
+
+
+    /**
+     * Към кое разширение да се конвертира
+     *
+     * @var string
+     */
+    public static $toExt = 'mp4';
 }
