d2024bb19c9ae93bc1e51a549d13c38cc0a4e67d
ivelin1012 <ivelin_pdimov@abv.bg>
2025-01-23 15:58:48 +0200

    партиди: бъгфикс с проверка

--- batch/BatchesInDocuments.class.php
+++ batch/BatchesInDocuments.class.php
@@ -244,10 +244,12 @@ class batch_BatchesInDocuments extends core_Manager
                     }
                 }
 
-                $batchQuantityInStore = batch_Items::getQuantity($rec->productId, $rec->batch, $storeId);
-                if ($rec->quantity > $batchQuantityInStore) {
-                    $batchQuantityInStoreVerbal = core_Type::getByName('double(smartRound)')->toVerbal($batchQuantityInStore / $quantityInPack);
-                    $batch = ht::createHint($batch, 'Над наличното количество|* ' . $batchQuantityInStoreVerbal . ' |в|* "' . store_Stores::getTitleById($storeId) . '". |Проверете за контирани документи по партидата с по-нова дата|*.', 'warning');
+                if ($rInfo->operation['out'] && in_array($rInfo->state, array('draft', 'pending'))) {
+                    $batchQuantityInStore = batch_Items::getQuantity($rec->productId, $rec->batch, $storeId);
+                    if ($rec->quantity > $batchQuantityInStore) {
+                        $batchQuantityInStoreVerbal = core_Type::getByName('double(smartRound)')->toVerbal($batchQuantityInStore / $quantityInPack);
+                        $batch = ht::createHint($batch, 'Над наличното количество|* ' . $batchQuantityInStoreVerbal . ' |в|* "' . store_Stores::getTitleById($storeId) . '". |Проверете за контирани документи по партидата с по-нова дата|*.', 'warning');
+                    }
                 }
 
                 $label = ($count == 0) ? "{$label} " : '';
