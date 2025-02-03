b9ce0162e4e225bb7b26284599f926a5388da736
ivelin1012 <ivelin_pdimov@abv.bg>
2025-02-03 10:21:26 +0200

    дебъг код

--- planning/TaskConstraints.class.php
+++ planning/TaskConstraints.class.php
@@ -521,8 +521,9 @@ class planning_TaskConstraints extends core_Master
 
         // Извличат се графиците на всички ПО с интервали за планиране
         $assetIds = arr::extractValuesFromArray($tasks, 'assetId');
-        $intervals = array();
+        $intervals = $assets = array();
         foreach ($assetIds as $assetId) {
+            $assets[$assetId] = planning_AssetResources::fetchField($assetId, 'code');
             if($Interval = planning_AssetResources::getWorkingInterval($assetId)) {
                 $intervals[$assetId] = $Interval;
             }
@@ -532,9 +533,11 @@ class planning_TaskConstraints extends core_Master
         // Попринцип не би трябвало да има машина без график, но за всеки случай
         $tasksWithActualStart = $tasksWithoutActualStartByAssetId = array();
         $assetsWithIntervals = array_keys($intervals);
-        $allTasks = array();
-        array_walk($tasks, function ($task) use ($assetsWithIntervals, &$allTasks, &$tasksWithActualStart, &$tasksWithoutActualStartByAssetId) {
+        $allTasks = $taskLinks = array();
+        array_walk($tasks, function ($task) use ($assetsWithIntervals, &$allTasks, &$tasksWithActualStart, &$tasksWithoutActualStartByAssetId, &$taskLinks) {
             if(in_array($task->assetId, $assetsWithIntervals)) {
+                $taskLinks[$task->id] = ht::createLink("Opr{$task->id}", array('planning_Tasks', 'single', $task->id), false, 'target=_blank')->getContent();
+
                 $allTasks[$task->id] = $task;
                 if(!empty($task->actualStart)){
                     $tasksWithActualStart[$task->id] = $task;
@@ -549,7 +552,7 @@ class planning_TaskConstraints extends core_Master
         arr::sortObjects($tasksWithActualStart, 'actualStart', 'ASC');
 
         // Първо ще се наместят в графика тези с фактическо начало
-
+        $debugRes = "Разполагане на тези с ФАКТИЧЕСКО начало <hr />";
 
         $debugArr = array();
         $planned = array();
@@ -557,9 +560,13 @@ class planning_TaskConstraints extends core_Master
         $now = dt::now();
         foreach ($tasksWithActualStart as $taskRec1){
             $begin = max($taskRec1->actualStart, $now);
+
             if($Interval = $intervals[$taskRec1->assetId]){
                 $offset = isset($interruptionArr[$taskRec1->productId]) ?? null;
+                $beginDate = $begin;
                 $begin = strtotime($begin);
+
+                $debugRes .= "{$taskLinks[$taskRec1->id]} храни <b>[{$assets[$taskRec1->assetId]}]($taskRec1->assetId)</b> с начало {$beginDate} / прод. {$taskRec1->calcedCurrentDuration} <br />";
                 $timeArr = $Interval->consume($taskRec1->calcedCurrentDuration, $begin, null, $offset);
 
                 // Опит за смятане на очакваното начало/край. Ако не може значи е `9999-12-31 23:59:59`
@@ -567,13 +574,20 @@ class planning_TaskConstraints extends core_Master
                 if(is_array($timeArr)){
                     $planned[$taskRec1->id]->expectedTimeStart = date('Y-m-d H:i:00', $timeArr[0]);
                     $planned[$taskRec1->id]->expectedTimeEnd = date('Y-m-d H:i:00', $timeArr[1]);
+                    $debugRes .="--------Изчислено за S: <b>{$planned[$taskRec1->id]->expectedTimeStart}</b> / Е: <b>{$planned[$taskRec1->id]->expectedTimeEnd}</b> <br />";
+                } else {
+                    $debugRes .="--------Не е изчислено начало/край<br />";
                 }
-
+                $debugRes .= "<hr />";
                 $debugArr[$taskRec1->assetId][$taskRec1->id] = $planned[$taskRec1->id]->expectedTimeStart;
             }
         }
 
+        $debugRes .= " <hr />Разполагане на тези с БЕЗ начало <hr />";
+
         foreach ($tasksWithoutActualStartByAssetId as $assetId => $assetTasks){
+            $debugRes .= " Слагане на задачи на <b>{$assets[$assetId]}</b><br />";
+
             $Interval = $intervals[$assetId];
 
             //@todo потребителската подредба
@@ -593,23 +607,31 @@ class planning_TaskConstraints extends core_Master
             $sortedArr = $withStart + $withoutStart;
 
             foreach ($sortedArr as $task){
-
                 $isPlannable = true;
                 if(!array_key_exists($task->id, $previousTasks)){
                     $startTime = max($now, $task->timeStart);
+                    $debugRes .= "{$taskLinks[$task->id]} - Няма ограничения <br />";
                 } else {
+                    $debugStr = "";
                     $calcedTimes = array();
                     foreach ($previousTasks[$task->id] as $prevId => $prevTask){
                         $plannedPrevTime = $planned[$prevId]->expectedTimeStart;
                         if(empty($plannedPrevTime)) {
                             $isPlannable = false;
+                            $debugStr .= "|{$taskLinks[$prevId]} not planned|";
                         } else {
                             $plannedPrevTime = dt::addSecs($prevTask->waitingTime, $plannedPrevTime);
                             $calcedTimes[$plannedPrevTime] = $plannedPrevTime;
+                            $debugStr .= "|{$taskLinks[$prevId]} planned: {$plannedPrevTime} - offset {$prevTask->waitingTime}|";
                         }
                     }
 
-                    if(!$isPlannable) continue;
+                    if(!$isPlannable) {
+                        $debugRes .= "{$taskLinks[$task->id]} - <b>НЕ МОЖЕ ДА СЕ ПЛАНИРА</b> предходни ({$debugStr})<br />";
+                        continue;
+                    }
+
+                    $debugRes .= "{$taskLinks[$task->id]} - <b>МОЖЕ да се планира</b> предходни ({$debugStr})<br />";
 
                     $calcedTimes[$now] = $now;
                     $calcedTimes[$task->timeStart] = $task->timeStart;
@@ -619,17 +641,25 @@ class planning_TaskConstraints extends core_Master
                 $offset = isset($interruptionArr[$task->productId]) ?? null;
                 $begin = strtotime($startTime);
 
+                $debugRes .= "{$taskLinks[$task->id]} храни <b>[{$assets[$task->assetId]}]($task->assetId)</b> с начало {$startTime} / прод. {$task->calcedCurrentDuration} <br />";
                 $timeArr = $Interval->consume($task->calcedCurrentDuration, $begin, null, $offset);
                 $planned[$task->id] = (object)array('assetId' => $task->assetId, 'calcedCurrentDuration' => $task->calcedCurrentDuration, 'expectedTimeStart' => $notFoundDate, 'expectedTimeEnd' => $notFoundDate);
                 if(is_array($timeArr)){
                     $planned[$task->id]->expectedTimeStart = date('Y-m-d H:i:00', $timeArr[0]);
                     $planned[$task->id]->expectedTimeEnd = date('Y-m-d H:i:00', $timeArr[1]);
+                    $debugRes .="--------Изчислено за S: <b>{$planned[$task->id]->expectedTimeStart}</b> / Е: <b>{$planned[$task->id]->expectedTimeEnd}</b> <br />";
+                } else {
+                    $debugRes .="--------Не е изчислено начало/край<br />";
                 }
                 $debugArr[$assetId][$task->id] = $planned[$task->id]->expectedTimeStart;
                 unset($tasksWithoutActualStartByAssetId[$assetId][$task->id]);
+
+                $debugRes .= "<hr />";
             }
         }
 
+        echo $debugRes;
+        bp($debugArr);
 
         bp($debugArr, $tasksWithoutActualStartByAssetId);
         bp($tasksWithoutActualStartByAssetId);
