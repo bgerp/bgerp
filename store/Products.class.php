6e4cf2f7f72256ddc21611d465238e085a0953d3
ivelin1012 <ivelin_pdimov@abv.bg>
2025-02-20 15:40:31 +0200

    таб наличности бъгфикс

--- store/Products.class.php
+++ store/Products.class.php
@@ -202,7 +202,7 @@ class store_Products extends core_Detail
      * @param stdClass $data
      */
     protected static function on_AfterPrepareListFilter($mvc, $data)
-    {
+    {;
         if($data->masterMvc instanceof cat_Products){
             $data->query->EXT('storeName', 'store_Stores', 'externalName=name,externalKey=storeId');
             
@@ -215,7 +215,8 @@ class store_Products extends core_Detail
             } else {
                 $data->query->orderBy('storeName', 'ASC');
             }
-            
+
+            unset($data->listFilter->showFields);
             return;
         }
 
@@ -266,9 +267,9 @@ class store_Products extends core_Detail
         $data->query->EXT('name', 'cat_Products', 'externalName=name,externalKey=productId');
         $data->query->EXT('productCreatedOn', 'cat_Products', 'externalName=createdOn,externalKey=productId');
 
-
         if (isset($data->masterMvc)) {
             $showFieldsArr = arr::make('horizon,search,groupId', 2);
+
             $data->listFilter->showFields = implode(',', $showFieldsArr);
             if($data->masterMvc instanceof store_Stores){
                 $data->listFilter->setDefault('setting', $data->masterData->rec->displayStockMeasure);
