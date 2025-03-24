6c26ac72687c3161e25821d18cc82d10209de791
nevena <nevena@experta.bg>
2025-03-24 16:57:02 +0200

    Добавен текст на бутона за скриване/показване на филтри

--- js/efCommon.js
+++ js/efCommon.js
@@ -6523,6 +6523,7 @@ function toggleListFilter()
         $('.listFilter tr.toggable').hide();
         hiddenItems = []; // Инициализираме празен масив, за да не е null
         $('.toggleListFilterBtn').css('background-image', 'url(' + $('.toggleListFilterBtn').data('plus') + ")");
+        $('.toggleListFilterBtn').val($('.toggleListFilterBtn').data('close'));
     } else if(hiddenItems.length) {
         // Скриваме редовете, които са били записани като скрити
         $('.listFilter tr.toggable').each(function() {
@@ -6534,6 +6535,7 @@ function toggleListFilter()
             }
         });
         $('.toggleListFilterBtn').css('background-image', 'url(' + $('.toggleListFilterBtn').data('plus') + ")");
+        $('.toggleListFilterBtn').val($('.toggleListFilterBtn').data('open'));
     }
 
     $('.toggleListFilterBtn').on('click', function() {
@@ -6581,12 +6583,14 @@ function toggleListFilter()
                     }
                 });
                 $('.toggleListFilterBtn').css('background-image', 'url(' + $('.toggleListFilterBtn').data('plus') + ")");
+                $('.toggleListFilterBtn').val($('.toggleListFilterBtn').data('open'));
             } else if (!$row.is(':visible')){
                 // Показваме реда и го премахваме от LocalStorage
                 $row.fadeIn('slow', function() {
                     hiddenItems = hiddenItems.filter(id => id !== rowId);
                     localStorage.setItem(formId, JSON.stringify(hiddenItems));
                     $('.toggleListFilterBtn').css('background-image', 'url(' + $('.toggleListFilterBtn').data('minus') + ")");
+                    $('.toggleListFilterBtn').val($('.toggleListFilterBtn').data('close'));
 
                     $(this).find('input.combo').each(function(){
                         var idComboBox = $(this).attr('id');
