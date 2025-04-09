dc582bd0d4d6934698d436ce827cb996a33421e1
Yusein <yyuseinov@gmail.com>
2025-04-09 17:19:20 +0300

    rtac_Plugin #Tsk8101 - друг тест за премахване на #Tsk8100

--- rtac/Plugin.class.php
+++ rtac/Plugin.class.php
@@ -18,9 +18,9 @@ class rtac_Plugin extends core_Plugin
     /**
      * Шаблон за намиране на потребителите
      */
-    public static $pattern = "/\B(?'pre'@)(?'nick'(\w|\.)+(\w){1})/";
-    
-    
+    public static $pattern = "/\B(?'pre'@)(?'nick'(\w|\.)+(\w){1})(?'emoji'([\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}\x{1F900}-\x{1F9FF}]))*/uxi";
+
+
     /**
      * Масив с всички потребители, споделени в
      *
