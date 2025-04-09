e7369f76f02825127a30fe55358150786b640ab6
arsov <arsov@experta.bg>
2025-04-09 16:19:51 +0300

    Revert "rtac_Plugin #Tsk8100 - тестово премахване на иконки след ника"
    This reverts commit 4e25323fe909ca8602f0a7cef7759d1dfe10522b.

--- rtac/Plugin.class.php
+++ rtac/Plugin.class.php
@@ -18,8 +18,8 @@ class rtac_Plugin extends core_Plugin
     /**
      * Шаблон за намиране на потребителите
      */
-    public static $pattern = "/\B(?'pre'@)(?'nick'(\w|\.)+(\w){1})(?'emoji'([\p{Emoji_Presentation}\p{Extended_Pictographic}]))*/ui";
-
+    public static $pattern = "/\B(?'pre'@)(?'nick'(\w|\.)+(\w){1})/";
+    
     
     /**
      * Масив с всички потребители, споделени в
