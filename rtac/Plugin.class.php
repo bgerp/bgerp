2deb916d92c427e8f0bb442f647ef694ad89433c
Yusein <yyuseinov@gmail.com>
2025-04-09 18:03:00 +0300

    rtac_Plugin - fix2 тестов

--- rtac/Plugin.class.php
+++ rtac/Plugin.class.php
@@ -18,7 +18,7 @@ class rtac_Plugin extends core_Plugin
     /**
      * Шаблон за намиране на потребителите
      */
-    public static $pattern = "/\B(?'pre'@)(?'nick'(\w|\.)+(\w){1})(?'emoji'([\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}\x{1F900}-\x{1F9FF}]))*/uxi";
+    public static $pattern = "/\B(?'pre'@)(?'nick'(\w|\.)+(\w){1})(?'emoji'([\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}\x{1F900}-\x{1F9FF}\x{1F700}-\x{1F77F}\x{1F780}-\x{1F7FF}\x{1F800}-\x{1F8FF}\x{1FA70}-\x{1FAFF}\x{FE0F}]))*/uxi";
 
 
     /**
