1f8025a25131fbee94adb5b28ff39e5a22c9a12f
nevena <nevena@experta.bg>
2025-06-12 10:49:53 +0300

    Стилове за справка

--- sales/DeliveryData.class.php
+++ sales/DeliveryData.class.php
@@ -299,7 +299,7 @@ class sales_DeliveryData extends core_Manager
                 $Document = doc_Containers::getDocument($rec->containerId);
 
                 $row->address = "{$row->countryId}, {$row->pCode} {$row->place}";
-                $row->link = "<span class='state-{$rec->state} document-handler'>{$Document->getLink(0)} <small>{$row->address}</small></span>";
+                $row->link = "<span class='state-{$rec->state} document-handler'>{$Document->getLink(0)} <small class='nowrap'>{$row->address}</small></span>";
 
                 $link = new core_ET("<div style='float:left;padding-bottom:2px;padding-top: 2px;'>[#link#]</div>");
                 $link->placeObject($row);
