419c58aef9a50a81faf855e91716a39bb9f90b5e
ivelin1012 <ivelin_pdimov@abv.bg>
2025-07-31 10:30:15 +0300

    доставка с наш транспорт махане на излишно съобщение ако няма избрана локация

--- sales/interface/FreeRegularDelivery.class.php
+++ sales/interface/FreeRegularDelivery.class.php
@@ -124,10 +124,12 @@ class sales_interface_FreeRegularDelivery extends core_BaseClass
             $form->setField('deliveryPCode', 'input=hidden');
             $form->setField('deliveryPlace', 'input=hidden');
             $form->setField('deliveryAddress', 'input=hidden');
-            
-            if(!$countRoutes){
-                $infoText = tr('За съжаление, няма планирани маршрути до вашата локация. Ако имате въпроси, моля да се свържете с нас|*!');
-                $form->info = new core_ET("<div id='editStatus'><div class='warningMsg'>{$infoText}</div></div>");
+
+            if(isset($locationId)){
+                if(!$countRoutes){
+                    $infoText = tr('За съжаление, няма планирани маршрути до вашата локация. Ако имате въпроси, моля да се свържете с нас|*!');
+                    $form->info = new core_ET("<div id='editStatus'><div class='warningMsg'>{$infoText}</div></div>");
+                }
             }
         } elseif($Document instanceof sales_Sales){
             $form->setField('deliveryAdress', 'input=hidden');
