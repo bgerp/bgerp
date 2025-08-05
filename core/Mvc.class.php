b293bf72d1dfe5d17b3f0bdcbd11acc4a091aafb
MilenGG <milen@experta.bg>
2025-08-05 09:46:33 +0300

    Insert on duplicate Update

--- core/Mvc.class.php
+++ core/Mvc.class.php
@@ -441,11 +441,13 @@ class core_Mvc extends core_FieldSet
                     $query = "INSERT DELAYED `{$table}` SET {$query}";
                     $timer = "{$table} INSERT";
                     break;
-                
                 case '':
+                    $query = "INSERT INTO `{$table}` SET {$query}";
+                    $timer = "{$table} INSERT";
+                    break;
                 case 'update':
                     $query = "INSERT INTO `{$table}` SET {$query} ON DUPLICATE KEY UPDATE {$query}";
-                    $timer = "{$table} INSERT";
+                    $timer = "{$table} UPDATE";
                     break;
                 default:
                     error('Неподдържан режим на запис', $mode, $rec);
