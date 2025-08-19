875de8dc855c7af147ce83cdbfb8e834de9f25ae
ivelin1012 <ivelin_pdimov@abv.bg>
2025-08-19 10:56:09 +0300

    Сделки:бъгфикс с бутона за активиране ако има обща отстъпка #C74629

--- deals/DealMaster.class.php
+++ deals/DealMaster.class.php
@@ -1133,7 +1133,9 @@ abstract class deals_DealMaster extends deals_DealBase
     {
         // Ако няма сума, но има обща отстъпка да не може да се активира
         if(isset($rec->id)){
-            if(empty($rec->amountDeal) && price_DiscountsPerDocuments::haveDiscount(get_called_class(), $rec->id)) return false;
+            $Detail = cls::get(get_called_class())->mainDetail;
+            $dCount = $Detail->count("#{$Detail->masterKey} = {$rec->id}");
+            if(!$dCount && price_DiscountsPerDocuments::haveDiscount(get_called_class(), $rec->id)) return false;
         }
 
         return true;
