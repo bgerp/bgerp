8550da70d65e6a384294cbc1bec0832d82e9db52
Yusein <yyuseinov@gmail.com>
2025-08-26 16:27:36 +0300

    core_Manager #Tsk8369 - fix2

--- core/Manager.class.php
+++ core/Manager.class.php
@@ -561,6 +561,8 @@ class core_Manager extends core_Mvc
     {
         $data = parent::prepareListFilter($data);
 
+        $data->listFields = arr::make($data->listFields);
+
         if ($data->query && $data->listFields && $data->listFields['id']) {
             $data->query->orderBy('id', 'ASC');
         }
