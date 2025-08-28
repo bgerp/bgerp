24a0fde57490e465bbcb8129db4d3d4ded5dca96
ivelin1012 <ivelin_pdimov@abv.bg>
2025-08-28 15:47:37 +0300

    ПОС:погрешно закоментиран кеш

--- pos/Terminal.class.php
+++ pos/Terminal.class.php
@@ -2211,7 +2211,7 @@ class pos_Terminal extends peripheral_Terminal
         core_Debug::stopTimer('RES_RENDER_PREPARE_RECS');
 
         $cacheKey = "{$rec->_policy1}_{$rec->_policy2}_{$priceCache}_{$rec->_selectedGroupId}_{$searchString}";
-        $result = null;core_Cache::get('pos_Terminal', $cacheKey);
+        $result = core_Cache::get('pos_Terminal', $cacheKey);
 
         if(!is_array($result)){
             core_Debug::startTimer('RES_RENDER_RESULT_VERBAL');
