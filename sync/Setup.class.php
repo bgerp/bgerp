943d74cecd7b22bc4c38a8ec96013b0a0f467535
ivelin1012 <ivelin_pdimov@abv.bg>
2025-09-11 15:42:51 +0300

    някои пакети добавен втори параметър isInstalled = true

--- sync/Setup.class.php
+++ sync/Setup.class.php
@@ -151,7 +151,7 @@ class sync_Setup extends core_ProtoSetup
         $Bucket = cls::get('fileman_Buckets');
         $html .= $Bucket->createBucket('importedProductFiles', 'Файлове от импортирани артикули', null, '1GB', 'user', 'user');
 
-        if(core_Packs::isInstalled('eshop')){
+        if(core_Packs::isInstalled('eshop', true)){
             $eSettings = cls::get('eshop_Settings');
             $html .= $eSettings->setupMvc();
         }
