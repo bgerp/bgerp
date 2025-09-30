8957fb649a0a4db12b20de911cc051c471399ad1
ivelin1012 <ivelin_pdimov@abv.bg>
2025-09-30 16:07:20 +0300

    тл: добавен спан на к-то в логистичните единици

--- trans/LineDetails.class.php
+++ trans/LineDetails.class.php
@@ -317,7 +317,7 @@ class trans_LineDetails extends doc_Detail
             // Подготовка на логистичната информация за документа
             $logisticArr = array();
             if(!empty($transportInfo['transportUnits'])){
-                $transUnits = trans_helper::displayTransUnits($transportInfo['transportUnits'], false, ', ');
+                $transUnits = trans_Helper::displayTransUnits($transportInfo['transportUnits'], false, ', ');
                 $logisticArr[] = $transUnits;
             } elseif(isset($transportInfo['volume'])){
                 $logisticArr[] = "<i>" . core_Type::getByName('cat_type_Volume')->toVerbal($transportInfo['volume']) . "<i>";
