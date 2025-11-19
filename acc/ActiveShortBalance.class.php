d5bd873e90c3e31ae9e718e64440ef0880de3a28
ivelin1012 <ivelin_pdimov@abv.bg>
2025-11-19 11:32:51 +0200

    баланси: намалена граница на зареждане от предходния баланс от 0.05 на 0.01

--- acc/ActiveShortBalance.class.php
+++ acc/ActiveShortBalance.class.php
@@ -219,7 +219,7 @@ class acc_ActiveShortBalance
             $bQuery = acc_BalanceDetails::getQuery();
             $bQuery->show('accountId,ent1Id,ent2Id,ent3Id,blAmount,blQuantity');
             acc_BalanceDetails::filterQuery($bQuery, $balanceRec->id, $accs, $this->params['itemsAll'], $this->params['item1'], $this->params['item2'], $this->params['item3']);
-            $bQuery->where('ABS(#blQuantity) > 0.001 OR ABS(#blAmount) > 0.05');
+            $bQuery->where('ABS(#blQuantity) > 0.001 OR ABS(#blAmount) > 0.01');
             
             while ($bRec = $bQuery->fetch()) {
                 if (!isset($accInfos[$bRec->accountId])) {
