77d261a2be1cf5c020418afddaab1d0c927851be
arsov <arsov@experta.bg>
2026-01-21 02:14:59 +0200

    Revert "Още малко допълване ..."
    This reverts commit 0c32568fdc3bcc07a34d40c27285c7e2c23a2507.

--- currency/CurrencyRates.class.php
+++ currency/CurrencyRates.class.php
@@ -595,7 +595,6 @@ class currency_CurrencyRates extends core_Detail
                 $exchangeDeviation = 0.01;
 
                 if ($expectedAmount < 0.26) {
-                    $minAmount = min($amountTo, $expectedAmount);
                     $exchangeDeviation = round(abs($amountTo - $expectedAmount) / $minAmount * 100) / 100;
                 }
             }
