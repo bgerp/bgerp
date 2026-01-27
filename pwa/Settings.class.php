dd3e7ee1ddcf730fe8f640eca7a6867a1313653f
Yusein <yyuseinov@gmail.com>
2026-01-27 15:52:08 +0200

    pwa - вдигане на времето

--- pwa/Settings.class.php
+++ pwa/Settings.class.php
@@ -244,7 +244,7 @@ class pwa_Settings extends core_Manager
                     'url' => $rec->{"sc{$i}Url"},
                     'icons' => array(
                         (object)array(
-                            'src' => fileman_Download::getDownloadUrl($rec->{"sc{$i}Icon"}, 10000, 'handler', false),
+                            'src' => fileman_Download::getDownloadUrl($rec->{"sc{$i}Icon"}, 100000, 'handler', false),
 //                            'src' => toUrl(array('fileman_Download', 'Download', 'fh' => $rec->{"sc{$i}Icon"}, 'forceDownload' => true)),
                             'sizes' => '512x512',
                         ),
