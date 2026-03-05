d2986619c8f3c14f73809b674523d5847c359993
ivelin1012 <ivelin_pdimov@abv.bg>
2026-03-05 10:08:29 +0200

    ПВ: възможност за пренасяне на вложените материали в ПО

--- planning/Setup.class.php
+++ planning/Setup.class.php
@@ -248,6 +248,12 @@ defIfNot('PLANNING_ALLOW_STORE_DOCS_IN_JOB', '0');
 
 
 /**
+ * Автоматично добавяне на артикули от протокол за влагане в ПО->Избор
+ */
+defIfNot('PLANNING_AUTO_ADD_CONVERTABLE_TO_TASK', 'no');
+
+
+/**
  * Производствено планиране - инсталиране / деинсталиране
  *
  *
@@ -336,6 +342,7 @@ class planning_Setup extends core_ProtoSetup
         'PLANNING_MANUAL_ORDER_IN_ASSET' => array('enum(yes=Да,no=Не)', array('caption' => 'САМО ръчно подреждане на операциите на оборудването->Избор')),
         'PLANNING_TASK_SUB_PRODUCT_MIN_BUTTONS' => array('int(Min=0)', array('caption' => 'Колко индивидуални бутони за субпродукти да се показват в ПО->Брой')),
         'PLANNING_ALLOW_STORE_DOCS_IN_JOB' => array('time', array('caption' => 'До кога след приключване на заданието да се контират протоколи за влагане и връщане->Време')),
+        'PLANNING_AUTO_ADD_CONVERTABLE_TO_TASK' => array('enum(no=Изключено,yes=Включено)', array('caption' => 'Автоматично добавяне на артикули от протокол за влагане в ПО->Избор')),
     );
 
 
