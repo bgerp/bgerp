b9ea2d770cb53ae666d03f798e2d3ea03b889649
ivelin1012 <ivelin_pdimov@abv.bg>
2026-03-17 10:36:29 +0200

    Артикули:бъгфикс с връщане на дефолтна цена при обратни складови разписки

--- cat/Products.class.php
+++ cat/Products.class.php
@@ -3261,14 +3261,18 @@ class cat_Products extends embed_Manager
         // Намира се цената на последния дебит в складовата сметка където участва артикула, с най-голямо количество
         if ($itemId = acc_Items::fetchField("#classId = '{$this->getClassId()}' AND #objectId = '{$id}'")) {
             $jQuery = acc_JournalDetails::getQuery();
+            $jQuery->EXT('valior', 'acc_Journal', 'externalKey=journalId');
             $sysId = acc_Accounts::getRecBySystemId('321')->id;
             $jQuery->where("#debitAccId = {$sysId} AND #debitItem2 = {$itemId} AND #debitPrice > 0");
             $jQuery->orderBy('debitQuantity', 'DESC');
-            $jQuery->show('debitPrice');
+            $jQuery->show('debitPrice,valior');
             $jQuery->limit(1);
-            
+            $lastRec = $jQuery->fetch();
+
             // Ако има таква цена, то това ще е дефолтната цена
-            if ($biggestDebitPrice = $jQuery->fetch()->debitPrice) {
+            if (is_object($lastRec)) {
+                $biggestDebitPrice = deals_Helper::getSmartBaseCurrency($lastRec->debitPrice, $lastRec->valior, $valior);
+
                 return $biggestDebitPrice;
             }
         }
