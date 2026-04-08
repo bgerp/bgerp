e706fad7bcf8bbd35de5a525ba3b909226b257c3
ivelin1012 <ivelin_pdimov@abv.bg>
2026-04-08 10:07:16 +0300

    РБД–та: възможност за предсрочно плащане

--- bank/SpendingDocuments.class.php
+++ bank/SpendingDocuments.class.php
@@ -8,7 +8,7 @@
  * @package   bank
  *
  * @author    Ivelin Dimov <ivelin_pdimov@abv.bg>
- * @copyright 2006 - 2017 Experta OOD
+ * @copyright 2006 - 2026 Experta OOD
  * @license   GPL 3
  *
  * @since     v 0.1
@@ -70,12 +70,21 @@ class bank_SpendingDocuments extends bank_Document
 
 
     /**
+     * Поле за филтриране по дата
+     */
+    public $filterDateField = 'createdOn,termDateCalc,valior,modifiedOn,activatedOn';
+
+
+    /**
      * Описание на модела
      */
     public function description()
     {
         parent::getFields($this);
         $this->setField('termDate', 'caption=Срок');
+        $this->FLD('earlyPaymentUntil', 'date', 'caption=Отстъпка за предсрочно плащане->Краен срок,input=none,autohide');
+        $this->FLD('earlyPaymentPercent', 'percent(min=0)', 'caption=Отстъпка за предсрочно плащане->Отстъпка,input=none,autohide');
+        $this->XPR('termDateCalc', 'date', 'COALESCE(#earlyPaymentUntil, #termDate)', 'caption=Краен срок');
     }
     
     
@@ -85,6 +94,7 @@ class bank_SpendingDocuments extends bank_Document
     protected static function on_AfterPrepareEditForm($mvc, $res, $data)
     {
         $form = &$data->form;
+        $rec = &$form->rec;
         $today = dt::verbal2mysql();
         
         $contragentId = doc_Folders::fetchCoverId($form->rec->folderId);
@@ -111,12 +121,38 @@ class bank_SpendingDocuments extends bank_Document
         $cData = cls::get($contragentClassId)->getContragentData($contragentId);
         $form->setReadOnly('contragentName', ($cData->person) ? $cData->person : $cData->company);
 
-
         $form->setField('ownAccount', 'caption=От->Сметка,after=reason');
         $form->setField('currencyId', 'caption=От->Валута,after=ownAccount');
         $form->setField('amount', 'caption=От->Заверени,after=reason');
         $form->setField('contragentName', 'caption=Към->Контрагент,after=reason');
         $form->setField('contragentIban', 'caption=Към->Сметка,after=reason');
+
+        // Ако документа е към покупка
+        $firstDoc = doc_Threads::getFirstDocument($rec->threadId);
+        if($firstDoc->isInstanceOf('purchase_Purchases')){
+
+            // И нейния метод за плащане е с отстъпка за предсрочно плащане
+            if($paymentMethodId = $firstDoc->fetchField('paymentMethodId')){
+                $paymentRec = cond_PaymentMethods::fetch($paymentMethodId);
+                if(!empty($paymentRec->discountPercent) || !empty($paymentRec->discountPeriod)){
+                    $form->setField('earlyPaymentUntil', 'input');
+                    $form->setField('earlyPaymentPercent', 'input');
+
+                    // Ако е към входяща фактура да излизат попълнени данните за плащането
+                    if(isset($rec->originId)) {
+                        $originDoc = doc_Containers::getDocument($rec->originId);
+                        if($originDoc->isInstanceOf('purchase_Invoices')){
+                            $form->setDefault('earlyPaymentPercent', $paymentRec->discountPercent);
+                            $form->setDefault('earlyPaymentUntil', dt::addSecs($paymentRec->discountPeriod, $originDoc->fetchField('date')));
+                        } elseif($originDoc->isInstanceOf('purchase_Purchases')){
+                            $form->setDefault('earlyPaymentPercent', $paymentRec->discountPercent);
+                        }
+                    } else {
+                        $form->setDefault('earlyPaymentPercent', $paymentRec->discountPercent);
+                    }
+                }
+            }
+        }
     }
     
     
@@ -153,6 +189,89 @@ class bank_SpendingDocuments extends bank_Document
             
             if (bank_CashWithdrawOrders::haveRightFor('add', (object) array('originId' => $rec->containerId, 'folderId' => $rec->folderId))) {
                 $data->toolbar->addBtn('Нареждане разписка', array('bank_CashWithdrawOrders', 'add', 'originId' => $rec->containerId, 'ret_url' => true, ''), null, 'ef_icon=img/16/nrrz.png,title=Създаване на ново нареждане разписка');
+            }
+        }
+    }
+
+
+    /**
+     * Обработки по вербалното представяне на данните
+     */
+    protected static function on_AfterRecToVerbal($mvc, &$row, $rec, $fields = array())
+    {
+        if(!empty($rec->earlyPaymentUntil) && !empty($rec->earlyPaymentPercent)){
+            if(isset($fields['-single'])) {
+                $row->earlyPaymentInfo = tr("|*<b>{$row->earlyPaymentPercent}</b> |отстъпка при плащане до|* <b>{$row->earlyPaymentUntil}</b>");
+            }
+
+            $valior = $rec->valior ?? dt::today();
+            if($valior > $rec->earlyPaymentUntil){
+                $row->earlyPaymentClass = 'quiet';
+                $row->earlyPaymentInfo .= " (" . tr('изтекло') . ")";
+            } elseif(!Mode::isReadOnly()) {
+                $row->earlyPaymentClass = 'earlyPaymentDiscountActive';
+                $row->earlyPaymentPercent = $row->earlyPaymentPercent ?? $mvc->getFieldType('earlyPaymentPercent')->toVerbal($rec->earlyPaymentPercent);
+
+                // Ако вальора е в срока на предсрочно плащане да се показва с каква сума е намалена
+                $amountWithoutDiscount = round($rec->amount * (1 - $rec->earlyPaymentPercent), 2);
+                $amountWithoutDiscountVerbal = $mvc->getFieldType('amount')->toVerbal($amountWithoutDiscount);
+                $hintColor = in_array($rec->state, array('draft', 'pending')) ? "#3939ef;" : "black";
+                $row->amount = ht::createHint("<span style='color:{$hintColor}'>{$amountWithoutDiscountVerbal}</span>", "Намалена с|* {$row->earlyPaymentPercent} |от|* " . currency_Currencies::decorate($row->amount, $rec->currencyId, true), 'noicon');
+
+                // Ако сме в сингъла да се показва и намалената сума
+                if(isset($fields['-single'])) {
+                    if(!empty($row->amountDeal)){
+                        $amountDealWithoutDiscount = round($rec->amountDeal * (1 - $rec->earlyPaymentPercent), 2);
+                        $amountDealWithoutDiscountVerbal = $mvc->getFieldType('amountDeal')->toVerbal($amountDealWithoutDiscount);
+                        $row->amountDeal = ht::createHint("<span style='color:{$hintColor}'>{$amountDealWithoutDiscountVerbal}</span>", "Намалена с|* {$row->earlyPaymentPercent} |от|* " . currency_Currencies::decorate($row->amountDeal, $rec->dealCurrencyId, true), 'noicon');
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Проверка след изпращането на формата
+     */
+    protected static function on_AfterInputEditForm($mvc, $form)
+    {
+        $rec = &$form->rec;
+
+        if($form->isSubmitted()){
+            if((!empty($rec->earlyPaymentUntil) && empty($rec->earlyPaymentPercent)) || (empty($rec->earlyPaymentUntil) && !empty($rec->earlyPaymentPercent))){
+                $form->setError('earlyPaymentUntil,earlyPaymentPercent', 'Трябва и двете полета за отстъпка при предсрочно плащане да са попълнени');
+            }
+        }
+    }
+
+
+    /**
+     * Подготовка на филтър формата
+     */
+    protected static function on_AfterPrepareListFilter($mvc, $data)
+    {
+        // Ако се филтрира по краен-срок да се показва колоната
+        if($filterDateField = $data->listFilter->rec->filterDateField){
+            $data->query->orderBy($filterDateField, 'DESC', 1);
+            if($filterDateField == 'termDateCalc'){
+                unset($data->listFields['termDate']);
+                arr::placeInAssocArray($data->listFields, array('termDateCalc' => 'Кр. срок'), 'valior');
+            }
+        }
+    }
+
+
+    /**
+     * Метод по подразбиране допълващ полетата за филтриране в съмърито в лист изгледа
+     * @see acc_plg_DocumentSummary
+     */
+    public function fillSummaryRec(&$rec, &$summaryFields)
+    {
+        if(!empty($rec->earlyPaymentUntil)){
+            $valior = $rec->valior ?? dt::today();
+            if($valior <= $rec->earlyPaymentUntil){
+                $rec->amount = round($rec->amount * (1 - $rec->earlyPaymentPercent), 2);
             }
         }
     }
