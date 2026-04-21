b96a3de3e6625cd21de53eee928970844d0b50f5
Mustafov1 <mmustafov3@icloud.com>
2026-04-21 13:54:39 +0300

    Промяна на retUrl (оптимизирано)

--- sales/Sales.class.php
+++ sales/Sales.class.php
@@ -1766,11 +1766,11 @@ class sales_Sales extends deals_DealMaster
         // Създаване на продажба и редирект към добавянето на артикула
         try {
             expect($saleId = sales_Sales::createNewDraft($cover->getInstance(), $cover->that, $fields));
-
-            $quantity = core_Request::get('quantity', 'double');
+            
             $redirectArr = array('sales_SalesDetails', 'add', 'saleId' => $saleId, 'productId' => $productId);
+            $quantity = core_Request::get('quantity', 'double');
             if(!empty($quantity)){
-                $redirectArr = array('sales_SalesDetails', 'add', 'saleId' => $saleId, 'productId' => $productId, 'packQuantity' => $quantity);
+                $redirectArr['packQuantity'] = $quantity;
             }
             redirect($redirectArr);
 
