284b2c2f5a8bef433491126ee1ef32b5c777980d
ivelin1012 <ivelin_pdimov@abv.bg>
2026-04-24 16:03:14 +0300

    ешоп: разширение на логиката за изображенията

--- eshop/Settings.class.php
+++ eshop/Settings.class.php
@@ -262,8 +262,9 @@ class eshop_Settings extends core_Master
         $this->FLD('showParams', 'keylist(mvc=cat_Params,select=typeExt)', 'caption=Показване на е-артикулите във външната част->Общи параметри (Изглед),optionsFunc=cat_Params::getPublic');
         $this->FLD('showProductsWithoutPrices', 'enum(yes=Показване,no=Скриване)', 'caption=Показване на е-артикулите във външната част->Без цени,notNull,value=yes');
         $this->FLD('showListParams', 'keylist(mvc=cat_Params,select=typeExt)', 'caption=Показване на е-артикулите във външната част->Общи параметри (Списък),optionsFunc=cat_Params::getPublic');
-
         $this->FLD('showPacks', 'keylist(mvc=cat_UoM,select=name)', 'caption=Показване на е-артикулите във външната част->Опаковки/Мерки');
+        $this->FLD('imageDisplayType', 'enum(standart=Дефолтен,carousel=Каросел)', 'caption=Показване на е-артикулите във външната част->Изображения (преглед),notNull,value=standart');
+
         $this->FLD('enableCart', 'enum(yes=Винаги,no=Ако съдържа артикули)', 'caption=Показване на количката във външната част->Показване,notNull,value=no');
         $this->FLD('cartName', 'varchar(16)', 'caption=Показване на количката във външната част->Надпис');
         $this->FLD('locationIsMandatory', 'enum(no=Опционална,yes=Задължителна)', 'caption=Настройки на партньори за онлайн магазина->Локация,notNull,value=no');
