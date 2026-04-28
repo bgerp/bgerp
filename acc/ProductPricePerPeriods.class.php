820ab9e5e85417b5371fe3c656547fd298d36a44
arsov <arsov@experta.bg>
2026-04-28 10:36:07 +0300

    Опит за добавяне възможност за изтриване на лоши записи

--- acc/ProductPricePerPeriods.class.php
+++ acc/ProductPricePerPeriods.class.php
@@ -44,6 +44,12 @@ class acc_ProductPricePerPeriods extends core_Manager
      * Кой може да го разглежда?
      */
     public $canList = 'debug';
+    
+
+    /**
+     * Кой може да го изтрие?
+     */
+    public $canDelete = 'debug';
 
 
     /**
