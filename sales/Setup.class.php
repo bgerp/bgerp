bedde916404f172e3f86a187231486f41448a3db
ivelin1012 <ivelin_pdimov@abv.bg>
2026-05-08 15:00:01 +0300

    махнати уеб излишни вече уеб константи: #C79646

--- sales/Setup.class.php
+++ sales/Setup.class.php
@@ -207,14 +207,10 @@ defIfNot('SALES_SHOW_PRICE_IN_PRODUCT_SELECTION', 'no');
 
 
 /**
- * Дни след "Ден от месеца за изчисляване на Счетоводна дата на входяща фактура" за приключване на валутни сделки
- */
-defIfNot('SALES_CURRENCY_CLOSE_AFTER_ACC_DATE', '5');
-
-/**
  * Дефолтен текст на циркулярен имейл за просрочени фактури
  */
 defIfNot('SALES_DEFAULT_BLAST_BODY_BG',  "Здравейте,\nС настоящото бихме желали да Ви обърнем внимание, че към [#date#] година имате просрочия по фактури:\n[#docs#]\nна обща стойност [#sum#] [#currency#]\nМолим във възможно най-кратки срокове да погасите цитираното задължение.\nВ случай, че вече сте направили плащане по посочените документи, молим да ни извините и да игнорирате настоящото писмо.\n\nТова е автоматично генерирано съобщение, което не изисква отговор. За допълнителни въпроси и информация моля, свържете се с наш представител.\n\nС уважение");
+
 
 /**
  * Дефолтен subject на циркулярен имейл за просрочени фактури
@@ -222,10 +218,12 @@ defIfNot('SALES_DEFAULT_BLAST_BODY_BG',  "Здравейте,\nС настоящ
 defIfNot('SALES_DEFAULT_BLAST_SUBJECT_BG', "Просрочени задължения към [#date#]
 ");
 
+
 /**
  * Дефолтен текст на циркулярен имейл за просрочени фактури на английски
  */
 defIfNot('SALES_DEFAULT_BLAST_BODY_EN',  "Hello,\nThis letter is to bring to your attention that as of [#date#] no payment has been received for the invoices\nlisted below [#docs#],\ntotal value [#sum#] [#currency#].\n\nWe kindly ask you to promptly settle the outstanding invoices.\n\nIn case you have already made payment, please excuse us and ignore this reminder.\n\nThis letter is automatically generated that does not require a response. For additional questions and information, please contact our representative.\n\nWith respect");
+
 
 /**
  * Дефолтен subject на циркулярен имейл за просрочени фактури на английски
@@ -331,10 +329,6 @@ class sales_Setup extends core_ProtoSetup
         'SALE_CLOSE_OLDER_THAN' => array(
             'time(uom=days,suggestions=1 ден|2 дена|3 дена)',
             'caption=Изчакване преди автоматично приключване на продажби в BGN / EUR->Дни'
-        ),
-        'SALES_CURRENCY_CLOSE_AFTER_ACC_DATE' => array(
-            'int(Min=0)',
-            'caption=Дни след "Ден от месеца за изчисляване на Счетоводна дата на изходяща фактура" за приключване на валутни сделки->Дни'
         ),
         'SALE_CLOSE_OLDER_NUM' => array(
             'int',
