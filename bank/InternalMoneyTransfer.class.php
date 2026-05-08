60e57cb029aa2e01b32e82f0d559ffad8ed823f4
ivelin1012 <ivelin_pdimov@abv.bg>
2026-05-08 10:37:46 +0300

    Касови и банкови трансфери и обмени: винаги да се минава през 481-а сметка

--- bank/InternalMoneyTransfer.class.php
+++ bank/InternalMoneyTransfer.class.php
@@ -354,7 +354,7 @@ class bank_InternalMoneyTransfer extends core_Master
             }
 
             if($rec->currencyId != $defaultDebitId){
-                $form->setError('debitBank', "Входящата сметка не е в избраната валута към този вальор|* <b>{$dateVerbal}!");
+                $form->setError('debitBank', "Входящата сметка не е в избраната валута към този вальор|* <b>{$dateVerbal}</b>!");
             }
         } elseif ($rec->operationSysId == 'bank2case') {
             $caseRec = cash_Cases::fetch($rec->debitCase);
--- bank/transaction/InternalMoneyTransfer.class.php
+++ bank/transaction/InternalMoneyTransfer.class.php
@@ -37,26 +37,25 @@ class bank_transaction_InternalMoneyTransfer extends acc_DocumentTransactionSour
     {
         // Извличаме записа
         expect($rec = $this->class->fetchRec($id));
-        
-        ($rec->debitCase) ? $debitArr = array('cash_Cases', $rec->debitCase) : $debitArr = array('bank_OwnAccounts', $rec->debitBank);
-        
-        $entry = array('debit' => array($rec->debitAccId,
-            $debitArr,
-            array('currency_Currencies', $rec->currencyId),
-            'quantity' => $rec->amount),
-        'credit' => array($rec->creditAccId,
-            array('bank_OwnAccounts', $rec->creditBank),
-            array('currency_Currencies', $rec->currencyId),
-            'quantity' => $rec->amount));
-        $entry = array($entry);
-        
         $rec->valior = empty($rec->valior) ? dt::today() : $rec->valior;
-        
+
+        $cCode = currency_Currencies::getCodeById($rec->currencyId);
+        $amount = currency_CurrencyRates::convertAmount($rec->amount, $rec->valior, $cCode);
+        $debitArr = ($rec->debitCase) ? array('cash_Cases', $rec->debitCase) : array('bank_OwnAccounts', $rec->debitBank);
+        $entries = array();
+        $entries[] = array('amount' => $amount, 'debit' => array($rec->debitAccId, $debitArr, array('currency_Currencies', $rec->currencyId),
+                                        'quantity' => $rec->amount),
+                            'credit' => array('481', array('currency_Currencies', $rec->currencyId), 'quantity' => $rec->amount));
+
+        $entries[] = array('debit' => array('481', array('currency_Currencies', $rec->currencyId), 'quantity' => $rec->amount),
+                           'credit' => array($rec->creditAccId, array('bank_OwnAccounts', $rec->creditBank), array('currency_Currencies', $rec->currencyId),
+                                            'quantity' => $rec->amount));
+
         // Подготвяме информацията която ще записваме в Журнала
         $result = (object) array(
             'reason' => $rec->reason,   // основанието за ордера
             'valior' => $rec->valior,   // датата на ордера
-            'entries' => $entry,
+            'entries' => $entries,
         );
         
         return $result;
