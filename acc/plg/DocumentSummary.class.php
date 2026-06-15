35ad82dc8e47e1ac0a4c18d905072343be5c6dfe
Mustafov1 <mmustafov3@icloud.com>
2026-06-15 10:46:18 +0300

    php 8.2

--- acc/plg/DocumentSummary.class.php
+++ acc/plg/DocumentSummary.class.php
@@ -289,7 +289,7 @@ class acc_plg_DocumentSummary extends core_Plugin
             // Филтър по "Наша фирма", ако е инсталиран пакета за многофирменост
             $mvc->invoke('afterGetDocumentSummaryListFields', array(&$data));
             $data->listFilter->FNC('users', "users(rolesForAll={$mvc->filterRolesForAll},rolesForTeams={$mvc->filterRolesForTeam}, showClosedGroups)", 'caption=Потребители,silent,autoFilter,remember');
-            if($mvc->showFilterFolderField){
+            if(!empty($mvc->showFilterFolderField)){
                 $data->listFilter->FNC('folder', 'key2(mvc=doc_FoldersProxy, allowEmpty, selectSourceArr=doc_Folders::getSelectArr, forceProxy)', 'caption=Папка,silent,after=users');
                 $data->listFilter->showFields .= ',folder';
             }
