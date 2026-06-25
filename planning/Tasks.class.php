307d39aea2dada6b5a864cd905824d704244e8db
Mustafov1 <mmustafov3@icloud.com>
2026-06-25 13:13:54 +0300

    Добавено поле за избор за показване на планиращи параметри в производствен Етап

--- planning/Tasks.class.php
+++ planning/Tasks.class.php
@@ -2212,7 +2212,7 @@ class planning_Tasks extends core_Master
         } else {
             $tpl = $contentTpl;
         }
-
+ 
         // Връщаме шаблона
         return $tpl;
     }
@@ -3133,7 +3133,25 @@ class planning_Tasks extends core_Master
 
         // Ако има намерени планиращи параметри - показват се в таблицата
         $firstColumnsIfNotSelected = arr::make(array_keys($data->listFields), true);
+    
+        // Параметрите от Етапите да са планиращи
+        if(!empty($data->listFilter->rec->productId)){
+            $productId = $data->listFilter->rec->productId;
+            if($Driver = cat_Products::getDriver($productId)){
+                $productionData = $Driver->getProductionData($productId);
 
+                $centerId = $productionData['centerId'];
+                $center = planning_Centers::fetch($centerId);
+                $showTaskPlanningParams = $center->showTaskPlanningParams;
+
+                if($showTaskPlanningParams == 'yes'){
+                    $plannedParams = keylist::toArray($center->planningParams);
+                } elseif($showTaskPlanningParams == 'yesAdd'){
+                    $plannedParams += keylist::toArray($center->planningParams);
+                }
+            }
+        }
+       
         if (countR($plannedParams)) {
             $pQuery = cat_Params::getQuery();
             $pQuery->in('id', $plannedParams);
