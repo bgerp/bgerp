b079b692d796fc2f8150ef51fec8474459cb543f
Dimitar Minekov <dminekov@abv.bg>
2026-07-05 08:11:21 +0300

    deals_DealDetail: guard $masterRec->priceListId (липсва за purchase_Purchases) (PHP 8.2)

--- deals/DealDetail.class.php
+++ deals/DealDetail.class.php
@@ -226,7 +226,7 @@ abstract class deals_DealDetail extends doc_Detail
         }
 
         // Добавяне да се показва ценовата информация за стандартните артикули
-        $priceData = array('valior' => $masterRec->valior, 'rate' => $masterRec->currencyRate, 'chargeVat' => $masterRec->chargeVat, 'listId' => $masterRec->priceListId, 'currencyId' => $masterRec->currencyId, 'threadId' => $masterRec->threadId);
+        $priceData = array('valior' => $masterRec->valior, 'rate' => $masterRec->currencyRate, 'chargeVat' => $masterRec->chargeVat, 'listId' => $masterRec->priceListId ?? null, 'currencyId' => $masterRec->currencyId, 'threadId' => $masterRec->threadId);
         $form->setFieldTypeParams('productId', array('customerClass' => $masterRec->contragentClassId, 'customerId' => $masterRec->contragentId, 'hasProperties' => $mvc->metaProducts, 'hasnotProperties' => 'generic', 'priceData' => $priceData));
 
         if (empty($rec->id)) {
