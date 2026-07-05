14309373f74ef34c7103b92e57d1b1c69491e59d
Dimitar Minekov <dminekov@abv.bg>
2026-07-05 11:17:14 +0300

    forum_Postings: guard undefined postings when topic has no comments

--- forum/Postings.class.php
+++ forum/Postings.class.php
@@ -577,7 +577,7 @@ class forum_Postings extends core_Detail
         $tpl->placeObject($data->row);
         
         // Ако има коментари ние ги рендираме
-        if (countR($data->postings)) {
+        if (countR($data->postings ?? null)) {
             $cloneTpl = clone($detailsTpl);
             
             foreach ($data->postings as $row) {
