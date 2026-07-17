ad6e459bf9c4c40420e4f761d822dc3d54ad97fd
Dimitar Minekov <mitko@experta.bg>
2026-07-17 15:31:22 +0300

    php 8.2 - 21

--- plg/GroupByField.class.php
+++ plg/GroupByField.class.php
@@ -67,9 +67,7 @@ class plg_GroupByField extends core_Plugin
             $rowAttr = array();
             
             // Създаваме по един ред с името му, разпънат в цялата таблица
-            if (strstr($rowAttr['class'], 'group-by-field-row') === false) {
-                $rowAttr['class'] .= ' group-by-field-row';
-            }
+            $rowAttr['class'] = 'group-by-field-row';
             
             if(array_key_exists($field, $originalFields)){
                 $rows['|' . $groupId] = ht::createElement(
