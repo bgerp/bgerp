f9b84d13f1aebd8ff235c37afbbd25d6115aa294
Dimitar Minekov <mitko@experta.bg>
2026-07-17 17:11:53 +0300

    php 8.2 - 38

--- blast/ListDetails.class.php
+++ blast/ListDetails.class.php
@@ -207,7 +207,8 @@ class blast_ListDetails extends doc_Detail
      */
     public static function on_AfterPrepareEditForm($mvc, &$res, $data)
     {
-        if ($bData = $data->form->rec->data) {
+        if (!empty($data->form->rec->data)) {
+            $bData = $data->form->rec->data;
             $fieldsArr = $mvc->getFncFieldsArr($data->masterRec->allFields);
             
             $bData = unserialize($bData);
