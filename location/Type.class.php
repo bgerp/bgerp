49cd91f8ed54b6276dd7b35fcbbce9c70f73312a
Dimitar Minekov <mitko@experta.bg>
2026-07-21 15:05:59 +0300

    Fix role checks to ensure $rec is an object and update default width/height handling

--- location/Type.class.php
+++ location/Type.class.php
@@ -125,8 +125,8 @@ class location_Type extends type_Varchar
         
         $id = 'map' . $n;
         
-        setIfNot($width, $this->params['width'], 400);
-        setIfNot($height, $this->params['height'], 400);
+        setIfNot($width, $this->params['width'] ?? null, 400);
+        setIfNot($height, $this->params['height'] ?? null, 400);
         
         $conf = core_Packs::getConfig('google');
         $apiKey = $conf->GOOGLE_API_KEY;
