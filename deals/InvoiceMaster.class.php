3b8f5422fac09712a326cc945348ae43b5886573
ivelin <ivo@experta.bg>
2026-07-29 18:46:26 +0300

    проформи пхп 8.2

--- deals/InvoiceMaster.class.php
+++ deals/InvoiceMaster.class.php
@@ -2331,7 +2331,7 @@ abstract class deals_InvoiceMaster extends core_Master
      */
     public function getContragentCoverFieldsToUpdate($rec)
     {
-        if($rec->state == 'rejected') return array();
+        if(($rec->state ?? null) == 'rejected' || !isset($rec->folderId, $rec->contragentName)) return array();
 
         $Cover = doc_Folders::getCover($rec->folderId);
         Mode::push('htmlEntity', 'none');
