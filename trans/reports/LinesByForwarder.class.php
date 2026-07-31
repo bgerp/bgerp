a6a82208a98f87cb1217968ca4d41be0bff2e62d
ivelin1012 <ivelin_pdimov@abv.bg>
2026-07-31 16:48:16 +0300

    Данни от транспортна линия:bugfix php 8.2

--- trans/reports/LinesByForwarder.class.php
+++ trans/reports/LinesByForwarder.class.php
@@ -75,7 +75,7 @@
          
          
          $suggestions = array();
-         foreach (keylist::toArray($rec->forwarderPersonId ?? null) as $val) {
+         foreach (keylist::toArray($rec->forwarderPersonId) as $val) {
              $suggestions[$val] = crm_Persons::fetch($val)->name;
          }
          
