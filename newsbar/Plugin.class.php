ef87d086192e9cd70617b2810da165358e843ad8
Dimitar Minekov <mitko@experta.bg>
2026-08-17 15:29:24 +0300

    php 8.2 fix 4

--- newsbar/Plugin.class.php
+++ newsbar/Plugin.class.php
@@ -275,7 +275,7 @@ class newsbar_Plugin extends core_Plugin
         static $resArr = array();
         $hash = md5(serialize($nRec) . '|' . $class);
         
-        if (!$resArr[$hash]) {
+        if (empty($resArr[$hash])) {
             $convertText = cls::get('type_Richtext');
             
             $html = newsbar_News::generateHTML($nRec);
