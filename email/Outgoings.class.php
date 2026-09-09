09501c2644d92c7d39aedd061869cd7a4a18f861
Mustafov1 <mmustafov3@icloud.com>
2026-09-09 11:37:33 +0300

    Премахнах индекс

--- email/Outgoings.class.php
+++ email/Outgoings.class.php
@@ -236,7 +236,6 @@ class email_Outgoings extends core_Master
         $this->FLD('address', 'varchar', 'caption=Адресат->Адрес,class=contactData,changable');
         
         $this->setDbIndex('createdOn');
-        //$this->setDbIndex('lastSendedOn,autoReplyRuleId');
     }
     
     
