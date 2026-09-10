4bf17631c284fd6b0148eb4e32abba022fb66cde
Yusein <yyuseinov@gmail.com>
2026-09-10 17:37:42 +0300

    drdata_Setup - изчистване на phoneCache

--- drdata/Setup.class.php
+++ drdata/Setup.class.php
@@ -76,7 +76,7 @@ class drdata_Setup extends core_ProtoSetup
     /**
      * Версия на пакета
      */
-    public $version = '0.16';
+    public $version = '0.1';
     
     
     /**
@@ -132,15 +132,15 @@ class drdata_Setup extends core_ProtoSetup
         'drdata_Languages',
         'drdata_bg_Places',
         'drdata_CanonizedStrings',
-        'migrate::fixPhoneCacheGroupedNumbers2637'
+        'migrate::fixPhoneCachePartialNumbers2637'
     );
 
 
     /**
-     * Изчиства кеша на номерата с регионален код по подразбиране
+     * Изчиства и старите частични резултати без регионален код по подразбиране
      */
-    public static function fixPhoneCacheGroupedNumbers2637()
+    public static function fixPhoneCachePartialNumbers2637()
     {
-        drdata_PhoneCache::delete("#dAC != ''");
+        drdata_PhoneCache::delete('1=1');
     }
 }
