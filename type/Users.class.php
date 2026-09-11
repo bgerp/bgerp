29499ebed345f0138fa2562209b62ae8c1d65b37
Dimitar Minekov <mitko@experta.bg>
2026-09-11 16:44:33 +0300

    bugfix оттеглени потребители

--- type/Users.class.php
+++ type/Users.class.php
@@ -255,17 +255,15 @@ class type_Users extends type_Keylist
                         $this->options[$key]->title = $uRec->nick . ' (' . $uRec->names . ')';
                         $this->options[$key]->keylist = '|' . $uId . '|';
                         $haveTeamMembers = true;
+
+                        if ($this->params['cuFirst'] == 'yes' && empty($cuRecArr) && ($uId == $cu)) {
+                            $cuRecArr[$key] = $this->options[$key];
+                        }
                     } else {
                         $rejected .= $rejected ? '|' . $uId : $uId;
                     }
-                    
+
                     $teamMembers .= $teamMembers ? '|' . $uId : $uId;
-                    
-                    if ($this->params['cuFirst'] == 'yes' && empty($cuRecArr)) {
-                        if ($this->options[$key] && ($uId == $cu)) {
-                            $cuRecArr[$key] = $this->options[$key];
-                        }
-                    }
                 }
                 
                 if ($haveTeamMembers) {
